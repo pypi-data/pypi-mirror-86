d5218c65ff7b57c3ab51c2e360a508f40107467d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:20 +0000

    Add package zort/1.5/zort-1.5-py3-none-any.whl

---
+++ packages/zort/zort-1.5-py3-none-any.whl/zort/initialize.py
@@ -0,0 +1,121 @@
+#! /usr/bin/env python
+"""
+initialize.py
+Initialization of object and rcip_map files.
+"""
+
+
+import glob
+import os
+import pickle
+from collections import defaultdict
+import numpy as np
+from scipy.spatial import cKDTree
+
+
+def gather_lightcurve_files(dataDir):
+    fis = [f for f in glob.glob('%s/field*.txt' % dataDir) if
+           not os.path.exists(f.replace('.txt', '.rcid_map'))]
+    fis.sort()
+    return fis
+
+
+def generate_objects_file(lightcurve_file):
+    f_in = open(lightcurve_file, 'r')
+
+    object_keys = ['id', 'nepochs', 'filterid',
+                   'fieldid', 'rcid', 'ra', 'dec', 'lightcurve_position']
+
+    objects_file = lightcurve_file.replace('.txt', '.objects')
+    with open(objects_file, 'w') as f_out:
+        f_out.write('%s\n' % ','.join(object_keys))
+
+        while True:
+            line = f_in.readline()
+
+            # Check for end of the file
+            if not line:
+                break
+
+            # Only want to look at object lines
+            if not line.startswith('#'):
+                continue
+
+            data = line.replace('\n', '').split()[1:]
+            data_str = ','.join(data)
+
+            lightcurve_position = f_in.tell() - len(line)
+            f_out.write('%s,%i\n' % (data_str, lightcurve_position))
+
+    f_in.close()
+
+
+def save_rcid_map(DR1_object_file, rcid_map):
+    rcid_map_filename = DR1_object_file.replace('.objects', '.rcid_map')
+    with open(rcid_map_filename, 'wb') as fileObj:
+        pickle.dump(rcid_map, fileObj)
+
+
+def return_rcid_map_size(rcid_map):
+    rcid_map_size = 0
+    for _, v in rcid_map.items():
+        rcid_map_size += len(v)
+    return rcid_map_size
+
+
+def return_rcid_map_filesize(rcid_map):
+    rcid_map_filesize = 0
+    for k, _ in rcid_map.items():
+        for _, t in rcid_map[k].items():
+            rcid_map_filesize += t[1] - t[0]
+    return rcid_map_filesize
+
+
+def generate_rcid_map(lightcurve_file):
+    objects_file = lightcurve_file.replace('.txt', '.objects')
+
+    f_in = open(objects_file, 'r')
+    _ = f_in.readline()  # skip past the header
+
+    rcid, rcid_current = None, None
+    filterid, filterid_current = None, None
+    ra_arr, dec_arr, lightcurve_position_arr = [], [], []
+    rcid_map = defaultdict(dict)
+
+    for line in f_in:
+        data = line.replace('\n', '').split(',')
+
+        rcid = int(data[4])
+        filterid = int(data[2])
+
+        # Initialize the rcid
+        if rcid_current is None:
+            rcid_current = rcid
+            filterid_current = filterid
+
+        # Check to see if the block has switched
+        if rcid != rcid_current:
+            objects = np.array([ra_arr, dec_arr]).T
+            kdtree = cKDTree(objects)
+            rcid_map[filterid_current][rcid_current] = \
+                (kdtree, lightcurve_position_arr)
+
+            rcid_current = rcid
+            filterid_current = filterid
+            ra_arr, dec_arr, lightcurve_position_arr = [], [], []
+
+        ra, dec = float(data[5]), float(data[6])
+        lightcurve_position = int(data[-1])
+        ra_arr.append(ra)
+        dec_arr.append(dec)
+        lightcurve_position_arr.append(lightcurve_position)
+
+    objects = np.array([ra_arr, dec_arr]).T
+    kdtree = cKDTree(objects)
+    rcid_map[filterid_current][rcid_current] = \
+        (kdtree, lightcurve_position_arr)
+
+    f_in.close()
+
+    rcid_map_file = objects_file.replace('.objects', '.rcid_map')
+    save_rcid_map(rcid_map_file, rcid_map)
---
+++ packages/zort/zort-1.5-py3-none-any.whl/zort/lightcurveFile.py
@@ -0,0 +1,163 @@
+#! /usr/bin/env python
+"""
+lightcurveFile.py
+ZTF objects are stored in lightcurve files. This class allows for
+iteration through a lightcurve file in order to inspect each object's
+lightcurve. Filters can be applied to objects and/or lightcurves.
+"""
+
+import pickle
+from zort.object import Object
+from zort.utils import return_filename
+from zort.utils import return_objects_filename
+from zort.utils import return_rcid_map_filename
+from zort.radec import return_rcid
+
+
+################################
+#                              #
+#  LightcurveFile Class        #
+#                              #
+################################
+
+class LightcurveFile:
+    """
+    ZTF objects are stored in lightcurve files. This class allows for
+    iteration through a lightcurve file in order to inspect each object's
+    lightcurve. The user can select to start looping through the file at a
+    specific object position.
+
+    LightcurveFile should be used as an iterator to loop over all of the
+    objects in the lightcurve file. Here is an example for how to
+    (1) filter through the lightcurves in a lightcurve file and (2) recover
+    the lightcurves of objects that pass a customized filter.
+
+    The Object class (object.py) and the Lightcurve class (lightcurve.py) list
+    out the attributes that can be run through the filter. These attributes
+    include the object's number of epochs, filter, and sky location. Filters
+    will almost certainly want to filter on the object's lightcurve, which
+    contains the observation dates, magnitudes and magnitude errors.
+
+    EXAMPLE 1:
+
+    filename = 'field000000_lightcurve_file_example.txt'
+    interesting_objects = []
+
+    from zort.lightcurveFile import LightcurveFile
+    for obj in LightcurveFile(filename):
+        if my_interesting_filter(obj):
+            interesting_objects.append(obj)
+
+    from zort.objcet import save_obejcts
+    save_objects('objects.list', interesting_objects)
+
+    from zort.object import load_objects
+    interesting_objects = load_objects('objects.list')
+    for obj in interesting_objects:
+        print(obj)
+        print(obj.lightcurve)
+
+    EXAMPLE 2:
+
+    from mpi4py import MPI
+    comm = MPI.COMM_WORLD
+    rank = comm.rank
+    size = comm.size
+
+    filename = 'field000000_lightcurve_file_example.txt'
+    interesting_objects = []
+
+    from zort.lightcurveFile import LightcurveFile
+    for obj in LightcurveFile(filename, proc_rank=rank, proc_size=size):
+        if my_interesting_filter(obj):
+            interesting_objects.append(obj)
+
+    from zort.object import save_objects
+    save_objects('objects.%i.list' % rank, interesting_objects)
+    """
+
+    def __init__(self, filename, init_object_position=60,
+                 proc_rank=0, proc_size=1, apply_catmask=True):
+        # Load filenames and check for existence
+        self.filename = return_filename(filename)
+        self.objects_filename = return_objects_filename(filename)
+        self.rcid_map_filename = return_rcid_map_filename(filename)
+
+        self.init_object_position = init_object_position
+        self.objects_file = self.return_objects_file()
+        self.rcid_map = self.load_rcid_map()
+        self.proc_rank = proc_rank
+        self.proc_size = proc_size
+        self.objects_file_counter = 0
+        self.apply_catmask = apply_catmask
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while self.objects_file_counter % self.proc_size != self.proc_rank:
+            line = self._return_objects_file_line()
+            if line == '':
+                raise StopIteration
+
+        line = self._return_objects_file_line()
+        if line == '':
+            raise StopIteration
+        return self.return_object(line)
+
+    def __exit__(self):
+        self.objects_file.close()
+
+    def _return_objects_file_line(self):
+        line = self.objects_file.readline()
+        self.objects_file_counter += 1
+        return line
+
+    def _return_parsed_line(self, line):
+        return line.replace('\n', '').split(',')
+
+    def return_filename(self, filename):
+        try:
+            filename = filename.decode()
+        except AttributeError:
+            pass
+
+        if filename is None:
+            raise FileNotFoundError(filename)
+
+        return filename
+
+    def return_object(self, line):
+        lightcurve_position = self._return_parsed_line(line)[-1]
+        return Object(self.filename, lightcurve_position,
+                      apply_catmask=self.apply_catmask,
+                      rcid_map=self.rcid_map)
+
+    def return_objects_file(self):
+        file = open(self.objects_filename, 'r')
+        file.seek(self.init_object_position)
+        return file
+
+    def load_rcid_map(self):
+        rcid_map = pickle.load(open(self.rcid_map_filename, 'rb'))
+        return rcid_map
+
+    def locate_objects_by_radec(self, ra, dec, rcid=None, radius_as=2):
+        if rcid is None:
+            fieldid = int(self.filename.split('_')[0].replace('field', ''))
+            rcid = return_rcid(fieldid, ra, dec)
+            if rcid is None:
+                return
+
+        radius_deg = radius_as / 3600.
+        objects = []
+        for filterid in self.rcid_map:
+            kdtree, lightcurve_position_arr = self.rcid_map[filterid][rcid]
+            idx_arr = kdtree.query_ball_point((ra, dec), radius_deg)
+            if len(idx_arr) == 0:
+                continue
+            for idx in idx_arr:
+                lightcurve_position = int(lightcurve_position_arr[idx])
+                obj = Object(self.filename, lightcurve_position)
+                objects.append(obj)
+        return objects
