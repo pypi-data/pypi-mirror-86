7a8d459545f1b3a3b0f05aa3afafca2f94105589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:34 +0000

    Add package plover-start-words/0.0.10/plover_start_words-0.0.10.tar.gz

---
+++ packages/plover-start-words/plover_start_words-0.0.10.tar.gz/plover_start_words-0.0.10/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+	name='plover_start_words',
+	version='0.0.10',
+	description='Plugins for assigning strokes to different output if it starts or not a word',
+	long_description='file: README.md',
+	author='Noelia Ruiz',
+	author_email='nrm1977@gmail.com',
+	license='GNU General Public License v3 or later (GPLv3+)',
+	url='https://github.com/nvdaes/plover_start_words',
+	classifiers=[
+		'Development Status :: 1 - Planning',
+		'Environment :: Plugins',
+		'Intended Audience :: End Users/Desktop',
+		'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.6',
+	],
+	keywords='plover plover_plugin',
+)
