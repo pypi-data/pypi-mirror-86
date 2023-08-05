165d019beba48020c4cb5f722012e5657b70697c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:19 +0000

    Add package piimp/0.0.0/piimp-0.0.0-py3-none-any.whl

---
+++ packages/piimp/piimp-0.0.0-py3-none-any.whl/piimp/__init__.py
@@ -0,0 +1,5 @@
+"""
+This is the Python-based Ice Irradiation Module for Postprocessing (PIIMP)
+
+Author: Brian C. Ferrari 
+"""
---
+++ packages/piimp/piimp-0.0.0-py3-none-any.whl/piimp/esd/__init__.py
@@ -0,0 +1,7 @@
+"""
+This is the Electron Stimulated Desorption sub-module for PIIMP.
+
+Author: Brian C. Ferrari
+"""
+
+from .config import *
