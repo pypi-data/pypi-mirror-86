36a894f899dd2488caf3d76a434f006ab5e55889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:12 +0000

    Add package giigd/0.0.5/giigd-0.0.5.tar.gz

---
+++ packages/giigd/giigd-0.0.5.tar.gz/giigd-0.0.5/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding="utf-8") as f:
+    readme = f.read()
+
+setup(
+    name='giigd',
+    version='0.0.5',
+    author='Dion',
+    author_email='idion@idion.dev',
+    url='https://github.com/ksundong/gitignore-cli-py',
+    install_requires=[
+        'click',
+        'fuzzywuzzy',
+        'python-Levenshtein',
+        'requests',
+    ],
+    keywords=[
+        'gitignore'
+    ],
+    python_requires='>=3',
+    description='gitignore cli',
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "giig = giig.main:cli"
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    long_description=readme,
+    long_description_content_type='text/markdown',
+)
