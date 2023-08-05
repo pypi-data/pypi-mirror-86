2b8685580ba2f14be3af12f66c61316d5edf77c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:20 +0000

    Add package TestLibrary1/0.0.1/TestLibrary1-0.0.1-py3-none-any.whl

---
+++ packages/TestLibrary1/TestLibrary1-0.0.1-py3-none-any.whl/TestLibrary1/__init__.py
@@ -0,0 +1,12 @@
+# _*_ coding: utf-8 _*_
+# .@Author  :__init__
+# .@time    :2020/11/19 17:43
+
+from .test_module3 import TestModule3
+
+
+class TestLibrary1(TestModule3):
+    """
+    test library
+    """
+    pass
---
+++ packages/TestLibrary1/TestLibrary1-0.0.1-py3-none-any.whl/TestLibrary1/test_module3.py
@@ -0,0 +1,13 @@
+
+
+class TestModule3(object):
+
+    def __init__(self):
+        pass
+
+    def test_func(self, para1=None, para2=None):
+        return para1, para2
+
+
+
+
