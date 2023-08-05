29c71ac3155e7cb4ea4a1a637f2ba30809b1029a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:27 +0000

    Add package async-btree/1.1.0/async_btree-1.1.0-py3-none-any.whl

---
+++ packages/async-btree/async_btree-1.1.0-py3-none-any.whl/async_btree/leaf.py
@@ -0,0 +1,53 @@
+"""Leaf definition."""
+
+from .decorator import is_success
+from .definition import AsyncInnerFunction, CallableFunction, ControlFlowException, node_metadata
+from .utils import to_async
+
+__all__ = ['action', 'condition']
+
+
+def action(target: CallableFunction, **kwargs) -> AsyncInnerFunction:
+    """Declare an action leaf.
+
+    Action is an awaitable closure of specified function,
+    (See alias function).
+
+    Args:
+        target (CallableFunction): awaitable function
+        kwargs: optional kwargs argument to pass on target function
+
+    Returns:
+        (AsyncInnerFunction): an awaitable function.
+
+    Raises:
+        ControlFlowException : if error occurs
+
+    """
+
+    _target = to_async(target)
+
+    @node_metadata(properties=['_target'])
+    async def _action():
+        try:
+            return await _target(**kwargs)
+        except Exception as e:
+            raise ControlFlowException.instanciate(e)
+
+    return _action
+
+
+def condition(target: CallableFunction, **kwargs) -> AsyncInnerFunction:
+    """Declare a condition leaf.
+
+    Condition is an awaitable closure of specified function.
+
+    Args:
+        target (CallableFunction):  awaitable function which be evaluated as True/False.
+        kwargs: optional kwargs argument to pass on target function
+
+    Returns:
+        (AsyncInnerFunction): an awaitable function.
+    """
+
+    return node_metadata(name='condition', properties=['target'])(is_success(action(target=target, **kwargs)))
