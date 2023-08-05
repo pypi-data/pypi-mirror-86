b80c3b0583a749d4e7ad14ded2551af047e9c19f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:15 +0000

    Add package ubiops-cli/2.0.0/ubiops-cli-2.0.0.tar.gz

---
+++ packages/ubiops-cli/ubiops-cli-2.0.0.tar.gz/ubiops-cli-2.0.0/pkg/constants.py
@@ -0,0 +1,11 @@
+STRUCTURED_TYPE = 'structured'
+ML_MODEL_FILE_NAME_KEY = "ML_MODEL_FILE_NAME"
+ML_MODEL_FILE_NAME_VALUE = "model"
+SYS_DEPLOYMENT_FILE_NAME_KEY = "SYS_DEPLOYMENT_FILE_NAME"
+SYS_DEPLOYMENT_FILE_NAME_VALUE = "deployment"
+
+STATUS_INITIALISED = 'initialised'
+SUCCESS_STATUSES = ['completed', 'available', 'success']
+WARNING_STATUSES = ['pending', 'processing', 'building', 'available_building', 'deploying', 'running']
+ERROR_STATUSES = ['failed', 'building_failed', 'available_building_failed', 'deploying_failed']
+DEFAULT_IGNORE_FILE = '.ubiops-ignore'
