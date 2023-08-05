80b1448ca214babcbb51fd6e93d9a1e49ae38fe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:59 +0000

    Add package temp-to-rgbw/0.0.4/temp-to-rgbw-0.0.4.tar.gz

---
+++ packages/temp-to-rgbw/temp-to-rgbw-0.0.4.tar.gz/temp-to-rgbw-0.0.4/README.md
@@ -0,0 +1,8 @@
+# TemperatureToRGBW
+Convert a Kelvin Temperature to RGBW
+
+Usage:
+```python
+from temptorgbw_pkg import TemperatureToRGBW
+r,g,b,w = TemperatureToRGBW.kelvin_to_rgbw(5000)
+```
---
+++ packages/temp-to-rgbw/temp-to-rgbw-0.0.4.tar.gz/temp-to-rgbw-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md","r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name = "temp-to-rgbw",
+    version = "0.0.4",
+    author = "Hypercookie",
+    author_email="hypercookie@gmx.de",
+    description="Converts a Kelvin Temperatur to RBGW",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/TwoSolutions/TemperaturToRGBW",
+    packages = setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requres='>3.6',
+    install_requires=["numpy"]
+)
