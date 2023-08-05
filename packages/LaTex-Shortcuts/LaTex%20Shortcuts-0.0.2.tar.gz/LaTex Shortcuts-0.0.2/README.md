91e6e139ff8f9caad17d6590495d67db19d38e80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:23 +0000

    Add package LaTex-Shortcuts/0.0.2/LaTex%20Shortcuts-0.0.2.tar.gz

---
+++ packages/LaTex-Shortcuts/LaTex%20Shortcuts-0.0.2.tar.gz/LaTex Shortcuts-0.0.2/README.md
@@ -0,0 +1,24 @@
+# LaTexShortcuts
+
+LaTex Shortcut is adds a couple of shortcuts that you can memorize to speed up your writing LaTex.
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
+
+```bash
+pip install LaTexShortcuts
+```
+
+## Usage
+
+```python LaTexShortcuts.py```
+```python3 LaTexShortcuts.py```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ packages/LaTex-Shortcuts/LaTex%20Shortcuts-0.0.2.tar.gz/LaTex Shortcuts-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="LaTex Shortcuts",  # Replace with your own username
+    version="0.0.2",
+    author="Aria Bagheri",
+    author_email="ariab9342@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AriaBagheri/LatexShortcuts",
+    packages=setuptools.find_packages(),
+    py_modules=["keyboards", ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
