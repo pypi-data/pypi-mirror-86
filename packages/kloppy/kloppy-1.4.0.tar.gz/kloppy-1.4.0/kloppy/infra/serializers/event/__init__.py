2f920c724019e7cd8b6f81069593c1cbe3755c0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:37 +0000

    Add package kloppy/1.4.0/kloppy-1.4.0.tar.gz

---
+++ packages/kloppy/kloppy-1.4.0.tar.gz/kloppy-1.4.0/kloppy/infra/serializers/event/__init__.py
@@ -0,0 +1,7 @@
+from .base import EventDataSerializer
+
+# from .metrica import MetricaEventSerializer
+from .statsbomb import StatsBombSerializer
+from .opta import OptaSerializer
+from .metrica import MetricaEventsJsonSerializer
+from .sportec import SportecEventSerializer
