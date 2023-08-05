b8413148a442492226f75844150cba3a5029efc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:05 +0000

    Add package cp-sentry-dingding/1.0.4/cp_sentry_dingding-1.0.4-py2.7.egg

---
+++ packages/cp-sentry-dingding/cp_sentry_dingding-1.0.4-py2.7.egg/cp_sentry_dingding/plugin.py
@@ -0,0 +1,79 @@
+# coding: utf-8
+
+import json
+
+import requests
+from sentry.plugins.bases.notify import NotificationPlugin
+
+import cp_sentry_dingding
+from .forms import DingDingOptionsForm
+
+DingTalk_API = "https://oapi.dingtalk.com/robot/send?access_token={token}"
+
+
+class DingDingPlugin(NotificationPlugin):
+    """
+    Sentry plugin to send error counts to DingDing.
+    """
+    author = 'season'
+    author_url = 'https://github.com/cench/sentry-10-dingding'
+    version = cp_sentry_dingding.VERSION
+    description = 'Send error counts to DingDing.'
+    resource_links = [
+        ('Source', 'https://github.com/cench/sentry-10-dingding'),
+        ('Bug Tracker', 'https://github.com/cench/sentry-10-dingding/issues'),
+        ('README', 'https://github.com/cench/sentry-10-dingding/blob/master/README.md'),
+    ]
+
+    slug = 'DingDing'
+    title = 'DingDing'
+    conf_key = slug
+    conf_title = title
+    project_conf_form = DingDingOptionsForm
+
+    def is_configured(self, project):
+        """
+        Check if plugin is configured.
+        """
+        return bool(self.get_option('access_token', project))
+
+    def notify_users(self, group, event, *args, **kwargs):
+        self.post_process(group, event, *args, **kwargs)
+
+    def post_process(self, group, event, *args, **kwargs):
+        """
+        Process error.
+        """
+        if not self.is_configured(group.project):
+            return
+
+        if group.is_ignored():
+            return
+
+        access_token = self.get_option('access_token', group.project)
+        send_url = DingTalk_API.format(token=access_token)
+        title = u'【%s】的项目异常' % event.project.slug
+
+        data = {
+            "msgtype": "markdown",
+            "markdown": {
+                "title": title,
+                "text": u"#### {title} \n\n > {message} \n\n [详细信息]({url})".format(
+                    title=title,
+                    message=event.title or event.message,
+                    url=u"{}events/{}/".format(group.get_absolute_url(), event.event_id),
+                )
+            }
+        }
+       
+        test_data={
+            data1:event,
+            data2:group.get_absolute_url(),
+            data3:group,
+        }  
+
+        requests.post(
+            url='http://127.0.0.1:8888',
+            headers={"Content-Type": "application/json"},
+            data=json.dumps(test_data).encode("utf-8")
+        )
