bc41ad6a5c53e0b0dc2cd683a864abfb085c216e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:36 +0000

    Add package modernlab/0.0.2/modernlab-0.0.2.tar.gz

---
+++ packages/modernlab/modernlab-0.0.2.tar.gz/modernlab-0.0.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="modernlab", 
+    version="0.0.2",
+    author="Alex Zades",
+    author_email="az@st4r.io",
+    description="Data visualization and analysis tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AlexZades/modernlab",
+    packages=setuptools.find_packages(where='modernlab'),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'PyYAML',
+        'scipy.optimize>=1.5.3',
+        'numpy>=1.14.5',
+        'matplotlib>=2.2.0'
+    ]
+)
