5e41af37b88b7fd9dd13d9f4c562e64590884dfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:20 +0000

    Add package JarvisAI/0.2.1/JarvisAI-0.2.1.tar.gz

---
+++ packages/JarvisAI/JarvisAI-0.2.1.tar.gz/JarvisAI-0.2.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+from setuptools import find_namespace_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="JarvisAI",
+    version="0.2.1",
+    author="Dipesh",
+    author_email="dipeshpal17@gmail.com",
+    description="JarvisAI is AI python library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Dipeshpal/Jarvis_AI",
+    include_package_data=True,
+    packages=find_namespace_packages(include=['JarvisAI.*', 'JarvisAI']),
+    install_requires=['numpy==1.18.5', 'gtts', 'playsound', 'SpeechRecognition',
+                      'pipwin', 'lxml',
+                      'beautifulsoup4', 'wikipedia', 'pipwin', 'auto_face_recognition'],
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
