c9feb30528688c6ab4615dd29a358f30569ba082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:16 +0000

    Add package gff-io/0.0.1/gff_io-0.0.1-py3-none-any.whl

---
+++ packages/gff-io/gff_io-0.0.1-py3-none-any.whl/gff_io/__init__.py
@@ -0,0 +1,10 @@
+from ._reader import Item, ParsingError, Reader, read_gff
+from ._version import __version__
+
+__all__ = [
+    "Item",
+    "ParsingError",
+    "Reader",
+    "__version__",
+    "read_gff",
+]
