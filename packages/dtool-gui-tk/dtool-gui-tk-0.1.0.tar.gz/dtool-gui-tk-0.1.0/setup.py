000edb584760dfca8ee3353f197befa8d0da9857
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:19 +0000

    Add package dtool-gui-tk/0.1.0/dtool-gui-tk-0.1.0.tar.gz

---
+++ packages/dtool-gui-tk/dtool-gui-tk-0.1.0.tar.gz/dtool-gui-tk-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+url = "https://github.com/jic-dtool/dtool-gui-tk"
+version = "0.1.0"
+readme = open('README.rst').read()
+
+setup(
+    name="dtool-gui-tk",
+    packages=["dtool_gui_tk"],
+    version=version,
+    description="Graphical user interface for managing data using dtool",
+    long_description=readme,
+    include_package_data=True,
+    author="Tjelvar Olsson",
+    author_email="tjelvar.olsson@jic.ac.uk",
+    url=url,
+    install_requires=[],
+    entry_points={
+        'console_scripts': ['dtool-tk=dtool_gui_tk.tkgui:tkgui'],
+    },
+    download_url="{}/tarball/{}".format(url, version),
+    license="MIT"
+)
