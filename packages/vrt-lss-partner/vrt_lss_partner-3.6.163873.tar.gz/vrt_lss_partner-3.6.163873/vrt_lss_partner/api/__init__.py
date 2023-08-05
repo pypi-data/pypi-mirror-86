c3f3017103756ea9de232197e96b232881086764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:04 +0000

    Add package vrt-lss-partner/3.6.163873/vrt_lss_partner-3.6.163873.tar.gz

---
+++ packages/vrt-lss-partner/vrt_lss_partner-3.6.163873.tar.gz/vrt_lss_partner-3.6.163873/vrt_lss_partner/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from vrt_lss_partner.api.reports_api import ReportsApi
+from vrt_lss_partner.api.statistics_api import StatisticsApi
+from vrt_lss_partner.api.system_api import SystemApi
