cdf8c07881f66540a1a7ce828d2f1bfdcde8f89a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:07 +0000

    Add package hns-logicmonitor-api/0.1/hns_logicmonitor_api-0.1.tar.gz

---
+++ packages/hns-logicmonitor-api/hns_logicmonitor_api-0.1.tar.gz/hns_logicmonitor_api-0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='hns_logicmonitor_api',
+    version='0.1',
+    packages=find_packages(),
+    url='https://gitlab.com/horsebridge/hns_logicmonitor_api.git',
+    license='MIT',
+    author='Nitin Sidhu',
+    author_email='nitin.sidhu23@gmail.com',
+    description='Logicmonitor API',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8"
+    ],
+    python_requires='~=3.6',
+    install_requires=['requests>=2,<3']
+)
+
