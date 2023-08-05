5b6482c975fdef0a05caf72977f62bc37ed2d9aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:30 +0000

    Add package py-facebook-scraper/0.2.29/py-facebook-scraper-0.2.29.tar.gz

---
+++ packages/py-facebook-scraper/py-facebook-scraper-0.2.29.tar.gz/py-facebook-scraper-0.2.29/facebook_scraper/__main__.py
@@ -0,0 +1,37 @@
+import argparse
+import logging
+
+from . import write_posts_to_csv, enable_logging
+
+
+def run():
+    """facebook-scraper entry point when used as a script"""
+    parser = argparse.ArgumentParser(
+        prog='py-facebook-scraper', description="Scrape Facebook public pages without an API key",
+    )
+    parser.add_argument('account', type=str, help="Facebook account")
+    parser.add_argument('-f', '--filename', type=str, help="Output filename")
+    parser.add_argument('-p', '--pages', type=int, help="Number of pages to download", default=10)
+    parser.add_argument('-g', '--group', action='store_true', help="Use group scraper")
+    parser.add_argument('-v', '--verbose', action='count', help="Enable logging", default=0)
+    parser.add_argument(
+        '--encoding', action='store', help="Encoding for the output file", default=None
+    )
+
+    args = parser.parse_args()
+
+    # Choose the right argument to pass to write_posts_to_csv (group or account)
+    account_type = 'group' if args.group else 'account'
+    kwargs = {account_type: args.account}
+
+    # Enable logging
+    if args.verbose > 0:
+        args.verbose = min(args.verbose, 3)
+        level = {1: logging.WARNING, 2: logging.INFO, 3: logging.DEBUG}[args.verbose]
+        enable_logging(level)
+
+    write_posts_to_csv(**kwargs, filename=args.filename, pages=args.pages, encoding=args.encoding)
+
+
+if __name__ == '__main__':
+    run()
---
+++ packages/py-facebook-scraper/py-facebook-scraper-0.2.29.tar.gz/py-facebook-scraper-0.2.29/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py-facebook-scraper",
+    version="0.2.29",
+    include_package_data=True,
+    author="Madhouse Inc.",
+    author_email="evoex123@gmail.com",
+    description="Facebook-scraper python client package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/evoup/py-facebook-scraper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux",
+    ],
+)
