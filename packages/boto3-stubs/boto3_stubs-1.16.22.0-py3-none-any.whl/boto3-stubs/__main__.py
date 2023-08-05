76782fe08426464d209305213e800407919d80f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:51 +0000

    Add package boto3-stubs/1.16.22.0/boto3_stubs-1.16.22.0-py3-none-any.whl

---
+++ packages/boto3-stubs/boto3_stubs-1.16.22.0-py3-none-any.whl/boto3-stubs/__main__.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    print(
+        "Type annotations for boto3 1.16.22\n"
+        "Version:         1.16.22.0\n"
+        "Builder version: 3.2.2\n"
+        "Docs:            https://pypi.org/project/boto3-stubs/\n"
+        "Changelog:       https://github.com/vemel/mypy_boto3_builder/releases"
+    )
