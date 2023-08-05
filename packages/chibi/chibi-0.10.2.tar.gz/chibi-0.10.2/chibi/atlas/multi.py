45c9f9dd478bbb1727c108d53644e18822873f90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:05 +0000

    Add package chibi/0.10.2/chibi-0.10.2.tar.gz

---
+++ packages/chibi/chibi-0.10.2.tar.gz/chibi-0.10.2/chibi/atlas/multi.py
@@ -0,0 +1,15 @@
+from . import Chibi_atlas
+import copy
+
+
+class Chibi_atlas_multi( Chibi_atlas ):
+    def __setitem__( self, name, value ):
+        if name in self:
+            current = self[ name ]
+            if not isinstance( current, list ):
+                current = [ current ]
+                del self[ name ]
+                self[ name ] = current
+            self[ name ].append( value )
+        else:
+            super().__setitem__( name, value )
