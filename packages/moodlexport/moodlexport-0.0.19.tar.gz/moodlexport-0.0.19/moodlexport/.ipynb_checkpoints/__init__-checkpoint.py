4279f0cb091639c862e1c8371e74d467aabfbb15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:56 +0000

    Add package moodlexport/0.0.19/moodlexport-0.0.19.tar.gz

---
+++ packages/moodlexport/moodlexport-0.0.19.tar.gz/moodlexport-0.0.19/moodlexport/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,10 @@
+#from moodlexport.python_to_moodle import *
+#from moodlexport.tex_to_python import *
+#from moodlexport.python_to_latex import *
+
+from .python_to_moodle import Category, Question, printmk
+from .tex_to_python import latextopython, latextomoodle
+
+
+
+#__all__ = [ Category]
