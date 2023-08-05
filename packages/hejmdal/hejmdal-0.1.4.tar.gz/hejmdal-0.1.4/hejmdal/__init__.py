cf9360376a16af33609281144d7505301047e34b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:16 +0000

    Add package hejmdal/0.1.4/hejmdal-0.1.4.tar.gz

---
+++ packages/hejmdal/hejmdal-0.1.4.tar.gz/hejmdal-0.1.4/hejmdal/__init__.py
@@ -0,0 +1,11 @@
+from .base import *
+
+__all__ =  ['base', 'formatting', 'forms', 'datacalls', 'operations', 'main']
+
+__version__ = "0.1.4"
+
+from . import formatting
+from . import forms
+from . import datacalls
+from . import operations
+from . import main
---
+++ packages/hejmdal/hejmdal-0.1.4.tar.gz/hejmdal-0.1.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+#see configuration in setup.cfg
+setup(
+    install_requires=[
+        "pandas",
+        "sqlalchemy",
+        "pymysql",
+        "requests",
+        ],
+)
