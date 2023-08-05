27c4880ce5ab7b33fa24936033ec5d6236c3cf4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:57 +0000

    Add package detect-contamination/0.1.1/detect_contamination-0.1.1.tar.gz

---
+++ packages/detect-contamination/detect_contamination-0.1.1.tar.gz/detect_contamination-0.1.1/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['detect_contamination']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['detect_contamination = '
+                     'detect_contamination.__main__:main']}
+
+setup_kwargs = {
+    'name': 'detect-contamination',
+    'version': '0.1.1',
+    'description': 'A wrapper for centrifuge that can be used to detect contamination',
+    'long_description': None,
+    'author': 'Yasir Kusay',
+    'author_email': 'yasir.kusay@student.unsw.edu.au',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
