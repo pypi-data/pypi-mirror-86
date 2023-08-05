61ddb9836aafabbc1e2071754d1a4a6e215e563b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:13 +0000

    Add package npal/0.1.2/npal-0.1.2.tar.gz

---
+++ packages/npal/npal-0.1.2.tar.gz/npal-0.1.2/npal/__init__.py
@@ -0,0 +1,9 @@
+from npal.npal import Npal
+
+__author__ = 'Yoong Hor Meng'
+__versioninfo__ = (0, 1, 2)
+__version__ = '.'.join(map(str, __versioninfo__))
+
+__all__ = [
+    'Npal'
+    ]
---
+++ packages/npal/npal-0.1.2.tar.gz/npal-0.1.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='npal',
+    version='0.1.2',
+    author='Yoong Hor Meng',
+    author_email='yoong+del+hm@gmail.com',
+    description='Utilities to access NPAL',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.6',
+    url='https://github.com/npal2/npal/',
+    packages=setuptools.find_packages(),
+    install_requires = [
+        'selenium>=3.141.0',
+        'helium>=3.0.5',
+        'cryptography>=2.9.2',
+        'pandas>=1.0.0'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
