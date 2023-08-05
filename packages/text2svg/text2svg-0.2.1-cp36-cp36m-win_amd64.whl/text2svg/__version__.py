919e268d1c8b55b776667bc2bef3e62816b42134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:50 +0000

    Add package text2svg/0.2.1/text2svg-0.2.1-cp36-cp36m-win_amd64.whl

---
+++ packages/text2svg/text2svg-0.2.1-cp36-cp36m-win_amd64.whl/text2svg/__version__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+__title__ = "text2svg"
+__version__ = "0.2.1"
+__summary__ = "Convert text to svg"
+__uri__ = "https://github.com/naveen521kk/text2svg"
+__author__ = "Naveen M K"
+__license__ = "GPLv3"
