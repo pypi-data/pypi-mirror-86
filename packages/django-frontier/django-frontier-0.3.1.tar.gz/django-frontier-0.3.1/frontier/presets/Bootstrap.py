b32305e395e674cf37694b7fc11487db9de11f9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:02 +0000

    Add package django-frontier/0.3.1/django-frontier-0.3.1.tar.gz

---
+++ packages/django-frontier/django-frontier-0.3.1.tar.gz/django-frontier-0.3.1/frontier/presets/Bootstrap.py
@@ -0,0 +1,29 @@
+from .Preset import update_packages
+from pathlib import Path
+import shutil
+from frontier.utils import prepare_scaffold
+import json
+
+
+class Bootstrap():
+    def install(self, resource_path, base_dir):
+        prepare_scaffold(base_dir, resource_path)
+        components_dir = Path.joinpath(resource_path, "js/")
+        sass_components_dir = Path.joinpath(resource_path, "scss/")
+        base_packages_path = Path.joinpath(
+            Path(__file__).resolve().parent, "samples/default")
+        components_source = Path.joinpath(
+            Path(__file__).resolve().parent, "samples/bootstrap/src/js")
+        scss_components_source = Path.joinpath(
+            Path(__file__).resolve().parent, "samples/bootstrap/src/scss")
+
+        shutil.copytree(str(components_source), str(components_dir))
+        shutil.copytree(str(scss_components_source),
+                        str(sass_components_dir))
+        shutil.copy2(str(f"{base_packages_path}/package.json"), str(base_dir))
+        dependencies = {
+            "bootstrap": "^4.5.1",
+            "jquery": "^3.2",
+            "popper.js": "^1.14"
+        }
+        update_packages(base_dir, dependencies)
