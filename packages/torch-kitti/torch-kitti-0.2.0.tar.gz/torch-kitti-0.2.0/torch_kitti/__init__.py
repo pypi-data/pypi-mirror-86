47c5ad3744a2cc2c74989b197da65f3b0075df5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:30 +0000

    Add package torch-kitti/0.2.0/torch-kitti-0.2.0.tar.gz

---
+++ packages/torch-kitti/torch-kitti-0.2.0.tar.gz/torch-kitti-0.2.0/torch_kitti/__init__.py
@@ -0,0 +1,9 @@
+"""
+Utilities to handle the KITTI Vision Benchmark Suite in PyTorch
+"""
+
+__version__ = "0.2.0"
+
+__all__ = ["raw", "depth_completion", "depth_prediction", "transforms", "metrics"]
+
+from . import depth_completion, depth_prediction, metrics, raw, transforms
