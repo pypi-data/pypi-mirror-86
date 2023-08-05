f0d4f10f2dd796cfab4104be9aca38983c535f98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:54 +0000

    Add package opensubtitles-downloader/1.0.4/opensubtitles_downloader-1.0.4-py3-none-any.whl

---
+++ packages/opensubtitles-downloader/opensubtitles_downloader-1.0.4-py3-none-any.whl/opensubtitles_downloader/logger.py
@@ -0,0 +1,15 @@
+"""
+Logger that print every message to standard output.
+"""
+
+import logging
+import sys
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+#formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+#handler.setFormatter(formatter)
+logger.addHandler(handler)
