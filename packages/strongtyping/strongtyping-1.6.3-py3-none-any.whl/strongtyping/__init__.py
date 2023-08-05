9963818f43fd3e5fe9457edce6af4b1fac590469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:28 +0000

    Add package strongtyping/1.6.3/strongtyping-1.6.3-py3-none-any.whl

---
+++ packages/strongtyping/strongtyping-1.6.3-py3-none-any.whl/strongtyping/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@created: 04.06.20
+@author: felix
+"""
+
+__all__ = ['strong_typing', 'docstring_typing', 'cached_set', 'cached_dict', 'type_namedtuple']
