b09aa044904ea908e5dd45b082bddc9d220e86da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:05 +0000

    Add package svgmanip/0.0.9/svgmanip-0.0.9.tar.gz

---
+++ packages/svgmanip/svgmanip-0.0.9.tar.gz/svgmanip-0.0.9/setup.py
@@ -0,0 +1,47 @@
+import setuptools
+
+CLASSIFIERS = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Natural Language :: English",
+    "License :: OSI Approved :: Apache Software License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Programming Language :: Python :: Implementation :: IronPython",
+    "Programming Language :: Python :: Implementation :: Jython",
+    "Programming Language :: Python :: Implementation :: MicroPython",
+    "Programming Language :: Python :: Implementation :: PyPy",
+    "Programming Language :: Python :: Implementation :: Stackless",
+    "Topic :: Multimedia :: Graphics :: Editors :: Vector-Based",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+
+KEYWORDS = ["svg", "image", "simple"]
+
+INSTALL_REQUIRES = [
+    'svgutils',
+    'mpmath',
+    'ensure'
+]
+
+setuptools.setup(
+    name='svgmanip',
+    version='0.0.9',
+    py_modules=['svgmanip'],
+    url='https://github.com/CrazyPython/svgmanip',
+    license='Apache 2.0',
+    author='James Lu',
+    author_email='jamtlu@gmail.com',
+    description='`svgmanip` helps import and composite together existing SVG drawings. More information is available at the GitHub README.',
+    classifiers=CLASSIFIERS,
+    install_requires=INSTALL_REQUIRES,
+    keywords=KEYWORDS,
+    packages=setuptools.find_packages(),
+)
