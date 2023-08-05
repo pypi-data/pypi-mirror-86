7323e29ba199e9da4e8b44d995428267289964d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:17 +0000

    Add package robotframework-camunda/0.4.2/robotframework-camunda-0.4.2.tar.gz

---
+++ packages/robotframework-camunda/robotframework-camunda-0.4.2.tar.gz/robotframework-camunda-0.4.2/CamundaLibrary/Deployment.py
@@ -0,0 +1,45 @@
+from robot.api.deco import library, keyword
+from robot.api.logger import librarylogger as logger
+import requests
+import os
+import json
+
+
+@library(scope='GLOBAL', version='0.3.4')
+class Deployment:
+
+    CAMUNDA_HOST = None
+
+    def __init__(self, camunda_host: str = ''):
+        self.CAMUNDA_HOST = camunda_host
+
+    @keyword("Set Camunda URL")
+    def set_camunda_url(self, url: str):
+        """
+        Sets url for camunda eninge. Only necessary when URL cannot be set during initialization of this library or
+        you want to switch camunda url for some reason.
+        """
+        if not url:
+            raise ValueError('Cannot set camunda engine url: no url given.')
+        self.CAMUNDA_HOST = url
+
+    @keyword(name='Deploy model from file')
+    def deploy_bpmn(self, path_bpmn_file: str):
+        """
+        Uploads a camunda model to camunda.
+        """
+        if not path_bpmn_file:
+            raise ValueError('Failed deploying model, because no file provided.')
+
+        filename = os.path.basename(path_bpmn_file)
+
+        camunda_deployment_info_part = {
+            'deployment-name': filename,
+        }
+        model_part = {'data': (os.path.basename(path_bpmn_file), open(path_bpmn_file, 'r'))}
+        response = requests.post(f'{self.CAMUNDA_HOST}/engine-rest/deployment/create', data=camunda_deployment_info_part, files=model_part)
+        logger.info(f'Response from camunda:\t{response.status_code}')
+        response.raise_for_status()
+
+        return json.loads(response.content)
+
---
+++ packages/robotframework-camunda/robotframework-camunda-0.4.2.tar.gz/robotframework-camunda-0.4.2/CamundaLibrary/ProcessDefinition.py
@@ -0,0 +1,51 @@
+from robot.api.deco import library, keyword
+from robot.api.logger import librarylogger as logger
+from typing import Dict
+import requests
+import json
+
+
+@library(scope='GLOBAL', version='0.3.4')
+class ProcessDefinition:
+
+    CAMUNDA_HOST = None
+
+    def __init__(self, camunda_host: str = ''):
+        self.CAMUNDA_HOST = camunda_host
+
+    @keyword("Set Camunda URL")
+    def set_camunda_url(self, url: str):
+        """
+        Sets url for camunda eninge. Only necessary when URL cannot be set during initialization of this library or
+        you want to switch camunda url for some reason.
+        """
+        if not url:
+            raise ValueError('Cannot set camunda engine url: no url given.')
+        self.CAMUNDA_HOST = url
+
+    @keyword("Start process")
+    def start_process(self, process_key: str, variables: Dict = None):
+        """
+        Starts a new process instance from a process definition with given key.
+        """
+        endpoint = f'{self.CAMUNDA_HOST}/engine-rest/process-definition/key/{process_key}/start'
+
+        header = {
+            'Content-type': 'application/json'
+        }
+
+        json_body = { }
+
+        if variables:
+            json_body["variables"]=variables
+
+        logger.info(f"Request:\t{json_body}")
+
+        response = requests.post(endpoint,
+                            data=json.dumps(json_body),
+                            headers=header, timeout=(3.05, 15))
+
+        logger.debug(f"Response {response.status_code}:\t{response.content}")
+        response.raise_for_status()
+
+        return json.loads(response.content)
