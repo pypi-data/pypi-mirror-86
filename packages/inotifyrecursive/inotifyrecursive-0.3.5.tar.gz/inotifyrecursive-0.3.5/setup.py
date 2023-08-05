5b9332b3e0afb044d9d0b9e54121b7173411c889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:11 +0000

    Add package inotifyrecursive/0.3.5/inotifyrecursive-0.3.5.tar.gz

---
+++ packages/inotifyrecursive/inotifyrecursive-0.3.5.tar.gz/inotifyrecursive-0.3.5/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+import sys
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="inotifyrecursive",
+    version="0.3.5",
+    author="Torben Haase",
+    author_email="git@letorbi.com",
+    description="Recursive inotify watches based on inotify_simple.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/letorbi/inotifyrecursive",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+    ],
+    install_requires=["inotify-simple>=1.3.5"],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*"
+)
