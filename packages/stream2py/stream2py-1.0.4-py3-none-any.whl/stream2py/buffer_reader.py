76eb5274b794adade26940330196991654d2bf88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:40 +0000

    Add package stream2py/1.0.4/stream2py-1.0.4-py3-none-any.whl

---
+++ packages/stream2py/stream2py-1.0.4-py3-none-any.whl/stream2py/buffer_reader.py
@@ -0,0 +1,236 @@
+__all__ = ['BufferReader']
+
+import threading
+import time
+
+from stream2py.utility.locked_sorted_deque import RWLockSortedDeque
+from stream2py.utility.typing_hints import Union
+
+
+class BufferReader:
+    """Reader that is constructed from StreamBuffer.mk_reader()
+
+    >>> from stream2py import StreamBuffer, BufferReader
+    >>> from stream2py.examples.source_reader import SimpleCounterString
+    >>>
+    >>> source_reader = SimpleCounterString(start=0, stop=100)
+    >>> stream_buffer = StreamBuffer(source_reader=source_reader, maxlen=100)
+    >>> stream_buffer.start()
+    >>>
+    >>> buffer_reader = stream_buffer.mk_reader()
+    >>> isinstance(buffer_reader, BufferReader)
+    True
+    >>> buffer_reader.is_stopped
+    False
+    >>> buffer_reader.last_item == None  # None until value is seen
+    True
+    >>> buffer_reader.next() # get first item
+    's0'
+    >>> buffer_reader.last_item # check last_item cursor
+    's0'
+    >>> buffer_reader.next(peek=True), buffer_reader.last_item # next will not update last_item cursor
+    ('s1', 's0')
+    >>> buffer_reader.next(), buffer_reader.last_item # same as what was peeked but now cursor is updated
+    ('s1', 's1')
+    >>> buffer_reader.range(start=5, stop=10)
+    ['s5', 's6', 's7', 's8', 's9', 's10']
+    >>> buffer_reader.last_item
+    's10'
+    >>> buffer_reader.range(start=5, stop=10, step=3), buffer_reader.last_item
+    (['s5', 's8'], 's8')
+    >>> buffer_reader.range(start=10, stop=15, peek=True)
+    ['s10', 's11', 's12', 's13', 's14', 's15']
+    >>> buffer_reader.last_item # unchanged with peek
+    's8'
+    >>> buffer_reader.last_key # key of last_item
+    8
+    >>> buffer_reader.range(start=0, stop=9, only_new_items=True) # only 9 is greater than last item key
+    ['s9']
+    >>> buffer_reader.head(), buffer_reader.last_item, buffer_reader.last_key
+    ('s0', 's0', 0)
+    >>> buffer_reader.tail(), buffer_reader.last_item, buffer_reader.last_key
+    ('s99', 's99', 99)
+    >>>
+    >>> stream_buffer.stop()
+    >>>
+    >>> buffer_reader.is_stopped
+    True
+    """
+
+    def __init__(self, buffer: RWLockSortedDeque, source_reader_info: dict, stop_event: threading.Event):
+        """
+
+        :param buffer:
+        :param source_reader_info:
+        :param stop_event: threading.Event for source read loop
+        """
+        assert isinstance(buffer, RWLockSortedDeque)
+        assert isinstance(stop_event, threading.Event)
+
+        self._source_reader_info = source_reader_info
+        self._buffer = buffer
+        self._last_item = None
+        self._last_key = None
+        self._stop_event = stop_event
+        self._sleep_time_on_iter_none_s = 0.1
+
+    def __iter__(self):
+        while True:
+            _next = self.next(ignore_no_item_found=True)
+            if _next is not None:
+                yield _next
+            elif self.is_stopped:
+                return None
+            else:
+                time.sleep(self._sleep_time_on_iter_none_s)
+
+    def set_sleep_time_on_iter_none(self, sleep_time_s: Union[int, float] = 0.1):
+        """Set the sleep time of the iter yield loop when next data item is not yet available.
+
+        :param sleep_time_s: seconds to sleep
+        """
+        self._sleep_time_on_iter_none_s = sleep_time_s
+
+    def is_same_buffer(self, other_buffer_reader):
+        """Check if reader is looking at the same buffer"""
+        return self.__class__ == other_buffer_reader.__class__ and self._stop_event == other_buffer_reader._stop_event
+
+    @property
+    def is_stopped(self) -> bool:
+        """Checks if stop event has been set by StreamBuffer.
+
+        :return: bool
+        """
+        return self._stop_event.is_set()
+
+    @property
+    def source_reader_info(self) -> dict:
+        """A dict with important source info set by SourceReader.
+
+        :return: dict
+        """
+        return self._source_reader_info
+
+    @property
+    def last_key(self):
+        """key to last seen item cursor"""
+        return self._last_key
+
+    def _getlast_item(self):
+        return self._last_item
+
+    def _setlast_item(self, item):
+        self._last_key = self._buffer.key(item)
+        self._last_item = item
+
+    def _dellast_item(self):
+        del self._last_item
+        self._last_item = None
+        del self._last_key
+        self._last_key = None
+
+    last_item = property(_getlast_item, _setlast_item, _dellast_item, 'last seen item cursor')
+
+    def next(self, *, peek=False, ignore_no_item_found=False):
+        """Finds an item with a key greater than the last returned item.
+        Raise ValueError if no item found with key above last item.
+
+        :param peek: if True, last_item cursor will not be updated
+        :param ignore_no_item_found: if True, return None when no next item instead of raising exception
+        :return: next item
+        """
+        with self._buffer.reader_lock() as reader:
+            try:
+                next_item = reader.find_gt(self.last_key)
+            except ValueError as e:  # ValueError: No item found with key above: self.last_key
+                if ignore_no_item_found:
+                    return None
+                else:
+                    raise e
+            except TypeError as e:  # TypeError: '<' not supported between instances of 'NoneType' and type(key)
+                if self.last_item is None:  # first time reading a value from buffer
+                    next_item = reader[0]
+                else:
+                    raise e
+        if not peek:
+            self.last_item = next_item
+        return next_item
+
+    def range(self, start, stop, step=None, *, peek=False, ignore_no_item_found=False, only_new_items=False):
+        """Enables:
+        1. Get last n minutes
+        2. Give me data I don't have
+
+        :param start: starting range key of item inclusive
+        :param stop: ending range key of item inclusive
+        :param step:
+        :param peek: if True, last_item cursor will not be updated
+        :param ignore_no_item_found: if True, return None when no next item instead of raising exception
+        :param only_new_items: if True and no new items, raise ValueError or return None if ignore_no_item_found
+        :return: list of items in range
+        """
+        with self._buffer.reader_lock() as reader:
+            if only_new_items and self.last_key is not None:
+                _next = self.next(peek=True, ignore_no_item_found=ignore_no_item_found)
+                try:
+                    _next_key = reader.key(_next)
+                except TypeError as e:  # TypeError: 'NoneType' object is not subscriptable
+                    if ignore_no_item_found:
+                        return None
+                    else:
+                        raise e
+                _start = start if start > _next_key else _next_key
+            else:
+                _start = start
+            items = reader.range(_start, stop, step)
+
+        if not peek:
+            try:
+                self.last_item = items[-1]
+            except IndexError as e:  # IndexError: list index out of range
+                if ignore_no_item_found:
+                    return None
+                else:
+                    raise e
+        return items
+
+    def tail(self, *, peek=False, ignore_no_item_found=False, only_new_items=False):
+        """Finds the last item in buffer. Raise ValueError if no item found.
+
+        :param peek: if True, last_item cursor will not be updated
+        :param ignore_no_item_found: if True, return None when no next item instead of raising exception
+        :param only_new_items: if True and no new items, raise ValueError or return None if ignore_no_item_found
+        :return: tail item
+        """
+        with self._buffer.reader_lock() as reader:
+            if only_new_items:
+                try:
+                    item = reader.find_last_gt(self.last_key)
+                except ValueError as e:  # ValueError: No item found with key above: self.last_key
+                    if ignore_no_item_found:
+                        return None
+                    else:
+                        raise e
+                except TypeError as e:  # TypeError: '>' not supported between instances of type(key) and 'NoneType'
+                    if self.last_item is None:  # first time reading a value from buffer
+                        item = reader[-1]
+                    else:
+                        raise e
+            else:
+                try:
+                    item = reader[-1]
+                except IndexError as e:  # IndexError: deque index out of range
+                    if ignore_no_item_found:
+                        return None
+                    else:
+                        raise e
+        if not peek:
+            self.last_item = item
+        return item
+
+    def head(self, *, peek=False):
+        with self._buffer.reader_lock() as reader:
+            item = reader[0]
+        if not peek:
+            self.last_item = item
+        return item
---
+++ packages/stream2py/stream2py-1.0.4-py3-none-any.whl/stream2py/source_reader.py
@@ -0,0 +1,154 @@
+__all__ = ['SourceReader']
+
+
+from abc import ABCMeta, abstractmethod
+import time
+
+from stream2py.utility.typing_hints import Optional, Union, ComparableType, Any, Callable
+
+
+class SourceReader(metaclass=ABCMeta):
+    """Abstract class interface to be used by StreamBuffer.
+
+    >>> from stream2py import SourceReader
+    >>>
+    >>> class SimpleCounterString(SourceReader):
+    ...     '''Count in range returning a string formatted as f"s{count}"'''
+    ...     def __init__(self, start, stop):
+    ...         assert start < stop
+    ...         self.start = start
+    ...         self.stop = stop
+    ...         self.range_iterator = None
+    ...         self.open_count = 0
+    ...
+    ...     def open(self):
+    ...         self.open_count += 1
+    ...         self.range_iterator = iter(range(self.start, self.stop))
+    ...
+    ...     def read(self):
+    ...         value = next(self.range_iterator, None)
+    ...         if value is not None:
+    ...             return f's{value}'
+    ...         else:
+    ...             return None
+    ...
+    ...     def close(self):
+    ...         del self.range_iterator
+    ...         self.range_iterator = None
+    ...
+    ...     @property
+    ...     def info(self):
+    ...         return dict(start=self.start, stop=self.stop, open_count=self.open_count)
+    ...
+    ...     def key(self, data):
+    ...         return int(data[1:])
+    ...
+    >>> source_reader = SimpleCounterString(start=0, stop=10)
+    >>> source_reader.open()
+    >>> source_reader.info
+    {'start': 0, 'stop': 10, 'open_count': 1}
+    >>> source_reader.read()
+    's0'
+    >>> source_reader.close()
+    >>> with source_reader:
+    ...     source_reader.info
+    ...     source_reader.read()
+    ...     source_reader.read()
+    ...     source_reader.read()
+    {'start': 0, 'stop': 10, 'open_count': 2}
+    's0'
+    's1'
+    's2'
+    """
+
+    _sleep_time_on_iter_none_s = 0.001
+
+    def __iter__(self):
+        while True:
+            _next = self.read()
+            if _next is not None:
+                yield _next
+            else:
+                time.sleep(self._sleep_time_on_iter_none_s)
+
+    def set_sleep_time_on_iter_none(self, sleep_time_s: Union[int, float] = 0.001):
+        """Set the sleep time of the iter yield loop when next data item is not yet available.
+
+        :param sleep_time_s: seconds to sleep
+        """
+        self._sleep_time_on_iter_none_s = sleep_time_s
+
+    @abstractmethod
+    def open(self) -> None:
+        """Set up source to be read and set some source info affected by open time like the time of open.
+        Will be called in StreamBuffer immediately before first read."""
+
+    @abstractmethod
+    def read(self) -> Optional[Any]:
+        """Must return data that is sortable with 'key' method or None. Data that is not readily sortable such as
+        ordered words of a sentence can be wrapped in a tuple (word_index, word).
+
+        :return: data or None
+        """
+        raise NotImplementedError("Implement the 'read' method returning data that is sortable with 'key' method")
+
+    @abstractmethod
+    def close(self) -> None:
+        """Close and clean up source reader.
+        Will be called when StreamBuffer stops or if an exception is raised during read and append loop.
+        """
+
+    @property
+    @abstractmethod
+    def info(self) -> dict:
+        """A dict with important source info. Default can be init_kwargs and open timestamp.
+
+        This info will be available in BufferReader after SourceReader has opened but any changes made between open and
+        close are not guaranteed to be visible. Any continuous status updates should be included with read data instead.
+
+        :return: dict
+        """
+        raise NotImplementedError("no source info set")
+
+    @abstractmethod
+    def key(self, data: Any) -> ComparableType:
+        """
+        Converts data into a comparable value to sort by
+
+        :param data: the return value of the 'read' method
+        :return: ComparableType
+        """
+        raise NotImplementedError("Implement the 'key' method to convert data into a comparable value to sort by")
+
+    @property
+    def sleep_time_on_read_none_s(self) -> Optional[Union[int, float]]:
+        """Sets default sleep time for StreamBuffer when it reads None from SourceReader.
+        Useful when you know the period between data points.
+
+        For example if data only comes once every 10 seconds,
+        it would be inefficient to try and read again every 0.1 seconds and get None up to 100 times
+        but every 10 seconds or even 1 second would be much more reasonable depending on the use case.
+        Not necessary if read is a blocking function that always returns a data point.
+
+        Default is None which delegates the sleep time to StreamBuffer's default.
+
+        :return: Optional[Union[int, float]] number of seconds to sleep
+        """
+        return None
+
+    timestamp_seconds_to_unit_conversion: Union[int, float] = 1e6  # to microseconds
+
+    @classmethod
+    def get_timestamp(cls) -> int:
+        """Get timestamp of now as int microseconds
+
+        :return: timestamp in UTC microseconds
+        """
+        return int(time.time() * cls.timestamp_seconds_to_unit_conversion)
+
+    def __enter__(self):
+        self.open()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        self.close()
