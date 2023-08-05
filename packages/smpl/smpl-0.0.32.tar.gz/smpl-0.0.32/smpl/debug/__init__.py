5117b031d0eefda44bdb184c56577e9768cad8e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:59 +0000

    Add package smpl/0.0.32/smpl-0.0.32.tar.gz

---
+++ packages/smpl/smpl-0.0.32.tar.gz/smpl-0.0.32/smpl/debug/__init__.py
@@ -0,0 +1,2 @@
+from .debug import *
+__all__ = ['msg', 'msg1', 'table', 'table1']
---
+++ packages/smpl/smpl-0.0.32.tar.gz/smpl-0.0.32/smpl/doc/doc.py
@@ -0,0 +1,61 @@
+params = 0
+name = 1
+
+def append(original):
+    """
+    Append doc string of ``original`` to ``target`` object.
+
+    Paramerters
+    ===========
+    original : ``class`` or ``function``
+        ``orignal.__doc__`` is appended to the ``__doc__`` of the ``target``
+
+    Examples
+    ========
+
+    >>> def ho():
+    ...     '''Ho'''
+    ...     print(ho.__doc__)
+    >>> @append(ho)
+    ... def hi():
+    ...     '''Hi'''
+    ...     print(hi.__doc__)
+    >>> hi()
+    HiHo
+    """
+    def wrapper(target):
+        if target.__doc__ is None:
+            target.__doc__ = ""
+        target.__doc__ += original.__doc__
+        return target
+    return wrapper
+
+def insert(original):
+    def wrapper(target):
+        if target.__doc__ is None:
+            target.__doc__ = ""
+        target.__doc__ = original.__doc__ + target.__doc__
+        return target
+    return wrapper
+
+def insert_eq():
+    """
+    """
+    def wrapper(target):
+        if target.__doc__ is None:
+            target.__doc__ = ""
+        safe = target.__doc__
+        target.__doc__ = target.__name__  + "("
+        for v in target.__code__.co_varnames:
+            target.__doc__ += v + ","
+        target.__doc__ = target.__doc__[:-1]
+        target.__doc__ += ") = " + safe
+        return target
+    return wrapper
+
+
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
