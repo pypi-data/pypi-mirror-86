b23e6d150fe358a287c3d1de5514d882ae1efd54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:29 +0000

    Add package packageadms/0.1/packageadms-0.1.tar.gz

---
+++ packages/packageadms/packageadms-0.1.tar.gz/packageadms-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(name="packageadms",
+version="0.1",
+description="This is first My package for basic mathamatical operations...",
+long_description="packageadms is mainly use for basic calculations \n in  python using some funtions like, add_numbers(num1,num2),divide_numbers(num1,num2),multiply_numbers(num1,num2),substract_numbers(num1,num2)\n all these funtions can be use for calculations. It provides ease to do some.",  
+author="addzz",
+packages=['packageadms'],
+install_requires=[])
