b722cc4a5fbbf2e452d78cae2daa3670c874cef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:29 +0000

    Add package flupy/1.1.5/flupy-1.1.5.tar.gz

---
+++ packages/flupy/flupy-1.1.5.tar.gz/flupy-1.1.5/flupy/cli/cli.py
@@ -0,0 +1,77 @@
+import argparse
+import importlib
+import sys
+from signal import SIG_DFL, SIGPIPE, signal
+from typing import List, Optional
+
+from flupy import __version__, flu
+
+
+def read_file(filename):
+    with open(filename, "r") as f:
+        yield from f
+
+
+def parse_args(args: List[str]):
+    """Parse input arguments"""
+    parser = argparse.ArgumentParser(
+        description="flupy: a fluent interface for python collections", formatter_class=argparse.RawTextHelpFormatter
+    )
+    parser.add_argument("-v", "--version", action="version", version="%(prog)s " + __version__)
+    parser.add_argument("command", help="command to execute against input")
+    parser.add_argument("-f", "--file", help="path to input file")
+    parser.add_argument(
+        "-i",
+        "--import",
+        nargs="*",
+        default=[],
+        help="modules to import\n"
+        "Syntax: <module>:<object>:<alias>\n"
+        "Examples:\n"
+        "\t'import os' = '-i os'\n"
+        "\t'import os as op_sys' = '-i os::op_sys'\n"
+        "\t'from os import environ' = '-i os:environ'\n"
+        "\t'from os import environ as env' = '-i os:environ:env'\n",
+    )
+    return parser.parse_args(args)
+
+
+def execute_imports(imps: List[str]):
+    """Execute CLI scoped imports"""
+    for imp_stx in imps:
+        module, _, obj_alias = imp_stx.partition(":")
+        obj, _, alias = obj_alias.partition(":")
+        if not obj:
+            globals()[alias or module] = importlib.import_module(module)
+        else:
+            _garb = importlib.import_module(module)
+            globals()[alias or obj] = getattr(_garb, obj)
+
+
+def main(argv: Optional[List[str]] = None):
+    """CLI Entrypoint"""
+    args = parse_args(argv[1:] if argv is not None else sys.argv[1:])
+
+    _command = args.command
+    _file = args.file
+    _import = getattr(args, "import")
+
+    execute_imports(_import)
+
+    if _file:
+        _ = flu(read_file(_file)).map(str.rstrip)
+    else:
+        # Do not raise exception for Broken Pipe
+        signal(SIGPIPE, SIG_DFL)
+        _ = flu(sys.stdin).map(str.rstrip)
+
+    pipeline = eval(_command)
+
+    if hasattr(pipeline, "__iter__") and not isinstance(pipeline, (str, bytes)):
+        for r in pipeline:
+            sys.stdout.write(str(r) + "\n")
+
+    elif pipeline is None:
+        pass
+    else:
+        sys.stdout.write(str(pipeline) + "\n")
---
+++ packages/flupy/flupy-1.1.5.tar.gz/flupy-1.1.5/flupy/cli/utils.py
@@ -0,0 +1,33 @@
+# pylint: disable=invalid-name
+import os
+
+from flupy.fluent import Fluent, flu
+
+
+def walk_files(*pathes: str, abspath=True) -> "Fluent[str]":
+    """Yield files recursively starting from each location in *pathes"""
+
+    if pathes == ():
+        pathes = (".",)
+
+    def _impl():
+        for path in pathes:
+            for d, _, files in os.walk(path):
+                for x in files:
+                    rel_path = os.path.join(d, x)
+                    if abspath:
+                        yield os.path.abspath(rel_path)
+                    else:
+                        yield rel_path
+
+    return flu(_impl())
+
+
+def walk_dirs(path: str = ".") -> "Fluent[str]":
+    """Yield files recursively starting from *path"""
+
+    def _impl():
+        for d, _, _ in os.walk(path):
+            yield d
+
+    return flu(_impl())
