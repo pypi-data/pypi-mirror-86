598d75ad0ae241b14109422cc38f5cfecdec2cbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:22 +0000

    Add package actions-toolkit/0.0.4/actions-toolkit-0.0.4.tar.gz

---
+++ packages/actions-toolkit/actions-toolkit-0.0.4.tar.gz/actions-toolkit-0.0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='actions-toolkit',
+    version='0.0.4',
+    description='🛠 The GitHub ToolKit for developing GitHub Actions in Python.',
+    author='yanglbme',
+    author_email='contact@yanglibin.info',
+    url='https://github.com/yanglbme/actions-toolkit',
+    packages=setuptools.find_packages(),
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
