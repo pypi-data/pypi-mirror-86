b609837c0e307d38722d5dcfdcdd29153a4be2c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:42 +0000

    Add package w3cpull/1.1.1/w3cpull-1.1.1.tar.gz

---
+++ packages/w3cpull/w3cpull-1.1.1.tar.gz/w3cpull-1.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name="w3cpull",
+    version="1.1.1",
+    author="Mikalai Lisitsa",
+    author_email="Mikalai.Lisitsa@ibm.com",
+    url="https://github.com/soulless-viewer/w3cpull",
+    description="w3cpull is an application for pulling data from IBM w3 Connections.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    keywords='w3-connections w3c ibm',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        "docopt == 0.6.2",
+        "requests == 2.22.0",
+        "schema == 0.7.2",
+        "selenium == 3.141.0",
+    ],
+    include_package_data=True,
+    python_requires='>=3.6',
+    scripts=['bin/w3cpull'],
+)
