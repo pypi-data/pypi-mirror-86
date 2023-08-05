7cfb297d0eb564da5a3f3306a9a87fb23fb75f41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:58 +0000

    Add package geoana/0.1.0/geoana-0.1.0.tar.gz

---
+++ packages/geoana/geoana-0.1.0.tar.gz/geoana-0.1.0/README.rst
@@ -0,0 +1,80 @@
+| `getting_started`_ | `connecting`_ | `installing`_ | `license`_ | `documentation <http://geoana.readthedocs.io/en/latest/>`_ |
+
+geoana
+======
+
+.. image:: https://readthedocs.org/projects/geoana/badge/?version=latest
+    :target: https://geoana.readthedocs.io/en/latest/?badge=latest
+    :alt: Documentation Status
+
+.. image:: https://img.shields.io/github/license/simpeg/geoana.svg
+    :target: https://github.com/simpeg/geoana/blob/master/LICENSE
+    :alt: MIT license
+
+.. image:: https://travis-ci.org/simpeg/geoana.svg?branch=master
+    :target: https://travis-ci.org/simpeg/geoana
+    :alt: Travis status
+
+.. image:: https://api.codacy.com/project/badge/Grade/2e32cd28f4424dc1800f1590a64c244f
+    :target: https://www.codacy.com/app/lindseyheagy/geoana?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=simpeg/geoana&amp;utm_campaign=Badge_Grade
+    :alt: codacy status
+
+
+`geoana` is a collection of (mostly) analytic functions in geophysics. We take an object oriented
+approach with the aim of having users be able to readily interact with the functions using `Jupyter <https://jupyter.org>`_
+
+
+.. _getting_started:
+
+Getting started
+---------------
+
+- If you do not already have python installed, we recommend downloading and installing it through `anaconda <https://www.anaconda.com/download/>`_
+- `Install <https://geoana.readthedocs.io/en/latest/#installing>`_ `geoana`
+- Browse the `gallery <http://geoana.readthedocs.io/en/latest/auto_examples/>`_ for ideas and example usage
+- Read the `documentation <http://geoana.readthedocs.io/en/latest/>`_ for more information on the library and what it can do
+
+.. - See the `contributor guide` and `code of conduct` if you are interested in helping develop or maintain geoana
+
+.. _connecting:
+
+Connecting with the community
+-----------------------------
+
+geoana is a part of the larger `SimPEG <https://simpeg.xyz>`_ ecosystem. There are several avenues for connecting:
+
+- a discourse website for questions: https://simpeg.discourse.group/
+- a youtube channel where meeting recordings are posted: `GeoSci.xyz <https://www.youtube.com/channel/UCBrC4M8_S4GXhyHht7FyQqw>`_
+- a slack group for real-time chat with users and developers of SimPEG: http://slack.simpeg.xyz/
+
+.. _installing:
+
+Installing
+----------
+
+**geoana** is on conda-forge
+
+.. code:: shell
+
+    conda install -c conda-forge geoana
+
+**geoana** is available on `pypi <https://pypi.org/project/geoana/>`_ and can be installed by opening a command window and running:
+
+.. code::
+
+    pip install geoana
+
+
+To install from source, you can
+
+.. code::
+
+    git clone https://github.com/simpeg/geoana.git
+    python setup.py install
+
+.. _license:
+
+License
+-------
+
+geoana is licensed under the `MIT license <https://github.com/simpeg/geoana/blob/master/LICENSE>`_ .
---
+++ packages/geoana/geoana-0.1.0.tar.gz/geoana-0.1.0/geoana/em/base.py
@@ -0,0 +1,119 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import numpy as np
+import properties
+from scipy.constants import mu_0, pi, epsilon_0
+
+from .. import spatial
+
+
+###############################################################################
+#                                                                             #
+#                              Base Classes                                   #
+#                                                                             #
+###############################################################################
+
+
+class BaseEM(properties.HasProperties):
+    """
+    Base class for electromanetics. Contains physical properties that are
+    relevant to all problems that use Maxwell's equations
+    """
+
+    mu = properties.Float(
+        "Magnetic permeability (H/m)",
+        default=mu_0,
+        min=0.0
+    )
+
+    sigma = properties.Float(
+        "Electrical conductivity (S/m)",
+        default=1.0,
+        min=0.0
+    )
+
+    epsilon = properties.Float(
+        "Permitivity value (F/m)",
+        default=epsilon_0,
+        min=0.0
+    )
+
+
+class BaseDipole(BaseEM):
+    """
+    Base class for dipoles.
+    """
+
+    orientation = properties.Vector3(
+        "orientation of dipole",
+        default="X",
+        length=1.0
+    )
+
+    location = properties.Vector3(
+        "location of the electric dipole source",
+        default="ZERO"
+    )
+
+    def vector_distance(self, xyz):
+        """
+        Vector distance from the dipole location
+        :param numpy.ndarray xyz: grid
+        """
+        return spatial.vector_distance(xyz, np.array(self.location))
+
+    def distance(self, xyz):
+        """
+        Distance from the dipole location
+        """
+        return spatial.distance(xyz, np.array(self.location))
+
+    def dot_orientation(self, xyz):
+        """
+        Take the dot product between a grid and the orientation of the dipole
+        """
+        return spatial.vector_dot(xyz, np.array(self.orientation))
+
+    def cross_orientation(self, xyz):
+        """
+        Take the cross product between a grid and the orientation of the dipole
+        """
+        orientation = np.kron(
+            np.atleast_2d(
+                np.array(self.orientation)
+            ), np.ones((xyz.shape[0], 1))
+        )
+        return np.cross(xyz, orientation)
+
+
+class BaseElectricDipole(BaseDipole):
+    """
+    Base class for electric current dipoles
+    """
+
+    length = properties.Float(
+        "length of the dipole (m)",
+        default=1.0,
+        min=0.0
+    )
+
+    current = properties.Float(
+        "magnitude of the injected current (A)",
+        default=1.0,
+        min=0.0
+    )
+
+
+class BaseMagneticDipole(BaseDipole):
+    """
+    Base class for magnetic dipoles
+    """
+
+    moment = properties.Float(
+        "moment of the dipole (Am^2)",
+        default=1.0,
+        min=0.0
+    )
