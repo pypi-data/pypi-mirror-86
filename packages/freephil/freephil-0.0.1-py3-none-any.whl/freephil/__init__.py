fbb27fea26365d4901fb5c31ed66108e9fd46bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:53 +0000

    Add package freephil/0.0.1/freephil-0.0.1-py3-none-any.whl

---
+++ packages/freephil/freephil-0.0.1-py3-none-any.whl/freephil/__init__.py
@@ -0,0 +1,15 @@
+try:
+    from libtbx import Auto
+    from libtbx.utils import Sorry
+except ModuleNotFoundError:
+    from .legacy import AutoType
+
+    Auto = AutoType()
+
+    class Sorry(SystemExit):
+        pass
+
+
+from .common import *  # noqa
+
+__version__ = "0.0.1"
