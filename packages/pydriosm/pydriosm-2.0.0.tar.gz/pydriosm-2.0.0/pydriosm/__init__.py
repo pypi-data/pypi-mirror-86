eb36f77e0855dea08404160692d5c7c21f1f6807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:37 +0000

    Add package pydriosm/2.0.0/pydriosm-2.0.0.tar.gz

---
+++ packages/pydriosm/pydriosm-2.0.0.tar.gz/pydriosm-2.0.0/pydriosm/__init__.py
@@ -0,0 +1,17 @@
+from .downloader import BBBikeDownloader, GeofabrikDownloader
+from .ios import PostgresOSM
+from .reader import BBBikeReader, GeofabrikReader
+from .settings import gdal_configurations
+
+gdal_configurations(reset=False)
+
+__all__ = ['downloader', 'GeofabrikDownloader', 'BBBikeDownloader',
+           'reader', 'GeofabrikReader', 'BBBikeReader',
+           'ios', 'PostgresOSM']
+
+__package_name__ = 'pydriosm'
+__package_name_alt__ = u'PyDriosm'
+__version__ = '2.0.0'
+__author__ = u'Qian Fu'
+__email__ = 'qian.fu@outlook.com'
+__description__ = "An open-source tool for downloading, reading and PostgreSQL-based I/O of OpenStreetMap data."
---
+++ packages/pydriosm/pydriosm-2.0.0.tar.gz/pydriosm-2.0.0/setup.py
@@ -0,0 +1,52 @@
+import setuptools
+
+import pydriosm
+
+with open("README.rst", 'r', encoding='utf-8') as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+
+    name=pydriosm.__package_name__,
+    version=pydriosm.__version__,
+    author=pydriosm.__author__,
+    author_email=pydriosm.__email__,
+
+    description=pydriosm.__description__,
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+
+    url='https://github.com/mikeqfu/pydriosm',
+
+    install_requires=[
+        'beautifulsoup4~=4.9.3',
+        'Fiona~=1.8.17',
+        'fuzzywuzzy',
+        'GDAL~=3.1.4',
+        'geopandas~=0.8.1',
+        'html5lib',
+        'humanfriendly~=8.2',
+        'lxml',
+        'pandas~=1.1.4',
+        'psycopg2',
+        'pyhelpers>=1.2.7',
+        'pyproj',
+        'pyshp',
+        'requests',
+        'Shapely~=1.7.1',
+        'SQLAlchemy',
+        'SQLAlchemy-Utils',
+        'tqdm',
+    ],
+
+    packages=setuptools.find_packages(exclude=["*.tests", "tests.*", "tests"]),
+
+    include_package_data=True,
+
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux'
+    ],
+)
