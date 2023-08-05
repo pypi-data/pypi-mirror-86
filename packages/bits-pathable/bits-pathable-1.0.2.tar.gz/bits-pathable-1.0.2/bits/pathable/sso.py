7fde25276ae9f7adeca0775efa78ee11aea627d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:00 +0000

    Add package bits-pathable/1.0.2/bits-pathable-1.0.2.tar.gz

---
+++ packages/bits-pathable/bits-pathable-1.0.2.tar.gz/bits-pathable-1.0.2/bits/pathable/sso.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Pathable SSO api class."""
+
+
+class SSO:
+    """Pathable SSO class."""
+
+    def __init__(self):
+        """Initialize a class instance."""
+
+    def signin(self, auth_token):
+        """Sign a user in with SSO."""
