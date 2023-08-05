5fb653968a6df33c34930757e6e3cb9f35c0e192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:27 +0000

    Add package django-modelapiview/1.4.12/django_modelapiview-1.4.12-py3-none-any.whl

---
+++ packages/django-modelapiview/django_modelapiview-1.4.12-py3-none-any.whl/django_modelapiview/views.py
@@ -0,0 +1,34 @@
+from django.views import View
+from django.contrib.auth import authenticate
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.urls import get_resolver
+from django.utils.translation import gettext as _
+
+from http import HTTPStatus
+import json
+
+from . import APIView, Token
+from .responses import APIResponse
+
+class LoginView(APIView):
+    route = "login"
+
+    def post(self, request, **kwargs) -> APIResponse:
+        data = request.body.decode('utf-8')
+        json_data = json.loads(data)
+
+        user = authenticate(username=json_data['username'], password=json_data['password'])
+        if user is not None:
+            token = Token({'uid': user.id})
+            token.sign()
+            return APIResponse(HTTPStatus.OK, _("User logged in"), {'token': str(token), 'user': user.serialize(request)})
+        else:
+            return APIResponse(HTTPStatus.UNAUTHORIZED, _("Wrong user credentials"))
+
+
+class URLsView(APIView):
+    route = ""
+
+    def get(self, request, **kwargs) -> APIResponse:
+        return APIResponse(HTTPStatus.OK, _("URLs available"), sorted(set(view[1] for view in get_resolver(None).reverse_dict.values())))
