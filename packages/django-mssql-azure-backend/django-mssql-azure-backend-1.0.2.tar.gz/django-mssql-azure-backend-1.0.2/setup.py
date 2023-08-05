2c0c0a86e7eca55456ba01410a1598a1bd0cad2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:17 +0000

    Add package django-mssql-azure-backend/1.0.2/django-mssql-azure-backend-1.0.2.tar.gz

---
+++ packages/django-mssql-azure-backend/django-mssql-azure-backend-1.0.2.tar.gz/django-mssql-azure-backend-1.0.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+
+CLASSIFIERS = [
+    'License :: OSI Approved :: BSD License',
+    'Framework :: Django',
+    "Operating System :: POSIX :: Linux",
+    "Operating System :: Microsoft :: Windows",
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Framework :: Django :: 2.2',
+    'Framework :: Django :: 3.0',
+]
+
+setup(
+    name='django-mssql-azure-backend',
+    version='1.0.2',
+    description='Django backend for Microsoft SQL Server',
+    long_description=open('README.rst').read(),
+    author='Sathiamoorthy M',
+    author_email='shakthifuture@gmail.com',
+    url='https://github.com/shakthifuture/django-mssql-azure-backend',
+    license='BSD',
+    packages=find_packages(),
+    install_requires=[
+        'pyodbc>=3.0',
+        'requests>=2.24.0'
+    ],
+    package_data={'sql_server.pyodbc': ['regex_clr.dll']},
+    classifiers=CLASSIFIERS,
+    keywords='django',
+)
