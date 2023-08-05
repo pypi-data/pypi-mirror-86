8df91688dccdb64ab05cfd073fb5a06e2a13a992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:08 +0000

    Add package django-user-defined-fields/0.0.12/django_user_defined_fields-0.0.12-py3-none-any.whl

---
+++ packages/django-user-defined-fields/django_user_defined_fields-0.0.12-py3-none-any.whl/userdefinedfields/migrations/0002_auto_20200923_0122.py
@@ -0,0 +1,16 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('userdefinedfields', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name='extrafield',
+            old_name='required',
+            new_name='is_required',
+        ),
+    ]
