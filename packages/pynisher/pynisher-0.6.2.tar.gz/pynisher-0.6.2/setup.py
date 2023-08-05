5b7be62b94d833ec080989187e8d6613459b383d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:01 +0000

    Add package pynisher/0.6.2/pynisher-0.6.2.tar.gz

---
+++ packages/pynisher/pynisher-0.6.2.tar.gz/pynisher-0.6.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='pynisher',
+    version="0.6.2",
+    packages=['pynisher'],
+    install_requires=['docutils>=0.3', 'setuptools', 'psutil'],
+    author="Stefan Falkner, Christina Hernandez-Wunsch, Samuel Mueller and Matthias Feurer",
+    author_email="feurerm@informatik.uni-freiburg.de",
+    description="A small Python library to limit the resources used by a function by executing it inside a subprocess.",
+    include_package_data=False,
+    keywords="resources",
+    license="MIT",
+    url="https://github.com/automl/pynisher",
+)
