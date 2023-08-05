407e43c2cad08b07412b33da15b5d5d81593ff06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:58 +0000

    Add package diamondback/1.0.36/diamondback-1.0.36.tar.gz

---
+++ packages/diamondback/diamondback-1.0.36.tar.gz/diamondback-1.0.36/diamondback/__init__.py
@@ -0,0 +1,9 @@
+from diamondback import clients
+from diamondback import commons
+from diamondback import filters
+from diamondback import interfaces
+from diamondback import models
+from diamondback import transforms
+
+
+__version__ = '1.0.36'
---
+++ packages/diamondback/diamondback-1.0.36.tar.gz/diamondback-1.0.36/diamondback/interfaces/IUser.py
@@ -0,0 +1,73 @@
+""" **Description**
+
+        User interface.
+
+    **Example**
+
+        ::
+
+            from diamondback.interfaces.IUser import IUser
+
+
+            class Test( IUser ) :
+
+                def __init__( self ) -> None :
+
+                    super( ).__init__( )
+
+            test = Test( )
+
+            print( test.user )
+
+    **License**
+
+        `BSD-3C. <https://github.com/larryturner/diamondback/blob/master/license>`_
+
+        Copyright (c) 2020, Larry Turner, Schneider Electric.  All rights reserved.
+
+    **Author**
+
+        Larry Turner, Schneider Electric, Analytics & AI, 2020-11-02.
+
+    **Definition**
+
+"""
+
+from diamondback.interfaces.IEqual import IEqual
+import getpass
+
+
+class IUser( IEqual ) :
+
+    """ User interface.
+    """
+
+    @property
+    def user( self ) :
+
+        """ User ( str ).
+        """
+
+        return getpass.getuser( )
+
+    def __eq__( self, other : any ) -> bool :
+
+        """ Equality.
+
+            Arguments :
+
+                other - Other object ( object ).
+
+            Returns :
+
+                equality - Equality condition ( bool ).
+        """
+
+        return ( ( super( ).__eq__( other ) ) and ( self.user == other.user ) )
+
+    def __init__( self ) -> None :
+
+        """ Initialize.
+        """
+
+        super( ).__init__( )
