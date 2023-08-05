f5456d3f97041be8cbd3e7b911d0f0edc1faa3b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:09 +0000

    Add package darwin-py/0.5.13/darwin-py-0.5.13.tar.gz

---
+++ packages/darwin-py/darwin-py-0.5.13.tar.gz/darwin-py-0.5.13/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="darwin-py",
+    version="0.5.13",
+    author="V7",
+    author_email="info@v7labs.com",
+    description="Library and command line interface for darwin.v7labs.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/v7labs/darwin-py",
+    install_requires=[
+        "argcomplete",
+        "dataclasses",
+        "docutils",
+        "factory_boy",
+        "humanize",
+        "numpy",
+        "pillow",
+        "pyyaml>=5.1",
+        "requests",
+        "scikit-learn",
+        "sh",
+        "tqdm",
+        "pillow",
+        "upolygon==0.1.2",
+    ],
+    packages=setuptools.find_packages(),
+    entry_points={"console_scripts": ["darwin=darwin.cli:main"]},
+    classifiers=["Programming Language :: Python :: 3", "License :: OSI Approved :: MIT License"],
+    python_requires=">=3.6",
+)
