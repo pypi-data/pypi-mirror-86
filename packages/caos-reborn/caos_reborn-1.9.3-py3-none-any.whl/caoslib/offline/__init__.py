d6103b4be0a743c10f07a2c5b1ac4aea481ff631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:17 +0000

    Add package caos-reborn/1.9.3/caos_reborn-1.9.3-py3-none-any.whl

---
+++ packages/caos-reborn/caos_reborn-1.9.3-py3-none-any.whl/caoslib/offline/__init__.py
@@ -0,0 +1,29 @@
+from clint.textui import puts, colored
+
+options = ['test', 'args', 'folder', 'help']
+
+def handler(mode, args):
+    from .test import test
+    from .modify import set_gcc_args, set_tasks_dir
+    from .help import print_help
+
+    if mode == 'test':
+        grouped = args.grouped
+        if '-c' not in grouped or '-t' not in grouped or len(grouped['-c']) == 0 or len(grouped['-t']) == 0:
+            puts(colored.red("Provide both -c and -t flags for testing."))
+            exit(0)
+        debug_mode = []
+        if '-d' in grouped:
+            if len(grouped['-d']) == 0:
+                debug_mode = ['000']
+            else:
+                debug_mode = grouped['-d'].all
+        test(grouped['-c'][0], grouped['-t'][0], debug_mode)
+    elif mode == 'args':
+        set_gcc_args()
+    elif mode == 'folder':
+        set_tasks_dir()
+    elif mode == 'help':
+        print_help()
+
+    exit(0)
