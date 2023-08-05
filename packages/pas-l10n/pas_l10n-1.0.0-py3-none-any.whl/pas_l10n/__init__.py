46aa80b0482cc0bb067508a2aecb9a99b74473c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:08 +0000

    Add package pas-l10n/1.0.0/pas_l10n-1.0.0-py3-none-any.whl

---
+++ packages/pas-l10n/pas_l10n-1.0.0-py3-none-any.whl/pas_l10n/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""
+direct PAS
+Python Application Services
+----------------------------------------------------------------------------
+(C) direct Netware Group - All rights reserved
+https://www.direct-netware.de/redirect?pas;l10n
+
+This Source Code Form is subject to the terms of the Mozilla Public License,
+v. 2.0. If a copy of the MPL was not distributed with this file, You can
+obtain one at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------------------------
+https://www.direct-netware.de/redirect?licenses;mpl2
+----------------------------------------------------------------------------
+v1.0.0
+pas_l10n/__init__.py
+"""
+
+from .l10n import L10n
+from .l10n_instance import L10nInstance
+from .number_formatter import NumberFormatter
+from .translatable_exception import TranslatableException
