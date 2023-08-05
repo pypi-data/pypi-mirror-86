00622127c23e01ba451730d3df2270df09bff299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:38 +0000

    Add package h3roku/0.1.4/h3roku-0.1.4.tar.gz

---
+++ packages/h3roku/h3roku-0.1.4.tar.gz/h3roku-0.1.4/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Note!
+# ' are required, do not use any ".
+
+# setup.
+from setuptools import setup, find_packages
+setup(
+	name='h3roku',
+	version='0.1.4',
+	description='Some description.',
+	url="http://github.com/vandenberghinc/h3roku",
+	author='Daan van den Bergh',
+	author_email='vandenberghinc.contact@gmail.com',
+	license='MIT',
+	packages=find_packages(),
+	zip_safe=False,
+	install_requires=[
+		"cl1",
+		"fil3s",
+		"r3sponse",
+	])
