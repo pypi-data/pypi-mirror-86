0c463b790fdfb701e081ecad393b09db75e79926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:35 +0000

    Add package simlib/0.0.92/simlib-0.0.92.tar.gz

---
+++ packages/simlib/simlib-0.0.92.tar.gz/simlib-0.0.92/setup.py
@@ -0,0 +1,57 @@
+"""
+setup.py
+written in Python3
+author: C. Lockhart <chris@lockhartlab.org>
+"""
+
+from setuptools import setup
+
+
+# Read version
+with open('version.yml', 'r') as f:
+    data = f.read().splitlines()
+version_dict = dict([element.split(': ') for element in data])
+
+# Convert the version_data to a string
+version = '.'.join([str(version_dict[key]) for key in ['major', 'minor', 'patch']])
+
+# Read in requirements.txt
+with open('requirements.txt', 'r') as stream:
+    requirements = stream.read().splitlines()
+
+# Long description
+with open('README.md', 'r') as stream:
+    long_description = stream.read()
+
+# Setup
+setup(
+    name='simlib',
+    version=version,
+    author='C. Lockhart',
+    author_email='chris@lockhartlab.org',
+    description='A toolkit for molecular dynamics simulations',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://www.lockhartlab.org",
+    packages=[
+        'simlib',
+        'simlib.analysis',
+        'simlib.analysis.protein',
+        'simlib.core',
+        'simlib.external',
+        'simlib.geometry',
+        'simlib.io',
+        'simlib.misc',
+        'simlib.transform',
+        'simlib.viz'
+    ],
+    install_requires=[
+        'glovebox',
+        'numpy',
+        'pandas',
+        'privatize',
+        'typelike', 'hypothesis', 'numba'
+    ],
+    include_package_data=True,
+    zip_safe=True
+)
---
+++ packages/simlib/simlib-0.0.92.tar.gz/simlib-0.0.92/simlib/io/__init__.py
@@ -0,0 +1,11 @@
+
+from .read import *
+
+__all__ = [
+    'loadtxt',
+    'read_dcd',
+    'read_peptide_sequence',
+    'read_pdb',
+]
+
+# TODO write IC coordinates for CHARMM
