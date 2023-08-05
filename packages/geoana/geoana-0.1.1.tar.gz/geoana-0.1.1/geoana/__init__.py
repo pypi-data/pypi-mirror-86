d25da6390c51cdf1054351ae10026939a41c7ef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:07 +0000

    Add package geoana/0.1.1/geoana-0.1.1.tar.gz

---
+++ packages/geoana/geoana-0.1.1.tar.gz/geoana-0.1.1/geoana/__init__.py
@@ -0,0 +1,9 @@
+from . import earthquake
+from . import em
+from . import utils
+
+__version__ = '0.1.1'
+__author__ = 'SimPEG developers'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017-2018 SimPEG developers'
+
