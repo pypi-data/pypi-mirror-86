a4e346a0562777aa1a68f6bfdfeeed9e298ed399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:39 +0000

    Add package restsql/0.1.24/restsql-0.1.24.tar.gz

---
+++ packages/restsql/restsql-0.1.24.tar.gz/restsql-0.1.24/restsql/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+
+from middleware import RestSqlMiddleware
+from restsql import RestSqlClient
+from restsqldb import EnumDataBase
+from table import Table, NumberField, StringField, BoolField, IntField
