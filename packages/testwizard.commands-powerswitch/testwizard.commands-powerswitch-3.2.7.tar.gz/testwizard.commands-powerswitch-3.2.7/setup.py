e4fc73157df2a832bdfcb3b50f81909e5e202c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:36 +0000

    Add package testwizard.commands-powerswitch/3.2.7/testwizard.commands-powerswitch-3.2.7.tar.gz

---
+++ packages/testwizard.commands-powerswitch/testwizard.commands-powerswitch-3.2.7.tar.gz/testwizard.commands-powerswitch-3.2.7/setup.py
@@ -0,0 +1,68 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="testwizard.commands-powerswitch",
+    version="3.2.7",
+    author="Eurofins Digital Testing - Belgium",
+    author_email="support-be@eurofins.com",
+    description="Testwizard Powerswitch commands",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['testwizard.commands_powerswitch'],
+    install_requires=[
+        'testwizard.commands-core==3.2.7'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.3",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
