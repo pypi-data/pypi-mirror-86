c61c780ce99d5fd28d12fb969f4aa57a7cc3b7db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:12 +0000

    Add package deepvac/0.1.22/deepvac-0.1.22.tar.gz

---
+++ packages/deepvac/deepvac-0.1.22.tar.gz/deepvac-0.1.22/deepvac/syszux_helper.py
@@ -0,0 +1,397 @@
+# -*- coding:utf-8 -*-
+# Author: RubanSeven
+import math
+import numpy as np
+from PIL import Image
+from deepvac.syszux_log import LOG
+
+
+class WarpMLS:
+    def __init__(self, src, src_pts, dst_pts, dst_w, dst_h, trans_ratio=1.):
+        self.src = src
+        self.src_pts = src_pts
+        self.dst_pts = dst_pts
+        self.pt_count = len(self.dst_pts)
+        self.dst_w = dst_w
+        self.dst_h = dst_h
+        self.trans_ratio = trans_ratio
+        self.grid_size = 100
+        self.rdx = np.zeros((self.dst_h, self.dst_w))
+        self.rdy = np.zeros((self.dst_h, self.dst_w))
+
+    @staticmethod
+    def __bilinear_interp(x, y, v11, v12, v21, v22):
+        return (v11 * (1 - y) + v12 * y) * (1 - x) + (v21 * (1 - y) + v22 * y) * x
+
+    def generate(self):
+        self.calc_delta()
+        return self.gen_img()
+
+    def calc_delta(self):
+        w = np.zeros(self.pt_count, dtype=np.float32)
+
+        if self.pt_count < 2:
+            return
+
+        i = 0
+        while 1:
+            if self.dst_w <= i < self.dst_w + self.grid_size - 1:
+                i = self.dst_w - 1
+            elif i >= self.dst_w:
+                break
+
+            j = 0
+            while 1:
+                if self.dst_h <= j < self.dst_h + self.grid_size - 1:
+                    j = self.dst_h - 1
+                elif j >= self.dst_h:
+                    break
+
+                sw = 0
+                swp = np.zeros(2, dtype=np.float32)
+                swq = np.zeros(2, dtype=np.float32)
+                new_pt = np.zeros(2, dtype=np.float32)
+                cur_pt = np.array([i, j], dtype=np.float32)
+
+                k = 0
+                for k in range(self.pt_count):
+                    if i == self.dst_pts[k][0] and j == self.dst_pts[k][1]:
+                        break
+
+                    w[k] = 1. / ((i - self.dst_pts[k][0]) * (i - self.dst_pts[k][0]) +
+                                 (j - self.dst_pts[k][1]) * (j - self.dst_pts[k][1]))
+
+                    sw += w[k]
+                    swp = swp + w[k] * np.array(self.dst_pts[k])
+                    swq = swq + w[k] * np.array(self.src_pts[k])
+
+                if k == self.pt_count - 1:
+                    pstar = 1 / sw * swp
+                    qstar = 1 / sw * swq
+
+                    miu_s = 0
+                    for k in range(self.pt_count):
+                        if i == self.dst_pts[k][0] and j == self.dst_pts[k][1]:
+                            continue
+                        pt_i = self.dst_pts[k] - pstar
+                        miu_s += w[k] * np.sum(pt_i * pt_i)
+
+                    cur_pt -= pstar
+                    cur_pt_j = np.array([-cur_pt[1], cur_pt[0]])
+
+                    for k in range(self.pt_count):
+                        if i == self.dst_pts[k][0] and j == self.dst_pts[k][1]:
+                            continue
+
+                        pt_i = self.dst_pts[k] - pstar
+                        pt_j = np.array([-pt_i[1], pt_i[0]])
+
+                        tmp_pt = np.zeros(2, dtype=np.float32)
+                        tmp_pt[0] = np.sum(pt_i * cur_pt) * self.src_pts[k][0] - \
+                                    np.sum(pt_j * cur_pt) * self.src_pts[k][1]
+                        tmp_pt[1] = -np.sum(pt_i * cur_pt_j) * self.src_pts[k][0] + \
+                                    np.sum(pt_j * cur_pt_j) * self.src_pts[k][1]
+                        tmp_pt *= (w[k] / miu_s)
+                        new_pt += tmp_pt
+
+                    new_pt += qstar
+                else:
+                    new_pt = self.src_pts[k]
+
+                self.rdx[j, i] = new_pt[0] - i
+                self.rdy[j, i] = new_pt[1] - j
+
+                j += self.grid_size
+            i += self.grid_size
+
+    def gen_img(self):
+        src_h, src_w = self.src.shape[:2]
+        dst = np.zeros_like(self.src, dtype=np.float32)
+
+        for i in np.arange(0, self.dst_h, self.grid_size):
+            for j in np.arange(0, self.dst_w, self.grid_size):
+                ni = i + self.grid_size
+                nj = j + self.grid_size
+                w = h = self.grid_size
+                if ni >= self.dst_h:
+                    ni = self.dst_h - 1
+                    h = ni - i + 1
+                if nj >= self.dst_w:
+                    nj = self.dst_w - 1
+                    w = nj - j + 1
+
+                di = np.reshape(np.arange(h), (-1, 1))
+                dj = np.reshape(np.arange(w), (1, -1))
+                delta_x = self.__bilinear_interp(di / h, dj / w,
+                                                 self.rdx[i, j], self.rdx[i, nj],
+                                                 self.rdx[ni, j], self.rdx[ni, nj])
+                delta_y = self.__bilinear_interp(di / h, dj / w,
+                                                 self.rdy[i, j], self.rdy[i, nj],
+                                                 self.rdy[ni, j], self.rdy[ni, nj])
+                nx = j + dj + delta_x * self.trans_ratio
+                ny = i + di + delta_y * self.trans_ratio
+                nx = np.clip(nx, 0, src_w - 1)
+                ny = np.clip(ny, 0, src_h - 1)
+                nxi = np.array(np.floor(nx), dtype=np.int32)
+                nyi = np.array(np.floor(ny), dtype=np.int32)
+                nxi1 = np.array(np.ceil(nx), dtype=np.int32)
+                nyi1 = np.array(np.ceil(ny), dtype=np.int32)
+
+                if len(self.src.shape) == 3:
+                    x = np.tile(np.expand_dims(ny - nyi, axis=-1), (1, 1, 3))
+                    y = np.tile(np.expand_dims(nx - nxi, axis=-1), (1, 1, 3))
+                else:
+                    x = ny - nyi
+                    y = nx - nxi
+                dst[i:i + h, j:j + w] = self.__bilinear_interp(x,
+                                                               y,
+                                                               self.src[nyi, nxi],
+                                                               self.src[nyi, nxi1],
+                                                               self.src[nyi1, nxi],
+                                                               self.src[nyi1, nxi1]
+                                                               )
+
+        dst = np.clip(dst, 0, 255)
+        dst = np.array(dst, dtype=np.uint8)
+
+        return dst
+
+def separateBN4OptimizerPG(modules):
+    paras_only_bn = []
+    paras_wo_bn = []
+    memo = set()
+    gemfield_set = set()
+    gemfield_set.update(set(modules.parameters()))
+    LOG.logI("separateBN4OptimizerPG set len: {}".format(len(gemfield_set)))
+    named_modules = modules.named_modules(prefix='')
+    for module_prefix, module in named_modules:
+        if "module" not in module_prefix:
+            LOG.logI("separateBN4OptimizerPG skip {}".format(module_prefix))
+            continue
+
+        members = module._parameters.items()
+        for k, v in members:
+            name = module_prefix + ('.' if module_prefix else '') + k
+            if v is None:
+                continue
+            if v in memo:
+                continue
+            memo.add(v)
+            if "batchnorm" in str(module.__class__):
+                paras_only_bn.append(v)
+            else:
+                paras_wo_bn.append(v)
+
+    LOG.logI("separateBN4OptimizerPG param len: {} - {}".format(len(paras_wo_bn),len(paras_only_bn)))
+    return paras_only_bn, paras_wo_bn
+
+class AverageMeter:
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+def get_image(image_path):
+    # if the image_path is a remote url, read the image at first
+    if isinstance(image_path, str):
+        image = Image.open(image_path)
+    else:
+        image = image_path
+
+    # convert non-RGB mode to RGB mode
+    if image.mode != "RGB":
+        image = image.convert("RGB")
+    return image
+
+def get_thumbnail(image):
+    image.thumbnail((256, 256))
+    return image
+
+def get_colors(image_path):
+    """ image instance
+    """
+    image = get_image(image_path)
+
+    """ image thumbnail
+        size: 256 * 256
+        reduce the calculate time 
+    """
+    thumbnail = get_thumbnail(image)
+
+
+    """ calculate the max colors the image cound have
+        if the color is different in every pixel, the color counts may be the max.
+        so : 
+        max_colors = image.height * image.width
+    """
+    image_height = thumbnail.height
+    image_width = thumbnail.width
+    max_colors = image_height * image_width
+
+    image_colors = image.getcolors(max_colors)
+    return image_colors
+
+
+def sort_by_rgb(colors_tuple):
+    """ colors_tuple contains color count and color RGB
+        we want to sort the tuple by RGB
+        tuple[1]
+    """
+    sorted_tuple = sorted(colors_tuple, key=lambda x:x[1])
+    return sorted_tuple
+
+def rgb_maximum(colors_tuple):
+    """ 
+        colors_r max min
+        colors_g max min
+        colors_b max min
+
+    """
+    r_sorted_tuple = sorted(colors_tuple, key=lambda x:x[1][0])
+    g_sorted_tuple = sorted(colors_tuple, key=lambda x:x[1][1])
+    b_sorted_tuple = sorted(colors_tuple, key=lambda x:x[1][2])
+
+    r_min = r_sorted_tuple[0][1][0]
+    g_min = g_sorted_tuple[0][1][1]
+    b_min = b_sorted_tuple[0][1][2]
+
+    r_max = r_sorted_tuple[len(colors_tuple)-1][1][0]
+    g_max = g_sorted_tuple[len(colors_tuple)-1][1][1]
+    b_max = b_sorted_tuple[len(colors_tuple)-1][1][2]
+
+    return {
+        "r_max":r_max,
+        "r_min":r_min,
+        "g_max":g_max,
+        "g_min":g_min,
+        "b_max":b_max,
+        "b_min":b_min,
+        "r_dvalue":(r_max-r_min)/3,
+        "g_dvalue":(g_max-g_min)/3,
+        "b_dvalue":(b_max-b_min)/3
+    }
+
+def group_by_accuracy(sorted_tuple, accuracy=3):
+    """ group the colors by the accuaracy was given
+        the R G B colors will be depart to accuracy parts
+        default accuracy = 3
+        d_value = (max-min)/3
+        [min, min+d_value), [min+d_value, min+d_value*2), [min+d_value*2, max)
+    """
+    rgb_maximum_json = rgb_maximum(sorted_tuple)
+    r_min = rgb_maximum_json["r_min"]
+    g_min = rgb_maximum_json["g_min"]
+    b_min = rgb_maximum_json["b_min"]
+    r_dvalue = rgb_maximum_json["r_dvalue"]
+    g_dvalue = rgb_maximum_json["g_dvalue"]
+    b_dvalue = rgb_maximum_json["b_dvalue"]
+
+    rgb = [
+            [[[], [], []], [[], [], []], [[], [], []]],
+            [[[], [], []], [[], [], []], [[], [], []]],
+            [[[], [], []], [[], [], []], [[], [], []]]
+        ]
+
+    for color_tuple in sorted_tuple:
+        r_tmp_i = color_tuple[1][0]
+        g_tmp_i = color_tuple[1][1]
+        b_tmp_i = color_tuple[1][2]
+        r_idx = 0 if r_tmp_i < (r_min+r_dvalue) else 1 if r_tmp_i < (r_min+r_dvalue*2) else 2
+        g_idx = 0 if g_tmp_i < (g_min+g_dvalue) else 1 if g_tmp_i < (g_min+g_dvalue*2) else 2
+        b_idx = 0 if b_tmp_i < (b_min+b_dvalue) else 1 if b_tmp_i < (b_min+b_dvalue*2) else 2
+        rgb[r_idx][g_idx][b_idx].append(color_tuple)
+
+    return rgb
+
+
+def get_weighted_mean(grouped_image_color):
+    """ calculate every group's weighted mean
+
+        r_weighted_mean = sigma(r * count) / sigma(count)
+        g_weighted_mean = sigma(g * count) / sigma(count)
+        b_weighted_mean = sigma(b * count) / sigma(count)
+    """
+    sigma_count = 0
+    sigma_r = 0
+    sigma_g = 0
+    sigma_b = 0
+
+    for item in grouped_image_color:
+        sigma_count += item[0]
+        sigma_r += item[1][0] * item[0]
+        sigma_g += item[1][1] * item[0]
+        sigma_b += item[1][2] * item[0]
+
+    r_weighted_mean = int(sigma_r / sigma_count)
+    g_weighted_mean = int(sigma_g / sigma_count)
+    b_weighted_mean = int(sigma_b / sigma_count)
+    
+    weighted_mean = (sigma_count, (r_weighted_mean, g_weighted_mean, b_weighted_mean))
+    return weighted_mean
+
+class Haishoku(object):
+
+    """ init Haishoku obj
+    """
+    def __init__(self):
+        self.dominant = None
+        self.palette = None
+
+    """ immediate api
+
+        1. showPalette
+        2. showDominant
+        3. getDominant
+        4. getPalette
+    """
+    def getColorsMean(image):
+        # get colors tuple 
+        image_colors = get_colors(image)
+
+        # sort the image colors tuple
+        sorted_image_colors = sort_by_rgb(image_colors)
+
+        # group the colors by the accuaracy
+        grouped_image_colors = group_by_accuracy(sorted_image_colors)
+
+        # get the weighted mean of all colors
+        colors_mean = []
+        for i in range(3):
+            for j in range(3):
+                for k in range(3):
+                    grouped_image_color = grouped_image_colors[i][j][k]
+                    if 0 != len(grouped_image_color):
+                        color_mean = get_weighted_mean(grouped_image_color)
+                        colors_mean.append(color_mean)
+
+        # return the most 8 colors
+        temp_sorted_colors_mean = sorted(colors_mean)
+        if 8 < len(temp_sorted_colors_mean):
+            colors_mean = temp_sorted_colors_mean[len(temp_sorted_colors_mean)-8 : len(temp_sorted_colors_mean)]
+        else:
+            colors_mean = temp_sorted_colors_mean
+
+        # sort the colors_mean
+        colors_mean = sorted(colors_mean, reverse=True)
+
+        return colors_mean
+        
+    def getDominant(image=None):
+        # get the colors_mean
+        colors_mean = Haishoku.getColorsMean(image)
+        colors_mean = sorted(colors_mean, reverse=True)
+
+        # get the dominant color
+        dominant_tuple = colors_mean[0]
+        dominant = dominant_tuple[1]
+        return dominant
