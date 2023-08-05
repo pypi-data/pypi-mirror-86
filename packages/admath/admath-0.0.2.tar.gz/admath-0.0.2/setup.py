11cd52df78878d826081bc3f851a6631788df8c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:01 +0000

    Add package admath/0.0.2/admath-0.0.2.tar.gz

---
+++ packages/admath/admath-0.0.2.tar.gz/admath-0.0.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+ 
+with open('README.md', 'r') as fh:
+	long_description = fh.read()
+ 
+admath_module = setuptools.Extension('admath',
+							sources=['main.cpp',
+									'info.h'
+									],
+							language='c++')
+ 
+setuptools.setup(
+	name="admath",
+	version="0.0.2",
+	author="XYZboom",
+	author_email="XYZboom@qq.com",
+	description="高级数学库",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	#url="https://github.com/lpe234/strings_pkg",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	ext_modules=[admath_module]
+)
