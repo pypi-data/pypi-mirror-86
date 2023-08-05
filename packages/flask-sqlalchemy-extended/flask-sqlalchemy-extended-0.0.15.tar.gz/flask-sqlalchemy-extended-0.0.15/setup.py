56c010f256fcb884beef36f6abfa05e966bc0675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:50 +0000

    Add package flask-sqlalchemy-extended/0.0.15/flask-sqlalchemy-extended-0.0.15.tar.gz

---
+++ packages/flask-sqlalchemy-extended/flask-sqlalchemy-extended-0.0.15.tar.gz/flask-sqlalchemy-extended-0.0.15/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="flask-sqlalchemy-extended",
+    version="0.0.15",
+    author="luanws",
+    author_email="luan.w.silveira@gmail.com",
+    description="SQLAlchemy autocomplete",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/luanws/flask-sqlalchemy-extended",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
