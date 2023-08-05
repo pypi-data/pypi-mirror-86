003509d3c3fb797bb6d2ca28bef05cb9e852b668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:04 +0000

    Add package diamondback/1.0.37/diamondback-1.0.37.tar.gz

---
+++ packages/diamondback/diamondback-1.0.37.tar.gz/diamondback-1.0.37/diamondback/interfaces/IClear.py
@@ -0,0 +1,59 @@
+""" **Description**
+
+        Clear interface.
+
+    **Example**
+
+        ::
+
+            from diamondback.interfaces.IClear import IClear
+            from diamondback.interfaces.IPhase import IPhase
+
+
+            class Test( IClear, IPhase ) :
+
+                def clear( self ) -> None :
+
+                    self.phase = 0.0
+
+            test = Test( )
+
+            test.clear( )
+
+    **License**
+
+        `BSD-3C. <https://github.com/larryturner/diamondback/blob/master/license>`_
+
+        Copyright (c) 2019, Larry Turner, Schneider Electric.  All rights reserved.
+
+    **Author**
+
+        Larry Turner, Schneider Electric, Analytics & AI, 2019-01-25.
+
+    **Definition**
+
+"""
+
+from abc import ABC, abstractmethod
+
+
+class IClear( ABC ) :
+
+    """ Clear interface.
+    """
+
+    def __init__( self ) -> None :
+
+        """ Initialize.
+        """
+
+        super( ).__init__( )
+
+    @abstractmethod
+    def clear( self ) -> None :
+
+        """ Clears an instance.
+        """
+
+        pass
+
