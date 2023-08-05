7708fa8734070766f89b0f29493386747ee742fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:42 +0000

    Add package pybiopax/0.0.5/pybiopax-0.0.5.tar.gz

---
+++ packages/pybiopax/pybiopax-0.0.5.tar.gz/pybiopax-0.0.5/pybiopax/biopax/__init__.py
@@ -0,0 +1,8 @@
+"""This module implements the BioPAX Level 3 object model as
+a set of classes with inheritance. At the top of the class
+hierarchy is the generic BioPaxObject."""
+from .base import *
+from .interaction import *
+from .physical_entity import *
+from .util import *
+from .model import *
