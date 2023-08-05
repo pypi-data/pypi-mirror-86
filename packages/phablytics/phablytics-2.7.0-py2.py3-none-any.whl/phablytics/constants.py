f0fa940df60a29544cbdbbc2fc34c266ed95bbde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:17 +0000

    Add package phablytics/2.7.0/phablytics-2.7.0-py2.py3-none-any.whl

---
+++ packages/phablytics/phablytics-2.7.0-py2.py3-none-any.whl/phablytics/constants.py
@@ -0,0 +1,26 @@
+# Phablytics
+GITHUB_URL = 'https://github.com/hacktoolkit/phablytics'
+
+
+# Phabricator
+MANIPHEST_STATUSES_OPEN = [
+    'awaitingbusiness',
+    'inprogress',
+    'open',
+    'stalled',
+]
+
+MANIPHEST_STATUSES_CLOSED = [
+    'foolish',
+    'invalid',
+    'magicallyfixed',
+    'resolved',
+    'wontfix',
+]
+
+MANIPHEST_SUBTYPES = [
+    'bug',
+    'default',
+    'feature',
+    'story',
+]
