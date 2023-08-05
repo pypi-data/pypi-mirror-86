eb49a82ef1d740877c4b0f6673fd7baaff7b73bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:37 +0000

    Add package catsh/0.1.7/catsh-0.1.7.tar.gz

---
+++ packages/catsh/catsh-0.1.7.tar.gz/catsh-0.1.7/Catsh/main.py
@@ -0,0 +1,39 @@
+"""
+Copyright (C) JheysonDev ~ All right reserved
+"""
+from Catsh.commands import Close, Clean, Print, Exec, Env
+from Catsh.utils import Error
+from colorama import Fore
+
+
+class Main:
+    def __init__(self, prompt="-| "):
+        self.prompt = prompt
+        self.cli: str = input(self.prompt)
+
+    def run(self):
+        self.shell()
+
+    def shell(self):
+        while self.cli >= "":
+            command: list = self.cli.split(" ", 1)
+            if command[0] == "print":
+                Print.PrintCommand(command[1]).run()
+            elif command[0] == "clean":
+                Clean.CleanCommand().run()
+            elif command[0] == "close":
+                Close.CloseCommand().run()
+            elif command[0] == "exec":
+                Exec.ExecCommand(command[1]).run()
+            elif command[0] == "env":
+                Env.EnvCommand(command[1]).run()
+            elif command[0] == "":
+                Error.Error("You must enter a command").run()
+            else:
+                Error.Error("Command no found").run()
+            self.cli = input(self.prompt)
+
+
+if __name__ == "__main__":
+    main = Main()
+    main.run()
---
+++ packages/catsh/catsh-0.1.7.tar.gz/catsh-0.1.7/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="catsh",
+    version="0.1.7",
+    author="JheysonDev",
+    url="https://github.com/JheysonDev/catsh",
+    license="GPLv3",
+    packages=find_packages(),
+    description="A cross-platform shell write in python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Environment :: Console",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+    install_requires=["colorama==0.4.4"],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "catsh=Catsh.main:main.run",
+        ]
+    },
+)
