3bdf32fd9fdeeea2f46b9d324aedf2e703dfde5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:28 +0000

    Add package shimoudle/1.0.0/shimoudle-1.0.0.tar.gz

---
+++ packages/shimoudle/shimoudle-1.0.0.tar.gz/shimoudle-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+
+       name          ='shimoudle',
+       version       ='1.0.0',
+       py_modules    =['shimoudle'],
+       author        ='hfpython',
+       author_email  ='hfpython@headfirstlabs.com',
+       url           ='http://www.headfirstlabs.com',
+       description   ='A simple printer of nested lists',
+
+       )
---
+++ packages/shimoudle/shimoudle-1.0.0.tar.gz/shimoudle-1.0.0/shimoudle.py
@@ -0,0 +1,9 @@
+def print_lol(the_list，level):
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,level+1)
+        else:
+            for tap_stop in range(level):
+                print("t",end='')
+            print(each_item)
+            
