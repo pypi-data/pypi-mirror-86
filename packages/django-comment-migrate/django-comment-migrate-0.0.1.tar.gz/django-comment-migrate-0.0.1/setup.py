e18ac17c4fcc921175acb5b887cc5bd709b31802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:20 +0000

    Add package django-comment-migrate/0.0.1/django-comment-migrate-0.0.1.tar.gz

---
+++ packages/django-comment-migrate/django-comment-migrate-0.0.1.tar.gz/django-comment-migrate-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='django-comment-migrate',
+    version='0.0.1',
+    packages=['django_comment_migrate'],
+    url='',
+    license='',
+    author='starryrbs',
+    install_requires=['django>=2.2'],
+    python_requires='>=3.5',
+    author_email='1322096624@qq.com',
+    description=''
+)
