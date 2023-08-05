d09fd2e670c011d8d17321cedb66b85a2c09743f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:49 +0000

    Add package salmon-lib/0.1.1/salmon_lib-0.1.1.tar.gz

---
+++ packages/salmon-lib/salmon_lib-0.1.1.tar.gz/salmon_lib-0.1.1/README.md
@@ -0,0 +1,6 @@
+# salmon_lib
+## a library to read and write CRiSP harvest files
+### born out of [the saga of salmon steve](https://salmon.sibr.dev/steve.html)
+
+#### todo
+- implement parser+writer for .fp, .enh, .cei, .msc, .pnv, .idl, .config and .monte
---
+++ packages/salmon-lib/salmon_lib-0.1.1.tar.gz/salmon_lib-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='salmon_lib',
+      version='0.1.1',
+      description='a library to read and write CRiSP harvest files',
+      url='https://github.com/alisww/salmon_lib',
+      author='alisww',
+      author_email='waylandalis@gmail.com',
+      license='MIT',
+      packages=['salmon_lib'],
+      zip_safe=False)
