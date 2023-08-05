52dc68c37c212ced6ac3e28f02054b354b410bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:08 +0000

    Add package nanome/0.18.2/nanome-0.18.2.tar.gz

---
+++ packages/nanome/nanome-0.18.2.tar.gz/nanome-0.18.2/nanome/_internal/__init__.py
@@ -0,0 +1,14 @@
+from . import *
+#folders
+from . import _util
+from . import _shapes
+from . import _structure
+from . import _ui
+from . import _network
+from . import _volumetric
+from . import _macro
+#classes
+from ._addon import _Addon
+from ._plugin_instance import _PluginInstance
+from ._room import _Room
+from ._plugin import _Plugin
---
+++ packages/nanome/nanome-0.18.2.tar.gz/nanome-0.18.2/nanome/api/__init__.py
@@ -0,0 +1,9 @@
+from . import *
+from .room import Room
+from .plugin_instance import PluginInstance, _DefaultPlugin
+from .plugin import Plugin
+from . import streams
+from . import ui
+from . import structure
+from . import macro
+from . import user
