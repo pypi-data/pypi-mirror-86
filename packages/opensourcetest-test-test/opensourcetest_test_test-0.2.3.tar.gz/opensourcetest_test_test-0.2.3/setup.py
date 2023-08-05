f0da898307d4ac763a729bd41f11fbd4a9f52afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:32 +0000

    Add package opensourcetest-test-test/0.2.3/opensourcetest_test_test-0.2.3.tar.gz

---
+++ packages/opensourcetest-test-test/opensourcetest_test_test-0.2.3.tar.gz/opensourcetest_test_test-0.2.3/setup.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['opensourcetest_test_test',
+ 'opensourcetest_test_test.builtin',
+ 'opensourcetest_test_test.common',
+ 'opensourcetest_test_test.model',
+ 'opensourcetest_test_test.model.Base',
+ 'opensourcetest_test_test.model.Common',
+ 'opensourcetest_test_test.model.Common.FileOption',
+ 'opensourcetest_test_test.model.Common.StringOption',
+ 'opensourcetest_test_test.model.Parameter',
+ 'opensourcetest_test_test.model.TestCases']
+
+package_data = \
+{'': ['*'],
+ 'opensourcetest_test_test': ['banner/*'],
+ 'opensourcetest_test_test.model': ['Conf/*', 'Parameter/Login/*']}
+
+install_requires = \
+['PyYAML>=5.1.2,<6.0.0',
+ 'allure-pytest>=2.8.19,<3.0.0',
+ 'jmespath>=0.9.5,<0.10.0',
+ 'mkdocs-material>=6.0.2,<7.0.0',
+ 'mkdocs>=1.1.2,<2.0.0',
+ 'pydantic>=1.4,<2.0',
+ 'pytest-html>=2.1.1,<3.0.0',
+ 'pytest>=5.2,<6.0',
+ 'requests>=2.22.0,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['OST = opensourcetest_test_test.cli:main',
+                     'opensourcetest_test_test = '
+                     'opensourcetest_test_test.cli:main']}
+
+setup_kwargs = {
+    'name': 'opensourcetest-test-test',
+    'version': '0.2.3',
+    'description': 'We need more free software interface testing.',
+    'long_description': '# OpenSourceTest\n夜行者社区接口自动化项目，提供的是更多地灵活性和可配置性\n',
+    'author': 'chineseluo',
+    'author_email': '848257135@qq.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/chineseluo/opensourcetest',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8.2,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
