db2ae40d463367007c26cfb9fe2396f700bdf97b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:47 +0000

    Add package APL/0.0.8/APL-0.0.8.tar.gz

---
+++ packages/APL/APL-0.0.8.tar.gz/APL-0.0.8/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+setup(
+  name='APL',
+  version='0.0.8',
+  description="A library includes my important funcs",
+  long_description="A library includes my important funcs",
+  url='https://www.youtube.com/channel/UCknfQGHQG0iXJVLOwijKdRA',  
+  author='Ahmad Draie',
+  author_email='ahmaddraa2000@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='a lot of things', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
