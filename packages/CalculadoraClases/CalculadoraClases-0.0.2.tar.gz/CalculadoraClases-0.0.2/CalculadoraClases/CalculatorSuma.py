7d969d41747794c28840f3700a114acd4951a7f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:47 +0000

    Add package CalculadoraClases/0.0.2/CalculadoraClases-0.0.2.tar.gz

---
+++ packages/CalculadoraClases/CalculadoraClases-0.0.2.tar.gz/CalculadoraClases-0.0.2/CalculadoraClases/CalculatorSuma.py
@@ -0,0 +1,38 @@
+import pickle
+import tkinter as tk
+
+#Encapsulamiento de GUI de Tkinder
+class Suma():
+    def __init__(self, master, num1, num2):
+        self.master = master
+        var1 = tk.DoubleVar()
+        t1 = tk.Entry(master, textvariable=var1)
+        t1.pack()
+
+        var2 = tk.DoubleVar()
+        t2 = tk.Entry(master, textvariable=var2)
+        t2.pack()
+
+        result = tk.DoubleVar()
+        l = tk.Label(master, textvariable=result)
+        l.pack()
+
+        #Configurar las variables
+        var1.set(num1)
+        var2.set(num2)
+        
+        result.set(var1.get() + var2.get())
+        
+#Main function
+def main(number_1, number_2): 
+
+    root = tk.Tk()
+    root.title('Sumar')
+    root.geometry("300x300")
+    app = Suma(root, number_1, number_2)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
+
+
