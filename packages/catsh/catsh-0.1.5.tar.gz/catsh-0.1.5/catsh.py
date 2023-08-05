bf96befd57327f68eb4a5ee5f75b3528962b0bfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:57 +0000

    Add package catsh/0.1.5/catsh-0.1.5.tar.gz

---
+++ packages/catsh/catsh-0.1.5.tar.gz/catsh-0.1.5/catsh.py
@@ -0,0 +1,4 @@
+from catsh.main import Main
+
+main = Main()
+main.run()
---
+++ packages/catsh/catsh-0.1.5.tar.gz/catsh-0.1.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="catsh",
+    version="0.1.5",
+    author="JheysonDev",
+    url="https://github.com/JheysonDev/catsh",
+    license="GPLv3",
+    packages=find_packages(),
+    description="A cross-platform shell write in python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Environment :: Console",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+    python_requires=">=3.6",
+)
