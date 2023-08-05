0c40e75056bb428038355f3eaa871c6cf0a74cbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:07 +0000

    Add package py3afipws/0.10.15/py3afipws-0.10.15.tar.gz

---
+++ packages/py3afipws/py3afipws-0.10.15.tar.gz/py3afipws-0.10.15/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='py3afipws',
+    packages=['py3afipws'],  # this must be the same as the name above
+    version='0.10.15',
+    description='pyafipws for python3',
+    author='Mastercore',
+    author_email='far@mastercore.net',
+    # use the URL to the github repo
+    url='https://github.com/odoo-mastercore/pyafipws',
+    download_ur3='https://github.com/odoo-mastercore/py3afipws/tarball/0.10.11',
+    keywords=['py3afipws', 'py3'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
