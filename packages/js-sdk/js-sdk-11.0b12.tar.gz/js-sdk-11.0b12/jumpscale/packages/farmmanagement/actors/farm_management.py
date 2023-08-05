0219cebaf3436f55b3c30a57cc8c125847e083ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:23 +0000

    Add package js-sdk/11.0b12/js-sdk-11.0b12.tar.gz

---
+++ packages/js-sdk/js-sdk-11.0b12.tar.gz/js-sdk-11.0b12/jumpscale/packages/farmmanagement/actors/farm_management.py
@@ -0,0 +1,30 @@
+from jumpscale.servers.gedis.baseactor import BaseActor, actor_method
+from jumpscale.clients.explorer.models import Farm
+from jumpscale.loader import j
+
+
+class FarmManagemenet(BaseActor):
+    def __init__(self):
+        super().__init__()
+        self._explorer = j.core.identity.me.explorer
+
+    @actor_method
+    def update_farm(self, farm_id, farm):
+        farm = Farm.from_dict(farm)
+        farm.id = farm_id
+        self._explorer.farms.update(farm)
+
+    @actor_method
+    def delete_node_farm(self, farm_id, node_id):
+        self._explorer.farms.delete(farm_id, node_id)
+
+    @actor_method
+    def mark_node_free(self, node_id, free):
+        return self._explorer.nodes.configure_free_to_use(node_id=node_id, free=free)
+
+    @actor_method
+    def list_farms(self, user_id) -> str:
+        return j.data.serializers.json.dumps([f.to_dict() for f in self._explorer.farms.list(user_id)])
+
+
+Actor = FarmManagemenet
