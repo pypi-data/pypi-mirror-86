18c605db4bb9338f421a90af148d216e742e4f43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:38 +0000

    Add package squeezebox-cli/0.1.8/squeezebox-cli-0.1.8.tar.gz

---
+++ packages/squeezebox-cli/squeezebox-cli-0.1.8.tar.gz/squeezebox-cli-0.1.8/squeezebox_cli/database/__init__.py
@@ -0,0 +1,3 @@
+from .commands import rescan, rescanning, rescan_playlists, rescan_progress, \
+        total_genres, total_artists, total_albums, total_songs, \
+        genres, artists, albums, years, songinfo, tracks, search
