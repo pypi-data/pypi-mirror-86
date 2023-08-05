5478caaac9647ae0a80aa047d082b78f94bf486f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:43 +0000

    Add package testwizard.set-top-box/3.2.7/testwizard.set-top-box-3.2.7.tar.gz

---
+++ packages/testwizard.set-top-box/testwizard.set-top-box-3.2.7.tar.gz/testwizard.set-top-box-3.2.7/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="testwizard.set-top-box",
+    version="3.2.7",
+    author="Eurofins Digital Testing - Belgium",
+    author_email="support-be@eurofins.com",
+    description="Testwizard for set-top box testobjects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['testwizard.set_top_box'],
+    install_requires=[
+        'testwizard.test==3.2.7',
+        'testwizard.testobjects-core==3.2.7',
+        'testwizard.commands-audio==3.2.7',
+        'testwizard.commands-mobile==3.2.7',
+        'testwizard.commands-powerswitch==3.2.7',
+        'testwizard.commands-remotecontrol==3.2.7',
+        'testwizard.commands-video==3.2.7'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.3",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
