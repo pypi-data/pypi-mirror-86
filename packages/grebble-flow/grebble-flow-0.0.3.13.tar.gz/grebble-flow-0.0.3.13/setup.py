a9fb3e914e7ee93793e7e9b8ca8f10a7fad94a22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:09 +0000

    Add package grebble-flow/0.0.3.13/grebble-flow-0.0.3.13.tar.gz

---
+++ packages/grebble-flow/grebble-flow-0.0.3.13.tar.gz/grebble-flow-0.0.3.13/setup.py
@@ -0,0 +1,30 @@
+import os
+from distutils.command.install import install
+from setuptools import setup, find_packages
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, "README.rst")).read()
+
+
+class custom_install(install):
+    def run(self):
+        install.run(self)
+        from grebble_flow.grpc.commands import install_proto
+        install_proto()
+
+
+setup(
+    name="grebble-flow",
+    version="0.0.3.13",
+    packages=find_packages(exclude=("tests", "example")),
+    description="Grebble flow",
+    long_description=README,
+    author="Greble",
+    author_email="info@greble.io",
+    url="https://github.com/greble/python-flow-helper",
+    install_requires=["click", "grpcio", "grpcio-tools"],
+    cmdclass={
+        'install': custom_install,
+    },
+)
