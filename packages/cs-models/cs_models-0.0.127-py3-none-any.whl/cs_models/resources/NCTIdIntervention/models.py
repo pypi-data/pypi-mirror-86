a10bd52c4a2ce29109f4871cb6f6ebd49052e58e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:02 +0000

    Add package cs-models/0.0.127/cs_models-0.0.127-py3-none-any.whl

---
+++ packages/cs-models/cs_models-0.0.127-py3-none-any.whl/cs_models/resources/NCTIdIntervention/models.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    DateTime,
+    UniqueConstraint,
+    ForeignKey,
+)
+
+from ...database import Base
+
+
+class NCTIdInterventionModel(Base):
+    __tablename__ = "nct_interventions"
+
+    id = Column(Integer, primary_key=True)
+    nct_id = Column(String(128), nullable=False, index=True)
+    intervention_id = Column(
+        Integer,
+        ForeignKey('interventions.id'),
+        nullable=False,
+    )
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        # https://stackoverflow.com/questions/58776476/why-doesnt-freezegun-work-with-sqlalchemy-default-values
+        default=lambda: datetime.utcnow(),
+        onupdate=lambda: datetime.utcnow(),
+    )
+
+    __table_args__ = (UniqueConstraint("nct_id", "intervention_id"),)
