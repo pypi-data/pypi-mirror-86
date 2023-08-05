853ff4969e12228a02e949eed3e907a30c8e5d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:39 +0000

    Add package sparkautomapper.fhir/0.1.50/sparkautomapper.fhir-0.1.50-py3-none-any.whl

---
+++ packages/sparkautomapper.fhir/sparkautomapper.fhir-0.1.50-py3-none-any.whl/tests/resources/patient/test_automapper_fhir_patient.py
@@ -0,0 +1,213 @@
+from typing import Dict
+
+from pyspark.sql import SparkSession, Column, DataFrame
+# noinspection PyUnresolvedReferences
+from pyspark.sql.functions import col, expr, regexp_replace, substring
+from pyspark.sql.functions import lit, struct, array, coalesce, to_date
+from spark_auto_mapper.automappers.automapper import AutoMapper
+from spark_auto_mapper.helpers.automapper_helpers import AutoMapperHelpers as A
+from spark_auto_mapper.helpers.spark_higher_order_functions import filter
+
+from spark_auto_mapper_fhir.complex_types.codeableConcept import CodeableConcept
+from spark_auto_mapper_fhir.complex_types.human_name import HumanName
+from spark_auto_mapper_fhir.complex_types.identifier import Identifier
+from spark_auto_mapper_fhir.fhir_types.id import FhirId
+from spark_auto_mapper_fhir.fhir_types.list import FhirList
+from spark_auto_mapper_fhir.resources.patient import Patient
+from spark_auto_mapper_fhir.valuesets.administrative_gender import AdministrativeGenderCode
+from spark_auto_mapper_fhir.valuesets.identifier_type import IdentifierTypeCode
+from spark_auto_mapper_fhir.valuesets.identifier_use import IdentifierUseCode
+from spark_auto_mapper_fhir.complex_types.coding import Coding
+from spark_auto_mapper_fhir.valuesets.marital_status import MaritalStatusCode
+from spark_auto_mapper_fhir.valuesets.name_use import NameUseCode
+
+
+def test_auto_mapper_fhir_patient(spark_session: SparkSession) -> None:
+    # Arrange
+    spark_session.createDataFrame(
+        [
+            (1, 'Qureshi', 'Imran', '1970-01-01', "F"),
+            (2, 'Vidal', 'Michael', '1970-02-02', "M"),
+        ],
+        ['member_id', 'last_name', 'first_name', 'date_of_birth', "my_gender"]
+    ).createOrReplaceTempView("patients")
+
+    source_df: DataFrame = spark_session.table("patients")
+
+    df = source_df.select("member_id")
+    df.createOrReplaceTempView("members")
+
+    # Act
+    mapper = AutoMapper(
+        view="members", source_view="patients", keys=["member_id"]
+    ).columns(
+        patient=Patient(
+            id_=FhirId(A.column("member_id")),
+            identifier=FhirList(
+                [
+                    Identifier(
+                        use=IdentifierUseCode.Usual,
+                        value=A.column("member_id"),
+                        type_=CodeableConcept(
+                            coding=FhirList(
+                                [
+                                    Coding(
+                                        code=IdentifierTypeCode.
+                                        MedicalRecordNumber
+                                    )
+                                ]
+                            )
+                        )
+                    )
+                ]
+            ),
+            name=FhirList(
+                [
+                    HumanName(
+                        use=NameUseCode("usual"),
+                        family=A.column("last_name"),
+                        given=FhirList(["first_name", "middle_name"])
+                    )
+                ]
+            ),
+            birthDate=A.date(A.column("date_of_birth")),
+            maritalStatus=CodeableConcept(
+                coding=FhirList(
+                    [
+                        Coding(
+                            code=MaritalStatusCode.Married,
+                            system=MaritalStatusCode.codeset
+                        )
+                    ]
+                )
+            ),
+            gender=AdministrativeGenderCode(
+                A.expression(
+                    """
+    CASE
+        WHEN `my_gender` = 'F' THEN 'female'
+        WHEN `my_gender` = 'M' THEN 'male'
+        ELSE 'other'
+    END
+    """
+                )
+            )
+        )
+    )
+
+    assert isinstance(mapper, AutoMapper)
+    sql_expressions: Dict[str, Column] = mapper.get_column_specs(
+        source_df=source_df
+    )
+    for column_name, sql_expression in sql_expressions.items():
+        print(f"{column_name}: {sql_expression}")
+
+    result_df: DataFrame = mapper.transform(df=df)
+
+    # Assert
+    assert str(sql_expressions["patient"]) == str(
+        struct(
+            lit("Patient").alias("resourceType"),
+            substring(
+                regexp_replace(col("b.member_id"), r"[^A-Za-z0-9\-\.]", "_"),
+                0, 63
+            ).alias("id"),
+            filter(
+                array(
+                    struct(
+                        lit("usual").alias("use"),
+                        struct(
+                            filter(
+                                array(
+                                    struct(
+                                        lit(None).alias("system"),
+                                        lit(None).alias("version"),
+                                        lit("MR").alias("code"),
+                                        lit(None).alias("display"),
+                                        lit(None).alias("userSelected"),
+                                    )
+                                ), lambda x: x.isNotNull()
+                            ).alias("coding"),
+                            lit(None).alias("text"),
+                        ).alias("type"),
+                        lit(None).alias("system"),
+                        col("b.member_id").alias("value"),
+                        lit(None).alias("period"),
+                        lit(None).alias("assigner"),
+                    )
+                ), lambda x: x.isNotNull()
+            ).alias("identifier"),
+            lit(None).alias("active"),
+            filter(
+                array(
+                    struct(
+                        lit("usual").alias("use"),
+                        lit(None).alias("text"),
+                        col("b.last_name").alias("family"),
+                        filter(
+                            array(lit("first_name"), lit("middle_name")),
+                            lambda x: x.isNotNull()
+                        ).alias("given"),
+                        lit(None).alias("prefix"),
+                        lit(None).alias("suffix"),
+                        lit(None).alias("period"),
+                    )
+                ), lambda x: x.isNotNull()
+            ).alias("name"),
+            lit(None).alias("telecom"),
+            expr(
+                """CASE WHEN (`my_gender` = F) THEN 'female' WHEN (`my_gender` = M) THEN 'male' ELSE other END"""
+            ).alias("gender"),
+            coalesce(
+                to_date(col("b.date_of_birth"), 'yyyy-MM-dd'),
+                to_date(col("b.date_of_birth"), 'yyyyMMdd'),
+                to_date(col("b.date_of_birth"), 'MM/dd/yyyy'),
+                to_date(col("b.date_of_birth"), 'MM/dd/yy')
+            ).alias("birthDate"),
+            lit(None).alias("deceasedBoolean"),
+            lit(None).alias("deceasedDateTime"),
+            lit(None).alias("address"),
+            struct(
+                filter(
+                    array(
+                        struct(
+                            lit(
+                                "http://terminology.hl7.org/CodeSystem/v3-MaritalStatus"
+                            ).alias("system"),
+                            lit(None).alias("version"),
+                            lit("M").alias("code"),
+                            lit(None).alias("display"),
+                            lit(None).alias("userSelected"),
+                        )
+                    ), lambda x: x.isNotNull()
+                ).alias("coding"),
+                lit(None).alias("text"),
+            ).alias("maritalStatus"),
+            lit(None).alias("multipleBirthBoolean"),
+            lit(None).alias("multipleBirthInteger"),
+            lit(None).alias("photo"),
+            lit(None).alias("contact"),
+            lit(None).alias("communication"),
+            lit(None).alias("generalPractitioner"),
+            lit(None).alias("managingOrganization"),
+            lit(None).alias("link"),
+            lit(None).alias("extension"),
+        ).alias("patient")
+    )
+
+    result_df.printSchema()
+    result_df.show()
+
+    result_df.where("member_id == 1").select("patient").show()
+    result_df.where("member_id == 1").select("patient").printSchema()
+
+    assert result_df.where("member_id == 1").selectExpr(
+        "patient.name[0].use"
+    ).collect()[0][0] == "usual"
+    assert result_df.where("member_id == 1").selectExpr(
+        "patient.name[0].family"
+    ).collect()[0][0] == "Qureshi"
+
+    # foo = FhirAdministrativeGender.male
+    #
+    # print(foo)
