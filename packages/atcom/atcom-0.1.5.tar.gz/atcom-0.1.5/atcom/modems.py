80b4c09c0114454099273ca24221d63c3d7c9a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:20 +0000

    Add package atcom/0.1.5/atcom-0.1.5.tar.gz

---
+++ packages/atcom/atcom-0.1.5.tar.gz/atcom-0.1.5/atcom/modems.py
@@ -0,0 +1,13 @@
+modems = {
+    "2c7c": { # Quectel VENDOR_ID
+        "EG25": { # Modem Name
+            "0125": ["02"] # USB_COMPOSITION_ID
+        }
+    },
+    "1bc7": { # Telit VENDOR_ID
+        "LE910": { # Modem Name
+            "1201": ["4"], # USB_COMPOSITION_ID
+            "1206": ["5"] # USB_COMPOSITION_ID
+        }
+    }
+}
