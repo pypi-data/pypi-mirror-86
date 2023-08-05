ef57a901925c72255012fa8a81b70f6ff1b034af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:13 +0000

    Add package py3simplesoap/0.10.17/py3simplesoap-0.10.17.tar.gz

---
+++ packages/py3simplesoap/py3simplesoap-0.10.17.tar.gz/py3simplesoap-0.10.17/py3simplesoap/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"Py3SimpleSOAP"
+from . import client
+from . import server
+from . import simplexml
+from . import transport
---
+++ packages/py3simplesoap/py3simplesoap-0.10.17.tar.gz/py3simplesoap-0.10.17/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='py3simplesoap',
+    packages=['py3simplesoap'],  # this must be the same as the name above
+    version='0.10.17',
+    description='py3simplesoap for python3',
+    author='Mastercore',
+    author_email='far@mastercore.net',
+    # use the URL to the github repo
+    url='https://github.com/odoo-mastercore/py3simplesoap',
+    download_url='https://github.com/odoo-mastercore/py3simplesoap/tarball/0.1',
+    keywords=['py3simplesoap', 'py3'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
