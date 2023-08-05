ec0864081900d3a725c173a98cf9ca9f9c394863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:49 +0000

    Add package kodexa/2.3.119/kodexa-2.3.119.tar.gz

---
+++ packages/kodexa/kodexa-2.3.119.tar.gz/kodexa-2.3.119/kodexa/__init__.py
@@ -0,0 +1,17 @@
+"""
+    Kodexa is a Python framework to enable flexible data engineering with semi-structured and unstructured documents and
+    data.
+"""
+from .cloud import RemoteSession, RemotePipeline, RemoteAction, KodexaPlatform
+from .connectors import FolderConnector, FileHandleConnector, UrlConnector, registered_connectors, get_connectors, \
+    get_connector, add_connector, get_source
+from .extractors import *
+from .model import DocumentMetadata, ContentNode, ContentFeature, SourceMetadata, \
+    Document, LocalModelStore, RemoteStore, RemoteModelStore, DocumentStore
+from .pipeline import Pipeline, PipelineStatistics, PipelineContext
+from .sinks import InMemoryDocumentSink, FolderSink
+from .steps import NodeTagger, TextParser, RollupTransformer, TagsToKeyValuePairExtractor
+from .stores import JsonDocumentStore, TableDataStore, DictDataStore, DataStoreHelper, LocalDocumentStore, \
+    RemoteDocumentStore
+from .workflow import Workflow
+from .taxonomy import Taxonomy
