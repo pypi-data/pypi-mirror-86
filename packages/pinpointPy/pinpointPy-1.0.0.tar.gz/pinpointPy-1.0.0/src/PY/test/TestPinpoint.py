2e8c7ec897c63aa335b4222b58284a9e1053dbe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:40 +0000

    Add package pinpointPy/1.0.0/pinpointPy-1.0.0.tar.gz

---
+++ packages/pinpointPy/pinpointPy-1.0.0.tar.gz/pinpointPy-1.0.0/src/PY/test/TestPinpoint.py
@@ -0,0 +1,58 @@
+﻿from unittest import TestCase
+import unittest
+import pinpointPy
+import sys
+
+
+def output(msg):
+    print(msg)
+
+class TestAgent(TestCase):
+     
+    def setUp(self):
+        # self.assertTrue(pinpointPy.enable_debug(output))
+        # self.assertTrue(pinpointPy.enable_debug(None))
+        self.assertTrue(pinpointPy.set_agent(collector_host='unix:/tmp/collector-agent.sock'))
+
+    def test_trace_life(self):
+        pinpointPy.start_trace()
+        pinpointPy.start_trace()
+        pinpointPy.start_trace()
+        pinpointPy.start_trace()
+        pinpointPy.add_clue("key","value")
+        pinpointPy.add_clue("key","value3")
+
+        pinpointPy.add_clues("key","values")
+        pinpointPy.add_clues("key","values")
+        pinpointPy.add_clues("key","values")
+
+        pinpointPy.end_trace()
+        pinpointPy.end_trace()
+        pinpointPy.end_trace()
+        pinpointPy.end_trace()
+
+    def test_set_collector_host(self):
+        self.assertTrue(pinpointPy.set_agent(collector_host='unix:/tmp/collector1.sock'))
+        self.assertTrue(pinpointPy.set_agent(collector_host='Unix:/tmp/collector1.sock'))
+        self.assertTrue(pinpointPy.set_agent(collector_host='TCP:dev-collector:11331'))
+        self.assertTrue(pinpointPy.set_agent(collector_host='Tcp:dev-collector:11331'))
+
+        try:
+            pinpointPy.set_agent(collector_host='dev-collector:11331')
+            self.assertFalse(1)
+        except:
+            pass
+        try:
+            self.assertFalse(pinpointPy.set_agent(collector_host='/tmp/collector1.sock'))
+            self.assertFalse(1)
+        except:
+            pass
+        
+        self.assertTrue(pinpointPy.set_agent(collector_host='Tcp:dev-collector:11331',trace_limit=1000))
+
+
+# pinpointPy.start_trace()
+# pinpointPy.end_trace()
+
+if __name__ == '__main__':
+    unittest.main()
