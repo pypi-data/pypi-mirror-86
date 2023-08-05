69e5deea5481ef33e5bde52088fa4fa4048b5ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:06 +0000

    Add package fpapicli/1.0.1/fpapicli-1.0.1.tar.gz

---
+++ packages/fpapicli/fpapicli-1.0.1.tar.gz/fpapicli-1.0.1/README.md
@@ -0,0 +1,17 @@
+# fpapicli
+This is a Python CLI for Football Player API.
+
+Link to API : [footballplayer-api](http://footballplayerapi.herokuapp.com/)
+
+## Install
+-- pip install fpapicli
+
+-- pip install -e .
+
+## Usage
+
+-> fpapicli get-all
+
+-> fpapicli get-player
+
+for help -> fpapicli --help
---
+++ packages/fpapicli/fpapicli-1.0.1.tar.gz/fpapicli-1.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="fpapicli",
+    version='1.0.1',
+    author="Pranav V P",
+    author_email="pranavvp07@gmail.com",
+    description="CLI for Football Player API",
+    long_description="CLI for football player API. API source : http://footballplayerapi.herokuapp.com/",
+    url="https://github.com/pranavvp10/fpapicli",
+    packages=find_packages(),
+    install_requires=[
+        'Click',
+        'Requests'
+    ],
+    keywords=['python', 'cli','api'],
+    entry_points='''
+        [console_scripts]
+        fpapicli=fpapicli.fpapicli:fpapicli
+    ''',
+    classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+            "License :: OSI Approved :: MIT License"
+        ]
+)
