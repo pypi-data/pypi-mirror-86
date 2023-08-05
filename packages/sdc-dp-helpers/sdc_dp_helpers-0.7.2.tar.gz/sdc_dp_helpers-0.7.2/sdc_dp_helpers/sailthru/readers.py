dc5adf9e78aaaffc8fa6a7215bb4478b1b16963a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:19 +0000

    Add package sdc-dp-helpers/0.7.2/sdc_dp_helpers-0.7.2.tar.gz

---
+++ packages/sdc-dp-helpers/sdc_dp_helpers-0.7.2.tar.gz/sdc_dp_helpers-0.7.2/sdc_dp_helpers/sailthru/readers.py
@@ -0,0 +1,46 @@
+"""
+    CUSTOM READERS CLASSES
+        - Class which manages reader tasks like auth, requests, pagination
+"""
+import json
+
+from sailthru import SailthruClient
+
+
+class CustomSailThruReader():
+    """
+        Custom SailThru Reader
+    """
+
+    # pylint: disable=super-init-not-called
+    def __init__(self, api_keys_path: str):
+        self.sailthru_client = self.auth_credentials(api_keys_path)
+
+    @staticmethod
+    def auth_credentials(
+            api_keys_path
+    ):
+        """
+        Create a client to communicate with Sailthru
+        Args:
+            api_keys_path: The file path to the api key and secret.
+        Returns:
+            A service that is connected to the specified API.
+        """
+        with open(api_keys_path) as json_file:
+            data = json.load(json_file)
+
+        return SailthruClient(data['api_key'], data['api_secret'])
+
+    def call_api(self, query: dict):
+        """
+            returns results of API call
+        """
+        response = self.sailthru_client.api_get(query.get('endpoint'), query.get('parameters')[0])
+        if not response.is_ok():
+            error = response.get_error()
+            raise Exception("APIError: {}, Status Code: {}, Error Code: {}".format(
+                error.get_message(),
+                str(response.get_status_code()),
+                str(error.get_error_code())))
+        return response.get_body()
