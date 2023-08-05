6933eec69790dae06a252cd8853ef5e86a22918b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:46 +0000

    Add package TelegramBotInterface/4.1/TelegramBotInterface-4.1.tar.gz

---
+++ packages/TelegramBotInterface/TelegramBotInterface-4.1.tar.gz/TelegramBotInterface-4.1/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+from os import path
+
+
+setup(
+  name = 'TelegramBotInterface',         # How you named your package folder (MyLib)
+  packages = ['TelegramBotInterface'],   # Chose the same as "name"
+  version = '4.1',                # Start with a small number and increase it with every change you make
+  license='mit',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'This a module who is work like an interface to telegram bot API',   # Give a short description about your library
+  long_description = 'All Docs on https://github.com/emaaForlin/TelegramBotInterface',
+  long_description_content_type = 'text/markdown',
+  author = 'Emanuel Forlin',                   # Type in your name
+  author_email = 'emaforlin@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/emaaForlin/TelegramBotInterface',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/emaaForlin/TelegramBotInterface/archive/v4.1.tar.gz',    # I explain this later on
+  keywords = ['API', 'BOT', 'TELEGRAM', 'INTERFACE'],   # Keywords that define your package best
+  install_requires=['requests'],            # I get to this in a second
+  classifiers=[
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support'
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8'
+    ],
+)
