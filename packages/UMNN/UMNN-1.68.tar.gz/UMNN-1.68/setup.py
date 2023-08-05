3804e797df062eb7bd2b00081979285db965335d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:31 +0000

    Add package UMNN/1.68/UMNN-1.68.tar.gz

---
+++ packages/UMNN/UMNN-1.68.tar.gz/UMNN-1.68/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+import setuptools
+
+setup(
+    name='UMNN',
+    version='1.68',
+    author="Antoine Wehenkel",
+    author_email="antoine.wehenkel@gmail.com",
+    description="Implementation of unconstrained monotonic neural networks and their application for autoregressive normalizing flows.",
+    long_description='',
+    long_description_content_type="text/markdown",
+    url="https://github.com/awehenkel/umnn",
+    packages=setuptools.find_packages(),
+    classifiers=[
+     "Programming Language :: Python :: 3",
+     "License :: OSI Approved :: MIT License",
+     "Operating System :: OS Independent",
+     ],
+)
+
