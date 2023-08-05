c1d168a025e76f225f17494d5c5c2395f464b973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:13 +0000

    Add package labml/0.4.61/labml-0.4.61-py3-none-any.whl

---
+++ packages/labml/labml-0.4.61-py3-none-any.whl/labml_helpers/metrics/accuracy.py
@@ -0,0 +1,44 @@
+import dataclasses
+
+import torch
+
+from labml import tracker
+from . import Metric
+
+
+@dataclasses.dataclass
+class AccuracyState:
+    samples: int = 0
+    correct: int = 0
+
+    def reset(self):
+        self.samples = 0
+        self.correct = 0
+
+
+class Accuracy(Metric):
+    data: AccuracyState
+
+    def __init__(self):
+        super().__init__()
+
+    def __call__(self, output: torch.Tensor, target: torch.Tensor):
+        output = output.view(-1, output.shape[-1])
+        target = target.view(-1)
+        pred = output.argmax(dim=-1)
+        self.data.correct += pred.eq(target).sum().item()
+        self.data.samples += len(target)
+
+    def create_state(self):
+        return AccuracyState()
+
+    def set_state(self, data: any):
+        self.data = data
+
+    def on_epoch_start(self):
+        self.data.reset()
+
+    def on_epoch_end(self):
+        if self.data.samples == 0:
+            return
+        tracker.add("accuracy.", self.data.correct / self.data.samples)
