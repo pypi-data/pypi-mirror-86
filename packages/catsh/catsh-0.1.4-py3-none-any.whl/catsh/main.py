55150d6c9c22647f57038cbda362d8f545c4376a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:19 +0000

    Add package catsh/0.1.4/catsh-0.1.4-py3-none-any.whl

---
+++ packages/catsh/catsh-0.1.4-py3-none-any.whl/catsh/main.py
@@ -0,0 +1,36 @@
+"""
+Copyright (C) JheysonDev ~ All right reserved
+"""
+from commands import Close, Clean, Print, Exec, Env
+from utils import Error
+from colorama import Fore
+
+
+class Main:
+    def __init__(self, prompt="-| "):
+        self.prompt = prompt
+        self.cli: str = input(self.prompt)
+
+    def shell(self):
+        while self.cli >= "":
+            command: list = self.cli.split(" ", 1)
+            if command[0] == "print":
+                Print.PrintCommand(command[1]).run()
+            elif command[0] == "clean":
+                Clean.CleanCommand().run()
+            elif command[0] == "close":
+                Close.CloseCommand().run()
+            elif command[0] == "exec":
+                Exec.ExecCommand(command[1]).run()
+            elif command[0] == "env":
+                Env.EnvCommand(command[1]).run()
+            elif command[0] == "":
+                Error.Error("You must enter a command").run()
+            else:
+                Error.Error("Command no found").run()
+            self.cli = input(self.prompt)
+
+
+if __name__ == "__main__":
+    main = Main()
+    main.shell()
