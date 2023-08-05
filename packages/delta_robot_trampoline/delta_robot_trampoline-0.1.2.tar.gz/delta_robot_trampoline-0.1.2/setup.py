91c7eca36572999c06c314298b8cd54ca15ece20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:04 +0000

    Add package delta_robot_trampoline/0.1.2/delta_robot_trampoline-0.1.2.tar.gz

---
+++ packages/delta_robot_trampoline/delta_robot_trampoline-0.1.2.tar.gz/delta_robot_trampoline-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='delta_robot_trampoline',
+      version='0.1.2',
+      description='A Delta robot that bounces a soccer ball',
+      url='https://github.com/RicoJia/delta_robot_trampoline',
+      author='Rico Ruotong Jia',
+      author_email='ruotongjia2020@u.northwestern.edu',
+      license='BSD',
+      install_requires=['numpy', 'pybullet'],
+      packages=['delta_robot_trampoline'],
+      zip_safe=False,
+      python_requires='>=3')
+
