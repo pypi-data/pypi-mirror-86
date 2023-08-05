814bf7677ebe1b9fa7add3ed085bc2e006a69ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:55 +0000

    Add package hejmdal/0.1.6/hejmdal-0.1.6-py3-none-any.whl

---
+++ packages/hejmdal/hejmdal-0.1.6-py3-none-any.whl/hejmdal/__init__.py
@@ -0,0 +1,10 @@
+from .base import *
+
+__all__ =  ['base', 'formatting', 'forms', 'datacalls', 'operations', 'main']
+
+__version__ = "0.1.6"
+
+from . import formatting
+from . import forms
+from . import datacalls
+from . import main
