eed0a124ef759fe69c3398c142553f18e7c89c4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:35 +0000

    Add package mpl-interactions/0.12.0/mpl_interactions-0.12.0.tar.gz

---
+++ packages/mpl-interactions/mpl_interactions-0.12.0.tar.gz/mpl_interactions-0.12.0/mpl_interactions/ipyplot.py
@@ -0,0 +1,6 @@
+from .pyplot import interactive_hist as hist
+from .pyplot import interactive_plot as plot
+from .pyplot import interactive_imshow as imshow
+from .pyplot import interactive_scatter as scatter
+from .pyplot import interactive_axhline as axhline
+from .pyplot import interactive_axvline as axvline
