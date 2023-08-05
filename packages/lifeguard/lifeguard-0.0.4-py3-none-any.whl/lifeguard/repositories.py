0627cfa9d308016b099c73c507ca9ed5d0220705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:11 +0000

    Add package lifeguard/0.0.4/lifeguard-0.0.4-py3-none-any.whl

---
+++ packages/lifeguard/lifeguard-0.0.4-py3-none-any.whl/lifeguard/repositories.py
@@ -0,0 +1,43 @@
+"""
+Interface repositories
+"""
+from datetime import datetime
+from lifeguard.logger import lifeguard_logger as logger
+
+IMPLEMENTATIONS = {}
+
+
+class BaseRepository(object):
+    def __init_repository__(self, repository):
+        self.__implementation__ = IMPLEMENTATIONS[repository]
+
+
+class ValidationRepository(BaseRepository):
+    def __init__(self):
+        BaseRepository.__init_repository__(self, "validation")
+
+    def save_validation_result(self, validation_result):
+        validation_result.last_execution = datetime.now()
+        self.__implementation__.save_validation_result(validation_result)
+
+    def fetch_last_validation_result(self, validation_name):
+        return self.__implementation__.fetch_last_validation_result(validation_name)
+
+
+def declare_implementation(repository, implementation):
+
+    if not implementation:
+        return
+
+    if repository in IMPLEMENTATIONS:
+        logger.warning("overwriting implementation for respository %s", repository)
+    logger.info(
+        "loading implementation %s for repository %s", implementation, repository,
+    )
+    IMPLEMENTATIONS[repository] = load_implementation(implementation)
+
+
+def load_implementation(implementation):
+    package_path = implementation.split(".")
+    class_name = package_path.pop(-1)
+    return getattr(__import__(".".join(package_path)), class_name)()
