b6aaa518ca609095dc9914b484ee69ae4a13a4f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:45 +0000

    Add package dnd-character/20.11.18/dnd_character-20.11.18-py3-none-any.whl

---
+++ packages/dnd-character/dnd_character-20.11.18-py3-none-any.whl/dnd_character/__init__.py
@@ -0,0 +1,27 @@
+"""
+dnd-character is a Python package for integrating DnD characters
+into external applications.
+"""
+
+
+__author__ = "Brianna Rainey"
+__copyright__ = "Copyright 2019-2020 Brianna Rainey & Markis Cook"
+__credits__ = ["Brianna Rainey (Current Programmer)", "Markis Cook (Original Creator)"]
+__license__ = "EPL-2.0"
+__version__ = "20.11.18"
+__maintainer__ = "Brianna Rainey"
+
+
+from .character import Character as CharacterObj
+from .classes import *
+
+
+def Character(**kwargs):
+    """
+    Factory function for a new Character
+    """
+    new = CharacterObj(**kwargs)
+    if "experience" in kwargs:
+        new.experience = kwargs["experience"]
+        new._level = kwargs.get("level", new._level)
+    return new
