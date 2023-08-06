9655a721318db8468142f31bcdc0da002632381b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:54 +0000

    Add package pycells/0.1.7/pycells-0.1.7.tar.gz

---
+++ packages/pycells/pycells-0.1.7.tar.gz/pycells-0.1.7/pycells/__init__.py
@@ -0,0 +1,3 @@
+from .simulations import count, pattern
+
+__all__ = ("count", "pattern")
---
+++ packages/pycells/pycells-0.1.7.tar.gz/pycells-0.1.7/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pycells",
+    version="v0.1.7",
+    author="Tim Fischer",
+    author_email="t.fischer98@hotmail.com",
+    description="A small package for simulating simple cellular automata.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/timfi/pycells",
+    packages=["pycells"],
+    package_data={"pycells": ["presets/*.yml"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={'console_scripts': ['pycells = pycells.cli:cli']},
+    python_requires='>=3.7',
+    install_requires=[
+        "Click==7.0",
+        "numpy==1.18.1",
+        "Pillow==7.0.0",
+        "PyYAML==5.3",
+    ]
+)
