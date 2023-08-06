147ffe545e0e1d1c4356652239c3b27c43eb8a50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:11 +0000

    Add package SamplerBox/0.0.1/SamplerBox-0.0.1.tar.gz

---
+++ packages/SamplerBox/SamplerBox-0.0.1.tar.gz/SamplerBox-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='SamplerBox',
+    version='0.0.1',
+    author="Joseph Ernest",
+    author_email="contact@samplerbox.org",
+    description="SamplerBox is an open-source audio sampler project based on RaspberryPi.",
+    long_description="No release on PyPI yet, see https://github.com/josephernest/SamplerBox.",
+    url="https://github.com/josephernest/SamplerBox",
+    platforms='any',
+    license='CC BY-SA 3.0',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
