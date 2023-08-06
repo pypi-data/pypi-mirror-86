be477ff393fd63c4880481a42060c7ef52b71865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:27 +0000

    Add package vade-api/0.3/vade_api-0.3-py3-none-any.whl

---
+++ packages/vade-api/vade_api-0.3-py3-none-any.whl/vade_api_src/zone_crud.py
@@ -0,0 +1,11 @@
+from vade_api_src.vade_enums import *
+
+
+class ZoneCrud:
+
+    api_key: str
+    production_level: ProductionLevel
+
+    def __init__(self, api_key: str, production_level: ProductionLevel):
+        self.production_level = production_level
+        self.api_key = api_key
---
+++ packages/vade-api/vade_api-0.3-py3-none-any.whl/vade_crud_api_src/vade_enums.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class ProductionLevel(Enum):
+    DEV = "dev"
+    BETA = "beta"
+    PROD = "prod"
