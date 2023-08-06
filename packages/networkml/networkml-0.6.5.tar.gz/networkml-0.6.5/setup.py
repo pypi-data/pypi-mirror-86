58a938f7b0b27aa3cc72525dd7b8d08a54f1d97d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:18 +0000

    Add package networkml/0.6.5/networkml-0.6.5.tar.gz

---
+++ packages/networkml/networkml-0.6.5.tar.gz/networkml-0.6.5/setup.py
@@ -0,0 +1,21 @@
+# WARNING about imp deprecation because of setuptools
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='networkml',
+    version=open('VERSION', 'r').read().strip(),
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=open('requirements.txt', 'r').read().splitlines(),
+    scripts=['bin/networkml'],
+    license='Apache License 2.0',
+    author='cglewis',
+    author_email='clewis@iqt.org',
+    maintainer='cglewis',
+    maintainer_email='clewis@iqt.org',
+    description=(
+        'A utility package for extracting and analyzing features in network traffic.'),
+    keywords='machine learning network analysis utilities',
+    url='https://github.com/IQTLabs/NetworkML',
+)
