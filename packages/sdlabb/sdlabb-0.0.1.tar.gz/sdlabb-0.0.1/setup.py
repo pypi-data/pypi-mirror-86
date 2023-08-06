060fe106771415f44594c41a661626691063d6c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:09 +0000

    Add package sdlabb/0.0.1/sdlabb-0.0.1.tar.gz

---
+++ packages/sdlabb/sdlabb-0.0.1.tar.gz/sdlabb-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+    
+setup(name = 'sdlabb',
+      version = '0.0.1',
+      description = 'Code for Svenskt språkdatalabb',
+      author = 'Fredrik Möller',
+      author_email = 'fredrikmoller@recordefuture.com',
+      long_description = readme(),
+      long_description_content_type = 'text/markdown',
+      url = 'https://github.com/BernhardMoller/sdlabb',
+      packagages = find_packages(exclude=("tests",)),
+	classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+      python_requires='>=3.6',
+      )
+
+ 
