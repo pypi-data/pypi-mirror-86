1d707fe129683c2bd4ff4d08b45f153493b3926f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:04 +0000

    Add package imagemap/0.0.0/imagemap-0.0.0.tar.gz

---
+++ packages/imagemap/imagemap-0.0.0.tar.gz/imagemap-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='imagemap',
+    version='0.0.0',
+    url='https://github.com/njanakiev/imagemap.git',
+    author='Author Name',
+    author_email='nikolai.janakiev@gmail.com',
+    description='imagemap',
+    platforms='any',
+    packages=['imagemap'],    
+    install_requires=[],
+)
