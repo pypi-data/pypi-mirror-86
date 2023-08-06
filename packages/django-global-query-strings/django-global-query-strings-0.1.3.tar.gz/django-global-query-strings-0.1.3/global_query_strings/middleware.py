dd21fb5527e51aa5d65520eb2122df0c869f9d69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:31 +0000

    Add package django-global-query-strings/0.1.3/django-global-query-strings-0.1.3.tar.gz

---
+++ packages/django-global-query-strings/django-global-query-strings-0.1.3.tar.gz/django-global-query-strings-0.1.3/global_query_strings/middleware.py
@@ -0,0 +1,21 @@
+from django.http import StreamingHttpResponse
+
+from .utils import add_query_strings_to_links
+
+
+class GlobalQueryStringsMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+
+        # Handle image in Content-Type response
+        # and handle StreamingHttpResponse response such as large CSV or SVG files.
+        if (
+            "Content-Type" in response and "image" in response["Content-Type"]
+        ) or isinstance(response, StreamingHttpResponse):
+            return response
+
+        response.content = add_query_strings_to_links(response.content)
+        return response
