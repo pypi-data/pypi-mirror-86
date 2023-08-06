f3e12ef7116ef426c0f84f753a9109bcc4780779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:56 +0000

    Add package alps/0.0.1/alps-0.0.1.tar.gz

---
+++ packages/alps/alps-0.0.1.tar.gz/alps-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="alps",
+    version="0.0.1",
+    author="Jiri Kuncar",
+    author_email="jiri.kuncar@gmail.com",
+    description="ALPS",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/jirikuncar/alps",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
