b224f41150b359fad0469ae319d8bbd4a3e77fa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:38 +0000

    Add package dathost/0.1.5/dathost-0.1.5-py3-none-any.whl

---
+++ packages/dathost/dathost-0.1.5-py3-none-any.whl/dathost/http/base.py
@@ -0,0 +1,43 @@
+import typing
+
+from httpx import Response
+
+from ..exceptions import (
+    NotFound,
+    BadRequest,
+    ExceededStorage,
+    ServerStart
+)
+
+
+class BaseHttp:
+    def handle_resp(self, resp: Response, json: bool = True,
+                    read: bool = True) -> typing.Any:
+        """Handles resp response.
+
+        Parameters
+        ----------
+        resp : Response
+        json : bool, optional
+            by default True
+        read : bool, optional
+            by default True
+        """
+
+        if resp.status_code == 200:
+            if json:
+                return resp.json()
+            elif read:
+                return resp.read()
+            else:
+                return True
+        elif resp.status_code == 404:
+            raise NotFound()
+        elif resp.status_code == 400:
+            raise BadRequest()
+        elif resp.status_code == 507:
+            raise ExceededStorage()
+        elif resp.status_code == 500:
+            raise ServerStart()
+        else:
+            resp.raise_for_status()
