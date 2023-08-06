7f99e160108afbf3085872139541c50a964603e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:16 +0000

    Add package pytapo/1.2/pytapo-1.2-py3-none-any.whl

---
+++ packages/pytapo/pytapo-1.2-py3-none-any.whl/pytapo/const.py
@@ -0,0 +1,12 @@
+ERROR_CODES = {
+    "-40401": "Invalid stok value",
+    "-64324": "Privacy mode is ON, not able to execute",
+    "-64302": "Preset ID not found",
+    "-64321": "Preset ID was deleted so no longer exists",
+    "-40106": "Parameter to get/do does not exist",
+    "-40105": "Method does not exist",
+    "-40101": "Parameter to set does not exist",
+}
+DEVICE_MODEL_C100 = "C100"
+DEVICE_MODEL_C200 = "C200"
+DEVICES_WITH_NO_PRESETS = [DEVICE_MODEL_C100]
