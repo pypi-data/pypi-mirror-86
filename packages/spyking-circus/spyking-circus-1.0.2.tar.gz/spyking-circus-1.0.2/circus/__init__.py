681b6bd633452ab3bf52691d7a3fb157cc64ce7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:44 +0000

    Add package spyking-circus/1.0.2/spyking-circus-1.0.2.tar.gz

---
+++ packages/spyking-circus/spyking-circus-1.0.2.tar.gz/spyking-circus-1.0.2/circus/__init__.py
@@ -0,0 +1,23 @@
+import importlib
+# import logging
+
+
+__version__ = '1.0.2'
+
+
+def launch(task, filename, nb_cpu, nb_gpu, use_gpu, output=None, benchmark=None, extension='', sim_same_elec=None):
+
+    from circus.shared.parser import CircusParser
+    params = CircusParser(filename)
+
+    if task not in ['filtering', 'benchmarking']:
+        params.get_data_file()
+
+    module = importlib.import_module('circus.' + task)
+
+    if task == 'benchmarking':
+        module.main(params, nb_cpu, nb_gpu, use_gpu, output, benchmark, sim_same_elec)
+    elif task in ['converting', 'deconverting', 'merging']:
+        module.main(params, nb_cpu, nb_gpu, use_gpu, extension)
+    else:
+        module.main(params, nb_cpu, nb_gpu, use_gpu)
