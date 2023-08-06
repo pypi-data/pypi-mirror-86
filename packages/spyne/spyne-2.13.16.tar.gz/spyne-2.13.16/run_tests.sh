b9d84c7fd280523dd0b81793f62b90e2e7831267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:46 +0000

    Add package spyne/2.13.16/spyne-2.13.16.tar.gz

---
+++ packages/spyne/spyne-2.13.16.tar.gz/spyne-2.13.16/run_tests.sh
@@ -0,0 +1,206 @@
+#!/bin/bash -x
+#
+# Sets up a Python testing environment from scratch. Mainly written for Jenkins.
+# Works for CPython. Not working for Jython, IronPython and PyPy.
+#
+# Requirements:
+#   A working build environment inside the container with OpenSSL, bzip2,
+#   libxml2 and libxslt development files. Only tested on Linux variants.
+#
+#   Last time we looked, for ubuntu, that meant:
+#     $ sudo apt-get install build-essential libssl-dev lilbbz2-dev \
+#                                                       libxml2-dev libxslt-dev
+#
+# Usage:
+#   Example:
+#
+#     $ PYFLAV=cpy-3.3 ./run_tests.sh
+#
+#   Variables:
+#     - PYFLAV: Defaults to 'cpy-2.7'. See other values below.
+#     - WORKSPACE: Defaults to $PWD. It's normally set by Jenkins.
+#     - MAKEOPTS: Defaults to '-j2'.
+#     - MONOVER: Defaults to '2.11.4'. Only relevant for ipy-* flavors.
+#
+# Jenkins guide:
+#   1. Create a 'Multi configuration project'.
+#   2. Set up stuff like git repo the usual way.
+#   3. In the 'Configuration Matrix' section, create a user-defined axis named
+#      'PYVER'. and set it to the Python versions you'd like to test, separated
+#      by whitespace. For example: 'cpy-2.7 cpy-3.4'
+#   4. Add a new "Execute Shell" build step and type in './run_tests.sh'.
+#   5. Add a new "Publish JUnit test report" post-build action and type in
+#      'test_result.*.xml'
+#   6. Add a new "Publish Cobertura Coverage Report" post-build action and type
+#      in 'coverage.xml'. Install the "Cobertura Coverage Report" plug-in if you
+#      don't see this option.
+#   7. Nonprofit!
+#
+
+
+# Sanitization
+[ -z "$PYFLAV" ] && PYFLAV=cpy-2.7;
+[ -z "$MONOVER" ] && MONOVER=2.11.4;
+[ -z "$WORKSPACE" ] && WORKSPACE="$PWD";
+[ -z "$MAKEOPTS" ] && MAKEOPTS="-j2";
+
+PYIMPL=(${PYFLAV//-/ });
+PYVER=${PYIMPL[1]};
+PYFLAV="${PYFLAV/-/}";
+PYFLAV="${PYFLAV/./}";
+if [ -z "$PYVER" ]; then
+    PYVER=${PYIMPL[0]};
+    PYIMPL=cpy;
+    PYFLAV=cpy${PYVER/./};
+else
+    PYIMPL=${PYIMPL[0]};
+fi
+
+PYNAME=python$PYVER;
+
+if [ -z "$FN" ]; then
+    declare -A URLS;
+    URLS["cpy27"]="2.7.17/Python-2.7.17.tar.xz";
+    URLS["cpy35"]="3.5.10/Python-3.5.10.tar.xz";
+    URLS["cpy36"]="3.6.12/Python-3.6.12.tar.xz";
+    URLS["cpy37"]="3.7.9/Python-3.7.9.tar.xz";
+    URLS["cpy38"]="3.8.6/Python-3.8.6.tar.xz";
+    URLS["cpy39"]="3.9.0/Python-3.9.0.tar.xz";
+    URLS["jyt27"]="2.7.1/jython-installer-2.7.1.jar";
+    URLS["ipy27"]="ipy-2.7.4.zip";
+
+    FN="${URLS["$PYFLAV"]}";
+
+    if [ -z "$FN" ]; then
+        echo "Unknown Python version $PYFLAV";
+        exit 2;
+    fi;
+fi;
+
+
+# Initialization
+IRONPYTHON_URL_BASE=https://github.com/IronLanguages/main/archive;
+CPYTHON_URL_BASE=http://www.python.org/ftp/python;
+JYTHON_URL_BASE=http://search.maven.org/remotecontent?filepath=org/python/jython-installer;
+MAKE="make $MAKEOPTS";
+
+
+# Set specific variables
+if [ $PYIMPL == "cpy" ]; then
+    PREFIX="$(basename $(basename $FN .tgz) .tar.xz)";
+
+elif [ $PYIMPL == "ipy" ]; then
+    PREFIX="$(basename $FN .zip)";
+    MONOPREFIX="$WORKSPACE/mono-$MONOVER"
+    XBUILD="$MONOPREFIX/bin/xbuild"
+
+elif [ $PYIMPL == "jyt" ]; then
+    PYNAME=jython;
+    PREFIX="$(basename $FN .jar)";
+fi;
+
+# Set common variables
+PYTHON="$WORKSPACE/$PREFIX/bin/$PYNAME";
+PIP="$WORKSPACE/$PREFIX/bin/pip$PYVER";
+TOX="$WORKSPACE/$PREFIX/bin/tox";
+TOX2="$HOME/.local/bin/tox"
+
+
+# Set up requested python environment.
+if [ $PYIMPL == 'cpy' ]; then
+    if [ ! -x "$PYTHON" ]; then
+      (
+        mkdir -p .data; cd .data;
+
+        wget -ct0 $CPYTHON_URL_BASE/$FN;
+        tar xf $(basename $FN);
+        cd "$PREFIX";
+        if [ ! -f "Makefile" ]; then
+            ./configure --prefix="$WORKSPACE/$PREFIX" --without-pydebug --with-ensurepip;
+        fi
+
+        $MAKE && make install;
+      );
+    fi;
+
+    export PATH="$WORKSPACE/$PREFIX/bin":"$PATH";
+
+elif [ $PYIMPL == 'jyt' ]; then
+    if [ ! -x "$PYTHON" ]; then
+      (
+        mkdir -p .data; cd .data;
+
+        FILE=$(basename $FN);
+        wget -O $FILE -ct0 "$JYTHON_URL_BASE/$FN";
+        java -jar $FILE -s -d "$WORKSPACE/$PREFIX"
+
+      );
+    fi
+
+elif [ $PYIMPL == 'ipy' ]; then
+    # Set up Mono first
+    # See: http://www.mono-project.com/Compiling_Mono_From_Tarball
+    if [ ! -x "$XBUILD" ]; then
+      (
+        mkdir -p .data; cd .data;
+
+        wget -ct0 http://download.mono-project.com/sources/mono/mono-$MONOVER.tar.bz2
+        tar xf mono-$MONOVER.tar.bz2;
+        cd mono-$MONOVER;
+        ./configure --prefix=$WORKSPACE/mono-$MONOVER;
+        $MAKE && make install;
+      );
+    fi
+
+    # Set up IronPython
+    # See: https://github.com/IronLanguages/main/wiki/Building#the-mono-runtime
+    if [ ! -x "$PYTHON" ]; then
+      (
+        mkdir -p .data; cd .data;
+        export PATH="$(dirname "$XBUILD"):$PATH"
+
+        wget -ct0 "$IRONPYTHON_URL_BASE/$FN";
+        unzip -q "$FN";
+        cd "main-$PREFIX";
+
+        $XBUILD /p:Configuration=Release Solutions/IronPython.sln || exit 1
+
+        mkdir -p "$(dirname "$PYTHON")";
+        echo 'mono "$PWD/bin/Release/ir.exe" "${@}"' > $PYTHON;
+        chmod +x $PYTHON;
+      ) || exit 1;
+    fi;
+
+fi;
+
+
+# Set up pip
+$PYTHON -m ensurepip --upgrade || exit 10;
+
+# Set up tox
+if [ ! -x "$TOX" ]; then
+   $PIP install tox || exit 11;
+fi;
+
+
+set
+
+"$PIP" install cython || exit 12
+
+if [ "$PYVER" == "2.7" ]; then
+    "$PIP" install numpy\<1.16.99 || exit 13;
+    "$PIP" install -rrequirements/test_requirements_py27.txt || exit 14;
+
+else
+    "$PIP" install numpy || exit 15;
+    "$PIP" install -rrequirements/test_requirements.txt || exit 16;
+
+fi
+
+[ -e .coverage ] && rm -v .coverage
+[ -e .coverage ] && rm -v coverage.xml
+
+export PYTHONHASHSEED=0
+
+# ignore return value -- result information is in the produced xml files
+"$PYTHON" setup.py test || true;
---
+++ packages/spyne/spyne-2.13.16.tar.gz/spyne-2.13.16/setup.py
@@ -0,0 +1,307 @@
+#!/usr/bin/env python
+#encoding: utf8
+
+from __future__ import print_function
+
+import io
+import os
+import re
+import sys
+import inspect
+
+from glob import glob
+from itertools import chain
+from os.path import join, dirname, abspath
+
+from setuptools import setup
+from setuptools import find_packages
+from setuptools.command.test import test as TestCommand
+
+try:
+    import colorama
+    colorama.init()
+    from colorama import Fore
+    RESET = Fore.RESET
+    GREEN = Fore.GREEN
+    RED = Fore.RED
+except ImportError:
+    RESET = ''
+    GREEN = ''
+    RED = ''
+
+IS_PYPY = '__pypy__' in sys.builtin_module_names
+OWN_PATH = abspath(inspect.getfile(inspect.currentframe()))
+EXAMPLES_DIR = join(dirname(OWN_PATH), 'examples')
+PYVER = ''.join([str(i) for i in sys.version_info[:2]])
+
+with io.open(os.path.join(os.path.dirname(__file__), 'spyne', '__init__.py'), 'r') as v:
+    VERSION = re.match(r".*__version__ = '(.*?)'", v.read(), re.S).group(1)
+
+SHORT_DESC="A transport and architecture agnostic rpc library that focuses on" \
+" exposing public services with a well-defined API."
+
+LONG_DESC = """Homepage: http://spyne.io
+
+Spyne aims to save the protocol implementers the hassle of
+implementing their own remote procedure call api and the application programmers
+the hassle of jumping through hoops just to expose their services using multiple
+protocols and transports.
+"""
+
+try:
+    os.stat('CHANGELOG.rst')
+    with io.open('CHANGELOG.rst', 'rb') as f:
+        LONG_DESC += u"\n\n" + f.read().decode('utf8')
+except OSError:
+    pass
+
+
+###############################
+# Testing stuff
+
+def call_test(f, a, tests, env={}):
+    import spyne.test
+    from multiprocessing import Process, Queue
+
+    tests_dir = os.path.dirname(spyne.test.__file__)
+    if len(tests) > 0:
+        a.extend(chain(*[glob(join(tests_dir, test)) for test in tests]))
+
+    queue = Queue()
+    p = Process(target=_wrapper(f), args=[a, queue, env])
+    p.start()
+    p.join()
+
+    ret = queue.get()
+    if ret == 0:
+        print(tests or a, "OK")
+    else:
+        print(tests or a, "FAIL")
+
+    print()
+
+    return ret
+
+
+def _wrapper(f):
+    import traceback
+    def _(args, queue, env):
+        print("env:", env)
+        for k, v in env.items():
+            os.environ[k] = v
+        try:
+            retval = f(args)
+        except SystemExit as e:
+            retval = e.code
+        except BaseException as e:
+            print(traceback.format_exc())
+            retval = 1
+
+        queue.put(retval)
+
+    return _
+
+
+def run_tests_and_create_report(report_name, *tests, **kwargs):
+    import spyne.test
+    import pytest
+
+    if os.path.isfile(report_name):
+        os.unlink(report_name)
+
+    tests_dir = os.path.dirname(spyne.test.__file__)
+
+    args = [
+        '--verbose',
+        '--cov-report=', '--cov', 'spyne',
+        '--cov-append',
+        '--tb=short',
+        '--junitxml=%s' % report_name,
+    ]
+    args.extend('--{0}={1}'.format(k, v) for k, v in kwargs.items())
+    args.extend(chain(*[glob("%s/%s" % (tests_dir, test)) for test in tests]))
+
+    return pytest.main(args)
+
+
+_ctr = 0
+
+
+def call_pytest(*tests, **kwargs):
+    global _ctr
+
+    _ctr += 1
+    file_name = 'test_result.%d.xml' % _ctr
+    os.environ['COVERAGE_FILE'] = '.coverage.%d' % _ctr
+
+    return run_tests_and_create_report(file_name, *tests, **kwargs)
+
+
+def call_pytest_subprocess(*tests, **kwargs):
+    global _ctr
+    import pytest
+
+    _ctr += 1
+    file_name = 'test_result.%d.xml' % _ctr
+    if os.path.isfile(file_name):
+        os.unlink(file_name)
+
+    # env = {'COVERAGE_FILE': '.coverage.%d' % _ctr}
+    env = {}
+
+    args = [
+        '--verbose',
+        '--cov-append',
+        '--cov-report=',
+        '--cov', 'spyne',
+        '--tb=line',
+        '--junitxml=%s' % file_name
+    ]
+    args.extend('--{0}={1}'.format(k, v) for k, v in kwargs.items())
+    return call_test(pytest.main, args, tests, env)
+
+
+def call_tox_subprocess(env):
+    import tox.session
+
+    args = ['-e', env]
+
+    return call_test(tox.session.main, args, [])
+
+def call_coverage():
+    import coverage.cmdline
+
+    # coverage.cmdline.main(['combine'])
+    # call_test(coverage.cmdline.main, ['combine'], [])
+    call_test(coverage.cmdline.main, ['xml', '-i'], [])
+
+    return 0
+
+
+class ExtendedTestCommand(TestCommand):
+    """TestCommand customized to project needs."""
+
+    user_options = TestCommand.user_options + [
+        ('capture=', 'k', "py.test output capture control (see py.test "
+                          "--capture)"),
+    ]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.capture = 'fd'
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+
+        self.test_args = []
+        self.test_suite = True
+
+
+class RunTests(ExtendedTestCommand):
+    def run_tests(self):
+        cfn = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])),
+                                                                      'tox.ini')
+        from collections import OrderedDict
+        djenvs = tuple(OrderedDict(((k, None) for k in
+                            re.findall('py%s-dj[0-9]+' % PYVER,
+                                open(cfn, 'rb').read().decode('utf8')))).keys())
+
+        ret = 0
+        tests = [
+            'interface', 'model', 'multipython', 'protocol', 'util',
+
+            'interop/test_pyramid.py',
+            'interop/test_soap_client_http_twisted.py',
+
+            'transport/test_msgpack.py'
+
+            'test_null_server.py',
+            'test_service.py',
+            'test_soft_validation.py',
+            'test_sqlalchemy.py',
+            'test_sqlalchemy_deprecated.py',
+        ]
+
+        print("Test stage 1: Unit tests")
+        ret = call_pytest_subprocess(*tests, capture=self.capture) or ret
+
+        print("\nTest stage 2: End-to-end tests")
+        ret = call_pytest_subprocess('interop/test_httprpc.py',
+                                                    capture=self.capture) or ret
+        ret = call_pytest_subprocess('interop/test_soap_client_http.py',
+                                                    capture=self.capture) or ret
+        ret = call_pytest_subprocess('interop/test_soap_client_zeromq.py',
+                                                    capture=self.capture) or ret
+
+        # excluding PyPy as it chokes here on LXML
+        if not IS_PYPY:
+            ret = call_pytest_subprocess('interop/test_suds.py',
+                                                    capture=self.capture) or ret
+            ret = call_pytest_subprocess('interop/test_zeep.py',
+                                                    capture=self.capture) or ret
+
+        print("\nTest stage 3: Tox-managed tests")
+        for djenv in djenvs:
+            ret = call_tox_subprocess(djenv) or ret
+
+        if ret == 0:
+            print(GREEN + "All that glisters is not gold." + RESET)
+        else:
+            print(RED + "Something is rotten in the state of Denmark." + RESET)
+
+        print ("Generating coverage.xml")
+        call_coverage()
+
+        raise SystemExit(ret)
+
+
+# Testing stuff ends here.
+###############################
+
+setup(
+    name='spyne',
+    packages=find_packages(),
+
+    version=VERSION,
+    description=SHORT_DESC,
+    long_description=LONG_DESC,
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        #'Programming Language :: Python :: Implementation :: Jython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    keywords='soap wsdl wsgi zeromq rest rpc json http msgpack xml'
+             ' django pyramid postgresql sqlalchemy twisted yaml'.split(),
+    author='Burak Arslan',
+    author_email='burak+package@spyne.io',
+    maintainer='Burak Arslan',
+    maintainer_email='burak+package@spyne.io',
+    url='http://spyne.io',
+    license='LGPL-2.1',
+    zip_safe=False,
+    install_requires=[
+      'pytz',
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'sort_wsdl=spyne.test.sort_wsdl:main',
+        ]
+    },
+
+    cmdclass={
+        'test': RunTests,
+    },
+)
