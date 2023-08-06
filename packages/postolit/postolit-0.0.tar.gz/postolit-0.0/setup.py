773e617f5c82b4dc403471d419591b83954bcbc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:27 +0000

    Add package postolit/0.0/postolit-0.0.tar.gz

---
+++ packages/postolit/postolit-0.0.tar.gz/postolit-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='postolit',
+      version='0.0',
+      description='Postoperative Library for Image Transformation',
+      packages=['postolit'],
+      author_email='anat_post@mail.ru',
+      zip_safe=False)
