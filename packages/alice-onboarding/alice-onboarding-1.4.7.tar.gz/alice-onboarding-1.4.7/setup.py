65ac4d0bfe6ee4a13428f823a275677d806dc784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:29 +0000

    Add package alice-onboarding/1.4.7/alice-onboarding-1.4.7.tar.gz

---
+++ packages/alice-onboarding/alice-onboarding-1.4.7.tar.gz/alice-onboarding-1.4.7/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+VERSION = open("alice/VERSION", "r").read()
+REQUIRES = [
+    "pyjwt>=1.7.1",
+    "requests>=2.18.0",
+    "dataclasses>=0.6",
+    "dataclasses-json>=0.2.14",
+    "meiga>=0.1.4",
+]
+
+setup(
+    name="alice-onboarding",
+    version=VERSION,
+    description="Alice Onboarding Python SDK",
+    url="https://alicebiometrics.com",
+    author="ALiCE Biometrics",
+    author_email="support@alicebiometrics.com",
+    license="ALiCE Copyright",
+    packages=[
+        "alice",
+        "alice/onboarding",
+        "alice/auth",
+        "alice/sandbox",
+        "alice/webhooks",
+    ],
+    zip_safe=False,
+    install_requires=REQUIRES,
+    include_package_data=True,
+)
