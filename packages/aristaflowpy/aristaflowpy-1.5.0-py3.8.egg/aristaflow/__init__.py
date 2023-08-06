3346dc6e81f65d068a159fb7be3dafccf09b0ff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:33 +0000

    Add package aristaflowpy/1.5.0/aristaflowpy-1.5.0-py3.8.egg

---
+++ packages/aristaflowpy/aristaflowpy-1.5.0-py3.8.egg/aristaflow/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from aristaflow.configuration import Configuration
+from aristaflow.client_service import AristaFlowClientService
+from aristaflow.client_platform import AristaFlowClientPlatform
+from aristaflow.worklist_service import WorklistService
