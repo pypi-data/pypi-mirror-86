893ce325ab16d19333e631a3690aecbf5212c03e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:44 +0000

    Add package makefile-creator/0.0.8b0/makefile_creator-0.0.8b0-py3-none-any.whl

---
+++ packages/makefile-creator/makefile_creator-0.0.8b0-py3-none-any.whl/makefile_creator/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+import makefile_creator.config as config
+import makefile_creator.makefiles as makefiles
+
+config.PACKAGE_NAME = 'makefile_creator'
+config.VERSION = 'v0.0.8-beta'
+
+makefiles.PROJECT_ROOT = config.DEFAULTS['PROJECT_ROOT']
+makefiles.BUILD_DIRECTORY = config.DEFAULTS['BUILD_DIRECTORY']
+makefiles.BIN_DIRECTORY = config.DEFAULTS['BIN_DIRECTORY']
+makefiles.CC = config.DEFAULTS['CC']
+makefiles.EXTENSIONS.extend(config.DEFAULTS['EXTENSIONS'])
+makefiles.C_FLAGS.extend(config.DEFAULTS['C_FLAGS'])
+makefiles.RM = config.DEFAULTS['RM']
+makefiles.TARGET = config.DEFAULTS['TARGET']
+# makefiles.CLEAN = config.DEFAULTS['CLEAN']
+makefiles.VERBOSE = config.DEFAULTS['VERBOSE']
