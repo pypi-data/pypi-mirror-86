ebd626a8ce0139008106a7ffe3550f615669ba65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:43 +0000

    Add package traceroute-win/1.5.0/traceroute-win-1.5.0.tar.gz

---
+++ packages/traceroute-win/traceroute-win-1.5.0.tar.gz/traceroute-win-1.5.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages 
+  
+with open('requirements.txt') as f: 
+    requirements = f.readlines() 
+  
+long_description = 'Traceroute with Python for Windows'
+  
+setup( 
+        name ='traceroute-win', 
+        version ='1.5.0', 
+        author ='Javed & Emeric', 
+        author_email ='jsyedk@gmail.com', 
+        description ='Traceroute with Python for Windows', 
+        long_description = long_description, 
+        long_description_content_type ="text/markdown", 
+        license ='MIT', 
+        packages = find_packages(), 
+        entry_points ={ 
+            'console_scripts': [ 
+                'traceroute_win = traceroute_win.traceroute_win:main'
+            ] 
+        }, 
+        classifiers =( 
+            "Programming Language :: Python :: 3", 
+            "License :: OSI Approved :: MIT License", 
+            "Operating System :: OS Independent", 
+        ), 
+        keywords ='traceroute python package windows fastjaved', 
+        install_requires = requirements, 
+        dependency_links=['https://github.com/ozgur/python-firebase@0d79d7609844569ea1cec4ac71cb9038e834c355'],
+        zip_safe = False
+) 
