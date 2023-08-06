e42e260eca262a358af66eca4a5acc1fbde0a224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:58 +0000

    Add package archivy/0.9.2/archivy-0.9.2.tar.gz

---
+++ packages/archivy/archivy-0.9.2.tar.gz/archivy-0.9.2/archivy/config.py
@@ -0,0 +1,49 @@
+import os
+import appdirs
+
+
+class Config(object):
+    """Configuration object for the application"""
+    ELASTICSEARCH_ENABLED = int(os.environ.get("ELASTICSEARCH_ENABLED") or 0)
+    ELASTICSEARCH_URL = os.environ.get(
+        "ELASTICSEARCH_URL") or "http://localhost:9200"
+    SECRET_KEY = os.urandom(32)
+    INDEX_NAME = "dataobj"
+    APP_PATH = \
+        os.getenv('ARCHIVY_DATA_DIR') or appdirs.user_data_dir('archivy')
+    os.makedirs(APP_PATH, exist_ok=True)
+
+    PANDOC_HIGHLIGHT_THEME = os.environ.get("PANDOC_THEME") or "pygments"
+    ELASTIC_CONF = {
+        "settings": {
+            "highlight": {
+                "max_analyzed_offset": 100000000
+            },
+            "analysis": {
+                "analyzer": {
+                    "rebuilt_standard": {
+                        "stopwords": "_english_",
+                        "tokenizer": "standard",
+                        "filter": [
+                            "lowercase",
+                            "kstem",
+                            "trim",
+                            "unique"
+                        ]
+                    }
+                }
+            }
+        },
+        "mappings": {
+            "properties": {
+                "title": {
+                    "type": "text",
+                    "analyzer": "rebuilt_standard",
+                    "term_vector": "with_positions_offsets"
+                },
+                "tags": {"type": "text", "analyzer": "rebuilt_standard"},
+                "body": {"type": "text", "analyzer": "rebuilt_standard"},
+                "desc": {"type": "text", "analyzer": "rebuilt_standard"}
+            }
+        }
+    }
