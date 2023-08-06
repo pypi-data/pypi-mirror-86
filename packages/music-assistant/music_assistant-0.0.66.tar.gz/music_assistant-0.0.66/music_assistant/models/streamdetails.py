7fe7e71b879b9652ca1b20a02edc932895f8ef94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:33 +0000

    Add package music-assistant/0.0.66/music_assistant-0.0.66.tar.gz

---
+++ packages/music-assistant/music_assistant-0.0.66.tar.gz/music_assistant-0.0.66/music_assistant/models/streamdetails.py
@@ -0,0 +1,44 @@
+"""Models and helpers for the streamdetails of a MediaItem."""
+
+from dataclasses import dataclass
+from enum import Enum
+from typing import Any
+
+from mashumaro import DataClassDictMixin
+
+
+class StreamType(Enum):
+    """Enum with stream types."""
+
+    EXECUTABLE = "executable"
+    URL = "url"
+    FILE = "file"
+    CACHE = "cache"
+
+
+class ContentType(Enum):
+    """Enum with stream content types."""
+
+    OGG = "ogg"
+    FLAC = "flac"
+    MP3 = "mp3"
+    RAW = "raw"
+    AAC = "aac"
+    MPEG = "mpeg"
+
+
+@dataclass
+class StreamDetails(DataClassDictMixin):
+    """Model for streamdetails."""
+
+    type: StreamType
+    provider: str
+    item_id: str
+    path: str
+    content_type: ContentType
+    sample_rate: int
+    bit_depth: int
+    player_id: str = ""
+    details: Any = None
+    seconds_played: int = 0
+    sox_options: str = None
