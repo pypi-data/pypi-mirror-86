1534e55557518a17d8ee542e35953bcda0a9307f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:55 +0000

    Add package jwst-proposal-helper/1.2.1/jwst_proposal_helper-1.2.1.tar.gz

---
+++ packages/jwst-proposal-helper/jwst_proposal_helper-1.2.1.tar.gz/jwst_proposal_helper-1.2.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="jwst_proposal_helper",
+    version="1.2.1",
+    author="Kornpob Bhirombhakdi",
+    author_email="kbhirombhakdi@stsci.edu",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/bkornpob/jwst_proposal_helper.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+        ,"License :: OSI Approved :: MIT License"
+        ,"Operating System :: OS Independent"
+    ],
+    python_requires='>=3.'
+)
