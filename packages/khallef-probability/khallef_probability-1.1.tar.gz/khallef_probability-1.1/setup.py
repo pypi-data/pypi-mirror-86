8d9a7165884c43f4519ab41dc2d90e48e9a47d8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:53 +0000

    Add package khallef-probability/1.1/khallef_probability-1.1.tar.gz

---
+++ packages/khallef-probability/khallef_probability-1.1.tar.gz/khallef_probability-1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='khallef_probability',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['khallef_probability'],
+      zip_safe=False)
