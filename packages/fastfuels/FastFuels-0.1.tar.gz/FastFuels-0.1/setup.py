804b2ba516cd877e3eab04e81770ade04bf7cdad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:50 +0000

    Add package fastfuels/0.1/FastFuels-0.1.tar.gz

---
+++ packages/fastfuels/FastFuels-0.1.tar.gz/FastFuels-0.1/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+setup(
+  name = 'FastFuels',
+  packages = ['fastfuels'],
+  version = '0.1',
+  license='GNU GPLv3',
+  description = '3D fuelscapes for the contiguous US',
+  author = 'Lucas Wells',
+  author_email = 'lucas@holtzforestry.com',
+  url = 'https://github.com/holtzforestry/FastFuels',
+  download_url = 'https://github.com/holtzforestry/FastFuels/archive/v0.1-alpha.tar.gz',
+  keywords = ['fire model', 'fuelscape', 'wildfire'],
+  install_requires=[
+          'colorcet',
+          'gcsfs',
+          'numpy',
+          'pyvista',
+          'scipy',
+          'zarr',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
