d84946d33cb4cf37e00b9ab2211c8f2feb8fcf6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:56 +0000

    Add package agilentaspparser/0.0.4/agilentaspparser-0.0.4.tar.gz

---
+++ packages/agilentaspparser/agilentaspparser-0.0.4.tar.gz/agilentaspparser-0.0.4/README.md
@@ -0,0 +1,5 @@
+# Agilent IR spectrum file parsers
+
+
+
+In our lab we have an Agilent Cary 660 ATR-IR spectrometer. The ASCII files produced by this device are a nuissance to work with beacause they don't feature a wavenumber column. This 'library' intends to make work with said files a bit easier by parsing spectra and adding missing information.
---
+++ packages/agilentaspparser/agilentaspparser-0.0.4.tar.gz/agilentaspparser-0.0.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="agilentaspparser", # Replace with your own username
+    version="0.0.4",
+    author="Eduardo Gonik",
+    author_email="gonik@quimica.unlp.edu.ar",
+    description="Parser for Agilent IR .asp files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/egonik-unlp/agilent_ir",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
