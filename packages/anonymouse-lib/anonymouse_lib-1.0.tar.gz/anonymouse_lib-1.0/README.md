23227bcdb16814fc2d69cd4230199e7879a653b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:48 +0000

    Add package anonymouse-lib/1.0/anonymouse_lib-1.0.tar.gz

---
+++ packages/anonymouse-lib/anonymouse_lib-1.0.tar.gz/anonymouse_lib-1.0/README.md
@@ -0,0 +1,16 @@
+# anonymouse_lib
+# need 
+python 3.7+
+
+# use
+```python 
+
+async main():
+  anonymouse = AnonyMouse()
+  anonymouse.send_email_message("mail@mail.org","new message","hello!")
+  anonymouse.close()
+ ```
+ # install 
+ ```bash
+ pip install anonymouse_lib
+ ```
---
+++ packages/anonymouse-lib/anonymouse_lib-1.0.tar.gz/anonymouse_lib-1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+requirements = [
+    "aiohttp"
+]
+
+setup(
+    name='anonymouse_lib',
+    version='1.0',
+    description="",
+    author="dark0ghost",
+    url='https://github.com/dark0ghost/anonymouse_lib',
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT License",
+    zip_safe=False,
+    classifiers=[
+        'Natural Language :: English',
+    ],
+)
