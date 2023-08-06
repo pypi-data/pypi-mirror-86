75ab47b8e6da10a09e479a47c0ce49586908e676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:30 +0000

    Add package igit/2020.11.28/igit-2020.11.28.tar.gz

---
+++ packages/igit/igit-2020.11.28.tar.gz/igit-2020.11.28/igit/core/commands.py
@@ -0,0 +1,253 @@
+import os
+from pathlib import Path
+
+import requests
+from git import InvalidGitRepositoryError
+from git import GitCommandError
+
+from igit.core.fs_ops import get_files
+from igit.interactive.display import Display
+from igit.interactive.interact import Interact
+from igit.core.shell_ops import Shell
+from igit.core.config import *
+
+from igit.core.git_ops import in_gitignore, add_gitignore, GitOps
+
+
+class Igit:
+
+    def __init__(self, path=os.getcwd()):
+        try:
+            self.gitops = GitOps(path)
+            self.display = Display()
+            self.interact = Interact()
+        except InvalidGitRepositoryError:
+            raise
+
+    def add(self, _files, _all):
+        try:
+            unstaged = self.gitops.get_changed_files('unstaged') + \
+                       self.gitops.get_untracked_files()
+            if unstaged:
+                if _all:
+                    selected = unstaged
+                elif _files:
+                    selected = self._intersection(_files, unstaged)
+                else:
+                    selected = self.interact.choose('choose files to add', unstaged)
+                if selected:
+                    self.gitops.repo.git.add(selected)
+                    self.display.list('added', selected, 'green', 'thumbsup')
+                else:
+                    self.display.message('command had no effect', 'yellow', 'speak_no_evil')
+            else:
+                self.display.message('nothing to add', 'yellow', 'speak_no_evil')
+        except GitCommandError as e:
+            self.display.message(f'unable to add\n{e}', 'red', 'x')
+
+    def commit(self, _message, _add):
+        if not _message:
+            prompt = self.interact.text(f'commit message [{DEFAULT_COMMIT}]')
+            _message = prompt if prompt else DEFAULT_COMMIT
+        if _add:
+            self.add(_files=None, _all=True)
+        try:
+            if len(self.gitops.repo.index.diff("HEAD")) > 0:
+                commit_resp = self.gitops.repo.git.commit('-m', _message)
+                self.display.message(commit_resp, 'white', 'cat')
+                self.display.message('committed', 'yellow', 'lock')
+            else:
+                self.display.message('nothing to commit', 'yellow', 'lock')
+        except GitCommandError as e:
+            self.display.message(f'unable to commit\n{e}', 'red', 'x')
+
+    def push(self, _add, _commit):
+        if _add:
+            self.add(_files=None, _all=False)
+            self.commit(_message=None, _add=False)
+        elif _commit:
+            self.commit(_message=None, _add=True)
+        try:
+            self.gitops.repo.git.push('origin', self.gitops.branch)
+            self.display.message('pushed', 'cyan', 'rocket')
+        except GitCommandError as e:
+            self.display.message(f'unable to push\n{e}', 'red', 'x')
+
+    def save(self, message):
+        self.add(_files=None, _all=True)
+        message = message if message else DEFAULT_COMMIT
+        self.commit(_message=message, _add=False)
+
+    def up(self, message, commit, add):                
+        self.add(_files=None, _all=False if add else True)
+        self.commit(_message=None if commit else message if message else DEFAULT_COMMIT, _add=False)
+        self.push(_add=False, _commit=False)
+
+    def branch(self, target_branch, hopping_mode, create_new):
+        """
+        Swith to target_branch if specified, else prompt branch menu.
+        Implements auto-stashing to allow flex branch hopping.
+        :param target_branch:
+        :return:
+        """
+        if create_new:
+            branch_name = Interact().text('Give it a name')
+            self.gitops.create_branch(branch_name)
+            return
+
+        if not target_branch:
+            branches = [branch.name for branch in list(self.gitops.repo.branches)]
+            if len(branches) == 1:
+                create_new = Interact().confirm('No local branches detected. create new?')
+                if create_new:
+                    branch_name = Interact().text('Give it a name')
+                    self.gitops.create_branch(branch_name)
+                    return
+                return
+            else:
+                target_branch = self.interact.select('choose terget branch', branches)
+                if not target_branch:
+                    return
+
+        if hopping_mode:
+            # handle branch hopping in case of untracked files
+            untracked = self.gitops.get_untracked_files()
+            if untracked:
+                self.display.list('Untracked files found', untracked, 'white', 'speak_no_evil')
+                if not self.interact.confirm(f'Stage them to continue?'):
+                    self.display.message('Stage files to branch with igit', 'yellow', 'sweat_smile')
+                    return
+                else:
+                    self.display.message('Staging untracked files', 'yellow', 'floppy_disk')
+                    self.gitops.repo.git.add(untracked)
+
+            change_list = self.gitops.get_all_changes()
+
+            if len(change_list) > 0:
+                self.gitops.repo.git.stash(f'save', f'{GLOBAL_STASH_PREFIX}_{self.gitops.branch}')
+                self.display.message(f'Saving diff (stash): {GLOBAL_STASH_PREFIX}_{self.gitops.branch}', 'green', 'thumbsup')
+
+        self.gitops.switch_branch(target_branch)
+
+        if hopping_mode:
+            stash_list = self.gitops.repo.git.stash('list')
+            stash_stub = f'{GLOBAL_STASH_PREFIX}_{self.gitops.branch}'
+            if stash_list:
+                stash_list = stash_list.split("\n")
+
+            for stash in stash_list:
+                stash_name = stash.split(' ')[-1]
+                if stash_stub == stash_name:
+                    stash_index = stash.split(' ')[0].replace(':', '')
+                    self.gitops.repo.git.stash(f'pop', stash_index)
+                    self.display.message(f'Loading diff (stash): {stash_name}', 'green', 'thumbsup')
+                    break
+
+    def diff(self):
+        change_list = self.gitops.get_changed_files('unstaged')
+        if not change_list:
+            self.display.message('No diff found', 'yellow', 'speak_no_evil')
+
+        file = self.interact.select('select file to view diff', change_list)
+        if not file:
+            return
+        diffs = self.gitops.repo.git.diff(file).split('\n')
+        for diff in diffs:
+            self.display.diff(diff)
+
+    def rename(self, name):
+        try:
+            # TODO - interactivize
+            self.gitops.repo.active_branch.rename(name)
+        except Exception as e:
+            print(e)
+
+    def undo(self, _files, _all):
+        try:
+            unstaged = self.gitops.get_changed_files('unstaged')
+            if unstaged:
+                if _all:
+                    selected = unstaged
+                elif _files:
+                    selected = self._intersection(_files, unstaged)
+                else:
+                    selected = self.interact.choose('choose files to undo changes', unstaged)
+                if selected:
+                    self.gitops.repo.git.checkout('--', selected)
+                    self.display.list('changes undone', selected, 'green', 'thumbsup')
+                else:
+                    self.display.message('command had no effect', 'yellow', 'speak_no_evil')
+            else:
+                self.display.message('nothing to undo', 'yellow', 'speak_no_evil')
+        except GitCommandError as e:
+            self.display.message(f'unable to undo\n{e}', 'red', 'x')
+
+    def unstage(self, _files, _all):
+        try:
+            unstaged = self.gitops.get_changed_files('staged')
+            if unstaged:
+                if _all:
+                    selected = unstaged
+                elif _files:
+                    selected = self._intersection(_files, unstaged)
+                else:
+                    selected = self.interact.choose('choose files to unstage', unstaged)
+                if selected:
+                    self.gitops.repo.git.reset('--', selected)
+                    self.display.list('files unstaged', selected, 'green', 'thumbsup')
+                else:
+                    self.display.message('command had no effect', 'yellow', 'speak_no_evil')
+            else:
+                self.display.message('nothing to unstage', 'yellow', 'speak_no_evil')
+        except GitCommandError as e:
+            self.display.message(f'unable to unstage\n{e}', 'red', 'x')
+
+    def revert(self, commit):
+        if commit:
+            self.gitops.repo.head.reset(commit, index=True)
+        else:
+            self.gitops.repo.head.reset('HEAD~1', index=True)
+            # FOR HARD RESET:
+            # self.gitops.repo.head.reset('HEAD~1', index=True, working_tree=True)
+
+    def ignore(self, reset, create):
+        # check for .gitignore file, prompt to create if none detected
+        gitignore_path = os.path.join(self.gitops.repo_path, '.gitignore')
+
+        if create:
+            self.gitops.create_gitignore(gitignore_path)
+            return
+
+        if not os.path.exists(gitignore_path):
+            if self.interact.confirm('no gitignore file detected, create new?'):
+                self.gitops.create_gitignore(gitignore_path)
+
+        if reset:
+            shell = Shell()
+            shell >> 'git rm -r --cached .'
+            self.commit('.gitignore fixed by igit', True)
+
+        else:
+            ex_dirs = [Path(ex_dir) for ex_dir in ['.git']]
+            opt_files = get_files(Path(self.gitops.repo_path), ex_dirs, [], False)
+            opt_files = [str(file) for file in opt_files]
+
+            # subtract files that are already added
+            if not opt_files:
+                return 'No files found to add.'
+            else:
+                files = self.interact.choose('select files to gitignore', opt_files)
+                if files:
+                    try:
+                        for file in files:
+                            if not in_gitignore(gitignore_path, file):
+                                add_gitignore(gitignore_path, file)
+                    except Exception as e:
+                        print(f'No gitignore file found: {e}')
+                else:
+                    self.display.message('gitignore unchanged (use space key to select!)', 'yellow', 'speak_no_evil')
+
+
+    @staticmethod
+    def _intersection(iterable1, iterable2):
+        return list(set(iterable1).intersection(iterable2))
---
+++ packages/igit/igit-2020.11.28.tar.gz/igit-2020.11.28/igit/core/git_ops.py
@@ -0,0 +1,106 @@
+import os
+
+import requests
+from git import Repo
+from git import InvalidGitRepositoryError
+from igit.interactive.display import Display
+from igit.interactive.interact import Interact
+from igit.core.ignore_files import gitignore_files
+
+
+class GitOps:
+
+    change_types = {
+        'unstaged': None,
+        'staged': 'HEAD'
+    }
+
+    def __init__(self, path=os.getcwd()) -> None:
+        try:
+            self.repo = Repo(path, search_parent_directories=True)
+            self.repo_path = os.path.dirname(self.repo.git_dir)
+            self.branch = self.repo.active_branch
+            self.display = Display()
+            self.interact = Interact()
+
+        except InvalidGitRepositoryError:
+            raise InvalidGitRepositoryError('Not a git repo, I have no power here...')
+
+    def get_changed_files(self, change):
+        return [item.a_path for item in self.repo.index.diff(self.change_types[change])]
+
+    def get_untracked_files(self):
+        return self.repo.untracked_files
+
+    def get_all_changes(self):
+        return self.get_changed_files('staged') + \
+               self.get_changed_files('unstaged') + \
+               self.get_untracked_files()
+
+    def calculate_change_list(self, diff_types=None):
+        change_list = []
+        for diff_type in diff_types:
+            change_list += [item.a_path for item
+                            in self.repo.index.diff(self.change_types[diff_type])]
+        change_list += self.repo.untracked_files
+        return change_list
+
+    def switch_branch(self, branch_name):
+        self.repo.git.checkout(branch_name)
+        self.branch = self.repo.active_branch.name
+        self.display.message(f'Switched to branch: {self.branch}', 'yellow', 'checkered_flag')
+
+    def create_branch(self, branch_name):
+        self.repo.create_head(branch_name)
+        self.switch_branch(branch_name)
+
+    def create_gitignore(self, gitignore_path):
+        if os.path.exists(gitignore_path):
+            self.display.message('Attention - gitignore file detected,'
+                                 'creating a new one will overwrite existing',
+                                 color='red',
+                                 icon='exclamation')
+        rv = self.interact.confirm('Create from template [y] or blank [N]')
+        if rv is None:
+            return
+        if rv:
+            rv = self.interact.select('What kind of .gitignore do you need', gitignore_files)
+            url = f'https://raw.githubusercontent.com/github/gitignore/master/{rv}.gitignore'
+            r = requests.get(url)
+            if r.status_code != 200:
+                raise Exception(f'unable to fetch gitignore file for {rv}')
+            with open(gitignore_path, 'wb') as f:
+                f.write(r.content)
+        else:
+            open(gitignore_path, 'a').close()
+        self.display.message('Created .gitignore', icon='tada')
+        return
+
+
+def in_gitignore(gitignore_path, item):
+    gitignore = []
+    with open(gitignore_path, 'r') as ignore_file:
+        for line in ignore_file.readlines():
+            gitignore.append(line.strip())
+    return item in gitignore
+
+
+def add_gitignore(gitignore_path, item):
+    with open(gitignore_path, 'a') as ignore_file:
+        ignore_file.write('\n')
+        ignore_file.write(item)
+
+
+if __name__ == '__main__':
+    dir_path = '/Users/kobarhan/workspace/igit_test'
+    gc = GitOps(dir_path)
+
+    staged = gc.get_changed_files('staged')
+    unstaged = gc.get_changed_files('unstaged')
+    untracked = gc.get_untracked_files()
+
+    print(f'staged changes: {staged}')
+    print(f'unstaged changes: {unstaged}')
+    print(f'untracked changes: {untracked}')
+
+    print(f'all changes: {gc.get_all_changes()}')
