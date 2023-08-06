559f79c5583debeef948e66cc223ee5ae5363c92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:13 +0000

    Add package gitty/1.1.4/gitty-1.1.4.tar.gz

---
+++ packages/gitty/gitty-1.1.4.tar.gz/gitty-1.1.4/gitty/gitty_project_type_maven.py
@@ -0,0 +1,143 @@
+from os import path
+from xml.etree import ElementTree
+
+from gitty import GittyProjectType
+
+
+# this is to make the xml in the pom retain comments...
+from gitty.gitty_xml_tree_builder import CommentedTreeBuilder
+
+
+class GittyMaven(GittyProjectType):
+
+    pom_ns = 'http://maven.apache.org/POM/4.0.0'
+    ns = {'pom': pom_ns}
+
+    def get_name(self):
+        return 'maven'
+
+    def is_in_use(self, context):
+        is_maven = path.exists('pom.xml')
+        if is_maven:
+            context['project_type_name'] = self.get_name()
+            context['project_file'] = 'pom.xml'
+        return is_maven
+
+    def get_version_info(self, context):
+
+        pom, pom_doc = self.read_pom()
+
+        current_version_tag = pom.find('pom:version', self.ns)
+        current_version = current_version_tag.text
+        context['current_version'] = current_version
+
+        context['hotfix'] = current_version in context['tags_on_commit']
+
+        if context['current_version'].endswith('-SNAPSHOT'):
+
+            # we're not on a release branch
+
+            context['release_version'] = context['current_version'][: -9]
+
+            # build the next version
+            release_version_split = context['release_version'].split(".")
+            if context['a_task']:
+                # we're on a task, so no release or stabilization is needed
+                context['new_stabilization_branch'] = None
+                context['new_release_branch'] = None
+                context['new_stabilization_version'] = None
+            else:
+                if context['is_stable']:
+                    # we're already on a stabilization branch, so this includes the full version
+                    context['new_stabilization_branch'] = '.'.join(release_version_split) + '/master'
+                    context['new_release_branch'] = '.'.join(release_version_split) + '/releases'
+                    context['new_stabilization_version'] = '.'.join(release_version_split) + '.0-SNAPSHOT'
+                else:
+                    # we're not in a stabilization ecosystem - this will make one...
+                    context['new_stabilization_branch'] = '.'.join(release_version_split[:-1]) + '/master'
+                    context['new_release_branch'] = '.'.join(release_version_split[:-1]) + '/releases'
+                    context['new_stabilization_version'] = '.'.join(release_version_split) + '-SNAPSHOT'
+
+            if context['is_stable']:
+                context['current_release_branch'] = '/'.join([
+                    context['branch_parts'][0],
+                    'releases'
+                ])
+            else:
+                # we're on THE master branch - so there is not a current release branch
+                context['current_release_branch'] = None
+
+            self.build_next_version_numbers(context, release_version_split)
+
+        else:
+            # this can happen when making a hotfix!
+
+            # we want to make a stabilization ecosystem from this commit...
+            context['release_version'] = context['current_version']
+
+            # build the next version
+            release_version_split = context['release_version'].split(".")
+
+            # we're on an actual release branch, so this includes the full version
+            context['new_release_branch'] = '.'.join(release_version_split) + '/releases'
+            context['new_stabilization_branch'] = '.'.join(release_version_split) + '/master'
+            context['new_stabilization_version'] = '.'.join(release_version_split) + '.0-SNAPSHOT'
+
+            context['current_release_branch'] = '/'.join([
+                context['branch_parts'][0],
+                'releases'
+            ])
+
+            self.build_next_version_numbers(context, release_version_split)
+
+    def bump_version_to(self, context, new_version):
+        print('# setting pom version to "{}"'.format(new_version))
+
+        if not context['dry_run']:
+
+            pom, pom_doc = self.read_pom()
+
+            version_tag = pom.find('pom:version', self.ns)
+            version_tag.text = new_version
+            pom_doc.write("pom.xml")
+
+            # read the pom back in...
+            f = open("pom.xml", "r")
+            contents = f.readlines()
+            f.close()
+
+            # insert xml declaration
+            contents.insert(0, '<?xml version="1.0" encoding="UTF-8"?>\n')
+            # append a blank line
+            contents.append('\n')
+
+            f = open("pom.xml", "w")
+            contents = "".join(contents)
+            f.write(contents)
+            f.close()
+
+    def read_pom(self):
+        # a pom has a namespace
+        ElementTree.register_namespace('', self.pom_ns)
+        # we use this so we preserve comments in the pom
+        parser = ElementTree.XMLParser(target=CommentedTreeBuilder())
+        pom_doc = ElementTree.parse('pom.xml', parser)
+        pom = pom_doc.getroot()
+        return pom, pom_doc
+
+    @staticmethod
+    def build_next_version_numbers(context, release_version_split):
+        next_sub = str(int(release_version_split[2]) + 1)
+        next_min = str(int(release_version_split[1]) + 1)
+        patch_version = release_version_split.copy()
+        patch_version[2] = next_sub
+        context['next_master_version'] = '.'.join([
+            release_version_split[0],
+            next_min,
+            '0'
+        ]) + '-SNAPSHOT'
+        # we want to increment the last number, regardless of how many there are...
+        next_stable_version = release_version_split.copy()
+        next_stable_version[-1] = str(int(next_stable_version[-1]) + 1)
+        context['next_stable_version'] = '.'.join(next_stable_version) + '-SNAPSHOT'
+
---
+++ packages/gitty/gitty-1.1.4.tar.gz/gitty-1.1.4/tests/test_gitty_project_type_maven.py
@@ -0,0 +1,279 @@
+import os
+import tempfile
+from shutil import copyfile
+from gitty.gitty_project_type_maven import GittyMaven
+from tests.test_gitty_project_type_base_test import ProjectTypeTestCase
+
+
+class TestGittyMaven(ProjectTypeTestCase):
+
+    def __init__(self, test_method_name):
+        super().__init__(test_method_name)
+        self.sample_dir = 'sample_files/maven_snapshot'
+
+    def test_is_not_in_use(self):
+        cwd = self.go_to_temp_dir()
+        context = {}
+        project = GittyMaven()
+        self.assertFalse(project.is_in_use(context))
+        self.assertEqual('lol, nope', context.get('project_type_name', 'lol, nope'))
+        os.chdir(cwd)
+
+    def test_is_in_use_and_get_name(self):
+        cwd = self.go_to_sample_dir('sample_files/maven_snapshot')
+
+        project = GittyMaven()
+        context = {}
+        self.assertTrue(project.is_in_use(context))
+        self.assertEqual(project.get_name(), context['project_type_name'])
+
+        # go back where we started
+        os.chdir(cwd)
+
+    def test_get_version_info_from_the_master(self):
+        # save our location and go to the sample dir we need
+        cwd = self.go_to_sample_dir('sample_files/maven_snapshot')
+
+        expected = {
+            'project_type_name': 'maven',
+            'project_file': 'pom.xml',
+
+            'current_version': '1.0.0-SNAPSHOT',
+            'release_version': '1.0.0',
+            'next_stable_version': '1.0.1-SNAPSHOT',
+            'next_master_version': '1.1.0-SNAPSHOT',
+
+            'branch_parts': ['master'],
+            'current_branch': 'master',
+            'current_release_branch': None,
+            'task_prefix': 'tasks/',
+
+            'new_release_branch': '1.0/releases',
+            'new_stabilization_branch': '1.0/master',
+            'new_stabilization_version': '1.0.0-SNAPSHOT',
+
+            'the_master': True,
+            'a_master': True,
+            'a_task': False,
+            'a_release': False,
+            'is_stable': False,
+            'hotfix': False,
+            'tags_on_commit': [],
+
+            'git_ref': 'HEAD',
+            'git_hash': 'git_hash_here',
+
+        }
+
+        # create the project and verify it is setting the context up as expected
+        self.check_project_type_version_info(expected, GittyMaven(), 'master')
+
+        # go back where we started
+        os.chdir(cwd)
+
+    def test_get_version_info_from_stable_master(self):
+        # save our location and go to the sample dir we need
+        cwd = self.go_to_sample_dir('sample_files/maven_snapshot')
+
+        expected = {
+            'current_branch': '1.0/master',
+            'is_stable': True,
+            'tags_on_commit': [],
+            'branch_parts': ['1.0', 'master'],
+            'project_file': 'pom.xml',
+            'project_type_name': 'maven',
+            'current_version': '1.0.0-SNAPSHOT',
+            'hotfix': False,
+            'release_version': '1.0.0',
+            'new_stabilization_branch': '1.0.0/master',
+            'new_release_branch': '1.0.0/releases',
+            'new_stabilization_version': '1.0.0.0-SNAPSHOT',
+            'next_stable_version': '1.0.1-SNAPSHOT',
+            'next_master_version': '1.1.0-SNAPSHOT',
+            'the_master': False,
+            'a_master': True,
+            'a_task': False,
+            'a_release': False,
+            'task_prefix': '1.0/tasks/',
+            'current_release_branch': '1.0/releases',
+            'git_ref': 'HEAD',
+            'git_hash': 'git_hash_here'
+        }
+
+        # create the project and verify it is setting the context up as expected
+        self.check_project_type_version_info(expected, GittyMaven(), '1.0/master')
+
+        # go back where we started
+        os.chdir(cwd)
+
+    def test_get_version_info_from_master_task(self):
+        # save our location and go to the sample dir we need
+        cwd = self.go_to_sample_dir('sample_files/maven_snapshot')
+
+        expected = {
+            'project_type_name': 'maven',
+            'current_branch': 'tasks/123_snapped_the_frame',
+            'tags_on_commit': [],
+            'is_stable': False,
+            'branch_parts': ['tasks', '123_snapped_the_frame'],
+            'project_file': 'pom.xml',
+            'current_version': '1.0.0-SNAPSHOT',
+            'hotfix': False,
+            'release_version': '1.0.0',
+            'new_stabilization_branch': None,
+            'new_release_branch': None,
+            'new_stabilization_version': None,
+            'next_stable_version': '1.0.1-SNAPSHOT',
+            'next_master_version': '1.1.0-SNAPSHOT',
+            'the_master': False,
+            'a_master': False,
+            'a_task': True,
+            'a_release': False,
+            'task_prefix': None,
+            'current_release_branch': None,
+            'git_ref': 'HEAD',
+            'git_hash': 'git_hash_here'
+        }
+
+        self.check_project_type_version_info(expected, GittyMaven(), 'tasks/123_snapped_the_frame')
+
+        # go back where we started
+        os.chdir(cwd)
+
+    def test_get_version_info_from_stable_task(self):
+        # save our location and go to the sample dir we need
+        cwd = self.go_to_sample_dir('sample_files/maven_snapshot')
+
+        expected = {
+            'current_branch': '1.0/tasks/123_snapped_the_frame',
+            'is_stable': True,
+            'tags_on_commit': [],
+            'branch_parts': ['1.0', 'tasks', '123_snapped_the_frame'],
+            'project_type_name': 'maven',
+            'project_file': 'pom.xml',
+            'current_version': '1.0.0-SNAPSHOT',
+            'hotfix': False,
+            'release_version': '1.0.0',
+            'new_stabilization_branch': None,
+            'new_release_branch': None,
+            'new_stabilization_version': None,
+            'next_stable_version': '1.0.1-SNAPSHOT',
+            'next_master_version': '1.1.0-SNAPSHOT',
+            'the_master': False,
+            'a_master': False,
+            'a_task': True,
+            'a_release': False,
+            'task_prefix': None,
+            'current_release_branch': '1.0/releases',
+            'git_ref': 'HEAD',
+            'git_hash': 'git_hash_here'
+        }
+
+        self.check_project_type_version_info(expected, GittyMaven(), '1.0/tasks/123_snapped_the_frame')
+
+        # go back where we started
+        os.chdir(cwd)
+
+    def test_get_version_info_from_release_for_hotfix(self):
+        # save our location and go to the sample dir we need
+        cwd = self.go_to_sample_dir('sample_files/maven_release')
+
+        tags = ['1.2.3']
+        expected = {
+            'current_branch': '1.2/releases',
+            'is_stable': True,
+            'tags_on_commit': tags,
+            'branch_parts': ['1.2', 'releases'],
+            'project_type_name': 'maven',
+            'project_file': 'pom.xml',
+            'current_version': '1.2.3',
+            'hotfix': True,
+            'release_version': '1.2.3',
+            'new_stabilization_branch': '1.2.3/master',
+            'new_release_branch': '1.2.3/releases',
+            'new_stabilization_version': '1.2.3.0-SNAPSHOT',
+            'next_stable_version': '1.2.4-SNAPSHOT',
+            'next_master_version': '1.3.0-SNAPSHOT',
+            'the_master': False,
+            'a_master': False,
+            'a_task': False,
+            'a_release': True,
+            'task_prefix': None,
+            'current_release_branch': '1.2/releases',
+            'git_ref': 'HEAD',
+            'git_hash': 'git_hash_here'
+        }
+
+        self.check_project_type_version_info(expected, GittyMaven(), '1.2/releases', tags)
+
+        # go back where we started
+        os.chdir(cwd)
+
+    def test_bump_version(self):
+        # go to the sample directory
+        cwd = self.go_to_sample_dir()
+
+        # copy the pom to a temp location (and go there)
+        destination = self.copy_sample_to_temp_dir('pom.xml')
+
+        context = {
+            'dry_run': False,
+            'branch_parts': ['master'],
+            'a_task': False,
+            'is_stable': False,
+            'tags_on_commit': [],
+        }
+        new_version = '1.3.5-SNAPSHOT'
+
+        maven = GittyMaven()
+
+        # pre-check the version
+        maven.get_version_info(context)
+        self.assertEqual('1.0.0-SNAPSHOT', context['current_version'])
+
+        # update the version
+        maven.bump_version_to(context, new_version)
+
+        # check the version after we bump it
+        maven.get_version_info(context)
+        self.assertEqual(new_version, context['current_version'])
+
+        # remove the copied project file
+        os.remove(destination)
+
+        # go back where we started
+        os.chdir(cwd)
+
+    def test_do_not_bump_version_if_dry_run(self):
+        cwd = self.go_to_sample_dir()
+
+        # copy the pom to a temp location first
+        destination = self.copy_sample_to_temp_dir('pom.xml')
+
+        context = {
+            'dry_run': True,
+            'branch_parts': ['master'],
+            'a_task': False,
+            'is_stable': False,
+            'tags_on_commit': [],
+        }
+
+        maven = GittyMaven()
+
+        # pre-check the version
+        maven.get_version_info(context)
+        self.assertEqual('1.0.0-SNAPSHOT', context['current_version'])
+
+        # set it...but not really - just a dry run...
+        new_version = '1.3.5-SNAPSHOT'
+        maven.bump_version_to(context, new_version)
+
+        # verify it did not change
+        maven.get_version_info(context)
+        self.assertEqual('1.0.0-SNAPSHOT', context['current_version'])
+
+        # clean up the mess
+        os.remove(destination)
+
+        # go back where we started
+        os.chdir(cwd)
