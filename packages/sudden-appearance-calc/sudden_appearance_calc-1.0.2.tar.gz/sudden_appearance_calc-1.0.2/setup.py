fda41f8e82f752bdc254639ca33399360c6fb128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:15 +0000

    Add package sudden-appearance-calc/1.0.2/sudden_appearance_calc-1.0.2.tar.gz

---
+++ packages/sudden-appearance-calc/sudden_appearance_calc-1.0.2.tar.gz/sudden_appearance_calc-1.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(name='sudden_appearance_calc',
+      version='1.0.2',
+      description='Calc',
+      packages=['sudden_appearance_calc'],
+      entry_points={
+          'console_scripts': [
+              'calculator=sudden_appearance_calc.Calculator.Calculator:main'
+          ]
+      },
+      author_email='kuz.damir.01@gmail.com',
+      zip_safe=False)
