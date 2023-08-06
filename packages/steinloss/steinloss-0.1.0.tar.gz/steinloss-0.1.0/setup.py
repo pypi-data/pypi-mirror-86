29f2ff8cd3e157dd784f3aa3ceccc1a7ab4cb970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:40 +0000

    Add package steinloss/0.1.0/steinloss-0.1.0.tar.gz

---
+++ packages/steinloss/steinloss-0.1.0.tar.gz/steinloss-0.1.0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+
+setuptools.setup(
+    name="steinloss",  # Replace with your own username
+    version="0.1.0",
+    author="Steinwurf ApS",
+    author_email="contact@steinwurf.com",
+    description="Package loss measuring tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/steinwurf/steinloss",
+    entry_points={"console_scripts": ['steinloss=steinloss.stein_parser:cli']},
+    packages=setuptools.find_packages(),
+    install_requires=REQUIREMENTS,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Framework :: Dash"
+    ],
+    python_requires='>=3.8',
+)
