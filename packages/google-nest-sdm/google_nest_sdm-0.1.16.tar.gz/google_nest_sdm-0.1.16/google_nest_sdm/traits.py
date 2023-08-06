ee9870f3658c70d539c55999cdc866f9ef93a582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:22 +0000

    Add package google-nest-sdm/0.1.16/google_nest_sdm-0.1.16.tar.gz

---
+++ packages/google-nest-sdm/google_nest_sdm-0.1.16.tar.gz/google_nest_sdm-0.1.16/google_nest_sdm/traits.py
@@ -0,0 +1,37 @@
+import aiohttp
+
+from .auth import AbstractAuth
+from .registry import Registry
+
+DEVICE_TRAITS = "traits"
+
+TRAIT_MAP = Registry()
+
+
+class Command:
+    """Base class for executing commands."""
+
+    def __init__(self, device_id: str, auth: AbstractAuth):
+        self._device_id = device_id
+        self._auth = auth
+
+    async def execute(self, data: dict) -> aiohttp.ClientResponse:
+        resp = await self._auth.request(
+            "post", f"{self._device_id}:executeCommand", json=data
+        )
+        return AbstractAuth.raise_for_status(resp)
+
+
+def _TraitsDict(traits: dict, trait_map: dict, cmd: Command):
+    d = {}
+    for (trait, trait_data) in traits.items():
+        if trait not in trait_map:
+            continue
+        cls = trait_map[trait]
+        d[trait] = cls(trait_data, cmd)
+    return d
+
+
+def BuildTraits(traits: dict, cmd: Command) -> dict:
+    """Builds a trait map out of a response dict."""
+    return _TraitsDict(traits, TRAIT_MAP, cmd)
