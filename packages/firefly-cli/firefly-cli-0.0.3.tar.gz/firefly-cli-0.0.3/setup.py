84f330cb336a9f5062e5cb50a1f8bd9c8a6feb99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:10 +0000

    Add package firefly-cli/0.0.3/firefly-cli-0.0.3.tar.gz

---
+++ packages/firefly-cli/firefly-cli-0.0.3.tar.gz/firefly-cli-0.0.3/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="firefly-cli",
+    version="0.0.3",
+    author="Afonso Costa",
+    description="A python-based command line interface for practically entering expenses in Firefly III",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/afonsoc12/firefly-cli",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pandas==1.1.4',
+        'requests==2.25.0',
+        'tabulate==0.8.7'
+    ],
+    entry_points = {
+        'console_scripts': [
+            'firefly-cli = firefly_cli.__main__:main'
+        ]
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
