c88ccf392e638ef7b23d996b99e07fba888b46c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:30 +0000

    Add package frog-lib/0.2.8/frog_lib-0.2.8-py3-none-any.whl

---
+++ packages/frog-lib/frog_lib-0.2.8-py3-none-any.whl/frog_lib/omnigit.py
@@ -0,0 +1,20 @@
+from github import Github
+
+def create_repo(repo_name, token=None, provider='github'):
+  if not token:
+    print('Token required')
+  if 'github' in provider:
+    g = Github(token)
+    user = g.get_user()
+    repo = user.create_repo(repo_name, private=True)
+    return repo
+
+def delete_repo(repo_name, token=None, provider='github'):
+  if not token:
+    print('Token required')
+  if 'github' in provider:
+    g = Github(token)
+
+    repo = g.get_repo(f"{g.get_user().login}/{repo_name}")
+    repo.delete()
+    print(f"Repo {repo_name} deleted")
