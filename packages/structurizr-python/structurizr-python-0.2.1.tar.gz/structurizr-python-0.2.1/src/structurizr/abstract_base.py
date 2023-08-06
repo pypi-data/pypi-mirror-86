879575e3d96d652f7cddb9489d158680d80d84b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:59 +0000

    Add package structurizr-python/0.2.1/structurizr-python-0.2.1.tar.gz

---
+++ packages/structurizr-python/structurizr-python-0.2.1.tar.gz/structurizr-python-0.2.1/src/structurizr/abstract_base.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2020, Moritz E. Beber.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Provide a common abstract base class."""
+
+
+from abc import ABC
+
+
+__all__ = ("AbstractBase",)
+
+
+class AbstractBase(ABC):
+    """Define common business logic through an abstract base class."""
+
+    def __hash__(self) -> int:
+        """Return an integer that represents a unique hash value for this instance."""
+        return id(self)
+
+    def __repr__(self) -> str:
+        """Return a string representation of this instance."""
+        return f"{type(self).__name__}({id(self)})"
---
+++ packages/structurizr-python/structurizr-python-0.2.1.tar.gz/structurizr-python-0.2.1/src/structurizr/model/person.py
@@ -0,0 +1,82 @@
+# Copyright (c) 2020, Moritz E. Beber.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Provide a person model."""
+
+
+from typing import TYPE_CHECKING, Optional
+
+from pydantic import Field
+
+from .location import Location
+from .relationship import Relationship
+from .static_structure_element import StaticStructureElement, StaticStructureElementIO
+from .tags import Tags
+
+
+if TYPE_CHECKING:  # pragma: no cover
+    from .model import Model
+
+__all__ = ("PersonIO", "Person")
+
+
+class PersonIO(StaticStructureElementIO):
+    """
+    Represent a person in the C4 model.
+
+    Attributes:
+        location (Location): The location of this person.
+
+    """
+
+    location: Location = Field(
+        default=Location.Unspecified, description="The location of this person."
+    )
+
+
+class Person(StaticStructureElement):
+    """
+    Represent a person in the C4 model.
+
+    Attributes:
+        location (Location): The location of this person.
+
+    """
+
+    def __init__(self, *, location: Location = Location.Unspecified, **kwargs) -> None:
+        """Initialise a Person."""
+        super().__init__(**kwargs)
+        self.location = location
+
+        self.tags.add(Tags.PERSON)
+
+    @classmethod
+    def hydrate(cls, person_io: PersonIO, model: "Model") -> "Person":
+        """Create a new person and hydrate from its IO.
+
+        This will also automatically register with the model.
+        """
+        person = cls(
+            **cls.hydrate_arguments(person_io),
+            location=person_io.location,
+        )
+        model += person
+        return person
+
+    def interacts_with(
+        self, destination: "Person", description: str, **kwargs
+    ) -> Optional[Relationship]:
+        """Create a relationship with the given other Person."""
+        return self.uses(destination=destination, description=description, **kwargs)
