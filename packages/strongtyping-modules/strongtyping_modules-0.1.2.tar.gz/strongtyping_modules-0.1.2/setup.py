c4b13c61399a4b7a33e5c71018dbadf46925e28e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:19 +0000

    Add package strongtyping-modules/0.1.2/strongtyping_modules-0.1.2.tar.gz

---
+++ packages/strongtyping-modules/strongtyping_modules-0.1.2.tar.gz/strongtyping_modules-0.1.2/setup.py
@@ -0,0 +1,41 @@
+import pathlib
+
+from setuptools import find_packages
+from setuptools import setup
+
+try:
+    from Cython.Build import cythonize
+
+    ext_modules = cythonize('strongtyping_modules/strongtyping_modules.pyx',
+                            compiler_directives={'language_level': 3,
+                                                 'embedsignature': True})
+except (ImportError, NameError):
+    ext_modules = None
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(name='strongtyping_modules',
+      version='0.1.2',
+      description='Utils for strongtyping.',
+      author='Felix Eisenmenger',
+      author_email='fberndt87@gmail.com',
+      long_description=README,
+      long_description_content_type="text/markdown",
+      ext_modules=ext_modules,
+      zip_safe=False,
+      packages=find_packages(),
+      package_data={
+          '': ('*.pyx', '*.c'),
+      },
+      classifiers=[
+          'Programming Language :: Python :: 3',
+          'License :: OSI Approved :: MIT License',
+      ],
+      install_requires=[
+          'Cython==0.29.21'
+      ]
+      )
---
+++ packages/strongtyping-modules/strongtyping_modules-0.1.2.tar.gz/strongtyping_modules-0.1.2/strongtyping_modules/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@created: 17.08.20
+@author: felix
+"""
+from strongtyping_modules.install import install
+
+__all__ = (
+    'install',
+)
