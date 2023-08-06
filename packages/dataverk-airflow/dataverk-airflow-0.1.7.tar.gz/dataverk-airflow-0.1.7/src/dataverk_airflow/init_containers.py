71ab66c642da50343b9b870e9bda4db09970a555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:20 +0000

    Add package dataverk-airflow/0.1.7/dataverk-airflow-0.1.7.tar.gz

---
+++ packages/dataverk-airflow/dataverk-airflow-0.1.7.tar.gz/dataverk-airflow-0.1.7/src/dataverk_airflow/init_containers.py
@@ -0,0 +1,48 @@
+import os
+import kubernetes.client as k8s
+
+
+envs = [
+    {"name": "HTTPS_PROXY", "value": os.environ["HTTPS_PROXY"]},
+    {"name": "https_proxy", "value": os.environ["HTTPS_PROXY"]},
+]
+
+
+def create_git_clone_init_container(
+    repo: str,
+    branch: str,
+    mount_path: str
+):
+    return k8s.V1Container(
+        name="clone-repo",
+        image=os.getenv("CLONE_REPO_IMAGE", "navikt/knada-git-sync:2020-10-23-98963f6"),
+        volume_mounts=[
+            k8s.V1VolumeMount(
+                name="dags-data", mount_path=mount_path, sub_path=None, read_only=False
+            ),
+            k8s.V1VolumeMount(
+                name="git-clone-secret",
+                mount_path="/keys",
+                sub_path=None,
+                read_only=False,
+            ),
+        ],
+        env=envs,
+        command=["/bin/sh", "/git-clone.sh"],
+        args=[repo, branch, "/repo"],
+    )
+
+
+def change_permissions_init_container(
+    mount_path: str
+):
+    return k8s.V1Container(
+        name="change-permissions",
+        image="busybox",
+        volume_mounts=[
+            k8s.V1VolumeMount(
+                name="dags-data", mount_path=mount_path, sub_path=None, read_only=False
+            )
+        ],
+        command=['sh', '-c', f'chmod -R 777 {mount_path}'],
+    )
