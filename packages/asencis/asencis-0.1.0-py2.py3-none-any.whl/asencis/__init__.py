1e89c079a7d0b1bcbf8e16e103ac100f0bf27fdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:42 +0000

    Add package asencis/0.1.0/asencis-0.1.0-py2.py3-none-any.whl

---
+++ packages/asencis/asencis-0.1.0-py2.py3-none-any.whl/asencis/__init__.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import, division, print_function
+
+import os
+
+# Asencis Python bindings
+# API docs at https://asencis.com/documentation
+# Authors:
+# Michael Roberts <michael@asencis.com>
+
+__title__ = "asencis"
+__version__ = "0.1.0"
+__author__ = "asencis Ltd"
+__license__ = "MPL 2.0"
+__copyright__ = "2020 asencis, Ltd"
+
+# API resources
+from asencis.resources import *
+
+Datasets = Dataset()
+Domains = Domain()
+Prefixes = Prefix()
+Quantities = Quantity()
