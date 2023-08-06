0ca7f90dc4f3a2f01295d3f21d1289c2183dfaa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:50 +0000

    Add package pear-cli/0.2.0/pear_cli-0.2.0-py3-none-any.whl

---
+++ packages/pear-cli/pear_cli-0.2.0-py3-none-any.whl/pear/file_formatter.py
@@ -0,0 +1,42 @@
+from typing import List
+from pear.loader import Line
+
+
+class FileFormatter:
+
+    def apply(self, lines: List[Line]):
+        raise NotImplementedError
+
+    def remove(self, line_number: int, lines: List[Line]) -> List[Line]:
+        output = lines.copy()
+        output.pop(self._position_of_line_with_line_number(line_number, lines))
+        return output
+
+    def insert(self, line_number: int, text: str,
+               lines: List[Line]) -> List[Line]:
+        output = lines.copy()
+        pos = self._determine_position_for_insertion(line_number, lines)
+        output.insert(pos, Line(text))
+        return output
+
+    def _position_of_line_with_line_number(self, line_number: int,
+                                           lines: List[Line]) -> int:
+        try:
+            return next(i for i, line in enumerate(lines)
+                        if line.line_number == line_number)
+        except StopIteration:
+            raise MissingLineError()
+
+    def _determine_position_for_insertion(self, line_number: int,
+                                          lines: List[Line]) -> int:
+        if len(lines) == 0:
+            return 0
+
+        for i, line in enumerate(lines):
+            if (line.line_number is not None and line_number <= line.line_number):
+                return i
+        raise Exception
+
+
+class MissingLineError(Exception):
+    pass
