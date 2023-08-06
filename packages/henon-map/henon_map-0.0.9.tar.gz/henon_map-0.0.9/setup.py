50b3861cc513c6c6ed49c992ab6765e2ecc56a7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:20 +0000

    Add package henon-map/0.0.9/henon_map-0.0.9.tar.gz

---
+++ packages/henon-map/henon_map-0.0.9.tar.gz/henon_map-0.0.9/setup.py
@@ -0,0 +1,21 @@
+import os
+import re
+import sys
+import platform
+import subprocess
+
+from setuptools import setup
+
+__version__ = '0.0.9'
+
+setup(
+    name='henon_map',
+    version=__version__,
+    author='Carlo Emilio Montanari',
+    author_email='carlidel95@gmail.com',
+    description='Henon map implemented in Numba',
+    packages=["henon_map"],
+    install_requires=['numba', 'numpy', 'matplotlib'],
+    setup_requires=['numba', 'numpy', 'matplotlib'],
+    license='MIT',
+)
