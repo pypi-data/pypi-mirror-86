f2c0ee659cf66a072b470d38c91a23efacb7c6c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:49 +0000

    Add package autofront/0.1.1a4/autofront-0.1.1a4-py3-none-any.whl

---
+++ packages/autofront/autofront-0.1.1a4-py3-none-any.whl/autofront/__init__.py
@@ -0,0 +1,8 @@
+import autofront.autofront as autofront
+import autofront.utilities as utilities
+
+initialize = autofront.initialize
+create_route = autofront.create_route
+run = autofront.run
+
+get_display = utilities.get_display
