0592c8f1060ba8376ef0d84b93e950470605b47f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:59 +0000

    Add package sudden-appearance-tictactoe/1.0.4/sudden_appearance_tictactoe-1.0.4.tar.gz

---
+++ packages/sudden-appearance-tictactoe/sudden_appearance_tictactoe-1.0.4.tar.gz/sudden_appearance_tictactoe-1.0.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(name='sudden_appearance_tictactoe',
+      version='1.0.4',
+      description='Calc',
+      packages=['sudden_appearance_tictactoe'],
+      entry_points={
+          'console_scripts': [
+              'tic-tac-toe=sudden_appearance_tictactoe.Game:run'
+          ]
+      },
+      author_email='kuz.damir.01@gmail.com',
+      zip_safe=False)
---
+++ packages/sudden-appearance-tictactoe/sudden_appearance_tictactoe-1.0.4.tar.gz/sudden_appearance_tictactoe-1.0.4/sudden_appearance_tictactoe/Game.py
@@ -0,0 +1,33 @@
+import argparse
+import random
+
+from sudden_appearance_tictactoe.Grid import Grid
+from sudden_appearance_tictactoe.O import O
+from sudden_appearance_tictactoe.X import X
+
+
+def run():
+    parser = argparse.ArgumentParser(description="Tic-tac-toe game. 2 players. To make a move command <"
+                                                 "[x] [y]>, x - pos left, y - pos right")
+    a = Grid()
+    finished = None
+    players = [X(), O()]
+    player = random.randint(0, 1)
+    while not finished:
+        try:
+            print(a)
+            move = input(f"{players[player]}'s move\n").split()
+            a.insert(int(move[0]), int(move[1]), players[player])
+            player = 1 - player
+            finished = a.over()
+        except IndexError or ValueError:
+            print("Incorrect move. Try again")
+            continue
+        except IOError:
+            print("This spot is already taken. Try again")
+            continue
+    print(a)
+    if finished == "Draw":
+        print("Draw")
+    else:
+        print(str(finished) + " won")
