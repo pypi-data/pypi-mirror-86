c14297f1ab18373b41555f8b37e04c0d4a61eaac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:05 +0000

    Add package open-answerx/2.0.3/open-answerx-2.0.3.tar.gz

---
+++ packages/open-answerx/open-answerx-2.0.3.tar.gz/open-answerx-2.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+setup(
+    name='open-answerx',
+    version='2.0.3',
+    description='{OPEN} client for AnswerX Cloud and Managed',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    author='Diego Xirinachs',
+    author_email='dxirinac@akamai.com',
+    scripts=['open-answerx.py'],
+    url='https://github.com/dxiri/OAT',
+    namespace_packages=['akamai'],
+    packages=find_packages(),
+    python_requires=">=3.4",
+    install_requires=[
+        'edgegrid-python',
+        'requests'
+    ],
+    license='LICENSE.txt'
+
+)
