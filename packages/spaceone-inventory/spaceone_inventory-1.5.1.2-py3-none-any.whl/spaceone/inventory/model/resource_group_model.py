256d23b897ae6041d47642056bf424d7c0975d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:43 +0000

    Add package spaceone-inventory/1.5.1.2/spaceone_inventory-1.5.1.2-py3-none-any.whl

---
+++ packages/spaceone-inventory/spaceone_inventory-1.5.1.2-py3-none-any.whl/spaceone/inventory/model/resource_group_model.py
@@ -0,0 +1,48 @@
+from mongoengine import *
+from spaceone.core.model.mongo_model import MongoModel
+
+
+class Resource(EmbeddedDocument):
+    resource_type = StringField()
+    filter = ListField(DictField())
+    keyword = StringField(default=None, null=True)
+
+
+class ResourceGroup(MongoModel):
+    resource_group_id = StringField(max_length=40, generate_id='rsc-grp', unique=True)
+    name = StringField(max_length=255)
+    resources = ListField(EmbeddedDocumentField(Resource))
+    options = DictField()
+    tags = DictField()
+    project_id = StringField(max_length=255)
+    domain_id = StringField(max_length=255)
+    created_at = DateTimeField(auto_now_add=True)
+
+    meta = {
+        'updatable_fields': [
+            'name',
+            'resources',
+            'project_id',
+            'options',
+            'tags'
+        ],
+        'exact_fields': [
+            'resource_group_id',
+            'project_id',
+            'domain_id'
+        ],
+        'minimal_fields': [
+            'resource_group_id',
+            'name',
+            'project_id'
+        ],
+        'ordering': [
+            'name'
+        ],
+        'indexes': [
+            'resource_group_id',
+            'resources.resource_type',
+            'project_id',
+            'domain_id',
+        ]
+    }
