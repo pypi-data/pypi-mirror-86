f231f58833421719655fcefb7b254ca324dc15ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:17 +0000

    Add package giteapython/1.12.2/giteapython-1.12.2.tar.gz

---
+++ packages/giteapython/giteapython-1.12.2.tar.gz/giteapython-1.12.2/giteapython/api/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from giteapython.api.admin_api import AdminApi
+from giteapython.api.issue_api import IssueApi
+from giteapython.api.miscellaneous_api import MiscellaneousApi
+from giteapython.api.notification_api import NotificationApi
+from giteapython.api.organization_api import OrganizationApi
+from giteapython.api.repository_api import RepositoryApi
+from giteapython.api.user_api import UserApi
