93174110d5800f02ec827d9dc48f1f0e6dca96bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:26 +0000

    Add package oython/0.0.3/oython-0.0.3.tar.gz

---
+++ packages/oython/oython-0.0.3.tar.gz/oython-0.0.3/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="oython",
+    version="0.0.3",
+    author="Chengze Li",
+    author_email="3161399620@qq.com",
+    description="oython库，作者:李承泽（正常线路下载失败时，请切换为python原版路线）。",
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
