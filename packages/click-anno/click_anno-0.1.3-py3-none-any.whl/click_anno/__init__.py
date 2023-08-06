937d8bedcaed9f15e2f29c4ec9a151ce9e6da527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:18 +0000

    Add package click-anno/0.1.3/click_anno-0.1.3-py3-none-any.whl

---
+++ packages/click-anno/click_anno-0.1.3-py3-none-any.whl/click_anno/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2019~2999 - Cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+from .core import (
+    click_app, command, attrs
+)
+from .injectors import (
+    find, ensure, Injectable, inject
+)
