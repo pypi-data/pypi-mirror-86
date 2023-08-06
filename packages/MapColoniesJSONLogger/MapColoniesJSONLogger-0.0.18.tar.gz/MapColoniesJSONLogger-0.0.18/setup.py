bdf005ebe29943091c4663f21f55a26c74e8ec03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:22 +0000

    Add package MapColoniesJSONLogger/0.0.18/MapColoniesJSONLogger-0.0.18.tar.gz

---
+++ packages/MapColoniesJSONLogger/MapColoniesJSONLogger-0.0.18.tar.gz/MapColoniesJSONLogger-0.0.18/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="MapColoniesJSONLogger", 
+    version="0.0.18",
+    author="MapColonies",
+    author_email="mapcolonies@gmail.com",
+    description="A JSON logger for map colonies project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MapColonies/json-logger",
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages('src'),
+    install_requires=[
+        "python-json-logger >= 2.0.1"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.6',
+)
