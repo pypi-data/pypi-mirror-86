39e07c869d665f7a716c48905cac1c4194f8cbd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:04 +0000

    Add package meross-iot/0.4.1.0rc4/meross_iot-0.4.1.0rc4-py2.py3-none-any.whl

---
+++ packages/meross-iot/meross_iot-0.4.1.0rc4-py2.py3-none-any.whl/meross_iot/controller/known/humidifiers.py
@@ -0,0 +1,16 @@
+from meross_iot.controller.device import BaseDevice
+from meross_iot.controller.mixins.light import LightMixin
+from meross_iot.controller.mixins.spray import SprayMixin
+from meross_iot.controller.mixins.toggle import ToggleXMixin
+
+
+class MSXH0(SprayMixin, LightMixin, ToggleXMixin, BaseDevice):
+    """
+    MSXH0 Smart Humidifier devices
+    """
+    def __init__(self, device_uuid: str, manager, **kwargs):
+        if 'channels' not in kwargs:
+            kwargs['channels'] = [{}]
+        super().__init__(device_uuid=device_uuid,
+                         manager=manager,
+                         **kwargs)
