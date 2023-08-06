a4c038cc3f1810e8b0822c0b7a7f35471abb26c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:50 +0000

    Add package screen-brightness-control/0.5.0/screen_brightness_control-0.5.0-py3-none-any.whl

---
+++ packages/screen-brightness-control/screen_brightness_control-0.5.0-py3-none-any.whl/screen_brightness_control/__init__.py
@@ -0,0 +1,205 @@
+import platform,time,threading
+
+class ScreenBrightnessError(Exception):
+    '''raised when the brightness cannot be set/retrieved'''
+    def __init__(self, message="Cannot set/retrieve brightness level"):
+        self.message=message
+        super().__init__(self.message)
+
+def list_monitors():
+    '''
+    list all monitors that are controllable by this library (not yet implemented on Linux)
+
+    Returns:
+        list of strings (Windows) but None on Linux
+    '''
+    if platform.system() == 'Windows':
+        return windows.list_monitors()
+    elif platform.system() == 'Linux':
+        pass #return linux.list_monitors()
+
+def flatten_list(thick_list):
+    '''
+    internal function I use to flatten lists, because I do that often
+    
+    Args:
+        thick_list (list): The list to be flattened. Can be as deep as you wish (within recursion limits)
+
+    Returns:
+        one dimensional list
+    '''
+    flat_list = []
+    for item in thick_list:
+        if type(item) is list:
+            flat_list+=flatten_list(item)
+        else:
+            flat_list.append(item)
+    return flat_list
+
+def set_brightness(value,force=False,verbose_error=False,**kwargs):
+    '''
+    Sets the screen brightness
+
+    Args:
+        value (int or str): a value 0 to 100. This is a percentage or a string as '+5' or '-5'
+        force (bool): [Linux Only] if false the brightness will never be set lower than 1 (as 0 usually turns the screen off). If True, this check is bypassed
+        verbose_error (bool): boolean value controls the amount of detail error messages will contain
+        kwargs (dict): passed to the OS relevant brightness method
+    
+    Returns:
+        Returns the result of get_brightness()
+    '''
+    
+    if type(value) not in (int, float, str):
+        raise TypeError(f'value must be int, float or str, not {type(value)}')
+
+    if type(value) is str and value.startswith(('+', '-')):
+        if 'display' in kwargs.keys():
+            current = get_brightness(display=kwargs['display'])
+        else:
+            current = get_brightness()
+            if type(current) is list:
+                out = []
+                for i in range(len(current)):
+                    out.append(set_brightness(current[i] + int(float(str(value))), display = i, **kwargs))
+                #flatten the list output
+                out = flatten_list(out)
+                return out[0] if len(out)==1 else out 
+
+        value = current + int(float(str(value)))
+    else:
+        value = int(float(str(value)))
+
+    value = max(0, min(100, value))
+
+    method = None
+    if platform.system()=='Windows':
+        method = windows.set_brightness
+    elif platform.system()=='Linux':
+        if not force:
+            value = max(1, value)
+        method = linux.set_brightness
+    elif platform.system()=='Darwin':
+        error = 'MAC is unsupported'
+    else:
+        error = f'{platform.system()} is not supported'
+
+    if method!=None:
+        try:
+            return method(value, **kwargs)
+        except Exception as e:
+            if verbose_error:
+                raise ScreenBrightnessError from e
+            error = e
+
+    #if the function has not returned by now it failed
+    raise ScreenBrightnessError(f'Cannot set screen brightness: {error}')
+
+def fade_brightness(finish, start=None, interval=0.01, increment=1, blocking=True, **kwargs):
+    '''
+    A function to somewhat gently fade the screen brightness from start_value to finish_value
+
+    Args:
+        finish - the brighness level we end on
+        start - where the brightness should fade from
+        interval - the time delay between each step in brightness
+        increment - the amount to change the brightness by per loop
+        blocking - whether this should occur in the main thread (True) or a new daemonic thread (False)
+        kwargs - passed directly to set_brightness (see set_brightness docstring for available kwargs)
+    
+    Returns:
+        Returns a thread object if blocking is set to False, otherwise it returns the result of get_brightness()
+    '''
+    def fade(start, finish, increment, **kwargs):
+        if 'no_return' not in kwargs.keys():
+            kwargs['no_return']=True
+        for i in range(min(start,finish),max(start,finish),increment):
+            val=i
+            if start>finish:
+                val = start - (val-finish)
+            set_brightness(val, **kwargs)
+            time.sleep(interval)
+
+        del(kwargs['no_return'])
+        if get_brightness(**kwargs)!=finish:
+            set_brightness(finish, no_return = True, **kwargs)
+        return
+
+    current_vals = get_brightness(**kwargs)
+    current_vals = [current_vals, ] if type(current_vals)==int else current_vals
+
+    threads = []
+    a = 0
+    for current in current_vals:
+        st, fi = start, finish
+        #convert strings like '+5' to an actual brightness value
+        if type(fi)==str:
+            if "+" in fi or "-" in fi:
+                fi=current+int(float(fi))
+        if type(st)==str:
+            if "+" in st or "-" in st:
+                st=current+int(float(st))
+
+        st = current if st==None else st
+        #make sure both values are within the correct range
+        fi = min(max(int(fi),0),100)
+        st = min(max(int(st),0),100)
+
+        kw=kwargs.copy()
+        if 'display' not in kw.keys():
+            kw['display'] = a
+
+        if finish!=start:
+            t1 = threading.Thread(target=fade, args=(st, fi, increment), kwargs=kw)
+            t1.start()
+            threads.append(t1)
+        a+=1
+
+    if not blocking:
+        return threads
+    else:
+        for t in threads:
+            t.join()
+        return get_brightness(**kwargs)
+
+def get_brightness(verbose_error=False,**kwargs):
+    '''
+    Returns the current display brightness
+
+    Args:
+        verbose_error (bool): controls the level of detail in the error messages
+        kwargs (dict): is passed directly to the OS relevant brightness method
+    
+    Returns:
+        An integer between 0 and 100. However, it may return a list of integers if multiple monitors are detected
+    '''
+
+    method = None
+    if platform.system()=='Windows':
+        method = windows.get_brightness
+    elif platform.system()=='Linux':
+        method = linux.get_brightness
+    elif platform.system()=='Darwin':
+        error = 'MAC is unsupported'
+    else:
+        error = f'{platform.system()} is not supported'
+
+    if method!=None:
+        try:
+            return method(**kwargs)
+        except Exception as e:
+            if verbose_error:
+                raise ScreenBrightnessError from e
+            error = e
+
+    #if the function has not returned by now it failed
+    raise ScreenBrightnessError(f'Cannot get screen brightness: {error}')
+
+
+if platform.system()=='Windows':
+    from . import windows
+elif platform.system()=='Linux':
+    from . import linux
+
+__version__='0.5.0'
+__author__='Crozzers'
---
+++ packages/screen-brightness-control/screen_brightness_control-0.5.0-py3-none-any.whl/screen_brightness_control/linux.py
@@ -0,0 +1,240 @@
+import subprocess, os
+
+class Light:
+    '''collection of screen brightness related methods using the light executable'''
+    def get_display_names():
+        '''returns the names of each display, as reported by light'''
+        command = 'light -L'
+        res=subprocess.run(command.split(' '),stdout=subprocess.PIPE).stdout.decode().split('\n')
+        displays = []
+        for r in res:
+            if 'backlight' in r and 'sysfs/backlight/auto' not in r:
+                r = r[r.index('backlight/')+10:]
+                displays.append(r)
+        return displays
+
+    def set_brightness(value, display = None, no_return = False):
+        '''
+        Sets the brightness for a display using the light executable
+
+        Args:
+            value (int): Sets the brightness to this value
+            display (int): The index of the display you wish to change
+            no_return (bool): if True, this function returns None, returns the result of self.get_brightness() otherwise
+
+        Returns:
+            list, int (0 to 100) or None
+        '''
+        display_names = Light.get_display_names()
+        if display!=None:
+            if type(display) is str:
+                display = display_names.index(display)
+            display_names = [display_names[display]]
+        for name in display_names:
+            command = f'light -S {value} -s sysfs/backlight/{name}'
+            subprocess.call(command.split(" "))
+        return Light.get_brightness(display=display) if not no_return else None
+
+    def get_brightness(display = None):
+        '''
+        Sets the brightness for a display using the light executable
+
+        Args:
+            display (int): The index of the display you wish to query
+        
+        Returns:
+            list or int (0 to 100)
+        '''
+        display_names = Light.get_display_names()
+        if display!=None:
+            if type(display) is str:
+                display = display_names.index(display)
+            display_names = [display_names[display]]
+        results = []
+        for name in display_names:
+            command = f'light -G -s sysfs/backlight/{name}'
+            results.append(subprocess.run(command.split(' '),stdout=subprocess.PIPE).stdout.decode())
+        results = [int(round(float(str(i)),0)) for i in results]
+        return results[0] if len(results)==1 else results
+
+class XBacklight:
+    '''collection of screen brightness related methods using the xbacklight executable'''
+    def set_brightness(value, no_return = False, **kwargs):
+        '''
+        Sets the screen brightness to a supplied value
+
+        Args:
+            no_return (bool): if True, this function returns None, returns the result of self.get_brightness() otherwise
+
+        Returns:
+            int (0 to 100) or None
+        '''
+        command = 'xbacklight -set {}'.format(value)
+        subprocess.call(command.split(" "))
+        return XBacklight.get_brightness() if not no_return else None
+
+    def get_brightness(**kwargs):
+        '''Returns the screen brightness as reported by xbacklight'''
+        command = 'xbacklight -get'
+        res=subprocess.run(command.split(' '),stdout=subprocess.PIPE).stdout.decode()
+        return int(round(float(str(res)),0))
+
+class XRandr:
+    '''collection of screen brightness related methods using the xrandr executable'''
+    def get_display_names():
+        '''returns the names of each display, as reported by xrandr'''
+        out = subprocess.check_output(['xrandr', '-q']).decode().split('\n')
+        return [i.split(' ')[0] for i in out if 'connected' in i and not 'disconnected' in i]   
+
+    def get_brightness(display = None):
+        '''
+        Returns the brightness for a display using the xrandr executable
+
+        Args:
+            display (int): The index of the display you wish to query
+        
+        Returns:
+            list or int (0 to 100)
+        '''
+        out = subprocess.check_output(['xrandr','--verbose']).decode().split('\n')
+        lines = [int(float(i.replace('Brightness:','').replace(' ','').replace('\t',''))*100) for i in out if 'Brightness:' in i]
+        if display!=None:
+            if type(display) is str:
+                names = XRandr.get_display_names()
+                display = names.index(display)
+            return lines[display]
+        return lines[0] if len(lines)==1 else lines
+
+    def set_brightness(value, display = None, no_return = False):
+        '''
+        Sets the brightness for a display using the xrandr executable
+
+        Args:
+            value (int): Sets the brightness to this value
+            display (int): The index of the display you wish to change
+            no_return (bool): if True, this function returns None, returns the result of self.get_brightness() otherwise
+        
+        Returns:
+            The result of self.get_brightness()
+        '''
+        value = str(float(value)/100)
+        names = XRandr.get_display_names()
+        if display!=None:
+            if type(display) is str:
+                display = names.index(display)
+            names = [names[display]]
+        for name in names:
+            subprocess.run(['xrandr','--output', name, '--brightness', value])
+        return XRandr.get_brightness(display=display) if not no_return else None
+
+def get_brightness_from_sysfiles(display = None):
+    '''
+    Returns the current display brightness by reading files from /sys/class/backlight
+
+    Args:
+        display (int): The index of the display you wish to query
+    
+    Returns:
+        An integer between 0 and 100
+    '''
+    backlight_dir = '/sys/class/backlight/'
+    error = []
+    #if function has not returned yet try reading the brightness file
+    if os.path.isdir(backlight_dir) and os.listdir(backlight_dir)!=[]:
+        #if the backlight dir exists and is not empty
+        folders=[folder for folder in os.listdir(backlight_dir) if os.path.isdir(os.path.join(backlight_dir,folder))]
+        if display!=None:
+            folders = [folders[display]]
+        for folder in folders:
+            try:
+                #try to read the brightness value in the file
+                with open(os.path.join(backlight_dir,folder,'brightness'),'r') as f:
+                    brightness_value=int(float(str(f.read().rstrip('\n'))))
+
+                try:
+                    #try open the max_brightness file to calculate the value to set the brightness file to
+                    with open(os.path.join(backlight_dir,folder,'max_brightness'),'r') as f:
+                        max_brightness=int(float(str(f.read().rstrip('\n'))))
+                except:
+                    #if the file does not exist we cannot calculate the brightness
+                    return False
+                brightness_value=int(round((brightness_value/max_brightness)*100,0))
+                return brightness_value
+            except Exception as e:
+                error.append([type(Exception).__name__,e])
+        #if function hasn't returned, it failed
+        exc = f'Failed to get brightness from {backlight_dir}:'
+        for e in error:
+            exc+=f'\n    {e[0]}: {e[1]}'
+        raise Exception(exc)
+    raise FileNotFoundError(f'Backlight directory {backlight_dir} not found')
+
+def set_brightness(value, method = None, **kwargs):
+    '''
+    Sets the brightness for a display, cycles through Light, XRandr and XBacklight methods untill one works
+
+    Args:
+        value (int): Sets the brightness to this value
+        method (str): the method to use ('light', 'xrandr' or 'xbacklight')
+        kwargs (dict): passed directly to the chosen brightness method
+    
+    Returns:
+        The result of get_brightness()
+    '''
+    # use this as we will be modifying this list later and we don't want to change the global version
+    # just the local one
+    methods = globals()['methods'].copy()
+    if method != None:
+        try:
+            method = methods[method.lower()]
+        except:
+            raise IndexError("Chosen method is not valid, must be 'light', 'xrandr' or 'xbacklight'")
+    errors = []
+    for n,m in methods.items():
+        try:
+            return m.set_brightness(value, **kwargs)
+        except Exception as e:
+            errors.append([n, type(e).__name__, e])
+    #if function hasn't already returned it has failed
+    msg='\n'
+    for e in errors:
+        msg+=f'    {e[0]} -> {e[1]}: {e[2]}\n'
+    raise Exception(msg)
+
+def get_brightness(method = None, **kwargs):
+    '''
+    Returns the brightness for a display, cycles through Light, XRandr and XBacklight methods untill one works
+
+    Args:
+        method (str): the method to use ('light', 'xrandr' or 'xbacklight')
+        kwargs (dict): passed directly to chosen brightness method
+    
+    Returns:
+        An int between 0 and 100
+    '''
+    # use this as we will be modifying this list later and we don't want to change the global version
+    # just the local one
+    methods = globals()['methods'].copy()
+    if method != None:
+        try:
+            method = methods[method.lower()]
+        except:
+            raise IndexError("Chosen method is not valid, must be 'light', 'xrandr' or 'xbacklight'")
+    errors = []
+    for n,m in methods.items():
+        try:
+            return m.get_brightness(**kwargs)
+        except Exception as e:
+            errors.append([n, type(e).__name__, e])
+    #if function hasn't already returned it has failed
+    if method==None:
+        try:
+            return get_brightness_from_sysfiles(**kwargs)
+        except Exception as e:
+            errors.append(['/sys/class/backlight/*', type(e).__name__, e])
+    msg='\n'
+    for e in errors:
+        msg+=f'\t{e[0]} -> {e[1]}: {e[2]}\n'
+    raise Exception(msg)
+
+methods = {'Light': Light, 'XRandr': XRandr, 'XBacklight': XBacklight}
