b63093b8aed21d086fa65b887f5f8eff19044d4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:56 +0000

    Add package pydriosm/2.0.2/pydriosm-2.0.2.tar.gz

---
+++ packages/pydriosm/pydriosm-2.0.2.tar.gz/pydriosm-2.0.2/pydriosm/__init__.py
@@ -0,0 +1,14 @@
+from .downloader import BBBikeDownloader, GeofabrikDownloader
+from .ios import PostgresOSM
+from .reader import BBBikeReader, GeofabrikReader
+
+__all__ = ['downloader', 'GeofabrikDownloader', 'BBBikeDownloader',
+           'reader', 'GeofabrikReader', 'BBBikeReader',
+           'ios', 'PostgresOSM']
+
+__package_name__ = 'pydriosm'
+__package_name_alt__ = 'PyDriosm'
+__version__ = '2.0.2'
+__author__ = u'Qian Fu'
+__email__ = 'qian.fu@outlook.com'
+__description__ = 'An open-source tool for downloading, reading and PostgreSQL-based I/O of OpenStreetMap data.'
