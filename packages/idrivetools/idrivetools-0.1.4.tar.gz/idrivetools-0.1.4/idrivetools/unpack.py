7e490e9302ea06de90b9f0edec431336567897dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:06 +0000

    Add package idrivetools/0.1.4/idrivetools-0.1.4.tar.gz

---
+++ packages/idrivetools/idrivetools-0.1.4.tar.gz/idrivetools-0.1.4/idrivetools/unpack.py
@@ -0,0 +1,15 @@
+import click
+"""
+Take a folder containing a BMW backup and unpack it into a regular music folder
+"""
+
+
+@click.command()
+@click.argument("source", type=click.Path(exists=True), default=".")
+@click.argument("destination", default="restore")
+def unpack_files(source, destination):
+    print(f"Unpacking from {source} to {destination}!")
+
+
+if __name__ == "__main__":
+    unpack_files()
