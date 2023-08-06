f41617d376edbc1f107c1598cb49a7a8b993f0f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:19 +0000

    Add package f-durand-tests-github-actions/0.1.0/f_durand_tests_github_actions-0.1.0-py2.py3-none-any.whl

---
+++ packages/f-durand-tests-github-actions/f_durand_tests_github_actions-0.1.0-py2.py3-none-any.whl/f_durand_tests_github_actions/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for F Durand Tests GitHub Actions."""
+
+__author__ = """François Durand"""
+__email__ = 'fradurand@gmail.com'
+__version__ = '0.1.0'
+
+
+from f_durand_tests_github_actions.SubPackage1.MyClass1 import MyClass1
+from f_durand_tests_github_actions.SubPackage2.MyClass2 import MyClass2
+from f_durand_tests_github_actions.SubPackage2.MyClass3 import MyClass3
