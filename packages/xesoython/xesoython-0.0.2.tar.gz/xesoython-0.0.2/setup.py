a74e3ef2a4787f795404a358d55e8d68109a5c42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:27 +0000

    Add package xesoython/0.0.2/xesoython-0.0.2.tar.gz

---
+++ packages/xesoython/xesoython-0.0.2.tar.gz/xesoython-0.0.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="xesoython",
+    version="0.0.2",
+    author="Chengze Li",
+    author_email="3161399620@qq.com",
+    description="xesoython 本库作者:李承泽 (若正常路线下载失败，请切换python原版路线！)",
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
