64d17411619ae214f998d6b15a82b17aace8bee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:30 +0000

    Add package jinfo/0.17/jinfo-0.17.tar.gz

---
+++ packages/jinfo/jinfo-0.17.tar.gz/jinfo-0.17/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="jinfo",
+    version="0.17",
+    description="",
+    author="Jake Bowden",
+    url="https://github.com/JBwdn/jinfo",
+    author_email="jake.bowden95@gmail.com",
+    license="MIT",
+    packages=["jinfo"],
+    zip_safe=False,
+)
