d5e792673c7adae39fdadb6e683044f410035137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:04 +0000

    Add package razorback/0.3.0.3/razorback-0.3.0.3.tar.gz

---
+++ packages/razorback/razorback-0.3.0.3.tar.gz/razorback-0.3.0.3/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import pathlib
+
+
+PKG = pathlib.Path(__file__).parent
+
+
+setup(
+    name='razorback',
+    version='0.3.0.3',
+    description='Robust estimation of linear response functions',
+    author='Farid Smai',
+    author_email='f.smai@brgm.fr',
+    url='https://github.com/BRGM/razorback',
+    license='GNU GPLv3',
+    long_description=(PKG/"README.md").read_text(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=['numpy', 'scipy', 'dask'],
+)
