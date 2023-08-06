a42ddbbd41a7d49188f14eff007d7ed4b4c78257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:31 +0000

    Add package capybaras/0.1.2/capybaras-0.1.2.tar.gz

---
+++ packages/capybaras/capybaras-0.1.2.tar.gz/capybaras-0.1.2/gpu_plus/__init__.py
@@ -0,0 +1,9 @@
+#from . import funcDictionaryLevenshtein
+#from . import plotData
+#from . import opencl_matrix
+#from . import opencl_matrixCopy2
+from . import test_gpu
+
+__version__ = "0.1.0"
+__author__ = "Stephen Hudson"
+__credits__ = "Argonne National Laboratory"
