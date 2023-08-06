79e0e38997410e234c28e82a2c698a28b78e61a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:24 +0000

    Add package requests-random-user-agent/2020.11.22/requests_random_user_agent-2020.11.22.tar.gz

---
+++ packages/requests-random-user-agent/requests_random_user_agent-2020.11.22.tar.gz/requests_random_user_agent-2020.11.22/requests_random_user_agent/__init__.py
@@ -0,0 +1,12 @@
+import os
+import random
+
+import requests.utils
+
+__version__ = "2020.11.22"
+
+USER_AGENTS_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'useragents.txt')
+with open(USER_AGENTS_FILE) as fh:
+    USER_AGENTS = fh.read().splitlines()
+
+requests.utils.default_user_agent = lambda: random.choice(USER_AGENTS)
