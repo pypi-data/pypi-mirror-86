3603cf0152a131f2f8b4603417bf1e601b420c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:46 +0000

    Add package piepd/0.0.2.5/piepd-0.0.2.5.tar.gz

---
+++ packages/piepd/piepd-0.0.2.5.tar.gz/piepd-0.0.2.5/piepd/fonts.py
@@ -0,0 +1,8 @@
+import os,pathlib
+from pkg_resources import resource_string
+fontPath = resource_string(__name__, 'fonts/')
+castoro = os.path.join(fontPath,'castoro/Castoro-Regular.ttf')
+texturina = os.path.join(fontPath,'texturina/Texturina-Regular-opsz=24.ttf')
+inconsolata = os.path.join(fontPath,'inconsolata/static/Inconsolata-Black.ttf')
+nova = os.path.join(fontPath,'nova/NovaMono-Regular.ttf')
+print(nova)
