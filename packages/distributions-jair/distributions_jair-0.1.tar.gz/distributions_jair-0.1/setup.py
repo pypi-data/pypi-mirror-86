bc801867d086d31d3eef7607c3069ddd41b2f807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:36 +0000

    Add package distributions-jair/0.1/distributions_jair-0.1.tar.gz

---
+++ packages/distributions-jair/distributions_jair-0.1.tar.gz/distributions_jair-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_jair',
+      version='0.1',
+      description='Gaussian distributions Jair',
+      packages=['distributions'],
+      zip_safe=False)
