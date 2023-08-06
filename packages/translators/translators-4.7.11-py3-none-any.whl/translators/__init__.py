6ae596c55690e19148621c201dd3063fce94c0b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:27 +0000

    Add package translators/4.7.11/translators-4.7.11-py3-none-any.whl

---
+++ packages/translators/translators-4.7.11-py3-none-any.whl/translators/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "4.7.11"
+__author__ = "UlionTse"
+
+from .apis import alibaba, baidu, bing, deepl, google, sogou, tencent, yandex, youdao
+from .apis import _alibaba, _baidu, _bing, _deepl, _google, _sogou, _tencent, _yandex, _youdao
+from .apis import translate_html
