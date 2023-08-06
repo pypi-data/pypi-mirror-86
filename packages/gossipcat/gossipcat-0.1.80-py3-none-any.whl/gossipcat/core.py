4458547d31d71e0fc2131e770282aa13b8e46f21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:55 +0000

    Add package gossipcat/0.1.80/gossipcat-0.1.80-py3-none-any.whl

---
+++ packages/gossipcat/gossipcat-0.1.80-py3-none-any.whl/gossipcat/core.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+author:     Ewen Wang
+email:      wolfgangwong2012@gmail.com
+license:    Apache License 2.0
+"""
+
+import .FileSys
+import .Logging
+import .Presentation
+import .toolkit
