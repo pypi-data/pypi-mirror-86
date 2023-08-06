1b25d91b73dd7c845b6e700d57f6dab7f93fcf90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:34 +0000

    Add package petit-ts/0.1.5/petit_ts-0.1.5.tar.gz

---
+++ packages/petit-ts/petit_ts-0.1.5.tar.gz/petit_ts-0.1.5/petit_ts/const.py
@@ -0,0 +1,22 @@
+from typing import Any, List, Union
+
+INLINE_TOKEN = "__inline__"
+
+NoneType = type(None)
+
+DEFAULT_TYPES = {
+    str(bool): "boolean",
+    str(None): "void",
+    str(Any): "any",
+    str(int): "number /*int*/",
+    str(float): "number /*float*/",
+    str(str): "string",
+    str(dict): "object",
+    str(list): "any[]",
+    str(List[Any]): "any[]",
+    str(List): "any[]",
+    str(List[int]): "number[]",
+    str(List[str]): "string[]",
+}
+
+pseudo_classes = Union[dict, None, Any]
