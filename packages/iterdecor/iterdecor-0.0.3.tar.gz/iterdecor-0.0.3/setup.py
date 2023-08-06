99ffe7e0a41b57d34391b0f573ee079a4330bcce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:44 +0000

    Add package iterdecor/0.0.3/iterdecor-0.0.3.tar.gz

---
+++ packages/iterdecor/iterdecor-0.0.3.tar.gz/iterdecor-0.0.3/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="iterdecor",  # Required
+    version="0.0.3",  # Required
+    author="David Balogh",
+    author_email="david.balogh@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cinegemadar/iterabledecorator/",
+    classifiers=[  # Optional
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 4 - Beta",
+        # Indicate who your project is intended for
+        "Intended Audience :: Developers",
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate you support Python 3. These classifiers are *not*
+        # checked by 'pip install'. See instead 'python_requires' below.
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    keywords="decorator, iterable, iter",
+    # You can just specify package directories manually here if your project is
+    # simple. Or you can use find_packages().
+    #
+    # Alternatively, if you just want to distribute a single Python file, use
+    # the `py_modules` argument instead as follows, which will expect a file
+    # called `my_module.py` to exist:
+    #
+    #   py_modules=["my_module"],
+    #
+    packages=find_packages(include=["iterdecor.py"]),
+    python_requires=">=3.5, <4",
+)
