09a24499fe07a600da058de0ad8ddfd147e4068f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:40 +0000

    Add package picosvg/0.7.3/picosvg-0.7.3.tar.gz

---
+++ packages/picosvg/picosvg-0.7.3.tar.gz/picosvg-0.7.3/setup.py
@@ -0,0 +1,49 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Just a toy, enough setuptools to be able to install.
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name="picosvg",
+    use_scm_version={"write_to": "src/picosvg/_version.py"},
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    entry_points={
+        'console_scripts': [
+            'picosvg=picosvg.picosvg:main',
+        ],
+    },
+
+    setup_requires=["setuptools_scm"],
+    install_requires=[
+        "dataclasses>=0.7; python_version < '3.7'",
+        "lxml>=4.0",
+        "skia-pathops>=0.4.1",
+    ],
+    python_requires=">=3.6",
+
+    # this is for type checker to use our inline type hints:
+    # https://www.python.org/dev/peps/pep-0561/#id18
+    package_data={"picosvg": ["py.typed"]},
+
+    # metadata to display on PyPI
+    author="Rod S",
+    author_email="rsheeter@google.com",
+    description=(
+        "Exploratory utility for svg simplification, "
+        "meant for use playing with COLR fonts"
+    ),
+)
