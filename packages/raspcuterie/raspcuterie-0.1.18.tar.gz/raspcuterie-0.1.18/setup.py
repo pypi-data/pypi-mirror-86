72b82d31edcc7d326d4ea8877ca174d23155779c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:26 +0000

    Add package raspcuterie/0.1.18/raspcuterie-0.1.18.tar.gz

---
+++ packages/raspcuterie/raspcuterie-0.1.18.tar.gz/raspcuterie-0.1.18/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+minimal_requirements = [
+    "Flask==1.1.2",
+    "Click==7.1.2",
+    "pyyaml==5.3.1",
+    "flask-babel==2.0.0",
+]
+
+
+def get_long_description():
+    """
+    Return the README.
+    """
+    return open("README.md", "r", encoding="utf8").read()
+
+
+rpi_requirements = ["rpi.gpio==0.7.0", "Adafruit_DHT==1.4.0"]
+
+setup(
+    description="Charcuterie dashboard and controller for the Raspberry PI",
+    license="MIT",
+    name="raspcuterie",
+    version="0.1.18",
+    py_modules=["raspcuterie"],
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    url="https://github.com/foarsitter/raspcuterie",
+    include_package_data=True,
+    entry_points={
+        "console_scripts": ["raspcuterie=raspcuterie.cli:cli"],
+    },
+    install_requires=minimal_requirements,
+    extras_require={"rpi": rpi_requirements},
+)
