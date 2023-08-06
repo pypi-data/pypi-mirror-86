3035be6080298359989b86183122771bfe110d98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:49 +0000

    Add package eloquentarduino/0.0.19/eloquentarduino-0.0.19.tar.gz

---
+++ packages/eloquentarduino/eloquentarduino-0.0.19.tar.gz/eloquentarduino-0.0.19/eloquentarduino/ml/metrics/device/__init__.py
@@ -0,0 +1,3 @@
+from eloquentarduino.ml.metrics.device.Resources import Resources
+from eloquentarduino.ml.metrics.device.Runtime import Runtime
+from eloquentarduino.ml.metrics.device.BenchmarkEndToEnd import BenchmarkEndToEnd
