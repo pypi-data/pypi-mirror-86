9d08d6f8b31a5df8d1c1be060eafe752979cbc0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:27 +0000

    Add package mamba-gator/4.0.0/mamba_gator-4.0.0.tar.gz

---
+++ packages/mamba-gator/mamba_gator-4.0.0.tar.gz/mamba_gator-4.0.0/packages/navigator/src/index.ts
@@ -0,0 +1,23 @@
+import 'whatwg-fetch';
+
+import { Gator } from './app/app';
+
+import '../style/index.css';
+
+/**
+ * The main function
+ */
+async function main(): Promise<void> {
+  const app = new Gator();
+  const mods = [
+    require('./plugins/paths'),
+    require('./plugins/navigator'),
+    require('./plugins/top')
+  ];
+
+  app.registerPluginModules(mods);
+
+  await app.start();
+}
+
+window.addEventListener('load', main);
