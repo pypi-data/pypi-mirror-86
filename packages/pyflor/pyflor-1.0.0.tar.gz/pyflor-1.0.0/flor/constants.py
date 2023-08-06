40da79b562680045bd7633a9034ea53b630710a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:30 +0000

    Add package pyflor/1.0.0/pyflor-1.0.0.tar.gz

---
+++ packages/pyflor/pyflor-1.0.0.tar.gz/pyflor-1.0.0/flor/constants.py
@@ -0,0 +1,12 @@
+# Constants
+class EXEC: pass
+class REEXEC: pass
+class ANY: pass
+
+class REDUNDANT: pass
+class SEPARATOR: pass
+class LBRACKET: pass
+class RBRACKET: pass
+
+class WEAK: pass
+class STRONG: pass
