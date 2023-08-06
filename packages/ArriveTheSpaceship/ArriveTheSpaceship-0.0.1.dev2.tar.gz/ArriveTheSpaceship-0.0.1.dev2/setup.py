e6b46da203ca104835b989211abe1cd23f79386a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:16 +0000

    Add package ArriveTheSpaceship/0.0.1.dev2/ArriveTheSpaceship-0.0.1.dev2.tar.gz

---
+++ packages/ArriveTheSpaceship/ArriveTheSpaceship-0.0.1.dev2.tar.gz/ArriveTheSpaceship-0.0.1.dev2/setup.py
@@ -0,0 +1,27 @@
+import pygame
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ArriveTheSpaceship",
+    version="0.0.1.dev2",
+    author="Vinícius Azevedo",
+    author_email="sousa0240@gmail.com",
+    description="A frogger-like game made with Pygame",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/viniciussousaazevedo/arrive_the_spaceship",
+    packages=setuptools.find_packages(),
+    install_requires = ['pygame >= 2.0'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts':['arrive-the-spaceship = src.__main__:start']
+    },
+    python_requires='>=3.6',
+)
