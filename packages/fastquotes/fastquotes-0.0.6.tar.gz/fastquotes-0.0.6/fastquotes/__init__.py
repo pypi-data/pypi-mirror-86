19791181610d24d32cdaafd6bd8c532471232d3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:58 +0000

    Add package fastquotes/0.0.6/fastquotes-0.0.6.tar.gz

---
+++ packages/fastquotes/fastquotes-0.0.6.tar.gz/fastquotes-0.0.6/fastquotes/__init__.py
@@ -0,0 +1,18 @@
+from fastquotes.async_quotes.async_sina import AsyncSinaQuote
+from fastquotes.async_quotes.async_tencent import AsyncTencentQuote
+from fastquotes.quotes.sina import SinaQuote
+from fastquotes.quotes.tencent import TencentQuote
+
+from .stock_list import (
+    async_exchange_stock_list,
+    async_stock_list,
+    exchange_stock_list,
+    stock_list,
+    stock_list_sh,
+    stock_list_sz,
+)
+from .trade_calendar import TradeCalendar
+from .utils import exchange_prefix
+
+__version__ = "0.0.6"
+__author__ = "nocilantro"
