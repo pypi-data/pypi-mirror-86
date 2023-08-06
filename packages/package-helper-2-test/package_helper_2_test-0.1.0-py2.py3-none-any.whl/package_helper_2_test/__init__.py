ccabc118d67dcd1fb4152e1f06b812c4c2255308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:09 +0000

    Add package package-helper-2-test/0.1.0/package_helper_2_test-0.1.0-py2.py3-none-any.whl

---
+++ packages/package-helper-2-test/package_helper_2_test-0.1.0-py2.py3-none-any.whl/package_helper_2_test/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for Package Helper 2 Test."""
+
+__author__ = """François Durand"""
+__email__ = 'fradurand@gmail.com'
+__version__ = '0.1.0'
+
+
+from package_helper_2_test.sub_package_1.my_class_1 import MyClass1
+from package_helper_2_test.sub_package_2.my_class_2 import MyClass2
+from package_helper_2_test.sub_package_2.my_class_3 import MyClass3
