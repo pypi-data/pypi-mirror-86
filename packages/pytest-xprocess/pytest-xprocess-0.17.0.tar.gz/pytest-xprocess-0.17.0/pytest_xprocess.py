0a48bf57c0f1a60483ee0da7fcbd6d81513c6ada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:11 +0000

    Add package pytest-xprocess/0.17.0/pytest-xprocess-0.17.0.tar.gz

---
+++ packages/pytest-xprocess/pytest-xprocess-0.17.0.tar.gz/pytest-xprocess-0.17.0/pytest_xprocess.py
@@ -0,0 +1,55 @@
+import py
+import pytest
+
+from xprocess import XProcess
+
+
+def pytest_addoption(parser):
+    group = parser.getgroup(
+        "xprocess", "managing external processes across test-runs [xprocess]"
+    )
+    group.addoption("--xkill", action="store_true", help="kill all external processes")
+    group.addoption(
+        "--xshow", action="store_true", help="show status of external process"
+    )
+
+
+def getrootdir(config):
+    return config.cache.makedir(".xprocess")
+
+
+def pytest_cmdline_main(config):
+    xkill = config.option.xkill
+    xshow = config.option.xshow
+    if xkill or xshow:
+        config._do_configure()
+        tw = py.io.TerminalWriter()
+        rootdir = getrootdir(config)
+        xprocess = XProcess(config, rootdir)
+    if xkill:
+        return xprocess._xkill(tw)
+    if xshow:
+        return xprocess._xshow(tw)
+
+
+@pytest.fixture(scope="session")
+def xprocess(request):
+    """Return session-scoped XProcess helper to manage long-running
+    processes required for testing.
+    """
+    rootdir = getrootdir(request.config)
+    return XProcess(request.config, rootdir)
+
+
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item, call):
+    logfiles = getattr(item.config, "_extlogfiles", None)
+    report = yield
+    if logfiles is None:
+        return
+    for name in sorted(logfiles):
+        content = logfiles[name].read()
+        if content:
+            longrepr = getattr(report, "longrepr", None)
+            if hasattr(longrepr, "addsection"):  # pragma: no cover
+                longrepr.addsection("%s log" % name, content)
