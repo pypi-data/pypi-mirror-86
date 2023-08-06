a629be9cc4f2c0f9c99a8ab1c3c6ce34f83d74b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:39 +0000

    Add package seldon-deploy-sdk/0.2.1/seldon-deploy-sdk-0.2.1.tar.gz

---
+++ packages/seldon-deploy-sdk/seldon-deploy-sdk-0.2.1.tar.gz/seldon-deploy-sdk-0.2.1/seldon_deploy_sdk/auth/session.py
@@ -0,0 +1,40 @@
+from urllib3 import Retry
+
+from seldon_deploy_sdk.configuration import Configuration
+from seldon_deploy_sdk.rest import RESTClientObject
+
+from .base import Authenticator
+
+
+class SessionAuthenticator(Authenticator):
+    """
+    Returns the cookie token.
+    """
+
+    def __init__(self, config: Configuration):
+        super().__init__(config)
+        self._client = RESTClientObject(config)
+
+    def authenticate(self, user: str, password: str) -> str:
+        auth_path = self._get_auth_path()
+        session_cookie = self._submit_auth(auth_path, user, password)
+        return session_cookie
+
+    def _get_auth_path(self) -> str:
+        # Send unauthenticated request
+        res = self._client.GET(self._server)
+        return res.urllib3_response.geturl()
+
+    def _submit_auth(self, auth_path: str, user: str, password: str) -> str:
+        auth_endpoint = f"{self._host}{auth_path}"
+        auth_payload = {"login": user, "password": password}
+
+        res = self._client.pool_manager.request(
+            "POST",
+            auth_endpoint,
+            fields=auth_payload,
+            encode_multipart=True,
+            retries=Retry(redirect=2, raise_on_redirect=False),
+        )
+
+        return res.getheader("set-cookie")
