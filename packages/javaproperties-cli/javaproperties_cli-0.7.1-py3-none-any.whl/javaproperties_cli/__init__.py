677482f89a31e0ff26dadd452dee583f2bd29b56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:16 +0000

    Add package javaproperties-cli/0.7.1/javaproperties_cli-0.7.1-py3-none-any.whl

---
+++ packages/javaproperties-cli/javaproperties_cli-0.7.1-py3-none-any.whl/javaproperties_cli/__init__.py
@@ -0,0 +1,17 @@
+"""
+Command-line manipulation of Java .properties files
+
+``javaproperties-cli`` is a wrapper around the ``javaproperties`` package (from
+which it was split off) that provides programs for basic command-line
+manipulation of ``.properties`` files, including getting, setting, & deleting
+values and converting to & from JSON.
+
+Visit <https://github.com/jwodder/javaproperties-cli> or
+<http://javaproperties-cli.rtfd.io> for more information.
+"""
+
+__version__      = '0.7.1'
+__author__       = 'John Thorvald Wodder II'
+__author_email__ = 'javaproperties-cli@varonathe.org'
+__license__      = 'MIT'
+__url__          = 'https://github.com/jwodder/javaproperties-cli'
