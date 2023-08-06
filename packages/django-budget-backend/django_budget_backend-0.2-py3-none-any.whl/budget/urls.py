259b30e4a6e87327ddb48a4840288ede383b6ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:38 +0000

    Add package django-budget-backend/0.2/django_budget_backend-0.2-py3-none-any.whl

---
+++ packages/django-budget-backend/django_budget_backend-0.2-py3-none-any.whl/budget/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from budget import views
+
+urlpatterns = [
+    path('budget', views.BudgetListCreateView.as_view(), name='budget-lc'),
+    path('budget/<uuid:id>', views.BudgetRetrieveUpdateDestroyView.as_view(), name='budget-rud'),
+    path('lineitem', views.LineItemListCreateView.as_view(), name='line-item-lc'),
+    path('lineitem/<uuid:id>', views.LineItemRetrieveUpdateDestroyView.as_view(), name='line-item-rud'),
+    path('transaction', views.TransactionListCreateView.as_view(), name='transaction-lc'),
+    path('transaction/<uuid:id>', views.TransactionRetrieveUpdateDestroyView.as_view(), name='transaction-rud')
+]
