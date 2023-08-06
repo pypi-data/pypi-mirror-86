a8064db9af67edbb717e6b0fe84c23e814d2338c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:31 +0000

    Add package osha.oira/6.2.11/osha.oira-6.2.11.tar.gz

---
+++ packages/osha.oira/osha.oira-6.2.11.tar.gz/osha.oira-6.2.11/src/osha/oira/client/conditions.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+from five import grok
+from euphorie.client.conditions import TermsAndConditions as BaseTermsAndConditions  # noqa
+from .interfaces import IOSHAClientSkinLayer
+
+grok.templatedir("templates")
+
+
+class TermsAndConditions(BaseTermsAndConditions):
+    grok.name("terms-and-conditions")
+    grok.layer(IOSHAClientSkinLayer)
+    grok.template("privacy-policy")
