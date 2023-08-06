ca09e682d8ac1d12b12c209f6cd2c284c34b4188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:11 +0000

    Add package pip-tree/0.4.0/pip-tree-0.4.0.tar.gz

---
+++ packages/pip-tree/pip-tree-0.4.0.tar.gz/pip-tree-0.4.0/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pip-tree',
+    version='0.4.0',
+    description='Get the dependency tree of your Python virtual environment via Pip.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/justintime50/pip-tree',
+    author='Justintime50',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    extras_require={
+        'dev': [
+            'pytest >= 6.0.0',
+            'pytest-cov >= 2.10.0',
+            'coveralls >= 2.1.2',
+            'flake8 >= 3.8.0',
+            'mock >= 4.0.0',
+        ]
+    },
+    entry_points={
+        'console_scripts': [
+            'pip-tree=pip_tree.tree:main'
+        ]
+    },
+    python_requires='>=3.6',
+)
