699c49b7e43350c119092311e44973698a1ff550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:28 +0000

    Add package jpe/0.1.0.1c/jpe-0.1.0.1c.tar.gz

---
+++ packages/jpe/jpe-0.1.0.1c.tar.gz/jpe-0.1.0.1c/jpe/__init__.py
@@ -0,0 +1,7 @@
+from jpe.Jfloat import Jfloat as jfloat
+from jpe.random import perlinNoise
+import jpe.math.linalg
+import jpe.crono.timer as crono
+
+
+    
---
+++ packages/jpe/jpe-0.1.0.1c.tar.gz/jpe-0.1.0.1c/jpe/math/linalg/__init__.py
@@ -0,0 +1,2 @@
+from jpe.math.linalg.vector import vector
+from jpe.math.linalg.linearEquasionSys import linearEquasionSystem, rowEdulants, rowEtulantSystemJfloat, linearEquasionSystemJfloat
