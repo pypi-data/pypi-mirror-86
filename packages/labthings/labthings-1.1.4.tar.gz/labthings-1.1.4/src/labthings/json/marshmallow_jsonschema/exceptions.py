a2c5ca4f08e952b48a2d5f5cf8c68dd8497cd047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:21 +0000

    Add package labthings/1.1.4/labthings-1.1.4.tar.gz

---
+++ packages/labthings/labthings-1.1.4.tar.gz/labthings-1.1.4/src/labthings/json/marshmallow_jsonschema/exceptions.py
@@ -0,0 +1,4 @@
+class UnsupportedValueError(Exception):
+    """ """
+
+    pass
---
+++ packages/labthings/labthings-1.1.4.tar.gz/labthings-1.1.4/src/labthings/monkey.py
@@ -0,0 +1,14 @@
+import logging
+
+
+def patch_all(*args, **kwargs):
+    """
+
+    :param *args:
+    :param **kwargs:
+
+    """
+    logging.warning(
+        "monkey.patch_all is deprecated and will be removed in a future version"
+    )
+    return
