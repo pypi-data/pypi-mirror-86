8699a0492ab7421fd0113066724fc19be3c25eff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:50 +0000

    Add package fns/0.4.30/fns-0.4.30-py3-none-any.whl

---
+++ packages/fns/fns-0.4.30-py3-none-any.whl/fns/colab.py
@@ -0,0 +1,28 @@
+import subprocess
+import os
+
+
+def run_foreground(cmd):
+    """from http://blog.kagesenshi.org/2008/02/teeing-python-subprocesspopen-output.html
+    """
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    while True:
+        line = p.stdout.readline()
+        print(line.strip())
+        if line == '' and p.poll() != None:
+            break
+    return None
+
+
+def run_background(command):
+    subprocess.Popen(command, shell=True)
+
+
+def jupyter():
+    from google.colab import drive
+    drive.mount('/content/drive')
+    os.chdir('/content/drive/MyDrive/colab/')
+    run_background('pip install jupyterlab --upgrade -qqq')
+    command = 'nohup jupyter lab --notebook-dir="/content/drive/MyDrive/colab" --no-browser --allow-root --ip="0.0.0.0" --port="6006" &'
+    run_background(command)
+    run_foreground('npx localtunnel --port 6006')
