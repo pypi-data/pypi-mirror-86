41a49ea451fd18a82699177ede77ef71e22bf498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:09 +0000

    Add package b-continuous-subprocess/0.0.3/b_continuous_subprocess-0.0.3.tar.gz

---
+++ packages/b-continuous-subprocess/b_continuous_subprocess-0.0.3.tar.gz/b_continuous_subprocess-0.0.3/HISTORY.md
@@ -0,0 +1,12 @@
+# Release history
+
+### 0.0.3
+* Start using Pylint.
+* Start using Black.
+
+### 0.0.2
+* Finalize pipelines.
+* Finalize project structure and naming.
+
+### 0.0.1
+* Initial build.
---
+++ packages/b-continuous-subprocess/b_continuous_subprocess-0.0.3.tar.gz/b_continuous_subprocess-0.0.3/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    HISTORY = history_file.read()
+
+with open('VERSION') as file:
+    VERSION = file.read()
+    VERSION = ''.join(VERSION.split())
+
+setup(
+    name='b_continuous_subprocess',
+    version=VERSION,
+    license='Apache License 2.0',
+    packages=find_packages(
+        exclude=[
+            # Exclude virtual environment.
+            'venv',
+            # Exclude test source files.
+            'b_continuous_subprocess_test',
+        ]
+    ),
+    description=(
+        'Allows to execute subprocess commands and get their output in real-time.'
+    ),
+    long_description=README + '\n\n' + HISTORY,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    install_requires=['pytest>=6.0.2,<7.0.0', 'pytest-cov>=2.10.1,<3.0.0'],
+    author='Laimonas Sutkus',
+    author_email='laimonas.sutkus@biomapas.com',
+    keywords='Subprocess',
+    url='https://github.com/biomapas/B.ContinuousSubprocess.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
