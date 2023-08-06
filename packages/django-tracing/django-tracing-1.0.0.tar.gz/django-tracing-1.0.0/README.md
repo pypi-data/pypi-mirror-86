82c723bd12e437de1f5f47acff361e4054b44731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:13 +0000

    Add package django-tracing/1.0.0/django-tracing-1.0.0.tar.gz

---
+++ packages/django-tracing/django-tracing-1.0.0.tar.gz/django-tracing-1.0.0/README.md
@@ -0,0 +1,21 @@
+=====
+Hydra
+=====
+
+Hydra is a Django app to build templates easily, and generate site page like django admin.
+
+Detailed documentation is in the "docs" directory.
+
+Quick start
+-----------
+
+1. Add "hydra" to your INSTALLED_APPS setting like this::
+
+    INSTALLED_APPS = [
+        ...
+        'tracing',
+    ]
+
+3. Run ``python manage.py migrate`` to create the polls migrations.
+
+4. Continue
---
+++ packages/django-tracing/django-tracing-1.0.0.tar.gz/django-tracing-1.0.0/tracing/admin.py
@@ -0,0 +1,23 @@
+""" Audit model admin """
+
+# Django
+from django.contrib import admin
+
+# Models
+from .models import Trace, Rule
+
+# Forms
+from .forms import RuleForm
+
+@admin.register(Trace)
+class TraceAdmin(admin.ModelAdmin):
+    list_display = ('content_object', 'date', 'user', 'action')
+    list_filter = ("action", "date")
+
+    def get_date(self, obj):
+        return obj.date
+
+@admin.register(Rule)
+class RuleAdmin(admin.ModelAdmin):
+    fields = ('content_type', 'check_create', 'check_edit', 'check_delete', 'is_active')
+    list_display = ('content_type', 'check_create', 'check_edit', 'check_delete', 'created_user', 'modified_user')
