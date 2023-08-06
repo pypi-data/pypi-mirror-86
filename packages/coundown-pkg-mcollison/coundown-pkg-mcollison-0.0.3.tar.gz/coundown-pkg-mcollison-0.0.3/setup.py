e05a53ee102fb27aa663638c41bfd544dac2c1be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:50 +0000

    Add package coundown-pkg-mcollison/0.0.3/coundown-pkg-mcollison-0.0.3.tar.gz

---
+++ packages/coundown-pkg-mcollison/coundown-pkg-mcollison-0.0.3.tar.gz/coundown-pkg-mcollison-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="coundown-pkg-mcollison", # Replace with your own username
+    version="0.0.3",
+    author="Matt Collison",
+    author_email="m.collison@exeter.ac.uk",
+    description="A simulation of the countdown game show used for demonstration.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mcollison/CA1-countdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
