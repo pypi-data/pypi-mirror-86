dee0953b7c9cf9113c0e59f51d85ba583a45e119
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:31 +0000

    Add package sparkautomapper/0.1.67/sparkautomapper-0.1.67.tar.gz

---
+++ packages/sparkautomapper/sparkautomapper-0.1.67.tar.gz/sparkautomapper-0.1.67/spark_auto_mapper/automappers/complex.py
@@ -0,0 +1,31 @@
+from typing import Optional, Dict
+
+from pyspark.sql.types import StructType, StructField
+
+from spark_auto_mapper.automappers.container import AutoMapperContainer
+from spark_auto_mapper.data_types.complex.complex_base import AutoMapperDataTypeComplexBase
+
+
+class AutoMapperWithComplex(AutoMapperContainer):
+    def __init__(
+        self, entity: AutoMapperDataTypeComplexBase, use_schema: bool,
+        include_extension: bool
+    ) -> None:
+        super().__init__()
+
+        # ask entity for its schema
+        schema: Optional[StructType] = entity.get_schema(
+            include_extension=include_extension
+        )
+        column_schema: Dict[str,
+                            StructField] = {f.name: f
+                                            for f in schema
+                                            } if schema and use_schema else {}
+
+        self.generate_mappers(
+            mappers_dict={
+                key: value
+                for key, value in entity.get_child_mappers().items()
+            },
+            column_schema=column_schema
+        )
