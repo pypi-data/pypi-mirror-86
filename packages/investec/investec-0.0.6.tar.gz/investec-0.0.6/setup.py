c61dacaffa932c6af393543c555f4fc1060930f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:00 +0000

    Add package investec/0.0.6/investec-0.0.6.tar.gz

---
+++ packages/investec/investec-0.0.6.tar.gz/investec-0.0.6/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="investec",
+    version="0.0.6",
+    author="Kevin Smith",
+    author_email="kevinsmith.kis@gmail.com",
+    url="https://github.com/Aurelian-Shuttleworth/investec_banking_integration",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
