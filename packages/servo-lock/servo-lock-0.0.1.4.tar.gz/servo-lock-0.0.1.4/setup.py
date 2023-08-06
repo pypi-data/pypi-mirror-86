78011e5fdad738148825dad486083fdbff08a82a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:14 +0000

    Add package servo-lock/0.0.1.4/servo-lock-0.0.1.4.tar.gz

---
+++ packages/servo-lock/servo-lock-0.0.1.4.tar.gz/servo-lock-0.0.1.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+long_description = open("README.md").read()
+
+setup(
+        name='servo-lock',
+        version='0.0.1.4',
+        description='Servo utility for the MG996R; but any servo works.',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        author_email='william.wyatt@cgu.edu',
+        url='https://github.com/Tsangares/lock_servo',
+        include_package_data=True,
+        packages=find_packages(),
+        install_requires=[
+            ],
+        entry_points={
+            'console_scripts': [
+                'servo = src.servo:main',
+                ]
+            },
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        python_requires='>=3.7',
+)
