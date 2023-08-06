f6984cc653f6ac0a6df05da5cb6e3d7f670cacc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:30 +0000

    Add package pylumi/1.0.1/pylumi-1.0.1-cp36-cp36m-macosx_10_15_x86_64.whl

---
+++ packages/pylumi/pylumi-1.0.1-cp36-cp36m-macosx_10_15_x86_64.whl/pylumi/__init__.py
@@ -0,0 +1,8 @@
+from pylumi import exc
+from pylumi.context import Context
+from pylumi.provider import Provider
+from pylumi.urn import URN
+
+__version__ = "1.0.1"
+
+__pulumi_version__ = "2.12.0"
