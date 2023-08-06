b583b63de282b892fe36e59156b30f166712a5cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:01 +0000

    Add package genie.libs.clean/20.10.2b1/genie.libs.clean-20.10.2b1-py3-none-any.whl

---
+++ packages/genie.libs.clean/genie.libs.clean-20.10.2b1-py3-none-any.whl/genie/libs/clean/recovery/nxos/recovery.py
@@ -0,0 +1,25 @@
+'''NXOS specific recovery functions'''
+
+# Python
+import time
+import logging
+
+# Logger
+log = logging.getLogger(__name__)
+
+# Power Cycler handlers
+def sendbrk_handler(spawn, break_count):
+    ''' Send break while rebooting device
+        Args:
+            spawn ('obj'): Spawn connection object
+            break_count ('int'): Number of sending break times
+        Returns:
+            None
+    '''
+
+    count = 1
+    while count < break_count:
+        log.debug('Sending break')
+        spawn.sendline("\003")
+        time.sleep(.002)
+        count += 1
