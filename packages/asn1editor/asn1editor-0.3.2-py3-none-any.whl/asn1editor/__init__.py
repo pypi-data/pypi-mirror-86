c2c06cbbcba34ce57a3e86d8e3c60b08d10cb05b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:55 +0000

    Add package asn1editor/0.3.2/asn1editor-0.3.2-py3-none-any.whl

---
+++ packages/asn1editor/asn1editor-0.3.2-py3-none-any.whl/asn1editor/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.3.2"
+# noinspection SpellCheckingInspection
+__author__ = "Florian Fetz"
+
+from . import wxPython
+from .Plugin import Plugin
+from .PluginInterface import PluginInterface
