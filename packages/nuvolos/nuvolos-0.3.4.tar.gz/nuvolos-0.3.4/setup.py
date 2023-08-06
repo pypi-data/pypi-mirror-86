9b8acd6e0c1e1366874813ee20cad9a046642986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:03 +0000

    Add package nuvolos/0.3.4/nuvolos-0.3.4.tar.gz

---
+++ packages/nuvolos/nuvolos-0.3.4.tar.gz/nuvolos-0.3.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+def readme():
+    with open("README.rst") as f:
+        return f.read()
+
+
+setup(
+    name="nuvolos",
+    version="0.3.4",
+    description="The Nuvolos python library for database connectivity",
+    long_description=readme(),
+    url="https://github.com/nuvolos-cloud/python-connector",
+    author="Alphacruncher",
+    author_email="support@nuvolos.cloud",
+    license="MIT",
+    packages=["nuvolos"],
+    install_requires=["pyodbc"],
+    zip_safe=False,
+)
