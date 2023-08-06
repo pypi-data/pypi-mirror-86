2406abd38856d31968be35df867ef3002026d245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:00 +0000

    Add package martinez/0.6.0/martinez-0.6.0-cp38-cp38-manylinux2010_x86_64.whl

---
+++ packages/martinez/martinez-0.6.0-cp38-cp38-manylinux2010_x86_64.whl/martinez/point.py
@@ -0,0 +1,36 @@
+import math
+
+from reprit.base import generate_repr
+
+from .bounding_box import BoundingBox
+from .hints import Scalar
+
+
+class Point:
+    __slots__ = '_x', '_y'
+
+    def __init__(self, x: Scalar, y: Scalar) -> None:
+        self._x = x
+        self._y = y
+
+    __repr__ = generate_repr(__init__)
+
+    def __eq__(self, other: 'Point') -> bool:
+        return (self._x == other._x and self._y == other._y
+                if isinstance(other, Point)
+                else NotImplemented)
+
+    @property
+    def bounding_box(self) -> BoundingBox:
+        return BoundingBox(self._x, self._y, self._x, self._y)
+
+    @property
+    def x(self) -> Scalar:
+        return self._x
+
+    @property
+    def y(self) -> Scalar:
+        return self._y
+
+    def distance_to(self, other: 'Point') -> Scalar:
+        return math.sqrt((self._x - other._x) ** 2 + (self._y - other._y) ** 2)
