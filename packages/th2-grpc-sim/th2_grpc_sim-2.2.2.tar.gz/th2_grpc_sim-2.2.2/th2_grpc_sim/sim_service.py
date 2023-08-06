feb2a8a703cbc1234d7eab0047d353e8481dfa86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:20 +0000

    Add package th2-grpc-sim/2.2.2/th2_grpc_sim-2.2.2.tar.gz

---
+++ packages/th2-grpc-sim/th2_grpc_sim-2.2.2.tar.gz/th2_grpc_sim-2.2.2/th2_grpc_sim/sim_service.py
@@ -0,0 +1,12 @@
+from . import sim_pb2_grpc as importStub
+
+class SimService(object):
+
+    def __init__(self, router):
+        self.connector = router.get_connection(SimService, importStub.SimStub)
+
+    def removeRule(self, request, timeout=None):
+        return self.connector.create_request('removeRule', request, timeout)
+
+    def getRulesInfo(self, request, timeout=None):
+        return self.connector.create_request('getRulesInfo', request, timeout)
