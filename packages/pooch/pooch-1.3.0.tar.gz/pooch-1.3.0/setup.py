4c7c16e782e500f8985dea829d0688a01b95521d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:09 +0000

    Add package pooch/1.3.0/pooch-1.3.0.tar.gz

---
+++ packages/pooch/pooch-1.3.0.tar.gz/pooch-1.3.0/setup.py
@@ -0,0 +1,87 @@
+# Copyright (c) 2018 The Pooch Developers.
+# Distributed under the terms of the BSD 3-Clause License.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# This code is part of the Fatiando a Terra project (https://www.fatiando.org)
+#
+"""
+Build and install the project.
+
+Uses versioneer to manage version numbers using git tags.
+"""
+import os
+from setuptools import setup, find_packages
+
+import versioneer
+
+
+NAME = "pooch"
+FULLNAME = "Pooch"
+AUTHOR = "Leonardo Uieda"
+AUTHOR_EMAIL = "leouieda@gmail.com"
+MAINTAINER = AUTHOR
+MAINTAINER_EMAIL = AUTHOR_EMAIL
+LICENSE = "BSD License"
+URL = "https://github.com/fatiando/pooch"
+DESCRIPTION = (
+    "Pooch manages your Python library's sample data files: "
+    "it automatically downloads and stores them in a local directory, "
+    "with support for versioning and corruption checks."
+)
+KEYWORDS = ""
+with open("README.rst") as f:
+    LONG_DESCRIPTION = "".join(f.readlines())
+
+VERSION = versioneer.get_version()
+CMDCLASS = versioneer.get_cmdclass()
+CLASSIFIERS = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Science/Research",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Education",
+    "Topic :: Scientific/Engineering",
+    "Topic :: Software Development :: Libraries",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3 :: Only",
+    f"License :: OSI Approved :: {LICENSE}",
+]
+PLATFORMS = "Any"
+PACKAGES = find_packages(exclude=["doc"])
+SCRIPTS = []
+PACKAGE_DATA = {
+    "pooch.tests": [
+        os.path.join("data", "*"),
+        os.path.join("data", "store", "*"),
+        os.path.join("data", "store", "subdir", "*"),
+    ]
+}
+INSTALL_REQUIRES = ["requests", "packaging", "appdirs"]
+PYTHON_REQUIRES = ">=3.6"
+
+if __name__ == "__main__":
+    setup(
+        name=NAME,
+        fullname=FULLNAME,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        version=VERSION,
+        author=AUTHOR,
+        author_email=AUTHOR_EMAIL,
+        maintainer=MAINTAINER,
+        maintainer_email=MAINTAINER_EMAIL,
+        license=LICENSE,
+        url=URL,
+        platforms=PLATFORMS,
+        scripts=SCRIPTS,
+        packages=PACKAGES,
+        package_data=PACKAGE_DATA,
+        classifiers=CLASSIFIERS,
+        keywords=KEYWORDS,
+        install_requires=INSTALL_REQUIRES,
+        python_requires=PYTHON_REQUIRES,
+        cmdclass=CMDCLASS,
+    )
