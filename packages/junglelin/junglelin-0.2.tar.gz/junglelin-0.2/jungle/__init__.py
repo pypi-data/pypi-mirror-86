9da3947f929bba70fb2df6f9339bc5aabe772b48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:45 +0000

    Add package junglelin/0.2/junglelin-0.2.tar.gz

---
+++ packages/junglelin/junglelin-0.2.tar.gz/junglelin-0.2/jungle/__init__.py
@@ -0,0 +1,3 @@
+def joke():
+    return (u'干哈呢 '
+            u'瞅你咋的')
---
+++ packages/junglelin/junglelin-0.2.tar.gz/junglelin-0.2/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+
+setuptools.setup(name='junglelin',
+      version='0.2',
+      description='The funniest joke in the world by kunlin',
+      # url='http://github.com/storborg/funniest',
+      author='kunlin',
+      author_email='tanlandy6@gmail.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      zip_safe=False)
