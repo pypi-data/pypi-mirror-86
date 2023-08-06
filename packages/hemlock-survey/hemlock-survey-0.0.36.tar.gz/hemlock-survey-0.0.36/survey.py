dfb88a9d86676658453eaedf42fa3b6f7df620c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:28 +0000

    Add package hemlock-survey/0.0.36/hemlock-survey-0.0.36.tar.gz

---
+++ packages/hemlock-survey/hemlock-survey-0.0.36.tar.gz/hemlock-survey-0.0.36/survey.py
@@ -0,0 +1,28 @@
+from hemlock import (
+    Branch, Page, Embedded, Blank, Binary, Check, Input, Label, Range, Textarea, 
+    Compile as C, Validate as V, route, settings
+)
+from hemlock.tools import consent_page, completion_page
+
+from flask_login import current_user
+
+@route('/survey')
+def start():
+    return Branch(
+        Page(
+            Label(
+                'Page 0'
+            )
+        ),
+        Page(
+            Label(
+                'Page 1'
+            ),
+            back=True, terminal=True,
+            compile=log,
+            compile_worker=True
+        )
+    )
+
+def log(page):
+    print('here')
