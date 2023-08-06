17324cb2a08909b61270904056737fb7051eb586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:00 +0000

    Add package octo-api-client/0.1.4/OCTo_API_client-0.1.4-py2.py3-none-any.whl

---
+++ packages/octo-api-client/OCTo_API_client-0.1.4-py2.py3-none-any.whl/octo_client/client.py
@@ -0,0 +1,268 @@
+from datetime import date
+import logging
+from typing import Callable, Dict, List
+
+import requests
+
+from octo_client import exceptions, models
+
+logger = logging.getLogger('octo_client')
+logger.setLevel(logging.INFO)
+
+
+class OctoClient(object):
+    """
+    HTTP client for OCTo (Open Connection for Tourism) APIs.
+    """
+
+    def __init__(self, url: str, token: str, custom_logger: logging.Logger = None) -> None:
+        self.url = url.rstrip('/')
+        self.token = token
+        self.logger = custom_logger or logger
+        self.suppliers: List[models.Supplier] = []
+        self.supplier_url_map: Dict[str, str] = {}
+        self.requests_loglevel = logging.DEBUG
+
+    @staticmethod
+    def _raise_for_status(status_code: int, response_content: str) -> None:
+        CODE_EXCEPTION_MAP = {
+            400: exceptions.InvalidRequest,
+            403: exceptions.Unauthorized,
+            404: exceptions.ApiError,
+            500: exceptions.ApiError,
+        }
+        if status_code in CODE_EXCEPTION_MAP:
+            raise CODE_EXCEPTION_MAP[status_code](response_content)
+
+    def _make_request(self, http_method: Callable, path: str, json=None, params=None):
+        if path.startswith('suppliers/'):
+            supplier_id = path.split('/')[1]
+            if not self.suppliers:
+                self.get_suppliers()
+            try:
+                endpoint_url = self.supplier_url_map[supplier_id]
+            except KeyError:
+                raise exceptions.InvalidRequest('Incorrect supplierId')
+            full_url = f'{endpoint_url}/{path}'
+        else:
+            full_url = f'{self.url}/{path}'
+        self.logger.log(self.requests_loglevel, 'Sending request to %s (%s)', full_url, http_method.__name__.upper())
+        response = http_method(
+            full_url,
+            params=params or {},
+            json=json,
+            headers=self._get_headers(),
+        )
+        self._raise_for_status(response.status_code, response.content)
+        try:
+            response_json = response.json()
+        except Exception:
+            raise exceptions.ApiError('Non-JSON response')
+        self.logger.log(self.requests_loglevel, 'Get response from %s (%s)', full_url, http_method.__name__.upper())
+        return response_json
+
+    def _get_headers(self) -> Dict[str, str]:
+        return {'Authorization': f'Bearer {self.token}'}
+
+    def _http_get(self, path: str, params=None):
+        return self._make_request(requests.get, path, params=params)
+
+    def _http_post(self, path: str, json: dict, params=None):
+        return self._make_request(requests.post, path, json=json, params=params)
+
+    def _http_delete(self, path: str, params=None):
+        return self._make_request(requests.delete, path, params=params)
+
+    def get_suppliers(self) -> List[models.Supplier]:
+        '''
+        This list MAY be limited based on the suppliers that the authenticated user has been granted access to.
+        '''
+        response = self._http_get('suppliers')
+        self.suppliers = [models.Supplier.from_dict(supplier) for supplier in response]
+        self.logger.info('Found %s suppliers', len(self.suppliers), extra={'suppliers': response})
+        self.supplier_url_map = {
+            supplier.id: supplier.endpoint for supplier in self.suppliers
+        }
+        return self.suppliers
+
+    def get_products(self, supplier_id: str) -> List[models.Product]:
+        '''
+        Contains all product details necessary to ingest, map, and sell.
+        '''
+        response = self._http_get(f'suppliers/{supplier_id}/products')
+        products = [models.Product.from_dict(product) for product in response]
+        self.logger.info('Found %s products', len(products), extra={'products': products})
+        return products
+
+    def get_calendar(
+        self,
+        supplier_id: str,
+        product_id: str,
+        option_id: str,
+        start_date: date,
+        end_date: date,
+        extra_fields: dict = None,
+    ) -> List[models.AvailabilityCalendarItem]:
+        '''
+        Returns an availability for given product and option.
+        '''
+        params = {
+            'productId': product_id,
+            'optionId': option_id,
+            'localDateStart': start_date.isoformat(),
+            'localDateEnd': end_date.isoformat(),
+        }
+        if extra_fields:
+            params.update(extra_fields)
+        response = self._http_get(f'suppliers/{supplier_id}/availability/calendar', params=params)
+        daily_availability = [models.AvailabilityCalendarItem.from_dict(availability) for availability in response]
+        self.logger.info('Found %s days', len(daily_availability), extra={'calendar': response})
+        return daily_availability
+
+    def get_availability(
+        self,
+        supplier_id: str,
+        product_id: str,
+        option_id: str,
+        start_date: date,
+        end_date: date,
+        extra_fields: dict = None,
+    ) -> List[models.AvailabilityItem]:
+        '''
+        For any dates which are never available for booking, the response MUST exclude those dates entirely.
+
+        If the product's availabilityType is OPENING_HOURS then the localDateTimeStart and localDateTimeEnd
+        are the hours of operation. If a product has more than one hours of operation on the same day
+        (e.g. the supplier is open 8-5 but closed for lunch from 12-1) then one availability object
+        MUST be returned for each contiguous range of time for that day.
+
+        The availability id value MUST be sent when making a Booking request.
+
+        The status field SHOULD be used to infer how frequently your cache should be updated from
+        the Booking Platform. The RECOMMENDED frequency is as follows:
+
+        FREESALE: Always available. Refresh no more than once/week.
+        AVAILABLE: Currently available for sale, but has a fixed capacity. Refresh every 12 hours.
+        LIMITED: Currently available for sale, but has a fixed capacity and may be sold out soon.
+                    Refresh at least once/hour.
+        SOLD_OUT: Currently sold out, but additional availability may free up. Refresh no more
+                    than once/hour.
+        CLOSED: Currently not available for sale, but not sold out (e.g. temporarily on stop-sell)
+                and may be available for sale soon. Refresh no more than once/12 hours.
+
+        '''
+        params = {
+            'productId': product_id,
+            'optionId': option_id,
+            'localDateStart': start_date.isoformat(),
+            'localDateEnd': end_date.isoformat(),
+        }
+        if extra_fields:
+            params.update(extra_fields)
+        response = self._http_get(f'suppliers/{supplier_id}/availability', params=params)
+        detailed_availability = [models.AvailabilityItem.from_dict(availability) for availability in response]
+        self.logger.info('Found %s items', len(detailed_availability), extra={'availability': response})
+        return detailed_availability
+
+    def test_availability(
+        self,
+        supplier_id: str,
+        product_id: str,
+        option_id: str,
+        availability_ids: List[str],
+        units: List[models.UnitQuantity],
+        extra_fields: dict = None,
+    ) -> List[models.AvailabilityItem]:
+        '''
+        This request is intended to provide the Booking Platform a complete view of the Unit IDs, Unit quantity,
+        and Availability IDs so that additional restrictions and policies can be validated within
+        the Booking Platform prior to making a Booking. The purpose is to provide a clear and accurate
+        answer to the Reseller about whether the requested booking configuration could be accepted by the Supplier.
+        This is to support complex booking requirements without the Reseller needing to know the details
+        of the restriction (e.g. "must purchase at least 1 adult ticket if a child ticket is purchased").
+        '''
+        json_data = {
+            'productId': product_id,
+            'optionId': option_id,
+            'availabilityIds': availability_ids,
+            'units': [unit.as_dict() for unit in units],
+        }
+        if extra_fields:
+            json_data.update(extra_fields)
+        response = self._http_post(f'suppliers/{supplier_id}/availability', json=json_data)
+        detailed_availability = [models.AvailabilityItem.from_dict(availability) for availability in response]
+        self.logger.info('Found %s items', len(detailed_availability), extra={'availability': response})
+        return detailed_availability
+
+    def create_booking(
+        self,
+        supplier_id: str,
+        booking_request: models.BookingRequest,
+    ) -> models.Booking:
+        '''
+        This creates a new pending booking.
+
+        This call has to be idempotent. To be able to safely retry a call on any network error or timeout,
+        therefore it MUST not fail on retry or create a duplicate booking. The idempotency key is the UUID.
+        A supplier SHOULD verify that a retried request with the same UUID is matching the original booking data,
+        to avoid erroneous clients generating repeating UUIDs and response with the status 400
+        and ErrorCode 1005 in such case.
+        '''
+        response = self._http_post(f'suppliers/{supplier_id}/bookings', json=booking_request.as_dict())
+        self.logger.info('Booking created', extra={'booking': response})
+        return models.Booking.from_dict(response)
+
+    def confirm_booking(
+        self,
+        supplier_id: str,
+        uuid: str,
+        confirmation_request: models.BookingConfirmationRequest,
+    ) -> models.Booking:
+        '''
+        This confirms an in-progress booking. The utcHoldExpiration MUST NOT be elapsed when this request is sent,
+        otherwise the response MAY show a status of EXPIRED.
+
+        This call MUST be idempotent so that a Reseller may retry the request for any network error or timeout.
+        The Booking uuid MUST be used to ensure idempotency of the request.
+        '''
+        response = self._http_post(
+            f'suppliers/{supplier_id}/bookings/{uuid}/confirm',
+            json=confirmation_request.as_dict(),
+        )
+        self.logger.info('Booking confirmed', extra={'booking': response})
+        return models.Booking.from_dict(response)
+
+    def get_booking_details(self, supplier_id: str, uuid: str) -> models.Booking:
+        '''
+        This returns the current state of any valid booking. This request MAY be made at any point after
+        the initial createBooking request is processed successfully and it MUST return the booking object.
+        '''
+        response = self._http_get(f'suppliers/{supplier_id}/bookings/{uuid}')
+        self.logger.info('Got booking details', extra={'booking': response})
+        return models.Booking.from_dict(response)
+
+    def delete_booking(
+        self,
+        supplier_id: str,
+        uuid: str,
+        reason: models.CancelReason,
+        reason_details: str,
+        extra_fields: dict = None,
+    ) -> models.Booking:
+        '''
+        This expires the availability hold of an in-progress booking so that the availablity is release
+        for other bookings. This request is a courtesy, however Resellers SHOULD send this in order
+        to ensure proper cleanup of any outstanding holds.
+
+        This call has to be idempotent. To be able to safely retry a call on any network error or timeout,
+        therefore it MUST not fail on retry. The idempotency key is the UUID.
+        '''
+        json_data = {
+            'reason': reason.value,
+            'reasonDetails': reason_details,
+        }
+        if extra_fields:
+            json_data.update(extra_fields)
+        response = self._http_post(f'suppliers/{supplier_id}/bookings/{uuid}/cancel', json=json_data)
+        self.logger.info('Booking removed', extra={'booking': response})
+        return models.Booking.from_dict(response)
---
+++ packages/octo-api-client/OCTo_API_client-0.1.4-py2.py3-none-any.whl/octo_client/models.py
@@ -0,0 +1,273 @@
+from dataclasses import asdict, dataclass, field, fields
+from datetime import date, datetime
+from enum import Enum
+from typing import List, Optional
+from unittest.mock import patch
+
+import attr
+from dacite import Config, from_dict
+
+from .dacite_patch import _build_value
+
+
+@dataclass
+@attr.s(auto_attribs=True)
+class BaseModel:
+
+    # extra_fields: Optional[dict] = field(default_factory=dict)
+    # ^ this field cannot be inherited by other models due to a python bug
+    # with ordering fields that have any default value and the ones that do not
+    # "error: Attributes without a default cannot follow attributes with one"
+    # https://bugs.python.org/issue36077
+    # Workaround: adding this field at the end of every model
+
+    @staticmethod
+    def _datetime_from_iso_format(datetime_str: Optional[str]) -> Optional[datetime]:
+        if datetime_str:
+            return datetime.fromisoformat(datetime_str.replace('Z', '+00:00'))
+        return None
+
+    def as_dict(self) -> dict:
+        """
+        Dumps dataclass into dictionary.
+        """
+        model_as_dict = asdict(self)
+        model_as_dict.update(model_as_dict.pop('extra_fields'))
+        return model_as_dict
+
+    @classmethod
+    def from_dict(cls, data: dict, config=None):
+        """
+        Populating data class from a dictionary with strict type checking.
+        """
+        if config is None:
+            config = Config(strict=True, type_hooks={
+                date: date.fromisoformat,
+                datetime: cls._datetime_from_iso_format,
+            })
+
+        extra_fields = set(data.keys()).difference([f.name for f in fields(cls)])
+        data['extra_fields'] = {}
+        for extra_field in extra_fields:
+            data['extra_fields'][extra_field] = data.pop(extra_field)
+        with patch('dacite.core._build_value', side_effect=_build_value):
+            return from_dict(data_class=cls, data=data, config=config)
+
+
+@dataclass
+class SupplierContact(BaseModel):
+    email: str
+    address: Optional[str] = None
+    telephone: Optional[str] = None
+    description: Optional[str] = None
+    website: Optional[str] = None
+    extra_fields: Optional[dict] = field(default_factory=dict)
+
+
+@dataclass
+class Supplier(BaseModel):
+    id: str
+    name: str
+    endpoint: str
+    contact: SupplierContact
+    extra_fields: Optional[dict] = field(default_factory=dict)
+
+
+@dataclass
+class Capability(BaseModel):
+    id: str
+    revision: int
+    required: bool
+    extra_fields: Optional[dict] = field(default_factory=dict)
+
+
+@dataclass
+class Unit(BaseModel):
+    id: str
+    internalName: str
+    type: str
+    reference: Optional[str] = None
+    extra_fields: Optional[dict] = field(default_factory=dict)
+
+
+@dataclass
+class Option(BaseModel):
+    id: str
+    internalName: str
+    units: List[Unit]
+    reference: Optional[str] = None
+    extra_fields: Optional[dict] = field(default_factory=dict)
+
+
+@dataclass
+class Product(BaseModel):
+    id: str
+    internalName: str
+    locale: str
+    timeZone: str
+    instantConfirmation: bool
+    instantDelivery: bool
+    availabilityType: str
+    deliveryFormats: List[str]
+    deliveryMethods: List[str]
+    redemptionMethod: str
+    capabilities: List[Capability]
+    options: List[Option]
+    reference: Optional[str] = None
+    extra_fields: Optional[dict] = field(default_factory=dict)
+
+
+@dataclass
+class AvailabilityCalendarItem(BaseModel):
+    localDate: date
+    status: str
+    vacancies: Optional[int] = None
+    extra_fields: Optional[dict] = field(default_factory=dict)
+
+
+@dataclass
+class AvailabilityItem(BaseModel):
+    id: str
+    localDateTimeStart: datetime
+    localDateTimeEnd: datetime
+    status: str
+    vacancies: Optional[int] = None
+    extra_fields: Optional[dict] = field(default_factory=dict)
+
+
+@dataclass
+class UnitItem(BaseModel):
+    uuid: str
+    unitId: str
+    resellerReference: Optional[str] = None
+    extra_fields: Optional[dict] = field(default_factory=dict)
+
+
+@dataclass
+class UnitQuantity(BaseModel):
+    unitId: str
+    quantity: int
+    extra_fields: Optional[dict] = field(default_factory=dict)
+
+
+@dataclass
+class BookingRequest(BaseModel):
+    uuid: str
+    productId: str
+    optionId: str
+    availabilityId: str
+    unitItems: List[UnitItem]
+    holdExpirationMinutes: Optional[int] = None
+    resellerReference: Optional[str] = None
+    extra_fields: Optional[dict] = field(default_factory=dict)
+
+
+@dataclass
+class Availability(BaseModel):
+    id: str
+    localDateTimeStart: datetime
+    localDateTimeEnd: datetime
+    extra_fields: Optional[dict] = field(default_factory=dict)
+
+
+@dataclass
+class Contact(BaseModel):
+    locales: List[str]
+    fullName: Optional[str] = None
+    emailAddress: Optional[str] = None
+    phoneNumber: Optional[str] = None
+    country: Optional[str] = None
+    extra_fields: Optional[dict] = field(default_factory=dict)
+
+
+@dataclass
+class DeliveryOption(BaseModel):
+    deliveryFormat: str
+    deliveryValue: str
+    extra_fields: Optional[dict] = field(default_factory=dict)
+
+
+@dataclass
+class Ticket(BaseModel):
+    deliveryOptions: List[DeliveryOption]
+    redemptionMethod: str
+    utcDeliveredAt: Optional[datetime] = None
+    utcRedeemedAt: Optional[datetime] = None
+    extra_fields: Optional[dict] = field(default_factory=dict)
+
+
+@dataclass
+class BookingTicket(BaseModel):
+    deliveryOptions: List[DeliveryOption]
+    redemptionMethod: str
+    utcDeliveredAt: Optional[datetime] = None
+    utcRedeemedAt: Optional[datetime] = None
+    extra_fields: Optional[dict] = field(default_factory=dict)
+
+
+@dataclass
+class UnitItemTicket(BaseModel):
+    uuid: str
+    unitId: str
+    ticket: Optional[BookingTicket] = None
+    resellerReference: Optional[str] = None
+    supplierReference: Optional[str] = None
+    extra_fields: Optional[dict] = field(default_factory=dict)
+
+
+@dataclass
+class CancellationRequest(BaseModel):
+    reason: str
+    reasonDetails: str
+    status: str
+    refund: str
+    utcRequestedAt: datetime
+    utcHoldExpiration: Optional[datetime] = None
+    utcConfirmedAt: Optional[datetime] = None
+    utcResolvedAt: Optional[datetime] = None
+    extra_fields: Optional[dict] = field(default_factory=dict)
+
+
+@dataclass
+class Booking(BaseModel):
+    uuid: str
+    status: str
+    productId: str
+    optionId: str
+    availability: Availability
+    deliveryMethods: List[str]
+    unitItems: List[UnitItemTicket]
+    contact: Optional[Contact] = None
+    utcHoldExpiration: Optional[datetime] = None
+    utcConfirmedAt: Optional[datetime] = None
+    utcDeliveredAt: Optional[datetime] = None
+    resellerReference: Optional[str] = None
+    supplierReference: Optional[str] = None
+    refreshFrequency: Optional[str] = None
+    voucher: Optional[Ticket] = None
+    cancellationRequest: Optional[CancellationRequest] = None
+    extra_fields: Optional[dict] = field(default_factory=dict)
+
+
+@dataclass
+class BookingConfirmationRequest(BaseModel):
+    contact: Contact
+    resellerReference: Optional[str] = None
+    extra_fields: Optional[dict] = field(default_factory=dict)
+
+
+class CancelReason(Enum):
+    # is the most common and indicates that the customer requested the cancellation
+    CUSTOMER = 'CUSTOMER'
+
+    # indicates that the supplier requested the cancellation
+    # (possibly due to bad weather or other unexpected circumstances)
+    SUPPLIER = 'SUPPLIER'
+
+    # indicates that the booking cancellation is being requested by the Reseller
+    # because it has been determined the booking was fraudulent
+    FRAUD = 'FRAUD'
+
+    # indicates that the cancellation reason does not fall into one of these categories.
+    # This SHOULD be used only in rare circumstances
+    OTHER = 'OTHER'
