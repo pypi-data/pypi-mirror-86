ae4acfaa6ae8ebfc174d1d3bcf48c7f68539c10d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:44 +0000

    Add package boto3-stubs/1.16.24.0/boto3-stubs-1.16.24.0.tar.gz

---
+++ packages/boto3-stubs/boto3-stubs-1.16.24.0.tar.gz/boto3-stubs-1.16.24.0/boto3-stubs/__main__.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    print(
+        "Type annotations for boto3 1.16.24\n"
+        "Version:         1.16.24.0\n"
+        "Builder version: 3.3.0\n"
+        "Docs:            https://pypi.org/project/boto3-stubs/\n"
+        "Changelog:       https://github.com/vemel/mypy_boto3_builder/releases"
+    )
