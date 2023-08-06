b7806c0f3fbb462ba61038f6afb07ea6aa98e874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:50 +0000

    Add package janet/0.1.6/janet-0.1.6.tar.gz

---
+++ packages/janet/janet-0.1.6.tar.gz/janet-0.1.6/janet/api/cli_utils.py
@@ -0,0 +1,89 @@
+from .run import run_code
+from .check_for_imports import install
+
+from colorama import Fore
+import argparse
+import os
+import time
+import logging
+import signal
+
+from pyfiglet import Figlet
+
+
+
+def print_header():
+	f = Figlet(font='slant')
+	print(f.renderText('JANET'))
+	print_cmd()
+
+def print_cmd():
+    print(Fore.WHITE + 'Cool stuff you could do:')
+    print(Fore.GREEN+ '	1. run (to run the selected entry point, running again will kill the current python process)')
+    print(Fore.GREEN+ '	2. install <PACKAGE_1> <PACKAGE_2> ... (install packages manually)')
+    print(Fore.GREEN+ '	3. change-entrypoint (change the script that janet will run)')
+    print(Fore.GREEN+ '	4. kill (to manually end a run)')
+    print(Fore.GREEN+ '	5. exit (to stop janet)')
+    print(Fore.GREEN+ '	6. menu')
+    print(Fore.GREEN+ '	\nUse TAB to autocomplete commands')
+    print(Fore.WHITE)
+
+
+
+
+def command(command, process, entry_point, project_path):
+	command_no_spaces = command.replace(" ", "")
+	command_splited = command.split(" ")
+
+	if command_no_spaces.lower() == 'exit':
+		if process is not None:
+			process.send_signal(signal.SIGINT)
+		exit()
+
+	elif command_splited[0].lower() == "install":
+		modules = []
+		for arg in command_splited[1:]:
+			if (arg == " ") or (arg == ""):
+				continue
+			else:
+				modules.append(arg)
+		install(modules)
+		return process, entry_point
+
+
+	elif command_no_spaces.lower() == 'run':
+		if process is not None:
+			print("you have to kill the current process before starting a new one")
+			process.send_signal(signal.SIGINT)
+
+		process = run_code(project_path, entry_point, debug=False)
+		return process, entry_point
+
+	elif command_no_spaces.lower() == 'change-entrypoint':
+		new_entry_point = ''
+		while len(new_entry_point) == 0:
+			new_entry_point = input(Fore.WHITE+'New entry point ? ')
+			new_entry_point = new_entry_point.replace(" ","")
+		entry_point = new_entry_point
+		return process, entry_point
+
+
+	elif command_no_spaces.lower() == 'kill':
+		if process is not None:
+			process.send_signal(signal.SIGINT)
+		else:
+			print("Nothing to kill")
+
+		return None, entry_point
+
+	elif command_no_spaces.lower() == 'menu':
+		print_cmd()
+		return process, entry_point
+
+	else:
+		return process, entry_point
+
+
+
+
+		
---
+++ packages/janet/janet-0.1.6.tar.gz/janet-0.1.6/janet/api/janet_record.py
@@ -0,0 +1,104 @@
+import json
+import os
+from datetime import datetime
+import subprocess
+import sys
+
+
+class JanetRecord:
+	def __init__(self, project_dir):
+		self.project_dir = project_dir
+
+		janetrecord_exists = self.check_for_existing_records()
+
+		if janetrecord_exists:
+			self.janetrecord = self.load_janetrecord()
+		else:
+			self.janetrecord = self.create_janetrecord()
+		
+		self.add_to_gitignore()
+		self.save_janetrecord()
+
+		self.janet_requirements = self.query_janet_requirements()
+
+	def query_janet_requirements(self):
+		janet_requirements = [sys.executable, "-m", "pip", "show", "janet"]
+		janet_freeze = subprocess.run(janet_requirements, universal_newlines=True, stdout=subprocess.PIPE)
+	
+		janet_freeze_stdout_str = janet_freeze.stdout
+		Requires_index = janet_freeze_stdout_str.find("Requires: ")
+		janet_requirements = janet_freeze_stdout_str[Requires_index:]
+		janet_requirements = janet_requirements[:janet_requirements.find("\n")]
+		janet_requirements = janet_requirements.replace("Requires: ","")
+		janet_requirements = janet_requirements.replace(" ","")
+		janet_requirements = janet_requirements.split(",")
+		janet_requirements.append("janet")
+
+		return janet_requirements
+
+
+	def check_for_existing_records(self):
+		project_files = os.listdir(self.project_dir)
+
+		return ".janetrecord.json" in project_files
+
+	def create_janetrecord(self):
+		curr_time = datetime.now()
+		curr_time = curr_time.strftime("%m/%d/%Y, %H:%M:%S")
+		janetrecord = 	{
+							"_project_dir": self.project_dir,
+							"_creation_time": curr_time,
+							"records": {}
+						}
+		return janetrecord
+
+
+	def save_janetrecord(self):
+		janetrecord_filepath = os.path.join(self.project_dir, ".janetrecord.json")
+		with open(janetrecord_filepath, 'w') as f:
+			json.dump(self.janetrecord, f, indent=4, sort_keys=True)
+
+
+	def load_janetrecord(self):
+		janetrecord_filepath = os.path.join(self.project_dir, ".janetrecord.json")
+		with open(janetrecord_filepath) as f:
+			janetrecord = json.load(f)
+		return janetrecord
+
+	def add_to_gitignore(self):
+		project_files = os.listdir(self.project_dir)
+		if ".gitignore" in project_files:
+			gitignore_path = os.path.join(self.project_dir, ".gitignore")
+			if "janetrecord.json" not in open(gitignore_path).read():
+				with open(gitignore_path, "a") as gitignore:
+					gitignore.write("\n#janet records\n.janetrecord.json")
+		else:
+			print("Did not find a gitignore")
+			print("use git init and run janet again or add it to .gitignore manually!")
+
+
+	def get_last_modified_time(self, filepath):
+		unix_epoch = os.path.getmtime(filepath)
+		return unix_epoch
+
+	def store_last_modified(self, filepath):
+		last_modified = self.get_last_modified_time(filepath)
+		self.janetrecord["records"][filepath] = last_modified
+
+
+	def get_last_modified_from_records(self, filepath):
+		try:
+			last_modified = self.janetrecord["records"][filepath] 
+		except:
+			last_modified = None
+		
+		return last_modified
+
+	def it_exists(self, filepath):
+		try:
+			last_modified = self.janetrecord["records"][filepath]
+			exists = True 
+		except:
+			exists = False
+		
+		return exists
