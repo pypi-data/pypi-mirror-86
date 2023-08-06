f9d29695109703ad0be8259bb42f7acfd593e62c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:44 +0000

    Add package sshremote/0.0.9/sshremote-0.0.9.tar.gz

---
+++ packages/sshremote/sshremote-0.0.9.tar.gz/sshremote-0.0.9/sshremote/__main__.py
@@ -0,0 +1,44 @@
+import re
+from argparse import ArgumentParser
+from contextlib import contextmanager
+from getpass import getpass
+from subprocess import check_output, check_call
+
+from pydropbear import start_ssh_server
+from pyngrok import ngrok
+
+
+def get_user_name():
+    return check_output(["whoami"]).decode().strip()
+
+
+@contextmanager
+def ngrok_context(auth_toke, port):
+    ngrok.set_auth_token(args.ngrok_token)
+    tunnel = ngrok.connect(args.port, "tcp", options={"bind_tls": True})
+    yield tunnel
+    ngrok.disconnect(tunnel.public_url)
+
+
+if __name__ == "__main__":
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument("ngrok_token", type=str)
+    arg_parser.add_argument("--port", type=int, default=8443)
+    arg_parser.add_argument("--password", type=str)
+    args = arg_parser.parse_args()
+
+    user_name = get_user_name()
+
+    if user_name == "root":
+        if args.password is None:
+            password = getpass("It seems you are running under root user, please setup password: ")
+        else:
+            password = args.password
+
+        check_output('echo "root:{}" | chpasswd'.format(password), shell=True)
+
+    with ngrok_context(args.ngrok_token, args.port) as tunnel:
+        url, port = re.findall(r"//([a-z0-9.]+):(\d+)", tunnel.public_url)[0]
+        connection_string = "ssh {}@{} -p {}".format(user_name, url, port)
+        print('Type "{}" to connect'.format(connection_string))
+        start_ssh_server(port=args.port)
