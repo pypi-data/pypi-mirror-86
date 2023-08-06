82c98684bebcb398d7e19156d5b0b6bc2625b79e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:40 +0000

    Add package compling/0.0.20/compling-0.0.20-py3-none-any.whl

---
+++ packages/compling/compling-0.0.20-py3-none-any.whl/compling/analysis/lexical/unsupervised_learning/clustering/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ['kmeans', 'linkage']
+from compling.analysis.lexical.unsupervised_learning.clustering.kmeans import KMeans
+from compling.analysis.lexical.unsupervised_learning.clustering.linkage import Linkage
