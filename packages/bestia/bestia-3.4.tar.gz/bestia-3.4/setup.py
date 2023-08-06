1779c89c6b1334347b0b1bdbeab77956885189a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:24 +0000

    Add package bestia/3.4/bestia-3.4.tar.gz

---
+++ packages/bestia/bestia-3.4.tar.gz/bestia-3.4/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="bestia",
+    version="3.4",
+    author="Federico Rizzo",
+    author_email="synestem@ticATgmail.com",
+    description="A collection of tools for building dynamic Command-Line applications",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/synestematic/bestia",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: General",
+        "Topic :: Utilities",
+    ],
+    install_requires=[],
+)
