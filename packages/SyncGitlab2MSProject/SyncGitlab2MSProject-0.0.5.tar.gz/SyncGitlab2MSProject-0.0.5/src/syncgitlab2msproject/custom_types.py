4edcf32c54118642ca78ca4aeecc78325957d0b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:52 +0000

    Add package SyncGitlab2MSProject/0.0.5/SyncGitlab2MSProject-0.0.5.tar.gz

---
+++ packages/SyncGitlab2MSProject/SyncGitlab2MSProject-0.0.5.tar.gz/SyncGitlab2MSProject-0.0.5/src/syncgitlab2msproject/custom_types.py
@@ -0,0 +1,11 @@
+from typing import Union, Dict, NewType, Any
+
+from gitlab.v4.objects import ProjectIssue, GroupIssue
+
+GitlabUserDict = Dict[str, Union[str, int]]
+GitlabIssue = Union[GroupIssue, ProjectIssue]
+IssueRef = NewType("IssueRef", int)
+# Are Dynamic Types, therefore any
+ComMSProjectProject = Any
+ComMSProjectApplication = Any
+WebURL = NewType("WebURL", str)
