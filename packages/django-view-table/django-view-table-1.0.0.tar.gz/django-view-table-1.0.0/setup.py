4ef042238dbd557c74a76365480f7cdbfffc859a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:44 +0000

    Add package django-view-table/1.0.0/django-view-table-1.0.0.tar.gz

---
+++ packages/django-view-table/django-view-table-1.0.0.tar.gz/django-view-table-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='django-view-table',
+    version='1.0.0',
+    packages=['view_table', 'view_table.management.commands'],
+    url='https://github.com/pyohei/django-view-table',
+    license='MIT',
+    author='Shohei Mukai',
+    author_email='mukaishohei76@gmail.com',
+    description=''
+)
