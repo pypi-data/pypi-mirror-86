51f691607f2f891aba39a43d13f436497e9e1c8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:29 +0000

    Add package Python-ColorText/1.0/Python-ColorText-1.0.tar.gz

---
+++ packages/Python-ColorText/Python-ColorText-1.0.tar.gz/Python-ColorText-1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='Python-ColorText',
+    packages=['ColorText'],
+    version='1.0',
+    license='GPL v3',
+    description='Color Strings in Python easily',
+    author='Sebastian-byte',
+    author_email='sebastianmontoya209@gmail.com',
+    url='https://github.com/Sebastian-byte/ColorText',
+    download_url='https://github.com/Sebastian-byte/ColorText/archive/v1.0.tar.gz',
+    keywords='color python text',
+    classifiers=['Programming Language :: Python',
+                 'Programming Language :: Python :: 3.3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8',
+                 'Programming Language :: Python :: 3.9'],
+)
