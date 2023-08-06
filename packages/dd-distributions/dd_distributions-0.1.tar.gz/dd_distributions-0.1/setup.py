94f0a62e1cf1e6ab59ad8f9cbc3e994c791e60eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:19 +0000

    Add package dd-distributions/0.1/dd_distributions-0.1.tar.gz

---
+++ packages/dd-distributions/dd_distributions-0.1.tar.gz/dd_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dd_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dd_distributions'],
+      author='Omer Sarfraz',
+      zip_safe=False)
