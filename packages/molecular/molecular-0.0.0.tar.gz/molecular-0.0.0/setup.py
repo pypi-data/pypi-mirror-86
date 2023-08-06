7af241764ee294a60b597eff175a500125ea0335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:40 +0000

    Add package molecular/0.0.0/molecular-0.0.0.tar.gz

---
+++ packages/molecular/molecular-0.0.0.tar.gz/molecular-0.0.0/setup.py
@@ -0,0 +1,26 @@
+"""
+setup.py
+written in Python3
+author: C. Lockhart <chris@lockhartlab.org>
+"""
+
+from setuptools import setup
+
+
+# Setup
+setup(
+    name='molecular',
+    version='0.0.0',
+    author='C. Lockhart',
+    author_email='chris@lockhartlab.org',
+    description='A toolkit for molecular dynamics simulations',
+    long_description='A toolkit for molecular dynamics simulations',
+    long_description_content_type='text/markdown',
+    url="https://www.lockhartlab.org",
+    packages=[
+    ],
+    install_requires=[
+    ],
+    include_package_data=True,
+    zip_safe=True
+)
