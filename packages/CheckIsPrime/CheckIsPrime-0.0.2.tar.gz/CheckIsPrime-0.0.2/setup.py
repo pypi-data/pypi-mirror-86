a3497dd216d946cc9736d0f362be7375fa765b92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:08 +0000

    Add package CheckIsPrime/0.0.2/CheckIsPrime-0.0.2.tar.gz

---
+++ packages/CheckIsPrime/CheckIsPrime-0.0.2.tar.gz/CheckIsPrime-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="CheckIsPrime", # Replace with your own username
+    version="0.0.2",
+    author="C.P. Tsao",
+    author_email="cptsao@gmail.com",
+    description="example-package: Check_Is_it_Prime_No.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cptsao/PythonStudies.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
