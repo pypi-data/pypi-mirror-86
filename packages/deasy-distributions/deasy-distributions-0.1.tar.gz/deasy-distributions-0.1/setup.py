2915c3c764577328756b6dfdc44b839adf1aa5c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:48 +0000

    Add package deasy-distributions/0.1/deasy-distributions-0.1.tar.gz

---
+++ packages/deasy-distributions/deasy-distributions-0.1.tar.gz/deasy-distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='deasy-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['deasy-distributions'],
+      zip_safe=False)
