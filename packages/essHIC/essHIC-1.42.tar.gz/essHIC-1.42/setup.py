620079119dcc990e4aeaf5899669746d874f1d3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:36 +0000

    Add package essHIC/1.42/essHIC-1.42.tar.gz

---
+++ packages/essHIC/essHIC-1.42.tar.gz/essHIC-1.42/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+readfile = path.join(this_directory,'pipREADME.md')
+if path.exists(readfile):
+        with open(path.join(this_directory, 'pipREADME.md')) as f:
+                long_description = f.read()
+
+setup(name='essHIC',
+      version='1.42',
+      description='essHIC is a python module to compare HiC matrices by computing a metric distance between them',
+      url='https://github.com/stefanofranzini/essHIC',
+      author='Stefano Franzini',
+      author_email='sfranzin@sissa.it',
+      license='MIT',
+      packages=['essHIC'],
+  install_requires=[
+          'numpy',
+          'scipy',
+          'matplotlib',
+          'sklearn'
+      ],
+      zip_safe=True,
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      keywords=['HiC','distance','spectral'])
