394ab5d5dc6e8d24128a13e27a0c30b78867f1d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:45 +0000

    Add package tg/0.7.0/tg-0.7.0.tar.gz

---
+++ packages/tg/tg-0.7.0.tar.gz/tg-0.7.0/tg/__main__.py
@@ -0,0 +1,66 @@
+import logging.handlers
+import signal
+import threading
+from curses import window, wrapper  # type: ignore
+from functools import partial
+from types import FrameType
+
+from tg import config, update_handlers, utils
+from tg.controllers import Controller
+from tg.models import Model
+from tg.tdlib import Tdlib
+from tg.views import ChatView, MsgView, StatusView, View
+
+log = logging.getLogger(__name__)
+
+
+def run(tg: Tdlib, stdscr: window) -> None:
+
+    # handle ctrl+c, to avoid interrupting tg when subprocess is called
+    def interrupt_signal_handler(sig: int, frame: FrameType) -> None:
+        # TODO: draw on status pane: to quite press <q>
+        log.info("Interrupt signal is handled and ignored on purpose.")
+
+    signal.signal(signal.SIGINT, interrupt_signal_handler)
+
+    model = Model(tg)
+    status_view = StatusView(stdscr)
+    msg_view = MsgView(stdscr, model)
+    chat_view = ChatView(stdscr, model)
+    view = View(stdscr, chat_view, msg_view, status_view)
+    controller = Controller(model, view, tg)
+
+    # hanlde resize of terminal correctly
+    signal.signal(signal.SIGWINCH, controller.resize_handler)
+
+    for msg_type, handler in update_handlers.handlers.items():
+        tg.add_update_handler(msg_type, partial(handler, controller))
+
+    thread = threading.Thread(target=controller.run)
+    thread.daemon = True
+    thread.start()
+
+    controller.draw()
+
+
+def main() -> None:
+    utils.cleanup_cache()
+    tg = Tdlib(
+        api_id=config.API_ID,
+        api_hash=config.API_HASH,
+        phone=config.PHONE,
+        database_encryption_key=config.ENC_KEY,
+        files_directory=config.FILES_DIR,
+        tdlib_verbosity=config.TDLIB_VERBOSITY,
+        library_path=config.TDLIB_PATH,
+    )
+    tg.login()
+
+    utils.setup_log()
+    utils.set_shorter_esc_delay()
+
+    wrapper(partial(run, tg))
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/tg/tg-0.7.0.tar.gz/tg-0.7.0/tg/config.py
@@ -0,0 +1,97 @@
+"""
+Every parameter (except for CONFIG_FILE) can be
+overwritten by external config file
+"""
+import os
+import platform
+import runpy
+from typing import Dict, Optional
+
+_os_name = platform.system()
+_darwin = "Darwin"
+_linux = "Linux"
+
+
+CONFIG_DIR = os.path.expanduser("~/.config/tg/")
+CONFIG_FILE = os.path.join(CONFIG_DIR, "conf.py")
+FILES_DIR = os.path.expanduser("~/.cache/tg/")
+MAILCAP_FILE: Optional[str] = None
+
+LOG_LEVEL = "INFO"
+LOG_PATH = os.path.expanduser("~/.local/share/tg/")
+
+API_ID = "559815"
+API_HASH = "fd121358f59d764c57c55871aa0807ca"
+
+PHONE = None
+ENC_KEY = ""
+
+TDLIB_PATH = None
+TDLIB_VERBOSITY = 0
+
+MAX_DOWNLOAD_SIZE = "10MB"
+
+# TODO: check platform
+NOTIFY_CMD = "/usr/local/bin/terminal-notifier -title {title} -subtitle {subtitle} -message {msg} -appIcon {icon_path}"
+
+VIEW_TEXT_CMD = "less"
+FZF = "fzf"
+
+if _os_name == _linux:
+    # for more info see https://trac.ffmpeg.org/wiki/Capture/ALSA
+    VOICE_RECORD_CMD = (
+        "ffmpeg -f alsa -i hw:0 -c:a libopus -b:a 32k {file_path}"
+    )
+else:
+    VOICE_RECORD_CMD = (
+        "ffmpeg -f avfoundation -i ':0' -c:a libopus -b:a 32k {file_path}"
+    )
+
+# TODO: use mailcap instead of editor
+LONG_MSG_CMD = "vim + -c 'startinsert' {file_path}"
+EDITOR = os.environ.get("EDITOR", "vi")
+
+if _os_name == _linux:
+    DEFAULT_OPEN = "xdg-open {file_path}"
+else:
+    DEFAULT_OPEN = "open {file_path}"
+
+if _os_name == _linux:
+    COPY_CMD = "xclip -selection c"
+else:
+    COPY_CMD = "pbcopy"
+
+CHAT_FLAGS: Dict[str, str] = {}
+
+MSG_FLAGS: Dict[str, str] = {}
+
+ICON_PATH = os.path.join(os.path.dirname(__file__), "resources", "tg.png")
+
+URL_VIEW = "urlview"
+
+USERS_COLORS = tuple(range(2, 16))
+
+KEEP_MEDIA = 7
+
+FILE_PICKER_CMD = "ranger --choosefile={file_path}"
+
+DOWNLOAD_DIR = os.path.expanduser("~/Downloads/")
+
+if os.path.isfile(CONFIG_FILE):
+    config_params = runpy.run_path(CONFIG_FILE)
+    for param, value in config_params.items():
+        if param.isupper():
+            globals()[param] = value
+else:
+    os.makedirs(CONFIG_DIR, exist_ok=True)
+
+    if not PHONE:
+        print(
+            "Enter your phone number in international format (including country code)"
+        )
+        PHONE = input("phone> ")
+        if not PHONE.startswith("+"):
+            PHONE = "+" + PHONE
+
+    with open(CONFIG_FILE, "w") as f:
+        f.write(f"PHONE = '{PHONE}'\n")
