b35500cdd11a7f46a02c86e04413ba5194a85e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:42 +0000

    Add package makhzoom13pckg/1.0/makhzoom13pckg-1.0.tar.gz

---
+++ packages/makhzoom13pckg/makhzoom13pckg-1.0.tar.gz/makhzoom13pckg-1.0/pckg/hw.py
@@ -0,0 +1,6 @@
+class HW:
+	def printMessage(self):
+		print('Hello World from Salman PyPi package')
+
+hw = HW()
+hw.printMessage()
---
+++ packages/makhzoom13pckg/makhzoom13pckg-1.0.tar.gz/makhzoom13pckg-1.0/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name="makhzoom13pckg",
+	version="1.0",
+    packages=['pckg']
+)
