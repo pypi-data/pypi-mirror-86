b5826ee23099ae67a7e9e31eb00084aa9684fe4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:41 +0000

    Add package ors3handler/0.0.1/ors3handler-0.0.1.tar.gz

---
+++ packages/ors3handler/ors3handler-0.0.1.tar.gz/ors3handler-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ors3handler",
+    version="0.0.1",
+    author="Leonardo de Araújo",
+    author_email="dearaujo@uni-bremen.de",
+    description="AWS S3 Data Management.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/leonardomra/s3handler",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires = ["boto3==1.14.25", "botocore==1.17.47", "Werkzeug==1.0.1"],
+)
