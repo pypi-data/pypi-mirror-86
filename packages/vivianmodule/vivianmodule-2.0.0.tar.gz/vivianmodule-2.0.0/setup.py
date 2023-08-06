c7c092cd1c1149d8bea0d96d1729f27877326fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:49 +0000

    Add package vivianmodule/2.0.0/vivianmodule-2.0.0.tar.gz

---
+++ packages/vivianmodule/vivianmodule-2.0.0.tar.gz/vivianmodule-2.0.0/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(
+     name="vivianmodule",
+     version="2.0.0",
+     description="demo belongs to study",
+     author="vivian",
+     py_modules=["mypa.mypthon"])
