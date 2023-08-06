fc1e1a9f32a83ff56c9f7065c3551bb98ebef01f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:13 +0000

    Add package django-structlog/2.0.0/django-structlog-2.0.0.tar.gz

---
+++ packages/django-structlog/django-structlog-2.0.0.tar.gz/django-structlog-2.0.0/django_structlog/celery/steps.py
@@ -0,0 +1,42 @@
+from celery import bootsteps
+
+from . import receivers
+
+
+class DjangoStructLogInitStep(bootsteps.Step):
+    """``celery`` worker boot step to initialize ``django_structlog``.
+
+    >>> from celery import Celery
+    >>> from django_structlog.celery.steps import DjangoStructLogInitStep
+    >>>
+    >>> app = Celery("django_structlog_demo_project")
+    >>> app.steps['worker'].add(DjangoStructLogInitStep)
+
+    """
+
+    def __init__(self, parent, **kwargs):
+        super().__init__(parent, **kwargs)
+        import celery
+        from celery.signals import (
+            before_task_publish,
+            after_task_publish,
+            task_prerun,
+            task_retry,
+            task_success,
+            task_failure,
+            task_revoked,
+        )
+
+        before_task_publish.connect(receivers.receiver_before_task_publish)
+        after_task_publish.connect(receivers.receiver_after_task_publish)
+        task_prerun.connect(receivers.receiver_task_pre_run)
+        task_retry.connect(receivers.receiver_task_retry)
+        task_success.connect(receivers.receiver_task_success)
+        task_failure.connect(receivers.receiver_task_failure)
+        task_revoked.connect(receivers.receiver_task_revoked)
+        if celery.VERSION > (4,):
+
+            from celery.signals import task_unknown, task_rejected
+
+            task_unknown.connect(receivers.receiver_task_unknown)
+            task_rejected.connect(receivers.receiver_task_rejected)
---
+++ packages/django-structlog/django-structlog-2.0.0.tar.gz/django-structlog-2.0.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_namespace_packages
+
+import django_structlog
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="django-structlog",
+    version=django_structlog.__version__,
+    author="Jules Robichaud-Gagnon",
+    author_email="j.robichaudg+pypi@gmail.com",
+    description="Structured Logging for Django",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/jrobichaud/django-structlog",
+    packages=find_namespace_packages(
+        include=["django_structlog", "django_structlog.*"]
+    ),
+    install_requires=["django>=1.11", "structlog", "django-ipware"],
+    include_package_data=True,
+    license="MIT",
+    classifiers=[
+        "Framework :: Django",
+        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.1",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: System :: Logging",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
