02b869a939503d7ebb22afa0bcff7d790e8de7bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:42 +0000

    Add package mic/1.3.7/mic-1.3.7-py3-none-any.whl

---
+++ packages/mic/mic-1.3.7-py3-none-any.whl/mic/__main__.py
@@ -0,0 +1,112 @@
+import collections
+import sys
+from pathlib import Path
+
+import mic
+import semver
+from mic import _utils
+from mic.cli_docs import *
+from mic.click_encapsulate.commands import start, trace, configs, add_parameters, inputs, outputs, run, upload, wrapper
+from mic.credentials import configure_credentials, print_list_credentials
+from modelcatalog import Configuration
+
+
+@click.group()
+@click.option("--verbose", "-v", default=0, count=True)
+def cli(verbose):
+    _utils.init_logger()
+    try:
+        lv = ".".join(_utils.get_latest_version().split(".")[:3])
+    except Exception as e:
+        click.secho(
+            f"""WARNING: Unable to check if MIC is updated""",
+            fg="yellow",
+        )
+        return
+
+    cv = ".".join(mic.__version__.split(".")[:3])
+
+    if semver.compare(lv, cv) > 0:
+        click.secho(
+            f"""WARNING: You are using mic version {mic.__version__}, however version {lv} is available.
+You should consider upgrading via the 'pip install --upgrade mic' command.""",
+            fg="yellow",
+        )
+
+
+@cli.command(short_help="Show mic version")
+def version(debug=False):
+    click.echo(f"{Path(sys.argv[0]).name} v{mic.__version__}")
+
+
+@cli.command(short_help="Configure credentials", help="Configure your credentials to access the Model Catalog API, "
+                                                      "GitHub and Docker features")
+@click.option(
+    "--profile",
+    "-p",
+    envvar="MINT_PROFILE",
+    type=str,
+    default="default",
+    metavar="<profile-name>",
+)
+@click.option('--server', prompt='Model Catalog API',
+              help='The Model Catalog API', required=True, default=Configuration().host, show_default=True)
+@click.option('--username', prompt='Username',
+              help='Your email', required=True, default="mint@isi.edu", show_default=True)
+@click.option('--password', prompt="Password",
+              required=True, hide_input=True, help="Your password")
+@click.option('--name', prompt='Name', help='Your name', required=True)
+@click.option('--git_username', prompt='GitHub Username', help='Your GitHub Username', required=True)
+@click.option('--git_token', prompt='GitHub API token', help='Your GitHub API token', required=True, hide_input=False)
+@click.option('--dockerhub_username', prompt='Docker Username', help='Your Docker Username')
+def credentials(server, username, password, git_username, git_token, name, dockerhub_username, profile="default"):
+    try:
+        email = username
+        configure_credentials(server, username, password, git_username, git_token, name, email, dockerhub_username,
+                              profile)
+    except Exception as e:
+        click.secho("Unable to create configuration file", fg="red")
+
+
+@cli.command(short_help="List credentials profiles",
+             help="List credential parameters for mic profiles. Lists all profile credentials if no profile given")
+@click.option(
+    "--profile",
+    "-p",
+    envvar="MINT_PROFILE",
+    type=str,
+    default=None,
+    metavar="<profile-name>",
+    help="specify a profile to list"
+)
+@click.option(
+    "--short",
+    "-s",
+    is_flag=True,
+    help="Only show a list of profiles, not their contents"
+)
+def list_credentials(profile=None, short=False):
+    print_list_credentials(profile, short)
+
+
+class OrderedGroup(click.Group):
+    def __init__(self, name=None, commands=None, **attrs):
+        super(OrderedGroup, self).__init__(name, commands, **attrs)
+        #: the registered subcommands by their exported names.
+        self.commands = commands or collections.OrderedDict()
+
+    def list_commands(self, ctx):
+        return self.commands
+
+@cli.group(cls=OrderedGroup)
+def pkg():
+    """Command to encapsulate your model component"""
+pkg.add_command(start)
+pkg.add_command(trace)
+pkg.add_command(add_parameters)
+pkg.add_command(configs)
+pkg.add_command(inputs)
+pkg.add_command(outputs)
+pkg.add_command(wrapper)
+pkg.add_command(run)
+pkg.add_command(upload)
---
+++ packages/mic/mic-1.3.7-py3-none-any.whl/mic/publisher/docker.py
@@ -0,0 +1,76 @@
+import logging
+import os
+from pathlib import Path
+
+import click
+import docker
+from docker.errors import APIError
+from mic._utils import get_mic_logger
+from mic.config_yaml import get_key_spec, write_spec
+from mic.constants import DOCKER_KEY, DOCKER_USERNAME_KEY, VERSION_KEY, CONTAINER_NAME_KEY
+from mic.credentials import get_credentials
+
+logging = get_mic_logger()
+
+
+def build_docker(docker_path: Path, name: str):
+    client = docker.from_env()
+    click.echo("Downloading the base image and building your image")
+    logging.info("Downloading and building base docker image")
+    try:
+        image, logs = client.images.build(path=str(docker_path), tag="{}".format(name), nocache=True)
+        for chunk in logs:
+            if "stream" in chunk:
+                line = chunk["stream"].encode().decode('utf8').replace("\n", "")
+                click.echo(f'{line}')
+    except docker.errors.BuildError as err:
+        click.echo(f'Build Attempt Failed[{name}]')
+        logging.warning("Build attempt failed")
+        logging.debug(err)
+        click.echo(f'{err}')
+        for chunk in err.build_log:
+            if "stream" in chunk:
+                line = chunk["stream"].encode().decode('utf8').replace("\n", "")
+                click.echo(f'{line}')
+                logging.debug(err)
+        raise ValueError
+    return image.tags[0]
+
+
+def publish_docker(mic_config_path, profile):
+    # DOCKER_KEY doesn't have the docker username
+    docker_image = get_key_spec(mic_config_path, DOCKER_KEY)
+    try:
+        docker_image = docker_image.split('/')[-1]
+    except:
+        pass
+    try:
+        docker_image = docker_image.split(':')[0]
+    except:
+        pass
+    container_name = get_key_spec(mic_config_path, CONTAINER_NAME_KEY)
+    docker_username = get_docker_username(profile)
+    version = get_key_spec(mic_config_path, VERSION_KEY)
+    docker_image_with_version = f"""{docker_username}/{docker_image}:{version}"""
+    docker_container_cmd = f"""docker container commit {container_name} {docker_image_with_version} """
+    click.secho(f"Committing the changes into the Docker Image "
+                f"Please wait...")
+    os.system(docker_container_cmd)
+    click.secho("Uploading the Docker Image")
+    logging.info("Publish docker image")
+    try:
+        docker_push_cmd = f"""docker push {docker_image_with_version} """
+        os.system(docker_push_cmd)
+        write_spec(mic_config_path, DOCKER_KEY, docker_image_with_version)
+
+    except Exception as e:
+        raise e
+
+
+def get_docker_username(profile):
+    credentials = get_credentials(profile)
+    if DOCKER_USERNAME_KEY not in credentials:
+        click.secho(f"Docker username not found. Please run"
+                    f"$ mic credentials -p {profile}")
+        exit(0)
+    return credentials[DOCKER_USERNAME_KEY]
