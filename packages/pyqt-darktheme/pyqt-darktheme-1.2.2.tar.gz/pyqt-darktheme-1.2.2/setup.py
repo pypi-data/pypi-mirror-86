62a2fb25782ba9f5918449a85c5a0eda17414810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:34 +0000

    Add package pyqt-darktheme/1.2.2/pyqt-darktheme-1.2.2.tar.gz

---
+++ packages/pyqt-darktheme/pyqt-darktheme-1.2.2.tar.gz/pyqt-darktheme-1.2.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyqt-darktheme",
+    version="1.2.2",
+    author="Michael-Yongshi",
+    author_email="4registration@outlook.com",
+    description="A dark theme desktop gui package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Michael-Yongshi/Gui-Dark-Theme",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
