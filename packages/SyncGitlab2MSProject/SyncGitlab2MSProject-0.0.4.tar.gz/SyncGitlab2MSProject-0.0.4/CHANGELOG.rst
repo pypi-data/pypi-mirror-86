732ef0dec7c30d7193c58d32e55d23783528417d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:19 +0000

    Add package SyncGitlab2MSProject/0.0.4/SyncGitlab2MSProject-0.0.4.tar.gz

---
+++ packages/SyncGitlab2MSProject/SyncGitlab2MSProject-0.0.4.tar.gz/SyncGitlab2MSProject-0.0.4/CHANGELOG.rst
@@ -0,0 +1,30 @@
+=========
+Changelog
+=========
+
+Version 0.0.4
+=============
+- First auto release with travis
+    - Use `Travis AWS`_
+    - Use travis.com instead of travis.org
+
+Version 0.0.3
+=============
+- Also use Hyperlink or Text29 URL field to find related issues
+- Added documentation on how to fix the OpenURL Problem
+- Fixed some sync bugs
+
+Version 0.0.2
+=============
+- Documentation Only Changes
+    - Add Badges
+    - Import Readme to documentation
+    - Allow build of documentation even without pywin32 installed
+    - Use ReadTheDoc Sphinx Theme
+
+Version 0.0.1
+=============
+
+- First Release
+
+.. _Travis AWS: https://blog.travis-ci.com/2020-09-11-arm-on-aws
---
+++ packages/SyncGitlab2MSProject/SyncGitlab2MSProject-0.0.4.tar.gz/SyncGitlab2MSProject-0.0.4/tests/test_msproject.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+from typing import Any, Union, Optional, Callable
+from pathlib import Path
+from inspect import getattr_static, signature
+from datetime import datetime
+import pytest
+
+from syncgitlab2msproject.ms_project import MSProject
+from syncgitlab2msproject.exceptions import (
+    LoadingError,
+    ClassNotInitiated,
+    MSProjectSyncError,
+)
+
+__author__ = "Carli"
+__copyright__ = "Carli"
+__license__ = "mit"
+
+
+BASE_DIR = Path(__file__).absolute().parent
+TEST_FILE_NAME = "Project1.mpp"
+TEST_FILE = BASE_DIR / TEST_FILE_NAME
+
+
+class DoNotSave(Exception):
+    pass
+
+
+# TODO Set up Pytest with copying the Test file
+# TODO Make sure that the copied file was not modified
+#           --> https://docs.pytest.org/en/stable/fixture.html
+# TODO test saving and loading
+
+
+def get_prop_setter_func(obj: Any, property_name: str) -> Callable:
+    return getattr_static(obj, property_name).fset
+
+
+def get_prop_set_type_annotation(setter_function: Callable) -> Union[str, type]:
+    """
+    Get the type annotation of obj for property
+    """
+
+    # Combination of
+    # https://stackoverflow.com/a/9917213/3813064 and
+    # https://stackoverflow.com/questions/53949473/python-dynamically-access-type-annotation-of-a-property
+    sign = signature(setter_function)
+    return next(reversed(sign.parameters.values())).annotation
+
+
+def test_load_manually():
+    project = MSProject(TEST_FILE)
+    project.load()
+    print(project[0].name)
+    assert TEST_FILE_NAME in repr(project[0])
+    project.close()
+
+
+def test_not_loaded():
+    project: MSProject = MSProject(TEST_FILE)
+    assert "not loaded" in repr(project)
+    with pytest.raises(ClassNotInitiated):
+        print(project[0].name)
+    project.close()
+
+
+def test_loading_wrong_file():
+    with pytest.raises(LoadingError):
+        with MSProject(BASE_DIR / "not_existing.mpp"):
+            pass
+
+
+def test_printing_all():
+    assert TEST_FILE.is_file()
+    print(f"Loading '{TEST_FILE}'")
+
+    with MSProject(TEST_FILE) as tasks:
+        assert TEST_FILE_NAME in repr(tasks)
+        # Test defined Number of Tasks
+        assert len(tasks) == 17
+        # Test cycling through all tasks
+        for i, task in enumerate(tasks):
+            if task:
+                print(f"*{i:>4}: '{task.name}'")
+            else:
+                print(f"#{i:>4}  >is empty<")
+
+        task = tasks[0]
+
+        # Test printing all
+        for attribute in dir(task):
+            if not attribute.startswith("_"):
+                print(f"{attribute} = {getattr(task, attribute)}")
+
+
+def test_setting_everything():
+    def check_setting_val(obj: Any, attrib: str, prop_type: Union[type, None]):
+        if prop_type == datetime:
+            new_value = datetime(2020, 3, 4, 20, 15, 30).astimezone()
+        elif prop_type == int:
+            new_value = 1
+        elif prop_type == str:
+            new_value = "Test String"
+        elif prop_type == bool:
+            new_value = True
+        elif prop_type is None:
+            new_value = None
+        else:
+            raise ValueError(f"Invalid type {prop_type}")
+
+        # If new val is given lets try to set it and compare it with result
+        setattr(obj, attrib, new_value)
+        print(f"Checking {attrib} - {prop_type} with '{new_value}'")
+        if isinstance(new_value, datetime):
+            # We need
+            compare: datetime = getattr(obj, attrib)
+            assert str(compare.astimezone()) == str(new_value)
+        else:
+            assert getattr(obj, attrib) == new_value
+
+    with pytest.raises(DoNotSave):
+        with MSProject(TEST_FILE) as tasks:
+            # Test setting attributes based on typed annotation
+            task = tasks[0]
+            for attribute in dir(task):
+                if not attribute.startswith("_"):
+                    if setter_func := get_prop_setter_func(task, attribute):
+                        prop_type = get_prop_set_type_annotation(setter_func)
+                        if prop_type in (int, bool, None, datetime, str):
+                            check_setting_val(task, attribute, prop_type)
+                        elif prop_type == Optional[datetime]:
+                            check_setting_val(task, attribute, None)
+                            check_setting_val(task, attribute, datetime)
+                        else:
+                            print(f"{attribute} ignored {prop_type}")
+            raise DoNotSave("We do not want the file to save!")
+
+
+def test_percentage():
+    with pytest.raises(MSProjectSyncError):
+        with MSProject(TEST_FILE) as tasks:
+            # Test setting attributes based on typed annotation
+            task = tasks[0]
+            task.percent_complete = 101
