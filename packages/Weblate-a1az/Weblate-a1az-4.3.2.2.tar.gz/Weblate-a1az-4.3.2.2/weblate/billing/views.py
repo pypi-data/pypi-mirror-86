c7dbd9751236f239c7e4158d505066150671c1af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:34 +0000

    Add package Weblate-a1az/4.3.2.2/Weblate-a1az-4.3.2.2.tar.gz

---
+++ packages/Weblate-a1az/Weblate-a1az-4.3.2.2.tar.gz/Weblate-a1az-4.3.2.2/weblate/billing/views.py
@@ -0,0 +1,99 @@
+#
+# Copyright © 2012 - 2020 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate <https://weblate.org/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from django.http import Http404, HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+
+from weblate.billing.models import Billing, Invoice
+
+
+@login_required
+def download_invoice(request, pk):
+    """Download invoice PDF."""
+    invoice = get_object_or_404(Invoice, pk=pk)
+
+    if not invoice.ref:
+        raise Http404("No reference!")
+
+    allowed_billing = Billing.objects.for_user(request.user).filter(
+        pk=invoice.billing.pk
+    )
+
+    if not allowed_billing.exists():
+        raise PermissionDenied("Not an owner!")
+
+    if not invoice.filename_valid:
+        raise Http404("File {0} does not exist!".format(invoice.filename))
+
+    with open(invoice.full_filename, "rb") as handle:
+        data = handle.read()
+
+    response = HttpResponse(data, content_type="application/pdf")
+    response["Content-Disposition"] = "attachment; filename={0}".format(
+        invoice.filename
+    )
+    response["Content-Length"] = len(data)
+
+    return response
+
+
+def handle_post(request, billings):
+    def get(name):
+        try:
+            return int(request.POST[name])
+        except (KeyError, ValueError):
+            return None
+
+    recurring = get("recurring")
+    terminate = get("terminate")
+    if not recurring and not terminate:
+        return
+    try:
+        billing = billings.get(pk=recurring or terminate)
+    except Billing.DoesNotExist:
+        return
+    if recurring:
+        if "recurring" in billing.payment:
+            del billing.payment["recurring"]
+        billing.save()
+    elif terminate:
+        billing.state = Billing.STATE_TERMINATED
+        billing.save()
+
+
+@login_required
+def overview(request):
+    billings = Billing.objects.for_user(request.user).prefetch_related(
+        "plan", "projects", "invoice_set"
+    )
+    if request.method == "POST":
+        handle_post(request, billings)
+        return redirect("billing")
+    return render(
+        request,
+        "billing/overview.html",
+        {
+            "billings": billings,
+            "active_billing_count": billings.filter(
+                state__in=(Billing.STATE_ACTIVE, Billing.STATE_TRIAL)
+            ).count(),
+        },
+    )
---
+++ packages/Weblate-a1az/Weblate-a1az-4.3.2.2.tar.gz/Weblate-a1az-4.3.2.2/weblate/machinery/weblatetm.py
@@ -0,0 +1,74 @@
+#
+# Copyright © 2012 - 2020 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate <https://weblate.org/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+
+from django.utils.encoding import force_str
+
+from weblate.machinery.base import MachineTranslation, get_machinery_language
+from weblate.trans.models import Unit
+from weblate.utils.state import STATE_TRANSLATED
+
+
+class WeblateTranslation(MachineTranslation):
+    """Translation service using strings already translated in Weblate."""
+
+    name = "Weblate"
+    rank_boost = 1
+    cache_translations = False
+    do_cleanup = False
+
+    def convert_language(self, language):
+        """No conversion of language object."""
+        return get_machinery_language(language)
+
+    def is_supported(self, source, language):
+        """Any language is supported."""
+        return True
+
+    def is_rate_limited(self):
+        """This service has no rate limiting."""
+        return False
+
+    def download_translations(self, source, language, text, unit, user, search):
+        """Download list of possible translations from a service."""
+        if user:
+            base = Unit.objects.filter_access(user)
+        else:
+            base = Unit.objects.all()
+        source_language_id = unit.translation.component.source_language_id
+        matching_units = base.filter(
+            source__search=text,
+            translation__component__source_language_id=source_language_id,
+            translation__language=language,
+            state__gte=STATE_TRANSLATED,
+        )
+
+        for munit in matching_units:
+            source = munit.source_string
+            quality = self.comparer.similarity(text, source)
+            if quality < 10 or (quality < 75 and not search):
+                continue
+            yield {
+                "text": munit.get_target_plurals()[0],
+                "quality": quality,
+                "service": self.name,
+                "origin": force_str(munit.translation.component),
+                "origin_url": munit.get_absolute_url(),
+                "source": source,
+            }
