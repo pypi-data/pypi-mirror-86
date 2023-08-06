bf5de6af4b99697407906d767939d30459b9bc02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:12 +0000

    Add package ToBi-pkg/0.0.1/ToBi_pkg-0.0.1.tar.gz

---
+++ packages/ToBi-pkg/ToBi_pkg-0.0.1.tar.gz/ToBi_pkg-0.0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="ToBi_pkg",
+    version="0.0.1",
+    author="KPU_ToBi",
+    author_email="golapaduck29@naver.com",
+    description="A package for KPU_ToBi",
+    url="https://github.com/golapaduck",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+    ],
+)
