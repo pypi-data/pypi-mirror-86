ef1c3ecfebeb0f376f94e47c2b35bbb7a4033ae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:30 +0000

    Add package django-raw-sugar/0.1.13/django-raw-sugar-0.1.13.tar.gz

---
+++ packages/django-raw-sugar/django-raw-sugar-0.1.13.tar.gz/django-raw-sugar-0.1.13/tests/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from raw_sugar import raw_manager, RawManager, FromRaw
+
+
+class AnotherSimpleModel(models.Model):
+    pass
+
+
+class MySimpleModel(models.Model):
+    name = models.TextField()
+    number = models.IntegerField()
+    source = models.ForeignKey(
+        AnotherSimpleModel, models.DO_NOTHING, null=True)
+
+    objects = RawManager()
+
+    my_raw_manager = RawManager(FromRaw('SELECT "my str" as name, 111 as number',
+                                        null_fields=['id', 'source_id']))
+
+    @raw_manager
+    def my_raw_manager_2(cls):
+        return FromRaw('SELECT "my str" as name, 111 as number',
+                       null_fields=['id', 'source_id'])
+
+    @raw_manager(is_callable=True)
+    def my_callable_raw_manager(cls, name=""):
+        return FromRaw('SELECT %s as name, 111 as number',
+                       null_fields=['id', 'source_id'],
+                       params=[name])
