c925322592eaa96264a945d8f12a4073b0bec293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:01 +0000

    Add package cuttpy/1.0.0/cuttpy-1.0.0.tar.gz

---
+++ packages/cuttpy/cuttpy-1.0.0.tar.gz/cuttpy-1.0.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cuttpy",
+    version="1.0.0",
+    author="IAmThe2ndHuman",
+    license="MIT",
+    description="Wrapper for the cutt.ly URL shortener API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/IAmThe2ndHuman/cuttpy",
+    packages=setuptools.find_packages(),
+    install_requires=["requests"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
+)
