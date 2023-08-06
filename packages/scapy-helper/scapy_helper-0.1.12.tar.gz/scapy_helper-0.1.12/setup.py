14a490bca2213abb8fa706711377822d35c28aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:42 +0000

    Add package scapy-helper/0.1.12/scapy_helper-0.1.12.tar.gz

---
+++ packages/scapy-helper/scapy_helper-0.1.12.tar.gz/scapy_helper-0.1.12/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="scapy_helper",
+    description="Several features that should help you use Scapy",
+    author="Nex Sabre",
+    author_email="nexsabre@protonmail.com",
+    version="0.1.12",
+    url="https://github.com/NexSabre/scapy_helper",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["scapy~=2.4.4", "tabulate~=0.8.7"]
+)
