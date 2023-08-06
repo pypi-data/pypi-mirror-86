4f05106b73bb2907dc4e732f61f4e92f5269000d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:00 +0000

    Add package runpandas/0.1/runpandas-0.1-py2.py3-none-any.whl

---
+++ packages/runpandas/runpandas-0.1-py2.py3-none-any.whl/runpandas/__init__.py
@@ -0,0 +1,10 @@
+from runpandas.reader import _read_file as read_file  # noqa
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
+
+__all__ = [
+    "__version__",
+]
