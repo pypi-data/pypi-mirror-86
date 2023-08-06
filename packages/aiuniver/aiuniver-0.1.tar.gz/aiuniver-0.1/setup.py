2ced61c6d15796217d65cba8707cb34b94760899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:50 +0000

    Add package aiuniver/0.1/aiuniver-0.1.tar.gz

---
+++ packages/aiuniver/aiuniver-0.1.tar.gz/aiuniver-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='aiuniver',
+    version='0.1',
+    packages=['aiuniver'],
+    url='',
+    license='',
+    author='AI University',
+    author_email='nejronki@gmail.com'    
+)
