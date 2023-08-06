32fae7ccaa2ef1e35d2434c17f5c7fbfa26abca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:44 +0000

    Add package us-birth-data/0.1.0/us_birth_data-0.1.0.tar.gz

---
+++ packages/us-birth-data/us_birth_data-0.1.0.tar.gz/us_birth_data-0.1.0/setup.py
@@ -0,0 +1,43 @@
+import re
+from pathlib import Path
+
+import setuptools
+
+
+def read_version():
+    try:
+        v = [x for x in Path('us_birth_data/__init__.py').open() if x.startswith('__version__')][0]
+        v = re.match(r"__version__ *= *'(.*?)'\n", v)[1]
+        return v
+    except Exception as e:
+        raise RuntimeError(f"Unable to read version string: {e}")
+
+
+setuptools.setup(
+    name="us_birth_data",
+    version=read_version(),
+    author="Christopher Boyd",
+    description="A pre-processed longitudinal aggregate table of NVSS birth data in the US from 1968 onward",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Mikuana/us_birth_data",
+    long_description=Path('README.md').read_text(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+      'pandas>=1.1',
+      'pyarrow',
+      'tqdm'
+    ],
+    extras_require={
+        'Testing': [
+            'pytest', 'pytest-mock', 'pytest-cov',
+            'rumydata>=0.5.2'
+        ]
+    },
+    packages=setuptools.find_packages(),
+    include_package_data=True
+)
