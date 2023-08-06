2c06f112c7822b34ff5c587b72b3a7a67fba6beb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:56 +0000

    Add package pymunk/6.0.0.dev1/pymunk-6.0.0.dev1.tar.gz

---
+++ packages/pymunk/pymunk-6.0.0.dev1.tar.gz/pymunk-6.0.0.dev1/tools/create_release_win.py
@@ -0,0 +1,42 @@
+import os
+import shutil
+import sys
+
+
+def main():
+    os.chdir("..")
+    # shutil.rmtree("dist", True)
+    # shutil.rmtree("docs/html", True)
+
+    print(
+        """
+    Remember (before running this script!): 
+    - change version number in readme, setup.py, showcase.rst and __init__ 
+    - test in at least CPython 3.x and Pypy3
+    - validate test results of Travis and Appveyor
+    - write changelog entry and put in changelog.rst
+    - make sure all images are optimized (for example with tinypng.com)
+    
+    """
+    )
+    print(
+        """
+    Once the release is done, remember to:
+    - tag code on github with version
+    - Download dists from github release and appveyor
+    - Upload files on pypi (> python -m twine upload dist/*)
+    - Update Pymunk entry on pygame.org
+    - Update Pymunk on conda-forge
+    - Possibly: Make release announcement at the chipmunk forum
+    - Possibly: make release announcement on the pyglet list
+    - Possibly: make release announcement on the pygame list
+    - Possibly: make release announcement on the python announce list
+    - Possibly: make release announcement on the kivy mailing list
+    - Possibly: make PR to update pymunk in python-for-android (remember test!)
+    """
+    )
+    os.chdir("tools")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
