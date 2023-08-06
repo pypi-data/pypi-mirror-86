8660cd034b75fa85231a1ab481f9aee773614d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:12 +0000

    Add package newLibrary/1.2.5/newLibrary-1.2.5.tar.gz

---
+++ packages/newLibrary/newLibrary-1.2.5.tar.gz/newLibrary-1.2.5/setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+setup(
+	name='newLibrary',
+	packages=find_packages(),
+	version='1.2.5',
+	description='My first Python library',
+	author='Hamed',
+	)
