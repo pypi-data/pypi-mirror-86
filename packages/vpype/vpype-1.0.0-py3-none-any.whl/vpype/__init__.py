d81e878b4c925841629acfbe048fb06c46799514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:06 +0000

    Add package vpype/1.0.0/vpype-1.0.0-py3-none-any.whl

---
+++ packages/vpype/vpype-1.0.0-py3-none-any.whl/vpype/__init__.py
@@ -0,0 +1,11 @@
+"""vpype core API."""
+
+from .config import *
+from .decorators import *
+from .geometry import *
+from .io import *
+from .layers import *
+from .line_index import *
+from .model import *
+from .primitives import *
+from .utils import *
---
+++ packages/vpype/vpype-1.0.0-py3-none-any.whl/vpype_cli/__init__.py
@@ -0,0 +1,14 @@
+# register all commands
+from .blocks import *
+from .cli import begin, end
+from .debug import *
+from .frames import *
+from .generators import *
+from .layerops import *
+from .operations import *
+from .primitives import *
+from .read import *
+from .script import *
+from .show import *
+from .transforms import *
+from .write import *
