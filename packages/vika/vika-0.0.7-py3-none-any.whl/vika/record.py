7426a27ea8186d69b43b0064affce8c516886e52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:16 +0000

    Add package vika/0.0.7/vika-0.0.7-py3-none-any.whl

---
+++ packages/vika/vika-0.0.7-py3-none-any.whl/vika/record.py
@@ -0,0 +1,71 @@
+from .exceptions import RecordWasDeleted
+
+
+class Record:
+    def __init__(self, datasheet, record: str):
+        self._datasheet = datasheet
+        self._id = record.id
+        self._is_del = False
+
+    @property
+    def _record(self):
+        return self._datasheet.get_record_by_id(self._id)
+
+    def _check_record_status(self):
+        if self._is_del:
+            return RecordWasDeleted()
+
+        return None
+
+    def __str__(self):
+        return f"{self._id}"
+
+    def __getattr__(self, key):
+        if key in self._record.data:
+            return self._record.data.get(key)
+
+        # FIXME 因为目前缺少  Meta， 这里返回 None。后续可以通过 meta 判断是否是合法的 key
+        # return Exception(f"record has no field:[{key}]")
+        return None
+
+    def delete(self) -> bool:
+        self._check_record_status()
+        is_del_success = self._datasheet.delete_records([self._id])
+        if is_del_success:
+            self._datasheet.remove_records([self._record])
+            self._is_del = True
+        return is_del_success
+
+    def _is_attachment_field(self, field_name):
+        return field_name in self._datasheet.attachment_fields
+
+    def __setattr__(self, key, value):
+        if key.startswith("_"):
+            super().__setattr__(key, value)
+        else:
+            if self._is_attachment_field(key) and isinstance(value, dict):
+                if isinstance(value, list):
+                    value = [self._datasheet.upload_file(url) for url in value]
+                if not value:
+                    value = None
+            data = {"recordId": self._id, "fields": {key: value}}
+            update_success_count = self._datasheet.update_records(data)
+            if update_success_count == 1:
+                self._record.data[key] = value
+
+    def json(self):
+        self._check_record_status()
+        return self._record.data
+
+    def make_update_body(self, data):
+        data = {"recordId": self._id, "fields": data}
+        return data
+
+    def update(self, data):
+        """
+        更新多个字段
+        """
+        self._check_record_status()
+        # 更新单个记录的多个字段，只返回一条记录
+        data = self.make_update_body(data)
+        return self._datasheet.update_records(data)
