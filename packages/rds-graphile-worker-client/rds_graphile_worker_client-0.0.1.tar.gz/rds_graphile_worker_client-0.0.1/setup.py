31fe6d1184b13864aa0c0ad02bded46990c32a18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:05 +0000

    Add package rds-graphile-worker-client/0.0.1/rds_graphile_worker_client-0.0.1.tar.gz

---
+++ packages/rds-graphile-worker-client/rds_graphile_worker_client-0.0.1.tar.gz/rds_graphile_worker_client-0.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version_info = {}
+exec(open("rds_graphile_worker_client/package_version.py").read(), version_info)
+
+#readme = open("README.md", "rb").read().decode("utf-8")
+install_requires = open("requirements.txt", "rb").read().decode("utf-8")
+
+setup(
+    name="rds_graphile_worker_client",
+    version=version_info["__version__"],
+    description="A small, simple python client for adding jobs to the graphile-worker job queue on RDS postgresql database",
+    #long_description=readme,
+    #long_description_content_type="text/markdown",
+    author="Curvewise",
+    author_email="jacob@curvewise.com",
+    url="https://github.com/curvewise/python-rds-graphile-worker-client",
+    project_urls={
+        "Issue Tracker": "https://github.com/curvewise/python-rds-graphile-worker-client/issues"
+    },
+    packages=find_packages(),
+    install_requires=install_requires,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Other Audience",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Artistic Software",
+        "Topic :: Multimedia :: Graphics :: 3D Modeling",
+        "Topic :: Scientific/Engineering :: Visualization",
+    ],
+)
