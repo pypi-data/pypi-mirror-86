3d74889277160c849cb82381b668256ed9a12832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:17 +0000

    Add package django-edit-suggestion/1.1/django-edit-suggestion-1.1.tar.gz

---
+++ packages/django-edit-suggestion/django-edit-suggestion-1.1.tar.gz/django-edit-suggestion-1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='django-edit-suggestion',
+    version='1.01',
+    description='A django package for creating multiple users editable models',
+    url='https://github.com/smileservices/django-simple-history',
+    author='Vladimir Gorea',
+    author_email='vladimir.gorea@gmail.com',
+    license='MIT',
+    packages=['django_edit_suggestion'],
+    install_requires=[], # packages listed here will be automatically installed
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+    ],
+)
