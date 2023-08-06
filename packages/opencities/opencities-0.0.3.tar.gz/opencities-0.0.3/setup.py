ad17fcec3ac2769f74ae9c84e8cfaf302a9a463e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:24 +0000

    Add package opencities/0.0.3/opencities-0.0.3.tar.gz

---
+++ packages/opencities/opencities-0.0.3.tar.gz/opencities-0.0.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='opencities',
+    version='0.0.3',
+    author='Zac Thiel',
+    author_email='digitalservices@grcity.us',
+    description='Packages for the OpenCities API',
+    long_description_content_type='text/markdown',
+    url='https://github.com/GRInnovation/opencities',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3.7.7',
+)
