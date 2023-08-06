f963d32851616a2a44a57a62d186a5db4913496d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:48 +0000

    Add package pymlconf/2.5.1/pymlconf-2.5.1-py3-none-any.whl

---
+++ packages/pymlconf/pymlconf-2.5.1-py3-none-any.whl/tests/test_binary.py
@@ -0,0 +1,14 @@
+from pymlconf import Root
+
+
+class TestBinary:
+
+    def test_binary(self):
+        root = Root('''
+            app:
+              name: MyApp
+            secret: !!binary YWJj\n
+        ''')
+
+        assert root.app.name == 'MyApp'
+        assert root.secret == b'abc'
