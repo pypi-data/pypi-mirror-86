1fdd157d0442c5f4c559fe7a4ceb227b09ca023e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:37 +0000

    Add package colomoto-jupyter/0.7.3/colomoto_jupyter-0.7.3.tar.gz

---
+++ packages/colomoto-jupyter/colomoto_jupyter-0.7.3.tar.gz/colomoto_jupyter-0.7.3/colomoto_jupyter/io.py
@@ -0,0 +1,30 @@
+
+import os
+from urllib.parse import urlparse
+from urllib.request import urlretrieve
+
+from .sessionfiles import new_output_file
+from .ui import info
+
+import cellcollective
+
+def download(url, suffix=None):
+    filename = new_output_file(suffix=suffix)
+    info("Downloading %s" % url)
+    filename, _ = urlretrieve(url, filename=filename)
+    return filename
+
+def ensure_localfile(filename):
+    uri = urlparse(filename)
+    if uri.netloc:
+        if cellcollective.url_matches(filename):
+            cellc = cellcollective.connect(filename)
+            bname = cellc.sbml_basename
+            url = cellc.sbml_url
+        else:
+            bname = os.path.basename(filename)
+            url = uri.geturl()
+        filename = download(url, suffix=bname)
+    assert os.path.exists(filename)
+    return filename
+
