1d98dd734cb104d587d94bf221b4434d322d3d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:03 +0000

    Add package beginner/0.0.2/beginner-0.0.2.tar.gz

---
+++ packages/beginner/beginner-0.0.2.tar.gz/beginner-0.0.2/setup.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+
+import setuptools
+
+with open(r'D:\Python Project\Packages\self\beginner\README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='beginner',
+    version='0.0.2',
+    author='Dreambuilder4028',
+    author_email='jiade_student@163.com',
+    description='Python package to let beginners in Python studies more easily.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Dreambuilder4028/coder/',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=['request'],
+)
