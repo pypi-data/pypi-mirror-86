6362ed425954a511132cbcc8f93cd157864b422a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:48 +0000

    Add package compyt/1.4/compyt-1.4-py3-none-any.whl

---
+++ packages/compyt/compyt-1.4-py3-none-any.whl/compyt/__init__.py
@@ -0,0 +1,14 @@
+# __init__.py
+from .compyt import mulc, divc, expc, absc, abs2c, amp, angle, conj, cn2cp, cp2in, cp2rn, cp2cn, rn2cp, rp2cp
+
+# from compyt import ComplexMul
+# from compyt import ComplexDiv
+# from compyt import ComplexAbs
+# from compyt import ComplexAbs2
+# from compyt import ComplexExp
+
+# from compyt import rn2cp, cp2cn, cp2rn, cp2in, cn2cp
+
+# from compyt import angle, amp, conj
+
+# from compyt import mul_c, div_c, abs_c, abs2_c, exp_c
