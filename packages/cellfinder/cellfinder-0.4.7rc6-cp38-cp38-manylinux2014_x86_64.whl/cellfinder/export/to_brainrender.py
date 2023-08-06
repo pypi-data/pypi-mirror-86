51043b72a1f027514f8a93812b8bdb3076758a63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:04 +0000

    Add package cellfinder/0.4.7rc6/cellfinder-0.4.7rc6-cp38-cp38-manylinux2014_x86_64.whl

---
+++ packages/cellfinder/cellfinder-0.4.7rc6-cp38-cp38-manylinux2014_x86_64.whl/cellfinder/export/to_brainrender.py
@@ -0,0 +1,14 @@
+import logging
+
+import pandas as pd
+
+
+def export_points(
+    points,
+    resolution,
+    output_filename,
+):
+    logging.info("Exporting to brainrender")
+    points = pd.DataFrame(points * resolution)
+    points.columns = ["x", "y", "z"]
+    points.to_hdf(output_filename, key="df", mode="w")
