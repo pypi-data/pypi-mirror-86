77606414aefbdf1bac8baa1f0ece983a6c2a70bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:14 +0000

    Add package aferro-ml-lib/0.7.2/aferro_ml_lib-0.7.2-py3-none-any.whl

---
+++ packages/aferro-ml-lib/aferro_ml_lib-0.7.2-py3-none-any.whl/ml_lib/bar_module.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+Module mimicking foo with more expensive memory and runtime requirements.
+"""
+
+from typing import List
+from .foo_module import Foo
+
+
+class Bar(Foo):
+    """
+    Similar to Foo, with higher memory and runtime requirements.
+    """
+
+    def __init__(self, size: int = 1000000):
+        """
+        The instance will contain a list instead of a range, so memory
+        complexity is O(n) instead of 2*O(1)
+        """
+        super(Bar, self).__init__(size)
+        self._x: List[int] = list(self._x)  # memory overhead
+
+    def _computation(self) -> None:
+        """
+        The computation will be 2*O(n) instead of O(1)
+        """
+        super(Bar, self)._computation()
+        self._x.index(len(self._x) - 1)  # runtime overhead
---
+++ packages/aferro-ml-lib/aferro_ml_lib-0.7.2-py3-none-any.whl/ml_lib/foo_module.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+Module containing a simple class with low memory and runtime requirements.
+"""
+
+
+from typing import Iterable
+
+
+class Foo(object):
+    """
+    A simple class with low memory and runtime requirements.
+    """
+
+    def __init__(self, size: int = 1000000):
+        """
+        The instance will contain 2 small objects: 2*O(1) memory.
+        """
+        assert size > 0, "size has to be a positive int!"
+        self._x: Iterable[int] = range(size)
+        self._result: int = 0
+
+    def _computation(self) -> None:
+        """
+        A simple computation in O(1).
+        """
+        self._result += 1
+
+    def loop(self, times: int) -> None:
+        """
+        Restart result and run computation a number of times.
+
+        :param times: non-negative number.
+        :type times: int
+        """
+        self._result = 0
+        for i in range(times):
+            self._computation()
+
+    def get_result(self) -> int:
+        """
+        This function does something.
+
+        :returns: a number stored in ``self._result``
+        :rtype: integer or float
+        """
+        # """
+        # Basic getter.
+        # :returns: the field stored in ``self._result``
+        # """
+        return self._result
