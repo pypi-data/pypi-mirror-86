690888d9f5c9fd19018f28513083434203bcf004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:43 +0000

    Add package spaceone-plugin/1.5.1/spaceone_plugin-1.5.1-py3-none-any.whl

---
+++ packages/spaceone-plugin/spaceone_plugin-1.5.1-py3-none-any.whl/spaceone/plugin/info/plugin_info.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import functools
+
+from spaceone.api.plugin.v1 import plugin_pb2
+from spaceone.api.plugin.v1 import supervisor_pb2
+from spaceone.plugin.model.installed_plugin_ref_model import InstalledPluginRef
+from spaceone.core.pygrpc.message_type import *
+
+__all__ = ['PluginInfo', 'PluginsInfo', 'PluginEndpoint']
+
+
+def PluginInfo(plugin_vo: InstalledPluginRef, minimal=False):
+    info = {
+        'plugin_id': plugin_vo.plugin_id,
+        'version': plugin_vo.version,
+        'state': plugin_vo.plugin_owner.state,
+        'endpoint': plugin_vo.plugin_owner.endpoint,
+        'endpoints': change_list_value_type(plugin_vo.plugin_owner.endpoints)
+    }
+    if not minimal:
+        info.update({
+            'supervisor_id': plugin_vo.supervisor.supervisor_id,
+            'supervisor_name': plugin_vo.supervisor.name,
+            'managed': plugin_vo.managed
+        })
+
+    return supervisor_pb2.PluginInfo(**info)
+
+
+def PluginsInfo(plugin_vo: InstalledPluginRef, total_count):
+    results = list(map(functools.partial(PluginInfo), plugin_vo))
+
+    return supervisor_pb2.PluginsInfo(results=results, total_count=total_count)
+
+
+def PluginEndpoint(endpoint):
+    return plugin_pb2.PluginEndpoint(**endpoint)
---
+++ packages/spaceone-plugin/spaceone_plugin-1.5.1-py3-none-any.whl/spaceone/plugin/manager/__init__.py
@@ -0,0 +1,3 @@
+from spaceone.plugin.manager.plugin_manager.__init__ import *
+from spaceone.plugin.manager.supervisor_manager.__init__ import *
+from spaceone.plugin.manager.repository_manager import *
