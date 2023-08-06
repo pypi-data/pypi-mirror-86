6d020446e1522f97710fc9afe7d7e4eb20c644a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:01 +0000

    Add package coilpy/0.2.34/coilpy-0.2.34-py3-none-any.whl

---
+++ packages/coilpy/coilpy-0.2.34-py3-none-any.whl/coilpy/__init__.py
@@ -0,0 +1,24 @@
+"""
+This is a python package for plotting and data processing in stellarator optimization.
+
+It contains several python functions on HDF5, Fourier surfaces, FOCUS, cois, 
+magnetic dipoles, STELLOPT, VMEC, BOOZ_XFORM, etc.
+
+The repository is available at https://github.com/zhucaoxiang/CoilPy.
+
+For full documenation, please check https://zhucaoxiang.github.io/CoilPy/api/coilpy.html.
+"""
+__version__ = "0.2.34"
+
+# local packages
+from .misc import colorbar, get_figure, kwargs2dict, map_matrix
+from .misc import print_progress, toroidal_period, vmecMN, xy2rp
+from .misc import trigfft, fft_deriv, trig2real, vmec2focus
+from .hdf5 import HDF5
+from .surface import FourSurf
+from .dipole import Dipole
+from .focushdf5 import FOCUSHDF5
+from .coils import Coil, SingleCoil
+from .stellopt import STELLout
+from .vmec import VMECout
+from .booz_xform import BOOZ_XFORM
