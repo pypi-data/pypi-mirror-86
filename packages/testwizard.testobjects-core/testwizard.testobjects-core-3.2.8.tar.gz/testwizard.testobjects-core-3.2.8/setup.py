1b0d12867d223272e31aca33de1d4552514da2d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:49 +0000

    Add package testwizard.testobjects-core/3.2.8/testwizard.testobjects-core-3.2.8.tar.gz

---
+++ packages/testwizard.testobjects-core/testwizard.testobjects-core-3.2.8.tar.gz/testwizard.testobjects-core-3.2.8/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_namespace_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="testwizard.testobjects-core",
+    version="3.2.8",
+    author="Eurofins Digital Testing - Belgium",
+    author_email="support-be@eurofins.com",
+    description="Testwizard core components for testobjects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_namespace_packages(),
+    install_requires=[
+        'testwizard.commands-services==3.2.8'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.3",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
+
