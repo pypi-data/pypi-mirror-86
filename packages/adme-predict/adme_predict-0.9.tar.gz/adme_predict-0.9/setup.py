a7059a8d64012d21dde0227acfd2eca1340a7c6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:59 +0000

    Add package adme-predict/0.9/adme_predict-0.9.tar.gz

---
+++ packages/adme-predict/adme_predict-0.9.tar.gz/adme_predict-0.9/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+with open("README.md", "r") as fh:
+	long_description=fh.read()
+setuptools.setup(
+	name="adme_predict", # Replace with your own username
+	version="0.9",
+	author="zhuyang",
+	author_email="zhuyang0925@163.com",
+	description="Our package is used for ADME attribute prediction",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/pypa/sampleproject",
+	packages=setuptools.find_packages(),
+	package_data={
+		'':['*.npy','*.pkl']
+	},
+	classifiers=[
+	"Programming Language :: Python :: 2",
+	"License :: OSI Approved :: MIT License",
+	"Operating System :: OS Independent",
+	],
+	python_requires='>=2.6',)
