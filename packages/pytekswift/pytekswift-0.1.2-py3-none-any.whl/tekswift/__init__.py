9fb10267dd9c483b4e30f2415422c2c4e656eac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:13 +0000

    Add package pytekswift/0.1.2/pytekswift-0.1.2-py3-none-any.whl

---
+++ packages/pytekswift/pytekswift-0.1.2-py3-none-any.whl/tekswift/__init__.py
@@ -0,0 +1,35 @@
+"""tekswift package.
+
+Defines:
+    build_path,
+    countrycode_from_swiftcode,
+    load_country_data,
+    isvalid_swiftcode,
+    InvalidCountryCode,
+    InvalidSwiftCode,
+"""
+# import
+from .dataloader import (
+    InvalidCountryCode,
+    InvalidSwiftCode,
+    build_path,
+    countrycode_from_swiftcode,
+    isvalid_swiftcode,
+    load_country_data,
+    lookup_swiftcode,
+)
+
+
+__all__ = [
+    "InvalidCountryCode",
+    "InvalidSwiftCode",
+    "build_path",
+    "countrycode_from_swiftcode",
+    "isvalid_swiftcode",
+    "load_country_data",
+    "lookup_swiftcode",
+]
+
+__version__ = "0.1.2"
+
+__author__ = "Tralah M Brian <https://github.com/TralahM>"
