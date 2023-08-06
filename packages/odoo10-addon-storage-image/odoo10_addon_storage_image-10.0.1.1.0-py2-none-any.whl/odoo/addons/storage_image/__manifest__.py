02ea006a5fa7507f6cc75d3d1c91f862102d7ab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:39 +0000

    Add package odoo10-addon-storage-image/10.0.1.1.0/odoo10_addon_storage_image-10.0.1.1.0-py2-none-any.whl

---
+++ packages/odoo10-addon-storage-image/odoo10_addon_storage_image-10.0.1.1.0-py2-none-any.whl/odoo/addons/storage_image/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Storage Image",
+    "summary": "Store image and resized image in a storage backend",
+    "version": "10.0.1.1.0",
+    "category": "Storage",
+    "website": "https://www.github.com/OCA/storage",
+    "author": " Akretion, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "external_dependencies": {"python": [], "bin": []},
+    "depends": ["storage_thumbnail", "web_kanban"],
+    "data": [
+        "views/storage_image_view.xml",
+        "views/js.xml",
+        "data/ir_parameter.xml",
+        "security/res_group.xml",
+        "security/ir.model.access.csv",
+        "security/ir_rule.xml",
+    ],
+    "demo": [],
+    "qweb": ["static/src/xml/custom_xml.xml"],
+}
---
+++ packages/odoo10-addon-storage-image/odoo10_addon_storage_image-10.0.1.1.0-py2-none-any.whl/odoo/addons/storage_image/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from . import storage_image
+from . import storage_file
+from . import image_relation_abstract
