f2f6582ead89afcde4e4f701eb462bb9a2f3311b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:45 +0000

    Add package tsuru-hm/0.6.18/tsuru_hm-0.6.18-py2-none-any.whl

---
+++ packages/tsuru-hm/tsuru_hm-0.6.18-py2-none-any.whl/hm/managers/cloudstack.py
@@ -0,0 +1,180 @@
+# Copyright 2015 hm authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+from hm import managers
+from hm.model import host
+from hm.iaas.cloudstack_client import CloudStack
+
+
+class CloudStackManager(managers.BaseManager):
+
+    def __init__(self, config=None):
+        super(CloudStackManager, self).__init__(config)
+        url = self.get_conf("CLOUDSTACK_API_URL")
+        key = self.get_conf("CLOUDSTACK_API_KEY")
+        secret_key = self.get_conf("CLOUDSTACK_SECRET_KEY")
+        self.client = CloudStack(url, key, secret_key)
+        self.max_tries = int(self.get_conf("CLOUDSTACK_MAX_TRIES", 100))
+
+    def create_host(self, name=None, alternative_id=0):
+        group = self.get_conf("CLOUDSTACK_GROUP", "")
+        user_data = self.get_user_data()
+        if group and name:
+            name = "{}_{}".format(group, name)
+        elif not name:
+            name = group
+        data = {
+            "group": group,
+            "displayname": name,
+            "templateid": self._get_alternate_conf("CLOUDSTACK_TEMPLATE_ID", alternative_id),
+            "zoneid": self._get_alternate_conf("CLOUDSTACK_ZONE_ID", alternative_id),
+            "serviceofferingid": self._get_alternate_conf("CLOUDSTACK_SERVICE_OFFERING_ID", alternative_id),
+        }
+        if user_data:
+            data["userdata"] = self.client.encode_user_data(user_data)
+        project_id = self._get_alternate_conf("CLOUDSTACK_PROJECT_ID", alternative_id, None)
+        if project_id:
+            data["projectid"] = project_id
+        network_ids = self._get_alternate_conf("CLOUDSTACK_NETWORK_IDS", alternative_id, None)
+        if network_ids:
+            data["networkids"] = network_ids
+        vm_job = self.client.deployVirtualMachine(data)
+        if not vm_job.get("jobid"):
+            raise CloudStackException(
+                "unexpected response from deployVirtualMachine({}), expected jobid key, got: {}".format(
+                    repr(data), repr(vm_job)))
+        vm = self._wait_for_unit(vm_job, project_id)
+        tags = self.get_conf("HOST_TAGS", "")
+        if tags:
+            self.tag_vm(tags.split(","), vm["id"], project_id)
+        return host.Host(id=vm["id"], dns_name=self._get_dns_name(vm), alternative_id=alternative_id)
+
+    def tag_vm(self, tag_list, vm_id, project_id=None):
+        list_tags_params = {"resourcetype": "UserVm", "resourceid": vm_id}
+        delete_tags_params = {"resourcetype": "UserVm", "resourceids": vm_id}
+        add_tags_params = {"resourcetype": "UserVm", "resourceids": vm_id}
+        if project_id:
+            list_tags_params['projectid'] = project_id
+            add_tags_params["projectid"] = project_id
+            delete_tags_params['projectid'] = project_id
+        machine_tags = self.client.listTags(list_tags_params)
+        tag_add_count = 0
+        tag_del_count = 0
+        for tag in tag_list:
+            ignore_tag_key = False
+            parts = tag.split(":", 1)
+            if len(parts) < 2:
+                continue
+            key, value = parts
+            if 'tag' in machine_tags:
+                for m_tag in machine_tags['tag']:
+                    if key == m_tag['key'] and value != m_tag['value']:
+                        delete_tags_params.update({"tags[{}].key".format(tag_del_count): m_tag['key'],
+                                                   "tags[{}].value".format(tag_del_count): m_tag['value']})
+                        tag_del_count += 1
+                    if key == m_tag['key'] and value == m_tag['value']:
+                        ignore_tag_key = True
+            if value != '' and not ignore_tag_key:
+                add_tags_params["tags[{}].key".format(tag_add_count)] = key
+                add_tags_params["tags[{}].value".format(tag_add_count)] = value
+                tag_add_count += 1
+        if any(item.startswith('tags') for item in delete_tags_params.keys()):
+            job = self.client.deleteTags(delete_tags_params)
+            self.client.wait_for_job(job["jobid"], self.max_tries)
+        if not any(item.startswith('tags') for item in add_tags_params.keys()):
+            return
+        self.client.createTags(add_tags_params)
+
+    def destroy_host(self, host_id):
+        self.client.destroyVirtualMachine({"id": host_id})
+
+    def start_host(self, host_id):
+        self.client.startVirtualMachine({"id": host_id})
+
+    def stop_host(self, host_id, forced=False):
+        forced_stop = "false"
+        if forced:
+            forced_stop = "true"
+        job = self.client.stopVirtualMachine({"id": host_id, "forced": forced_stop})
+        self.client.wait_for_job(job["jobid"], self.max_tries)
+
+    def scale_host(self, host_id):
+        service_offering_id = self._get_alternate_conf("CLOUDSTACK_SERVICE_OFFERING_ID", 0)
+        if not service_offering_id:
+            raise Exception("scale_host: no CLOUDSTACK_SERVICE_OFFERING_ID defined to scale up instance")
+        data = {"id": host_id}
+        project_id = self._get_alternate_conf("CLOUDSTACK_PROJECT_ID", 0, None)
+        if project_id:
+            data["projectid"] = project_id
+        try:
+            vm = self.client.listVirtualMachines(data)
+            current_offering = vm["virtualmachine"][0]['serviceofferingid']
+        except:
+            raise Exception("scale_host: machine {} not found".format(host_id))
+        if current_offering != service_offering_id:
+            job = self.client.scaleVirtualMachine({"id": host_id, "serviceofferingid": service_offering_id})
+            self.client.wait_for_job(job["jobid"], self.max_tries)
+
+    def restore_host(self, host_id, reset_template=False, reset_tags=False, alternative_id=0):
+        restore_args = {'virtualmachineid': host_id}
+        list_tags_params = {"resourcetype": "UserVm", "resourceid": host_id}
+        project_id = self._get_alternate_conf("CLOUDSTACK_PROJECT_ID", 0, None)
+        if project_id:
+            list_tags_params['projectid'] = project_id
+        tags = None
+        if reset_template:
+            template_id = self._get_alternate_conf("CLOUDSTACK_TEMPLATE_ID", alternative_id)
+            restore_args['templateid'] = template_id
+        if reset_tags:
+            tags = self.get_conf("HOST_TAGS", "")
+            if tags:
+                current_tags = self.client.listTags(list_tags_params)
+                if 'tag' in current_tags:
+                    self.tag_vm(tags.split(","), host_id, project_id)
+                else:
+                    raise CloudStackException('''unexpected response from listTags on restore_host: {}
+                                              '''.format(current_tags))
+        try:
+            vm_job = self.client.make_request('restoreVirtualMachine', restore_args,
+                                              response_key='restorevmresponse')
+            self._wait_for_unit(vm_job, project_id)
+        except Exception as e:
+            if reset_tags and tags:
+                current_tags = ["{}:{}".format(tag['key'], tag['value']) for tag in current_tags['tag']]
+                self.tag_vm(current_tags, host_id, project_id)
+            raise CloudStackException(
+                "unexpected response from restoreVirtualMachine({}), expected jobid key, got: {} ({})".format(
+                    repr(restore_args), repr(vm_job), repr(e)))
+
+    def _get_dns_name(self, vm):
+        if not vm.get("nic"):
+            return ""
+        network_index = int(self.get_conf("CLOUDSTACK_PUBLIC_NETWORK_INDEX", 0))
+        dns_name = vm["nic"][network_index]["ipaddress"]
+        return dns_name
+
+    def _wait_for_unit(self, vm_job, project_id):
+        result = self.client.wait_for_job(vm_job["jobid"], self.max_tries)
+        if vm_job.get("id"):
+            data = {"id": vm_job["id"]}
+        else:
+            data = {"id": result['jobresult']['virtualmachine']['id']}
+        if project_id:
+            data["projectid"] = project_id
+        vms = self.client.listVirtualMachines(data)
+        return vms["virtualmachine"][0]
+
+    def _get_alternate_conf(self, name, alternative_id, default=None):
+        env_var = "{}_{}".format(name, alternative_id)
+        val = self.get_conf(env_var, default)
+        if val is not None:
+            return val
+        return self.get_conf(name, default)
+
+
+class CloudStackException(Exception):
+    pass
+
+
+managers.register('cloudstack', CloudStackManager)
---
+++ packages/tsuru-hm/tsuru_hm-0.6.18-py2-none-any.whl/tests/model/test_host.py
@@ -0,0 +1,301 @@
+# Copyright 2014 hm authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+import unittest
+
+from hm import managers, storage
+from hm.model.host import Host
+from mock import patch, call
+
+
+class FakeManager(managers.BaseManager):
+    def __init__(self, config=None):
+        super(FakeManager, self).__init__(config)
+
+    def create_host(self, name=None, alternative_id=0):
+        host_id = self.get_conf('HOST_ID')
+        alternative_id_failures = self.get_conf('ALTERNATIVE_IDS_FAILURES', '')
+        if str(alternative_id) in alternative_id_failures.split(","):
+            raise Exception("failure to create on alternative_id {}".format(alternative_id))
+        return Host(id=host_id, dns_name="{}.{}.com".format(host_id, name), alternative_id=alternative_id)
+
+    def destroy_host(self, id):
+        if id == "explode":
+            raise Exception("failure to destroy")
+
+    def restore_host(self, id, reset_template=False, reset_tags=False):
+        if id == "explode":
+            raise Exception("failure to restore")
+
+    def tag_vm(self, tags, id, projectid=None):
+        if id == "explode":
+            raise Exception("failure to add tag")
+
+    def stop_host(self, id, forced=False):
+        if id == "explode":
+            raise Exception("failure to stop")
+
+    def start_host(self, id):
+        if id == "explode":
+            raise Exception("failure to start")
+
+    def scale_host(self, id):
+        if id == "explode":
+            raise Exception("failure to scale")
+
+managers.register('fake', FakeManager)
+
+
+class HostTestCase(unittest.TestCase):
+
+    def setUp(self):
+        storage.MongoDBStorage()._hosts_collection().remove()
+
+    def test_create(self):
+        conf = {"HOST_ID": "fake-id"}
+        host = Host.create('fake', 'my-group', conf)
+        self.assertEqual(host.id, "fake-id")
+        self.assertEqual(host.dns_name, "fake-id.my-group.com")
+        self.assertEqual(host.manager, "fake")
+        self.assertEqual(host.group, "my-group")
+        self.assertEqual(host.config, conf)
+        self.assertEqual(host.alternative_id, 0)
+        db_host = Host.find('fake-id', conf=conf)
+        self.assertEqual(db_host.id, "fake-id")
+        self.assertEqual(db_host.dns_name, "fake-id.my-group.com")
+        self.assertEqual(db_host.manager, "fake")
+        self.assertEqual(db_host.group, "my-group")
+        self.assertEqual(db_host.config, conf)
+        self.assertEqual(db_host.alternative_id, 0)
+
+    def test_create_alternatives(self):
+        conf = {"HM_ALTERNATIVE_CONFIG_COUNT": "3"}
+        conf.update({"HOST_ID": "fake-1"})
+        host = Host.create('fake', 'my-group', conf)
+        self.assertEqual(host.alternative_id, 0)
+        conf.update({"HOST_ID": "fake-2"})
+        host = Host.create('fake', 'my-group', conf)
+        self.assertEqual(host.alternative_id, 1)
+        conf.update({"HOST_ID": "fake-3"})
+        host = Host.create('fake', 'my-group', conf)
+        self.assertEqual(host.alternative_id, 2)
+        conf.update({"HOST_ID": "fake-4"})
+        host = Host.create('fake', 'my-group', conf)
+        self.assertEqual(host.alternative_id, 0)
+        conf.update({"HOST_ID": "fake-5"})
+        host = Host.create('fake', 'my-group', conf)
+        self.assertEqual(host.alternative_id, 1)
+        conf.update({"HOST_ID": "fake-6"})
+        host = Host.create('fake', 'another-group', conf)
+        self.assertEqual(host.alternative_id, 0)
+        hosts = Host.list(filters={'alternative_id': 0})
+        self.assertEqual(len(hosts), 3)
+        hosts = Host.list(filters={'alternative_id': 1})
+        self.assertEqual(len(hosts), 2)
+        hosts = Host.list(filters={'alternative_id': 2})
+        self.assertEqual(len(hosts), 1)
+
+    def test_create_alternatives_ignore_failure_and_try_next(self):
+        conf = {"HM_ALTERNATIVE_CONFIG_COUNT": "4", "ALTERNATIVE_IDS_FAILURES": "1,2"}
+        conf.update({"HOST_ID": "fake-1"})
+        host = Host.create('fake', 'my-group', conf)
+        self.assertEqual(host.alternative_id, 0)
+        conf.update({"HOST_ID": "fake-2"})
+        host = Host.create('fake', 'my-group', conf)
+        self.assertEqual(host.alternative_id, 3)
+        conf.update({"HOST_ID": "fake-3"})
+        host = Host.create('fake', 'my-group', conf)
+        self.assertEqual(host.alternative_id, 0)
+        conf.update({"HOST_ID": "fake-4"})
+        host = Host.create('fake', 'my-group', conf)
+        self.assertEqual(host.alternative_id, 3)
+        conf.update({"HOST_ID": "fake-5"})
+        host = Host.create('fake', 'my-group', conf)
+        self.assertEqual(host.alternative_id, 0)
+        conf.update({"HOST_ID": "fake-6"})
+        host = Host.create('fake', 'another-group', conf)
+        self.assertEqual(host.alternative_id, 0)
+        hosts = Host.list(filters={'alternative_id': 0})
+        self.assertEqual(len(hosts), 4)
+        hosts = Host.list(filters={'alternative_id': 1})
+        self.assertEqual(len(hosts), 0)
+        hosts = Host.list(filters={'alternative_id': 2})
+        self.assertEqual(len(hosts), 0)
+        hosts = Host.list(filters={'alternative_id': 3})
+        self.assertEqual(len(hosts), 2)
+        conf = {"HM_ALTERNATIVE_CONFIG_COUNT": "4", "ALTERNATIVE_IDS_FAILURES": ""}
+        conf.update({"HOST_ID": "fake-7"})
+        host = Host.create('fake', 'my-group', conf)
+        self.assertEqual(host.alternative_id, 1)
+        conf.update({"HOST_ID": "fake-8"})
+        host = Host.create('fake', 'my-group', conf)
+        self.assertEqual(host.alternative_id, 2)
+        conf.update({"HOST_ID": "fake-9"})
+        host = Host.create('fake', 'my-group', conf)
+        self.assertEqual(host.alternative_id, 1)
+        conf.update({"HOST_ID": "fake-10"})
+        host = Host.create('fake', 'my-group', conf)
+        self.assertEqual(host.alternative_id, 2)
+        conf.update({"HOST_ID": "fake-11"})
+        host = Host.create('fake', 'my-group', conf)
+        self.assertEqual(host.alternative_id, 1)
+        conf.update({"HOST_ID": "fake-12"})
+        host = Host.create('fake', 'another-group', conf)
+        self.assertEqual(host.alternative_id, 1)
+        hosts = Host.list(filters={'alternative_id': 0})
+        self.assertEqual(len(hosts), 4)
+        hosts = Host.list(filters={'alternative_id': 1})
+        self.assertEqual(len(hosts), 4)
+        hosts = Host.list(filters={'alternative_id': 2})
+        self.assertEqual(len(hosts), 2)
+        hosts = Host.list(filters={'alternative_id': 3})
+        self.assertEqual(len(hosts), 2)
+
+    def test_create_alternatives_raise_last_error_when_no_alternatives_left(self):
+        conf = {"HM_ALTERNATIVE_CONFIG_COUNT": "4", "ALTERNATIVE_IDS_FAILURES": "0,1,2,3"}
+        conf.update({"HOST_ID": "fake-1"})
+        with self.assertRaises(Exception) as create_exception:
+            Host.create('fake', 'my-group', conf)
+        self.assertEqual(str(create_exception.exception), "failure to create on alternative_id 3")
+
+    def test_destroy(self):
+        host = Host.create('fake', 'my-group', {"HOST_ID": "fake-id"})
+        self.assertEqual(host.id, "fake-id")
+        host.destroy()
+        db_host = Host.find('fake-id')
+        self.assertIsNone(db_host)
+
+    @patch("hm.log.error")
+    def test_destroy_ignores_manager_error(self, log):
+        host = Host.create('fake', 'my-group', {"HOST_ID": "explode"})
+        self.assertEqual(host.id, "explode")
+        host.destroy()
+        self.assertEqual(log.call_args, call("Error trying to destroy host 'explode' "
+                                             "in 'fake': failure to destroy"))
+        db_host = Host.find('explode')
+        self.assertIsNone(db_host)
+
+    def test_restore(self):
+        host = Host.create('fake', 'my-group', {"HOST_ID": "fake-id"})
+        self.assertEqual(host.id, "fake-id")
+        host.restore()
+
+    def test_tag_vm(self):
+        host = Host.create('fake', 'my-group', {"HOST_ID": "fake-id"})
+        self.assertEqual(host.id, "fake-id")
+        tags = ['a:b', 'c:d']
+        host.tag_vm(tags)
+
+    @patch("hm.log.error")
+    def test_restore_log_and_raises_exception_on_error(self, log):
+        host = Host.create('fake', 'my-group', {"HOST_ID": "explode"})
+        self.assertEqual(host.id, "explode")
+        self.assertRaises(Exception, host.restore)
+        self.assertEqual(log.call_args, call("Error trying to restore host 'explode' "
+                                             "in 'fake': failure to restore"))
+        db_host = Host.find('explode')
+        self.assertEqual(db_host.id, "explode")
+
+    def test_stop(self):
+        host = Host.create('fake', 'my-group', {"HOST_ID": "fake-id"})
+        self.assertEqual(host.id, "fake-id")
+        host.stop()
+
+    @patch("hm.log.error")
+    def test_stop_log_and_raises_exception_on_error(self, log):
+        host = Host.create('fake', 'my-group', {"HOST_ID": "explode"})
+        self.assertEqual(host.id, "explode")
+        self.assertRaises(Exception, host.stop)
+        self.assertEqual(log.call_args, call("Error trying to stop host 'explode' "
+                                             "in 'fake': failure to stop"))
+        db_host = Host.find('explode')
+        self.assertEqual(db_host.id, "explode")
+
+    def test_scale(self):
+        host = Host.create('fake', 'my-group', {"HOST_ID": "fake-id"})
+        self.assertEqual(host.id, "fake-id")
+        host.scale()
+
+    @patch("hm.log.error")
+    def test_scale_log_and_raises_exception_on_error(self, log):
+        host = Host.create('fake', 'my-group', {"HOST_ID": "explode"})
+        self.assertEqual(host.id, "explode")
+        self.assertRaises(Exception, host.scale)
+        self.assertEqual(log.call_args, call("Error trying to scale host 'explode' "
+                                             "in 'fake': failure to scale"))
+        db_host = Host.find('explode')
+        self.assertEqual(db_host.id, "explode")
+
+    def test_start(self):
+        host = Host.create('fake', 'my-group', {"HOST_ID": "fake-id"})
+        self.assertEqual(host.id, "fake-id")
+        host.start()
+
+    @patch("hm.log.error")
+    def test_start_log_and_raises_exception_on_error(self, log):
+        host = Host.create('fake', 'my-group', {"HOST_ID": "explode"})
+        self.assertEqual(host.id, "explode")
+        self.assertRaises(Exception, host.start)
+        self.assertEqual(log.call_args, call("Error trying to start host 'explode' "
+                                             "in 'fake': failure to start"))
+        db_host = Host.find('explode')
+        self.assertEqual(db_host.id, "explode")
+
+    def test_list(self):
+        h1 = Host.create('fake', 'my-group1', {"HOST_ID": "fake-id-1"})
+        h2 = Host.create('fake', 'my-group1', {"HOST_ID": "fake-id-2"})
+        h3 = Host.create('fake', 'my-group2', {"HOST_ID": "fake-id-3"})
+        conf = {'MY_CONF': 1}
+        hosts = Host.list(conf=conf)
+        self.assertDictEqual(hosts[0].config, conf)
+        self.assertDictEqual(hosts[1].config, conf)
+        self.assertDictEqual(hosts[2].config, conf)
+        self.assertItemsEqual([h.to_json() for h in hosts], [h1.to_json(), h2.to_json(), h3.to_json()])
+        hosts = Host.list({'group': 'my-group1'})
+        self.assertItemsEqual([h.to_json() for h in hosts], [h1.to_json(), h2.to_json()])
+
+    def test_storage_use_database_conf(self):
+        storage.MongoDBStorage({"MONGO_DATABASE": "alternative_host_manager"})._hosts_collection().remove()
+        h1 = Host.create('fake', 'my-group1', {
+            "HOST_ID": "fake-id-x", "MONGO_DATABASE": "alternative_host_manager"
+        })
+        stor = h1.storage()
+        self.assertEqual(stor.mongo_database, "alternative_host_manager")
+        self.assertEqual(stor.db.name, "alternative_host_manager")
+        h1.destroy()
+        db_host = Host.find('fake-id-x')
+        self.assertIsNone(db_host)
+
+    def test_storage_use_uri_conf(self):
+        conf = {
+            "DBAAS_MONGODB_ENDPOINT": "mongodb://127.0.0.1:27017/some_other_db",
+            "MONGO_URI": "mongodb://127.0.0.1:27017/ignored",
+        }
+        storage.MongoDBStorage(conf)._hosts_collection().remove()
+        host_config = {
+            "HOST_ID": "fake-id-x"
+        }
+        host_config.update(conf)
+        h1 = Host.create('fake', 'my-group1', host_config)
+        stor = h1.storage()
+        self.assertEqual(stor.mongo_database, "some_other_db")
+        self.assertEqual(stor.db.name, "some_other_db")
+        h1.destroy()
+        db_host = Host.find('fake-id-x')
+        self.assertIsNone(db_host)
+        conf = {
+            "MONGO_URI": "mongodb://127.0.0.1:27017/now_used",
+        }
+        storage.MongoDBStorage(conf)._hosts_collection().remove()
+        host_config = {
+            "HOST_ID": "fake-id-x"
+        }
+        host_config.update(conf)
+        h1 = Host.create('fake', 'my-group1', host_config)
+        stor = h1.storage()
+        self.assertEqual(stor.mongo_database, "now_used")
+        self.assertEqual(stor.db.name, "now_used")
+        h1.destroy()
+        db_host = Host.find('fake-id-x')
+        self.assertIsNone(db_host)
