be1b40dee44f1000c25e6b4c43af93deb8226d1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:18 +0000

    Add package kartingpros/0.1.0/kartingpros-0.1.0-py3.7_1.egg

---
+++ packages/kartingpros/kartingpros-0.1.0-py3.7_1.egg/src/__main__.py
@@ -0,0 +1,11 @@
+import main as m
+
+
+def main():
+    m.main()
+    # pygame.init()
+    # window = screen.Screen()
+    # display_surface = window.get_display()
+    # mainmenu.main_menu(display_surface)
+# def main():
+#     print('asdf')
