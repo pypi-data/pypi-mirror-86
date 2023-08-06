8aea2653194f0f85bf8b1c263430597d170a98bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:17 +0000

    Add package cfunits/3.3.1/cfunits-3.3.1.tar.gz

---
+++ packages/cfunits/cfunits-3.3.1.tar.gz/cfunits-3.3.1/cfunits/test/run_tests.py
@@ -0,0 +1,62 @@
+from __future__ import print_function
+
+import unittest
+import os
+from random import choice, shuffle
+import sys
+
+import cftime
+import numpy
+import datetime
+import cfunits
+
+from platform import system, platform, python_version
+
+
+def randomise_test_order(*_args):
+    '''Return a random choice from 1 or -1.
+
+    When set as the test loader method for standard (merge)sort comparison
+    to order all methods in a test case (see 'sortTestMethodsUsing'), ensures
+    they run in a (semi-)random order, meaning implicit reliance on setup or
+    state, i.e. test dependencies, should become evident over repeated runs.
+    '''
+    return choice([1, -1])
+
+
+test_loader = unittest.TestLoader
+# Randomise the order to run the specific test_Units_* methods
+test_loader.sortTestMethodsUsing = randomise_test_order
+
+# Build the test suite from the tests found in the test files.
+testsuite = unittest.TestSuite()
+testsuite.addTests(test_loader().discover(
+    os.path.dirname(os.path.realpath(__file__)), pattern='test_*.py'))
+
+
+# Run the test suite.
+def run_test_suite(verbosity=2):
+    runner = unittest.TextTestRunner(verbosity=verbosity)
+    outcome = runner.run(testsuite)
+    # Note unittest.TextTestRunner().run() does not set an exit code, so (esp.
+    # for CI / GH Actions workflows) we need $? = 1 set if any sub-test fails:
+    if not outcome.wasSuccessful():
+        exit(1)  # else is zero for sucess as standard
+
+
+if __name__ == '__main__':
+    print('------------------')
+    print('CFUNITS TEST SUITE')
+    print('------------------')
+    print('Run date:', datetime.datetime.now())
+    print('Platform:', str(platform()))
+    print('python:', str(python_version() + ' ' + str(sys.executable)))
+    print('cftime version:', cftime.__version__)
+    print('numpy version:', numpy.__version__)
+    print('cfunits version:', cfunits.__version__)
+    print('cfunits path:', os.path.abspath(cfunits.__file__))
+    print('')
+    print('Running tests from', os.path.abspath(os.curdir))
+    print('')
+
+    run_test_suite()
---
+++ packages/cfunits/cfunits-3.3.1.tar.gz/cfunits-3.3.1/setup.py
@@ -0,0 +1,104 @@
+from distutils.core import setup, Extension
+from distutils.command.build import build
+import os
+import fnmatch
+import sys
+import imp
+import re
+
+def find_package_data_files(directory):
+    for root, dirs, files in os.walk(directory):
+        for basename in files:
+            if fnmatch.fnmatch(basename, '*'):
+                filename = os.path.join(root, basename)
+                yield filename.replace('cfunits/', '', 1)
+
+def _read(fname):
+    """Returns content of a file.
+
+    """
+    fpath = os.path.dirname(__file__)
+    fpath = os.path.join(fpath, fname)
+    with open(fpath, 'r') as file_:
+        return file_.read()
+
+def _get_version():
+    """Returns library version by inspecting __init__.py file.
+
+    """
+    return re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                     _read("cfunits/__init__.py"),
+                     re.MULTILINE).group(1)
+      
+version      = _get_version()
+packages     = ['cfunits']
+etc_files    = [f for f in find_package_data_files('cfunits/etc')]
+test_files    = [f for f in find_package_data_files('cfunits/test')]
+
+package_data = etc_files + test_files
+
+#with open('README.md') as ldfile:
+#    long_description = ldfile.read()
+
+long_description = """*A python interface to UNIDATA's UDUNITS-2 library with CF
+extensions*
+
+**Note: Versions 3.0.0 and later are only compatible with version Python 3. Use version 1.9 for Python 2 compatibility.**
+
+Store, combine and compare physical units and convert numeric values
+to different units.
+
+Units are as defined in `UNIDATA's UDUNITS-2 library
+<http://www.unidata.ucar.edu/software/udunits/>`_, except for
+reference time units (such as ``'days since 2000-12-1'`` in the
+``'proleptic_gregorian'`` calendar), which are handled by the `cftime
+python package <https://pypi.python.org/pypi/cftime>`_.
+
+In addition, some units are either new to, modified from, or removed
+from the standard UDUNITS-2 database in order to be more consistent
+with the `CF conventions <http://cfconventions.org/>`_.
+
+Documentation
+=============
+
+https://ncas-cms.github.io/cfunits
+
+Installation
+============
+
+https://ncas-cms.github.io/cfunits/installation.html
+
+"""
+
+# classifiers list at: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+
+# Get dependencies
+requirements = open('requirements.txt', 'r')
+install_requires = requirements.read().splitlines() 
+
+setup(name = "cfunits",
+      long_description = long_description,
+      version      = version,
+      description  = "A python interface to UNIDATA's UDUNITS-2 package with CF extensions ",
+      maintainer   = "David Hassell",
+      author       = "David Hassell",
+      maintainer_email = "david.hassell@ncas.ac.uk",
+      author_email = "david.hassell@ncas.ac.uk",
+      url          = "https://bitbucket.org/cfpython/cfunits-python",
+      download_url = "https://bitbucket.org/cfpython/cfunits-python/downloads",
+      platforms    = ["Linux", "MacOS"],
+      license      = ["MIT"],
+      keywords     = ['cf', 'udunits', 'UNIDATA', 'netcdf','data',
+                      'science', 'oceanography', 'meteorology', 'climate'],
+      classifiers  = ["Development Status :: 5 - Production/Stable",
+                      "Intended Audience :: Science/Research", 
+                      "License :: OSI Approved :: MIT License", 
+                      "Topic :: Scientific/Engineering",
+                      "Operating System :: MacOS",
+                      "Operating System :: POSIX :: Linux",
+                      "Programming Language :: Python :: 3",],
+      packages     = ['cfunits'],
+      package_data = {'cfunits': package_data},
+      python_requires='>=3.6',
+      install_requires = install_requires,
+  )
