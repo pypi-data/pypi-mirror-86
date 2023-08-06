42e6a1ce7b794c722d813ac8a50c93b55e7a0e53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:50 +0000

    Add package writer-cm/1.0.0/writer_cm-1.0.0-py3-none-any.whl

---
+++ packages/writer-cm/writer_cm-1.0.0-py3-none-any.whl/writer_cm/__init__.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+from contextlib import contextmanager
+from pathlib import Path
+from stat import S_IRUSR
+from stat import S_IRWXG
+from stat import S_IRWXU
+from stat import S_IWUSR
+from tempfile import TemporaryDirectory
+from typing import Generator
+from typing import Union
+
+from atomicwrites import move_atomic
+from atomicwrites import replace_atomic
+
+
+__all__ = ["writer_cm"]
+__version__ = "1.0.0"
+
+
+@contextmanager
+def writer_cm(
+    destination: Union[Path, str],
+    *,
+    overwrite: bool = False,
+    dir_perms: int = S_IRWXU | S_IRWXG,
+    file_perms: int = S_IRUSR | S_IWUSR,
+) -> Generator[Path, None, None]:
+    destination = Path(destination).expanduser().resolve()
+    parent = destination.parent
+    parts = parent.parts
+    for idx, _ in enumerate(parts):
+        path_parent = Path(*parts[: (idx + 1)])
+        try:
+            path_parent.mkdir(mode=dir_perms)
+        except (FileExistsError, IsADirectoryError, PermissionError):
+            pass
+    name = destination.name
+    with TemporaryDirectory(suffix=".tmp", prefix=name, dir=parent) as temp_dir:
+        source = Path(temp_dir).joinpath(name)
+        yield source
+        if overwrite:
+            replace_atomic(str(source), str(destination))
+        else:
+            move_atomic(str(source), str(destination))
+        destination.chmod(file_perms)
