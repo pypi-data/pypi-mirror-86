455354d3d93138e8ad25976fe92f9511ed6aca59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:54 +0000

    Add package ClickReaction/0.3.1/ClickReaction-0.3.1-py3-none-any.whl

---
+++ packages/ClickReaction/ClickReaction-0.3.1-py3-none-any.whl/ClickReaction/Reactions/AmideCoupling.py
@@ -0,0 +1,52 @@
+from ClickReaction.BaseReaction import BaseReaction, Reactant, Reactants
+
+
+class AmideCoupling(BaseReaction):
+    """
+    Amide coupling reaction to form amides.
+
+    Does not work with anilines.
+
+    amine + carboxylic acid -> Amide
+    """
+
+    reactant_names = ["amine", "acid"]
+
+    def __init__(self, amine: Reactant, acid: Reactant):
+        self.set_reactants({
+            "amine": amine,
+            "acid": acid,
+        })
+
+    def __runReaction__(self, reactants: Reactants):
+        return self._rdReaction.RunReactants((reactants["amine"], reactants["acid"]))
+
+
+smarts = """
+    [
+        $([NX3H3]),
+        $([NX4H4]),
+        $([NX3H2]-[CX4]),
+        $([NX4H3]-[CX4]),
+        $([NX3H1](-[CX4])(-[CX4])),
+        $([NX4H2](-[CX4])(-[CX4]))
+    :1]
+
+    .
+    
+    [C:2]
+        (=[OX1:3])
+        -[
+            $([OX2]-N1C(=O)CCC1(=O)),
+            $([OX2]-c1c(-F)c(-F)c(-F)c(-F)c1(-F)),
+            $([OX2]-c1ccc(-N(~O)(~O))cc1),
+            $([OX2H1]),
+            $([O-X1])
+        ]
+    
+    >>
+    
+    [*+0:1]-[*:2](=[*:3])
+"""
+
+AmideCoupling.set_reaction_smarts(smarts)
---
+++ packages/ClickReaction/ClickReaction-0.3.1-py3-none-any.whl/ClickReaction/Reactions/BocRemoval.py
@@ -0,0 +1,33 @@
+from ClickReaction.BaseReaction import BaseReaction, Reactant, Reactants
+
+
+class BocRemoval(BaseReaction):
+    """
+    Removal of Boc from amines
+
+    amine-boc -> amine
+    """
+
+    reactant_names = ["bocamine"]
+
+    def __init__(self, bocamine: Reactant):
+        self.set_reactants({
+            "bocamine": bocamine,
+        })
+
+    def __runReaction__(self, reactants: Reactants):
+        return self._rdReaction.RunReactants((reactants["bocamine"], ))
+
+
+smarts = """
+    [#7:1]
+    -C(=O)
+    -O
+    -[$(C(-[CH3])(-[CH3])(-[CH3]))]
+
+    >>
+
+    [*:1]
+"""
+
+BocRemoval.set_reaction_smarts(smarts)
