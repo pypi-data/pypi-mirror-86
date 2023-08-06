e43b8898bbb27d6976e98d80a8b0d8c3fb6ac08d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:04 +0000

    Add package r2g/1.0.5/r2g-1.0.5.tar.gz

---
+++ packages/r2g/r2g-1.0.5.tar.gz/r2g-1.0.5/src/r2g/__init__.py
@@ -0,0 +1,22 @@
+from datetime import date
+
+
+__banner__ = r"""
+               ___
+          ____|_  ___ _
+         / __/ __/ _ `/
+        /_/ /____\_, /
+ Reads to Genes /___/
+"""
+
+__title__ = "r2g"
+__full_name__ = "Reads to Genes"
+__description__ = "A homology-based, computationally lightweight pipeline " \
+                  "for discovering genes in the absence of an assembly"
+__url__ = "https://yangwu91.github.io/r2g/"
+__version__ = "1.0.5"
+__author__ = "Yang Wu"
+__maintainer__ = "Yang Wu"
+__author_email__ = "wuyang@drwu.ga"
+__license__ = "MIT Licence"
+__copyright__ = "Copyright {} Yang Wu".format(date.today().year)
