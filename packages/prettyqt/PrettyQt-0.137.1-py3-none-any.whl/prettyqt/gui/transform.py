3de3fb482aa34673bc17205cb5921c572c7d65cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:55 +0000

    Add package prettyqt/0.137.1/PrettyQt-0.137.1-py3-none-any.whl

---
+++ packages/prettyqt/PrettyQt-0.137.1-py3-none-any.whl/prettyqt/gui/transform.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+from typing import Tuple
+
+from qtpy import QtGui
+
+from prettyqt import core
+
+
+class Transform(QtGui.QTransform):
+    def __getstate__(self):
+        return core.DataStream.create_bytearray(self)
+
+    def __setstate__(self, ba):
+        self.__init__()
+        core.DataStream.write_bytearray(ba, self)
+
+    def __getitem__(self, value: Tuple[int, int]) -> float:
+        if value[0] == 0:
+            if value[1] == 0:
+                return self.m11()
+            elif value[1] == 1:
+                return self.m12()
+            elif value[1] == 2:
+                return self.m13()
+        elif value[0] == 1:
+            if value[1] == 0:
+                return self.m21()
+            elif value[1] == 1:
+                return self.m22()
+            elif value[1] == 2:
+                return self.m23()
+        elif value[0] == 2:
+            if value[1] == 0:
+                return self.m31()
+            elif value[1] == 1:
+                return self.m32()
+            elif value[1] == 2:
+                return self.m33()
+        raise ValueError(f"Wrong value {value}")
+
+    def __bytes__(self):
+        ba = core.DataStream.create_bytearray(self)
+        return bytes(ba)
+
+
+if __name__ == "__main__":
+    transform = Transform()
+    print(transform[0, 0])
