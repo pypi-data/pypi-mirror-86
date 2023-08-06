2493fe7cc14156bd06f46aa59c786bd3b5b3de25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:19 +0000

    Add package quantconnect-stubs/9861/quantconnect-stubs-9861.tar.gz

---
+++ packages/quantconnect-stubs/quantconnect-stubs-9861.tar.gz/quantconnect-stubs-9861/QuantConnect/Lean/Launcher/__init__.pyi
@@ -0,0 +1,14 @@
+import typing
+
+import QuantConnect.Lean.Launcher
+import System
+
+
+class Program(System.Object):
+    """This class has no documentation."""
+
+    @staticmethod
+    def Main(args: typing.List[str]) -> None:
+        ...
+
+
