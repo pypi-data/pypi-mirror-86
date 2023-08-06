375589d74d6944d7f6fe1124fc99576618658569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:19 +0000

    Add package joulescope/0.9.3/joulescope-0.9.3-cp37-cp37m-win_amd64.whl

---
+++ packages/joulescope/joulescope-0.9.3-cp37-cp37m-win_amd64.whl/joulescope/version.py
@@ -0,0 +1,10 @@
+__version__ = "0.9.3"
+
+__title__ = "joulescope"
+__description__ = 'Joulescope™ host driver and utilities'
+__url__ = 'https://joulescope.readthedocs.io'
+__author__ = 'Jetperch LLC'
+__author_email__ = 'joulescope-dev@jetperch.com'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2018-2020 Jetperch LLC'
+
