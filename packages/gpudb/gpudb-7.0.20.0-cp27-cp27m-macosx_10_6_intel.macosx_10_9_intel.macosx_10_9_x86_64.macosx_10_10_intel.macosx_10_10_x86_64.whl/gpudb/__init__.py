9cc69944d0e07f319393f5163e7c25e5fcfff239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:00 +0000

    Add package gpudb/7.0.20.0/gpudb-7.0.20.0-cp27-cp27m-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl

---
+++ packages/gpudb/gpudb-7.0.20.0-cp27-cp27m-macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl/gpudb/__init__.py
@@ -0,0 +1,46 @@
+import sys
+
+# C-extension package for avro encoding
+from .protocol import RecordColumn
+from .protocol import RecordType
+from .protocol import Record
+
+
+if (sys.version_info[0] == 3):  # checking the major component
+    from gpudb.gpudb import GPUdb
+    from gpudb.gpudb import GPUdbException
+    from gpudb.gpudb import GPUdbConnectionException
+    from gpudb.gpudb import GPUdbRecordColumn
+    from gpudb.gpudb import GPUdbRecordType
+    from gpudb.gpudb import GPUdbRecord
+    from gpudb.gpudb import GPUdbColumnProperty
+    from gpudb.gpudb import GPUdbTable
+    from gpudb.gpudb import GPUdbTableIterator
+    from gpudb.gpudb import GPUdbTableOptions
+
+    from gpudb.gpudb import AttrDict
+
+    from gpudb.gpudb_multihead_io import GPUdbWorkerList, GPUdbIngestor, InsertionException, RecordRetriever
+
+    from gpudb.gpudb_table_monitor import GPUdbTableMonitor
+
+    from gpudb.gpudb import collections
+else:
+    from gpudb import GPUdb
+    from gpudb import GPUdbException
+    from gpudb import GPUdbConnectionException
+    from gpudb import GPUdbRecordColumn
+    from gpudb import GPUdbRecordType
+    from gpudb import GPUdbRecord
+    from gpudb import GPUdbColumnProperty
+    from gpudb import GPUdbTable
+    from gpudb import GPUdbTableIterator
+    from gpudb import GPUdbTableOptions
+
+    from gpudb import AttrDict
+
+    from gpudb_multihead_io import GPUdbWorkerList, GPUdbIngestor, InsertionException, RecordRetriever
+
+    from gpudb_table_monitor import GPUdbTableMonitor
+
+    from gpudb import collections
