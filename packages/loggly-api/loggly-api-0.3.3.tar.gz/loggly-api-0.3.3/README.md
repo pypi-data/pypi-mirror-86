a148985f6dd3bbbfceab3eba13f72448df33bc22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:34 +0000

    Add package loggly-api/0.3.3/loggly-api-0.3.3.tar.gz

---
+++ packages/loggly-api/loggly-api-0.3.3.tar.gz/loggly-api-0.3.3/README.md
@@ -0,0 +1,205 @@
+# loggly-api
+
+## Description
+
+This library allows a user to work with Loggly's APIs through simple function calls.
+
+**Note:** As of version 0.3.0, this library only supports Loggly's API v2 as described
+[here](https://documentation.solarwinds.com/en/Success_Center/loggly/Content/admin/api-overview.htm).
+Additionally, only API tokens are supported as an authentication type. Username/password (HTTP Basic
+Auth) support has been ripped out. Other breaking changes have also been implemented, such as the
+change of the `loggly.token` variable to `loggly.customer_token` to be consistent with Loggly's
+terminology. `RequestError`s no longer represent HTTP responses. They are now called `ResponseError`s.
+A `RequestError` is now something completely different, and is documented below. `InvalidStatError`
+no longer exists, and has been converted into a `RequestError`.
+
+**If your code relies on these features and you cannot update your code, install version 0.2.3 of
+this library.**
+
+## Installation
+
+    pip install loggly-api
+
+You're done.
+
+Unless you need an old version, in which case, you should try:
+
+    pip install loggly-api<0.3.0
+
+## Usage
+
+    import loggly
+
+You can set various access credentials; different ones are needed for different operations.
+
+    loggly.subdomain = 'your_subdomain'
+    loggly.customer_token = 'your_customer_token'
+    loggly.api_token = 'your_api_token'
+
+### [Ship One Event, or a Multiline Event](https://documentation.solarwinds.com/en/Success_Center/loggly/Content/admin/http-endpoint.htm)
+
+_You need a customer token to ship logs (as opposed to an API token)._
+
+The Content-Type header of the request will be automatically determined, so you can ship JSON by
+passing in a dict or a JSON-parseable string. Anything else gets treated as plaintext.
+
+    loggly.submit('Event data')       # Plaintext
+    loggly.submit({ 'foo': 'bar' })   # JSON
+    loggly.submit('{ "foo": "bar" }') # JSON
+
+All forms of `submit` support tagging the events by setting the `tag=whatever` argument.
+
+    loggly.submit('Event data', tag='production')
+
+### [Ship Bulk Data](https://documentation.solarwinds.com/en/Success_Center/loggly/Content/admin/http-bulk-endpoint.htm)
+
+_You need a customer token to ship logs._
+
+You can pass in a list of events...
+
+    loggly.bulk_submit([
+        'Event one',
+        'Event two',
+        'Event three'])
+
+ ...or a single string.
+
+    with open('/path/to/a/big/log.file', 'r') as fh:
+        loggly.bulk_submit(fh.read())
+
+Both forms support tagging.
+
+    loggly.bulk_submit(eventlist, tag='production')
+
+Bear in mind there are restrictions to bulk shipping. At the time of this writing, you can't send
+single events larger than 1 MB each, and no more than 5 MB per request.
+
+### [Search Events](https://documentation.solarwinds.com/en/Success_Center/loggly/Content/admin/api-retrieving-data.htm)
+
+_To search events, you need a subdomain and an API token._
+
+**Note:** _Loggly has deprecated their single-block retrieval API endpoint, and this library does not
+support it. It doesn't matter, though, since the library obscures the method of retrieval and only
+presents the outcome of the search._
+
+You can start a search like this, and the process will hang until search results are returned by the
+API, at which point you'll have a list of events:
+
+    events = loggly.search('foo:"bar"', frm='-1D', til='now')
+
+Or you can set `paginate=True` to instead get a SearchIterator back immediately. Each "next" call
+will cause a request for more results to come from Loggly. This is essentially a less-automated way
+of accomplishing the same thing as above, except you can run other code between pages of results.
+Consider:
+
+    search = loggly.search('foo:"bar"', frm='-1D', til='now', paginate=True, pagesize=100)
+    events = list()
+    for page in search:
+        events.extend(page)
+        print('{} events collected so far!'.format(len(events)))
+
+`pagesize` is the number of results to return per page. Loggly limits this to 1,000 events at most,
+but this can sometimes cause problems as well. If you find yourself getting inexplicable errors from
+Loggly, try making your page size smaller. The effects of this setting are not always obvious when
+`paginate` is `False`, although the page size **is** applied in that case, fewer API calls are made,
+and the function may return faster. But when it's `True`, you'll get back that many events at a time
+from the iterator.
+
+### [Event Count](https://documentation.solarwinds.com/en/Success_Center/loggly/Content/admin/event-counts.htm)
+
+_To count events, you need a subdomain and an API token._
+
+Event counts support the same querying and time-boxing options as searching. It also supports the
+`include_volume` option, which reports the byte size of the events queried. This function call
+returns a dictionary that always contains a `count` field, and which also contains a `volume`
+field if you've asked for it. For example:
+
+    counts = loggly.count('foo:"bar"', frm='-1D', til='now', include_volume=True)
+
+    >>> {'count': 2574134, 'volume': 3095600785}
+
+### [Account Info](https://documentation.solarwinds.com/en/Success_Center/loggly/Content/admin/api-account-info.htm)
+
+_Retrieving account info requires a subdomain and an API token._
+
+    info = loggly.account_info()
+
+Calling `account_info()` also updates some convenience properties in the module:
+
+    loggly.customer_tokens
+    loggly.retention
+    loggly.subdomain
+    loggly.volume_limit
+
+### [Statistics](https://documentation.solarwinds.com/en/Success_Center/loggly/Content/admin/stats-api.htm)
+
+_The stats API requires a subdomain and an API token._
+
+You can call the `stats` function to get statistical data about numerical fields in your log events.
+In the fictional example below, you'd get back the average request time for your app.
+
+    stats = loggly.stats(stat='avg', field='json.log.request_time', query='*', frm=None, til=None)
+
+Refer to the [documentation](https://documentation.solarwinds.com/en/Success_Center/loggly/Content/admin/stats-api.htm)
+for a complete list of valid statistics you can call for.
+
+### Volume Metrics
+
+_The volume metrics API requires a subdomain and an API token._
+
+First of all, this doesn't appear to be a documented feature. I've recieved a PDF of docs from an account representative that I can't find a corresponding website for.
+
+This call lets you gather broad aggregate statistics about the number of events and their total size within a period of time, filtered by the host and "app" that generated the events. The following call would return the stats from the past day.
+
+    metrics = loggly.volume_metrics(frm='-1d', til='now')
+
+Other parameters that are supported:
+
+* group_by (str)
+* host (str)
+* app (str)
+* measurement_types (list of str)
+
+## Exceptions
+
+The base class for all exceptions in this module is `loggly.LogglyError`, but it's nothing special
+and is useless except to verify that some other error is a loggly error.
+
+    print(isinstance(loggly.RequestError, loggly.LogglyError))
+
+    Output:
+      True
+
+A `ResponseError` is thrown anytime this module makes a request to Loggly and gets something other
+than a `200 OK` in response. It will contain a `requests` response object as well as some
+convenience members which are extracted from it:
+
+    try:
+        stats = loggly.stats(field='json.log.bleh')
+    except loggly.ResponseError as e:
+        print('{} {}\n{}'.format(e.status_code, e.reason, e.message))
+
+    Output:
+     400 BAD REQUEST
+     The requested field 'json.log.bleh' is unknown.
+
+A `RequestError`, by contrast, is thrown when the library is able to detect a problem in the
+formation of a request before the request is made. It contains a `reason` field which will contain
+a plaintext explanation of the problem.
+
+    try:
+        loggly.bulk_submit({'adict': 'is invalid here'})
+    except loggly.RequestError as e:
+      print(e.reason)
+
+    Output:
+      events must be a list or string
+
+An `AuthenticationError` is raised when you try to make an API call without providing sufficient
+credentials for the operation.
+
+    try:
+        loggly.subdomain = 'mysub'
+        events = loggly.search()
+    except AuthenticationError as e:
+        print('You didn't set an api_token!')
---
+++ packages/loggly-api/loggly-api-0.3.3.tar.gz/loggly-api-0.3.3/loggly/__init__.py
@@ -0,0 +1,289 @@
+import json
+import requests
+
+# Auth data
+customer_token = None
+api_token = None
+
+# Account info
+customer_tokens = None
+retention = None
+subdomain = None
+volume_limit = None
+
+# Exception related stuff
+class LogglyError(Exception):
+    '''
+    Base exception with no function outside of identifying the error as one from
+    this library.
+    '''
+    pass
+
+class AuthenticationError(LogglyError):
+    '''
+    Exception related to authentication with the API
+    '''
+    pass
+
+class RequestError(LogglyError):
+    '''
+    Exception that gets raised when the library is able to detect a request
+    formation error before the request gets made.
+    '''
+    def __init__(self, reason):
+        self.reason = reason
+
+class ResponseError(LogglyError):
+    '''
+    Exception that gets raised when an API call results in a bad status code
+    '''
+    def __init__(self, response):
+        self.response = response
+        self.message = response.text
+        self.status_code = response.status_code
+        self.reason = response.reason
+
+# Internally used functions
+def get_auth():
+    '''
+    Returns an appropriate HTTP header for API token authentication
+    '''
+
+    if api_token is None: return None
+    return { 'Authorization': 'Bearer {}'.format(api_token) }
+
+def call_api(path, method='GET', params=None):
+    '''
+    Calls the API at the given path, using the given method and query parameters
+    '''
+
+    auth = get_auth()
+    if auth:
+        url = 'https://{}.loggly.com{}'.format(subdomain, path)
+        response = requests.request(method, url, headers=auth, params=params)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise ResponseError(response)
+    else:
+        raise AuthenticationError()
+
+def get_next_id_from_url(url):
+    '''
+    The Loggly search pagination endpoint returns a "next" field which contains
+    a full URL. Our call_api function wants just the "next" page ID. This
+    function extracts that ID so we can take advantage of the error handling and
+    other features of the call_api function.
+    '''
+
+    try:
+        query_str = url.split('?')[1]
+    except KeyError:
+        return None
+    params = query_str.split('&')
+    for param in params:
+        if param[:5] == 'next=':
+            return param[5:]
+    return None
+
+class SearchIterator(object):
+    '''
+    An iterator that handles event search pagination
+    '''
+
+    def __init__(self, params):
+        self.params = params
+        self.next = None
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        '''
+        Get the next page or raise a StopIteration exception
+        '''
+
+        params = dict()
+        if self.next == '':
+            raise StopIteration
+        elif self.next is not None:
+            self.params['next'] = self.next
+
+        try:
+            response = call_api('/apiv2/events/iterate', params=self.params)
+        except ResponseError:
+            raise
+
+        if 'next' in response:
+            self.next = get_next_id_from_url(response['next'])
+        else:
+            self.next = ''
+        return response['events'] if 'events' in response else list()
+
+# User-intended functions
+def account_info():
+    '''
+    Retrieves account-level metadata and stores it in some convenience variables
+    '''
+
+    global customer_tokens, subdomain, retention, volume_limit
+
+    response = call_api('/apiv2/customer')
+    customer_tokens = response['tokens']
+    subdomain = response['subdomain']
+    retention = response['subscription']['retention_days']
+    volume_limit = response['subscription']['volume_limit_mb']
+    return response
+
+def submit(event, tag=None):
+    '''
+    Ships a single event to Loggly. `tag` is an optional tag to apply to the event.
+    Autodetects what content type to use based on the contnet of `event`.
+    '''
+
+    # Don't bother with anything else if we're not authenticated
+    if not customer_token:
+        raise AuthenticationError()
+
+    # This feature of Loggly's API uses a different URL entirely, so we do not use
+    # the call_api convenience function here.
+    url = 'https://logs-01.loggly.com/inputs/{}'.format(customer_token)
+    if tag:
+        url += '/tag/{}'.format(tag)
+
+    # Detect event format
+    if isinstance(event, dict):
+        event = json.dumps(event)
+        content_type = 'application/x-www-form-urlencoded'
+    elif isinstance(event, str):
+        try:
+            json.loads(event)
+            content_type = 'application/x-www-form-urlencoded'
+        except ValueError:
+            content_type = 'text/plain'
+
+    response = requests.post(
+        url,
+        headers={'Content-Type': content_type},
+        data=event)
+
+    if response.status_code == 200:
+        return True
+    else:
+        raise ResponseError(response)
+
+def bulk_submit(events, tag=None):
+    '''
+    Ships multiple events in a single request. `tag` is an optional tag to apply
+    to the events. `events` is a Python list.
+
+    Loggly places limitations on the size of this data. At the time of this
+    writing, that's 1 MB per event and 5 MB per HTTP request.
+
+    https://documentation.solarwinds.com/en/Success_Center/loggly/Content/admin/http-bulk-endpoint.htm
+    '''
+
+    if not customer_token:
+        raise AuthenticationError()
+
+    url = 'https://logs-01.loggly.com/bulk/{}'.format(customer_token)
+    if tag:
+        url += '/tag/{}'.format(tag)
+
+    if isinstance(events, list):
+        events = '\n'.join(events)
+    elif not isinstance(events, str):
+        raise RequestError('events must be a list or string')
+
+    response = requests.post(
+        url,
+        headers={'Content-Type': 'text/plain'},
+        data=events)
+
+    if response.status_code == 200:
+        return True
+    else:
+        raise ResponseError(response)
+
+def count(query=None, frm=None, til=None, include_volume=None):
+    params = dict()
+    if query: params['q'] = query
+    if frm: params['from'] = frm
+    if til: params['until'] = til
+    if include_volume: params['include_volume'] = 'true'
+
+    count = call_api('/apiv2/events/count', params=params)
+    return count
+
+def search(query=None, frm=None, til=None, paginate=False, pagesize=None, order=None):
+    # Events Retrieval API: https://www.loggly.com/docs/paginating-event-retrieval-api/
+
+    if order and order not in [ 'asc', 'desc' ]:
+        raise RequestError('Search order must be "asc" or "desc"')
+
+    events = list()
+    params = dict()
+    if query: params['q'] = query
+    if frm: params['from'] = frm
+    if til: params['until'] = til
+    if pagesize: params['size'] = pagesize
+    if order: params['order'] = order
+
+    searcher = SearchIterator(params)
+    if paginate:
+        return searcher
+    else:
+        for page in searcher:
+            events.extend(page)
+        return events
+
+def stats(stat='all', field=None, query='*', frm=None, til=None):
+    if not field:
+        raise RequestError('For stats calls, "field" is required')
+
+    # Validate the stat field
+    stat = stat.lower()
+    if stat not in [
+        'avg',
+        'sum',
+        'min',
+        'max',
+        'percentiles',
+        'value_count',
+        'cardinality',
+        'stats',
+        'extended',
+        'all' ]:
+            raise RequestError('{} is not a valid stats option'.format(stat))
+
+    params = dict()
+    if query: params['q'] = query
+    if frm: params['from'] = frm
+    if til: params['until'] = til
+
+    path = '/apiv2/stats/{}'.format(stat)
+    if field: path = '{}/{}'.format(path, field)
+    return call_api(path, params=params)
+
+def volume_metrics(frm=None, til=None, group_by=None, host=None, app=None, measurement_types=None):
+    valid_group_bys = [ 'host', 'app' ]
+    valid_measurement_types = [ 'volume_bytes', 'count' ]
+    if group_by:
+        for gp in group_by:
+            if gp not in valid_group_bys:
+                return False
+    if measurement_types:
+        for mt in measurement_types:
+            if mt not in valid_measurement_types:
+                return False
+
+    params = dict()
+    if frm: params['from'] = frm
+    if til: params['until'] = til
+    if group_by: params['group_by'] = ','.join(group_by)
+    if host: params['host'] = host
+    if app: params['app'] = app
+    if measurement_types: params['measurement_types'] = ','.join(measurement_types)
+
+    return call_api('/apiv2/volume-metrics', params=params)
