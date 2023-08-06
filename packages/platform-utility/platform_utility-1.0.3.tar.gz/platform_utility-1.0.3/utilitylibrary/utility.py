5544407f86f84424cbb9cce679bedcd073303c0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:56 +0000

    Add package platform-utility/1.0.3/platform_utility-1.0.3.tar.gz

---
+++ packages/platform-utility/platform_utility-1.0.3.tar.gz/platform_utility-1.0.3/utilitylibrary/utility.py
@@ -0,0 +1,8 @@
+'''
+    @num_page number of page
+    @max_rows max_rows for query in one page.
+'''
+def setstartend(num_page,max_rows):
+    start    = (int(num_page)-1)*max_rows
+    end      = int(num_page)*max_rows
+    return {'start':start,'end':end}
