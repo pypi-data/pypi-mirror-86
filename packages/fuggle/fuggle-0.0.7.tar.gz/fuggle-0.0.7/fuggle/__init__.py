7e656c46c138cd01af5540fc31ae0ce5e29159aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:43 +0000

    Add package fuggle/0.0.7/fuggle-0.0.7.tar.gz

---
+++ packages/fuggle/fuggle-0.0.7.tar.gz/fuggle-0.0.7/fuggle/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+from fuggle_version import __version__
+
+from fuggle._setup import setup
+from fuggle.execution_engine import (
+    KaggleNativeExecutionEngine,
+    KaggleSparkExecutionEngine,
+)
+from fuggle.outputters import Plot, PlotBar, PlotBarH, PlotLine
