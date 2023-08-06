e43843509c7399674e14e69d52602e1275e024b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:19 +0000

    Add package merger/0.2/merger-0.2.tar.gz

---
+++ packages/merger/merger-0.2.tar.gz/merger-0.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+
+def generate_requirements():
+    requirements = []
+    with open("requirements.txt", "r") as f:
+        requirements = f.read().split("\n")
+
+    return requirements
+
+
+setup(
+    name="merger",
+    author="Nicholas Young",
+    author_email="nick@bolted.io",
+    description="Package used for merging branches, squashing them and saying who reviewed them",
+    version="0.2",
+    py_modules=["main"],
+    install_requires=[
+        "click==7.1.2",
+        "click-completion==0.5.2",
+        "colored==1.4.2",
+        "gitdb==4.0.5",
+        "GitPython==3.1.11",
+        "Jinja2==2.11.2",
+        "MarkupSafe==1.1.1",
+        "prompt-toolkit==3.0.8",
+        "pyfiglet==0.8.post1",
+        "sh==1.14.1",
+        "shellingham==1.3.2",
+        "six==1.15.0",
+        "smmap==3.0.4",
+        "wcwidth==0.2.5",
+    ],
+    scripts=["./bin/merger"],
+)
