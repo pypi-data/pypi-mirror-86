06eb7b4d99f75be64dd743f247e2d756916eba61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:15 +0000

    Add package itk-phasesymmetry/1.0.4/itk_phasesymmetry-1.0.4-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/itk-phasesymmetry/itk_phasesymmetry-1.0.4-cp38-cp38-macosx_10_9_x86_64.whl/itk/Configuration/PhaseSymmetryConfig.py
@@ -0,0 +1,28 @@
+depends = ('ITKPyBase', 'ITKImageSources', 'ITKImageIntensity', 'ITKFFT', 'ITKCommon', )
+templates = (
+  ('SinusoidSpatialFunction', 'itk::SinusoidSpatialFunction', 'itkSinusoidSpatialFunctionF2PD2', True, 'float,2,itk::Point< double,2 >'),
+  ('SinusoidSpatialFunction', 'itk::SinusoidSpatialFunction', 'itkSinusoidSpatialFunctionD2PD2', True, 'double,2,itk::Point< double,2 >'),
+  ('SinusoidSpatialFunction', 'itk::SinusoidSpatialFunction', 'itkSinusoidSpatialFunctionF3PD3', True, 'float,3,itk::Point< double,3 >'),
+  ('SinusoidSpatialFunction', 'itk::SinusoidSpatialFunction', 'itkSinusoidSpatialFunctionD3PD3', True, 'double,3,itk::Point< double,3 >'),
+  ('SinusoidImageSource', 'itk::SinusoidImageSource', 'itkSinusoidImageSourceIF2', True, 'itk::Image< float,2 >'),
+  ('SinusoidImageSource', 'itk::SinusoidImageSource', 'itkSinusoidImageSourceIF3', True, 'itk::Image< float,3 >'),
+  ('SinusoidImageSource', 'itk::SinusoidImageSource', 'itkSinusoidImageSourceID2', True, 'itk::Image< double,2 >'),
+  ('SinusoidImageSource', 'itk::SinusoidImageSource', 'itkSinusoidImageSourceID3', True, 'itk::Image< double,3 >'),
+  ('SteerableFilterFreqImageSource', 'itk::SteerableFilterFreqImageSource', 'itkSteerableFilterFreqImageSourceIF2', True, 'itk::Image< float,2 >'),
+  ('SteerableFilterFreqImageSource', 'itk::SteerableFilterFreqImageSource', 'itkSteerableFilterFreqImageSourceIF3', True, 'itk::Image< float,3 >'),
+  ('SteerableFilterFreqImageSource', 'itk::SteerableFilterFreqImageSource', 'itkSteerableFilterFreqImageSourceID2', True, 'itk::Image< double,2 >'),
+  ('SteerableFilterFreqImageSource', 'itk::SteerableFilterFreqImageSource', 'itkSteerableFilterFreqImageSourceID3', True, 'itk::Image< double,3 >'),
+  ('ButterworthFilterFreqImageSource', 'itk::ButterworthFilterFreqImageSource', 'itkButterworthFilterFreqImageSourceIF2', True, 'itk::Image< float,2 >'),
+  ('ButterworthFilterFreqImageSource', 'itk::ButterworthFilterFreqImageSource', 'itkButterworthFilterFreqImageSourceIF3', True, 'itk::Image< float,3 >'),
+  ('ButterworthFilterFreqImageSource', 'itk::ButterworthFilterFreqImageSource', 'itkButterworthFilterFreqImageSourceID2', True, 'itk::Image< double,2 >'),
+  ('ButterworthFilterFreqImageSource', 'itk::ButterworthFilterFreqImageSource', 'itkButterworthFilterFreqImageSourceID3', True, 'itk::Image< double,3 >'),
+  ('LogGaborFreqImageSource', 'itk::LogGaborFreqImageSource', 'itkLogGaborFreqImageSourceIF2', True, 'itk::Image< float,2 >'),
+  ('LogGaborFreqImageSource', 'itk::LogGaborFreqImageSource', 'itkLogGaborFreqImageSourceIF3', True, 'itk::Image< float,3 >'),
+  ('LogGaborFreqImageSource', 'itk::LogGaborFreqImageSource', 'itkLogGaborFreqImageSourceID2', True, 'itk::Image< double,2 >'),
+  ('LogGaborFreqImageSource', 'itk::LogGaborFreqImageSource', 'itkLogGaborFreqImageSourceID3', True, 'itk::Image< double,3 >'),
+  ('PhaseSymmetryImageFilter', 'itk::PhaseSymmetryImageFilter', 'itkPhaseSymmetryImageFilterIF2IF2', True, 'itk::Image< float,2 >, itk::Image< float,2 >'),
+  ('PhaseSymmetryImageFilter', 'itk::PhaseSymmetryImageFilter', 'itkPhaseSymmetryImageFilterIF3IF3', True, 'itk::Image< float,3 >, itk::Image< float,3 >'),
+  ('PhaseSymmetryImageFilter', 'itk::PhaseSymmetryImageFilter', 'itkPhaseSymmetryImageFilterID2ID2', True, 'itk::Image< double,2 >, itk::Image< double,2 >'),
+  ('PhaseSymmetryImageFilter', 'itk::PhaseSymmetryImageFilter', 'itkPhaseSymmetryImageFilterID3ID3', True, 'itk::Image< double,3 >, itk::Image< double,3 >'),
+)
+snake_case_functions = ('log_gabor_freq_image_source', 'phase_symmetry_image_filter', 'sinusoid_image_source', 'butterworth_filter_freq_image_source', 'steerable_filter_freq_image_source', )
