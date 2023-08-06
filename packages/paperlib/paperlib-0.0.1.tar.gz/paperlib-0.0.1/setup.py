5d606846aa5fbdba23eeddc265a2848bb39d4d71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:52 +0000

    Add package paperlib/0.0.1/paperlib-0.0.1.tar.gz

---
+++ packages/paperlib/paperlib-0.0.1.tar.gz/paperlib-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='paperlib',
+    version='0.0.1',
+    description='Manage your academic paper library by markdowns',
+    license='Apache 2.0',
+)
+
+
