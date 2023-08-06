24c4069301edccdbf8dc5c9cd954b1c7f8cf5ccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:57 +0000

    Add package BinaryClassificationMetrics/0.0.4/BinaryClassificationMetrics-0.0.4.tar.gz

---
+++ packages/BinaryClassificationMetrics/BinaryClassificationMetrics-0.0.4.tar.gz/BinaryClassificationMetrics-0.0.4/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(
+  name='BinaryClassificationMetrics',        
+  packages = ['BinaryClassificationMetrics'],   
+  version = '0.0.4',      
+  license='MIT',        
+  description='This package contains various binary classification metric methods.',  
+  author = 'Varun V',                  
+  author_email = 'varunofficial99@gmail.com',      
+  url = 'https://github.com/VarunV991/Binary-Classification-Metrics',  
+  download_url = 'https://github.com/VarunV991/Binary-Classification-Metrics/archive/v0.0.3.tar.gz',   
+  keywords = ['binary-classification', 'metrics'], 
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',     
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3.6',      
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
