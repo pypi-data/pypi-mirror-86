e4b8aefe1574e7ec0023b1d113c0b89b7daabc87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:48 +0000

    Add package fastcdc/1.4.2/fastcdc-1.4.2-cp36-cp36m-manylinux2010_x86_64.whl

---
+++ packages/fastcdc/fastcdc-1.4.2-cp36-cp36m-manylinux2010_x86_64.whl/fastcdc/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import click
+
+
+try:
+    from fastcdc.fastcdc_cy import fastcdc_cy as fastcdc
+except ImportError:
+    from fastcdc.fastcdc_py import fastcdc_py as fastcdc
+
+    click.secho("Running in pure python mode (slow)", fg="bright_magenta")
+
+__version__ = "1.4.2"
