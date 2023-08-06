b30cb77f5e4d4ee46ba87f3c01715fff06ec4d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:05 +0000

    Add package cli-config-manager/1.0.0/cli-config-manager-1.0.0.tar.gz

---
+++ packages/cli-config-manager/cli-config-manager-1.0.0.tar.gz/cli-config-manager-1.0.0/src/cli_config_manager/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '1.0.0'
+
+from .manager import Config
+
+__all__ = [
+    "Config"
+]
---
+++ packages/cli-config-manager/cli-config-manager-1.0.0.tar.gz/cli-config-manager-1.0.0/tests/test_cli_config_manager.py
@@ -0,0 +1,6 @@
+
+from cli_config_manager import Config
+
+
+def test_main():
+    Config("test")
