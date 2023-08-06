c8f51fcaed4fbfdc69b66b2d9eccff91e812cdf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:47 +0000

    Add package django-data-browser/3.0.3/django-data-browser-3.0.3.tar.gz

---
+++ packages/django-data-browser/django-data-browser-3.0.3.tar.gz/django-data-browser-3.0.3/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+./build_fe.sh
+
+git add data_browser/fe_build data_browser/templates/data_browser/index.html README.rst data_browser/__init__.py
+
+./build_whl.sh
+
+version=$(python -c "import data_browser; print(data_browser.version)")
+set +x
+echo SUCCESS
+echo
+echo "To release run the following:"
+echo "    git commit -m $version"
+echo "    git tag -a -m $version $version"
+echo "    git push --follow-tags"
+echo "    python -m twine upload -u __token__ dist/*"
