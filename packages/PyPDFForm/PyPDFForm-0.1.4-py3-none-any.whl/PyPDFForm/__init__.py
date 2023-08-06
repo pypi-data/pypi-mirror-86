cbda9694934c51176ccd05f6318529c9b3e6a3fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:19 +0000

    Add package PyPDFForm/0.1.4/PyPDFForm-0.1.4-py3-none-any.whl

---
+++ packages/PyPDFForm/PyPDFForm-0.1.4-py3-none-any.whl/PyPDFForm/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from .exceptions import (InvalidEditableParameterError, InvalidFontSizeError,
+                         InvalidFormDataError, InvalidImageCoordinateError,
+                         InvalidImageDimensionError, InvalidImageError,
+                         InvalidImageRotationAngleError, InvalidModeError,
+                         InvalidPageNumberError, InvalidTemplateError,
+                         InvalidTextOffsetError, InvalidWrapLengthError)
+from .wrapper import PyPDFForm
