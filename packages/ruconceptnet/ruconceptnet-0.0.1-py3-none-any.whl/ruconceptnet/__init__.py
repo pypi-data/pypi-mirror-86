6defb85ef93f86aa7ac2cfdc5be6daa50e611218
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:59 +0000

    Add package ruconceptnet/0.0.1/ruconceptnet-0.0.1-py3-none-any.whl

---
+++ packages/ruconceptnet/ruconceptnet-0.0.1-py3-none-any.whl/ruconceptnet/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from . import meta
+from .conceptnet import ConceptNet
+
+__version__ = meta.version
+__author__ = meta.authors[0]
+__license__ = meta.license
+__copyright__ = meta.copyright
