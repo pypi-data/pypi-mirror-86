47cd7324977ab153c4020101d20103a41b831ff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:38 +0000

    Add package abilian-core/0.11.16/abilian_core-0.11.16-py3-none-any.whl

---
+++ packages/abilian-core/abilian_core-0.11.16-py3-none-any.whl/abilian/web/resources/js/widgets/tags.js
@@ -0,0 +1,28 @@
+require(["AbilianWidget", "jquery"], function(Abilian, $) {
+  "use strict";
+
+  function initTagsSelect(params) {
+    var opts = {
+      multiple: true,
+      separator: ";",
+    };
+    $.extend(opts, params);
+
+    // as of Select2 3.5, we cannot use a <select> and
+    // createSearchChoices. We must convert it to a hidden input
+    var values = (this.val() || []).join(opts.separator);
+    var choices = $.map(this.get(0).options, function(option) {
+      return option.value;
+    });
+    var input = $('<input type="hidden" />')
+      .attr({ name: this.attr("name") })
+      .val(values);
+
+    opts.tags = choices;
+    input.insertBefore(this);
+    this.remove();
+    return Abilian.getWidgetCreator("select2").call(input, opts);
+  }
+
+  Abilian.registerWidgetCreator("tags-select", initTagsSelect);
+});
