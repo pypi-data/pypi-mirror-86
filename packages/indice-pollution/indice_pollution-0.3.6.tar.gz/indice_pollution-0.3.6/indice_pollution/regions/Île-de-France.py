07beefae7ecc55dc45d43df01ec2a43f42e5873e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:04 +0000

    Add package indice-pollution/0.3.6/indice_pollution-0.3.6.tar.gz

---
+++ packages/indice-pollution/indice_pollution-0.3.6.tar.gz/indice_pollution-0.3.6/indice_pollution/regions/Île-de-France.py
@@ -0,0 +1,57 @@
+from . import ForecastMixin, EpisodeMixin
+from datetime import datetime, timedelta
+from dateutil.parser import parse
+import requests
+from flask import current_app
+
+class Service(object):
+    website = 'https://www.airparif.asso.fr/'
+
+class Forecast(ForecastMixin, Service):
+    url = 'https://services8.arcgis.com/gtmasQsdfwbDAQSQ/arcgis/rest/services/ind_idf_agglo/FeatureServer/0/query'
+
+    insee_list = ['75056']
+
+    INDICE_TO_QUALIF = {
+        0: "tres_bon",
+        1: "tres_bon",
+        2: "bon",
+        3: "bon",
+        4: "bon",
+        5: "moyen",
+        6: "mediocre",
+        7: "mediocre",
+        8: "mediocre",
+        9: "mauvais",
+        10: "tres_mauvais"
+    }
+
+    def where(self, date_, insee):
+        return f"date_ech >= CURRENT_DATE - INTERVAL '2' DAY"
+    
+    def get_close_insee(self, insee):
+        return insee
+
+    def get_from_scraping(self, previous_results, date_, insee):
+        r = requests.get('https://www.airparif.asso.fr/')
+        r.raise_for_status()
+        soup = BeautifulSoup(r.text, 'html.parser')
+
+        data =  soup.find_all('div', class_='indices_data')[0].find_all('div')
+        indice = int(data[1].text)
+        qualif = self.INDICE_TO_QUALIF[indice]
+
+        return previous_results + [
+            {"date": str(date_), "valeur": indice, "indice": indice, "qualif": qualif}
+        ]
+
+class Episode(EpisodeMixin, Service):
+    url = 'https://services8.arcgis.com/gtmasQsdfwbDAQSQ/arcgis/rest/services/alrt_idf/FeatureServer/0/query'
+
+    def params(self, insee, date_):
+        return {
+            'where': '1=1',
+            'outFields': '*',
+            'outSR': '4326',
+            'f': 'json'
+        }
