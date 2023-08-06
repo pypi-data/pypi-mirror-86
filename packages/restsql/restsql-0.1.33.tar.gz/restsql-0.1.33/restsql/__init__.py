1848317a824dd0d6072f927703063c6d57140d42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:37 +0000

    Add package restsql/0.1.33/restsql-0.1.33.tar.gz

---
+++ packages/restsql/restsql-0.1.33.tar.gz/restsql-0.1.33/restsql/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+
+from restsql import RestSqlClient
+from dbsetting import EnumDataBase, DbSetting, db_settings
+from table import Table, NumberField, StringField, BoolField, IntField
