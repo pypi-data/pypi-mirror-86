df3c665df1ab121ac0ff0d027f625cc6adb24497
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:03 +0000

    Add package distributions-bk/0.1/distributions_bk-0.1.tar.gz

---
+++ packages/distributions-bk/distributions_bk-0.1.tar.gz/distributions_bk-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_bk',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_bk'],
+      zip_safe=False)
