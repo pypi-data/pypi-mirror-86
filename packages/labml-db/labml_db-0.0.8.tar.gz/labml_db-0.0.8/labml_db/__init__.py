6fb4ecbcbcc3a8985cb255830eb7afcddf9461d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:32 +0000

    Add package labml-db/0.0.8/labml_db-0.0.8.tar.gz

---
+++ packages/labml-db/labml_db-0.0.8.tar.gz/labml_db-0.0.8/labml_db/__init__.py
@@ -0,0 +1,5 @@
+from .index import Index
+from .model import Model, Key
+from .serializer import Serializer
+
+__all__ = [Serializer, Model, Key, Index]
---
+++ packages/labml-db/labml_db-0.0.8.tar.gz/labml_db-0.0.8/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("readme.rst", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='labml_db',
+    version='0.0.8',
+    author="Varuna Jayasiri, Nipun Wijerathne",
+    author_email="vpjayasiri@gmail.com, hnipun@gmail.com",
+    description="Minimalistic ORM for JSON/YAML/Pickle file based DB",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/lab-ml/db",
+    project_urls={
+        'Documentation': 'https://lab-ml.com/'
+    },
+    packages=setuptools.find_packages(exclude=('test',
+                                               'test.*')),
+    install_requires=['pyyaml>=5.3.1',
+                      'redis'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='database,json,yaml',
+)
