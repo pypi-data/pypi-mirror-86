809cd3aece021a89edab613778d01ab58f15662c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:22 +0000

    Add package xbox-remote/1.0.5/xbox-remote-1.0.5.tar.gz

---
+++ packages/xbox-remote/xbox-remote-1.0.5.tar.gz/xbox-remote-1.0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+setup(
+    name="xbox-remote",
+    version="1.0.5",
+    description="A Python package to connect to xbox and interface with its buttons and joysticks",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/FRC4564/Xbox",
+    author="Steven Jacobs",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7"
+    ],
+    packages=["xbox"],
+    include_package_data=True,
+)
