6995bcceb49059a229a0890c81b8ce25a8de1106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:11 +0000

    Add package collection-scanner/0.4.1/collection_scanner-0.4.1.tar.gz

---
+++ packages/collection-scanner/collection_scanner-0.4.1.tar.gz/collection_scanner-0.4.1/collection_scanner/tests/__init__.py
@@ -0,0 +1,93 @@
+"""
+utils for mocking hubstorage collection
+"""
+from operator import itemgetter
+from copy import deepcopy
+
+
+class FakeCollection(object):
+    def __init__(self, name, samples, return_less=0):
+        """
+        name is the collection name
+        samples is a list of tuples (key, record dict)
+        return_less is a parameter to simulate situation in which HS server returns less records than requested, even
+            if end of collection hasn't been reached. It just returns the given number less records than requested by
+            count.
+        """
+        self.colname = name
+        self.samples = sorted(samples, key=itemgetter(0))
+        self.return_less = return_less
+        self.base_time = 1441940400000 # 2015-09-11
+        self.timestamps = {}
+        for key, _ in self.samples:
+            self._get_basetime(key) # populate timestamps
+
+    def _must_issue_record(self, key, **kwargs):
+        prefix = kwargs.get('prefix')
+        retval = prefix is None or key.startswith(tuple(prefix))
+        startafter = kwargs.get('startafter') or b''
+        start = kwargs.get('start') or b''
+        if isinstance(startafter, list):
+            startafter = startafter[0] or b''
+        if isinstance(start, list):
+            start = start[0]
+        # start nulifies startafter
+        if start:
+            startafter = b''
+        endts = kwargs.get('endts')
+        retval = retval and key >= start and key > startafter and (not endts or self._get_basetime(key) < endts)
+        return retval
+
+    def _get_basetime(self, key):
+        if key not in self.timestamps:
+            self.timestamps[key] = self.base_time
+            self.base_time += 3600000 # each record separated by one hour
+        return self.timestamps[key]
+
+    def get(self, **kwargs):
+        if not self.samples:
+            raise KeyError(None)
+        include_key = b'_key' in kwargs.get('meta', {})
+        include_ts = b'_ts' in kwargs.get('meta', {})
+        count = kwargs.get('count') or None
+        if isinstance(count, list):
+            count = count[0] or None
+        for key, value in self.samples:
+            rvalue = deepcopy(value)
+            if self._must_issue_record(key, **kwargs):
+                if include_key:
+                    rvalue[b'_key'] = key
+                if include_ts:
+                    rvalue[b'_ts'] = self._get_basetime(key)
+                yield rvalue
+                if count is not None:
+                    count -= 1
+                    if count == self.return_less or count == 0:
+                        break
+
+class FakeCollections(object):
+    def __init__(self, project, **kwargs):
+        self.project = project
+        self.kwargs = kwargs
+        self.collection_list = [{'name': n, 'type': 's'} for n in self.project.client.samples.keys()]
+
+    def new_store(self, name):
+        return FakeCollection(name, self.project.client.samples[name], **self.kwargs)
+
+    def apiget(self, call):
+        if call == 'list':
+            return self.collection_list
+        raise NotImplementedError('Not supported in FakeCollections')
+
+class FakeProject(object):
+    def __init__(self, client, **kwargs):
+        self.client = client
+        self.collections = FakeCollections(self, **kwargs)
+
+class FakeClient(object):
+    def __init__(self, samples, **kwargs):
+        self.samples = samples
+        self.kwargs = kwargs
+
+    def get_project(self, *args):
+        return FakeProject(self, **self.kwargs)
---
+++ packages/collection-scanner/collection_scanner-0.4.1.tar.gz/collection_scanner-0.4.1/collection_scanner/utils.py
@@ -0,0 +1,53 @@
+import re
+import os
+import traceback
+
+
+LIMIT_KEY_CHAR = b'~'
+
+
+def retry_on_exception(exception):
+    print(f"Retried: {traceback.format_exc()}")
+    return not isinstance(exception, KeyboardInterrupt)
+
+
+def get_num_partitions(hsp, collection_name):
+    """Gets number of partitions of a partitioned collection.
+    Returns None if collection is not partitioned
+    """
+    partitions = []
+    partitions_re = re.compile(r'%s_(\d+)' % collection_name)
+    for entry in hsp.collections.apiget('list'):
+        m = partitions_re.match(entry['name'])
+        if m:
+            partitions.append(int(m.groups()[0]))
+    if partitions:
+        if len(partitions) == max(partitions) + 1:
+            return len(partitions)
+
+def filter_collections_exist(hsp, collection_names):
+    """
+    Filters a list of collections to return only those that do exist
+    """
+    filtered = []
+    for entry in hsp.collections.apiget('list'):
+        if entry['name'] in collection_names:
+            filtered.append(entry['name'])
+    return filtered
+
+def generate_prefixes(col, codelen, startafter=None, **kwargs):
+    data = True
+    while data:
+        data = False
+        for r in col.get(nodata=1, meta=['_key'], startafter=startafter, count=1, **kwargs):
+            data = True
+            code = r['_key'][:codelen]
+            startafter = code + LIMIT_KEY_CHAR
+            yield code
+
+
+def get_project_id():
+    try:
+        return os.environ['SHUB_JOBKEY'].split('/')[0]
+    except KeyError:
+        raise ValueError("Project id not found")
