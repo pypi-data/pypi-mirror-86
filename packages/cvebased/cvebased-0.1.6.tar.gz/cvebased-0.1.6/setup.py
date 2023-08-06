c4b0be149e9373f4c2599655860df9445982cf14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:45 +0000

    Add package cvebased/0.1.6/cvebased-0.1.6.tar.gz

---
+++ packages/cvebased/cvebased-0.1.6.tar.gz/cvebased-0.1.6/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cvebased']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['ruamel.yaml>=0.16.12,<0.17.0']
+
+setup_kwargs = {
+    'name': 'cvebased',
+    'version': '0.1.6',
+    'description': 'A utility library to ease interactions with cvebase.com data',
+    'long_description': '# cvebased',
+    'author': 'cvebase',
+    'author_email': 'hello@cvebase.com',
+    'maintainer': 'cvebase',
+    'maintainer_email': 'hello@cvebase.com',
+    'url': 'https://www.cvebase.com/',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
