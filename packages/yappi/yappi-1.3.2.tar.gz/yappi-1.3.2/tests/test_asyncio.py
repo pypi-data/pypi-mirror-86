d38b5db0eaf590deca10b8d4821e44061e146996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:53 +0000

    Add package yappi/1.3.2/yappi-1.3.2.tar.gz

---
+++ packages/yappi/yappi-1.3.2.tar.gz/yappi-1.3.2/tests/test_asyncio.py
@@ -0,0 +1,179 @@
+import unittest
+import yappi
+import asyncio
+import threading
+from utils import YappiUnitTestCase, find_stat_by_name, burn_cpu, burn_io
+
+
+@asyncio.coroutine
+def async_sleep(sec):
+    yield from asyncio.sleep(sec)
+
+
+class SingleThreadTests(YappiUnitTestCase):
+
+    def test_recursive_coroutine(self):
+
+        @asyncio.coroutine
+        def a(n):
+            if n <= 0:
+                return
+            yield from async_sleep(0.1)
+            burn_cpu(0.1)
+            yield from a(n - 1)
+            yield from a(n - 2)
+
+        yappi.set_clock_type("cpu")
+        yappi.start()
+        asyncio.get_event_loop().run_until_complete(a(3))
+        yappi.stop()
+
+        r1 = '''
+        ..p/yappi/tests/test_asyncio.py:11 a  9/1    0.000124  0.400667  0.044519
+        ../yappi/tests/utils.py:126 burn_cpu  4      0.000000  0.400099  0.100025
+        async_sleep                           4      0.000000  0.000444  0.000111
+        '''
+        stats = yappi.get_func_stats()
+        self.assert_traces_almost_equal(r1, stats)
+
+    def test_basic_old_style(self):
+
+        @asyncio.coroutine
+        def a():
+            yield from async_sleep(0.1)
+            burn_io(0.1)
+            yield from async_sleep(0.1)
+            burn_io(0.1)
+            yield from async_sleep(0.1)
+            burn_cpu(0.3)
+
+        yappi.set_clock_type("wall")
+        yappi.start(builtins=True)
+        asyncio.get_event_loop().run_until_complete(a())
+        asyncio.get_event_loop().run_until_complete(a())
+        yappi.stop()
+
+        r1 = '''
+        ..p/yappi/tests/test_asyncio.py:43 a  2      0.000118  1.604049  0.802024
+        async_sleep                           6      0.000000  0.603239  0.100540
+        ../yappi/tests/utils.py:126 burn_cpu  2      0.576313  0.600026  0.300013
+        ..p/yappi/tests/utils.py:135 burn_io  4      0.000025  0.400666  0.100166
+        time.sleep                            4      0.400641  0.400641  0.100160
+        '''
+        stats = yappi.get_func_stats()
+
+        self.assert_traces_almost_equal(r1, stats)
+
+        yappi.clear_stats()
+        yappi.set_clock_type("cpu")
+        yappi.start(builtins=True)
+        asyncio.get_event_loop().run_until_complete(a())
+        asyncio.get_event_loop().run_until_complete(a())
+        yappi.stop()
+        stats = yappi.get_func_stats()
+        r1 = '''
+        ..p/yappi/tests/test_asyncio.py:43 a  2      0.000117  0.601170  0.300585
+        ../yappi/tests/utils.py:126 burn_cpu  2      0.000000  0.600047  0.300024
+        async_sleep                           6      0.000159  0.000801  0.000134
+        time.sleep                            4      0.000169  0.000169  0.000042
+        '''
+        self.assert_traces_almost_equal(r1, stats)
+
+
+class MultiThreadTests(YappiUnitTestCase):
+
+    def test_basic(self):
+
+        @asyncio.coroutine
+        def a():
+            yield from async_sleep(0.3)
+            burn_cpu(0.4)
+
+        @asyncio.coroutine
+        def b():
+            yield from a()
+
+        @asyncio.coroutine
+        def recursive_a(n):
+            if not n:
+                return
+            burn_io(0.3)
+            yield from async_sleep(0.3)
+            yield from recursive_a(n - 1)
+
+        tlocal = threading.local()
+
+        def tag_cbk():
+            try:
+                return tlocal._tag
+            except:
+                return -1
+
+        yappi.set_clock_type("wall")
+        tlocal._tag = 0
+        yappi.set_tag_callback(tag_cbk)
+
+        def _thread_event_loop(loop, tag):
+            tlocal._tag = tag
+            asyncio.set_event_loop(loop)
+            loop.run_forever()
+
+        _TCOUNT = 3
+        _ctag = 1
+
+        ts = []
+        for i in range(_TCOUNT):
+            _loop = asyncio.new_event_loop()
+            t = threading.Thread(target=_thread_event_loop, args=(_loop, _ctag))
+            t._loop = _loop
+            t.start()
+
+            ts.append(t)
+            _ctag += 1
+
+        @asyncio.coroutine
+        def stop_loop():
+            asyncio.get_event_loop().stop()
+
+        @asyncio.coroutine
+        def driver():
+            futs = []
+            fut = asyncio.run_coroutine_threadsafe(a(), ts[0]._loop)
+            futs.append(fut)
+            fut = asyncio.run_coroutine_threadsafe(recursive_a(5), ts[1]._loop)
+            futs.append(fut)
+            fut = asyncio.run_coroutine_threadsafe(b(), ts[2]._loop)
+            futs.append(fut)
+            for fut in futs:
+                fut.result()
+
+            # stop asyncio loops in threads
+            for t in ts:
+                asyncio.run_coroutine_threadsafe(stop_loop(), t._loop)
+
+        yappi.start()
+        asyncio.get_event_loop().run_until_complete(driver())
+        yappi.stop()
+        traces = yappi.get_func_stats()
+        t1 = '''
+        tests/test_asyncio.py:137 driver      1      0.000061  3.744064  3.744064
+        tests/test_asyncio.py:96 recursive_a  6/1    0.000188  3.739663  0.623277
+        tests/test_asyncio.py:8 async_sleep   7      0.000085  2.375271  0.339324
+        tests/utils.py:135 burn_io            5      0.000044  1.700000  0.437400
+        tests/test_asyncio.py:87 a            2      0.000019  1.600000  0.921138
+        tests/utils.py:126 burn_cpu           2      0.800000  0.800000  0.509730
+        tests/test_asyncio.py:92 b            1      0.000005  0.800000  0.921055
+        '''
+        self.assert_traces_almost_equal(t1, traces)
+
+        traces = yappi.get_func_stats(filter={'tag': 2})
+        t1 = '''
+        tests/test_asyncio.py:96 recursive_a  6/1    0.000211  3.720011  0.620002
+        tests/utils.py:135 burn_io            5      0.000079  1.700000  0.431813
+        async_sleep                           5      0.000170  1.560735  0.312147
+        '''
+        self.assert_traces_almost_equal(t1, traces)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ packages/yappi/yappi-1.3.2.tar.gz/yappi-1.3.2/tests/utils.py
@@ -0,0 +1,183 @@
+import sys
+import yappi
+import time
+import unittest
+
+
+class YappiUnitTestCase(unittest.TestCase):
+
+    def setUp(self):
+        # reset everything back to default
+        yappi.stop()
+        yappi.clear_stats()
+        yappi.set_clock_type('cpu')  # reset to default clock type
+        yappi.set_context_backend('native_thread')
+        yappi.set_context_id_callback(None)
+        yappi.set_context_name_callback(None)
+        yappi.set_tag_callback(None)
+
+    def tearDown(self):
+        fstats = yappi.get_func_stats()
+        if not fstats._debug_check_sanity():
+            sys.stdout.write("ERR: Duplicates found in Func stats\r\n")
+
+            fstats.debug_print()
+        for fstat in fstats:
+            if not fstat.children._debug_check_sanity():
+                sys.stdout.write("ERR: Duplicates found in ChildFunc stats\r\n")
+                fstat.children.print_all()
+        tstats = yappi.get_func_stats()
+        if not tstats._debug_check_sanity():
+            sys.stdout.write("ERR: Duplicates found in Thread stats\r\n")
+            tstats.print_all()
+
+    def assert_traces_almost_equal(self, traces_str, traces):
+        for t in traces_str.split('\n'):
+            tline = t.strip()
+            if tline:
+                t = tline.split()
+                ttot_orig = float(t[-2].strip())
+                tsub_orig = float(t[-3].strip())
+                ncall_orig = t[-4].strip()
+
+                if '/' in ncall_orig:  # recursive func?
+                    ncall_orig = ncall_orig.split('/')
+                    ncall = int(ncall_orig[0])
+                    non_recursive_ncall = int(ncall_orig[1])
+                else:
+                    ncall = int(ncall_orig)
+                    non_recursive_ncall = ncall
+
+                t = find_stat_by_name(traces, t[-5])
+                self.assertEqual(ncall, t.ncall, tline)
+                self.assertEqual(non_recursive_ncall, t.nactualcall, tline)
+                if ttot_orig:
+                    self.assert_almost_equal(ttot_orig, t.ttot, err_msg=tline)
+                if tsub_orig:
+                    self.assert_almost_equal(tsub_orig, t.tsub, err_msg=tline)
+
+    def assert_ctx_stats_almost_equal(self, expected_stats_str, actual_stats):
+        for t in expected_stats_str.split('\n'):
+            tline = t.strip()
+            if tline:
+                t = tline.split()
+                ttot_orig = float(t[-2].strip())
+
+                ctx = find_ctx_stats_by_name(actual_stats, t[0])
+                if ttot_orig:
+                    self.assert_almost_equal(ttot_orig, ctx.ttot, err_msg=tline)
+
+    def assert_almost_equal(
+        self, x, y, negative_err=0.2, positive_err=0.6, err_msg=None
+    ):
+        # time sensitive tests fail on MacOS CI, increase threshold
+        if sys.platform == 'darwin':
+            positive_err = 1.5
+            negative_err = 0.6
+
+        pos_epsilon = (x * positive_err)
+        neg_epsilon = (x * negative_err)
+
+        # if too small, then use 0 as negative threshold
+        neg_threshold = x - neg_epsilon
+        if neg_threshold < 0.1:
+            neg_threshold = 0
+
+        pos_threshold = x + pos_epsilon
+        if pos_threshold < 0.1:
+            pos_threshold = 0.1
+
+        assert neg_threshold <= y <= pos_threshold, "%s <= %s <= %s is not True. [%s]" % (
+            neg_threshold, y, pos_threshold, err_msg
+        )
+
+
+def assert_raises_exception(func):
+    try:
+        _run(func)
+        assert 0 == 1
+    except:
+        pass
+
+
+def run_with_yappi(func, *args, **kwargs):
+    yappi.start()
+    func(*args, **kwargs)
+    yappi.stop()
+
+
+def run_and_get_func_stats(func, *args, **kwargs):
+    run_with_yappi(func, *args, **kwargs)
+    return yappi.get_func_stats()
+
+
+def run_and_get_thread_stats(func, *args, **kwargs):
+    run_with_yappi(func, *args, **kwargs)
+    return yappi.get_thread_stats()
+
+
+def is_py3x():
+    return sys.version_info > (3, 0)
+
+
+def find_stat_by_name(stats, name):
+    for stat in stats:
+        if hasattr(stat, 'module'):
+            if stat.module + '.' + stat.name == name:
+                return stat
+
+        if stat.name == name:
+            return stat
+
+
+def find_ctx_stats_by_name(ctx_stats, name):
+    for stat in ctx_stats:
+        if stat.name == name:
+            return stat
+
+
+def get_stat_names(stats):
+    return [stat.name for stat in stats]
+
+
+def find_stat_by_id(stats, id):
+    for stat in stats:
+        if stat.id == id:
+            return stat
+
+
+def burn_cpu(sec):
+    t0 = yappi.get_clock_time()
+    elapsed = 0
+    while (elapsed < sec):
+        for _ in range(1000):
+            pass
+        elapsed = yappi.get_clock_time() - t0
+
+
+def burn_io(sec):
+    time.sleep(sec)
+
+
+def burn_io_gevent(sec):
+    import gevent
+    gevent.sleep(sec)
+
+
+from contextlib import contextmanager
+
+
+@contextmanager
+def captured_output():
+    if is_py3x():
+        from io import StringIO
+    else:
+        from StringIO import StringIO
+
+    new_out, new_err = StringIO(), StringIO()
+    old_out, old_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = new_out, new_err
+        yield sys.stdout, sys.stderr
+    finally:
+        sys.stdout, sys.stderr = old_out, old_err
