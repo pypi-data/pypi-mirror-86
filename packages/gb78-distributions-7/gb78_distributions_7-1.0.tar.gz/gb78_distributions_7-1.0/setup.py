4b7160949ef9fcc49f312673e8ae0eecb990ba7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:14 +0000

    Add package gb78-distributions-7/1.0/gb78_distributions_7-1.0.tar.gz

---
+++ packages/gb78-distributions-7/gb78_distributions_7-1.0.tar.gz/gb78_distributions_7-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gb78_distributions_7',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['gb78_distributions7'],
+      author = 'Daniel Pedraza',
+      author_email = 'dpedrazatrivino2@gmail.com',
+      zip_safe=False)
