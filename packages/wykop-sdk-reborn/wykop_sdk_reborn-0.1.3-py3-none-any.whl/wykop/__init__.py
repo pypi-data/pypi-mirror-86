e9e9b4f01552ac9ebdcc74db94a2d13f269dd52b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:47 +0000

    Add package wykop-sdk-reborn/0.1.3/wykop_sdk_reborn-0.1.3-py3-none-any.whl

---
+++ packages/wykop-sdk-reborn/wykop_sdk_reborn-0.1.3-py3-none-any.whl/wykop/__init__.py
@@ -0,0 +1,6 @@
+"""Python library for the Wykop API."""
+from wykop.api.v2.clients import WykopAPIv2
+from wykop.api.exceptions import WykopAPIError
+from wykop.utils import get_version
+
+__version__ = get_version()
