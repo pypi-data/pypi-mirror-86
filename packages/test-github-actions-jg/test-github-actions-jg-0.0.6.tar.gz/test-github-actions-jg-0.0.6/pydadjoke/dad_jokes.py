0c8beae134c5ad056b1343fff832b9fb451719f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:08 +0000

    Add package test-github-actions-jg/0.0.6/test-github-actions-jg-0.0.6.tar.gz

---
+++ packages/test-github-actions-jg/test-github-actions-jg-0.0.6.tar.gz/test-github-actions-jg-0.0.6/pydadjoke/dad_jokes.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+
+def get_joke():
+    print("Dad, did you get a haircut?\nNo, I got them all cut!")
+    print("What time did the man go to the dentist?\nTooth hurt-y.")
