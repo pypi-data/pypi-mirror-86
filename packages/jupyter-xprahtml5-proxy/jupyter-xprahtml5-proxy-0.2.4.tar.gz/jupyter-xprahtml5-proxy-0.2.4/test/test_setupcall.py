3838851489bce35ae3469665c4e26dff218016f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:39 +0000

    Add package jupyter-xprahtml5-proxy/0.2.4/jupyter-xprahtml5-proxy-0.2.4.tar.gz

---
+++ packages/jupyter-xprahtml5-proxy/jupyter-xprahtml5-proxy-0.2.4.tar.gz/jupyter-xprahtml5-proxy-0.2.4/test/test_setupcall.py
@@ -0,0 +1,8 @@
+def test_setupcall():
+    """
+    Test the call of the setup function
+    """
+    import jupyter_xprahtml5_proxy as jx
+
+    print("\nRunning test_setupcall...")
+    print(jx.setup_xprahtml5())
