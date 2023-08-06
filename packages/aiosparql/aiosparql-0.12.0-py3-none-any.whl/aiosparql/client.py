7cd118b25f7e481eccc723cc576dcfff2a6bb6ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:33 +0000

    Add package aiosparql/0.12.0/aiosparql-0.12.0-py3-none-any.whl

---
+++ packages/aiosparql/aiosparql-0.12.0-py3-none-any.whl/aiosparql/client.py
@@ -0,0 +1,270 @@
+import asyncio
+import logging
+import re
+from io import IOBase
+from math import ceil, log10
+from string import Formatter
+from textwrap import dedent, indent
+from typing import Dict, Optional, Union
+
+import aiohttp
+
+from .syntax import IRI, all_prefixes
+
+__all__ = ["SPARQLClient", "SPARQLRequestFailed", "SPARQLQueryFormatter"]
+
+
+logger = logging.getLogger(__name__)
+
+
+class SPARQLRequestFailed(aiohttp.ClientResponseError):
+    def __init__(
+        self,
+        request_info,
+        history,
+        *,
+        status=0,
+        message="",
+        headers=None,
+        explanation=None
+    ):
+        super(SPARQLRequestFailed, self).__init__(
+            request_info, history, status=status, message=message, headers=headers
+        )
+        self.explanation = explanation
+
+    def __str__(self):
+        base_message = super(SPARQLRequestFailed, self).__str__()
+        return "%s, explanation=%r" % (base_message, self.explanation)
+
+
+class SPARQLQueryFormatter(Formatter):
+    """
+    This custom formatter redefine the parse method of the default Python's
+    formatter in order to use {{}} instead of {} as replacement field.
+
+    It also automatically indents text, example:
+    s = "\n".join(["b", "c", "d"])
+    SPARQLQueryFormatter().vformat("a\n  {{}}\ne", [s], {})
+    # returns:
+    # a
+    #     b
+    #     c
+    #     d
+    # e
+    """
+
+    re_token = re.compile(r"((?:[^{]+|\{[^{]+|\{$)*)(?:\{\{((?:[^}]+|}[^}])*)\}\})?")
+    re_field = re.compile(r"(.*)(?:!([sra]))?(?::([^{}]*))?")
+    re_indent = re.compile(r"(.*)^(\s*)$", flags=(re.M + re.S))
+    indent = ""
+
+    def parse(self, s):
+        if not s:
+            return
+        else:
+            for match in self.re_token.finditer(s):
+                imatch = self.re_indent.fullmatch(match.group(1))
+                if imatch:
+                    text = imatch.group(1)
+                    self.indent = imatch.group(2)
+                else:
+                    text = match.group(1)
+                    self.indent = ""
+                if match.group(2) is None:
+                    if match.end() != len(s):
+                        raise Exception(
+                            "Not terminated token: %r" % s[match.end() :]  # noqa
+                        )
+                    yield (text, None, None, None)
+                    break
+                else:
+                    fmatch = self.re_field.fullmatch(match.group(2))
+                    if not fmatch:
+                        raise Exception("Cannot parse token: %r" % match.group(2))
+                    yield (text, fmatch.group(1), fmatch.group(3), fmatch.group(2))
+
+    def format_field(self, value, format_spec):
+        return indent(format(value, format_spec), self.indent)
+
+
+class SPARQLClient:
+    def __init__(
+        self,
+        endpoint: str,
+        *,
+        update_endpoint: Optional[str] = None,
+        crud_endpoint: Optional[str] = None,
+        prefixes: Optional[Dict[str, IRI]] = None,
+        graph: Optional[IRI] = None,
+        **kwargs
+    ):
+        self._closed = False
+        self._endpoint = endpoint
+        self._update_endpoint = update_endpoint
+        self._crud_endpoint = crud_endpoint
+        self._graph = graph
+        self.session = aiohttp.ClientSession(**kwargs)
+        self._generate_prefixes(prefixes)
+
+    @property
+    def endpoint(self):
+        return self._endpoint
+
+    @property
+    def update_endpoint(self):
+        return self._update_endpoint or self._endpoint
+
+    @property
+    def crud_endpoint(self):
+        return self._crud_endpoint
+
+    @property
+    def graph(self):
+        return self._graph
+
+    def _generate_prefixes(self, prefixes):
+        header = [
+            "PREFIX %s: %s" % (prefix, ns.__iri__)
+            for prefix, ns in sorted(all_prefixes.items(), key=lambda x: x[0])
+        ]
+        if prefixes:
+            header.append("")
+            header.extend(
+                [
+                    "PREFIX %s: %s" % x
+                    for x in sorted(prefixes.items(), key=lambda x: x[0])
+                ]
+            )
+        self._prefixes_header = "\n".join(header) + "\n"
+
+    def _prepare_query(self, query: str, *args, **keywords) -> dict:
+        lines = [self._prefixes_header]
+        lines.extend([dedent(query).strip()])
+        query_args = {"graph": self.graph} if self.graph else {}
+        query_args.update(keywords)
+        query_formatter = SPARQLQueryFormatter()
+        return query_formatter.vformat("\n".join(lines), args, query_args)
+
+    def _pretty_print_query(self, query: str) -> str:
+        query = query.rstrip()
+        ln_indent = ceil(log10(query.count("\n")))
+        return "\n".join(
+            ("{:-%dd}: {}" % ln_indent).format(i, x)
+            for i, x in enumerate(query.split("\n"), 1)
+        )
+
+    async def _raise_for_status(self, resp: aiohttp.ClientResponse) -> None:
+        if resp.status >= 400:
+            explanation = await resp.text()
+            resp.release()
+            logger.debug("Server responded:\n%s\n%s", explanation, "=" * 40)
+            raise SPARQLRequestFailed(
+                resp.request_info,
+                resp.history,
+                status=resp.status,
+                message=resp.reason,
+                explanation=explanation,
+            )
+
+    async def query(self, query: str, *args, **keywords) -> dict:
+        headers = {"Accept": "application/json"}
+        full_query = self._prepare_query(query, *args, **keywords)
+        logger.debug(
+            "Sending SPARQL query to %s: \n%s\n%s",
+            self.endpoint,
+            self._pretty_print_query(full_query),
+            "=" * 40,
+        )
+        async with self.session.post(
+            self.endpoint, data={"query": full_query}, headers=headers
+        ) as resp:
+            await self._raise_for_status(resp)
+            return await resp.json()
+
+    async def update(self, query: str, *args, **keywords) -> dict:
+        headers = {"Accept": "application/json"}
+        full_query = self._prepare_query(query, *args, **keywords)
+        logger.debug(
+            "Sending SPARQL query to %s:\n%s\n%s",
+            self.endpoint,
+            self._pretty_print_query(full_query),
+            "=" * 40,
+        )
+        async with self.session.post(
+            self.update_endpoint, data={"update": full_query}, headers=headers
+        ) as resp:
+            await self._raise_for_status(resp)
+            # NOTE: some databases may still return HTML instead of JSON
+            if "application/json" not in resp.content_type:
+                return {"body": await resp.text()}
+            return await resp.json()
+
+    def _crud_request(
+        self, method, graph=None, data=None, accept=None, content_type=None
+    ):
+        if not self.crud_endpoint:
+            raise ValueError("CRUD endpoint not specified")
+        url = self.crud_endpoint
+        if graph:
+            params = {"graph": graph.value}
+        elif self.graph:
+            params = {"graph": self.graph.value}
+        else:
+            params = None
+            url += "?default"
+        headers = {}
+        if content_type:
+            headers["Content-Type"] = content_type
+        if accept:
+            headers["Accept"] = accept
+        logger.debug(
+            "Sending %s request to CRUD endpoint %s with headers " "%r, and params %r",
+            method,
+            url,
+            headers,
+            params,
+        )
+        return self.session.request(
+            method, url, params=params, headers=headers, data=data
+        )
+
+    def get(self, *, format: str, graph: Optional[IRI] = None):
+        return self._crud_request("GET", graph=graph, accept=format)
+
+    async def put(
+        self, data: Union[bytes, IOBase], *, format: str, graph: Optional[IRI] = None
+    ):
+        async with self._crud_request(
+            "PUT", graph=graph, data=data, content_type=format
+        ) as resp:
+            resp.raise_for_status()
+
+    async def delete(self, graph: Optional[IRI] = None):
+        async with self._crud_request("DELETE", graph=graph) as resp:
+            resp.raise_for_status()
+
+    async def post(
+        self, data: Union[bytes, IOBase], *, format: str, graph: Optional[IRI] = None
+    ):
+        async with self._crud_request(
+            "POST", graph=graph, data=data, content_type=format
+        ) as resp:
+            resp.raise_for_status()
+
+    @property
+    def closed(self):
+        return self._closed
+
+    @asyncio.coroutine
+    def close(self):
+        self._closed = True
+        # NOTE: TypeError: object _CoroGuard can't be used in 'await'
+        #       expression
+        yield from self.session.close()
+
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.close()
---
+++ packages/aiosparql/aiosparql-0.12.0-py3-none-any.whl/aiosparql/syntax.py
@@ -0,0 +1,295 @@
+import re
+from itertools import groupby
+from textwrap import indent
+
+from .escape import escape_any, escape_string, escapers
+
+__all__ = [
+    "RDFTerm",
+    "Node",
+    "Triples",
+    "PrefixedName",
+    "IRI",
+    "Literal",
+    "UNDEF",
+    "Namespace",
+    "RDF",
+]
+
+
+class RDFTerm:
+    """
+    A base class for any class that can be print (using str()) and return a
+    valid RDF term (i.e. that can be used in a SPARQL query).
+    """
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+    def __eq__(self, other):
+        if isinstance(other, RDFTerm):
+            return self.value == other.value
+        else:
+            return self.value == other
+
+    def __hash__(self):
+        return hash(self.value)
+
+
+class Node(list):
+    """
+    A magical list of tuples (p, o) with a subject that can be printed to a
+    SPARQL query.
+
+    It can also accept a dict in argument.
+    """
+
+    def __init__(self, subject, value=[]):
+        self.subject = subject
+        if isinstance(value, dict):
+            super(Node, self).__init__(value.items())
+        else:
+            super(Node, self).__init__(value)
+
+    def __str__(self):
+        return "".join(self._output_triples())
+
+    def _output_triples(self):
+        extra_nodes = []
+        it = iter(self)
+        p, o = next(it)
+        yield "%s %s %s" % (self.subject, p, escape_any(o))
+        if isinstance(o, Node):
+            extra_nodes.append(o)
+        for p, o in it:
+            assert p is not None, "predicate not defined"
+            yield " ;\n"
+            yield "    %s %s" % (p, escape_any(o))
+            if isinstance(o, Node):
+                extra_nodes.append(o)
+        yield " ."
+        for node in sorted(extra_nodes, key=lambda x: str(x.subject)):
+            yield "\n\n"
+            yield str(node)
+
+    def _group_key(self, x):
+        return (str(x[0]), str(x[1]))
+
+    def __iter__(self):
+        for p, o in sorted(super().__iter__(), key=self._group_key):
+            if o is None:
+                continue
+            yield (p, o)
+
+
+class Triples(list):
+    """
+    A magical list of tuples (s, p, o) that be printed to a SPARQL query.
+    """
+
+    def __init__(self, value=[]):
+        assert all(isinstance(x, (tuple, Node)) for x in value), (
+            "only tuples and Node are accepted, received: %r" % value
+        )
+        assert all(
+            len(x) == 3 for x in value if isinstance(x, tuple)
+        ), "tuples must be of length 3"
+        return super(Triples, self).__init__(value)
+
+    def append(self, triple):
+        assert isinstance(triple, (tuple, Node)), (
+            "only tuples are Node accepted, received: %r" % triple
+        )
+        assert (
+            len(triple) == 3 if isinstance(triple, tuple) else True
+        ), "tuple must be of length 3"
+        return super(Triples, self).append(triple)
+
+    def extend(self, value):
+        assert all(isinstance(x, (tuple, Node)) for x in value), (
+            "only tuples are Node accepted, received: %r" % value
+        )
+        assert all(
+            len(x) == 3 for x in value if isinstance(x, tuple)
+        ), "tuples must be of length 3"
+        return super(Triples, self).extend(value)
+
+    def __str__(self):
+        return "".join(self._output_triples())
+
+    def indent(self, spaces):
+        return indent(str(self), spaces)
+
+    def _output_triples(self):
+        item = None
+        for s, group in groupby(self, self._group_key):
+            assert s is not None, "subject not defined"
+            if item is None:
+                pass
+            elif isinstance(item, tuple):
+                yield " .\n\n"
+            elif isinstance(item, Node):
+                yield "\n\n"
+            item = next(group)
+            if isinstance(item, tuple):
+                s, p, o = item
+                yield "%s %s %s" % (s, p, escape_any(o))
+                for _, p, o in group:
+                    assert p is not None, "predicate not defined"
+                    if o is None:
+                        continue
+                    yield " ;\n"
+                    yield "    %s %s" % (p, escape_any(o))
+            elif isinstance(item, Node):
+                yield str(item)
+        if isinstance(item, tuple):
+            yield " ."
+
+    def _group_key(self, x):
+        if isinstance(x, tuple):
+            return x[0]
+        elif isinstance(x, Node):
+            return id(x)
+
+
+class PrefixedName(RDFTerm):
+    def __init__(self, base_iri, prefix_label, local_part):
+        self.base_iri = base_iri
+        self.prefix_label = prefix_label
+        self.local_part = local_part
+
+    def __str__(self):
+        return "%s:%s" % (self.prefix_label, self.local_part)
+
+    def __repr__(self):
+        return "<PrefixedName %s>" % self
+
+    def __eq__(self, other):
+        if isinstance(other, PrefixedName):
+            return (
+                self.prefix_label == other.prefix_label
+                and self.local_part == other.local_part
+            )
+        else:
+            return self.iri() == other
+
+    def __hash__(self):
+        return hash(self.iri())
+
+    def iri(self):
+        return IRI(self.base_iri.value + self.local_part)
+
+
+class IRI(RDFTerm):
+    __re_invalid_chars__ = re.compile('[<>"{}|^`[-\\]\x00-\x20]')
+
+    def __init__(self, value):
+        self.value = value
+        self.ref = "<%s>" % self.__re_invalid_chars__.sub(
+            lambda x: "%{:02X}".format(ord(x.group(0))), value
+        )
+
+    def __str__(self):
+        return self.ref
+
+    def __repr__(self):
+        return "<IRI %s>" % self.value
+
+    def __eq__(self, other):
+        if isinstance(other, IRI):
+            return self.value == other.value
+        else:
+            return self.value == other
+
+    def __hash__(self):
+        return hash(self.value)
+
+    def __add__(self, other):
+        return IRI("%s%s" % (self.value, other))
+
+
+class Literal(RDFTerm):
+    def __init__(self, value, lang=None):
+        self.value = value
+        self.lang = lang
+
+    def __str__(self):
+        return "%s@%s" % (escape_string(self.value), self.lang)
+
+    def __eq__(self, other):
+        if isinstance(other, Literal):
+            return self.value == other.value and self.lang == other.lang
+        else:
+            return self.value == other
+
+    def __hash__(self):
+        return hash((self.value, self.lang))
+
+
+class UNDEF(RDFTerm):
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return "UNDEF"
+
+    def __eq__(self, other):
+        return isinstance(other, UNDEF)
+
+    def __hash__(self):
+        return hash(UNDEF)
+
+
+all_prefixes = {}
+
+
+class MetaNamespace(type):
+    def __new__(mcs, name, bases, nmspc):
+        if bases:
+            assert "__iri__" in nmspc, "missing attribute __iri__ for class %s" % name
+            iri = nmspc["__iri__"]
+            prefix_label = nmspc.get("__prefix_label__", name.lower())
+            nmspc = {
+                k: (PrefixedName(iri, prefix_label, k) if v is PrefixedName else v)
+                for k, v in nmspc.items()
+            }
+            nmspc["__prefix_label__"] = prefix_label
+        return super(MetaNamespace, mcs).__new__(mcs, name, bases, nmspc)
+
+    def __init__(cls, name, bases, nmspc):
+        super(MetaNamespace, cls).__init__(name, bases, nmspc)
+        if bases:
+            all_prefixes[cls.__prefix_label__] = cls
+
+
+class Namespace(metaclass=MetaNamespace):
+    __prefix_label__ = ""
+
+
+class RDF(Namespace):
+    __iri__ = IRI("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+
+    HTML = PrefixedName
+    langString = PrefixedName
+    PlainLiteral = PrefixedName
+    type = PrefixedName
+    Property = PrefixedName
+    Statement = PrefixedName
+    subject = PrefixedName
+    predicate = PrefixedName
+    object = PrefixedName
+    Bag = PrefixedName
+    Seq = PrefixedName
+    Alt = PrefixedName
+    value = PrefixedName
+    List = PrefixedName
+    nil = PrefixedName
+    first = PrefixedName
+    rest = PrefixedName
+    XMLLiteral = PrefixedName
+
+
+escapers.extend([(Node, lambda x: x.subject), (RDFTerm, str)])
