54d87e0f2ffa797a567e758d3d239d9e968ec01b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:24 +0000

    Add package pikax/2.2.6/pikax-2.2.6.tar.gz

---
+++ packages/pikax/pikax-2.2.6.tar.gz/pikax-2.2.6/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pikax",
+    version="2.2.6",
+    author="Redcxx",
+    author_email="weilue.luo@student.manchester.ac.uk",
+    description="A Pixiv Mass Downloading Tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Redcxx/Pikax",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'requests',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Topic :: Utilities"
+    ],
+)
