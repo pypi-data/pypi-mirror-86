8a583d321850897b0f1abc5f84fb73cb111bebaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:21 +0000

    Add package micro-components/0.1.0/micro_components-0.1.0-py3.7.egg

---
+++ packages/micro-components/micro_components-0.1.0-py3.7.egg/micro-components/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import re
+import sys
+
+from pathlib import Path
+from utils.shell import shell_run
+
+
+result = shell_run(['micro-components', 'get', 'version'])
+
+if re.match(r'\w+ [\d\.]+', result):
+	print(' "micro-components" binary already installed. Skipping this part...')
+else:
+	...
+	bin_folder = Path(sys.executable).parent
+	builder_path = Path('./components/builder.py').resolve()
+	result = shell_run(['ln', '-s', str(builder_path), bin_folder / 'micro-components'])
+	print(' CLI "micro-components" created.')
