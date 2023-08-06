c5ee2b807815c63fe1823879a5282f72be40e0ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:36 +0000

    Add package pymlconf/2.5.8/pymlconf-2.5.8.tar.gz

---
+++ packages/pymlconf/pymlconf-2.5.8.tar.gz/pymlconf-2.5.8/pymlconf/__init__.py
@@ -0,0 +1,9 @@
+"""pymlconf package."""
+from .models import Mergable, MergableList, MergableDict, Root, \
+    ConfigurationNamespace, DeferredRoot
+
+from .errors import ConfigurationError, ConfigurationAlreadyInitializedError, \
+    ConfigurationNotInitializedError
+
+
+__version__ = '2.5.8'
