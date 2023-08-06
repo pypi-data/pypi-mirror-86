38c9e3243afccc61336e39d37a761d612c1f3d23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:34 +0000

    Add package setlr/0.2.16/setlr-0.2.16.tar.gz

---
+++ packages/setlr/setlr-0.2.16.tar.gz/setlr-0.2.16/README.md
@@ -0,0 +1,15 @@
+# setlr: The Semantic Extract, Transform and Load-er
+
+setlr is a tool for generating RDF graphs, including named graphs, from almost any kind of tabular data.
+
+# Installation
+
+Simply check out the code, optionally create a python virtual environment, and install it using pip:
+
+```bash
+pip install setlr
+```
+
+# Learning how to SETL
+
+To learn how to use setlr please visit [the tutorial wiki page](https://github.com/tetherless-world/setlr/wiki/SETLr-Basics-Tutorial).
---
+++ packages/setlr/setlr-0.2.16.tar.gz/setlr-0.2.16/setup.py
@@ -0,0 +1,50 @@
+import os
+from setuptools import setup, find_packages
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "setlr",
+    version = "0.2.16",
+    author = "Jamie McCusker",
+    author_email = "mccusj@cs.rpi.edu",
+    description = ("setlr is a tool for Semantic Extraction, Transformation, and Loading."),
+    license = "Apache License 2.0",
+    keywords = "rdf semantic etl",
+    url = "http://packages.python.org/setlr",
+    packages=['setlr'],
+    long_description='''SETLr is a tool for generating RDF graphs, including named graphs, from almost any kind of tabular data.''',
+    include_package_data = True,
+    install_requires = [
+        'future',
+        'pip>=9.0.0',
+        'cython',
+        'numpy',
+        'rdflib',
+        'rdflib-jsonld',
+        'pandas>=0.23.0',
+        'requests',
+        'toposort',
+        'beautifulsoup4',
+        'jinja2',
+        'lxml',
+        'six',
+        'xlrd',
+        'ijson',
+        'requests-testadapter',
+        'python-slugify',
+    ],
+    entry_points = {
+        'console_scripts': ['setlr=setlr:main'],
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
