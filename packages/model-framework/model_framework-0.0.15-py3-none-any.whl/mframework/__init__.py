77087d8ff1a7d3c5af21f8edc143de2788788578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:39 +0000

    Add package model-framework/0.0.15/model_framework-0.0.15-py3-none-any.whl

---
+++ packages/model-framework/model_framework-0.0.15-py3-none-any.whl/mframework/__init__.py
@@ -0,0 +1,5 @@
+MODULE_NAME = 'mframework'
+
+from ._mframework import FeatureSpec, ModelSpec, SchemadictValidators, UIDDict
+from ._docgen import doc2rst
+from ._log import disable_logger, enable_logger
