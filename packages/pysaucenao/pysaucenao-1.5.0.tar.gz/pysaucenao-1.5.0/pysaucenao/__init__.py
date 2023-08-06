2767401e88d04507cf123fe3a6fec3ca494023bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:07 +0000

    Add package pysaucenao/1.5.0/pysaucenao-1.5.0.tar.gz

---
+++ packages/pysaucenao/pysaucenao-1.5.0.tar.gz/pysaucenao-1.5.0/pysaucenao/__init__.py
@@ -0,0 +1,12 @@
+from pysaucenao.saucenao import SauceNao
+from pysaucenao.containers import GenericSource, PixivSource, BooruSource, VideoSource, MangaSource, AnimeSource
+from pysaucenao.errors import *
+
+__author__      = 'FujiMakoto'
+__copyright__   = 'Copyright 2020, Taiga Development'
+__credits__     = ['FujiMakoto']
+__license__     = 'gpl-3.0'
+__version__     = '1.5.0'
+__maintainer__  = 'FujiMakoto'
+__email__       = 'makoto+github@taiga.sh'
+__status__      = 'Stable'
