aa86905250728be1f2dd9411916d9569cd4178fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:47 +0000

    Add package bip39validator/1.0.3/bip39validator-1.0.3.tar.gz

---
+++ packages/bip39validator/bip39validator-1.0.3.tar.gz/bip39validator-1.0.3/docs/conf.py
@@ -0,0 +1,77 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'BIP39 Validator'
+copyright = '2020, Ali Sherief'
+author = 'Ali Sherief'
+
+# The full version, including alpha/beta/rc tags
+release = '1.0.0rc1'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+  "sphinx.ext.autodoc",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# The master toctree document.
+master_doc = 'index'
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.desktop']
+
+autodoc_default_options = {
+  'special-members': '__init__',
+}
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "alabaster"
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+html_theme_options = {
+    "show_powered_by": False,
+    "github_user": "ZenulAbidin",
+    "github_repo": "bip39validator",
+    "github_banner": True,
+    "show_related": False,
+    "note_bg": "#FFF59C",
+    "donate_url": "https://www.blockchain.com/btc/payment_request?address=bc1q4djl6pxt90nfs8fufdul26ufxukxxrczsfjj0h&message=Donation to bip39validator (NotATether)&amount=0.0005",
+    "show_powered_by": True,
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
