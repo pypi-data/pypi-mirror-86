0d184f93df0c873753d1fd8c470677b0452ed6d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:21 +0000

    Add package cutters-common/0.1.0/cutters-common-0.1.0.tar.gz

---
+++ packages/cutters-common/cutters-common-0.1.0.tar.gz/cutters-common-0.1.0/cutters_common/__init__.py
@@ -0,0 +1,3 @@
+from .kafka_helpers import create_consumer, create_producer  # noqa: F401
+
+__version__ = "0.1.0"
---
+++ packages/cutters-common/cutters-common-0.1.0.tar.gz/cutters-common-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cutters_common']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['kafka-python>=2.0.2,<3.0.0']
+
+setup_kwargs = {
+    'name': 'cutters-common',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Panu Oksiala',
+    'author_email': 'panu@oksiala.fi',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
