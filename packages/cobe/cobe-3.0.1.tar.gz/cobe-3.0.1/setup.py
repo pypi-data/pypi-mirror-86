c953fca62dd375ea94683c03b636dec4fa8e9323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:12 +0000

    Add package cobe/3.0.1/cobe-3.0.1.tar.gz

---
+++ packages/cobe/cobe-3.0.1.tar.gz/cobe-3.0.1/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# Require setuptools. See http://pypi.python.org/pypi/setuptools for
+# installation instructions, or run the ez_setup script found at
+# http://peak.telecommunity.com/dist/ez_setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name = "cobe",
+    version = "3.0.1",
+    author = "Peter Teichman",
+    author_email = "peter@teichman.org",
+    url = "https://github.com/pteichman/cobe/wiki",
+    description = "Markov chain based text generator library and chatbot",
+    packages = ["cobe"],
+    test_suite = "tests",
+
+    install_requires = [
+        "PyStemmer==2.0.1",
+        "irc==19.0.1"
+        ],
+
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence"
+        ],
+
+    entry_points = {
+        "console_scripts" : [
+            "cobe = cobe.control:main"
+        ]
+    }
+)
