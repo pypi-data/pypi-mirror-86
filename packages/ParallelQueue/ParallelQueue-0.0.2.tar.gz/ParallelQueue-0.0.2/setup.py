26d71c159baffc599fdc8749fc7faa4b365b9992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:02 +0000

    Add package ParallelQueue/0.0.2/ParallelQueue-0.0.2.tar.gz

---
+++ packages/ParallelQueue/ParallelQueue-0.0.2.tar.gz/ParallelQueue-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ParallelQueue',
+    version='0.0.2',
+    packages=['parallelqueue', 'parallelqueue.components', 'parallelqueue.base_models'],
+    url='',
+    license='MIT',
+    author='Aaron Janeiro Stone',
+    author_email='ajstone@uwaterloo.ca',
+    description='Parallel queueing models for SimPy'
+)
