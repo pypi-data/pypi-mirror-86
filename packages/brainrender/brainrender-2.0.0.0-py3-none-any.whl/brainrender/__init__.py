bac0726d03f39dd8454169879910cb01e3f43cb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:01 +0000

    Add package brainrender/2.0.0.0/brainrender-2.0.0.0-py3-none-any.whl

---
+++ packages/brainrender/brainrender-2.0.0.0-py3-none-any.whl/brainrender/__init__.py
@@ -0,0 +1,13 @@
+from pyinspect import install_traceback
+import os
+from pathlib import Path
+
+install_traceback()
+
+import brainrender.settings
+from brainrender.scene import Scene
+import brainrender.actors
+from brainrender.video import VideoMaker, Animation
+
+base_dir = Path(os.path.join(os.path.expanduser("~"), ".brainrender"))
+base_dir.mkdir(exist_ok=True)
