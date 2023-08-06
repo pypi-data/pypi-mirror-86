6e222e3c187e0c8cf2ab65de2626da06784cbad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:25 +0000

    Add package recruiter-utils/0.0.2/recruiter-utils-0.0.2.tar.gz

---
+++ packages/recruiter-utils/recruiter-utils-0.0.2.tar.gz/recruiter-utils-0.0.2/recruiter/utils/service_config_test.py
@@ -0,0 +1,37 @@
+from . import service_config
+
+
+def test_production_false(monkeypatch):
+    monkeypatch.setenv('SL_ENVIRONMENT', 'test')
+    assert service_config.is_production() is False
+
+
+def test_production_true(monkeypatch):
+    monkeypatch.setenv('SL_ENVIRONMENT', 'prod')
+    assert service_config.is_production() is True
+
+
+def test_stage_false(monkeypatch):
+    monkeypatch.setenv('SL_ENVIRONMENT', 'test')
+    assert service_config.is_stage() is False
+
+
+def test_stage_true(monkeypatch):
+    monkeypatch.setenv('SL_ENVIRONMENT', 'stage')
+    assert service_config.is_stage() is True
+
+
+def test_version(monkeypatch):
+    monkeypatch.setenv('SL_VERSION', 'recruiter-1912/dev:0.0.1')
+    assert service_config.get_version() == 'recruiter-1912/dev:0.0.1'
+
+
+def test_version_short(monkeypatch):
+    monkeypatch.setenv('SL_VERSION', 'recruiter-1912/dev:0.0.1')
+    assert service_config.get_version_short() == 'dev:0.0.1'
+
+
+def test_get_info(monkeypatch):
+    monkeypatch.setenv('SL_VERSION', 'recruiter-1912/dev:0.0.1')
+    monkeypatch.setenv('SL_ENVIRONMENT', 'test')
+    assert service_config.get_info() == {'version': 'recruiter-1912/dev:0.0.1', 'environment': 'test'}
