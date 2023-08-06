85c813a6d550b5de41aeec80f22ca45eabc6145b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:50 +0000

    Add package finstmt/0.7.0/finstmt-0.7.0-py3-none-any.whl

---
+++ packages/finstmt/finstmt-0.7.0-py3-none-any.whl/finstmt/exc.py
@@ -0,0 +1,51 @@
+
+class FinstmtException(Exception):
+    pass
+
+
+class NotACalculatedItemException(FinstmtException):
+    pass
+
+
+class NoSuchItemException(FinstmtException):
+    pass
+
+
+class CouldNotParseException(FinstmtException):
+    pass
+
+
+class MissingDataException(FinstmtException):
+    pass
+
+
+class MixedFrequencyException(FinstmtException):
+    pass
+
+
+class MismatchingDatesException(FinstmtException):
+    pass
+
+
+class ForecastException(FinstmtException):
+    pass
+
+
+class ForecastNotFitException(ForecastException):
+    pass
+
+
+class ForecastNotPredictedException(ForecastException):
+    pass
+
+
+class ImproperManualForecastException(ForecastException):
+    pass
+
+
+class BalanceSheetNotBalancedException(ForecastException):
+    pass
+
+
+class InvalidForecastEquationException(ForecastException):
+    pass
