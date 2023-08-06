53b12555ec4dd6d7bbbe4fe33b343757030daef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:51 +0000

    Add package databricks-bundle/0.5.9/databricks_bundle-0.5.9-py3-none-any.whl

---
+++ packages/databricks-bundle/databricks_bundle-0.5.9-py3-none-any.whl/databricksbundle/spark/NotebookSessionFactory.py
@@ -0,0 +1,23 @@
+from typing import List
+from pyspark.sql import SparkSession # pylint: disable = unused-import
+from databricksbundle.spark.SparkSessionLazy import SparkSessionLazy
+import IPython
+from databricksbundle.spark.config.ConfiguratorInterface import ConfiguratorInterface
+
+class NotebookSessionFactory:
+
+    def __init__(
+        self,
+        configurators: List[ConfiguratorInterface],
+    ):
+        self.__configurators = configurators
+
+    def create(self) -> SparkSessionLazy:
+        spark = IPython.get_ipython().user_ns['spark'] # type: SparkSession
+
+        for configurator in self.__configurators:
+            configurator.configure(spark)
+
+        IPython.get_ipython().user_ns['spark'] = spark
+
+        return SparkSessionLazy(lambda: spark)
