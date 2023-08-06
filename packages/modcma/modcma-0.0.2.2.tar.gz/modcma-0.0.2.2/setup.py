efe55ad986b16adae147288455476ffd7ff96bbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:41 +0000

    Add package modcma/0.0.2.2/modcma-0.0.2.2.tar.gz

---
+++ packages/modcma/modcma-0.0.2.2.tar.gz/modcma-0.0.2.2/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+    
+__version__ = "0.0.2.2"
+
+# CI will change the above to match the github tag,
+# if you run this manually, change __version__. 
+assert __version__ != "auto"
+
+setuptools.setup(
+    name='modcma',
+    version=__version__,
+    author="Jacob de Nobel",
+    author_email="jacobdenobel@gmail.com",
+    description="Package Containing Modular CMA-ES optimizer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'ghalton',
+        'sobol_seq',
+        'iohexperimenter'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
