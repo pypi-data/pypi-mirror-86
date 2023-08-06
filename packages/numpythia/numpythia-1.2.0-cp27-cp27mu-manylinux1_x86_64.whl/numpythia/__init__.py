6216e4b0ce388f9392763a35d7994a6acb1a42fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:32 +0000

    Add package numpythia/1.2.0/numpythia-1.2.0-cp27-cp27mu-manylinux1_x86_64.whl

---
+++ packages/numpythia/numpythia-1.2.0-cp27-cp27mu-manylinux1_x86_64.whl/numpythia/__init__.py
@@ -0,0 +1,28 @@
+from ._libnumpythia import _Pythia as Pythia, ReaderAscii, WriterAscii
+from ._libnumpythia import FILTERS
+import logging
+
+locals().update(FILTERS)
+
+log = logging.getLogger(__name__)
+
+__all__ = [
+    'Pythia',
+    'hepmc_read',
+    'hepmc_write',
+]
+
+PYTHIA_VERSION = '8.244'
+HEPMC_VERSION = '3.0.0'
+
+
+def hepmc_read(filename):
+    reader = ReaderAscii(filename)
+    for event in reader:
+        yield event
+
+def hepmc_write(filename, source):
+    writer = WriterAscii(filename)
+    for event in source:
+        writer.write(event)
+        yield event
