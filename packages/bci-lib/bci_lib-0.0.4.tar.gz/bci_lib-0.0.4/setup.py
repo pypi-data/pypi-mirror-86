c3c7e576c2ad62765b017173d72f2182b7c7c137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:21 +0000

    Add package bci-lib/0.0.4/bci_lib-0.0.4.tar.gz

---
+++ packages/bci-lib/bci_lib-0.0.4.tar.gz/bci_lib-0.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name='bci_lib',
+	version='0.0.4',
+	description='A useful tool for easily building bci pipelines.',
+	author='Sahand Sadeghpour',
+	author_email='sahand.2k.ss@gmail.com',
+	package_dir={'': 'src'},
+
+)
