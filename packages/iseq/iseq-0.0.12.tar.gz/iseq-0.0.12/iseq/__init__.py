7640860f5e3a6005bdf1f0e97355061503bf0228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:45 +0000

    Add package iseq/0.0.12/iseq-0.0.12.tar.gz

---
+++ packages/iseq/iseq-0.0.12.tar.gz/iseq-0.0.12/iseq/__init__.py
@@ -0,0 +1,20 @@
+from importlib import import_module as _import_module
+
+from . import gff, hmmer3, interval, protein
+from ._cli import cli
+from ._testit import test
+
+try:
+    __version__ = getattr(_import_module("iseq._version"), "version", "x.x.x")
+except ModuleNotFoundError:
+    __version__ = "x.x.x"
+
+__all__ = [
+    "__version__",
+    "cli",
+    "gff",
+    "hmmer3",
+    "interval",
+    "protein",
+    "test",
+]
