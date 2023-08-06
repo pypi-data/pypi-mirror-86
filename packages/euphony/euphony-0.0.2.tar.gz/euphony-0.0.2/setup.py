368afdfe50ec98724b9bcff5c7f95e94bda3571a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:23 +0000

    Add package euphony/0.0.2/euphony-0.0.2.tar.gz

---
+++ packages/euphony/euphony-0.0.2.tar.gz/euphony-0.0.2/setup.py
@@ -0,0 +1,33 @@
+"""Setup script for euphony"""
+
+import os.path
+from setuptools import setup
+
+# The directory containing this file
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# The text of the README file
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
+
+# This call to setup() does all the work
+setup(
+    name="euphony",
+    version="0.0.2",
+    description="Play music while your code runs",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vinaykale64/euphony",
+    author="Vinay Dinkar Kale",
+    author_email="vinaykale64@protonmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["euphony"],
+    include_package_data=True,
+    install_requires=[
+        'pygame>=2.0.0'
+    ]
+)
