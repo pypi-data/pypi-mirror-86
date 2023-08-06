61760d2bbc76dafd8523ea0a6ef97700df83b850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:06 +0000

    Add package waiter/1.2/waiter-1.2.tar.gz

---
+++ packages/waiter/waiter-1.2.tar.gz/waiter-1.2/tests/test_all.py
@@ -0,0 +1,158 @@
+import asyncio
+import itertools
+import pytest
+from waiter import wait, first
+
+
+def test_constructors():
+    w = wait([1] * 3)
+    assert w.delays == [1, 1, 1]
+    assert w.timeout == float('inf')
+
+    w = wait(1, timeout=10)
+    assert next(w.delays) == 1
+    assert w.timeout == 10
+
+    assert list(wait(1)[:3].delays) == [1, 1, 1]
+    assert list((wait(range(5)) <= 3).delays) == [0, 1, 2, 3, 3]
+    assert list((wait(range(5)) >= 2).delays) == [2, 2, 2, 3, 4]
+    assert list((wait(1)[:3] + 1).delays) == [1, 2, 3]
+    w = wait(1)[:3] * 2
+    assert list(w.delays) == list(w.delays) == [1, 2, 4]
+    for delay in wait(1)[:100].random(-1, 1).delays:
+        assert 0 <= delay < 2
+
+    assert list(wait.fibonacci(1)[:5].delays) == [1, 1, 2, 3, 5]
+    assert list(wait.count(1)[:5].delays) == [1, 2, 3, 4, 5]
+    if hasattr(itertools, 'accumulate'):
+        assert list(wait.accumulate(range(5)).delays) == [0, 1, 3, 6, 10]
+    assert list(wait.exponential(2)[:5].delays) == [1, 2, 4, 8, 16]
+    assert list(wait.polynomial(2)[:5].delays) == [0, 1, 4, 9, 16]
+
+
+def test_iteration():
+    w = wait([0] * 2)
+    assert ''.join(w.throttle('ab')) == 'ab'
+
+    it = iter('abcde')
+    throttled = w.throttle(it)
+    assert next(it) == 'a'
+    assert ''.join(throttled) == 'bcd'
+    assert next(it) == 'e'
+
+    assert ''.join(w.stream('abc')) == 'abc'
+    assert ''.join(w.stream('abc', 2)) == 'abc'
+    assert ''.join(w.stream(iter('abc'), 2)) == 'abc'
+    seq = ['a']
+    it = w.stream(seq)
+    assert next(it) == 'a'
+    seq.append('b')
+    assert list(it) == ['b']
+    assert list(w.suppressed(ValueError, int, 'a0')) == [('0', 0)]
+    assert list(w.filtered(str.isalpha, str.upper, iter('0a'))) == [('a', 'A')]
+
+
+def test_functional():
+    w = wait([0] * 2)
+    assert w.retry(ValueError, lambda it: int(next(it)), iter('ab0')) == 0
+    with pytest.raises(ValueError):
+        w.retry(ValueError, lambda it: int(next(it)), iter('abc'))
+
+    assert w.poll(str.islower, next, iter('ABc')) == 'c'
+    with pytest.raises(StopIteration):
+        assert w.poll(str.islower, next, iter('ABC'))
+
+    assert first(str.islower, w.repeat(next, iter('ABC')), None) is None
+    assert list(wait(1, timeout=-1)) == [0.0]
+
+
+def test_waiting():
+    elapsed = list(wait(0.1, timeout=1) * 2)
+    assert elapsed[0] == 0.0
+    assert 0.1 <= elapsed[1] < 0.3 <= elapsed[2] < 0.7 <= elapsed[3] < 1.0 <= elapsed[4] < 1.1
+
+
+def test_decorators():
+    w = wait(0)[:2]
+
+    @w.repeating
+    def func(x):
+        return x
+
+    assert ''.join(func('x')) == 'xxx'
+
+    @w.retrying(ValueError)
+    def func(it):
+        return int(next(it))
+
+    assert func(iter('ab0')) == 0
+
+    @w.polling(str.islower)
+    def func(it):
+        return next(it)
+
+    assert func(iter('ABc')) == 'c'
+
+    class cls:
+        @w.repeating
+        def repeat(self):
+            return self
+
+        @w.retrying(Exception)
+        def retry(self):
+            return self
+
+        @w.polling(bool)
+        def poll(self):
+            return self
+
+    obj = cls()
+    assert next(obj.repeat()) is obj
+    assert obj.retry() is obj
+    assert obj.poll() is obj
+
+
+def test_stats():
+    w = wait([0] * 2)
+    for index, elapsed in enumerate(w):
+        assert elapsed >= 0.0
+        assert w.stats.total == w.stats.failures + 1
+    assert w.stats == dict.fromkeys(range(3), 1)
+    assert next(iter(w)) == 0.0
+    assert w.stats.total == 4
+    assert w.stats.failures == 2
+
+
+def test_async():
+    run = asyncio.get_event_loop().run_until_complete
+
+    ws = wait([]), wait(0)
+    for ait in (ws[0].throttle(ws[1]), ws[1].throttle(ws[0])):
+        anext = ait.__aiter__().__anext__
+        assert run(anext()) == 0.0
+        with pytest.raises(StopAsyncIteration):
+            run(anext())
+    assert ws[0].stats.total == 2
+    assert ws[0].stats.failures == 0
+    assert ws[1].stats.total == 3
+    assert ws[1].stats.failures == 1
+
+    anext = wait([0.1, 0.1], timeout=0.1).__aiter__().__anext__
+    assert run(anext()) == 0.0
+    assert run(anext()) > 0.0
+    with pytest.raises(StopAsyncIteration):
+        run(anext())
+
+    w = wait([])
+    anext = w.repeat(asyncio.sleep, 0).__aiter__().__anext__
+    assert run(anext()) is None
+    with pytest.raises(StopAsyncIteration):
+        run(anext())
+
+    assert run(w.retry(TypeError, asyncio.sleep, 0)) is None
+    with pytest.raises(TypeError):
+        run(w.retry(TypeError, asyncio.sleep))
+
+    assert run(w.poll(None.__eq__, asyncio.sleep, 0)) is None
+    with pytest.raises(StopAsyncIteration):
+        run(w.poll(bool, asyncio.sleep, 0))
---
+++ packages/waiter/waiter-1.2.tar.gz/waiter-1.2/waiter/__init__.py
@@ -0,0 +1,268 @@
+# type: ignore[no-redef]
+import asyncio
+import collections
+import contextlib
+import itertools
+import operator
+import random
+import time
+import types
+from functools import partial
+from typing import AsyncIterable, Callable, Iterable, Iterator, Sequence
+from multimethod import multimethod, overload  # type: ignore
+
+__version__ = '1.2'
+iscoro = asyncio.iscoroutinefunction
+
+
+def fibonacci(x, y):
+    """Generate fibonacci sequence."""
+    while True:
+        yield x
+        x, y = y, (x + y)
+
+
+@contextlib.contextmanager
+def suppress(*exceptions: Exception):
+    """Variant of `contextlib.suppress`, which also records exception."""
+    excs = []
+    try:
+        yield excs
+    except exceptions as exc:
+        excs.append(exc)
+
+
+def first(predicate: Callable, iterable: Iterable, *default):
+    """Return first item which evaluates to true, like `any` with filtering."""
+    return next(filter(predicate, iterable), *default)
+
+
+class reiter(partial):
+    """A partial iterator which is re-iterable."""
+
+    __iter__ = partial.__call__
+
+
+class partialmethod(partial):
+    """Variant of functools.partialmethod."""
+
+    def __get__(self, instance, owner):
+        return self if instance is None else types.MethodType(self, instance)
+
+
+class Stats(collections.Counter):
+    """Mapping of attempt counts."""
+
+    def add(self, attempt, elapsed):
+        """Record attempt and return next value."""
+        self[attempt] += 1
+        return elapsed
+
+    @property
+    def total(self):
+        """total number of attempts"""
+        return sum(self.values())
+
+    @property
+    def failures(self):
+        """number of repeat attempts"""
+        return self.total - self[0]
+
+
+def grouped(queue, size=None):
+    """Generate slices from a sequence without relying on a fixed `len`."""
+    group, start = queue[:size], 0
+    while group:
+        start += len(group)
+        yield group
+        group = queue[start : size and start + size]
+
+
+class waiter:
+    """An iterable which sleeps for given delays.
+
+    Args:
+        delays: any iterable of seconds, or a scalar which is repeated endlessly
+        timeout: optional timeout for iteration
+    """
+
+    Stats = Stats
+
+    def __init__(self, delays, timeout=float('inf')):
+        with suppress(TypeError) as excs:
+            iter(delays)
+        self.delays = itertools.repeat(delays) if excs else delays
+        self.timeout = timeout
+        self.stats = self.Stats()
+
+    def __iter__(self):
+        """Generate a slow loop of elapsed time."""
+        start = time.time()
+        yield self.stats.add(0, 0.0)
+        for attempt, delay in enumerate(self.delays, 1):
+            remaining = start + self.timeout - time.time()
+            if remaining < 0:
+                break
+            time.sleep(min(delay, remaining))
+            yield self.stats.add(attempt, time.time() - start)
+
+    async def __aiter__(self):
+        """Asynchronously generate a slow loop of elapsed time."""
+        start = time.time()
+        yield self.stats.add(0, 0.0)
+        for attempt, delay in enumerate(self.delays, 1):
+            remaining = start + self.timeout - time.time()
+            if remaining < 0:
+                break
+            await asyncio.sleep(min(delay, remaining))
+            yield self.stats.add(attempt, time.time() - start)
+
+    def clone(self, func, *args):
+        return type(self)(reiter(func, *args), self.timeout)
+
+    def map(self, func: Callable, *iterables: Iterable) -> 'waiter':
+        """Return new waiter with function mapped across delays."""
+        return self.clone(map, func, self.delays, *iterables)
+
+    @classmethod
+    def fibonacci(cls, delay, **kwargs) -> 'waiter':
+        """Create waiter with fibonacci backoff."""
+        return cls(reiter(fibonacci, delay, delay), **kwargs)
+
+    @classmethod
+    def count(cls, *args, **kwargs) -> 'waiter':
+        """Create waiter based on `itertools.count`."""
+        return cls(reiter(itertools.count, *args), **kwargs)
+
+    @classmethod
+    def accumulate(cls, *args, **kwargs) -> 'waiter':
+        """Create waiter based on `itertools.accumulate`."""
+        return cls(reiter(itertools.accumulate, *args), **kwargs)
+
+    @classmethod
+    def exponential(cls, base, **kwargs) -> 'waiter':
+        """Create waiter with exponential backoff."""
+        return cls.count(**kwargs).map(base.__pow__)
+
+    @classmethod
+    def polynomial(cls, exp, **kwargs) -> 'waiter':
+        """Create waiter with polynomial backoff."""
+        return cls.count(**kwargs).map(exp.__rpow__)
+
+    def __getitem__(self, slc: slice) -> 'waiter':
+        """Slice delays, e.g., to limit attempt count."""
+        return self.clone(itertools.islice, self.delays, slc.start, slc.stop, slc.step)
+
+    def __le__(self, ceiling) -> 'waiter':
+        """Limit maximum delay generated."""
+        return self.map(partial(min, ceiling))
+
+    def __ge__(self, floor) -> 'waiter':
+        """Limit minimum delay generated."""
+        return self.map(partial(max, floor))
+
+    def __add__(self, step) -> 'waiter':
+        """Generate incremental backoff."""
+        return self.map(operator.add, reiter(itertools.count, 0, step))
+
+    def __mul__(self, factor) -> 'waiter':
+        """Generate exponential backoff."""
+        return self.map(operator.mul, reiter(map, factor.__pow__, reiter(itertools.count)))
+
+    def random(self, start, stop) -> 'waiter':
+        """Add random jitter within given range."""
+        return self.map(lambda delay: delay + random.uniform(start, stop))
+
+    @multimethod
+    def throttle(self, iterable):
+        """Delay iteration."""
+        return map(operator.itemgetter(1), zip(self, iterable))
+
+    @multimethod
+    async def throttle(self, iterable: AsyncIterable):
+        anext = iterable.__aiter__().__anext__
+        with suppress(StopAsyncIteration):
+            async for _ in self:
+                yield await anext()
+
+    def stream(self, queue: Sequence, size: int = None) -> Iterator:
+        """Generate chained values in groups from an iterable.
+
+        The queue can be extended while in use.
+        """
+        it = iter(queue)
+        groups = iter(lambda: list(itertools.islice(it, size)), [])
+        if isinstance(queue, Sequence):
+            groups = grouped(queue, size)
+        return itertools.chain.from_iterable(self.throttle(groups))
+
+    def suppressed(self, exception, func: Callable, iterable: Iterable) -> Iterator[tuple]:
+        """Generate `arg, func(arg)` pairs while exception isn't raised."""
+        queue = list(iterable)
+        for arg in self.stream(queue):
+            try:
+                yield arg, func(arg)
+            except exception:
+                queue.append(arg)
+
+    def filtered(self, predicate: Callable, func: Callable, iterable: Iterable) -> Iterator[tuple]:
+        """Generate `arg, func(arg)` pairs while predicate evaluates to true."""
+        queue = list(iterable)
+        for arg in self.stream(queue):
+            result = func(arg)
+            if predicate(result):
+                yield arg, result
+            else:
+                queue.append(arg)
+
+    @overload
+    def repeat(self, func, *args, **kwargs):
+        """Repeat function call."""
+        return (func(*args, **kwargs) for _ in self)
+
+    @overload
+    async def repeat(self, func: iscoro, *args, **kwargs):  # type: ignore
+        async for _ in self:
+            yield await func(*args, **kwargs)  # type: ignore
+
+    @overload
+    def retry(self, exception, func, *args, **kwargs):
+        """Repeat function call until exception isn't raised."""
+        for _ in self:
+            with suppress(exception) as excs:
+                return func(*args, **kwargs)
+        raise excs[0]
+
+    @overload
+    async def retry(self, exception, func: iscoro, *args, **kwargs):  # type: ignore
+        async for _ in self:
+            with suppress(exception) as excs:
+                return await func(*args, **kwargs)  # type: ignore
+        raise excs[0]
+
+    @overload
+    def poll(self, predicate, func, *args, **kwargs):
+        """Repeat function call until predicate evaluates to true."""
+        return first(predicate, self.repeat(func, *args, **kwargs))
+
+    @overload
+    async def poll(self, predicate, func: iscoro, *args, **kwargs):  # type: ignore
+        async for result in self.repeat(func, *args, **kwargs):
+            if predicate(result):
+                return result
+        raise StopAsyncIteration
+
+    def repeating(self, func: Callable):
+        """A decorator for [repeat][waiter.wait.repeat]."""
+        return partialmethod(self.repeat, func)
+
+    def retrying(self, exception: Exception):
+        """Return a decorator for [retry][waiter.wait.retry]."""
+        return partial(partialmethod, self.retry, exception)
+
+    def polling(self, predicate: Callable):
+        """Return a decorator for [poll][waiter.wait.poll]."""
+        return partial(partialmethod, self.poll, predicate)
+
+
+wait = waiter
