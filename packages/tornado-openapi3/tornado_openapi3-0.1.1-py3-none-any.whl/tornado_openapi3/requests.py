54acfa57ed4dba3272280e936e002fe5c3386799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:35 +0000

    Add package tornado-openapi3/0.1.1/tornado_openapi3-0.1.1-py3-none-any.whl

---
+++ packages/tornado-openapi3/tornado_openapi3-0.1.1-py3-none-any.whl/tornado_openapi3/requests.py
@@ -0,0 +1,42 @@
+import itertools
+
+from openapi_core.validation.request.datatypes import (  # type: ignore
+    RequestParameters,
+    OpenAPIRequest,
+)
+from openapi_core.validation.request import validators  # type: ignore
+from tornado.httputil import HTTPServerRequest  # type: ignore
+from werkzeug.datastructures import ImmutableMultiDict, Headers
+
+
+class TornadoRequestFactory:
+    @classmethod
+    def create(cls, request: HTTPServerRequest) -> OpenAPIRequest:
+        if request.uri:
+            path, _, _ = request.uri.partition("?")
+        else:
+            path = ""
+        query_arguments: ImmutableMultiDict[str, str] = ImmutableMultiDict(
+            itertools.chain(
+                *[
+                    [(k, v.decode("utf-8")) for v in vs]
+                    for k, vs in request.query_arguments.items()
+                ]
+            )
+        )
+        return OpenAPIRequest(
+            full_url_pattern=path,
+            method=request.method.lower() if request.method else "get",
+            parameters=RequestParameters(
+                query=query_arguments, header=Headers(request.headers.get_all())
+            ),
+            body=request.body.decode("utf-8"),
+            mimetype=request.headers.get(
+                "Content-Type", "application/x-www-form-urlencoded"
+            ),
+        )
+
+
+class RequestValidator(validators.RequestValidator):
+    def validate(self, request: HTTPServerRequest):
+        return super().validate(TornadoRequestFactory.create(request))
