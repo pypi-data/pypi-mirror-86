ba0ff9e2d67b6f699d303a09217821b1f3c5b65c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:50 +0000

    Add package dune-localfunctions/2.8.0.dev20201126/dune-localfunctions-2.8.0.dev20201126.tar.gz

---
+++ packages/dune-localfunctions/dune-localfunctions-2.8.0.dev20201126.tar.gz/dune-localfunctions-2.8.0.dev20201126/setup.py
@@ -0,0 +1,40 @@
+import sys, os
+from setuptools import find_packages
+from skbuild import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="dune-localfunctions",
+    version="2.8.0.dev20201126",
+    author="The Dune Core developers",
+    author_email="dune-devel@lists.dune-project.org",
+    description="Provides interface and implementation for shape functions defined on the DUNE reference elements",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.dune-project.org/core/dune-localfunctions",
+    packages=find_packages(where="python"),
+    package_dir={"": "python"},
+    install_requires=['dune-geometry >= 2.8.0.dev0'],
+    classifiers=[
+        "Programming Language :: C++",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+    ],
+    python_requires='>=3.4',
+    cmake_args=[
+        '-DBUILD_SHARED_LIBS=TRUE',
+        '-DDUNE_ENABLE_PYTHONBINDINGS=TRUE',
+        '-DDUNE_PYTHON_INSTALL_LOCATION=none',
+        '-DDUNE_GRID_GRIDTYPE_SELECTOR=ON',
+        '-DALLOW_CXXFLAGS_OVERWRITE=ON',
+        '-DUSE_PTHREADS=ON',
+        '-DCMAKE_BUILD_TYPE=Release',
+        '-DCMAKE_DISABLE_FIND_PACKAGE_LATEX=TRUE',
+        '-DCMAKE_DISABLE_DOCUMENTATION=TRUE',
+        '-DINKSCAPE=FALSE',
+        '-DCMAKE_INSTALL_RPATH='+sys.prefix+'/lib/',
+        '-DCMAKE_MACOSX_RPATH=TRUE',
+    ]
+)
