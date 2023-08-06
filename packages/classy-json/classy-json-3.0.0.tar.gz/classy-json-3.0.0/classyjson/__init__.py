24c0873f1c7730fe013c1cf521bc11e0dcde4157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:03 +0000

    Add package classy-json/3.0.0/classy-json-3.0.0.tar.gz

---
+++ packages/classy-json/classy-json-3.0.0.tar.gz/classy-json-3.0.0/classyjson/__init__.py
@@ -0,0 +1,23 @@
+"""
+Dot-access for Python dictionaries and more
+"""
+
+__version__ = '3.0.0'
+
+from json import loads as json_loads
+from json import load as json_load
+from json import *
+
+from .types import classify, ClassyDict
+
+def loads(*args, **kwargs):
+    """Override loads function"""
+
+    _data = json_loads(*args, **kwargs)
+    return classify(_data)
+
+def load(*args, **kwargs):
+    """Override load function"""
+
+    _data = json_load(*args, **kwargs)
+    return classify(_data)
