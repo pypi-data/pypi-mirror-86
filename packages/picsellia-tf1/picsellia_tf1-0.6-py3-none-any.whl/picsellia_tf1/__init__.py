ef14132d48c8ac9ae052e68bf2ad8a6a7872c139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:37 +0000

    Add package picsellia-tf1/0.6/picsellia_tf1-0.6-py3-none-any.whl

---
+++ packages/picsellia-tf1/picsellia_tf1-0.6-py3-none-any.whl/picsellia_tf1/__init__.py
@@ -0,0 +1,11 @@
+import sys
+import os 
+from setuptools import find_packages
+import picsellia_tf1
+for p in find_packages(where=picsellia_tf1.__path__[0]):
+    sys.path.append(os.path.join(picsellia_tf1.__path__[0],p.replace('.','/')))
+
+sys.path.append(picsellia_tf1.__path__[0])
+sys.path.append(os.path.join(picsellia_tf1.__path__[0],'object_detection'))
+sys.path.append(os.path.join(picsellia_tf1.__path__[0],'slim'))
+
