2c1a5672136b99e12bd1e5447e41bdedf0c7c9b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:36 +0000

    Add package epiccli/2020.1.2/epiccli-2020.1.2-py3-none-any.whl

---
+++ packages/epiccli/epiccli-2020.1.2-py3-none-any.whl/epiccli/exceptions.py
@@ -0,0 +1,25 @@
+class ConfigurationException(Exception):
+    """Exception raised for errors with the client configuration."""
+
+    def __init__(self, msg):
+        self.msg = msg
+
+
+class ResponseError(Exception):
+    """Exception raised for errors with the response from EPIC"""
+
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
+
+
+class CommandError(Exception):
+    """Exception raised for errors supplied command"""
+
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
