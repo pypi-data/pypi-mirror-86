bbfc792c362b0b00443c36d87a7e61fcc03b6dc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:50 +0000

    Add package django-bulk-sync/3.2.0/django-bulk-sync-3.2.0.tar.gz

---
+++ packages/django-bulk-sync/django-bulk-sync-3.2.0.tar.gz/django-bulk-sync-3.2.0/bulk_sync/__init__.py
@@ -0,0 +1,212 @@
+from collections import OrderedDict, defaultdict
+import logging
+import functools
+
+from django.db import transaction, router
+from django.core.exceptions import FieldDoesNotExist
+
+logger = logging.getLogger(__name__)
+
+
+def bulk_sync(
+    new_models,
+    key_fields,
+    filters,
+    batch_size=None,
+    fields=None,
+    exclude_fields=None,
+    skip_creates=False,
+    skip_updates=False,
+    skip_deletes=False,
+    db_class=None,
+):
+    """ Combine bulk create, update, and delete.  Make the DB match a set of in-memory objects.
+
+    `new_models`: Django ORM objects that are the desired state.  They may or may not have `id` set.
+    `key_fields`: Identifying attribute name(s) to match up `new_models` items with database rows.  If a foreign key
+            is being used as a key field, be sure to pass the `fieldname_id` rather than the `fieldname`.
+    `filters`: Q() filters specifying the subset of the database to work in. Use `None` or `[]` if you want to sync
+            against the entire table.
+    `batch_size`: (optional) passes through to Django `bulk_create.batch_size` and `bulk_update.batch_size`, and controls
+            how many objects are created/updated per SQL query.
+    `fields`: (optional) list of fields to update. If not set, will sync all fields that are editable and not
+            auto-created.
+    `exclude_fields`: (optional) list of fields to exclude from updates. Subtracts from the passed-in `fields` or
+            default-calculated `fields` (see `fields` documentation above.)
+    `skip_creates`: If truthy, will not perform any object creations needed to fully sync. Defaults to not skip.
+    `skip_updates`: If truthy, will not perform any object updates needed to fully sync. Defaults to not skip.
+    `skip_deletes`: If truthy, will not perform any object deletions needed to fully sync. Defaults to not skip.
+    `db_class`: (optional) Model class to operate on. If new_models always contains at least one object, this can
+            be set automatically so is optional.
+    """
+
+    if db_class is None:
+        try:
+            db_class = new_models[0].__class__
+        except IndexError:
+            try:
+                db_class = new_models.model
+            except AttributeError:
+                db_class = None
+
+    if db_class is None:
+        raise RuntimeError(
+            "Unable to identify model to sync. Need to provide at least one object in `new_models`, provide "
+            "`db_class`, or set `new_models` with a queryset like `db_class.objects.none()`."
+        )
+
+    if fields is None:
+        # Get a list of fields that aren't PKs and aren't editable (e.g. auto_add_now) for bulk_update
+        fields = [
+            field.name
+            for field in db_class._meta.fields
+            if not field.primary_key and not field.auto_created and field.editable
+        ]
+
+    if exclude_fields is not None:
+        model_fields = set(field.name for field in db_class._meta.fields)
+        fields_to_update = set(fields)
+        fields_to_exclude = set(exclude_fields)
+
+        # Check that we're not attempting to exclude non-existent fields
+        if not fields_to_exclude <= model_fields:
+            raise FieldDoesNotExist(f'model "{db_class.__name__}" has no field(s) {fields_to_exclude - model_fields}')
+
+        fields = list(fields_to_update - fields_to_exclude)
+
+    using = router.db_for_write(db_class)
+    with transaction.atomic(using=using):
+        objs = db_class.objects.all()
+        if filters:
+            objs = objs.filter(filters)
+        objs = objs.only("pk", *key_fields).select_for_update()
+
+        prep_functions = defaultdict(lambda: lambda x: x)
+        prep_functions.update({
+            field.name: functools.partial(field.to_python)
+            for field in (db_class._meta.get_field(k) for k in key_fields)
+            if hasattr(field, 'to_python')
+        })
+
+        def get_key(obj, prep_values=False):
+            return tuple(
+                prep_functions[k](getattr(obj, k)) if prep_values else getattr(obj, k)
+                for k in key_fields
+            )
+
+        obj_dict = {get_key(obj): obj for obj in objs}
+
+        new_objs = []
+        existing_objs = []
+        for new_obj in new_models:
+            old_obj = obj_dict.pop(get_key(new_obj, prep_values=True), None)
+            if old_obj is None:
+                # This is a new object, so create it.
+                new_objs.append(new_obj)
+            else:
+                new_obj.pk = old_obj.pk
+                existing_objs.append(new_obj)
+
+        if not skip_creates:
+            db_class.objects.bulk_create(new_objs, batch_size=batch_size)
+
+        if not skip_updates:
+            db_class.objects.bulk_update(existing_objs, fields=fields, batch_size=batch_size)
+
+        if not skip_deletes:
+            # delete stale objects
+            objs.filter(pk__in=[_.pk for _ in list(obj_dict.values())]).delete()
+
+        assert len(existing_objs) == len(new_models) - len(new_objs)
+
+        stats = {
+            "created": 0 if skip_creates else len(new_objs),
+            "updated": 0 if skip_updates else (len(new_models) - len(new_objs)),
+            "deleted": 0 if skip_deletes else len(obj_dict),
+        }
+
+        logger.debug(
+            "{}: {} created, {} updated, {} deleted.".format(
+                db_class.__name__, stats["created"], stats["updated"], stats["deleted"]
+            )
+        )
+
+    return {"stats": stats}
+
+
+def bulk_compare(old_models, new_models, key_fields, ignore_fields=None):
+    """ Compare two sets of models by `key_fields`.
+    `old_models`: Iterable of Django ORM objects to compare.
+    `new_models`: Iterable of Django ORM objects to compare.
+    `key_fields`: Identifying attribute name(s) to match up `new_models` items with database rows.  If a foreign key
+            is being used as a key field, be sure to pass the `fieldname_id` rather than the `fieldname`.
+    `ignore_fields`: (optional) If set, provide field names that should not be considered when comparing objects.
+
+    Returns: dict of
+        'added': list of all added objects.
+        'unchanged': list of all unchanged objects.
+        'updated': list of all updated objects.
+        'updated_details': dict of {obj: {field_name: (old_value, new_value)}} for all changed fields in each updated object.
+        'removed': list of all removed objects.
+
+    """
+
+    def get_key(obj):
+        return tuple(getattr(obj, k) for k in key_fields)
+
+    old_obj_dict = OrderedDict((get_key(obj), obj) for obj in old_models)
+
+    new_objs = []
+    change_details = {}
+    updated_objs = []
+    unchanged_objs = []
+
+    for new_obj in new_models:
+        old_obj = old_obj_dict.pop(get_key(new_obj), None)
+        if old_obj is None:
+            # This is a new object, so create it.
+            new_objs.append(new_obj)
+        else:
+            new_obj.pk = old_obj.pk
+
+            cmp_result = compare_objs(old_obj, new_obj, ignore_fields)
+            if cmp_result:
+                updated_objs.append(new_obj)
+                change_details[new_obj] = cmp_result
+            else:
+                unchanged_objs.append(new_obj)
+
+    return {
+        "added": new_objs,
+        "unchanged": unchanged_objs,
+        "updated": updated_objs,
+        "updated_details": change_details,
+        "removed": old_obj_dict.values(),
+    }
+
+
+def compare_objs(obj1, obj2, ignore_fields=None):
+    """ Compare two Django ORM objects (presumably of the same model class).
+
+    `obj1`: The first object to compare.
+    `obj2`: The second object to compare.
+    `key_fields`: Identifying attribute name(s) to match up `new_models` items with database rows.  If a foreign key
+            is being used as a key field, be sure to pass the `fieldname_id` rather than the `fieldname`.
+    `ignore_fields`: (optional) If set, provide field names that should not be considered when comparing objects.
+            If a foreign key is being used as an ignore_field, be sure to pass the `fieldname_id` rather than the `fieldname`.
+
+    Returns: dict of changed fields and their old/new values: {field_name: (old_value, new_value)}
+    """
+
+    ret = {}
+    fields = obj1._meta.get_fields()
+    for f in fields:
+        if ignore_fields and f.attname in ignore_fields:
+            continue
+
+        v1 = f.to_python(getattr(obj1, f.attname))
+        v2 = f.to_python(getattr(obj2, f.attname))
+        if v1 != v2:
+            ret[f.name] = (v1, v2)
+
+    return ret
---
+++ packages/django-bulk-sync/django-bulk-sync-3.2.0.tar.gz/django-bulk-sync-3.2.0/tests/tests.py
@@ -0,0 +1,414 @@
+from django.conf import settings
+from django.db import IntegrityError
+from django.core.exceptions import FieldDoesNotExist
+from django.db.models import Q
+from django.test import TestCase
+
+from bulk_sync import bulk_compare
+from bulk_sync import bulk_sync
+from .models import Company, Employee, EmployeeDifferentPk
+
+
+class BulkSyncTests(TestCase):
+    """ Test `bulk_sync` method """
+
+    def setUp(self):
+        pass
+
+    def test_all_features_at_once(self):
+        c1 = Company.objects.create(name="Foo Products, Ltd.")
+        c2 = Company.objects.create(name="Bar Microcontrollers, Inc.")
+
+        e1 = Employee.objects.create(name="Scott", age=40, company=c1)
+        e2 = Employee.objects.create(name="Isaac", age=9, company=c1)
+        e3 = Employee.objects.create(name="Zoe", age=9, company=c1)
+        e4 = Employee.objects.create(name="Bob", age=25, company=c2)
+
+        # We should update Scott's and Isaac's age, delete Zoe, add Newguy and
+        # add a second Bob (since he's not in company c1, which we filtered on.)
+        new_objs = [
+            Employee(name="Scott", age=41, company=c1),
+            Employee(name="Isaac", age=9, company=c1),
+            Employee(name="Newguy", age=10, company=c1),
+            Employee(name="Bob", age=50, company=c1),
+        ]
+
+        ret = bulk_sync(new_models=new_objs, filters=Q(company_id=c1.id), key_fields=("name",))
+
+        self.assertEqual(2, ret["stats"]["updated"])
+        self.assertEqual(2, ret["stats"]["created"])
+        self.assertEqual(1, ret["stats"]["deleted"])
+
+        self.assertEqual(4, Employee.objects.filter(company=c1).count())
+        self.assertEqual(1, Employee.objects.filter(company=c2).count())
+
+        new_e1 = Employee.objects.get(id=e1.id)
+        self.assertEqual("Scott", new_e1.name)
+        self.assertEqual(41, new_e1.age)
+        self.assertEqual(c1, new_e1.company)
+
+        new_e2 = Employee.objects.get(id=e2.id)
+        self.assertEqual("Isaac", new_e2.name)
+        self.assertEqual(9, new_e2.age)
+        self.assertEqual(c1, new_e2.company)
+
+        with self.assertRaises(Employee.DoesNotExist):
+            Employee.objects.get(id=e3.id)
+
+        new_e4 = Employee.objects.get(id=e4.id)
+        self.assertEqual("Bob", new_e4.name)
+        self.assertEqual(25, new_e4.age)
+        self.assertEqual(c2, new_e4.company)
+
+        new_e3 = Employee.objects.get(name="Newguy")
+        self.assertEqual("Newguy", new_e3.name)
+        self.assertEqual(10, new_e3.age)
+        self.assertEqual(c1, new_e3.company)
+
+        new_e5 = Employee.objects.get(name="Bob", company=c1)
+        self.assertEqual("Bob", new_e5.name)
+        self.assertEqual(50, new_e5.age)
+        self.assertEqual(c1, new_e5.company)
+
+    def test_all_features_at_once_nonstandard_pk(self):
+        c1 = Company.objects.create(name="Foo Products, Ltd.")
+        c2 = Company.objects.create(name="Bar Microcontrollers, Inc.")
+
+        e1 = EmployeeDifferentPk.objects.create(name="Scott", age=40, company=c1)
+        e2 = EmployeeDifferentPk.objects.create(name="Isaac", age=9, company=c1)
+        e3 = EmployeeDifferentPk.objects.create(name="Zoe", age=9, company=c1)
+        e4 = EmployeeDifferentPk.objects.create(name="Bob", age=25, company=c2)
+
+        # We should update Scott's and Isaac's age, delete Zoe, add Newguy and
+        # add a second Bob (since he's not in company c1, which we filtered on.)
+        new_objs = [
+            EmployeeDifferentPk(name="Scott", age=41, company=c1),
+            EmployeeDifferentPk(name="Isaac", age=9, company=c1),
+            EmployeeDifferentPk(name="Newguy", age=10, company=c1),
+            EmployeeDifferentPk(name="Bob", age=50, company=c1),
+        ]
+
+        ret = bulk_sync(new_models=new_objs, filters=Q(company_id=c1.id), key_fields=("name",))
+
+        self.assertEqual(2, ret["stats"]["updated"])
+        self.assertEqual(2, ret["stats"]["created"])
+        self.assertEqual(1, ret["stats"]["deleted"])
+
+        self.assertEqual(4, EmployeeDifferentPk.objects.filter(company=c1).count())
+        self.assertEqual(1, EmployeeDifferentPk.objects.filter(company=c2).count())
+
+        new_e1 = EmployeeDifferentPk.objects.get(employee_id=e1.employee_id)
+        self.assertEqual("Scott", new_e1.name)
+        self.assertEqual(41, new_e1.age)
+        self.assertEqual(c1, new_e1.company)
+
+        new_e2 = EmployeeDifferentPk.objects.get(employee_id=e2.employee_id)
+        self.assertEqual("Isaac", new_e2.name)
+        self.assertEqual(9, new_e2.age)
+        self.assertEqual(c1, new_e2.company)
+
+        with self.assertRaises(EmployeeDifferentPk.DoesNotExist):
+            EmployeeDifferentPk.objects.get(employee_id=e3.employee_id)
+
+        new_e4 = EmployeeDifferentPk.objects.get(employee_id=e4.employee_id)
+        self.assertEqual("Bob", new_e4.name)
+        self.assertEqual(25, new_e4.age)
+        self.assertEqual(c2, new_e4.company)
+
+        new_e3 = EmployeeDifferentPk.objects.get(name="Newguy")
+        self.assertEqual("Newguy", new_e3.name)
+        self.assertEqual(10, new_e3.age)
+        self.assertEqual(c1, new_e3.company)
+
+        new_e5 = EmployeeDifferentPk.objects.get(name="Bob", company=c1)
+        self.assertEqual("Bob", new_e5.name)
+        self.assertEqual(50, new_e5.age)
+        self.assertEqual(c1, new_e5.company)
+
+    def test_provided_pk_is_retained_but_raises_if_mismatch_with_keyfield(self):
+        c1 = Company.objects.create(name="Foo Products, Ltd.")
+        e1 = Employee.objects.create(name="Scott", age=40, company=c1)
+        new_objs = [Employee(id=e1.id, name="Notscott", age=41, company=c1)]
+
+        with self.assertRaises(IntegrityError):
+            # Crashes because e1.id already exists in database, even though 'name' doesnt match so it tries to INSERT.
+            ret = bulk_sync(new_models=new_objs, filters=Q(company_id=c1.id), key_fields=("name",))
+
+        unique_pk = Employee.objects.values_list("id", flat=True).order_by("-id").first() + 1
+        new_objs = [Employee(id=unique_pk, name="Notscott", age=41, company=c1)]
+        ret = bulk_sync(new_models=new_objs, filters=Q(company_id=c1.id), key_fields=("name",))
+
+        self.assertEqual(0, ret["stats"]["updated"])
+        self.assertEqual(1, ret["stats"]["created"])  # Added 'Notscott'
+        self.assertEqual(1, ret["stats"]["deleted"])  # Deleted 'Scott'
+
+        # Make sure we retained the PK
+        self.assertEqual(Employee.objects.filter(id=unique_pk).count(), 1)
+
+    def test_fields_parameter(self):
+        c1 = Company.objects.create(name="Foo Products, Ltd.")
+        c2 = Company.objects.create(name="Bar Microcontrollers, Inc.")
+
+        e1 = Employee.objects.create(name="Scott", age=40, company=c1)
+        e2 = Employee.objects.create(name="Isaac", age=9, company=c2)
+
+        # We should update Scott's age, and not touch company.
+        new_objs = [Employee(name="Scott", age=41, company=c1), Employee(name="Isaac", age=9, company=c1)]
+
+        ret = bulk_sync(new_models=new_objs, filters=None, key_fields=("name",), fields=["age"])
+
+        new_e1 = Employee.objects.get(id=e1.id)
+        self.assertEqual("Scott", new_e1.name)
+        self.assertEqual(41, new_e1.age)
+        self.assertEqual(c1, new_e1.company)
+
+        new_e2 = Employee.objects.get(id=e2.id)
+        self.assertEqual("Isaac", new_e2.name)
+        self.assertEqual(9, new_e2.age)
+        self.assertEqual(c2, new_e2.company)
+
+        self.assertEqual(2, ret["stats"]["updated"])
+        self.assertEqual(0, ret["stats"]["created"])
+        self.assertEqual(0, ret["stats"]["deleted"])
+
+    def test_exclude_fields_parameter(self):
+        c1 = Company.objects.create(name="Foo Products, Ltd.")
+        c2 = Company.objects.create(name="Bar Microcontrollers, Inc.")
+
+        e1 = Employee.objects.create(name="Scott", age=40, company=c1)
+        e2 = Employee.objects.create(name="Isaac", age=9, company=c2)
+
+        # We should update Scott's age, and not touch company.
+        new_objs = [Employee(name="Scott", age=41, company=c1), Employee(name="Isaac", age=9, company=c1)]
+
+        with self.assertRaises(FieldDoesNotExist):
+            # Crashes because we attempted to exclude a field that does not exist
+            bulk_sync(new_models=new_objs, filters=None, key_fields=("name",), exclude_fields=["missing_field"])
+
+        ret = bulk_sync(new_models=new_objs, filters=None, key_fields=("name",), exclude_fields=["company"])
+
+        new_e1 = Employee.objects.get(id=e1.id)
+        self.assertEqual("Scott", new_e1.name)
+        self.assertEqual(41, new_e1.age)
+        self.assertEqual(c1, new_e1.company)
+
+        new_e2 = Employee.objects.get(id=e2.id)
+        self.assertEqual("Isaac", new_e2.name)
+        self.assertEqual(9, new_e2.age)
+        self.assertEqual(c2, new_e2.company)
+
+        self.assertEqual(2, ret["stats"]["updated"])
+        self.assertEqual(0, ret["stats"]["created"])
+        self.assertEqual(0, ret["stats"]["deleted"])
+
+    def test_skip_deletes(self):
+        c1 = Company.objects.create(name="My Company LLC")
+
+        e1 = Employee.objects.create(name="Scott", age=40, company=c1)
+        e2 = Employee.objects.create(name="Isaac", age=9, company=c1)
+
+        # update Scott - this makes Isaac is the "stale object" that would be deleted if skip_deletes were False
+        new_objs = [Employee(name="Scott", age=41, company=c1)]
+
+        # but Isaac should remain when the skip_deletes flag is True
+        ret = bulk_sync(new_models=new_objs, filters=None, key_fields=("name",), skip_deletes=True)
+
+        self.assertEqual(["Scott", "Isaac"], [x.name for x in Employee.objects.all().order_by("id")])
+
+        new_e1 = Employee.objects.get(id=e1.id)
+        self.assertEqual(41, new_e1.age)
+
+        self.assertEqual(2, Employee.objects.count())
+
+        self.assertEqual(1, ret["stats"]["updated"])
+        self.assertEqual(0, ret["stats"]["created"])
+        self.assertEqual(0, ret["stats"]["deleted"])
+
+    def test_skip_creates(self):
+        c1 = Company.objects.create(name="My Company LLC")
+
+        e1 = Employee.objects.create(name="Scott", age=40, company=c1)
+        e2 = Employee.objects.create(name="Isaac", age=9, company=c1)
+
+        # create a new employee that will be ignored
+        new_objs = [Employee(name="John", age=52, company=c1)]
+
+        ret = bulk_sync(new_models=new_objs, filters=None, key_fields=("name",), skip_creates=True, skip_deletes=True)
+
+        self.assertEqual(2, Employee.objects.count())
+        self.assertEqual(["Scott", "Isaac"], [x.name for x in Employee.objects.all().order_by("id")])
+
+        self.assertEqual(0, ret["stats"]["updated"])
+        self.assertEqual(0, ret["stats"]["created"])
+        self.assertEqual(0, ret["stats"]["deleted"])
+
+    def test_skip_updates(self):
+        c1 = Company.objects.create(name="My Company LLC")
+
+        e1 = Employee.objects.create(name="Scott", age=40, company=c1)
+        e2 = Employee.objects.create(name="Isaac", age=9, company=c1)
+
+        # update employee that will be ignored, create a new one
+        new_objs = [Employee(name="Scott", age=100, company=c1), Employee(name="Alice", age=36, company=c1)]
+
+        ret = bulk_sync(new_models=new_objs, filters=None, key_fields=("name",), skip_updates=True)
+
+        # the age should not have been updated
+        new_e1 = Employee.objects.get(id=e1.id)
+        self.assertEqual(40, new_e1.age)
+
+        # Isaac is "stale" object - was deleted, Alice was created
+        self.assertEqual(2, Employee.objects.count())
+        self.assertEqual(["Scott", "Alice"], [x.name for x in Employee.objects.all().order_by("id")])
+
+        self.assertEqual(0, ret["stats"]["updated"])
+        self.assertEqual(1, ret["stats"]["created"])
+        self.assertEqual(1, ret["stats"]["deleted"])
+
+    def test_empty_new_models_class_detection_works(self):
+        c1 = Company.objects.create(name="My Company LLC")
+
+        with self.assertRaises(RuntimeError):
+            ret = bulk_sync(new_models=[], filters=None, key_fields=("name",))
+
+        ret = bulk_sync(new_models=[], filters=None, key_fields=("name",), db_class=Employee)
+        ret = bulk_sync(new_models=Employee.objects.none(), filters=None, key_fields=("name",))
+
+    def test_new_objs_with_unprepped_field_values_are_processed_correctly(self):
+        c1 = Company.objects.create(name="Foo Products, Ltd.")
+        c2 = Company.objects.create(name="Bar Microcontrollers, Inc.")
+        e1 = Employee.objects.create(name="Scott", age=40, company=c1)
+
+        new_objs = [Employee(name="Scott", age="40", company=c2)]
+
+        ret = bulk_sync(
+            new_models=new_objs,
+            filters=None,
+            key_fields=("name", "age"),
+        )
+
+        # we should should update e1's company to c2
+        self.assertEqual(1, ret["stats"]["updated"])
+        self.assertEqual(c2, Employee.objects.get(pk=e1.pk).company)
+
+        # we should not create or delete anything
+        self.assertEqual(0, ret["stats"]["created"])
+        self.assertEqual(0, ret["stats"]["deleted"])
+        self.assertEqual(1, Employee.objects.count())
+
+
+class BulkCompareTests(TestCase):
+    """ Test `bulk_compare` method """
+
+    # @classmethod
+    # def setUpTestData(cls):
+    def setupEmployees(self):
+        self.c1 = Company.objects.create(name="Foo Products, Ltd.")
+        self.c2 = Company.objects.create(name="Bar Microcontrollers, Inc.")
+
+        self.e1 = Employee.objects.create(name="Scott", age=40, company=self.c1)
+        self.e2 = Employee.objects.create(name="Isaac", age=9, company=self.c1)
+        self.e3 = Employee.objects.create(name="Zoe", age=9, company=self.c1)
+        self.e4 = Employee.objects.create(name="Bob", age=25, company=self.c2)
+
+        # We should update Scott's and Isaac's age, delete Zoe, add Newguy and
+        # add a second Bob (since he's not in company c1, which we filtered on.)
+        self.new_objs = [
+            Employee(name="Scott", age=41, company=self.c1),
+            Employee(name="Isaac", age=9, company=self.c1),
+            Employee(name="Newguy", age=10, company=self.c1),
+            Employee(name="Bob", age=50, company=self.c1),
+        ]
+
+    def setupEmployeeDiffPk(self):
+        self.c1 = Company.objects.create(name="Foo Products, Ltd.")
+        self.c2 = Company.objects.create(name="Bar Microcontrollers, Inc.")
+
+        self.e1 = EmployeeDifferentPk.objects.create(name="Scott", age=40, company=self.c1)
+        self.e2 = EmployeeDifferentPk.objects.create(name="Isaac", age=9, company=self.c1)
+        self.e3 = EmployeeDifferentPk.objects.create(name="Zoe", age=9, company=self.c1)
+        self.e4 = EmployeeDifferentPk.objects.create(name="Bob", age=25, company=self.c2)
+
+        # We should update Scott's and Isaac's age, delete Zoe, add Newguy and
+        # add a second Bob (since he's not in company c1, which we filtered on.)
+        self.new_objs = [
+            EmployeeDifferentPk(name="Scott", age=41, company=self.c1),
+            EmployeeDifferentPk(name="Isaac", age=9, company=self.c1),
+            EmployeeDifferentPk(name="Newguy", age=10, company=self.c1),
+            EmployeeDifferentPk(name="Bob", age=50, company=self.c1),
+        ]
+
+    def test_bulk_compare(self):
+        self.setupEmployees()
+
+        c1 = self.c1
+        e3 = self.e3
+        new_objs = self.new_objs
+
+        ret = bulk_compare(old_models=Employee.objects.filter(company=c1), new_models=new_objs, key_fields=("name",))
+
+        self.assertEqual([new_objs[2], new_objs[3]], ret["added"])
+        self.assertEqual([e3], list(ret["removed"]))
+        self.assertEqual([new_objs[0]], ret["updated"])
+        self.assertEqual({new_objs[0]: {"age": (40, 41)}}, ret["updated_details"])
+        self.assertEqual([new_objs[1]], ret["unchanged"])
+
+    def test_bulk_compare_with_ignore_int_field(self):
+        self.setupEmployees()
+
+        c1 = self.c1
+        e3 = self.e3
+        new_objs = self.new_objs
+
+        ret = bulk_compare(
+            old_models=Employee.objects.filter(company=c1).order_by("name"),
+            new_models=new_objs,
+            key_fields=("name",),
+            ignore_fields=("age",),
+        )
+
+        self.assertEqual([new_objs[2], new_objs[3]], ret["added"])
+        self.assertEqual([e3], list(ret["removed"]))
+        self.assertEqual([], ret["updated"])
+        self.assertEqual({}, ret["updated_details"])
+        self.assertEqual([new_objs[0], new_objs[1]], ret["unchanged"])
+
+    def test_bulk_compare_with_ignore_relation_field(self):
+        self.setupEmployees()
+
+        c1 = self.c1
+        e3 = self.e3
+        new_objs = self.new_objs
+
+        ret = bulk_compare(
+            old_models=Employee.objects.filter(company=c1).order_by("name"),
+            new_models=new_objs,
+            key_fields=("name",),
+            ignore_fields=("company_id",),
+        )
+
+        self.assertEqual([new_objs[2], new_objs[3]], ret["added"])
+        self.assertEqual([e3], list(ret["removed"]))
+        self.assertEqual([new_objs[0]], ret["updated"])
+        self.assertEqual({new_objs[0]: {"age": (40, 41)}}, ret["updated_details"])
+        self.assertEqual([new_objs[1]], ret["unchanged"])
+
+    def test_nonstandard_pk(self):
+        self.setupEmployeeDiffPk()
+
+        c1 = self.c1
+        e3 = self.e3
+        new_objs = self.new_objs
+
+        ret = bulk_compare(
+            old_models=EmployeeDifferentPk.objects.filter(company=c1).order_by("name"),
+            new_models=new_objs,
+            key_fields=("name",),
+            ignore_fields=("company_id",),
+        )
+
+        self.assertEqual([new_objs[2], new_objs[3]], ret["added"])
+        self.assertEqual([e3], list(ret["removed"]))
+        self.assertEqual([new_objs[0]], ret["updated"])
+        self.assertEqual({new_objs[0]: {"age": (40, 41)}}, ret["updated_details"])
+        self.assertEqual([new_objs[1]], ret["unchanged"])
