f35ab1ecba947d3a999303e7e7c8fb95956b8700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:13 +0000

    Add package thetravelers.online-Api/1.2.4/thetravelers.online%20Api-1.2.4.tar.gz

---
+++ packages/thetravelers.online-Api/thetravelers.online%20Api-1.2.4.tar.gz/thetravelers.online Api-1.2.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='thetravelers.online Api',
+	version='1.2.4',
+	description='Python API for thetravelers.online',
+	long_description='https://github.com/LightningWB/thetravelers.online-API',
+	author='LightningWB',
+	url='https://github.com/LightningWB/thetravelers.online-API',
+	packages=['travelersApi'],
+	install_requires=['selenium']
+)
