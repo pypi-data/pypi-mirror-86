01c10960a7cd98dea93c9352f9b375cebe9b0319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:41 +0000

    Add package fake-doctors/0.0.5.2/fake-doctors-0.0.5.2.tar.gz

---
+++ packages/fake-doctors/fake-doctors-0.0.5.2.tar.gz/fake-doctors-0.0.5.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='fake-doctors',
+    version='0.0.5.2',
+    author='Kim Du Nam',
+    author_email='timecostslives@gmail.com',
+    description='Tools for handling whole slide image',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/timecostslives/fake-doctors',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+    ],
+    python_requires='>=3.6',
+)
