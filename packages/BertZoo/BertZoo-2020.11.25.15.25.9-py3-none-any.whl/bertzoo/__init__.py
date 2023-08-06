90bda979f4a1d8cda38568749c0d4731519676cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:41 +0000

    Add package BertZoo/2020.11.25.15.25.9/BertZoo-2020.11.25.15.25.9-py3-none-any.whl

---
+++ packages/BertZoo/BertZoo-2020.11.25.15.25.9-py3-none-any.whl/bertzoo/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : BertZoo.
+# @File         : __init__.py
+# @Time         : 2020/11/25 3:24 下午
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : 
+
+
