2981a8244c8ab2e9433e19456007aece291a6747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:20 +0000

    Add package stuart-dataops/0.0.3/stuart-dataops-0.0.3.tar.gz

---
+++ packages/stuart-dataops/stuart-dataops-0.0.3.tar.gz/stuart-dataops-0.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="stuart-dataops", # Replace with your own username
+    version="0.0.3",
+    author="Lan Row KOK",
+    author_email="r.kok@stuart.com",
+    description="Package used by the French Dataops team of Stuart",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/StuartApp/ops-aws-scripts/blob/master/fr-scripts/lib/dataops.py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2',
+    py_modules=['dataops']
+)
