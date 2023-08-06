91ec1e4a4745c86cf183b417ba8a62324d5962f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:14 +0000

    Add package easydstools/0.0.1/easydstools-0.0.1.tar.gz

---
+++ packages/easydstools/easydstools-0.0.1.tar.gz/easydstools-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='easydstools',
+      version='0.0.1',
+      description='Data Science toolkit I use a lot',
+      url='https://github.com/celmore25/clay-ds/',
+      author='Clay Elmore',
+      author_email='celmore25@gmail.com',
+      license='Apache 2.0',
+      packages=['easydstools'],
+      include_package_data=True,
+      scripts=[],
+      zip_safe=False)
