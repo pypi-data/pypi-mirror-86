1fc1be9a78ae6f433c68005fcf24341bf6d6984a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:54 +0000

    Add package alpha-nest/1.2.0/alpha_nest-1.2.0.tar.gz

---
+++ packages/alpha-nest/alpha_nest-1.2.0.tar.gz/alpha_nest-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'alpha_nest',
+	version		= '1.2.0',
+	py_modules	= ['alpha_nest'],
+	author		= 'Alpha Diallo',
+	author_email	= 'diallo.omr.alpha@gmail.com',
+	url		= 'http://www.headfirstlabs.com',
+	description	= 'A simple printer of nested lists',
+)
