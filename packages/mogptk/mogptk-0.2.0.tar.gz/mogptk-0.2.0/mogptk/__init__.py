e9c1a85a5be5b550e14955269351c44ac9fcf235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:10 +0000

    Add package mogptk/0.2.0/mogptk-0.2.0.tar.gz

---
+++ packages/mogptk/mogptk-0.2.0.tar.gz/mogptk-0.2.0/mogptk/__init__.py
@@ -0,0 +1,18 @@
+"""
+.. include:: ./documentation.md
+"""
+from .serie import *
+from .data import *
+from .dataset import *
+
+from .model import *
+from .sm import *
+from .mosm import *
+from .csm import *
+from .conv import *
+from .sm_lmc import *
+
+from .plot import *
+from .errors import *
+
+from .kernels.config import *
