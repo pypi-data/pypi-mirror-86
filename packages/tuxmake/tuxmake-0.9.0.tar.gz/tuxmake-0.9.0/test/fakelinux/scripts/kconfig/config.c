b6598aca0d522ada0938f4cef80bf7455c83fa6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:52 +0000

    Add package tuxmake/0.9.0/tuxmake-0.9.0.tar.gz

---
+++ packages/tuxmake/tuxmake-0.9.0.tar.gz/tuxmake-0.9.0/test/fakelinux/scripts/kconfig/config.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+  printf("This is not really the config program\n");
+  return 0;
+}
---
+++ packages/tuxmake/tuxmake-0.9.0.tar.gz/tuxmake-0.9.0/test/fakelinux/vmlinux.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+  printf("This is not really Linux\n");
+  return 0;
+}
