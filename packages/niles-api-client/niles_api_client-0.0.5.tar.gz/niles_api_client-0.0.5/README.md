252bb008e9e1f7a8b60d9f49e8a4b1df2a0b6478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:14 +0000

    Add package niles-api-client/0.0.5/niles_api_client-0.0.5.tar.gz

---
+++ packages/niles-api-client/niles_api_client-0.0.5.tar.gz/niles_api_client-0.0.5/README.md
@@ -0,0 +1,34 @@
+# niles-api-client
+
+API client for [niles-backend](https://github.com/niles-household-management/niles-backend) written in python.
+
+# Features
+
+TODO
+
+# How to use
+
+TODO
+
+# Contributing
+
+GitHub is for social coding: if you want to write code, I encourage contributions 
+through pull requests from forks of this repository. Create GitHub tickets for 
+bugs and new features and comment on the ones that you are interested in.
+
+# License
+
+```text
+niles-api-client is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+```
---
+++ packages/niles-api-client/niles_api_client-0.0.5.tar.gz/niles_api_client-0.0.5/setup.py
@@ -0,0 +1,100 @@
+#  Copyright (c) 2019 Markus Ressel
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
+#
+import os
+
+from setuptools import setup, find_packages
+
+
+def read_version(package):
+    with open(os.path.join(package, '__init__.py'), 'r') as fd:
+        for line in fd:
+            if line.startswith('__version__ = '):
+                return line.split()[-1].strip().strip("'").strip("\"")
+
+
+def readme_type() -> str:
+    import os
+    if os.path.exists("README.rst"):
+        return "text/x-rst"
+    if os.path.exists("README.md"):
+        return "text/markdown"
+
+
+def readme() -> [str]:
+    if readme_type() == "text/markdown":
+        file_name = "README.md"
+    else:
+        file_name = "README.rst"
+
+    with open(file_name) as f:
+        return f.read()
+
+
+def locked_requirements(section):
+    """
+    Look through the 'Pipfile.lock' to fetch requirements by section.
+    """
+    import json
+
+    with open('Pipfile.lock') as pip_file:
+        pipfile_json = json.load(pip_file)
+
+    if section not in pipfile_json:
+        print("{0} section missing from Pipfile.lock".format(section))
+        return []
+
+    return [package
+            for package, detail in pipfile_json[section].items()]
+
+
+VERSION_NUMBER = read_version('niles_api_client')
+
+if "GITHUB_RELEASE" in os.environ:
+    # tagged release
+    DEVELOPMENT_STATUS = "Development Status :: 5 - Production/Stable"
+    VERSION_NAME = VERSION_NUMBER
+else:
+    print("Unknown git branch, using pre-alpha as default")
+    DEVELOPMENT_STATUS = "Development Status :: 2 - Pre-Alpha"
+    VERSION_NAME = "%s" % VERSION_NUMBER
+
+setup(
+    name='niles_api_client',
+    version=VERSION_NAME,
+    description='API client for Niles Household Management',
+    long_description=readme(),
+    long_description_content_type=readme_type(),
+    license='AGPLv3+',
+    author='Markus Ressel',
+    author_email='mail@markusressel.de',
+    url='https://github.com/niles-household-management/niles-api-client',
+    packages=find_packages(exclude=['tests']),
+    classifiers=[
+        DEVELOPMENT_STATUS,
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
+    install_requires=locked_requirements('default'),
+    tests_require=locked_requirements('develop'),
+)
