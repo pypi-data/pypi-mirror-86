3272bb0f95f478e3c56b60bb9b2723e546255481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:59 +0000

    Add package placekey/0.0.8/placekey-0.0.8.tar.gz

---
+++ packages/placekey/placekey-0.0.8.tar.gz/placekey-0.0.8/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="placekey",
+    version="0.0.8",
+    author="SafeGraph Inc.",
+    author_email="russ@safegraph.com",
+    description="Utilities for working with Placekeys",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Placekey/placekey-py",
+    packages=setuptools.find_packages(),
+    install_requires=['h3', 'shapely', 'requests', 'ratelimit', 'backoff'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
