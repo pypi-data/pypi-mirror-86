814b416510788eb653d3933636a8c9f8e8aec3ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:39 +0000

    Add package micro-components/0.1.0/micro-components-0.1.0.tar.gz

---
+++ packages/micro-components/micro-components-0.1.0.tar.gz/micro-components-0.1.0/setup.py
@@ -0,0 +1,45 @@
+import os
+import re
+import sys
+from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+
+from pathlib import Path
+from micro_components.utils.shell import shell_run
+from micro_components.setup import install_cli
+
+class PreDevelopCommand(develop):
+	def run(self):
+		install_cli()
+		develop.run(self)
+
+class PreInstallCommand(install):
+	def run(self):
+		install_cli()
+		install.run(self)
+
+def read(fname):
+	return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+	name = "micro-components",
+	version = "0.1.0",
+	author = "Dan Borufka",
+	author_email = "danborufka@gmail.com",
+	description = ("Lightweight library to create components that can automagically be used as either CLI or as native classes in other programming languages"),
+	license = "MIT",
+	keywords = "components",
+	url = "https://github.com/polygoat/micro-components-py",
+	packages=['micro_components'],
+	cmdclass={
+        'develop': PreDevelopCommand,
+        'install': PreInstallCommand,
+    },
+	long_description='Checkout https://github.com/polygoat/micro-components-py for a long description.',
+	classifiers=[
+		"Development Status :: 3 - Alpha",
+		"Topic :: Utilities",
+		"License :: OSI Approved :: MIT License",
+	],
+)
