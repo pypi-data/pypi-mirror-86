fb0220886c28dbb75afb297db69d198669edfa8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:42 +0000

    Add package nbresuse/0.4.0/nbresuse-0.4.0-py2.py3-none-any.whl

---
+++ packages/nbresuse/nbresuse-0.4.0-py2.py3-none-any.whl/nbresuse-0.4.0.data/data/share/jupyter/nbextensions/nbresuse/main.js
@@ -0,0 +1,79 @@
+define([
+    'jquery',
+    'base/js/utils'
+], function ($, utils) {
+    function setupDOM() {
+        $('#maintoolbar-container').append(
+            $('<div>').attr('id', 'nbresuse-display')
+                .addClass('btn-group')
+                .addClass('pull-right')
+                .append(
+                    $('<strong>').text('Memory: ')
+                ).append(
+                $('<span>').attr('id', 'nbresuse-mem')
+                    .attr('title', 'Actively used Memory (updates every 5s)')
+            )
+        );
+        // FIXME: Do something cleaner to get styles in here?
+        $('head').append(
+            $('<style>').html('.nbresuse-warn { background-color: #FFD2D2; color: #D8000C; }')
+        );
+        $('head').append(
+            $('<style>').html('#nbresuse-display { padding: 2px 8px; }')
+        );
+    }
+
+    function humanFileSize(size) {
+        var i = Math.floor(Math.log(size) / Math.log(1024));
+        return (size / Math.pow(1024, i)).toFixed(1) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
+    }
+
+    var displayMetrics = function () {
+        if (document.hidden) {
+            // Don't poll when nobody is looking
+            return;
+        }
+        $.getJSON({
+            url: utils.get_body_data('baseUrl') + 'api/metrics/v1',
+            success: function (data) {
+                totalMemoryUsage = humanFileSize(data['rss']);
+
+                var limits = data['limits'];
+                var display = totalMemoryUsage;
+
+                if (limits['memory']) {
+                    if (limits['memory']['rss']) {
+                        maxMemoryUsage = humanFileSize(limits['memory']['rss']);
+                        display += " / " + maxMemoryUsage
+                    }
+                    if (limits['memory']['warn']) {
+                        $('#nbresuse-display').addClass('nbresuse-warn');
+                    } else {
+                        $('#nbresuse-display').removeClass('nbresuse-warn');
+                    }
+                }
+
+                $('#nbresuse-mem').text(display);
+            }
+        });
+    };
+
+    var load_ipython_extension = function () {
+        setupDOM();
+        displayMetrics();
+        // Update every five seconds, eh?
+        setInterval(displayMetrics, 1000 * 5);
+
+        document.addEventListener("visibilitychange", function () {
+            // Update instantly when user activates notebook tab
+            // FIXME: Turn off update timer completely when tab not in focus
+            if (!document.hidden) {
+                displayMetrics();
+            }
+        }, false);
+    };
+
+    return {
+        load_ipython_extension: load_ipython_extension,
+    };
+});
