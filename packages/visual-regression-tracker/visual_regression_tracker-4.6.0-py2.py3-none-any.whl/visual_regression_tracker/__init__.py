1781b9204ed3b5fa1d5057e832a0701ab6c771b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:46 +0000

    Add package visual-regression-tracker/4.6.0/visual_regression_tracker-4.6.0-py2.py3-none-any.whl

---
+++ packages/visual-regression-tracker/visual_regression_tracker-4.6.0-py2.py3-none-any.whl/visual_regression_tracker/__init__.py
@@ -0,0 +1,30 @@
+"""
+Python SDK for Visual Regression Tracker
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Open source, self hosted solution for visual testing and managing results of visual testing.
+
+Basic usage:
+    from visual_regression_tracker import VisualRegressionTracker, Config, TestRun
+    config = Config(
+        apiUrl='http://localhost:4200',
+        project='Default project',
+        apiKey='tXZVHX0EA4YQM1MGDD',
+        branchName='develop',
+    )
+    with VisualRegressionTracker(config) as vrt:
+        vrt.track(TestRun(
+            name='Image name',
+            imageBase64=image,
+        ))
+"""
+
+from .types import Config, Build, IgnoreArea, TestRun, TestRunResponse, TestRunStatus
+from .exceptions import VisualRegressionTrackerError, ServerError, TestRunError
+from .visualRegressionTracker import VisualRegressionTracker
+
+__all__ = [
+    'Config', 'Build', 'IgnoreArea', 'TestRun', 'TestRunResponse', 'TestRunStatus',
+    'VisualRegressionTrackerError', 'ServerError', 'TestRunError',
+    'VisualRegressionTracker',
+]
