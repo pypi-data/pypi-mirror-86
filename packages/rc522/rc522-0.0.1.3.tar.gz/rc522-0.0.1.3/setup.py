1667f5bb635d71f81179a7819d4bdfeedb6bb2c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:37 +0000

    Add package rc522/0.0.1.3/rc522-0.0.1.3.tar.gz

---
+++ packages/rc522/rc522-0.0.1.3.tar.gz/rc522-0.0.1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+long_description = open("README.md").read()
+
+setup(
+        name='rc522',
+        version='0.0.1.3',
+        description='RFID Utility',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        author_email='william.wyatt@cgu.edu',
+        url='https://github.com/Tsangares/rc522',
+        include_package_data=True,
+        packages=find_packages(),
+        install_requires=[
+            ],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        python_requires='>=3.7',
+)
