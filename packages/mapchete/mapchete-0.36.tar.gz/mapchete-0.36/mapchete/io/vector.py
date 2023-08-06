c86f4197458db8880957c8e8dd3f4ec8d9344173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:24 +0000

    Add package mapchete/0.36/mapchete-0.36.tar.gz

---
+++ packages/mapchete/mapchete-0.36.tar.gz/mapchete-0.36/mapchete/io/vector.py
@@ -0,0 +1,252 @@
+"""Functions handling vector data."""
+
+import os
+import logging
+import fiona
+from fiona.errors import DriverError, FionaError, FionaValueError
+from fiona.io import MemoryFile
+from retry import retry
+from rasterio.crs import CRS
+from shapely.geometry import box, mapping
+from shapely.errors import TopologicalError
+from tilematrix import clip_geometry_to_srs_bounds
+from itertools import chain
+
+from mapchete.errors import GeometryTypeError, MapcheteIOError
+from mapchete.io._path import path_exists
+from mapchete.io._geometry_operations import (
+    reproject_geometry,
+    segmentize_geometry,
+    to_shape,
+    multipart_to_singleparts,
+    clean_geometry_type,
+    _repair
+)
+
+__all__ = [
+    "reproject_geometry",
+    "segmentize_geometry",
+    "to_shape",
+    "multipart_to_singleparts",
+    "clean_geometry_type"
+]
+
+logger = logging.getLogger(__name__)
+
+
+def read_vector_window(input_files, tile, validity_check=True):
+    """
+    Read a window of an input vector dataset.
+
+    Also clips geometry.
+
+    Parameters
+    ----------
+    input_file : string
+        path to vector file
+    tile : ``Tile``
+        tile extent to read data from
+    validity_check : bool
+        checks if reprojected geometry is valid and throws ``RuntimeError`` if
+        invalid (default: True)
+
+    Returns
+    -------
+    features : list
+      a list of reprojected GeoJSON-like features
+    """
+    try:
+        return [
+            feature
+            for feature in chain.from_iterable([
+                _read_vector_window(path, tile, validity_check=validity_check)
+                for path in (
+                    input_files if isinstance(input_files, list) else [input_files]
+                )
+            ])
+        ]
+    except FileNotFoundError:  # pragma: no cover
+        raise
+    except Exception as e:  # pragma: no cover
+        raise MapcheteIOError(e)
+
+
+def _read_vector_window(input_file, tile, validity_check=True):
+    if tile.pixelbuffer and tile.is_on_edge():
+        return chain.from_iterable(
+            _get_reprojected_features(
+                input_file=input_file,
+                dst_bounds=bbox.bounds,
+                dst_crs=tile.crs,
+                validity_check=validity_check
+            )
+            for bbox in clip_geometry_to_srs_bounds(
+                tile.bbox, tile.tile_pyramid, multipart=True
+            )
+        )
+    else:
+        features = _get_reprojected_features(
+            input_file=input_file,
+            dst_bounds=tile.bounds,
+            dst_crs=tile.crs,
+            validity_check=validity_check
+        )
+        return features
+
+
+def write_vector_window(
+    in_data=None, out_schema=None, out_tile=None, out_path=None, bucket_resource=None
+):
+    """
+    Write features to GeoJSON file.
+
+    Parameters
+    ----------
+    in_data : features
+    out_schema : dictionary
+        output schema for fiona
+    out_tile : ``BufferedTile``
+        tile used for output extent
+    out_path : string
+        output path for GeoJSON file
+    """
+    # Delete existing file.
+    try:
+        os.remove(out_path)
+    except OSError:
+        pass
+
+    out_features = []
+    for feature in in_data:
+        try:
+            # clip feature geometry to tile bounding box and append for writing
+            # if clipped feature still
+            for out_geom in multipart_to_singleparts(
+                clean_geometry_type(
+                    to_shape(feature["geometry"]).intersection(out_tile.bbox),
+                    out_schema["geometry"]
+                )
+            ):
+                if out_geom.is_empty:  # pragma: no cover
+                    continue
+                out_features.append({
+                    "geometry": mapping(out_geom),
+                    "properties": feature["properties"]
+                })
+        except Exception as e:
+            logger.warning("failed to prepare geometry for writing: %s", e)
+            continue
+
+    # write if there are output features
+    if out_features:
+        try:
+            if out_path.startswith("s3://"):
+                # write data to remote file
+                with VectorWindowMemoryFile(
+                    tile=out_tile,
+                    features=out_features,
+                    schema=out_schema,
+                    driver="GeoJSON"
+                ) as memfile:
+                    logger.debug((out_tile.id, "upload tile", out_path))
+                    bucket_resource.put_object(
+                        Key="/".join(out_path.split("/")[3:]),
+                        Body=memfile
+                    )
+            else:
+                # write data to local file
+                with fiona.open(
+                    out_path, 'w', schema=out_schema, driver="GeoJSON",
+                    crs=out_tile.crs.to_dict()
+                ) as dst:
+                    logger.debug((out_tile.id, "write tile", out_path))
+                    dst.writerecords(out_features)
+        except Exception as e:
+            logger.error("error while writing file %s: %s", out_path, e)
+            raise
+
+    else:
+        logger.debug((out_tile.id, "nothing to write", out_path))
+
+
+class VectorWindowMemoryFile():
+    """Context manager around fiona.io.MemoryFile."""
+
+    def __init__(
+        self, tile=None, features=None, schema=None, driver=None
+    ):
+        """Prepare data & profile."""
+        self.tile = tile
+        self.schema = schema
+        self.driver = driver
+        self.features = features
+
+    def __enter__(self):
+        """Open MemoryFile, write data and return."""
+        self.fio_memfile = MemoryFile()
+        with self.fio_memfile.open(
+            schema=self.schema,
+            driver=self.driver,
+            crs=self.tile.crs
+        ) as dst:
+            dst.writerecords(self.features)
+        return self.fio_memfile
+
+    def __exit__(self, *args):
+        """Make sure MemoryFile is closed."""
+        self.fio_memfile.close()
+
+
+@retry(
+    tries=3, logger=logger, exceptions=(DriverError, FionaError, FionaValueError), delay=1
+)
+def _get_reprojected_features(
+    input_file=None, dst_bounds=None, dst_crs=None, validity_check=False
+):
+    logger.debug("reading %s", input_file)
+    try:
+        with fiona.open(input_file, 'r') as src:
+            src_crs = CRS(src.crs)
+            # reproject tile bounding box to source file CRS for filter
+            if src_crs == dst_crs:
+                dst_bbox = box(*dst_bounds)
+            else:
+                dst_bbox = reproject_geometry(
+                    box(*dst_bounds),
+                    src_crs=dst_crs,
+                    dst_crs=src_crs,
+                    validity_check=True
+                )
+            for feature in src.filter(bbox=dst_bbox.bounds):
+
+                try:
+                    # check validity
+                    original_geom = _repair(to_shape(feature['geometry']))
+
+                    # clip with bounds and omit if clipped geometry is empty
+                    clipped_geom = original_geom.intersection(dst_bbox)
+
+                    if not clipped_geom.is_empty:
+                        # reproject each feature to tile CRS
+                        g = reproject_geometry(
+                            clean_geometry_type(clipped_geom, original_geom.geom_type),
+                            src_crs=src_crs,
+                            dst_crs=dst_crs,
+                            validity_check=validity_check
+                        )
+                        yield {
+                            'properties': feature['properties'],
+                            'geometry': mapping(g)
+                        }
+                # this can be handled quietly
+                except GeometryTypeError:
+                    pass
+                except TopologicalError as e:  # pragma: no cover
+                    logger.warning("feature omitted: %s", e)
+
+    except Exception as e:
+        if path_exists(input_file):
+            logger.error("error while reading file %s: %s", input_file, e)
+            raise e
+        else:
+            raise FileNotFoundError("%s not found" % input_file)
---
+++ packages/mapchete/mapchete-0.36.tar.gz/mapchete-0.36/mapchete/processes/convert.py
@@ -0,0 +1,106 @@
+import logging
+from rasterio.dtypes import dtype_ranges
+
+logger = logging.getLogger(__name__)
+
+
+def execute(
+    mp,
+    resampling="nearest",
+    band_indexes=None,
+    td_matching_method="gdal",
+    td_matching_max_zoom=None,
+    td_matching_precision=8,
+    td_fallback_to_higher_zoom=False,
+    clip_pixelbuffer=0,
+    scale_ratio=1.,
+    scale_offset=0.,
+    clip_to_output_dtype=None,
+    **kwargs
+):
+    """
+    Convert and optionally clip input raster data.
+
+    Inputs
+    ------
+    raster
+        Singleband or multiband data input.
+    clip (optional)
+        Vector data used to clip output.
+
+    Parameters
+    ----------
+    resampling : str (default: 'nearest')
+        Resampling used when reading from TileDirectory.
+    band_indexes : list
+        Bands to be read.
+    td_matching_method : str ('gdal' or 'min') (default: 'gdal')
+        gdal: Uses GDAL's standard method. Here, the target resolution is
+            calculated by averaging the extent's pixel sizes over both x and y
+            axes. This approach returns a zoom level which may not have the
+            best quality but will speed up reading significantly.
+        min: Returns the zoom level which matches the minimum resolution of the
+            extents four corner pixels. This approach returns the zoom level
+            with the best possible quality but with low performance. If the
+            tile extent is outside of the destination pyramid, a
+            TopologicalError will be raised.
+    td_matching_max_zoom : int (optional, default: None)
+        If set, it will prevent reading from zoom levels above the maximum.
+    td_matching_precision : int (default: 8)
+        Round resolutions to n digits before comparing.
+    td_fallback_to_higher_zoom : bool (default: False)
+        In case no data is found at zoom level, try to read data from higher
+        zoom levels. Enabling this setting can lead to many IO requests in
+        areas with no data.
+    clip_pixelbuffer : int
+        Use pixelbuffer when clipping output by geometry. (default: 0)
+    scale_ratio : float
+        Scale factor for input values. (default: 1.0)
+    scale_offset : float
+        Offset to add to input values. (default: 0.0)
+    clip_to_output_dtype : str
+        Clip output values to range of given dtype. (default: None)
+
+    Output
+    ------
+    np.ndarray
+    """
+    # read clip geometry
+    if "clip" in mp.params["input"]:
+        clip_geom = mp.open("clip").read()
+        if not clip_geom:
+            logger.debug("no clip data over tile")
+            return "empty"
+    else:
+        clip_geom = []
+
+    with mp.open("raster",) as raster:
+        logger.debug("reading input raster")
+        raster_data = raster.read(
+            indexes=band_indexes,
+            resampling=resampling,
+            matching_method=td_matching_method,
+            matching_max_zoom=td_matching_max_zoom,
+            matching_precision=td_matching_precision,
+            fallback_to_higher_zoom=td_fallback_to_higher_zoom
+        )
+        if raster_data.mask.all():
+            logger.debug("raster empty")
+            return "empty"
+
+    if scale_offset != 0.:
+        logger.debug("apply scale offset %s", scale_offset)
+        raster_data = raster_data.astype("float64", copy=False) + scale_offset
+    if scale_ratio != 1.:
+        logger.debug("apply scale ratio %s", scale_ratio)
+        raster_data = raster_data.astype("float64", copy=False) * scale_ratio
+    if (scale_offset != 0. or scale_ratio != 1.) and clip_to_output_dtype in dtype_ranges:
+        logger.debug("clip to output dtype ranges")
+        raster_data.clip(*dtype_ranges[clip_to_output_dtype], out=raster_data)
+
+    if clip_geom:
+        logger.debug("clipping output with geometry")
+        # apply original nodata mask and clip
+        return mp.clip(raster_data, clip_geom, clip_buffer=clip_pixelbuffer)
+    else:
+        return raster_data
