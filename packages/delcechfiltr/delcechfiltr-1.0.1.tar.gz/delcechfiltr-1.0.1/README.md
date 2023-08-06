76890ec6a6c7b5d8376cd31c7530eb13d85ad617
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:23 +0000

    Add package delcechfiltr/1.0.1/delcechfiltr-1.0.1.tar.gz

---
+++ packages/delcechfiltr/delcechfiltr-1.0.1.tar.gz/delcechfiltr-1.0.1/README.md
@@ -0,0 +1,67 @@
+# Delaunay-&Ccaron;ech filtrations
+
+This package provides functions for computing the smallest enclosing ball
+radius (miniball radius) of sets of three points in two and three dimensions and
+four points in three dimensions.
+
+These are used to produce Delaunay-&Ccaron;ech filtrations, which can be used to
+compute &Ccaron;ech persistence diagrams of points in two and three dimensions.
+
+Wrappers are provided for computing &Ccaron;ech persistence diagrams with
+<a href="http://gudhi.gforge.inria.fr/python/latest/">`gudhi`</a>
+for points in two and three dimensional space.
+
+## Installation
+
+The `setuptools`, `numpy`, `scipy`, and `gudhi` Python packages are
+prerequisites for using this package.
+
+The last one can be installed with `conda` running
+
+```
+>>> conda install -c conda-forge gudhi
+```
+
+You'll also need `pybind11`, `cmake` and a `C++` compiler in order to install this
+package. It is recommended to install both `pybind11` and `cmake` via `conda`.
+
+```
+>>> conda install -c anaconda cmake
+>>> conda install -c conda-forge pybind11
+```
+
+Finally run the following command to build and install this Python package
+
+```
+>>> pip install delcechfiltr
+```
+
+If the command above fails to build this package, tehn you might want to clone
+this repository to a directory `delcechfiltr/` on your computer and run
+
+```
+>>> cd /<path>/<to>/<cloned>/<repo>/delcechfiltr/
+>>> python setup.py install
+```
+
+which outputs more information on the errors causing the build to fail.
+
+**Windows.** After installing `conda`, run the above commands within an
+`Anaconda prompt`. For the C++ compiler install
+<a href="https://visualstudio.microsoft.com/vs/">Visual Studio community</a>.
+
+## Basic usage
+
+```python
+import numpy as np
+import delcechfiltr.dgms
+
+n, d = 20, 3
+np.random.seed(0)
+pts = np.random.rand(n, d)
+
+dgm_H0_cech1, dgm_H1_cech1                        = delcechfiltr.dgms.cech(pts, persistence_dim_max=False)
+dgm_H0_cech2, dgm_H1_cech2, dgm_H2_cech2          = delcechfiltr.dgms.cech(pts, persistence_dim_max=True)
+dgm_H0_delcech1, dgm_H1_delcech1                  = delcechfiltr.dgms.delcech_3D(pts, persistence_dim_max=False)
+dgm_H0_delcech2, dgm_H1_delcech2, dgm_H2_delcech2 = delcechfiltr.dgms.delcech_3D(pts, persistence_dim_max=True)
+```
---
+++ packages/delcechfiltr/delcechfiltr-1.0.1.tar.gz/delcechfiltr-1.0.1/setup.py
@@ -0,0 +1,81 @@
+import os
+import re
+import sys
+import platform
+import subprocess
+
+from setuptools import setup, Extension, find_packages
+from setuptools.command.build_ext import build_ext
+from distutils.version import LooseVersion
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = sourcedir
+
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError("CMake must be installed to build the following extensions: " +
+                               ", ".join(e.name for e in self.extensions))
+
+        if platform.system() == "Windows":
+            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
+            if cmake_version < '3.1.0':
+                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.sourcedir)))
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
+
+        if platform.system() == "Windows":
+            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
+            if sys.maxsize > 2**32:
+                cmake_args += ['-A', 'x64']
+            build_args += ['--', '/m']
+        else:
+            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
+                                                              self.distribution.get_version())
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        print("cmake call:\n", ['cmake', os.path.abspath(ext.sourcedir)] + cmake_args)
+        subprocess.check_call(['cmake', os.path.abspath(ext.sourcedir)] + cmake_args, cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
+
+binding_ext = CMakeExtension(name="binding",
+                             sourcedir="delcechfiltr/cpp/")
+
+#===========================================================
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name = "delcechfiltr",
+    version = "1.0.1",
+    author="Gabriele Beltramo",
+    description = "Radius parameters of simplices in Delaunay-Cech filtration of 2D and 3D points.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages("."),
+    ext_modules = [binding_ext],
+    cmdclass=dict(build_ext=CMakeBuild),
+    zip_safe=False,
+    url="https://github.com/gbeltramo/delcechfiltr",
+    license='GPLv3',
+    install_requires=['numpy', 'scipy'],
+    python_requires='>=3.1',
+    keywords='Delaunay triangulation, Cech complexes, radius parameter'
+)
