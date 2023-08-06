71bf4e7612150b411520fb4dd0cdd186c699ac4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:52 +0000

    Add package hover/0.2.3/hover-0.2.3.tar.gz

---
+++ packages/hover/hover-0.2.3.tar.gz/hover-0.2.3/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fd:
+    deps = [_line for _line in fd.read().split("\n") if not _line.startswith("#")]
+
+setuptools.setup(
+    name="hover",
+    version="0.2.3",
+    description="Data annotation done right: easy, fun, hyper-productive, and inducing insight.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Pavel",
+    author_email="pavelhurwicz@gmail.com",
+    url="https://github.com/phurwicz/hover",
+    packages=setuptools.find_packages(),
+    install_requires=deps,
+    python_requires=">=3.6, <3.9",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
