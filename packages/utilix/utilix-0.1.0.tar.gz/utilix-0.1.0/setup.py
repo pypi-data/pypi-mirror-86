2da06dad089ec1860fd99b0c984cb99d46451fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:21 +0000

    Add package utilix/0.1.0/utilix-0.1.0.tar.gz

---
+++ packages/utilix/utilix-0.1.0.tar.gz/utilix-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "utilix",
+    version = "0.1.0",
+    url='https://github.com/XENONnT/utilix',
+    description ="User-friendly interface to various utilities for XENON users",
+    packages=find_packages()
+)
