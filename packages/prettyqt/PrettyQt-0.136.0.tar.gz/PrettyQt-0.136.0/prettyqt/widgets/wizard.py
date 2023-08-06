f031be1df684a47aeaadea758d8f5b68c56f9d2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:18 +0000

    Add package prettyqt/0.136.0/PrettyQt-0.136.0.tar.gz

---
+++ packages/prettyqt/PrettyQt-0.136.0.tar.gz/PrettyQt-0.136.0/prettyqt/widgets/wizard.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from qtpy import QtWidgets
+
+from prettyqt import widgets
+
+QtWidgets.QWizard.__bases__ = (widgets.BaseDialog,)
+
+
+class Wizard(QtWidgets.QWizard):
+    def add_widget_as_page(self, widget: QtWidgets.QWidget) -> None:
+        page = widgets.WizardPage(self)
+        layout = widgets.BoxLayout("vertical", self)
+        layout += widget
+        page.set_layout(layout)
+
+
+if __name__ == "__main__":
+    app = widgets.app()
+    dlg = Wizard()
+    dlg.add_widget_as_page(widgets.RadioButton("test"))
+    dlg.show()
+    app.main_loop()
