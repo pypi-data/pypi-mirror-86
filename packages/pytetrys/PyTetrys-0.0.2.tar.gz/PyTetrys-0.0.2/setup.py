1ff33776c1de8b65b7795624085593c84fcc8b1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:05 +0000

    Add package pytetrys/0.0.2/PyTetrys-0.0.2.tar.gz

---
+++ packages/pytetrys/PyTetrys-0.0.2.tar.gz/PyTetrys-0.0.2/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyTetrys",
+    version="0.0.2",
+    author="Equipe PyTetrys",
+    author_email="adnan.bezerra@ccc.ufcg.edu.br",
+    description="Versão 0.2 do futuro jogo PyTetrys",
+    url="https://github.com/LucasBSousa04/PyTetris",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
