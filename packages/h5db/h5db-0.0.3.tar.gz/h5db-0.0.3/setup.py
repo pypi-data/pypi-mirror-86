7fbf67d1f8f93326b3d92c58084cff33c8fcfc6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:22 +0000

    Add package h5db/0.0.3/h5db-0.0.3.tar.gz

---
+++ packages/h5db/h5db-0.0.3.tar.gz/h5db-0.0.3/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+
+setup(
+    name='h5db',
+    version='0.0.3',
+    author='Larryjianfeng',
+    author_email='244060203@qq.com',
+    url='https://www.zhihu.com/people/feifeiaphy',
+    description=u'利用h5来做一个分布式的kv磁盘存储工具',
+    packages=['h5db'],
+    install_requires=['h5py'],
+    entry_points={
+        'console_scripts': [
+        ]
+    }
+)
