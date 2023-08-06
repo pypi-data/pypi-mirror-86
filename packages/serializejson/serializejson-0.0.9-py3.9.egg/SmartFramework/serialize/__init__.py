46f448a6d0f58f8b4ff6e81ce5ec3d06be46f95c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:26 +0000

    Add package serializejson/0.0.9/serializejson-0.0.9-py3.9.egg

---
+++ packages/serializejson/serializejson-0.0.9-py3.9.egg/SmartFramework/serialize/__init__.py
@@ -0,0 +1,7 @@
+class ExceptionPyQt(Exception):  # exception pour tout le module
+    pass
+
+
+# from SmartFramework.tools.dictionnaires import filtered
+
+# __all__  = ['serializeJson','serializePython','serializeAsModule','serializeTiny','serializeRepr','serialize_parameters','ExceptionPyQt']
