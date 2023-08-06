6cdbaf60262beda82c0903cb44670414b3bf1175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:51 +0000

    Add package webrtc-streaming/0.1.5/webrtc_streaming-0.1.5-py2.py3-none-any.whl

---
+++ packages/webrtc-streaming/webrtc_streaming-0.1.5-py2.py3-none-any.whl/webrtc_streaming/update_frame.py
@@ -0,0 +1,25 @@
+import grpc
+from webrtc_streaming.autogen import (
+    webrtc_streaming_pb2 as webrtc_streaming_pb2
+)
+from webrtc_streaming.autogen import (
+    webrtc_streaming_pb2_grpc as webrtc_streaming_pb2_grpc
+)
+
+
+def update_frame(ip_server="localhost:50051",
+                 data=None,
+                 width=None,
+                 height=None):
+    try:
+        assert data is not None
+        assert isinstance(width, int)
+        assert isinstance(height, int)
+        frame = webrtc_streaming_pb2.Frame(
+            data=data, width=width, height=height)
+        channel = grpc.insecure_channel(ip_server)
+        stub = webrtc_streaming_pb2_grpc.WebRTC_StreamingStub(channel)
+        response = stub.UpdateFrame(frame)
+        return isinstance(response, webrtc_streaming_pb2.Empty())
+    except Exception:
+        return False
