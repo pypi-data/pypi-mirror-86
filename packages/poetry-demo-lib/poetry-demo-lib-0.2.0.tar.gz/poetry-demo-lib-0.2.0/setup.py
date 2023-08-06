e2404040ce275a55eabb43ed4bada12ade6673b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:12 +0000

    Add package poetry-demo-lib/0.2.0/poetry-demo-lib-0.2.0.tar.gz

---
+++ packages/poetry-demo-lib/poetry-demo-lib-0.2.0.tar.gz/poetry-demo-lib-0.2.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['poetry_demo_lib']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['ipython>=7.19.0,<8.0.0']
+
+setup_kwargs = {
+    'name': 'poetry-demo-lib',
+    'version': '0.2.0',
+    'description': 'test poetry library publishing',
+    'long_description': None,
+    'author': 'webee.yw',
+    'author_email': 'webee.yw@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
