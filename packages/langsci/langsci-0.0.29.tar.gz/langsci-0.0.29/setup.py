c8c8bafe8fdea0ae145f1a89d2bd52173e14c17e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:39 +0000

    Add package langsci/0.0.29/langsci-0.0.29.tar.gz

---
+++ packages/langsci/langsci-0.0.29.tar.gz/langsci-0.0.29/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="langsci",
+    version="0.0.29",
+    author="Sebastian Nordhoff",
+    author_email="sebastian.nordhoff@langsci-press.org",
+    description="Tools for open access books used at Language Science Press",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/langsci/pypi",
+    packages=["langsci"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
