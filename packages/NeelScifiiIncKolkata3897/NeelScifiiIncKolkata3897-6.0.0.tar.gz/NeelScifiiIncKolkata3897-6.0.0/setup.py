f11e2a20562a2059f0375699d51ee5717da2fe62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:17 +0000

    Add package NeelScifiiIncKolkata3897/6.0.0/NeelScifiiIncKolkata3897-6.0.0.tar.gz

---
+++ packages/NeelScifiiIncKolkata3897/NeelScifiiIncKolkata3897-6.0.0.tar.gz/NeelScifiiIncKolkata3897-6.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup_args = dict(
+    name='NeelScifiiIncKolkata3897',
+    version='6.0.0',
+    description='Bruteforce proof cryptographic algorithm developed by Neelanjan Manna ',
+    #long_description_content_type="SCIFIIINCKOLKATA",
+    license='MIT',
+    #packages=find_packages(),
+    author='Neelanjan-Manna-Kolkata-VIT-MCA-2020',
+    author_email='scifiiinc@gmail.com',
+    keywords=['cryptography, bruteforce-proof,unhackable algorithm for cryptography'],
+    #url='https://github.com/ncthuc/elastictools',
+    #download_url='https://pypi.org/project/elastictools/'
+)
+
+install_requires = [
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
