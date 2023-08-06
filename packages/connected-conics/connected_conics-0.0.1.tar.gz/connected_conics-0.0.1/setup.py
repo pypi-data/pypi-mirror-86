76e7faec3619178bf0a36c5a4fc12896b32a82e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:46 +0000

    Add package connected-conics/0.0.1/connected_conics-0.0.1.tar.gz

---
+++ packages/connected-conics/connected_conics-0.0.1.tar.gz/connected_conics-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="connected_conics",
+    version="0.0.1",
+    author="Jake Brown",
+    author_email="jake@eyespacelenses.com",
+    description="Functions for geometric analysis of connected conic sections",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/eyespacelenses/connected-conics",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
