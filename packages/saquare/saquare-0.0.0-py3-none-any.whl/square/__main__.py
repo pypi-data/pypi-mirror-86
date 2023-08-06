73345f05d8bb6a2f666283ac117f5e4944e80380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:54 +0000

    Add package saquare/0.0.0/saquare-0.0.0-py3-none-any.whl

---
+++ packages/saquare/saquare-0.0.0-py3-none-any.whl/square/__main__.py
@@ -0,0 +1,7 @@
+def main():
+    number = int(input('Enter the number (only positive integers allowed\n'))
+    print(f'{number} squares is {number**2}')
+    print("happydvs?")
+
+if __name__ == '__main__':
+    main()
