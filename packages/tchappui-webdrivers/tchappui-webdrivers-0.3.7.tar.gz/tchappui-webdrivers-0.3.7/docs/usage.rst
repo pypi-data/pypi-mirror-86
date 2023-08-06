e0a4bfcd0e69245321c2941dcc5f08a2f742e00e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:15 +0000

    Add package tchappui-webdrivers/0.3.7/tchappui-webdrivers-0.3.7.tar.gz

---
+++ packages/tchappui-webdrivers/tchappui-webdrivers-0.3.7.tar.gz/tchappui-webdrivers-0.3.7/docs/usage.rst
@@ -0,0 +1,17 @@
+=====
+Usage
+=====
+
+To use install-webdrivers in a project, execute the install-webdrivers tool
+from the terminal. The following options are available::
+
+    Usage: install-webdrivers [OPTIONS]
+
+    Script installing the latest versions of chromedriver and geckodriver
+    locally.
+
+    Options:
+    -p, --path TEXT  Specify the installation directory  [default: .]
+    --chromedriver   Only install chromedriver
+    --geckodriver    Only install geckodriver
+    --help           Show this message and exit.
---
+++ packages/tchappui-webdrivers/tchappui-webdrivers-0.3.7.tar.gz/tchappui-webdrivers-0.3.7/webdrivers/webdrivers.py
@@ -0,0 +1,57 @@
+"""Main module."""
+
+import os
+import io
+import sys
+import zipfile
+import tarfile
+
+import requests
+
+chromedrivers = {
+    "win32": "win32.zip",
+    "linux": "linux64.zip",
+    "darwin": "mac64.zip",
+}
+
+geckodrivers = {
+    "win32": "win32.zip",
+    "linux": "linux64.tar.gz",
+    "darwin": "macos.tar.gz",
+}
+
+
+def install_latest_chromedriver(path="."):
+    """Downloads and installs the latest chromedriver for linux64."""
+    version = requests.get(
+        "https://chromedriver.storage.googleapis.com/LATEST_RELEASE"
+    ).text
+    url = (
+        "https://chromedriver.storage.googleapis.com"
+        f"/{version}/chromedriver_{chromedrivers[sys.platform]}"
+    )
+    buffer = io.BytesIO(requests.get(url).content)
+    zipfile.ZipFile(buffer).extractall(path)
+    if sys.platform != "win32":
+        os.chmod(os.path.join(path, 'chromedriver'), 0o755)
+
+
+def install_latest_geckodriver(path="."):
+    """Downloads and installs the latest geckodriver for linux64."""
+    response = requests.get(
+        "https://api.github.com/repos/mozilla/geckodriver/releases/latest"
+    )
+    urls = [
+        asset['browser_download_url']
+        for asset in response.json().get('assets', [])
+        if geckodrivers[sys.platform] in asset.get('name', '')
+    ]
+    for url in urls:
+        content = io.BytesIO(requests.get(url).content)
+        if url.endswith('tar.gz'):
+            tarfile.open(fileobj=content, mode="r:gz").extractall(path)
+        elif url.endswith('zip'):
+            zipfile.ZipFile(content).extractall(path)
+        if sys.platform != "win32":
+            os.chmod(os.path.join(path, 'geckodriver'), 0o775)
+        break
