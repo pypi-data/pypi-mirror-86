2ddc5188db70c08b7a571f5e6c6afc0a0ad610ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:40 +0000

    Add package tw-stock-plugin/0.0.9/tw-stock-plugin-0.0.9.tar.gz

---
+++ packages/tw-stock-plugin/tw-stock-plugin-0.0.9.tar.gz/tw-stock-plugin-0.0.9/tw_stock_plugin/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*
+"""
+      ┏┓       ┏┓
+    ┏━┛┻━━━━━━━┛┻━┓
+    ┃      ☃      ┃
+    ┃  ┳┛     ┗┳  ┃
+    ┃      ┻      ┃
+    ┗━┓         ┏━┛
+      ┗┳        ┗━┓
+       ┃          ┣┓
+       ┃          ┏┛
+       ┗┓┓┏━━━━┳┓┏┛
+        ┃┫┫    ┃┫┫
+        ┗┻┛    ┗┻┛
+    God Bless,Never Bug
+"""
+
+from tw_stock_plugin.core.update_stock import *
+from tw_stock_plugin.core.stock_tools import *
+from tw_stock_plugin.core.stock_info import *
+from tw_stock_plugin.core.stock_trading import *
+from tw_stock_plugin.core.stock_institutional_investors import *
+from tw_stock_plugin.core.stock_margin_trading import *
+from tw_stock_plugin.core.stock_peratio import *
+from tw_stock_plugin.core.stock_shareholdings import *
