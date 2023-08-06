8df45347d1063bf05fe9b0072e35ad9f2c6dd040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:52 +0000

    Add package cryptoasset-data-downloader/1.0.5/cryptoasset-data-downloader-1.0.5.tar.gz

---
+++ packages/cryptoasset-data-downloader/cryptoasset-data-downloader-1.0.5.tar.gz/cryptoasset-data-downloader-1.0.5/application/__main__.py
@@ -0,0 +1,10 @@
+"""Executes the application.
+"""
+from application.model_view_controller import Controller, Model, View
+
+if __name__ == "__main__":
+    """Executes the application.
+    """
+    # Instantiate application object
+    app = Controller(Model(), View())
+    app.start_app()
