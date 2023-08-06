70b694ecdb397b796f253156447bffd8be275f25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:11 +0000

    Add package ocbpy/0.2.1/ocbpy-0.2.1-py3-none-any.whl

---
+++ packages/ocbpy/ocbpy-0.2.1-py3-none-any.whl/ocbpy/__init__.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019, AGB & GC
+# Full license can be found in License.md
+# ----------------------------------------------------------------------------
+"""
+Open-Closed field line Boundary (OCB) magnetic gridding
+
+Functions
+---------
+match_data_ocb      Matches data and OCB records
+normal_evar         Normalise a variable proportional to the electric field
+normal_curl_evar    Normalise a variable proportional to the curl of the
+                    electric field
+
+Classes
+-------
+OCBoundary    OCB data for different times
+VectorData    Vector data point
+
+Modules
+-------
+boundaries     Boundary file utilities
+instruments    Instrument-specific OCB gridding functions
+ocb_time       Time manipulation routines
+ocb_scaling    Scaling functions for OCB gridded data
+ocb_correction Boundary correction utilities
+
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+import logging
+
+from ocbpy import ocboundary, ocb_scaling, ocb_time, ocb_correction
+from ocbpy.ocboundary import OCBoundary, match_data_ocb
+from ocbpy import instruments, boundaries
+
+# Define the global variables
+__version__ = str('0.2.1')
+
+# Define a logger object to allow easier log handling
+logging.raiseExceptions = False
+logger = logging.getLogger('ocbpy_logger')
