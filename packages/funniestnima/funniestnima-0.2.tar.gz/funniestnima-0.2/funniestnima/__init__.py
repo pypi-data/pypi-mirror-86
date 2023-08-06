f678ea0124ee12ce446c0ced6fbfdc2bbb0b7f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:21 +0000

    Add package funniestnima/0.2/funniestnima-0.2.tar.gz

---
+++ packages/funniestnima/funniestnima-0.2.tar.gz/funniestnima-0.2/funniestnima/__init__.py
@@ -0,0 +1,3 @@
+def joke():
+    return (u'Ja! ... '
+            u'')
---
+++ packages/funniestnima/funniestnima-0.2.tar.gz/funniestnima-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniestnima',
+      version='0.2',
+      description='The funniest joke in the world',
+      url='http://github.com/iw4p/funniestnima',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniestnima'],
+      zip_safe=False)
