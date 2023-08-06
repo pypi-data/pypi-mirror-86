a8aa6db926388ea3ad481c4e6b2e8c64184678dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:47 +0000

    Add package chocs/0.5.1/chocs-0.5.1-py3-none-any.whl

---
+++ packages/chocs/chocs-0.5.1-py3-none-any.whl/chocs/application.py
@@ -0,0 +1,108 @@
+from typing import Callable
+from typing import Dict
+from typing import List
+from typing import Union
+
+from .http_method import HttpMethod
+from .middleware import MiddlewarePipeline
+from .routing import Route
+from .routing import Router
+from .serverless import IS_SERVERLESS_ENVIRONMENT
+from .serverless import make_serverless_callback
+from .middleware import Middleware
+
+
+class HttpApplication:
+    """
+    should support
+
+    schema - for handling json schema
+    timeout - for time - outing
+    """
+    def __init__(self, *middleware: Union[Middleware, Callable]):
+
+        self.middleware = MiddlewarePipeline()
+        for item in middleware:
+            self.middleware.append(item)
+        self.methods: Dict[HttpMethod, Router] = {key: Router() for key in HttpMethod}
+
+    def get(self, route: str) -> Callable:
+        def _get(handler: Callable) -> Callable:
+            self.methods[HttpMethod.GET].append(Route(route), handler)
+            if IS_SERVERLESS_ENVIRONMENT:
+                return make_serverless_callback(self.middleware, handler, Route(route))
+            return handler
+
+        return _get
+
+    def post(self, route: str) -> Callable:
+        def _post(handler: Callable) -> Callable:
+            self.methods[HttpMethod.POST].append(Route(route), handler)
+            if IS_SERVERLESS_ENVIRONMENT:
+                return make_serverless_callback(self.middleware, handler, Route(route))
+            return handler
+
+        return _post
+
+    def put(self, route: str) -> Callable:
+        def _put(handler: Callable) -> Callable:
+            self.methods[HttpMethod.PUT].append(Route(route), handler)
+            if IS_SERVERLESS_ENVIRONMENT:
+                return make_serverless_callback(self.middleware, handler, Route(route))
+            return handler
+
+        return _put
+
+    def patch(self, route: str) -> Callable:
+        def _patch(handler: Callable) -> Callable:
+            self.methods[HttpMethod.PATCH].append(Route(route), handler)
+            if IS_SERVERLESS_ENVIRONMENT:
+                return make_serverless_callback(self.middleware, handler, Route(route))
+            return handler
+
+        return _patch
+
+    def delete(self, route: str) -> Callable:
+        def _delete(handler: Callable) -> Callable:
+            self.methods[HttpMethod.DELETE].append(Route(route), handler)
+            if IS_SERVERLESS_ENVIRONMENT:
+                return make_serverless_callback(self.middleware, handler, Route(route))
+            return handler
+
+        return _delete
+
+    def head(self, route: str) -> Callable:
+        def _head(handler: Callable) -> Callable:
+            self.methods[HttpMethod.HEAD].append(Route(route), handler)
+            if IS_SERVERLESS_ENVIRONMENT:
+                return make_serverless_callback(self.middleware, handler, Route(route))
+            return handler
+
+        return _head
+
+    def options(self, route: str) -> Callable:
+        def _options(handler: Callable) -> Callable:
+            self.methods[HttpMethod.OPTIONS].append(Route(route), handler)
+            if IS_SERVERLESS_ENVIRONMENT:
+                return make_serverless_callback(self.middleware, handler, Route(route))
+            return handler
+
+        return _options
+
+    def any(self, route: str) -> Callable:
+        def _any(handler: Callable) -> Callable:
+            self.methods[HttpMethod.GET].append(Route(route), handler)
+            self.methods[HttpMethod.POST].append(Route(route), handler)
+            self.methods[HttpMethod.PUT].append(Route(route), handler)
+            self.methods[HttpMethod.PATCH].append(Route(route), handler)
+            self.methods[HttpMethod.DELETE].append(Route(route), handler)
+            self.methods[HttpMethod.HEAD].append(Route(route), handler)
+            self.methods[HttpMethod.OPTIONS].append(Route(route), handler)
+            if IS_SERVERLESS_ENVIRONMENT:
+                return make_serverless_callback(self.middleware, handler, Route(route))
+            return handler
+
+        return _any
+
+
+__all__ = ["HttpApplication"]
---
+++ packages/chocs/chocs-0.5.1-py3-none-any.whl/chocs/wsgi.py
@@ -0,0 +1,73 @@
+from io import BytesIO
+from typing import Any
+from typing import Callable
+from typing import Dict
+
+from .application import HttpApplication
+from .http_error import HttpError
+from .http_headers import HttpHeaders
+from .http_method import HttpMethod
+from .http_query_string import HttpQueryString
+from .http_request import HttpRequest
+from .http_response import HttpResponse
+from .middleware import MiddlewarePipeline
+from .router_middleware import RouterMiddleware
+
+
+def create_http_request_from_wsgi(environ: Dict[str, Any]) -> HttpRequest:
+    headers = HttpHeaders()
+    for key, value in environ.items():
+        if not key.startswith("HTTP"):
+            continue
+        headers.set(key, value)
+    headers.set("Content-Type", environ.get("CONTENT_TYPE", "text/plain"))
+
+    return HttpRequest(
+        method=HttpMethod(environ.get("REQUEST_METHOD", "GET").upper()),
+        path=environ.get("PATH_INFO", "/"),
+        body=environ.get("wsgi.input", BytesIO(b"")),
+        query_string=HttpQueryString(environ.get("QUERY_STRING", "")),
+        headers=headers,
+    )
+
+
+def create_wsgi_handler(application: HttpApplication, debug: bool = False) -> Callable[[Dict[str, Any]], Callable]:
+
+    def _handler(environ: Dict[str, Any], start: Callable) -> bytes:
+        # Prepare pipeline
+        middleware_pipeline = MiddlewarePipeline(application.middleware.queue)
+        middleware_pipeline.append(RouterMiddleware.from_http_application(application))
+
+        request = create_http_request_from_wsgi(environ)
+
+        if debug:
+            try:
+                response = middleware_pipeline(request)
+            except HttpError as http_error:
+                response = HttpResponse(http_error.http_message, http_error.status_code)
+        else:
+            # Always send a response
+            try:
+                response = middleware_pipeline(request)
+            except HttpError as http_error:
+                response = HttpResponse(http_error.http_message, http_error.status_code)
+            except Exception:
+                response = HttpResponse("Internal Server Error", 500)
+
+        start(
+            str(int(response.status_code)),
+            [(key, value) for key, value in response.headers.items()],
+        )
+
+        response.body.seek(0)
+        return response.body
+
+    return _handler
+
+
+def serve(application: HttpApplication, host: str = "127.0.0.1", port=80, debug: bool = False) -> None:
+    import bjoern
+
+    wsgi_handler = create_wsgi_handler(application, debug=debug)
+
+    bjoern.run(wsgi_handler, host, port)
