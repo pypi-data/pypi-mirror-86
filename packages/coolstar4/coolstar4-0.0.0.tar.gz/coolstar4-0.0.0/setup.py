885ea5fe371e7394ab760b8f100322d0ddb11816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:58 +0000

    Add package coolstar4/0.0.0/coolstar4-0.0.0.tar.gz

---
+++ packages/coolstar4/coolstar4-0.0.0.tar.gz/coolstar4-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="coolstar4",
+    version="0.0.0",
+    keywords=("test", "xxx"),
+    license="MIT Licence",
+    author="seale",
+    author_email="seale@outlook.com",
+
+    packages=find_packages(),
+    install_requires=[]
+)
