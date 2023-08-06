db7f95bfc510a14d349489ab732d399134604d5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:38 +0000

    Add package sshremote/0.0.8/sshremote-0.0.8.tar.gz

---
+++ packages/sshremote/sshremote-0.0.8.tar.gz/sshremote-0.0.8/setup.py
@@ -0,0 +1,30 @@
+import os
+
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="sshremote",
+    version="0.0.8",
+    author="u1234x1234",
+    author_email="u1234x1234@gmail.com",
+    description=("sshremote"),
+    license="MIT",
+    keywords="",
+    url="https://github.com/u1234x1234/sshremote",
+    packages=["sshremote"],
+    setup_requres=["pyngrok>=4", "pydropbear==0.0.6"],  # TODO why install_requires do not work?
+    install_requires=["pyngrok>=4", "pydropbear==0.0.6"],
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
