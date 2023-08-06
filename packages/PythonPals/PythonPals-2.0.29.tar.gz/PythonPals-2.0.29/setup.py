c384de260cf94708a210663834c7214d5c3a56bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:56 +0000

    Add package PythonPals/2.0.29/PythonPals-2.0.29.tar.gz

---
+++ packages/PythonPals/PythonPals-2.0.29.tar.gz/PythonPals-2.0.29/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+
+setup(
+    name='PythonPals',
+    version='2.0.29',
+    packages=['PythonPals'],
+    url='https://github.com/nathanhilton/PythonPals/tree/development',
+    author='Sheamus Cooper',
+    author_email='sheamus.cooper@ufl.edu',
+    description='Production Release',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    classifiers=["Programming Language :: Python :: 3"],
+    entry_points={
+        "console_scripts": [
+            "python-pals=PythonPals.main:main",
+        ]
+    },
+)
