a98d73cd46027b34f19796a1c8d3d9af4a522d20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:51 +0000

    Add package vtb-state-service-enums/1.2.0/vtb-state-service-enums-1.2.0.tar.gz

---
+++ packages/vtb-state-service-enums/vtb-state-service-enums-1.2.0.tar.gz/vtb-state-service-enums-1.2.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+"""
+python setup.py sdist bdist_wheel
+python -m twine upload --repository pypi dist/*
+"""
+
+setup(
+    name='vtb-state-service-enums',
+    version='1.2.0',
+    packages=find_packages(exclude=['tests']),
+    url='https://bitbucket.org/Michail_Shutov/state_service_enums',
+    license='',
+    author='Mikhail Shutov',
+    author_email='michael-russ@yandex.ru',
+    description='enums for VTB state service'
+)
