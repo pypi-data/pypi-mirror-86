8ee541e19b341b8d8ccf368a8ca1b5d90bf60b64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:24 +0000

    Add package syndalib/0.1.1/syndalib-0.1.1.tar.gz

---
+++ packages/syndalib/syndalib-0.1.1.tar.gz/syndalib-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="syndalib",
+    packages=find_packages(
+        include=[
+            "syndalib",
+            "syndalib.utils",
+            "syndalib.makers",
+        ]
+    ),
+    version="0.1.1",
+    description="synthetic point cloud library",
+    author="William Bonvini",
+    license="MIT",
+    install_requires=["pytest-runner"],
+    tests_require=["pytest==4.4.1"],
+    test_suite="tests",
+)
