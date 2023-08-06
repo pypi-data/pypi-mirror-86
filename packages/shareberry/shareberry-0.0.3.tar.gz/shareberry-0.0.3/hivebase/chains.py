b7877622aa547150b8406f3ac5a702e580fc0f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:54 +0000

    Add package shareberry/0.0.3/shareberry-0.0.3.tar.gz

---
+++ packages/shareberry/shareberry-0.0.3.tar.gz/shareberry-0.0.3/hivebase/chains.py
@@ -0,0 +1,11 @@
+default_prefix = "SHARE"
+
+known_chains = {
+    "SHAREBERRY": {
+        "chain_id": "a3ee3a34dd82dcd8e4f9ed14202c6d35734ea4c8d9c0e9daee48ec859d135b6c",
+        "prefix": "SHARE",
+        "steem_symbol": "SHTO",
+        "sbd_symbol": "UNDEFINED",
+        "vests_symbol": "SHPO",
+    },
+}
