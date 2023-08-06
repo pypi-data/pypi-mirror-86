b668256f1496e897d06e08c0e7c2c2b577627c4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:40 +0000

    Add package piepd/0.0.1.5/piepd-0.0.1.5.tar.gz

---
+++ packages/piepd/piepd-0.0.1.5.tar.gz/piepd-0.0.1.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+long_description = open("README.md").read()
+
+setup(
+        name='piepd',
+        version='0.0.1.5',
+        description='High level ePaper display utilty',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        author_email='william.wyatt@cgu.edu',
+        url='https://github.com/Tsangares/epd',
+        include_package_data=True,
+        packages=find_packages(),
+        install_requires=[
+            'Pillow',
+            ],
+        entry_points={
+            'console_scripts': [
+                'beep = src.epaper:main',
+                ]
+            },
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        python_requires='>=3.7',
+)
