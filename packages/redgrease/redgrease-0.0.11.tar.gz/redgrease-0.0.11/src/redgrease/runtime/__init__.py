0b59819d4eb7c66b7a6dab2f86b5f4d037bc5994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:11 +0000

    Add package redgrease/0.0.11/redgrease-0.0.11.tar.gz

---
+++ packages/redgrease/redgrease-0.0.11.tar.gz/redgrease-0.0.11/src/redgrease/runtime/__init__.py
@@ -0,0 +1,26 @@
+import sys
+
+if 'redisgears' not in sys.modules:
+    from .placeholders import (   # noqa: F401
+        atomic,
+        execute,
+        hashtag,
+        configGet,
+        gearsConfigGet,
+        log,
+        GearsBuilder,
+        GB,
+    )
+else:
+    # from redisgears import atomicCtx as atomic  # noqa: F401
+    # from redisgears import executeCommand as execute  # noqa: F401
+    # from redisgears import getMyHashTag as hashtag  # noqa: F401
+    # from redisgears import config_get as configGet  # noqa: F401
+    atomic = atomic
+    execute = execute
+    hashtag = hashtag
+    configGet = configGet
+    gearsConfigGet = gearsConfigGet
+    log = log
+    GearsBuilder = GearsBuilder
+    GB = GB
