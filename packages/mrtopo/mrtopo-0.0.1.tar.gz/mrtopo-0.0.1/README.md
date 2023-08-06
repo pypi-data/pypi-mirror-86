0b4bf361d349a8ab192fbbd5b26358cebc1f966e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:06 +0000

    Add package mrtopo/0.0.1/mrtopo-0.0.1.tar.gz

---
+++ packages/mrtopo/mrtopo-0.0.1.tar.gz/mrtopo-0.0.1/README.md
@@ -0,0 +1,3 @@
+# Mr. Topo
+
+* Author: Faizaan Chishtie
---
+++ packages/mrtopo/mrtopo-0.0.1.tar.gz/mrtopo-0.0.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+import re
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+version = re.search(
+    '^__version__\s*=\s*"(.*)"',
+    open('mrtopo/__main__.py').read(),
+    re.M
+    ).group(1)
+
+setuptools.setup(
+    name='mrtopo',
+    version=version,
+    packages=setuptools.find_packages(),
+    url='https://github.com/FaizChishtie/mrtopo',
+    license='MIT',
+    author='faizchishtie',
+    author_email='faizchishtie@gmail.com',
+    description='Mutate Mininet topology files with MrTopo',
+    python_requires='>=3.0',
+    # entry_points={'console_scripts': ['mrtopo = mrtopo.__main__:main']},
+    long_description=long_description,
+    install_requires=[
+            'mininet',
+            'docopt'
+    ]
+)
