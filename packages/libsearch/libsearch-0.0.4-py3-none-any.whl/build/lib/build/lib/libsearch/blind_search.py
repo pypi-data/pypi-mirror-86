827ffcf390a7a007df1c6b0e5c1d330663a55bad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:01 +0000

    Add package libsearch/0.0.4/libsearch-0.0.4-py3-none-any.whl

---
+++ packages/libsearch/libsearch-0.0.4-py3-none-any.whl/build/lib/build/lib/libsearch/blind_search.py
@@ -0,0 +1,172 @@
+from .data_structs import Node, WeightNode, StackFrontier, QueueFrontier
+
+
+def depth_first_search(**kwargs):
+    actions = kwargs.get('actions', None)
+    start = kwargs.get('start', None)
+    goal = kwargs.get('goal', None)
+    show_explored = kwargs.get('show_explored', False) #if True it will return the explored(closed) set too.
+    depth = kwargs.get('depth', None)   #this is used only when dfs is called in iterative deepening to a certain depth
+    
+    #print("Depth: {}".format(depth))
+    
+    num_explored = 0
+    
+    if depth == None:
+        start = Node(state=start, parent=None, action=None)
+    else: #this is used only when dfs is called in iterative deepening to a certain depth
+        start = Node(state=start, parent=None, action=None, enable_depth=True) 
+
+    frontier = StackFrontier()
+    frontier.add(start)
+    explored = set()
+    print("dfs in pip")
+    while True:
+
+        if frontier.empty():
+            return None
+
+        node = frontier.remove()
+        print('In frontier')
+        num_explored += 1
+
+        if node.state == goal:
+            actions_to_goal = []
+            states_to_goal = []
+            while node.parent is not None:
+                actions_to_goal.append(node.action)
+                states_to_goal.append(node.state)
+                node = node.parent
+            actions_to_goal.reverse()
+            states_to_goal.reverse()
+            solution = (actions_to_goal, states_to_goal)
+            #print(solution)
+            if show_explored:
+                return solution, explored
+            else:
+                return solution
+
+        
+        explored.add(node.state)
+        if depth == None:
+            for action, state in actions(node.state):
+                if not frontier.contains_state(state) and state not in explored:
+                    child = Node(state=state, parent=node, action=action)
+                    frontier.add(child)
+        else:    #this is used only when dfs is called in iterative deepening to a certain depth
+            if node.depth <= depth:
+                for action, state in actions(node.state):
+                    if not frontier.contains_state(state) and state not in explored:
+                        child = Node(state=state, parent=node, action=action, enable_depth=True)
+                        print("Child depth: {}".format(depth))
+                        frontier.add(child)              
+
+
+
+
+def breadth_first_search(**kwargs):
+    actions = kwargs.get('actions', None)
+    start = kwargs.get('start', None)
+    goal = kwargs.get('goal', None)
+    #show_explored = kwargs.get('show_explored', False) #if True it will return the explored(closed) set too.
+
+    num_explored = 0
+    start = Node(state=start, parent=None, action=None)
+    frontier = QueueFrontier()
+    frontier.add(start)
+    explored = set()
+
+    while True:
+
+        if frontier.empty():
+            return None
+
+        node = frontier.remove()
+        #print('In frontier')
+        num_explored += 1
+
+        if node.state == goal:
+            actions_to_goal = []
+            states_to_goal = []
+            while node.parent is not None:
+                actions_to_goal.append(node.action)
+                states_to_goal.append(node.state)
+                node = node.parent
+            actions_to_goal.reverse()
+            states_to_goal.reverse()
+            solution = (actions_to_goal, states_to_goal)
+            return solution
+        
+        explored.add(node.state)
+
+        for action, state in actions(node.state):
+            if not frontier.contains_state(state) and state not in explored:
+                child = Node(state=state, parent=node, action=action)
+                frontier.add(child)
+
+
+
+def iterative_deepening(**kwargs):
+    actions = kwargs.get('actions', None)
+    start = kwargs.get('start', None)
+    goal = kwargs.get('goal', None)
+    show_explored = kwargs.get('show_explored', False) #if True it will return the explored(closed) set too.
+    
+    depth = 1
+    solution = None
+    while solution == None:
+        solution = depth_first_search(actions=actions, start=start, goal=goal, depth=depth, show_explored=show_explored)
+        depth += 1
+    print(solution)
+    return solution
+
+
+def branch_and_bound(**kwargs):
+    actions = kwargs.get('actions', None)
+    start = kwargs.get('start', None)
+    goal = kwargs.get('goal', None)
+    path_cost = kwargs.get('path_cost', None)
+    show_explored = kwargs.get('show_explored', False) #if True it will return the explored(closed) set too.
+
+    start = WeightNode(state=start, parent=None, action=None, cost=0)
+    frontier = QueueFrontier()
+    frontier.add(start)
+    explored = set()
+    best_cost = float('inf')
+    best_solution = None
+    while True:
+
+        if frontier.empty():
+            if show_explored:
+                return best_solution, explored
+            else:
+                return best_solution
+
+        node = frontier.remove()
+        #node.cost = node.parent.cost + path_cost(node.parent.state + node.state)
+        print("Pickedup:",node.action, node.state, node.cost)
+        if node.cost < best_cost:
+            print('inside')
+            if node.state == goal and node.parent:
+                print("solution found")
+                best_cost = node.cost
+                actions_to_goal = []
+                states_to_goal = []
+                while node.parent is not None:
+                    actions_to_goal.append(node.action)
+                    states_to_goal.append(node.state)
+                    node = node.parent
+                actions_to_goal.reverse()
+                states_to_goal.reverse()
+                best_solution = (actions_to_goal, states_to_goal)
+                print("best solution: {}".format(best_solution))
+                print("best cost: {}".format(best_cost))
+
+            explored.add((node.action,node.state))
+
+            for action, state in actions(node.action, node.state):
+                if not frontier.contains_node(action, state) and (action,state) not in explored:
+                    child = Node(state=state, parent=node, action=action)
+                    child.cost = child.parent.cost + path_cost(child.parent.state, child.state)
+                    print(child.action, child.cost)
+                    frontier.add(child)
---
+++ packages/libsearch/libsearch-0.0.4-py3-none-any.whl/build/lib/libsearch/informed_search.py
@@ -0,0 +1,198 @@
+from .data_structs import HeuristicNode, MyPriorityQueue, StackFrontier
+
+
+def a_star(**kwargs):
+    actions = kwargs.get('actions', None)
+    start = kwargs.get('start', None)
+    goal = kwargs.get('goal', None)
+    heuristic = kwargs.get('heuristic', None)
+    show_explored = kwargs.get('show_explored', False) #if True it will return the explored(closed) set too.
+    
+
+    num_explored = 0
+    start = HeuristicNode(state=start, parent=None, action=None, cost=0)
+    
+    frontier = MyPriorityQueue()
+    costs = {}
+    frontier.add_task(start, start.cost)
+    costs[start] = start.cost
+    
+    explored = set()
+
+    while True:
+        if frontier.empty():
+            return None
+    
+        node = frontier.pop_task()
+        print(node)
+        num_explored += 1
+        explored.add(node.state)
+
+        if node.state == goal:
+            actions_to_goal = []
+            states_to_goal = []
+            while node.parent is not None:
+                actions_to_goal.append(node.action)
+                states_to_goal.append(node.state)
+                node = node.parent
+            actions_to_goal.reverse()
+            states_to_goal.reverse()
+            solution = (actions_to_goal, states_to_goal)
+            if show_explored:
+                return solution, explored
+            else:
+                return solution
+            #return solution
+        print("node state: {} , goal: {}".format(node.state, goal))
+        print("Node cost: {}".format(node.cost))
+        
+        for action, state in actions(node.state):
+            if state in explored:
+                continue
+            child = HeuristicNode(state=state, parent=node, action=action)
+            child.cost = child.cost_from_start + heuristic(child.state, goal)
+            if child not in costs or  child.cost < costs[child]:
+                if child in costs:
+                    frontier.remove_task(child)
+                costs[child] = child.cost
+                frontier.add_task(child, child.cost)
+
+
+def best_first_search(**kwargs):
+    actions = kwargs.get('actions', None)
+    start = kwargs.get('start', None)
+    goal = kwargs.get('goal', None)
+    heuristic = kwargs.get('heuristic', None)
+    show_explored = kwargs.get('show_explored', False) #if True it will return the explored(closed) set too.
+    
+
+    num_explored = 0
+    start = HeuristicNode(state=start, parent=None, action=None, cost=0)
+    
+    frontier = MyPriorityQueue()
+    costs = {}
+    frontier.add_task(start, start.cost)
+    costs[start] = start.cost
+    
+    explored = set()
+
+    while True:
+        if frontier.empty():
+            return None
+    
+        node = frontier.pop_task()
+        print(node)
+        num_explored += 1
+        explored.add(node.state)
+
+        if node.state == goal:
+            actions_to_goal = []
+            states_to_goal = []
+            while node.parent is not None:
+                actions_to_goal.append(node.action)
+                states_to_goal.append(node.state)
+                node = node.parent
+            actions_to_goal.reverse()
+            states_to_goal.reverse()
+            solution = (actions_to_goal, states_to_goal)
+            if show_explored:
+                return solution, explored
+            else:
+                return solution
+            #return solution
+        print("node state: {} , goal: {}".format(node.state, goal))
+        print("Node cost: {}".format(node.cost))
+        
+        for action, state in actions(node.state):
+            if state in explored:
+                continue
+            child = HeuristicNode(state=state, parent=node, action=action)
+            child.cost = heuristic(child.state, goal)
+            if child not in costs or  child.cost < costs[child]:
+                if child in costs:
+                    frontier.remove_task(child)
+                costs[child] = child.cost
+                frontier.add_task(child, child.cost)
+
+from heapq import heappush, heappop
+def id_depth_first_search(**kwargs):
+    '''
+    This dfs implementation is used only for iterative deepening a* below.
+    Returns the solution or the new cost limit to be inserted into next dfs search
+    '''
+    actions = kwargs.get('actions', None)
+    start = kwargs.get('start', None)
+    goal = kwargs.get('goal', None)
+    heuristic = kwargs.get('heuristic', None)
+    show_explored = kwargs.get('show_explored', False) #if True it will return the explored(closed) set too.
+    depth = kwargs.get('depth', None)   #this is used only when dfs is called in iterative deepening to a certain depth
+    
+    #print("Depth: {}".format(depth))
+    
+    num_explored = 0
+    
+
+    start = HeuristicNode(state=start, parent=None, action=None, cost=0)
+
+    frontier = StackFrontier()
+    frontier.add(start)
+    encountered_costs = []
+    explored = set()
+    print("dfs in informed")
+    while True:
+
+        #when the search with the bound of cost finish without solution, returns the new bound which is the minimum from costs greater than current bound.
+        if frontier.empty():
+            return heappop(encountered_costs)
+
+        node = frontier.remove()
+        print('In frontier')
+        num_explored += 1
+
+        if node.state == goal:
+            actions_to_goal = []
+            states_to_goal = []
+            while node.parent is not None:
+                actions_to_goal.append(node.action)
+                states_to_goal.append(node.state)
+                node = node.parent
+            actions_to_goal.reverse()
+            states_to_goal.reverse()
+            solution = (actions_to_goal, states_to_goal)
+            print(solution)
+            if show_explored:
+                return solution, explored
+            else:
+                return solution
+            #return solution
+
+        
+        explored.add(node.state)
+        if node.cost <= depth:
+            for action, state in actions(node.state):
+                if not frontier.contains_state(state) and state not in explored:
+                    child = HeuristicNode(state=state, parent=node, action=action)
+                    child.cost = child.cost_from_start + heuristic(child.state, goal)
+                    frontier.add(child)
+        else:
+            heappush(encountered_costs, node.cost)
+
+def iterative_deepening_a_star(**kwargs):
+    actions = kwargs.get('actions', None)
+    start = kwargs.get('start', None)
+    goal = kwargs.get('goal', None)
+    heuristic = kwargs.get('heuristic', None)
+    show_explored = kwargs.get('show_explored', False) #if True it will return the explored(closed) set too.
+    
+    root_node = HeuristicNode(state=start, parent=None, action=None, cost=0)
+    depth = root_node.cost
+    solution = None
+    while True:
+        solution = id_depth_first_search(actions=actions, start=start, goal=goal, depth=depth, heuristic=heuristic, show_explored=show_explored)
+        if isinstance(solution, int):
+            depth  = solution
+            print("Depth id: {}".format(depth))
+        else:
+            break
+    print(solution)
+    return solution
