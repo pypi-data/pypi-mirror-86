9d0428b916d406cb8b0562f6a602686f8825314a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:12 +0000

    Add package kenworthy/0.1.1/kenworthy-0.1.1.tar.gz

---
+++ packages/kenworthy/kenworthy-0.1.1.tar.gz/kenworthy-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='kenworthy',
+    url='https://github.com/mkenworthy/kenworthy',
+    author='Matthew Kenworthy',
+    author_email='matthew.kenworthy@gmail.com',
+    # Needed to actually package something
+    packages=['kenworthy'],
+    # Needed for dependencies
+    install_requires=['numpy','astropy'],
+    # *strongly* suggested for sharing
+    version='0.1.1',
+    # The license can be anything you like
+    license='MIT',
+    description='Astronomy routines for extrasolar planets, exorings and dust scattering',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
