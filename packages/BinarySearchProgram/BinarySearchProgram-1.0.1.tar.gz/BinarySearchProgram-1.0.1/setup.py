6606f122ec56c36c2dd55d26b303bf6f153a79d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:09 +0000

    Add package BinarySearchProgram/1.0.1/BinarySearchProgram-1.0.1.tar.gz

---
+++ packages/BinarySearchProgram/BinarySearchProgram-1.0.1.tar.gz/BinarySearchProgram-1.0.1/setup.py
@@ -0,0 +1,27 @@
+﻿"""Сценарий установки"""
+
+
+from setuptools import setup, find_packages
+import pathlib
+
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+setup(
+    name='BinarySearchProgram',
+    version='1.0.1',
+    description='Binary search program',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Pavel Artemyev',
+    author_email='sanchello13pv@gmail.com',
+    packages=find_packages(),
+    package_dir={'': 'package'},
+    python_requires='>=3.5, <4',
+    entry_points={
+        'console_scripts': [
+                 'run=package:__main__',
+        ],
+    },
+
+)
