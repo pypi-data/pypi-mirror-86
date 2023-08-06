70ac208aefc658185599bb9fe1572c26a9a12904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:58 +0000

    Add package badFormatter/1.1.0/badFormatter-1.1.0.tar.gz

---
+++ packages/badFormatter/badFormatter-1.1.0.tar.gz/badFormatter-1.1.0/README.md
@@ -0,0 +1,29 @@
+# Bad Formatting
+
+This is gets rid (or at least moves) all those pesky '{', '}' and ';' that we all hate (dont we?)
+
+## Web version
+No need to install anything - if you prefer you can just head over [here](https://format.johnmontgomery.tech) in order to shitify your code online!
+
+## Installation
+
+Use the package manager [pip](https://pypi.org/project/bad_formatting/) to install bad formatting.
+
+```bash
+pip install badFormatter
+```
+or clone this repo and run in the same way
+
+## Usage (for Pip)
+
+```python
+import badFormatter
+
+badFormatter.shitify_print(filename) # prints the code given a filename
+badFormatter.shitify_return(filename) # returns the code given a filename
+badFormatter.shitify_print_text(text) # prints the code reformatted from the input
+badFormatter.shitify_return_text(text) # returns the code reformatted from the input
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ packages/badFormatter/badFormatter-1.1.0.tar.gz/badFormatter-1.1.0/badFormatter/form.py
@@ -0,0 +1,58 @@
+def read_file(filename):
+    return [line.replace('\n', '') for line in open(filename, 'r')]
+
+
+def formatter(text):
+    form = '{:<' + str(max([len(line) for line in text]) + 1) + '}'
+    for idx, line in enumerate(text):
+        line = form.format(line)
+        to_add = []
+        new_line = []
+        for charidx, char in enumerate(line):
+            if char in '{}':
+                to_add.append(char)
+                new_line.append(' ')
+            elif char == ';' and len(set(line[charidx:])) == 2:
+                to_add.append(char)
+                new_line.append(' ')
+            else:
+                new_line.append(char)
+
+        if len(set(new_line)) == 1 or len(set(new_line)) == 0:
+            found, i = False, 1
+            while not found:
+                if len(text[idx - i]) > 1:
+                    break
+                i += 1
+            text[idx - i] = ''.join(list(text[idx - i]) + to_add)
+            text[idx] = ''
+        else:
+            new_line += to_add
+            text[idx] = ''.join(new_line)
+
+    result = ''
+    for line in text:
+        if line:
+            result += line + '\n' if line else ''
+    return result
+
+
+def shitify_print(filename):
+    print(formatter(read_file(filename)))
+
+
+def shitify_return(filename):
+    return formatter(read_file(filename))
+
+
+def shitify_print_text(text):
+    text = [line.replace('\n', '') for line in text.split('\n')]
+    print(formatter(text))
+
+
+def shitify_return_text(text):
+    text = [line.replace('\n', '') for line in text.split('\n')]
+    return formatter(text)
+
+
+shitify_print('example.txt')
