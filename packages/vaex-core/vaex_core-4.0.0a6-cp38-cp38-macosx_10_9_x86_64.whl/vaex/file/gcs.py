c1759e95da95e799559dd5c291546263a78ae3d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:32 +0000

    Add package vaex-core/4.0.0a6/vaex_core-4.0.0a6-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/vaex-core/vaex_core-4.0.0a6-cp38-cp38-macosx_10_9_x86_64.whl/vaex/file/gcs.py
@@ -0,0 +1,34 @@
+import pyarrow.fs
+
+
+import vaex.utils
+gcsfs = vaex.utils.optional_import('gcsfs', '>=0.6.2')
+import vaex.file.cache
+from . import split_options, split_scheme
+from .cache import FileSystemHandlerCached
+
+normal_open = open
+
+
+def glob(path, fs_options={}):
+    if '?' in path:
+        __, query = path[:path.index('?')], path[path.index('?'):]
+    else:
+        query = ''
+    path, options = split_options(path, fs_options)
+    fs = gcsfs.GCSFileSystem(**options)
+    return ['gs://' + k + query for k in fs.glob(path)]
+
+
+def parse(path, fs_options={}):
+    path = path.replace('fsspec+gs://', 'gs://')
+    path, options = split_options(path, fs_options)
+    scheme, path = split_scheme(path)
+    assert scheme == 'gs'
+    use_cache = fs_options.pop('cache', 'true') in [True, 'true', 'True', '1']
+    fs = gcsfs.GCSFileSystem(**options,)
+    fs = pyarrow.fs.FSSpecHandler(fs)
+    if use_cache:
+        fs = FileSystemHandlerCached(fs, scheme='gs')
+    fs = pyarrow.fs.PyFileSystem(fs)
+    return fs, path
