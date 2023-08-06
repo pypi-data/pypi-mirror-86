94baece06c3010b03bd66b37509ef491020bf5e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:59 +0000

    Add package fret/0.3.4/fret-0.3.4.tar.gz

---
+++ packages/fret/fret-0.3.4.tar.gz/fret-0.3.4/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+from setuptools import setup
+import re
+
+with open('fret/__init__.py') as f:
+    version = re.search(r'__version__ = \'(.+)\'', f.read()).group(1)
+
+with open('README.md') as f:
+    long_description = f.read()
+
+test_deps = [
+    'pytest>=4.6.0',
+    'pytest-cov>=2.6.0',
+    'pytest-flake8>=1',
+]
+doc_deps = [
+    'sphinx',
+    'sphinx-rtd-theme',
+    'recommonmark'
+]
+dev_deps = test_deps + doc_deps + [
+    'setuptools>=40',
+    'wheel'
+]
+
+setup(
+    name='fret',
+    version=version,
+    url='https://github.com/yxonic/fret',
+    license='MIT',
+    author='yxonic',
+    author_email='yxonic@gmail.com',
+    description='Framework for Reproducible ExperimenTs.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['fret'],
+    platforms='any',
+    python_requires='>=3.4',
+    install_requires=[
+        'toml',
+    ],
+    tests_require=test_deps,
+    extras_require={
+        'test': test_deps,
+        'doc': doc_deps,
+        'dev': dev_deps
+    },
+    entry_points={
+        'console_scripts': [
+            'fret = fret.cli:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+    ],
+)
