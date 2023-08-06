163a5d118b97690968c54b2156ea843a4a48c7d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:40 +0000

    Add package hzd-aiida-toolkits/0.0.1/hzd_aiida_toolkits-0.0.1.tar.gz

---
+++ packages/hzd-aiida-toolkits/hzd_aiida_toolkits-0.0.1.tar.gz/hzd_aiida_toolkits-0.0.1/README.md
@@ -0,0 +1,4 @@
+# hzd_aiida_toolkits
+This package is for my own research purpose, it will accelerate my research
+
+## How to install the package?
---
+++ packages/hzd-aiida-toolkits/hzd_aiida_toolkits-0.0.1.tar.gz/hzd_aiida_toolkits-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+from setuptools import setup
+setup(
+    name='hzd_aiida_toolkits',
+    version='0.0.1',
+    description='My toolkits for using Aiida',
+    author='Zheng-Da He',
+    author_email='z.he@fz-juelich.de, jameshzd@mail.ustc.edu.cn, zhengdahe.electrocatalysis@gmail.com',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    zip_safe=False
+)
