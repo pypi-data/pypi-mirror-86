1160cc4698c17ac1265aad2ac5b8afa7c7f014d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:16 +0000

    Add package chives-exchange/0.1.1/chives-exchange-0.1.1.tar.gz

---
+++ packages/chives-exchange/chives-exchange-0.1.1.tar.gz/chives-exchange-0.1.1/README.md
@@ -0,0 +1,32 @@
+# chives-exchange
+Implementation of a stock exchange with matching engine and a web UI powered by 
+Flask.
+
+## Getting started
+The webserver communicates to the matching engine through RabbitMQ, which can be 
+most easily run using docker:
+```bash
+docker run -d --rm \
+    --name "rabbitmq" \
+    -p 5672:5672 \
+    -p 15672:15672 \
+    rabbitmq:3-management
+```
+
+Both the webserver and the matching engine reads from and writes to a SQL 
+database, which by default is a SQLite located in `/tmp/chives.sqlite`. To 
+initialize the database schema:
+```bash
+python -m chives initdb
+```
+
+Run the webserver and matching engine in two separate processes. Make sure that 
+the matching engine is not erroring out because of failure to connect to 
+RabbitMQ.
+```bash
+python -m chives start_engine
+python -m chives webserver
+```
+
+You can now follow the demo [here](https://xuganyu96.github.io/chives-exchange/) 
+to try it out.
---
+++ packages/chives-exchange/chives-exchange-0.1.1.tar.gz/chives-exchange-0.1.1/chives/blueprints/auth.py
@@ -0,0 +1,78 @@
+import functools 
+
+from flask import (
+    Blueprint, flash, g as flask_g, redirect, render_template, request, 
+    session as flask_session, url_for
+)
+from flask_login import login_user, logout_user
+from werkzeug.security import check_password_hash, generate_password_hash 
+
+from chives.webserver import login_manager
+from chives.db import get_db as get_db_session
+from chives.forms import RegistrationForm, LoginForm
+from chives.models.models import User, Asset
+
+bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+@login_manager.user_loader 
+def load_user(user_id):
+    db_session = get_db_session()
+    if user_id is not None:
+        return db_session.query(User).get(user_id)
+    return None
+
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return redirect(url_for("auth.login"))
+
+
+@bp.route("/register", methods=("GET", "POST"))
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == "POST" and form.validate_on_submit():
+        db_session = get_db_session()
+        if db_session.query(User).filter(
+            User.username==form.username.data).first() is not None:
+            form.username.errors.append("Username already taken!")
+        else:
+            new_user: User = User(
+                username=form.username.data, 
+                password_hash=generate_password_hash(form.password.data))
+            db_session.add(new_user)
+            db_session.commit()
+            # Each user receives $10,000.00 of cash upon registration
+            initial_cash = Asset(
+                owner_id=new_user.user_id,
+                asset_symbol="_CASH",
+                asset_amount=10000.00
+            )
+            db_session.add(initial_cash)
+            db_session.commit()
+
+            return redirect(url_for("auth.login"))
+
+    return render_template("auth/register.html", form=form)
+
+
+@bp.route("/login", methods=("GET", "POST"))
+def login():
+    form = LoginForm(request.form)
+    if request.method == "POST" and form.validate_on_submit():
+        db_session = get_db_session()
+        user = db_session.query(
+            User).filter(User.username==form.username.data).first()
+        if (user is not None) and check_password_hash(user.password_hash, 
+                                                      form.password.data):
+            login_user(user)
+            return redirect(url_for('exchange.dashboard'))
+        else:
+            form.password.errors.append(
+                "Login failed; check your username and password")
+    
+    return render_template("auth/login.html", form=form)
+
+@bp.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("auth.login"))
