0625dbb16bed89d59c3615385c064ce1ac51a177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:34 +0000

    Add package HypixelIO/1.1.0/HypixelIO-1.1.0-py3-none-any.whl

---
+++ packages/HypixelIO/HypixelIO-1.1.0-py3-none-any.whl/hypixelio/__init__.py
@@ -0,0 +1,7 @@
+from .lib import Client, Converters
+from .models.caching import CacheBackend, Caching
+from .utils import constants
+
+__author__ = "Sunrit Jana"
+__email__ = "warriordefenderz@gmail.com"
+__version__ = "1.1.0"
