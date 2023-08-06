6e31cf86678bb92fac61ccf8f07a4e8f9dd5fbb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:50 +0000

    Add package orsaytrace/1.0/orsaytrace-1.0.tar.gz

---
+++ packages/orsaytrace/orsaytrace-1.0.tar.gz/orsaytrace-1.0/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+        name='orsaytrace',
+        packages = ['orsaytrace'],
+        version = '1.0',
+        license = 'MIT',
+        description = 'Flexible ray tracing for optics',
+        author = 'Yves Auad',
+        author_email = 'yves.maia-auad@universite-paris-saclay.fr',
+        url = 'https://github.com/yvesauad/OrsayTrace',
+        download_url = 'https://github.com/yvesauad/OrsayTrace/archive/v1.0.tar.gz',
+        keywords = ['optics', 'ray', 'tracing'],
+        install_requires = [
+            'numpy',
+            'tqdm',
+            'matplotlib',
+            ],
+        classifiers = [
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Build Tools',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.6',
+            ],
+        )
+
+            
