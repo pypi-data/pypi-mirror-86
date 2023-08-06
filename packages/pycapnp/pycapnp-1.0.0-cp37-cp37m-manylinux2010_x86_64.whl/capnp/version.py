7688bdfe8023a2ba162689084264255d1fae0f26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:57 +0000

    Add package pycapnp/1.0.0/pycapnp-1.0.0-cp37-cp37m-manylinux2010_x86_64.whl

---
+++ packages/pycapnp/pycapnp-1.0.0-cp37-cp37m-manylinux2010_x86_64.whl/capnp/version.py
@@ -0,0 +1,8 @@
+version = '1.0.0'
+short_version = '1.0.0'
+
+# flake8: noqa E402 F401
+from .lib.capnp import _CAPNP_VERSION_MAJOR as LIBCAPNP_VERSION_MAJOR
+from .lib.capnp import _CAPNP_VERSION_MINOR as LIBCAPNP_VERSION_MINOR
+from .lib.capnp import _CAPNP_VERSION_MICRO as LIBCAPNP_VERSION_MICRO
+from .lib.capnp import _CAPNP_VERSION as LIBCAPNP_VERSION
