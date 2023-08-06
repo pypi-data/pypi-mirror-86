a68e1aa200edb431be6779bbbe4f0ec0ef7bf50c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:15 +0000

    Add package gda-score-code/2.5.4/gda-score-code-2.5.4.tar.gz

---
+++ packages/gda-score-code/gda-score-code-2.5.4.tar.gz/gda-score-code-2.5.4/setup.py
@@ -0,0 +1,57 @@
+import fnmatch
+import setuptools
+from setuptools.command.build_py import build_py as build_py_orig
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# excluded = ['common/gdaTool.py', 'common/gdaQuery.py']
+excluded = []
+
+
+class build_py(build_py_orig):
+    def find_package_modules(self, package, package_dir):
+        modules = super().find_package_modules(package, package_dir)
+        return [
+            (pkg, mod, file)
+            for (pkg, mod, file) in modules
+            if not any(fnmatch.fnmatchcase(file, pat=pattern) for pattern in excluded)
+        ]
+
+
+setuptools.setup(
+    # Use this for production:
+    name="gda-score-code",
+    version="2.5.4",
+    # Use this for testing:
+    #name="gda-score-code-test",
+    #version="2.5.13",  # for test.pypi
+    author="Paul Francis",
+    description="Tools for generating General Data Anonymity Scores (www.gda-score.org)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gda-score/code",
+    packages=setuptools.find_packages(include=['gdascore', 'gdascore.global_config']),
+    cmdclass={'build_py': build_py},
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_data={'': ['master.json', 'myCredentials.json', 'config_var.json']},
+    include_package_data=True,
+    install_requires=[
+        'coloredlogs>=14.0',
+        'matplotlib>=3.0.2',
+        'numpy>=1.16.0',
+        'pandas>=1.1.0',
+        'psycopg2>=2.8.3',
+        'python-dateutil>=2.7.5',
+        'simplejson>=3.16.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'gdascore_init = gdascore.cli:init',
+        ]
+    }
+)
