4ac8e0569ebf4c616ebfb1c31cc1c0ce7ee7de00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:43 +0000

    Add package sparkautomapper.fhir/0.1.56/sparkautomapper.fhir-0.1.56.tar.gz

---
+++ packages/sparkautomapper.fhir/sparkautomapper.fhir-0.1.56.tar.gz/sparkautomapper.fhir-0.1.56/spark_auto_mapper_fhir/complex_types/fill.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+from spark_auto_mapper_fhir.fhir_types.id import FhirId
+
+from spark_auto_mapper_fhir.complex_types.fhir_complex_type_base import FhirComplexTypeBase
+
+from spark_auto_mapper_fhir.complex_types.duration import Duration
+from spark_auto_mapper_fhir.complex_types.quantity import Quantity
+
+
+class Fill(FhirComplexTypeBase):
+    def __init__(
+        self,
+        id_: Optional[FhirId] = None,
+        quantity: Optional[Quantity] = None,
+        duration: Optional[Duration] = None
+    ):
+        """
+        Fill Resource in FHIR
+        https://hl7.org/FHIR/medicationrequest.html
+
+
+        :param quantity: fill quantity
+        :param duration: fill duration
+        """
+        super().__init__(id_=id_, quantity=quantity, duration=duration)
