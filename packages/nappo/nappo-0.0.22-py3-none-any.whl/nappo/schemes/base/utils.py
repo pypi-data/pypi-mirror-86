83d82b645732bd85356d14726adeb23e82fb2be4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:23 +0000

    Add package nappo/0.0.22/nappo-0.0.22-py3-none-any.whl

---
+++ packages/nappo/nappo-0.0.22-py3-none-any.whl/nappo/schemes/base/utils.py
@@ -0,0 +1,12 @@
+import socket
+from contextlib import closing
+
+
+def find_free_port():
+    """
+    from https://github.com/ray-project/ray/blob/master/python/ray/util/sgd/utils.py
+    """
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.bind(("", 0))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return s.getsockname()[1]
