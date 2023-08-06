5ab514c6009198cc69d13c6def1c0d7f555e0929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:11 +0000

    Add package kokos/1.0.14/kokos-1.0.14-py3-none-any.whl

---
+++ packages/kokos/kokos-1.0.14-py3-none-any.whl/kks/cli.py
@@ -0,0 +1,29 @@
+import click
+
+from kks.cmd.auth import auth
+from kks.cmd.gen import gen
+from kks.cmd.init import init
+from kks.cmd.lint import lint
+from kks.cmd.run import run
+from kks.cmd.sync import sync
+from kks.cmd.status import status
+from kks.cmd.test import test
+from kks.cmd.top import top
+from kks.cmd.open import open
+
+
+@click.group()
+def cli():
+    """KoKoS helper tool"""
+
+
+cli.add_command(auth)
+cli.add_command(init)
+cli.add_command(run)
+cli.add_command(test)
+cli.add_command(gen)
+cli.add_command(lint)
+cli.add_command(sync)
+cli.add_command(status)
+cli.add_command(top)
+cli.add_command(open)
