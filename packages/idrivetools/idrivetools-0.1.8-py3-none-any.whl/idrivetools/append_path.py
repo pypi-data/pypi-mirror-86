bceaf2628598beeb1e0b17c4d5a8588e453f9840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:32 +0000

    Add package idrivetools/0.1.8/idrivetools-0.1.8-py3-none-any.whl

---
+++ packages/idrivetools/idrivetools-0.1.8-py3-none-any.whl/idrivetools/append_path.py
@@ -0,0 +1,10 @@
+import sys
+"""
+This file is used to include the packages in the libs folder that were
+imported at time of build. PEP8 compliance.
+"""
+sys.path.append("idrivetools")
+
+
+def appended():
+    return True
