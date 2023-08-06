de15858a000f066aee26c9de00425720b1be97eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:17 +0000

    Add package dupper/0.1.2/dupper-0.1.2.tar.gz

---
+++ packages/dupper/dupper-0.1.2.tar.gz/dupper-0.1.2/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['dupper']
+install_requires = \
+['fire>=0.3.1,<0.4.0']
+
+entry_points = \
+{'console_scripts': ['dupper = src.dupper:main']}
+
+setup_kwargs = {
+    'name': 'dupper',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'nixiesquid',
+    'author_email': 'audu817@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
