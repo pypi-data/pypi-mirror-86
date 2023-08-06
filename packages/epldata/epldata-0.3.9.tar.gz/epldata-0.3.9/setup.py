78058b77faad607622e5e14386486156fe4382c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:39 +0000

    Add package epldata/0.3.9/epldata-0.3.9.tar.gz

---
+++ packages/epldata/epldata-0.3.9.tar.gz/epldata-0.3.9/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="epldata",
+    version="0.3.9",
+    author="Sarang Purandare",
+    author_email="purandare.sarang@gmail.com",
+    description="Stuff",
+    long_description="You dont gotta know",
+    long_description_content_type="text/markdown",
+    packages=['epldata'],
+    install_requires=[
+        'pandas',
+        'requests',
+        'ipython', 
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ]
+)
