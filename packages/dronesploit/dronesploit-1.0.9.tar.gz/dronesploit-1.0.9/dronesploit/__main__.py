d803411320dcc96445573221e4f28ab26d54d81e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:00 +0000

    Add package dronesploit/1.0.9/dronesploit-1.0.9.tar.gz

---
+++ packages/dronesploit/dronesploit-1.0.9.tar.gz/dronesploit-1.0.9/dronesploit/__main__.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from dronesploit.__info__ import __author__, __copyright__, __email__, __license__, __version__
+from dronesploit.lib import DronesploitConsole as BaseConsole
+from tinyscript import *
+
+
+__script__ = "dronesploit"
+__doc__    = """
+Dronesploit framework's launcher script.
+"""
+
+
+def at_exit():
+    subprocess.call("service network-manager restart", shell=True)
+    subprocess.call("reset", shell=True)
+
+
+class DronesploitConsole(BaseConsole):
+    sources = {
+        'banners':   "banners",
+        'entities':  ["commands", "models", "modules"],
+        'libraries': ".",
+    }
+
+
+def main():
+    parser.add_argument("--dev", action="store_true", help="development mode")
+    initialize(exit_at_interrupt=False, sudo=True)
+    c = DronesploitConsole(
+        __scriptname__,
+        banner_section_styles={'title': {'fgcolor': "random"}},
+        dev=args.dev,
+    )
+    if args.verbose:
+        c.execute("set DEBUG true")
+    c.start()
+
