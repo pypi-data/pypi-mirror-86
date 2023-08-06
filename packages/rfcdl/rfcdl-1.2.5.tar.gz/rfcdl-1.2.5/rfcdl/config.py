d5fe058072ea9bdcc08d9882045ee74c94b99150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:14 +0000

    Add package rfcdl/1.2.5/rfcdl-1.2.5.tar.gz

---
+++ packages/rfcdl/rfcdl-1.2.5.tar.gz/rfcdl-1.2.5/rfcdl/config.py
@@ -0,0 +1,62 @@
+import configparser
+import logging
+import os
+
+from pathlib import Path
+
+from .exception import RfcDLConfigurationException
+
+logger = logging.getLogger('rfcdl')
+
+
+def load_config(path):
+    msg = "Loading configuration '{}'."
+    logger.debug(msg.format(path))
+
+    config = configparser.ConfigParser(interpolation=None)
+
+    try:
+        config.read(path)
+    except configparser.Error:
+        msg = "Cannot load configuration '{}'."
+        logger.error(msg.format(path))
+        exit(1)
+
+    return config
+
+
+def get_default_config_file():
+    msg = 'Loading default configuration file.'
+    logger.debug(msg)
+
+    subdirectory = 'rfcdl'
+    filename = 'config.ini'
+
+    base = '~/.config'
+    base = os.getenv('XDG_CONFIG_HOME', base)
+    base = Path(base)
+    base = base.expanduser()
+    base = base.resolve()
+
+    config = base / subdirectory / filename
+
+    return config
+
+
+def get_root_dir(path):
+    if path is None:
+        msg = 'Invalid root directory.'
+        raise RfcDLConfigurationException(msg)
+
+    path = Path(path)
+    path = path.expanduser()
+    path = path.resolve()
+
+    if not path.exists():
+        path.mkdir(parents=True)
+
+    if not path.is_dir():
+        msg = 'Root directory path is not a directory.'
+        raise RfcDLConfigurationException(msg)
+
+    return path
---
+++ packages/rfcdl/rfcdl-1.2.5.tar.gz/rfcdl-1.2.5/rfcdl/rfcdl.py
@@ -0,0 +1,70 @@
+import logging
+
+from .arguments import parse_arguments
+from .config import load_config, get_default_config_file, get_root_dir
+from .downloader import RfcDownloader
+from .exception import RfcDLArgumentException, RfcDLConfigurationException
+from .logging import setup_logger
+
+logger = logging.getLogger('rfcdl')
+
+
+def main():
+    setup_logger(logger)
+
+    try:
+        args = parse_arguments()
+    except RfcDLArgumentException as e:
+        msg = 'Failed to parse arguments: ' + str(e)
+        logger.error(msg)
+        exit(1)
+
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+        msg = 'Application is running in debug mode.'
+        logger.debug(msg)
+    elif args.quiet:
+        logger.setLevel(logging.WARNING)
+
+    config_file = args.config_file
+
+    if config_file is None:
+        config_file = get_default_config_file()
+
+    config = load_config(config_file)
+
+    logger.info('Starting RfcDL.')
+
+    def load_parameter(required, conf_section, conf_name, arg_name):
+        arg = getattr(args, arg_name, None)
+
+        if arg is not None:
+            return arg
+
+        conf = config.get(conf_section, conf_name, fallback=None)
+
+        if not required:
+            return conf
+        else:
+            msg = "Required argument not provided: '{}'."
+            logger.error(msg.format(arg_name))
+            exit(1)
+
+    directory = load_parameter(True, 'GENERAL', 'RootDir', 'directory')
+
+    try:
+        directory = get_root_dir(directory)
+    except RfcDLConfigurationException as e:
+        msg = 'Initialization not successful: ' + str(e)
+        logger.error(msg)
+        exit(1)
+
+    dl = RfcDownloader(
+        path=directory,
+        samples=args.samples,
+        limit=args.limit,
+        retries=args.retries,
+    )
+    dl.download(
+        delete_obsolete=args.delete_obsolete,
+    )
