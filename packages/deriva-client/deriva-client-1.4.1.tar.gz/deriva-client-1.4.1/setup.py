ff376640d30aae3cb048b19e4de00c107dfdf0b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:55 +0000

    Add package deriva-client/1.4.1/deriva-client-1.4.1.tar.gz

---
+++ packages/deriva-client/deriva-client-1.4.1.tar.gz/deriva-client-1.4.1/setup.py
@@ -0,0 +1,60 @@
+#
+# Copyright 2019 University of Southern California
+# Distributed under the GNU GPL 3.0 license. See LICENSE for more info.
+#
+
+""" Installation script for DERIVA Client Tools
+"""
+import io
+from setuptools import setup
+from version import __version__
+
+
+def get_readme_contents():
+    with io.open('README.md') as readme_file:
+        return readme_file.read()
+
+
+url = 'https://github.com/informatics-isi-edu/deriva-client'
+author = 'USC Information Sciences Institute, Informatics Systems Research Division'
+author_email = 'isrd-support@isi.edu'
+
+
+setup(
+    name='deriva-client',
+    description='A Python-based application suite of client software for use with the DERIVA platform.',
+    long_description=get_readme_contents(),
+    long_description_content_type='text/markdown',
+    url=url,
+    author=author,
+    author_email=author_email,
+    maintainer=author,
+    maintainer_email=author_email,
+    version=__version__,
+    python_requires='>3.5.2',
+    install_requires=[
+        'bdbag[boto,globus]==1.5.6',
+        'fair-research-login==0.1.5',
+        'fair-identifiers-client==0.4.1',
+        'deriva==1.4.1',
+        'deriva-qt==1.4.1',
+        'deriva-catalog-manage==0.8.1',
+        'bdbag_gui==1.0.0',
+        "PyQt5==5.11.3"
+    ],
+    license='GNU GPL 3.0',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        "Operating System :: POSIX",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ]
+)
+
+
---
+++ packages/deriva-client/deriva-client-1.4.1.tar.gz/deriva-client-1.4.1/version.py
@@ -0,0 +1,6 @@
+import sys
+
+__version__ = "1.4.1"
+
+if __name__ == '__main__':
+    sys.exit(print(__version__))
