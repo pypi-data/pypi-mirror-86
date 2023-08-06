1888e51fa80c50777942cb0365dbd82ce7bc9244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:30 +0000

    Add package kiwoom/0.0.6/kiwoom-0.0.6.tar.gz

---
+++ packages/kiwoom/kiwoom-0.0.6.tar.gz/kiwoom-0.0.6/setup.py
@@ -0,0 +1,63 @@
+from setuptools import setup, find_packages
+
+
+# Project Information
+VERSION = "0.0.6"
+DISTNAME = "kiwoom"
+DESCRIPTION = "Python API of Kiwoom OPEN API+"
+LONG_DESCRIPTION = open("README.md", encoding="utf-8").read()
+LONG_DESCRIPTION_CONTENT_TYPE = 'text/markdown'
+PROJECT_URLS = {
+    "Git": "https://github.com/breadum/kiwoom",
+    "Tutorials": "https://github.com/breadum/kiwoom/tree/main/tutorials"
+}
+
+# Author Information
+AUTHOR = "Breadum"
+EMAIL = "breadum.kr@gmail.com"
+
+# Miscellaneous
+LICENSE = "MIT License"
+CLASSIFIERS = [
+    "Development Status :: 2 - Pre-Alpha",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: Microsoft :: Windows",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Topic :: Office/Business :: Financial :: Investment"
+]
+
+
+def setup_package():
+    kwargs = {
+        "install_requires": [
+            'PyQt5 >= 5.12'
+        ],
+        "setup_requires": [],
+        "keywords": ["Kiwoom", "Open API+", "키움", "영웅문"],
+        'zip_safe': False
+    }
+
+    setup(
+        name=DISTNAME,
+        version=VERSION,
+        author=AUTHOR,
+        author_email=EMAIL,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type=LONG_DESCRIPTION_CONTENT_TYPE,
+        project_urls=PROJECT_URLS,
+        packages=find_packages(),
+        python_requires=">=3.6",
+        license=LICENSE,
+        classifiers=CLASSIFIERS,
+        **kwargs
+    )
+
+
+if __name__ == '__main__':
+    setup_package()
