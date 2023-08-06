db27695697a8edf1096e9a4cabb53a4c64ba3a05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:49 +0000

    Add package pypix/0.1.1/pypix-0.1.1.tar.gz

---
+++ packages/pypix/pypix-0.1.1.tar.gz/pypix-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pypix']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pypix',
+    'version': '0.1.1',
+    'description': 'Python library for generating pix codes with CRC16 validation',
+    'long_description': None,
+    'author': 'Daniel Yohan',
+    'author_email': 'dyohan9@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
