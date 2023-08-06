ed695706a2de7e5cd05f8ab64e5242d1e318c041
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:48 +0000

    Add package pseudo-microbit/1.9/pseudo-microbit-1.9.tar.gz

---
+++ packages/pseudo-microbit/pseudo-microbit-1.9.tar.gz/pseudo-microbit-1.9/README.md
@@ -0,0 +1,11 @@
+![logo](https://raw.github.com/MrYsLab/pseudo-microbit/master/docs/images/logo.png)
+
+# pseudo-microbit
+
+A _pseudo_ implementation of the micro:bit micropython API. used in conjunction
+with tools such as PyCharm.
+
+Integrated the typehints from https://github.com/vlasovskikh/intellij-micropython
+
+A full tutorial may be found here: https://mryslab.github.io/pseudo-microbit/
+
---
+++ packages/pseudo-microbit/pseudo-microbit-1.9.tar.gz/pseudo-microbit-1.9/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='pseudo-microbit',
+    version='1.9',
+    packages=['microbit'],
+    py_modules=['microbit', 'compass', 'display',
+                'i2c', 'spi', 'uart', 'music', 'neopixel',
+                'os', 'speech', 'radio'],
+    install_requires=['microfs',
+                      'uflash',
+                      'pyminifier'],
+    url='https://github.com/MrYsLab/pseudo-microbit',
+    download_url='https://github.com/MrYsLab/pseudo-microbit',
+    license='MIT',
+    author='Alan Yorinks',
+    author_email='MisterYsLab@gmail.com',
+    description='API to be used with PyCharm for micro:bit python development',
+    keywords=['micro:bit', 'microbit', 'micropython'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development',
+    ],
+)
