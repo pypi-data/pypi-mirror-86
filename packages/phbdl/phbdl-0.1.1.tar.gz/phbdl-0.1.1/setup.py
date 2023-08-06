92cede4c3dab77c19aa3ce2b684070b4d97d2f8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:50 +0000

    Add package phbdl/0.1.1/phbdl-0.1.1.tar.gz

---
+++ packages/phbdl/phbdl-0.1.1.tar.gz/phbdl-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.1'
+
+setup(name='phbdl',
+      version=VERSION,
+      description="a tiny and smart cli video downloader for pxxxhub",
+      classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='video downloader',
+      author='sunbless',
+      author_email='sunblessliu@gmail.com',
+      url='https://github.com/yontonl/phbdl',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          'requests', 'js2py', 'bs4'
+      ],
+      entry_points={
+          'console_scripts': [
+              'phbdl = phbdl.main:main'
+          ]
+      },
+      )
