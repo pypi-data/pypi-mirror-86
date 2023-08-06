67da83bb7ad2e19ebda8b604991a8a4f1e6d214f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:34 +0000

    Add package virtool-workflow/0.1.2/virtool_workflow-0.1.2.tar.gz

---
+++ packages/virtool-workflow/virtool_workflow-0.1.2.tar.gz/virtool_workflow-0.1.2/tests/test_exec.py
@@ -0,0 +1,6 @@
+from virtool_workflow import execute
+
+
+async def test_sh():
+    out, _ = await execute.run_shell_command(["ls"])
+    assert out
---
+++ packages/virtool-workflow/virtool_workflow-0.1.2.tar.gz/virtool_workflow-0.1.2/virtool_workflow_runtime/autoload.py
@@ -0,0 +1,8 @@
+"""Workflow fixtures from this module are implicitly loaded by the runtime."""
+
+__fixtures__ = [
+    "virtool_workflow.execute",
+    "virtool_workflow_runtime.db.db",
+    "virtool_workflow_runtime.db.fixtures",
+    "virtool_workflow_runtime.config.environment",
+]
