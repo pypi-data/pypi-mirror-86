7e71e0ff3055c5dba7232c30c428a60e82cfbd4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:41 +0000

    Add package fastapi-mqtt/0.0.3/fastapi_mqtt-0.0.3-py3-none-any.whl

---
+++ packages/fastapi-mqtt/fastapi_mqtt-0.0.3-py3-none-any.whl/fastapi_mqtt/__init__.py
@@ -0,0 +1,15 @@
+
+from fastapi_mqtt.fastmqtt import FastMQTT
+from fastapi_mqtt.config import  MQQTConfig
+
+
+__author__ = "Sabuhi Shukurov"
+
+__email__ = 'sabuhi.shukurov@gmail.com'
+
+__credits__ = ["Sabuhi Shukurov","Hasan Aliyev", "Tural Muradov"]
+
+__version__ = "0.0.3"
+
+
+__all__ = ["FastMQTT", "MQQTConfig"]
