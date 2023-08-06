a529fb2306b027bf16822ec4163c83a957c36232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:16 +0000

    Add package pyalink/1.3.0/pyalink-1.3.0-py3-none-any.whl

---
+++ packages/pyalink/pyalink-1.3.0-py3-none-any.whl/pyalink/alink/stream/common/stream_op_4.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from ..base import StreamOperator, BaseSinkStreamOp, BaseModelStreamOp
+
+
+class Word2VecPredictStreamOp(BaseModelStreamOp):
+    CLS_NAME = 'com.alibaba.alink.operator.stream.nlp.Word2VecPredictStreamOp'
+    OP_TYPE = 'FUNCTION'
+
+    def __init__(self, *args, **kwargs):
+        kwargs['CLS_NAME'] = self.CLS_NAME
+        kwargs['OP_TYPE'] = self.OP_TYPE
+        super(Word2VecPredictStreamOp, self).__init__(*args, **kwargs)
+        pass
+
+    def setSelectedCol(self, val):
+        return self._add_param('selectedCol', val)
+
+    def setNumThreads(self, val):
+        return self._add_param('numThreads', val)
+
+    def setOutputCol(self, val):
+        return self._add_param('outputCol', val)
+
+    def setPredMethod(self, val):
+        return self._add_param('predMethod', val)
+
+    def setReservedCols(self, val):
+        return self._add_param('reservedCols', val)
+
+    def setWordDelimiter(self, val):
+        return self._add_param('wordDelimiter', val)
+
