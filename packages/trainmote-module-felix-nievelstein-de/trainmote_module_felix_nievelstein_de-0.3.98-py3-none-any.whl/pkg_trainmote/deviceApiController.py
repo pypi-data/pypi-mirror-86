ea3bd5db62ee82df2fe85ff93475ad084dcb1807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:09 +0000

    Add package trainmote-module-felix-nievelstein-de/0.3.98/trainmote_module_felix_nievelstein_de-0.3.98-py3-none-any.whl

---
+++ packages/trainmote-module-felix-nievelstein-de/trainmote_module_felix_nievelstein_de-0.3.98-py3-none-any.whl/pkg_trainmote/deviceApiController.py
@@ -0,0 +1,24 @@
+from flask import Blueprint
+from flask import request
+from flask import abort
+from flask import Response
+from . import baseAPI
+from . import deviceController
+from . import gpioservice
+
+
+deviceApiBlueprint = Blueprint('stopPointApi', __name__)
+
+@deviceApiBlueprint.route('/trainmote/api/v1/device/restart', methods=["POST"])
+def restartDevice():
+    gpioservice.clean()
+    timer = deviceController.RestartTimer()
+    timer.start()
+    return "", 200
+
+@deviceApiBlueprint.route('/trainmote/api/v1/device/shutdown', methods=["POST"])
+def shutdownDevice():
+    gpioservice.clean()
+    timer = deviceController.ShutDownTimer()
+    timer.start()
+    return "", 200
