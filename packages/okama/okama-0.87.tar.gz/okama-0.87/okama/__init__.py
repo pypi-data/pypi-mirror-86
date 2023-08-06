4c119a7ba3c2cfa98ec5171933b4059f01774989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:15 +0000

    Add package okama/0.87/okama-0.87.tar.gz

---
+++ packages/okama/okama-0.87.tar.gz/okama-0.87/okama/__init__.py
@@ -0,0 +1,9 @@
+from okama.assets import Asset, AssetList, Portfolio
+from okama.macro import Inflation, Rate
+from okama.frontier import EfficientFrontier
+from okama.frontier_reb import EfficientFrontierReb
+from okama.plots import Plots, PlotPortfolio
+from okama.data import QueryData, API, search
+from okama.helpers import Float, Frame, Rebalance, Date
+from okama.settings import namespaces
+import okama.settings
