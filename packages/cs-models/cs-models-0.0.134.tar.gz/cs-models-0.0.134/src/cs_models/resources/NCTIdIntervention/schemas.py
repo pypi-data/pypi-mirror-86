de0f81f47df98d32c6452f80358fcd2ae8368d01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:56 +0000

    Add package cs-models/0.0.134/cs-models-0.0.134.tar.gz

---
+++ packages/cs-models/cs-models-0.0.134.tar.gz/cs-models-0.0.134/src/cs_models/resources/NCTIdIntervention/schemas.py
@@ -0,0 +1,16 @@
+from marshmallow import (
+    Schema,
+    fields,
+    validate,
+)
+
+
+class NCTIdInterventionResourceSchema(Schema):
+    not_blank = validate.Length(min=1, error='Field cannot be blank')
+
+    id = fields.Integer(dump_only=True)
+    nct_id = fields.String(validate=not_blank, required=True)
+    intervention_id = fields.Integer(required=True)
+    narrow = fields.Boolean(required=True)
+    group = fields.Integer(required=True)
+    updated_at = fields.DateTime()
