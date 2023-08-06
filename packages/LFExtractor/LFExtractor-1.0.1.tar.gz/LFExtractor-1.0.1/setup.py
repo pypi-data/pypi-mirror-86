613ba15e97dbea6615c825097ad42421684165f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:40 +0000

    Add package LFExtractor/1.0.1/LFExtractor-1.0.1.tar.gz

---
+++ packages/LFExtractor/LFExtractor-1.0.1.tar.gz/LFExtractor-1.0.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="LFExtractor",
+    version="1.0.1",
+    packages=['LFE'],
+    description="A corpus-linguistic tool to extract and search for linguistic features",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    author="Jack Wang",
+    author_email="jackwang196531@gmail.com",
+
+    url="https://github.com/jaaack-wang/ling_feature_extractor",
+    keywords=['Computational linguistics', 'Corpus-linguistic tool', 'NLP', 'Natural Language Processing'],
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    license="MIT License",
+)
