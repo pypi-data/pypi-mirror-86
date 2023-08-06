8d482c476b53ef3d2968af11fc767c048da37d26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:42 +0000

    Add package spectralsequence-chart/0.0.14/spectralsequence_chart-0.0.14.tar.gz

---
+++ packages/spectralsequence-chart/spectralsequence_chart-0.0.14.tar.gz/spectralsequence_chart-0.0.14/spectralsequence_chart/__init__.py
@@ -0,0 +1,21 @@
+""" TODO: Overview of the package.
+
+"""
+
+# from __future__ import annotations
+
+# from spectralsequence_chart import chart_elements
+# import spectralsequence_chart.chart
+# import spectralsequence_chart.chart_elements
+# import spectralsequence_chart.helper_classes
+
+from .chart_class import ChartClass, ChartClassStyle, Shape
+from .chart_edge import (
+    ChartEdge, ChartEdgeStyle,
+    ChartStructline, ChartDifferential, ChartExtension,
+)
+from .infinity import INFINITY
+from .chart import SseqChart
+
+__version__ = "0.0.14"
+# __all__ = [ChartClass, ChartClassStyle, Shape, ]
