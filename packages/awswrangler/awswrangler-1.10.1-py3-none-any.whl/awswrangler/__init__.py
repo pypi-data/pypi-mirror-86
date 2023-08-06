65ad0078c0353968977ad1b833b46759095a5a6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:38 +0000

    Add package awswrangler/1.10.1/awswrangler-1.10.1-py3-none-any.whl

---
+++ packages/awswrangler/awswrangler-1.10.1-py3-none-any.whl/awswrangler/__init__.py
@@ -0,0 +1,31 @@
+"""Initial Module.
+
+Source repository: https://github.com/awslabs/aws-data-wrangler
+Documentation: https://aws-data-wrangler.readthedocs.io/
+
+"""
+
+import logging as _logging
+
+from awswrangler import athena, catalog, cloudwatch, db, emr, exceptions, quicksight, s3, sts  # noqa
+from awswrangler.__metadata__ import __description__, __license__, __title__, __version__  # noqa
+from awswrangler._config import config  # noqa
+
+__all__ = [
+    "athena",
+    "catalog",
+    "cloudwatch",
+    "db",
+    "emr",
+    "exceptions",
+    "quicksight",
+    "s3",
+    "sts",
+    "config",
+    "__description__",
+    "__license__",
+    "__title__",
+    "__version__",
+]
+
+_logging.getLogger("awswrangler").addHandler(_logging.NullHandler())
