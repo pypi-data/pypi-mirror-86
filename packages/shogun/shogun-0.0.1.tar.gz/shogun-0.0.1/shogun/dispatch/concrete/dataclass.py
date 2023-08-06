71e87cfd1db0d63997f41a6b62545f70c1e93228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:18 +0000

    Add package shogun/0.0.1/shogun-0.0.1.tar.gz

---
+++ packages/shogun/shogun-0.0.1.tar.gz/shogun-0.0.1/shogun/dispatch/concrete/dataclass.py
@@ -0,0 +1,9 @@
+import dataclasses
+
+from shogun.dispatch.record_class import DispatcherIsRecordClass
+
+
+class DispatcherIsDataclass(DispatcherIsRecordClass):
+    @classmethod
+    def is_type(cls, field_type: type) -> bool:
+        return dataclasses.is_dataclass(field_type)
