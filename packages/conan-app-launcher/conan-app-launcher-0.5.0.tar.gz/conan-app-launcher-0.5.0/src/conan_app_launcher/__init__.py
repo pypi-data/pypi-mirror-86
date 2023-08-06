cf0e1d9cba1dd5691a96bb2a8644f5fa25375ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:23 +0000

    Add package conan-app-launcher/0.5.0/conan-app-launcher-0.5.0.tar.gz

---
+++ packages/conan-app-launcher/conan-app-launcher-0.5.0.tar.gz/conan-app-launcher-0.5.0/src/conan_app_launcher/__init__.py
@@ -0,0 +1,27 @@
+"""
+Contains global constants and basic/ui variables.
+"""
+__version__ = "0.5.0"
+
+from pathlib import Path
+
+# this allows to use forward declarations to avoid circular imports
+from typing import TYPE_CHECKING, Optional
+if TYPE_CHECKING:
+    from .components import ConanWorker
+    from PyQt5 import QtWidgets
+
+### Global constants ###
+PROG_NAME = "conan_app_launcher"
+ICON_SIZE = 64
+
+### Global variables ###
+# 0: No debug, 1 = debug logging on
+DEBUG_LEVEL = 0
+
+# paths to find folders - points to the folder of this file
+base_path: Path = Path()
+
+# qt_application instance
+qt_app: Optional["QtWidgets.QApplication"] = None
+conan_worker: Optional["ConanWorker"] = None
