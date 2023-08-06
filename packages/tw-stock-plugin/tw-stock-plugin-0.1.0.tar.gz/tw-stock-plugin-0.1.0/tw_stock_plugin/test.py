a1d2eb4a27c14cb4b1656c09512b8423cd0ce1c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:50 +0000

    Add package tw-stock-plugin/0.1.0/tw-stock-plugin-0.1.0.tar.gz

---
+++ packages/tw-stock-plugin/tw-stock-plugin-0.1.0.tar.gz/tw-stock-plugin-0.1.0/tw_stock_plugin/test.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*
+"""
+      ┏┓       ┏┓
+    ┏━┛┻━━━━━━━┛┻━┓
+    ┃      ☃      ┃
+    ┃  ┳┛     ┗┳  ┃
+    ┃      ┻      ┃
+    ┗━┓         ┏━┛
+      ┗┳        ┗━┓
+       ┃          ┣┓
+       ┃          ┏┛
+       ┗┓┓┏━━━━┳┓┏┛
+        ┃┫┫    ┃┫┫
+        ┗┻┛    ┗┻┛
+    God Bless,Never Bug
+"""
+from datetime import datetime
+from tw_stock_plugin.core.stock_peratio import StockPERatio
+
+# UpdateStock.main()
+
+date_ = datetime(2020, 10, 30).date()
+
+stock_p_e_ratio = StockPERatio(date_=date_)
+
+
+print(stock_p_e_ratio.get_history(code='9962')[date_])
