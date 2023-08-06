2b6fd5716e6ea1bf88750b2f3b542b0c32c00e3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:03 +0000

    Add package mofapy2/0.5.8/mofapy2-0.5.8.tar.gz

---
+++ packages/mofapy2/mofapy2-0.5.8.tar.gz/mofapy2-0.5.8/R/make_example_data.R
@@ -0,0 +1,155 @@
+
+#' @title Simulate a data set using the generative model of MOFA
+#' @name make_example_data
+#' @description Function to simulate an example multi-view multi-group data set according to the generative model of MOFA2.
+#' @param n_views number of views
+#' @param n_features number of features in each view 
+#' @param n_samples number of samples in each group
+#' @param n_groups number of groups
+#' @param n_factors number of factors
+#' @param likelihood likelihood for each view, one of "gaussian" (default), "bernoulli", "poisson",
+#'  or a character vector of length n_views
+#' @param lscales vector of lengthscales, needs to be of length n_factors (default is 0 - no smooth factors)
+#' @param sample_cov (only for use with MEFISTO) matrix of sample covariates for one group with covariates in rows and samples in columns 
+#' or "equidistant" for sequential ordering, default is NULL (no smooth factors)
+#' @param as.data.frame return data and covariates as long dataframe 
+#' @return Returns a list containing the simulated data and simulation parameters.
+#' @importFrom stats rnorm rbinom rpois
+#' @importFrom dplyr left_join
+#' @importFrom mvtnorm rmvnorm
+#' @importFrom stats dist
+#' @export
+#' @examples
+#' # Generate a simulated data set
+#' MOFAexample <- make_example_data()
+
+
+make_example_data <- function(n_views=3, n_features=100, n_samples = 50, n_groups = 1,
+                            n_factors = 5, likelihood = "gaussian",
+                            lscales = 1, sample_cov = NULL, as.data.frame = FALSE) {
+  
+  # Sanity checks
+  if (!all(likelihood %in% c("gaussian", "bernoulli", "poisson")))
+    stop("Likelihood not implemented: Use either gaussian, bernoulli or poisson")
+  
+  if(length(lscales) == 1)
+    lscales = rep(lscales, n_factors)
+  if(!length(lscales) == n_factors)
+    stop("Lengthscalces lscales need to be of length n_factors")
+  if(all(lscales == 0)){
+    sample_cov <- NULL
+  }
+  
+  if (length(likelihood)==1) likelihood <- rep(likelihood, n_views) 
+  if (!length(likelihood) == n_views) 
+    stop("Likelihood needs to be a single string or matching the number of views!")
+  
+  if(!is.null(sample_cov)){
+    if(sample_cov[1] == "equidistant") {
+      sample_cov <- seq_len(n_samples)
+    }
+    if(is.null(dim(sample_cov))) sample_cov <- matrix(sample_cov, nrow = 1)
+    if(ncol(sample_cov) != n_samples){
+      stop("Number of columns in sample_cov must match number of samples n_samples.")
+    }
+  
+    # Simulate covariance for factors
+    Sigma = lapply(lscales, function(ls) {
+      if(ls == 0) diag(1, n_samples)
+      else (1) * exp(-as.matrix(stats::dist(t(sample_cov)))^2/(2*ls^2))
+      # else (1-0.001) * exp(-as.matrix(stats::dist(t(sample_cov)))^2/(2*ls^2)) + diag(0.001, n_samples)
+    })
+  
+    # simulate factors
+    alpha_z <- NULL
+    S_z <- lapply(seq_len(n_groups), function(vw) matrix(1, nrow=n_samples, ncol=n_factors))
+    Z <-  vapply(seq_len(n_factors), function(fc) mvtnorm::rmvnorm(1, rep(0, n_samples), Sigma[[fc]]), numeric(n_samples))
+    colnames(Z) <- paste0("simulated_factor_", 1:ncol(Z))
+    Z <- lapply(seq_len(n_groups), function(gr) Z)
+    sample_cov <- Reduce(cbind, lapply(seq_len(n_groups), function(gr) sample_cov))
+  } else {
+    # set sparsity for factors
+    theta_z <- 0.5
+    
+    # set ARD prior for factors, each factor being active in at least one group
+    alpha_z <- vapply(seq_len(n_factors), function(fc) {
+      active_gw <- sample(seq_len(n_groups), 1)
+      alpha_fc <- sample(c(1, 1000), n_groups, replace = TRUE)
+      if(all(alpha_fc==1000)) alpha_fc[active_gw] <- 1
+      alpha_fc
+    }, numeric(n_groups))
+    alpha_z <- matrix(alpha_z, nrow=n_factors, ncol=n_groups, byrow=TRUE)
+    
+    # simulate facors 
+    S_z <- lapply(seq_len(n_groups), function(vw) matrix(rbinom(n_samples * n_factors, 1, theta_z),
+                                                         nrow=n_samples, ncol=n_factors))
+    Z <- lapply(seq_len(n_groups), function(vw) vapply(seq_len(n_factors), function(fc) rnorm(n_samples, 0, sqrt(1/alpha_z[fc,vw])), numeric(n_samples)))
+  }
+  
+  # set sparsity for weights
+  theta_w <- 0.5
+  
+  # set ARD prior, each factor being active in at least one view
+  alpha_w <- vapply(seq_len(n_factors), function(fc) {
+    active_vw <- sample(seq_len(n_views), 1)
+    alpha_fc <- sample(c(1, 1000), n_views, replace = TRUE)
+    if(all(alpha_fc==1000)) alpha_fc[active_vw] <- 1
+    alpha_fc
+  }, numeric(n_views))
+  alpha_w <- matrix(alpha_w, nrow=n_factors, ncol=n_views, byrow=TRUE)
+  
+  # simulate weights 
+  S_w <- lapply(seq_len(n_views), function(vw) matrix(rbinom(n_features*n_factors, 1, theta_w),
+                                             nrow=n_features, ncol=n_factors))
+  W <- lapply(seq_len(n_views), function(vw) vapply(seq_len(n_factors), function(fc) rnorm(n_features, 0, sqrt(1/alpha_w[fc,vw])), numeric(n_features)))
+  
+  # set noise level (for gaussian likelihood)
+  tau <- 10
+  
+  # pre-compute linear term and rbind groups
+  mu <- lapply(seq_len(n_views), function(vw) lapply(seq_len(n_groups), function(gw)  (S_z[[gw]]*Z[[gw]]) %*% t(S_w[[vw]]*W[[vw]])))
+  mu <- lapply(mu, function(l) Reduce(rbind, l))
+  groups <- rep(paste("group",seq_len(n_groups), sep = "_"), each = n_samples)
+  
+  # simulate data according to the likelihood
+  data <- lapply(seq_len(n_views), function(vw){
+    lk <- likelihood[vw]
+    if (lk == "gaussian"){
+      dd <- t(mu[[vw]] + rnorm(length(mu[[vw]]),0,sqrt(1/tau)))
+    }
+    else if (lk == "poisson"){
+      term <- log(1+exp(mu[[vw]]))
+      dd <- t(apply(term, 2, function(tt) rpois(length(tt),tt)))
+    }
+    else if (lk == "bernoulli") {
+      term <- 1/(1+exp(-mu[[vw]]))
+      dd <- t(apply(term, 2, function(tt) rbinom(length(tt),1,tt)))
+    }
+    colnames(dd) <- paste0("sample_", seq_len(ncol(dd)))
+    rownames(dd) <- paste0("feature_", seq_len(nrow(dd)),"_view", vw)
+    dd
+  })
+
+  if(!is.null(sample_cov)) {
+    colnames(sample_cov) <- colnames(data[[1]])
+    rownames(sample_cov) <- paste0("covariate_", seq_len(nrow(sample_cov)))
+  }
+ 
+  names(data) <- paste0("view_", seq_len(n_views))
+  
+  if(as.data.frame){
+    gr_df <- data.frame(group = groups, sample = colnames(data[[1]]))
+    dat <- lapply(names(data), function(vw){
+            tmp <- data[[vw]]
+            df <- melt(tmp, varnames = c("feature", "sample"))
+            df$view <- vw
+            df
+    })
+    data <- bind_rows(dat)
+    data <- dplyr::left_join(data, gr_df, by = "sample")
+    
+    sample_cov <- melt(sample_cov, varnames = c("covariate", "sample"))
+  }
+  return(list(data = data, groups = groups, alpha_w=alpha_w, alpha_z =alpha_z,
+              lscales = lscales, sample_cov = sample_cov, Z = Z))
+}
---
+++ packages/mofapy2/mofapy2-0.5.8.tar.gz/mofapy2-0.5.8/R/run_mofa.R
@@ -0,0 +1,217 @@
+#######################################
+## Functions to train a MOFA model ##
+#######################################
+
+#' @title Train a MOFA model
+#' @name run_mofa
+#' @description Function to train an untrained \code{\link{MOFA}} object.
+#' @details This function is called once a MOFA object has been prepared (using \code{\link{prepare_mofa}})
+#' In this step the R package calls the \code{mofapy2} Python package, where model training is performed. \cr
+#' The interface with Python is done with the \code{\link{reticulate}} package. 
+#' If you have several versions of Python installed and R is not detecting the correct one, 
+#' you can change it using \code{reticulate::use_python} when loading the R session. 
+#' Alternatively, you can let us install mofapy2 for you using \code{basilisk} if you set use_basilisk to \code{TRUE}
+#' @param object an untrained \code{\link{MOFA}} object
+#' @param save_data logical indicating whether to save the training data in the hdf5 file. 
+#'  This is useful for some downstream analysis (mainly functions with the prefix \code{plot_data}), but it can take a lot of disk space.
+#' @param outfile output file for the model (.hdf5 format). If \code{NULL}, a temporary file is created.
+#' @param use_basilisk use \code{basilisk} to automatically install a conda environment with mofapy2 and all dependencies? 
+#' If \code{FALSE} (default), you should specify the right python binary when loading R with \code{reticulate::use_python(..., force=TRUE)}
+#' or the right conda environment with \code{reticulate::use_condaenv(..., force=TRUE)}.
+#' @return a trained \code{\link{MOFA}} object
+#' @import reticulate
+#' @import basilisk
+#' @export
+#' @examples
+#' # Load data (in data.frame format)
+#' file <- system.file("extdata", "test_data.RData", package = "MOFA2")
+#' load(file) 
+#' 
+#' # Create the MOFA object
+#' MOFAmodel <- create_mofa(dt)
+#' 
+#' # Prepare the MOFA object with default options
+#' MOFAmodel <- prepare_mofa(MOFAmodel)
+#' 
+#' # Run the MOFA model
+#' \dontrun{ MOFAmodel <- run_mofa(MOFAmodel, use_basilisk = TRUE) }
+run_mofa <- function(object, outfile = NULL, save_data = TRUE, use_basilisk = FALSE) {
+  
+  # Sanity checks
+  if (!is(object, "MOFA")) 
+    stop("'object' has to be an instance of MOFA")
+  if (object@status=="trained") 
+    stop("The model is already trained! If you want to retrain, create a new untrained MOFA")
+  
+  # If no outfile is provided, store a file in the /tmp folder with the respective timestamp
+  if (is.null(outfile) || is.na(outfile) || (outfile == "")) {
+    outfile <- object@training_options$outfile
+    if (is.null(outfile) || is.na(outfile) || (outfile == "")) {
+      outfile <- file.path("/tmp", paste0("mofa_", format(Sys.time(), format = "%Y%m%d-%H%M%S"), ".hdf5"))
+      warning(paste0("No output filename provided. Using ", outfile, " to store the trained model.\n\n"))
+    }
+  }
+  if (file.exists(outfile))
+    message(paste0("Warning: Output file ", outfile, " already exists, it will be replaced"))
+  
+  # Connect to mofapy2 using reticulate (default)
+  if (!use_basilisk) {
+
+    message("Connecting to the mofapy2 python package using reticulate (use_basilisk = FALSE)... 
+    Please make sure to manually specify the right python binary when loading R with reticulate::use_python(..., force=TRUE) or the right conda environment with reticulate::use_condaenv(..., force=TRUE)
+    If you prefer to let us automatically install a conda environment with 'mofapy2' installed using the 'basilisk' package, please use the argument 'use_basilisk = TRUE'\n")
+    
+    # Sanity checks
+    have_mofa2 <- py_module_available("mofapy2")
+    if (have_mofa2) {
+      .run_mofa_reticulate(object, outfile, save_data)
+    } else {
+      warning("mofapy2 is not detected in the specified python binary, see reticulate::py_config(). Setting use_basilisk = TRUE...")
+      use_basilisk <- TRUE
+    }
+  }
+    
+  # Connect to mofapy2 using basilisk (optional)
+  if (use_basilisk) {
+    
+    message("Connecting to the mofapy2 package using basilisk. 
+    Set 'use_basilik' to FALSE if you prefer to manually set the python binary using 'reticulate'.")
+    
+    proc <- basiliskStart(mofa_env)
+    on.exit(basiliskStop(proc))
+    tmp <- basiliskRun(proc, function(object, outfile, save_data) {
+      .run_mofa_reticulate(object, outfile, save_data)
+    }, object=object, outfile=outfile, save_data=save_data)
+  }
+  
+  # Load the trained model
+  object <- load_model(outfile)
+  
+  return(object)
+}
+
+
+
+.run_mofa_reticulate <- function(object, outfile, save_data) {
+  
+  # sanity checks
+  if (!is(object, "MOFA")) stop("'object' has to be an instance of MOFA")
+  if (!requireNamespace("reticulate", quietly = TRUE)) {
+    stop("Package \"reticulate\" is required but is not installed.", call. = FALSE)
+  }
+  
+  # Initiate reticulate
+  mofa <- import("mofapy2")
+  
+  # Call entry point
+  mofa_entrypoint <- mofa$run.entry_point$entry_point()
+  
+  # Set data options
+  mofa_entrypoint$set_data_options(
+    scale_views = object@data_options$scale_views,
+    scale_groups = object@data_options$scale_groups
+  )
+
+  # Set samples metadata
+  if (.hasSlot(object, "samples_metadata")) {
+    mofa_entrypoint$data_opts$samples_metadata <- r_to_py(lapply(object@data_options$groups,
+                                                                 function(g) object@samples_metadata[object@samples_metadata$group == g,]))
+  }
+
+  # Set features metadata
+  if (.hasSlot(object, "features_metadata")) {
+    mofa_entrypoint$data_opts$features_metadata <- r_to_py(unname(lapply(object@data_options$views,
+                                                                         function(m) object@features_metadata[object@features_metadata$view == m,])))
+  }
+  
+  # Set the data
+  mofa_entrypoint$set_data_matrix(
+    data = r_to_py( unname(lapply(object@data, function(x) unname( lapply(x, function(y) r_to_py(t(y)) ))) ) ),
+    likelihoods = unname(object@model_options$likelihoods),
+    views_names = r_to_py(as.list(object@data_options$views)),
+    groups_names = r_to_py(as.list(object@data_options$groups)),
+    samples_names = r_to_py(unname(lapply(object@data[[1]], colnames))),
+    features_names = r_to_py(unname(lapply(object@data, function(x) rownames(x[[1]]))))
+  )
+  
+  # Set covariates
+  if (!is.null(object@covariates)) {
+    sample_cov_to_py <- r_to_py(unname(lapply(object@covariates, function(x) unname(r_to_py(t(x))))))
+    cov_names_2_py <- r_to_py(covariates_names(object))
+    mofa_entrypoint$set_covariates(sample_cov_to_py, cov_names_2_py)
+  }
+  
+  # Set model options 
+  mofa_entrypoint$set_model_options(
+    factors     = object@model_options$num_factors,
+    spikeslab_factors = object@model_options$spikeslab_factors, 
+    spikeslab_weights = object@model_options$spikeslab_weights, 
+    ard_factors       = object@model_options$ard_factors,
+    ard_weights       = object@model_options$ard_weights 
+  )
+  
+  # Set training options  
+  mofa_entrypoint$set_train_options(
+    iter             = object@training_options$maxiter,
+    convergence_mode = object@training_options$convergence_mode,
+    dropR2           = object@training_options$drop_factor_threshold,
+    startELBO        = object@training_options$startELBO,
+    freqELBO         = object@training_options$freqELBO,
+    seed             = object@training_options$seed, 
+    gpu_mode         = object@training_options$gpu_mode,
+    verbose          = object@training_options$verbose,
+    outfile          = object@training_options$outfile,
+    save_interrupted = object@training_options$save_interrupted
+  )
+  
+  
+  # Set stochastic options
+  if (object@training_options$stochastic) {
+    mofa_entrypoint$set_stochastic_options(
+      learning_rate    = object@stochastic_options$learning_rate,
+      forgetting_rate  = object@stochastic_options$forgetting_rate,
+      batch_size       = object@stochastic_options$batch_size,
+      start_stochastic = object@stochastic_options$start_stochastic
+    )
+  }
+  
+  # Set smooth covariate options  
+  if (!is.null(object@covariates) & length(object@smooth_options)>1) {
+    warping_ref <- which(groups_names(object) == object@smooth_options$warping_ref)
+    mofa_entrypoint$set_smooth_options(
+      scale_cov           = object@smooth_options$scale_cov,
+      start_opt           = as.integer(object@smooth_options$start_opt),
+      n_grid              = as.integer(object@smooth_options$n_grid),
+      opt_freq            = as.integer(object@smooth_options$opt_freq),
+      model_groups        = object@smooth_options$model_groups,
+      sparseGP            = object@smooth_options$sparseGP,
+      frac_inducing       = object@smooth_options$frac_inducing,
+      warping             = object@smooth_options$warping,
+      warping_freq        = as.integer(object@smooth_options$warping_freq),
+      warping_ref         = warping_ref-1, # 0-based python indexing
+      warping_open_begin  = object@smooth_options$warping_open_begin,
+      warping_open_end    = object@smooth_options$warping_open_end
+    )
+  }
+  
+  # Build the model
+  mofa_entrypoint$build()
+  
+  # Run the model
+  mofa_entrypoint$run()
+
+  # Interpolate
+  if (!is.null(object@covariates) & length(object@smooth_options)>1) {
+    if(!is.null(object@smooth_options$new_values)) {
+      new_values <- object@smooth_options$new_values
+      if(is.null(dim(new_values))){
+        new_values <- matrix(new_values, nrow = 1)
+      }
+      mofa_entrypoint$predict_factor(new_covariates = r_to_py(t(new_values)))
+    }
+  }
+  
+  # Save the model output as an hdf5 file
+  mofa_entrypoint$save(outfile, save_data = save_data)
+
+}
