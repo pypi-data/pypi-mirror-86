3fef9a0e8d040647d95cef6d0c66e0b0ccf9e298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:51 +0000

    Add package selenide/0.0.14/selenide-0.0.14-py2.py3-none-any.whl

---
+++ packages/selenide/selenide-0.0.14-py2.py3-none-any.whl/selenide/configuration.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+
+# global setting
+DRIVER: webdriver
+# enum: headless or usual
+MODE: str
+# large time judgment conditions
+TIMEOUT: int = 6
+# base url
+URL: str
+# desktop size
+WIDTH: int
+HEIGHT: int
