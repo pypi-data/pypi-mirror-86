cbdfb08db101b8c7814268813ee4a02778b2f2bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:24 +0000

    Add package sopel-plugins.pingpong/1.0.3/sopel_plugins.pingpong-1.0.3.tar.gz

---
+++ packages/sopel-plugins.pingpong/sopel_plugins.pingpong-1.0.3.tar.gz/sopel_plugins.pingpong-1.0.3/sopel_pingpong/pingpong/__init__.py
@@ -0,0 +1,13 @@
+"""
+ping.py - Sopel Ping Plugin.
+Author: Sean B. Palmer, inamidst.com
+About: http://sopel.chat
+"""
+
+from sopel.module import commands
+
+
+@commands('ping')
+def ping2(bot, trigger):
+    """Reply to ping command."""
+    bot.say('Pong!')
