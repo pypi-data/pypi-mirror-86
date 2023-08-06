7ccdc1931c23658be8d894ae92166aaa45f63a9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:53 +0000

    Add package testwizard.smart-tv/3.2.8/testwizard.smart-tv-3.2.8.tar.gz

---
+++ packages/testwizard.smart-tv/testwizard.smart-tv-3.2.8.tar.gz/testwizard.smart-tv-3.2.8/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="testwizard.smart-tv",
+    version="3.2.8",
+    author="Eurofins Digital Testing - Belgium",
+    author_email="support-be@eurofins.com",
+    description="Testwizard for Smart TV testobjects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['testwizard.smart_tv'],
+    install_requires=[
+        'testwizard.test==3.2.8',
+        'testwizard.testobjects-core==3.2.8',
+        'testwizard.commands-audio==3.2.8',
+        'testwizard.commands-mobile==3.2.8',
+        'testwizard.commands-powerswitch==3.2.8',
+        'testwizard.commands-remotecontrol==3.2.8',
+        'testwizard.commands-video==3.2.8',
+        'testwizard.commands-camera==3.2.8'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.3",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
+
