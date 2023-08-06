fa022727b60b7d16c96808f91781a16dc66164b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:32 +0000

    Add package tenantpy/1.0.4/tenantpy-1.0.4.tar.gz

---
+++ packages/tenantpy/tenantpy-1.0.4.tar.gz/tenantpy-1.0.4/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+
+def requirements():
+    reqs = []
+    with open("requirements.txt") as f:
+        for line in f.readlines():
+            reqs.append(line)
+    return reqs
+
+
+def description():
+    with open("README.md") as f:
+        return f.read()
+
+setup(
+    name="tenantpy",
+    version="1.0.4",
+    author="zhouhao",
+    python_requires='>=3.7.0',
+    author_email="zhouhao19931002@hotmail.com",
+    long_description=description(),
+    long_description_content_type="text/markdown",
+    maintainer="zhouhao",
+    description="Tenant Management For Web Application",
+    packages=find_packages(exclude=("tests",)),
+    install_requires=requirements(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3.7"
+    ]
+)
