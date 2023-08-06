2a68f8485de60466d0d4455f4cd770288d354ec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:38 +0000

    Add package bfit/4.4.1/bfit-4.4.1.tar.gz

---
+++ packages/bfit/bfit-4.4.1.tar.gz/bfit-4.4.1/bfit/backend/colors.py
@@ -0,0 +1,20 @@
+# color definitions
+foreground      = 'white'
+background      = 'black'
+fieldbackground = 'grey30'
+focusbackground = 'grey'
+
+selected        = 'green2'
+
+readonly        = 'grey10'
+disabled        = 'grey40'
+
+entry_grey      = 'grey45'
+entry_white     = 'grey92'
+
+tab             = 'grey'
+
+red             = 'red3'
+
+font            = 'TkFixedFont'
+# ~ font            = 'Courier 10 bold'
