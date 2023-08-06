0f80ebf535193116331aa3b7451a7c74a3d62a68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:37 +0000

    Add package b-elasticsearch-layer/0.0.3/b_elasticsearch_layer-0.0.3-py3-none-any.whl

---
+++ packages/b-elasticsearch-layer/b_elasticsearch_layer-0.0.3-py3-none-any.whl/b_elasticsearch_layer/layer.py
@@ -0,0 +1,54 @@
+from functools import lru_cache
+from typing import List
+
+from aws_cdk.aws_lambda import Code, LayerVersion, Runtime
+from aws_cdk.core import Stack, AssetHashType, BundlingOptions, BundlingDockerImage
+
+
+class Layer(LayerVersion):
+    def __init__(self, scope: Stack, name: str):
+        self.__scope = scope
+
+        super().__init__(
+            scope=scope,
+            id=name,
+            code=Code.from_asset(
+                self.get_source_path(),
+                asset_hash_type=AssetHashType.BUNDLE,
+                bundling=BundlingOptions(
+                    image=BundlingDockerImage.from_registry('python:3.9'),
+                    command=[
+                        'bash', '-c',
+                        (
+                            'pip install -r requirements.txt -t /asset-output/python && '
+                            'find /asset-output -type f -name "*.py[co]" -delete && '
+                            'find /asset-output -type d -name "__pycache__" -delete'
+                        )
+                    ]
+                )
+            ),
+            compatible_runtimes=self.runtimes(),
+            layer_version_name=name,
+        )
+
+    @lru_cache
+    def get_source_path(self) -> str:
+        """
+        Returns path to layer source.
+
+        :return: Path to layer source.
+        """
+        from .source import root
+        return root
+
+    def runtimes(self) -> List[Runtime]:
+        """
+        Available runtimes for lambda functions.
+
+        :return: List of available runtimes.
+        """
+        return [
+            Runtime.PYTHON_3_6,
+            Runtime.PYTHON_3_7,
+            Runtime.PYTHON_3_8
+        ]
