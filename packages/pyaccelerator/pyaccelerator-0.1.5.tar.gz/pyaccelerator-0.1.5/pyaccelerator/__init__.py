d526b1d76a6849b3901c5e91fa17d3c84a109cd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:03 +0000

    Add package pyaccelerator/0.1.5/pyaccelerator-0.1.5.tar.gz

---
+++ packages/pyaccelerator/pyaccelerator-0.1.5.tar.gz/pyaccelerator-0.1.5/pyaccelerator/__init__.py
@@ -0,0 +1,23 @@
+"""Python package to build simple toy accelerator.
+"""
+import logging
+
+from . import elements
+from .beam import Beam
+from .constraints import TargetDispersion, TargetGlobal, TargetPhasespace, TargetTwiss
+from .elements import *
+from .lattice import Lattice
+
+__all__ = [
+    "Beam",
+    "Lattice",
+    "TargetPhasespace",
+    "TargetTwiss",
+    "TargetDispersion",
+    "TargetGlobal",
+]
+__all__.extend(elements.__all__)
+
+__version__ = "0.1.5"
+
+logger = logging.getLogger("pyaccelerator")
