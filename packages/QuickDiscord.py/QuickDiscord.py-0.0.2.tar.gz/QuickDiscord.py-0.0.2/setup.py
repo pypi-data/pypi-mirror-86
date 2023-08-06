0b7efd9b85c30daf7db69668f701a800230a7d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:28 +0000

    Add package QuickDiscord.py/0.0.2/QuickDiscord.py-0.0.2.tar.gz

---
+++ packages/QuickDiscord.py/QuickDiscord.py-0.0.2.tar.gz/QuickDiscord.py-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import quickdiscord
+with open("README.md", "r") as stream:
+    long_description = stream.read()
+
+setup(
+    name = 'QuickDiscord.py',
+    version = quickdiscord.__version__,
+    license = 'MIT',
+    author = 'Team Wezacon',
+    author_email = 'wezacon.com@gmail.com',
+    description = 'The package that makes coding discord bots quicker!',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    keywords = [
+        'bots',
+        'discord-py',
+        'wezacon'
+    ],
+    install_requires = [
+        'discord.py',
+        'json',
+        'requests'
+    ],
+    setup_requires = [
+        'wheel'
+    ],
+    packages = find_packages()
+)
