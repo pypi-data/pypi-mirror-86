a91a96c0620cd9820fcbfcf7b272ddcb2bf1dbae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:09 +0000

    Add package beta-rec/0.2.3/beta_rec-0.2.3.tar.gz

---
+++ packages/beta-rec/beta_rec-0.2.3.tar.gz/beta_rec-0.2.3/setup.py
@@ -0,0 +1,39 @@
+import pathlib
+
+import pkg_resources
+from setuptools import find_packages, setup
+
+__version__ = "0.2.3"
+url = "https://github.com/beta-team/beta-recsys"
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+with pathlib.Path("requirements.txt").open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement in pkg_resources.parse_requirements(requirements_txt)
+    ]
+
+setup_requires = ["pytest-runner", "isort"]
+tests_require = ["pytest", "pytest-cov", "mock"]
+
+setup(
+    name="beta_rec",
+    version=__version__,
+    description="Beta-RecSys: Build, Evaluate and Tune Automated Recommender Systems",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author_email="recsys.beta@gmail.com",
+    url=url,
+    download_url="{}/archive/{}.tar.gz".format(url, __version__),
+    keywords=["pytorch", "recommender system", "recommendations"],
+    python_requires=">=3.7",
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    tests_require=tests_require,
+    packages=find_packages(),
+)
