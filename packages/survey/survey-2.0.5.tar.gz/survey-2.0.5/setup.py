d23d154be7056e93fda3b520e8f06a132c97e55b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:20 +0000

    Add package survey/2.0.5/survey-2.0.5.tar.gz

---
+++ packages/survey/survey-2.0.5.tar.gz/survey-2.0.5/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open('README.rst') as file:
+    readme = file.read()
+
+name = 'survey'
+
+version = '2.0.5'
+
+author = 'Exahilosys'
+
+url = f'https://github.com/{author}/{name}'
+
+setuptools.setup(
+    name = name,
+    version = version,
+    url = url,
+    packages = setuptools.find_packages(),
+    license = 'MIT',
+    description = 'A simple library for creating beautiful interactive prompts.',
+    long_description = readme,
+    install_requires = [
+        'wrapio>=0.3.5'
+    ],
+    extras_require = {
+        'docs': [
+            'sphinx',
+            'sphinx_rtd_theme'
+        ]
+    }
+)
