65fe05a95771439dddb755d6afe9c3ea7ed5e645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:57 +0000

    Add package Prue1/0.0.3/Prue1-0.0.3.tar.gz

---
+++ packages/Prue1/Prue1-0.0.3.tar.gz/Prue1-0.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup (
+  name='Prue1',
+  version='0.0.3',
+  description='Operaciones de 2 digitos',
+  #long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Wilbert Sanchez',
+  author_email='wilbertenriquechable522@gmail.com',
+  license='MIT', 
+ # classifiers=classifiers,
+  keywords='calculator', 
+  packages=find_packages(),
+  install_requires=[''] 
+  )
