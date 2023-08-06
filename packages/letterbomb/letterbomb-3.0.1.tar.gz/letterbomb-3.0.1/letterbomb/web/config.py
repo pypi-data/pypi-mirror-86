fd54a6c10193fffb2ae9e7e89f2300cfa1b67c3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:00 +0000

    Add package letterbomb/3.0.1/letterbomb-3.0.1.tar.gz

---
+++ packages/letterbomb/letterbomb-3.0.1.tar.gz/letterbomb-3.0.1/letterbomb/web/config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: GPL-3.0-or-later
+"""Flask configuration."""
+
+# Where to save logfile, can be an absolute or relative path
+# Leave blank to not generate a log file
+LOG_FILE: str = ""
+
+# Fill these in if you intend on having a Captcha.
+# Leave both of these blank if you do not want to include a captcha
+RECAPTCHA_PUBLICKEY: str = ""
+RECAPTCHA_PRIVATEKEY: str = ""
+
+# Don't enable unless you know what you are doing
+DEBUG: bool = False
