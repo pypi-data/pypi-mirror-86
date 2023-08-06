2c28a837f51cde5ed5c36ae60a4e1df00726e72c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:36 +0000

    Add package aiuniver/1.2.7/aiuniver-1.2.7.tar.gz

---
+++ packages/aiuniver/aiuniver-1.2.7.tar.gz/aiuniver-1.2.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='aiuniver',
+    version='1.2.7',
+    packages=['aiuniver'],
+    url='',
+    license='',
+    author='AI University',
+    author_email='nejronki@gmail.com'    
+)
