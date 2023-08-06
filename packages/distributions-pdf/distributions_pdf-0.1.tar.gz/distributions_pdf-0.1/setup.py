a44c7421000c063a5cd01e715114c60bdb7bfb4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:46 +0000

    Add package distributions-pdf/0.1/distributions_pdf-0.1.tar.gz

---
+++ packages/distributions-pdf/distributions_pdf-0.1.tar.gz/distributions_pdf-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_pdf',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_pdf'],
+      zip_safe=False)
