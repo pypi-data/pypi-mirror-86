1a36b6d26816e8a09da011eb0219c6ec9a6319a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:02 +0000

    Add package netbox-vcenter/0.1.1/netbox-vcenter-0.1.1.tar.gz

---
+++ packages/netbox-vcenter/netbox-vcenter-0.1.1.tar.gz/netbox-vcenter-0.1.1/netbox_vcenter/navigation.py
@@ -0,0 +1,8 @@
+# from extras.plugins import PluginMenuItem
+#
+# menu_items = (
+#     PluginMenuItem(
+#         link='plugins:netbox_vcenter:compare',
+#         link_text='Compare with vCenter',
+#     ),
+# )
