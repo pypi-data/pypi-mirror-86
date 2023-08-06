6aa92888a5d0ee622fe6b182bed6040bbf9cdcf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:16 +0000

    Add package pySystem/1.0.2/pySystem-1.0.2.tar.gz

---
+++ packages/pySystem/pySystem-1.0.2.tar.gz/pySystem-1.0.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+import pySystem
+setuptools.setup(
+    name="pySystem",
+    version=pySystem.__version__,
+    author="Joran Beasley",
+    author_email="joranbeasley@gmail.com",
+    url="https://github.com/joranbeasley/PySystem",
+    description="A simple tool to help interfacing with simple os calls",
+    packages=['pySystem'],
+    install_requires=['six'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+)
