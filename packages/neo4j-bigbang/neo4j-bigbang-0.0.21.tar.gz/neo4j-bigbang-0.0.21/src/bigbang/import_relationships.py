50403e60e5becce17a49eec21d07bcc610dc6361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:18 +0000

    Add package neo4j-bigbang/0.0.21/neo4j-bigbang-0.0.21.tar.gz

---
+++ packages/neo4j-bigbang/neo4j-bigbang-0.0.21.tar.gz/neo4j-bigbang-0.0.21/src/bigbang/import_relationships.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import sys
+import load_dot_env
+from usecases.import_relationships import ImportRelationships, ACTION_TYPE_SKIP, ACTION_TYPE_UPDATE
+
+DEFAULT_FILE_NAME = 'relationships.csv'
+def main(args = sys.argv):
+
+    action_type = ACTION_TYPE_SKIP
+    file_name = DEFAULT_FILE_NAME
+    type_in_row = True
+    create_node = False
+
+    # Update or Skip
+    if '-u' in args:
+        i = args.index('-u')
+        action_type = ACTION_TYPE_UPDATE
+        # del args[i]
+
+    # File Name Specification
+    if '-n' in args:
+        i = args.index('-n')
+        if len(args) <= i + 1:
+            raise AttributeError('file_name is required.')
+        if args[i + 1].startswith('-'):
+            raise AttributeError('file_name is required.')
+        file_name = args[i + 1]
+        if '-a' in args:
+            type_in_row = False
+
+    # Create Node if not exists
+    if '-c' in args:
+        i = args.index('-c')
+        create_node = True
+
+    try:
+        import_relationships = ImportRelationships(file_name, type_in_row, create_node)
+        import_relationships.invoke(action_type)
+        print('[Treated results]')
+        print('created: %s' % (import_relationships.treated["created"]))
+        print('updated: %s' % (import_relationships.treated["updated"]))
+        print('skipped: %s' % (import_relationships.treated["skipped"]))
+        print('failed: %s' % (import_relationships.treated["failed"]))
+
+    except Exception as e:
+        print(e)
+
+if __name__ == '__main__':
+    main()
