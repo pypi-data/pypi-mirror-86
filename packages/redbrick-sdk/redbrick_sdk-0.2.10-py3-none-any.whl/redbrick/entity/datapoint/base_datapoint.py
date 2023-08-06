b2110ec8bad4611d0712bcfde4c8218065da2629
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:16 +0000

    Add package redbrick-sdk/0.2.10/redbrick_sdk-0.2.10-py3-none-any.whl

---
+++ packages/redbrick-sdk/redbrick_sdk-0.2.10-py3-none-any.whl/redbrick/entity/datapoint/base_datapoint.py
@@ -0,0 +1,17 @@
+"""
+Base class for datapoints.
+"""
+from dataclasses import dataclass, field
+from typing import List, Optional, Union, Dict, Any
+import datetime
+
+
+@dataclass
+class BaseDatapoint:
+    """Base class representation of a datapoint."""
+
+    org_id: str
+    label_set_name: str
+    taxonomy: Dict[str, int]
+    task_type: str
+    remote_labels: List[Any]
