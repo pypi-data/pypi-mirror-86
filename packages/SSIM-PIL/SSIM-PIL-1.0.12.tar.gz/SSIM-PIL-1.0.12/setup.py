8b13fe1024b416aecbda507a3682e193a28fb48a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:39 +0000

    Add package SSIM-PIL/1.0.12/SSIM-PIL-1.0.12.tar.gz

---
+++ packages/SSIM-PIL/SSIM-PIL-1.0.12.tar.gz/SSIM-PIL-1.0.12/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+from distutils.core import setup
+from SSIM_PIL import __version__
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='SSIM-PIL',
+    version=__version__,
+    description=long_description.split('\n')[1],
+    author='ChsHub',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ChsHub/SSIM-PIL",
+    packages=['SSIM_PIL'],
+    license='MIT License',
+    classifiers=['Programming Language :: Python :: 3']
+)
+
+# C:\Python37\python.exe setup.py sdist bdist_wheel
+# C:\Python37\python.exe -m twine upload dist/*
+# https://www.lfd.uci.edu/~gohlke/pythonlibs/#pyopencl
