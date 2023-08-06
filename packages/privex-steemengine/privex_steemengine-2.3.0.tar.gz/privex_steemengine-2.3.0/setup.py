59ca21dcee32cabb1e8c07aed00017db430c7ed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:14 +0000

    Add package privex-steemengine/2.3.0/privex_steemengine-2.3.0.tar.gz

---
+++ packages/privex-steemengine/privex_steemengine-2.3.0.tar.gz/privex_steemengine-2.3.0/setup.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+"""
++===================================================+
+|                 © 2020 Privex Inc.                |
+|               https://www.privex.io               |
++===================================================+
+|                                                   |
+|        Python Steem Engine library                |
+|        License: X11/MIT                           |
+|                                                   |
+|        Core Developer(s):                         |
+|                                                   |
+|          (+)  Chris (@someguy123) [Privex]        |
+|                                                   |
++===================================================+
+
+Python SteemEngine - A small library for querying and interacting with the SteemEngine network (https://steem-engine.com)
+Copyright (c) 2020    Privex Inc. ( https://www.privex.io )
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
+files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of 
+the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS 
+OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name(s) of the above copyright holders shall not be used in advertising or 
+otherwise to promote the sale, use or other dealings in this Software without prior written authorization.
+"""
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='privex_steemengine',
+
+    version='2.3.0',
+
+    description='A small library for querying and interacting with the Steem/HiveEngine network (https://steem-engine.com / https://hive-engine.com)',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Privex/python-steemengine",
+    author='Chris (Someguy123) @ Privex',
+    author_email='chris@privex.io',
+
+    license='MIT',
+    install_requires=[
+        'privex-jsonrpc>=1.0.0',
+        'beem>=0.24.15',
+        'requests>=2.20.1',
+        'privex-helpers>=3.0.0'
+    ],
+    extras_require={
+        "dev": ["pytest", "pytest-cov", "pytest-asyncio", "coverage", "codecov", "jupyter"],
+    },
+    packages=find_packages(exclude=['tests', 'tests.*', 'test.*', 'privex.db', 'privex.db.*']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
