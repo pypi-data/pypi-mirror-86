f8ceff1498f5f039b8ba78e2d477ccfb9f5910e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:10 +0000

    Add package covid-tracking-project/0.2/covid-tracking-project-0.2.tar.gz

---
+++ packages/covid-tracking-project/covid-tracking-project-0.2.tar.gz/covid-tracking-project-0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name='covid-tracking-project',
+    version='0.2',
+    author='Aarav Borthakur',
+    author_email='gadhaguy13@gmail.com',
+    description='A python wrapper for the Covid Tracking Project API',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/gadhagod/covid-tracking-project',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'requests',
+    ],
+    python_requires='>=3.6'
+)
