1c92bfa5071050a42bb22d8cfe58901575fbf2a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:11 +0000

    Add package jalali/1.2/jalali-1.2.tar.gz

---
+++ packages/jalali/jalali-1.2.tar.gz/jalali-1.2/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='jalali',
+    packages=['jalali'],
+    version='1.2',
+    license='MIT',
+    description='This package helps developers to easily work with Jalali (Shamsi Or Iranian) dates in Python applications.',
+    author='seyed sadeq shobeiri',
+    author_email='ssshobeiry@gmail.com',
+    url='https://github.com/shobeiry/jalali',
+    download_url='https://github.com/shobeiry/jalali/archive/master.zip',
+    keywords=['Jalali', 'python', 'sssh', 'shobeiry', 'jalali date'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
