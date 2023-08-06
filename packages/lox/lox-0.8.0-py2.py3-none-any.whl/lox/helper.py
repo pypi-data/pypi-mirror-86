87313e5e9de1907e52a6c29a81b0373388188e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:56 +0000

    Add package lox/0.8.0/lox-0.8.0-py2.py3-none-any.whl

---
+++ packages/lox/lox-0.8.0-py2.py3-none-any.whl/lox/helper.py
@@ -0,0 +1,43 @@
+"""
+.. module:: helper
+   :synopsis: Private general lox helper functions/classes
+"""
+
+__all__ = ["term_colors", ]
+
+
+class term_colors:
+    """ Escape sequences to colorize text in the terminal """
+
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    ORANGE = '\033[93m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+    def __init__(self, color: str):
+        self.color = color.lower()
+
+    def __enter__(self, ):
+        if self.color == 'red':
+            print(self.RED, end='')
+        elif self.color == 'green':
+            print(self.GREEN, end='')
+        elif self.color == 'blue':
+            print(self.BLUE, end='')
+        elif self.color == 'orange' or self.color == 'yellow':
+            print(self.ORANGE, end='')
+        elif self.color == 'bold':
+            print(self.BOLD, end='')
+        elif self.color == 'underline':
+            print(self.UNDERLINE, end='')
+        elif self.color == 'header':
+            print(self.HEADER, end='')
+        else:
+            raise ValueError("Invalid color string")
+
+    def __exit__(self, type, value, traceback):
+        print(self.ENDC, end='', flush=True)
---
+++ packages/lox/lox-0.8.0-py2.py3-none-any.whl/lox/worker/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+"""Add concurrency to methods and functions in a single line of code."""
+
+import lox.worker.thread
+import lox.worker.process
