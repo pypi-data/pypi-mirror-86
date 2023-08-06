49f8e451a126ade12b0797954177a682ea8a2fe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:25 +0000

    Add package aws-simple-pipeline/0.0.1/aws_simple_pipeline-0.0.1.tar.gz

---
+++ packages/aws-simple-pipeline/aws_simple_pipeline-0.0.1.tar.gz/aws_simple_pipeline-0.0.1/aws_simple_pipeline_/__init__.py
@@ -0,0 +1,17 @@
+"""AWS simple pipeline
+
+This package contains the modules for deploying a simple AWS CDK pipeline on AWS.
+
+Package contents one class named PipelineStack that
+it extended the core.Stack class of aws_cdk package.
+
+    >>> import aws_simple_pipeline
+    >>> help(aws_simple_pipeline)
+    >>> import aws_simple_pipeline.pipeline_stack as PipelineStack
+    >>> help(PipelineStack)
+
+# license MIT
+# support https://github.com/bilardi/aws-simple-pipeline/issues
+"""
+__version__ = '0.0.1'
+__author__ = 'Alessandra Bilardi'
