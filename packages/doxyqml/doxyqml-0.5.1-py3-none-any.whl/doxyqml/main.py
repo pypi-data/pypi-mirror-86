14065590fe02ddefc1adffcc79009e69ba091819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:31 +0000

    Add package doxyqml/0.5.1/doxyqml-0.5.1-py3-none-any.whl

---
+++ packages/doxyqml/doxyqml-0.5.1-py3-none-any.whl/doxyqml/main.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+import os
+import re
+import sys
+
+import doxyqml.qmlparser as qmlparser
+
+from doxyqml import __version__, DESCRIPTION
+from doxyqml.lexer import Lexer, LexerError
+from doxyqml.qmlclass import QmlClass
+
+
+def coord_for_idx(text, idx):
+    head, sep, tail = text[:idx].rpartition("\n")
+    if sep == "\n":
+        row = head.count("\n") + 2
+    else:
+        row = 1
+    col = len(tail) + 1
+    return row, col
+
+
+def line_for_idx(text, idx):
+    bol = text.rfind("\n", 0, idx)
+    if bol == -1:
+        bol = 0
+    eol = text.find("\n", idx)
+    return text[bol:eol]
+
+
+def info_for_error_at(text, idx):
+    row, col = coord_for_idx(text, idx)
+    line = line_for_idx(text, idx)
+    msg = line + "\n" + "-" * (col - 1) + "^"
+    return row, msg
+
+
+def parse_args(argv):
+    parser = argparse.ArgumentParser(
+        prog="doxyqml",
+        description=DESCRIPTION,
+        )
+    parser.add_argument("-d", "--debug",
+                        action="store_true",
+                        help="Log debug info to stderr")
+    parser.add_argument("--namespace",
+                        action='append',
+                        default=[],
+                        help="Wrap the generated C++ classes in NAMESPACE")
+    parser.add_argument("--no-since-version",
+                        action="store_true",
+                        default=False,
+                        help="Don't append \"Since: [version]\" info to docstring")
+    parser.add_argument('--version',
+                        action='version',
+                        version='%%(prog)s %s' % __version__)
+    parser.add_argument("qml_file",
+                        help="The QML file to parse")
+
+    return parser.parse_args(argv)
+
+
+def find_qmldir_file(qml_file):
+    dir = os.path.dirname(qml_file)
+
+    while True:
+        # Check if `dir` contains a file of the name "qmldir".
+        name = os.path.join(dir, 'qmldir')
+
+        if os.path.isfile(name):
+            return name
+
+        # Pick parent of `dir`. Abort once parent stops changing,
+        # either because we reached the root directory, or because
+        # relative paths were used and we reached the currrent
+        # working directory.
+        parent = os.path.dirname(dir)
+
+        if parent == dir:
+            return None
+
+        dir = parent
+
+
+def find_classname(qml_file, namespace=None):
+    classname = os.path.basename(qml_file).split(".")[0]
+    classversion = None
+    modulename = ''
+
+    qmldir = find_qmldir_file(qml_file)
+
+    if qmldir:
+        text = open(qmldir).read()
+        match = re.match(r'^module\s+((?:\w|\.)+)\s*$', text, re.MULTILINE)
+
+        if match:
+            modulename = match.group(1)
+
+        basedir = os.path.dirname(qmldir)
+
+        rx_object_type = re.compile(r'^(\w+)\s+(\d+(?:\.\d+)*)\s+(\S+)\s*$', re.MULTILINE)
+
+        for name, version, path in rx_object_type.findall(text):
+            filename = os.path.join(basedir, path)
+
+            if os.path.isfile(filename) and os.path.samefile(qml_file, filename):
+                classversion = version
+                classname = name
+                break
+
+    if modulename:
+        classname = modulename + '.' + classname
+
+    if namespace:
+        classname = '.'.join(namespace) + '.' + classname
+
+    return classname, classversion
+
+
+def main(argv=None, out=None):
+    if argv is None:
+        argv = sys.argv[1:]
+    if out is None:
+        out == sys.stdout
+
+    args = parse_args(argv)
+
+    name = args.qml_file
+    namespace = args.namespace
+    text = open(name, encoding="utf-8").read()
+
+    lexer = Lexer(text)
+    try:
+        lexer.tokenize()
+    except LexerError as exc:
+        logging.error("Failed to tokenize %s" % name)
+        row, msg = info_for_error_at(text, exc.idx)
+        logging.error("Lexer error line %d: %s\n%s", row, exc, msg)
+        if args.debug:
+            raise
+        else:
+            return -1
+
+    if args.debug:
+        for token in lexer.tokens:
+            print("%20s %s" % (token.type, token.value))
+
+    classname, classversion = find_classname(name, namespace)
+    if args.no_since_version:
+        classversion = None
+
+    qml_class = QmlClass(classname, classversion)
+
+    try:
+        qmlparser.parse(lexer.tokens, qml_class)
+    except qmlparser.QmlParserError as exc:
+        logging.error("Failed to parse %s" % name)
+        row, msg = info_for_error_at(text, exc.token.idx)
+        logging.error("Lexer error line %d: %s\n%s", row, exc, msg)
+        if args.debug:
+            raise
+        else:
+            return -1
+
+    print(qml_class, file=out)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
+# vi: ts=4 sw=4 et
---
+++ packages/doxyqml/doxyqml-0.5.1-py3-none-any.whl/doxyqml/qmlclass.py
@@ -0,0 +1,340 @@
+import logging
+import re
+import typing
+
+TYPE_RX = r"(?P<prefix>\s+type:)(?P<type>[\w.<>|]+)"
+
+BASE_NAME_DICT = {
+    "QtObject": "QtQml.QtObject",
+    "Item": "QtQuick.Item",
+}
+
+
+def post_process_type(rx, text, type):
+    match = rx.search(text)
+    if match:
+        type = match.group("type")
+        text = text[:match.start("prefix")] + text[match.end("type"):]
+    return text, type
+
+
+class QmlBaseComponent():
+    def __init__(self, name):
+        self.name = name
+        self.base_name = ""
+        self.elements = []
+
+        lst = name.split(".")
+        self.class_name = lst[-1]
+        self.namespaces = lst[:-1]
+
+    def get_attributes(self):
+        return [x for x in self.elements if isinstance(x, QmlAttribute)]
+
+    def get_properties(self):
+        return [x for x in self.elements if isinstance(x, QmlProperty)]
+
+    def get_functions(self):
+        return [x for x in self.elements if isinstance(x, QmlFunction)]
+
+    def get_signals(self):
+        return [x for x in self.elements if isinstance(x, QmlSignal)]
+
+    def add_element(self, element):
+        self.elements.append(element)
+
+    def __str__(self):
+        lst = []
+        self._export_content(lst)
+        return "\n".join(lst)
+
+    def _export_elements(self, input_list, lst, filter=None):
+        for element in input_list:
+            if filter and not filter(element):
+                continue
+            doc = str(element)
+            if doc:
+                lst.append(doc)
+
+    def _start_class(self, lst):
+        class_decl = "class " + self.class_name
+        if self.base_name:
+            for alias, replacement in self.alias.items():
+                self.base_name = re.sub(alias, replacement, self.base_name)
+            self.base_name = BASE_NAME_DICT.get(self.base_name, self.base_name)
+
+            class_decl += " : public " + self.base_name
+
+        class_decl += " {"
+        lst.append(class_decl)
+        lst.append("public:")
+
+    def _end_class(self, lst):
+        lst.append("};")
+
+
+class QmlClass(QmlBaseComponent):
+    SINGLETON_COMMENT = "/** @remark This component is a singleton */"
+    VERSION_COMMENT = "/** @since %s */"
+
+    def __init__(self, name, version=None):
+        QmlBaseComponent.__init__(self, name)
+        self.header_comments = []
+        self.footer_comments = []
+        self.imports = []
+        self.alias = {}
+        if version:
+            self.header_comments.append(QmlClass.VERSION_COMMENT % version)
+
+    def add_pragma(self, decl):
+        args = decl.split(' ', 2)[1].strip()
+
+        if args.lower() == "singleton":
+            self.header_comments.append(QmlClass.SINGLETON_COMMENT)
+
+    def add_import(self, decl):
+        modules = decl.split()
+        module = modules[1]
+        if module[0] == '"':
+            # Ignore directory or javascript imports for now
+            return
+        if len(modules) > 3:
+            self.alias[modules[4]] = modules[1]
+        self.imports.append(module)
+
+    def add_header_comment(self, obj):
+        self.header_comments.append(obj)
+
+    def add_footer_comment(self, obj):
+        self.footer_comments.append(obj)
+
+    def _export_header(self, lst):
+        for module in self.imports:
+            lst.append("using namespace %s;" % module.replace('.', '::'))
+        if self.namespaces:
+            lst.append("namespace %s {" % '::'.join(self.namespaces))
+
+        lst.extend([str(x) for x in self.header_comments])
+
+    def _export_footer(self, lst):
+        lst.extend([str(x) for x in self.footer_comments])
+
+        if self.namespaces:
+            lst.append("}")
+
+    def _export_content(self, lst):
+        self._export_header(lst)
+
+        # Public members.
+        self._start_class(lst)
+
+        public_members = []
+        private_members = []
+
+        # Sort elements before exporting to reduce the number of times element list must
+        # be iterated through.
+        last_element_was_public = True
+        for element in self.elements:
+            if str(element) == "" or isinstance(element, str) :
+                if last_element_was_public:
+                    public_members.append(element)
+                else:
+                    private_members.append(element)
+            elif element.is_public_element():
+                public_members.append(element)
+                last_element_was_public = True
+            else:
+                private_members.append(element)
+                last_element_was_public = False
+
+        self._export_elements(public_members, lst)
+
+        if len(private_members) > 0:
+            lst.append("private:")
+            self._export_elements(private_members, lst)
+
+        self._end_class(lst)
+        self._export_footer(lst)
+
+    def is_public_element(self):
+        return True
+
+
+class QmlComponent(QmlBaseComponent):
+    """A component inside a QmlClass"""
+
+    def __init__(self, name):
+        QmlBaseComponent.__init__(self, name)
+        self.comment = None
+
+    def _export_content(self, lst):
+        component_id = self.get_component_id()
+        if component_id:
+            if self.comment:
+                lst.append(self.comment)
+
+            lst.append("%s %s;" % (self.class_name, component_id))
+
+        # Export child components with the top-level component. This avoids
+        # very deep nesting in the generated documentation.
+        self._export_elements(self.elements, lst, filter=lambda x:
+                              isinstance(x, QmlComponent))
+
+    def get_component_id(self):
+        # Returns the id of the component, if it has one
+        for attr in self.get_attributes():
+            if attr.name == "id":
+                return attr.value
+        return None
+
+    def is_public_element(self):
+        return False
+
+
+class QmlArgument(object):
+    def __init__(self, name):
+        self.type = ""
+        self.name = name
+        self.default_value = None
+
+    def __str__(self):
+        if self.type == "":
+            return self.name + self.default_value_string()
+        else:
+            return self.type + " " + self.name + self.default_value_string()
+
+    def default_value_string(self):
+        if self.default_value is None:
+            return ''
+        else:
+            return ' = {}'.format(self.default_value)
+
+    def is_public_element(self):
+        return True
+
+
+class QmlAttribute(object):
+    def __init__(self):
+        self.name = ""
+        self.value = ""
+        self.type = "var"
+        self.doc = ""
+
+    def __str__(self):
+        if self.name != "id":
+            lst = []
+            if len(self.doc) > 0:
+                lst.append(self.doc)
+            lst.append(self.type + " " + self.name + ";")
+            return "\n".join(lst)
+        else:
+            return ""
+
+    def is_public_element(self):
+        return False
+
+
+class QmlProperty(object):
+    type_rx = re.compile(TYPE_RX)
+
+    DEFAULT_PROPERTY_COMMENT = "/** @remark This is the default property */"
+    READONLY_PROPERTY_COMMENT = "/** @remark This property is read-only */"
+
+    def __init__(self):
+        self.type = ""
+        self.is_default = False
+        self.is_readonly = False
+        self.name = ""
+        self.doc = ""
+        self.doc_is_inline = False
+
+    def __str__(self):
+        self.post_process_doc()
+        lst = []
+        if not self.doc_is_inline:
+            lst.append(self.doc + "\n")
+        if self.is_default:
+            lst.append(self.DEFAULT_PROPERTY_COMMENT + "\n")
+        elif self.is_readonly:
+            lst.append(self.READONLY_PROPERTY_COMMENT + "\n")
+        lst.append("Q_PROPERTY(%s %s READ dummyGetter_%s_ignore)"
+            % (self.type, self.name, self.name))
+        if self.doc_is_inline:
+            lst.append(" " + self.doc)
+        return "".join(lst)
+
+    def post_process_doc(self):
+        self.doc, self.type = post_process_type(self.type_rx, self.doc, self.type)
+
+    def is_public_element(self):
+        # Doxygen always adds Q_PROPERTY items as public members.
+        return True
+
+
+class QmlFunction(object):
+    doc_arg_rx = re.compile(r"[@\\]param" + TYPE_RX + r"\s+(?P<name>\w+)")
+    return_rx = re.compile(r"[@\\]returns?" + TYPE_RX)
+
+    def __init__(self):
+        self.type = "void"
+        self.name = ""
+        self.doc = ""
+        self.doc_is_inline = False
+        self.args = []
+
+    def __str__(self):
+        self.post_process_doc()
+        arg_string = ", ".join([str(x) for x in self.args])
+        lst = []
+        if not self.doc_is_inline:
+            lst.append(self.doc + "\n")
+        lst.append("%s %s(%s);" % (self.type, self.name, arg_string))
+        if self.doc_is_inline:
+            lst.append(" " + self.doc)
+        return "".join(lst)
+
+    def post_process_doc(self):
+        def repl(match):
+            # For each argument with a specified type, update arg.type and return a typeless @param line
+            type = match.group("type")
+            name = match.group("name")
+            for arg in self.args:
+                if arg.name == name:
+                    arg.type = type
+                    break
+            else:
+                logging.warning("In function %s(): Unknown argument %s" % (self.name, name))
+            return "@param %s" % name
+
+        self.doc = self.doc_arg_rx.sub(repl, self.doc)
+        self.doc, self.type = post_process_type(self.return_rx, self.doc, self.type)
+
+    def is_public_element(self):
+        return True
+
+
+class QmlSignal(object):
+    def __init__(self):
+        self.name = ""
+        self.doc = ""
+        self.doc_is_inline = False
+        self.args = []
+
+    def __str__(self):
+        arg_string = ", ".join([str(x) for x in self.args])
+        lst = []
+        if not self.doc_is_inline:
+            lst.append(self.doc + "\n")
+        lst.append("Q_SIGNALS: void %s(%s); " % (self.name, arg_string))
+        if self.doc_is_inline:
+            lst.append(self.doc + "\n")
+        # Appending "public:" here makes it possible to declare a signal without
+        # turning all functions defined after into signals.
+        # It could be replaced with the use of Q_SIGNAL, but my version of
+        # Doxygen (1.8.4) does not support it
+        lst.append("public:")
+        return "".join(lst)
+
+    def is_public_element(self):
+        # Doxygen always adds Q_SIGNALS items as public members.
+        return True
