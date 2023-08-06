4b1e03cdd3472c99e610270957c94661b17c8991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:16 +0000

    Add package pyOneFichierClient/1.0.4/pyOneFichierClient-1.0.4.tar.gz

---
+++ packages/pyOneFichierClient/pyOneFichierClient-1.0.4.tar.gz/pyOneFichierClient-1.0.4/pyOneFichierClient/setToken/__main__.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+path = os.path.join(os.path.dirname(os.path.dirname(__file__)),'1F.t')
+
+if len(sys.argv) == 2:
+	the_token = sys.argv[1]
+else:
+	try:
+		the_token = input('Token: ')
+	except KeyboardInterrupt:
+		try:
+			the_token = input('\nToken, last chance: ')
+		except KeyboardInterrupt:
+			print()
+			sys.exit(1)
+		
+		
+
+with open(path,'w') as f:
+	f.write(the_token)
+	
+print('\nYAY! Your token has now been stored under the following path:')
+print(path)
