dc77bc103ae0962bf667358e316c4e225966f5fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:03 +0000

    Add package dkbirdisland/0.1.0.dev5/dkbirdisland-0.1.0.dev5.tar.gz

---
+++ packages/dkbirdisland/dkbirdisland-0.1.0.dev5.tar.gz/dkbirdisland-0.1.0.dev5/dkbirdisland/src/tools.py
@@ -0,0 +1,10 @@
+import pygame
+import os
+
+SRC_PATH = os.path.split(os.path.abspath(__file__))[0]
+IMAGES_PATH = os.path.join(SRC_PATH, '../images')
+
+def load_img(name):
+    path = ps.path.join(IMAGES_PATH, name)
+    return pygame.image.load(path)
+
