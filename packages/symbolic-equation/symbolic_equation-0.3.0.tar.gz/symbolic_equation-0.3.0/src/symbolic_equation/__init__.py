ceed6b46c42690c87879c08900d06ec5631ce535
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:03 +0000

    Add package symbolic-equation/0.3.0/symbolic_equation-0.3.0.tar.gz

---
+++ packages/symbolic-equation/symbolic_equation-0.3.0.tar.gz/symbolic_equation-0.3.0/src/symbolic_equation/__init__.py
@@ -0,0 +1,494 @@
+"""Package providing the :class:`Eq` class for symbolic equations."""
+from uniseg.graphemecluster import grapheme_clusters
+
+
+__version__ = '0.3.0'
+
+
+__all__ = ['Eq']
+
+
+def _grapheme_len(text):
+    """Number of graphemes in `text`
+
+    This is the length of the `text` when printed::
+        >>> s = 'Â'
+        >>> len(s)
+        2
+        >>> _grapheme_len(s)
+        1
+    """
+    return len(list(grapheme_clusters(text)))
+
+
+def _ljust(text, width, fillchar=' '):
+    """Left-justify text to a total of `width`
+
+    The `width` is based on graphemes::
+
+        >>> s = 'Â'
+        >>> s.ljust(2)
+        'Â'
+        >>> _ljust(s, 2)
+        'Â '
+    """
+    len_text = _grapheme_len(text)
+    return text + fillchar * (width - len_text)
+
+
+def _rjust(text, width, fillchar=' '):
+    """Right-justify text for a total of `width` graphemes
+
+    The `width` is based on graphemes::
+
+        >>> s = 'Â'
+        >>> s.rjust(2)
+        'Â'
+        >>> _rjust(s, 2)
+        ' Â'
+    """
+    len_text = _grapheme_len(text)
+    return fillchar * (width - len_text) + text
+
+
+class Eq:
+    """Symbolic equation.
+
+    This class keeps track of the :attr:`lhs` and :attr:`rhs` of an equation
+    across arbitrary manipulations.
+
+    Args:
+        lhs: the left-hand-side of the equation
+        rhs: the right-hand-side of the equation. If None, defaults to zero.
+        tag: a tag (equation number) to be shown when printing
+             the equation
+        eq_sym_str: If given, a value that overrides the `eq_sym_str` class
+            attribute for this particular instance.
+        eq_sym_tex: If given, a value that overrides the `eq_sym_tex` class
+            attribute for this particular instance.
+
+    Class Attributes:
+        latex_renderer: If not None, a callable that must return a LaTeX
+            representation (:class:`str`) of `lhs` and `rhs`. When overriding
+            this, wrap the function with `staticmethod`.
+        eq_sym_str: default representation of the "equal" when rendering the
+            equation as a str
+        eq_sym_tex: default representation of the "equal" when rendering the
+            equation in latex
+    """
+
+    latex_renderer = None
+    eq_sym_str = "="
+    eq_sym_tex = "="
+
+    def __init__(
+        self,
+        lhs,
+        rhs=None,
+        tag=None,
+        eq_sym_str=None,
+        eq_sym_tex=None,
+        _prev_lhs=None,
+        _prev_rhs=None,
+        _prev_tags=None,
+    ):
+        if rhs is None:
+            try:
+                import sympy
+
+                rhs = sympy.sympify(0)
+            except ImportError:
+                rhs = 0
+        self._lhs = lhs
+        self._prev_lhs = _prev_lhs or []
+        self._prev_rhs = _prev_rhs or []
+        self._prev_tags = _prev_tags or []
+        self._rhs = rhs
+        try:
+            self._tag = int(tag)
+        except (ValueError, TypeError):
+            self._tag = tag
+        if eq_sym_str is not None:
+            self.eq_sym_str = eq_sym_str
+        if eq_sym_tex is not None:
+            self.eq_sym_tex = eq_sym_tex
+
+    @property
+    def lhs(self):
+        """The left-hand-side of the equation."""
+        lhs = self._lhs
+        i = 0
+        while lhs is None:
+            i -= 1
+            lhs = self._prev_lhs[i]
+        return lhs
+
+    @property
+    def rhs(self):
+        """The right-hand-side of the equation."""
+        return self._rhs
+
+    def tag(self, tag):
+        """Set the tag for the last line in the equation."""
+        return self.__class__(
+            self._lhs,
+            self._rhs,
+            tag=tag,
+            eq_sym_str=self.__dict__.get('eq_sym_str', None),
+            eq_sym_tex=self.__dict__.get('eq_sym_tex', None),
+            _prev_lhs=self._prev_lhs,
+            _prev_rhs=self._prev_rhs,
+            _prev_tags=self._prev_tags,
+        )
+
+    @property
+    def as_dict(self):
+        """Mapping of the lhs to the rhs.
+
+        This allows to plug an equation into another expression.
+        """
+        return {self.lhs: self.rhs}
+
+    def apply(self, func_or_mtd, *args, **kwargs):
+        """Apply `func_or_mtd` to both sides of the equation.
+
+        Returns a new equation where the left-hand-side and right-hand side
+        are replaced by the application of `func_or_mtd`, depending on its
+        type.
+
+        * If `func_or_mtd` is a string, it must be the name of a method `mtd`,
+          and equation is modified as
+
+          ::
+
+              lhs=lhs.mtd(*args, **kwargs)
+              rhs=rhs.mtd(*args, **kwargs)
+
+        * If `func_or_mtd` is a callable `func`, the equation is modified as
+
+          ::
+
+              lhs=func(lhs, *args, **kwargs)
+              rhs=func(rhs, *args, **kwargs)
+        """
+        if isinstance(func_or_mtd, str):
+            new_lhs = getattr(self.lhs, func_or_mtd)(*args, **kwargs)
+            new_rhs = getattr(self.rhs, func_or_mtd)(*args, **kwargs)
+        else:
+            new_lhs = func_or_mtd(self.lhs, *args, **kwargs)
+            new_rhs = func_or_mtd(self.rhs, *args, **kwargs)
+        if new_lhs == self.lhs:
+            new_lhs = None
+        return self._append(new_lhs, new_rhs)
+
+    def transform(self, func, *args, **kwargs):
+        """Apply `func` to the entire equation.
+
+        The lhs and the rhs of the equation is replaced with the lhs and rhs of
+        the equation returned by ``func(self, *args, **kwargs)``.
+        """
+        new_eq = func(self, *args, **kwargs)
+        new_lhs = new_eq.lhs
+        new_rhs = new_eq.rhs
+        if new_lhs == self.lhs:
+            new_lhs = None
+        return self._append(new_lhs, new_rhs)
+
+    def apply_to_lhs(self, func_or_mtd, *args, **kwargs):
+        """Apply `func_or_mtd` to the :attr:`lhs` of the equation only.
+
+        Like :meth:`apply`, but modifying only the left-hand-side.
+        """
+        if isinstance(func_or_mtd, str):
+            new_lhs = getattr(self.lhs, func_or_mtd)(*args, **kwargs)
+        else:
+            new_lhs = func_or_mtd(self.lhs, *args, **kwargs)
+        return self._append(new_lhs, self.rhs)
+
+    def apply_to_rhs(self, func_or_mtd, *args, **kwargs):
+        """Apply `func_or_mtd` to the :attr:`rhs` of the equation only.
+
+        Like :meth:`apply`, but modifying only the right-hand-side.
+        """
+        new_lhs = None
+        if isinstance(func_or_mtd, str):
+            new_rhs = getattr(self.rhs, func_or_mtd)(*args, **kwargs)
+        else:
+            new_rhs = func_or_mtd(self.rhs, *args, **kwargs)
+        return self._append(new_lhs, new_rhs)
+
+    def _append(self, new_lhs, new_rhs):
+        new_prev_lhs = self._prev_lhs.copy()
+        new_prev_lhs.append(self._lhs)
+        new_prev_rhs = self._prev_rhs.copy()
+        new_prev_rhs.append(self.rhs)
+        new_prev_tags = self._prev_tags.copy()
+        new_prev_tags.append(self._tag)
+        return self.__class__(
+            new_lhs,
+            new_rhs,
+            eq_sym_str=self.__dict__.get('eq_sym_str', None),
+            eq_sym_tex=self.__dict__.get('eq_sym_tex', None),
+            _prev_lhs=new_prev_lhs,
+            _prev_rhs=new_prev_rhs,
+            _prev_tags=new_prev_tags,
+        )
+
+    def amend(self, previous_lines=1):
+        """Amend the previous lhs and rhs with the current ones.
+
+        If `previous_lines` is greater than 1, overwrite the corresponding
+        number of previous lines.
+
+        This can be chained to e.g. an :meth:`apply` call to group multiple
+        steps so that they don't show up a separate lines in the output.
+        """
+        if previous_lines <= 0:
+            raise ValueError(
+                "Invalid previous_lines=%r, must be >= 1" % previous_lines
+            )
+        new_prev_lhs = self._prev_lhs.copy()[:-previous_lines]
+        new_prev_rhs = self._prev_rhs.copy()[:-previous_lines]
+        new_prev_tags = self._prev_tags.copy()[:-previous_lines]
+        return self.__class__(
+            self._lhs,
+            self.rhs,
+            tag=self._tag,
+            eq_sym_str=self.__dict__.get('eq_sym_str', None),
+            eq_sym_tex=self.__dict__.get('eq_sym_tex', None),
+            _prev_lhs=new_prev_lhs,
+            _prev_rhs=new_prev_rhs,
+            _prev_tags=new_prev_tags,
+        )
+
+    def reset(self):
+        """Discard the equation history."""
+        return self.__class__(
+            self.lhs,
+            self.rhs,
+            tag=self._tag,
+            eq_sym_str=self.__dict__.get('eq_sym_str', None),
+            eq_sym_tex=self.__dict__.get('eq_sym_tex', None),
+        )
+
+    def copy(self):
+        """Return a copy of the equation, including its history."""
+        return self.__class__(
+            self._lhs,
+            self._rhs,
+            tag=self._tag,
+            eq_sym_str=self.__dict__.get('eq_sym_str', None),
+            eq_sym_tex=self.__dict__.get('eq_sym_tex', None),
+            _prev_lhs=self._prev_lhs,
+            _prev_rhs=self._prev_rhs,
+            _prev_tags=self._prev_tags,
+        )
+
+    def __add__(self, other):
+        """Add another equation, or a constant."""
+        try:
+            return self.__class__(
+                lhs=(self.lhs + other.lhs),
+                rhs=(self.rhs + other.rhs),
+            )
+            # we ignore instance eq_sym_str/eq_sym_tex because we don't know
+            # which equation should take precedence
+        except AttributeError:
+            return self.__class__(
+                lhs=(self.lhs + other),
+                rhs=(self.rhs + other),
+                eq_sym_str=self.__dict__.get('eq_sym_str', None),
+                eq_sym_tex=self.__dict__.get('eq_sym_tex', None),
+            )
+
+    __radd__ = __add__
+
+    def __sub__(self, other):
+        try:
+            return self.__class__(
+                lhs=(self.lhs - other.lhs), rhs=(self.rhs - other.rhs)
+            )
+            # we ignore instance eq_sym_str/eq_sym_tex because we don't know
+            # which equation should take precedence
+        except AttributeError:
+            return self.__class__(
+                lhs=(self.lhs - other),
+                rhs=(self.rhs - other),
+                eq_sym_str=self.__dict__.get('eq_sym_str', None),
+                eq_sym_tex=self.__dict__.get('eq_sym_tex', None),
+            )
+
+    def __rsub__(self, other):
+        # we don't have to consier the case of `other` being an `Eq`, because
+        # that would be handled by `__sub__`.
+        return self.__class__(
+            lhs=(other - self.lhs),
+            rhs=(other - self.rhs),
+            eq_sym_str=self.__dict__.get('eq_sym_str', None),
+            eq_sym_tex=self.__dict__.get('eq_sym_tex', None),
+        )
+
+    def __mul__(self, other):
+        return self.__class__(
+            lhs=(self.lhs * other),
+            rhs=(self.rhs * other),
+            eq_sym_str=self.__dict__.get('eq_sym_str', None),
+            eq_sym_tex=self.__dict__.get('eq_sym_tex', None),
+        )
+
+    def __rmul__(self, other):
+        return self.__class__(
+            lhs=(other * self.lhs),
+            rhs=(other * self.rhs),
+            eq_sym_str=self.__dict__.get('eq_sym_str', None),
+            eq_sym_tex=self.__dict__.get('eq_sym_tex', None),
+        )
+
+    def __truediv__(self, other):
+        return self.__class__(
+            lhs=(self.lhs / other),
+            rhs=(self.rhs / other),
+            eq_sym_str=self.__dict__.get('eq_sym_str', None),
+            eq_sym_tex=self.__dict__.get('eq_sym_tex', None),
+        )
+
+    def __eq__(self, other):
+        """Compare to another equation, or a constant.
+
+        This does not take into account any mathematical knowledge, it merely
+        checks if the :attr:`lhs` and :attr:`rhs` are exactly equal. If
+        comparing against a constant, the :attr:`rhs` must be exactly equal to
+        that constant.
+        """
+        try:
+            return self.lhs == other.lhs and self.rhs == other.rhs
+        except AttributeError:
+            return self.rhs == other
+
+    def _render_str(self, renderer, *args, **kwargs):
+        rendered_lhs = []
+        rendered_rhs = []
+        rendered_tags = []
+
+        for i, rhs in enumerate(self._prev_rhs):
+            lhs = self._prev_lhs[i]
+            tag = self._prev_tags[i]
+            if lhs is None:
+                rendered_lhs.append('')
+            else:
+                rendered_lhs.append(renderer(lhs, *args, **kwargs))
+            rendered_rhs.append(renderer(rhs, *args, **kwargs))
+            if tag is None:
+                rendered_tags.append('')
+            else:
+                rendered_tags.append(renderer(tag, *args, **kwargs))
+        if self._lhs is None:
+            rendered_lhs.append('')
+        else:
+            rendered_lhs.append(renderer(self._lhs, *args, **kwargs))
+        rendered_rhs.append(renderer(self._rhs, *args, **kwargs))
+        if self._tag is None:
+            rendered_tags.append('')
+        else:
+            rendered_tags.append(renderer(self._tag, *args, **kwargs))
+        len_lhs = max([_grapheme_len(s) for s in rendered_lhs])
+        len_rhs = max([_grapheme_len(s) for s in rendered_rhs])
+        len_tag = max([_grapheme_len(s) for s in rendered_tags]) + 2
+
+        lines = []
+        for (lhs, rhs, tag) in zip(rendered_lhs, rendered_rhs, rendered_tags):
+            if len(tag) > 0:
+                tag = "(" + tag + ")"
+            lhs = _rjust(lhs, len_lhs)
+            rhs = _ljust(rhs, len_rhs)
+            tag = _ljust(tag, len_tag)
+            lines.append(
+                (
+                    lhs + " " + self.eq_sym_str + " " + rhs + "    " + tag
+                ).rstrip()
+            )
+        return "\n".join(lines)
+
+    def __str__(self):
+        return self._render_str(renderer=str)
+
+    def __repr__(self):
+        return self._render_str(renderer=repr)
+
+    def _latex_render_expr(self, expr):
+        if self.latex_renderer is not None:
+            return self.latex_renderer(expr)
+        else:
+            try:
+                return expr._latex()
+            except AttributeError:
+                try:
+                    import sympy
+
+                    return sympy.latex(expr)
+                except ImportError:
+                    raise ValueError("No latex_renderer available")
+
+    def _repr_latex_(self):
+        """LaTeX representation for Jupyter notebook."""
+        has_history = len(self._prev_rhs) > 0
+        if has_history:
+            res = r'\begin{align}' + "\n"
+            res += "  %s &= %s" % (
+                self._latex_render_expr(self._prev_lhs[0]),
+                self._latex_render_expr(self._prev_rhs[0]),
+            )
+            if self._prev_tags[0] is not None:
+                res += r'\tag{%s}' % self._prev_tags[0]
+            res += "\\\\\n"
+            for i, rhs in enumerate(self._prev_rhs[1:]):
+                lhs = self._prev_lhs[i + 1]
+                if lhs is None:
+                    res += "   &%s %s" % (
+                        self.eq_sym_tex,
+                        self._latex_render_expr(rhs),
+                    )
+                else:
+                    res += "  %s &%s %s" % (
+                        self._latex_render_expr(lhs),
+                        self.eq_sym_tex,
+                        self._latex_render_expr(rhs),
+                    )
+                if self._prev_tags[i + 1] is not None:
+                    res += r'\tag{%s}' % self._prev_tags[i + 1]
+                res += "\\\\\n"
+            lhs = self._lhs
+            if lhs is None:
+                res += "   &%s %s\n" % (
+                    self.eq_sym_tex,
+                    self._latex_render_expr(self.rhs),
+                )
+            else:
+                res += "  %s &%s %s\n" % (
+                    self._latex_render_expr(lhs),
+                    self.eq_sym_tex,
+                    self._latex_render_expr(self.rhs),
+                )
+            if self._tag is not None:
+                res += r'\tag{%s}' % self._tag
+            res += r'\end{align}' + "\n"
+        else:
+            res = r'\begin{equation}' + "\n"
+            res += "  %s %s %s\n" % (
+                self._latex_render_expr(self.lhs),
+                self.eq_sym_tex,
+                self._latex_render_expr(self.rhs),
+            )
+            try:
+                if self._tag is not None:
+                    res += r'\tag{%s}' % self._tag
+            except AttributeError:
+                pass
+            res += r'\end{equation}' + "\n"
+        return res
+
+    def _sympy_(self):
+        """Convert to a :class:`sympy.Eq`."""
+        from sympy import Eq as SympyEq
+
+        return SympyEq(self.lhs, self.rhs)
---
+++ packages/symbolic-equation/symbolic_equation-0.3.0.tar.gz/symbolic_equation-0.3.0/tests/test_symbolic_equation.py
@@ -0,0 +1,224 @@
+"""Tests for `symbolic_equation` package."""
+
+import pytest
+import sympy
+from pkg_resources import parse_version
+from sympy import symbols, sympify
+
+import symbolic_equation
+from symbolic_equation import Eq
+
+
+def test_valid_version():
+    """Check that the package defines a valid __version__"""
+    assert parse_version(symbolic_equation.__version__) >= parse_version("0.2")
+
+
+@pytest.fixture
+def eq1_eq2():
+    """Two exemplary equations"""
+    x, y = symbols('x y')
+    eq1 = Eq(2 * x - y, sympify(1), tag='I')
+    eq2 = Eq(x + y, sympify(5), tag='II')
+    return eq1, eq2
+
+
+def test_apply_with_same_lhs(eq1_eq2):
+    """Test that "apply" that does not change the lhs will not double-print the
+    lhs."""
+    eq1, _ = eq1_eq2
+    eq = eq1.apply(sympy.simplify)
+    assert str(eq) == '2*x - y = 1    (I)\n        = 1'
+
+
+def test_apply_mtd_with_same_lhs(eq1_eq2):
+    """Test that "apply" of method that does not change the lhs will not
+    double-print the lhs."""
+    eq1, _ = eq1_eq2
+    eq = eq1.apply('simplify')
+    assert str(eq) == '2*x - y = 1    (I)\n        = 1'
+
+
+def test_apply_to_lhs_print_unchanged(eq1_eq2):
+    """Test that "apply_to_lhs" always prints the lhs, even if it did not
+    change"""
+    eq1, _ = eq1_eq2
+    eq = eq1.apply_to_lhs(sympy.simplify)
+    assert str(eq) == '2*x - y = 1    (I)\n2*x - y = 1'
+
+
+def test_add_equations(eq1_eq2):
+    """Test adding two equations"""
+    eq1, eq2 = eq1_eq2
+    eq = eq1 + eq2
+    assert eq.lhs == 3 * symbols('x')
+    assert eq.rhs == 6
+
+
+def test_add_const(eq1_eq2):
+    """Test adding a constant to an equation"""
+    eq1, _ = eq1_eq2
+    eq = eq1 + 1
+    assert eq.lhs == eq1.lhs + 1
+    assert eq.rhs == eq1.rhs + 1
+    assert eq1 + 1 == 1 + eq1
+
+
+def test_subtract_equations(eq1_eq2):
+    """Test adding two equations"""
+    eq1, eq2 = eq1_eq2
+    eq = eq1 - eq2
+    x, y = symbols('x y')
+    assert eq.lhs == x - 2 * y
+    assert eq.rhs == -4
+
+
+def test_subtract_const(eq1_eq2):
+    """Test subtracing a constant to an equation"""
+    eq1, _ = eq1_eq2
+    eq = eq1 - 1
+    assert eq.lhs == eq1.lhs - 1
+    assert eq.rhs == eq1.rhs - 1
+    assert eq == -1 * (1 - eq1)
+
+
+def test_mul(eq1_eq2):
+    """Test multiplication of equation with constant"""
+    eq1, _ = eq1_eq2
+    assert 2 * eq1 == eq1 * 2
+
+
+def test_equality(eq1_eq2):
+    """Test equality of equations with other equations and constants"""
+    eq1, eq2 = eq1_eq2
+    assert eq1 == eq1
+    assert eq1 != eq2
+    assert eq1 == 1
+    assert eq1 != 0
+
+
+def test_copy_preserves_history(eq1_eq2):
+    """Test that copying preserves the history"""
+    eq1, eq2 = eq1_eq2
+    x = symbols('x')
+    eq = (
+        (eq1 - eq2)
+        .apply(lambda v: v - ((eq1 - eq2).rhs))
+        .apply('subs', {x: 1})
+    )
+    assert str(eq) == '    x - 2*y = -4\nx - 2*y + 4 = 0\n    5 - 2*y = 0'
+    assert str(eq.copy()) == str(eq)
+
+
+def test_amend(eq1_eq2):
+    """Test amending previous lines"""
+    eq1, eq2 = eq1_eq2
+    x = symbols('x')
+    z = symbols('z')
+    eq_y = (
+        (eq1 - 2 * eq2)
+        .tag("I - 2 II")
+        .apply(lambda v: v - 9)
+        .apply(lambda v: v / (-3))
+    )
+    eq_x = eq1.apply_to_lhs('subs', eq_y.as_dict).reset().tag(r'y in I')
+
+    # fmt: off
+    eq_x_sol = (
+        eq_x
+        .apply(lambda v: v + 3)
+        .apply(lambda v: v / 2).amend().tag('x')
+    )
+    # fmt: on
+    assert eq_x_sol.lhs == x
+    assert eq_x_sol.rhs == 2
+    assert str(eq_x_sol) == '2*x - 3 = 1    (y in I)\n      x = 2    (x)'
+
+    # fmt: off
+    eq_z = (
+        eq_x
+        .apply_to_lhs('subs', {x: z + 1})
+        .apply_to_lhs('subs', {z: 1}).amend()
+    )
+    # fmt: on
+    assert eq_z.lhs == eq_z.rhs == 1
+    assert str(eq_z) == '2*x - 3 = 1    (y in I)\n      1 = 1'
+
+    # fmt: off
+    eq_z = (
+        Eq(eq_x.rhs, eq_x.lhs)
+        .apply_to_rhs('subs', {x: z + 1})
+        .apply_to_rhs('subs', {z: 1}).amend()
+    )
+    # fmt: on
+    assert eq_z.lhs == eq_z.rhs == 1
+    assert str(eq_z) == '1 = 2*x - 3\n  = 1'
+
+
+def test_reset_idempotence(eq1_eq2):
+    """Test that 'reset' on a single-line equation preserves the equation."""
+    eq1, _ = eq1_eq2
+    assert eq1.reset() == eq1
+    assert eq1.reset()._tag == eq1._tag
+
+
+def test_amend_idempotence(eq1_eq2):
+    """Test that 'amend' on a single-line equation preserves the equation."""
+    eq1, _ = eq1_eq2
+    assert eq1.amend() == eq1
+    assert eq1.amend()._tag == eq1._tag
+
+
+def test_custom_eq_sym():
+    """Test setting the eq_sym."""
+    x, y = symbols('x y')
+
+    assert str(Eq(x, y)) == "x = y"
+    tex = Eq(x, y)._repr_latex_()
+    assert tex == '\\begin{equation}\n  x = y\n\\end{equation}\n'
+
+    # class attribute
+    eq = Eq(x, y)
+    Eq.eq_sym_str = "->"
+    Eq.eq_sym_tex = r"\rightarrow"
+    assert eq.eq_sym_str == "->"
+    assert 'eq_sym_str' not in eq.__dict__  # not an *instance* attribute
+    assert str(eq) == "x -> y"
+    tex = eq._repr_latex_()
+    assert tex == '\\begin{equation}\n  x \\rightarrow y\n\\end{equation}\n'
+    Eq.eq_sym_str = "="
+    Eq.eq_sym_tex = "="
+
+    # unsetting class attribute
+    assert str(eq) == "x = y"
+    tex = eq._repr_latex_()
+    assert tex == '\\begin{equation}\n  x = y\n\\end{equation}\n'
+
+    # instance attribute
+    eq = Eq(x, y)
+    eq.eq_sym_str = "->"
+    eq.eq_sym_tex = r"\rightarrow"
+    assert 'eq_sym_str' in eq.__dict__
+    assert str(eq) == "x -> y"
+    assert str(Eq(x, y)) == "x = y"  # new instances not affected
+    tex = eq._repr_latex_()
+    assert tex == '\\begin{equation}\n  x \\rightarrow y\n\\end{equation}\n'
+
+    # multiline
+    eq = eq.apply_to_rhs('subs', {y: 1}).tag(1)
+    assert 'eq_sym_str' in eq.__dict__
+    assert str(eq) == "x -> y\n  -> 1    (1)"
+    tex = eq._repr_latex_()
+    assert (
+        tex
+        == '\\begin{align}\n  x &= y\\\\\n   &\\rightarrow 1\n\\tag{1}\\end{align}\n'
+    )
+
+    # init argument
+    assert str(Eq(x, y, eq_sym_str="->")) == "x -> y"
+    tex = Eq(x, y, eq_sym_tex=r"\rightarrow")._repr_latex_()
+    assert tex == '\\begin{equation}\n  x \\rightarrow y\n\\end{equation}\n'
+
+    assert str(Eq(x, y)) == "x = y"
+    tex = Eq(x, y)._repr_latex_()
+    assert tex == '\\begin{equation}\n  x = y\n\\end{equation}\n'
