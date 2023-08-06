cbcd847234a5ead80af30284fe3219769f13903c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:12 +0000

    Add package opennem/3.2.0a4/opennem-3.2.0a4.tar.gz

---
+++ packages/opennem/opennem-3.2.0a4.tar.gz/opennem-3.2.0a4/opennem/settings/__init__.py
@@ -0,0 +1,50 @@
+"""
+    settings files - read settings from env
+
+
+    will read in order:
+
+        * .env
+        * .env.{environment}
+        * system env
+        * pydantic settings
+
+"""
+
+
+import logging.config
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+from opennem.settings.log import LOGGING_CONFIG
+from opennem.settings.utils import load_env_file
+
+from .schema import OpennemSettings
+
+if Path(".env").is_file():
+    load_dotenv()
+
+MODULE_DIR = os.path.dirname(__file__)
+
+ENV = os.getenv("ENV", default="development")
+
+_env_file = load_env_file(ENV)
+
+# @TODO add logging
+if _env_file:
+    load_dotenv(dotenv_path=_env_file, override=True)
+
+# @NOTE don't use pydantics env file support since it doesn't support multiple
+settings: OpennemSettings = OpennemSettings()
+
+
+# setup logging
+
+if LOGGING_CONFIG:
+    if settings.env == "development":
+        LOGGING_CONFIG["root"]["level"] = "DEBUG"
+        LOGGING_CONFIG["loggers"]["opennem"]["level"] = "DEBUG"
+
+    logging.config.dictConfig(LOGGING_CONFIG)
