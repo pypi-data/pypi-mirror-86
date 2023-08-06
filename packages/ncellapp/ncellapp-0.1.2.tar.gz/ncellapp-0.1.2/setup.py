1f58f81514a7afe49c0f39e19e109628b615ebae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:36 +0000

    Add package ncellapp/0.1.2/ncellapp-0.1.2.tar.gz

---
+++ packages/ncellapp/ncellapp-0.1.2.tar.gz/ncellapp-0.1.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setuptools.setup(
+    name="ncellapp", 
+    version="0.1.2",
+    author="Hemanta Pokharel",
+    author_email="hemantapkh@yahoo.com",
+    description="Unofficial Python API Wrapper for Ncell",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    install_requires=["requests","pycryptodome"],
+    url="https://github.com/hemantapkh/ncellapp",
+    project_urls={
+        "Issue tracker": "https://github.com/hemantapkh/ncellapp/issues",
+      },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+)
