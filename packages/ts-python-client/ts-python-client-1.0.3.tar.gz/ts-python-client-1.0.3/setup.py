92f68841c6098861fe5fc43467d2b462f048d6a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:46 +0000

    Add package ts-python-client/1.0.3/ts-python-client-1.0.3.tar.gz

---
+++ packages/ts-python-client/ts-python-client-1.0.3.tar.gz/ts-python-client-1.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='ts-python-client',
+
+    packages=['ts_python_client'],
+
+    version='1.0.3',
+
+    description='A python client for TrustSource (https://app.trustsource.io) to manage open source code compliance',
+
+    author='EACG GmbH',
+
+    license='ASL-2.0',
+
+    url='https://github.com/trustsource/ts-python-client.git',
+
+    download_url='',
+
+    keywords=['scanning', 'dependencies', 'modules', 'TrustSource'],
+
+    classifiers=[],
+
+    install_requires=['requests'],
+
+    scripts=[],
+
+    entry_points={}
+)
