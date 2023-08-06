53e4460913f96184cfc8bd23fa451e3e1e7c3967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:47 +0000

    Add package django-dynamic-storages/0.5.2/django-dynamic-storages-0.5.2.tar.gz

---
+++ packages/django-dynamic-storages/django-dynamic-storages-0.5.2.tar.gz/django-dynamic-storages-0.5.2/dynamic_storages/tests/fields/test_encrypted_fields.py
@@ -0,0 +1,34 @@
+import glob
+import os
+import random
+
+from django.core.management.utils import get_random_secret_key
+from test_plus.test import TestCase
+
+from .. import TEST_FILES_DIR, TEST_IMAGES_DIR
+from ..models import TestEncryptedFileFieldModel, TestEncryptedImageFieldModel
+from .util import open_file
+
+
+class TestEncryptedFileField(TestCase):
+    def setUp(self):
+        self.files = list(glob.glob(os.path.join(TEST_FILES_DIR, "*")))
+
+    def test_create(self):
+        f = TestEncryptedFileFieldModel()
+        f.file.save(*open_file(self.files.pop()), save=True)
+        f.save()
+        self.assertIsNotNone(f.pk)
+        return f
+
+
+class TestEncryptedImageField(TestCase):
+    def setUp(self):
+        self.files = list(glob.glob(os.path.join(TEST_IMAGES_DIR, "*")))
+
+    def test_create(self):
+        img = TestEncryptedImageFieldModel()
+        img.image.save(*open_file(self.files.pop()), save=True)
+        img.save()
+        self.assertIsNotNone(img.pk)
+        return img
