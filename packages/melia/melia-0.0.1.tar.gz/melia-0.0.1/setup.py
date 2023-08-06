c2dd284bcd8d6ef397d0f0a766e2fd5069ea47a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:55 +0000

    Add package melia/0.0.1/melia-0.0.1.tar.gz

---
+++ packages/melia/melia-0.0.1.tar.gz/melia-0.0.1/setup.py
@@ -0,0 +1,27 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE/'README.md').read_text()
+
+setup(
+    name='melia',
+    version='0.0.1',
+    description='Meta-linguistic abstractions',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    author='dindefi',
+    author_email='dindefi@gmail.com',
+    licence='Apache 2.0',
+    include_package_data=True,
+    pacakge_dir={'melia': 'src'},
+    pacakages=['melia'],
+    entry_points={
+        'console_scripts': [
+            'melia=melia.main:start',
+        ]  
+    }
+)
+
+
