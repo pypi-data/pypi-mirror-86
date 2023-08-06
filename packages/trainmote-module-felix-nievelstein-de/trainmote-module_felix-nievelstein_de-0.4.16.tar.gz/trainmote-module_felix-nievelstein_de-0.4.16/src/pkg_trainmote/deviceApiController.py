0eadcebdbacced22ed8bab710db3859d4a686748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:53 +0000

    Add package trainmote-module-felix-nievelstein-de/0.4.16/trainmote-module_felix-nievelstein_de-0.4.16.tar.gz

---
+++ packages/trainmote-module-felix-nievelstein-de/trainmote-module_felix-nievelstein_de-0.4.16.tar.gz/trainmote-module_felix-nievelstein_de-0.4.16/src/pkg_trainmote/deviceApiController.py
@@ -0,0 +1,35 @@
+from flask import Blueprint
+from flask import request
+from flask import abort
+from flask import Response
+from . import baseAPI
+from . import deviceController
+from . import gpioservice
+import json
+
+
+deviceApiBlueprint = Blueprint('deviceApi', __name__)
+
+@deviceApiBlueprint.route('/trainmote/api/v1/device/restart', methods=["POST"])
+def restartDevice():
+    try:
+        deviceController.restartAfter(2)
+        gpioservice.clean()
+        return "", 200
+    except PermissionError as e:
+        return json.dumps({"error": str(e)}), 401, baseAPI.defaultHeader()
+
+@deviceApiBlueprint.route('/trainmote/api/v1/device/shutdown', methods=["POST"])
+def shutdownDevice():
+    try:
+        deviceController.shutdownAfter(2)
+        gpioservice.clean()
+        return "", 200
+    except PermissionError as e:
+        return json.dumps({"error": str(e)}), 401, baseAPI.defaultHeader()
+
+@deviceApiBlueprint.route('/trainmote/api/v1/device/update', methods=["POST"])
+def updateDevice():    
+    deviceController.update()
+    gpioservice.clean()
+    return "", 200
