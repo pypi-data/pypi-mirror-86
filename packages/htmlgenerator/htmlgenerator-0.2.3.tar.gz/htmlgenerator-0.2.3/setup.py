124eb3a8731b9b2ddb347e69ff3bc2cd3a3a5d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:27 +0000

    Add package htmlgenerator/0.2.3/htmlgenerator-0.2.3.tar.gz

---
+++ packages/htmlgenerator/htmlgenerator-0.2.3.tar.gz/htmlgenerator-0.2.3/setup.py
@@ -0,0 +1,40 @@
+from setuptools import find_packages, setup
+
+with open("README.md") as f:
+    long_description = f.read()
+
+with open("htmlgenerator/__init__.py") as f:
+    # magic n stuff
+    version = (
+        [i for i in f.readlines() if "__version__" in i][-1]
+        .split("=", 1)[1]
+        .strip()
+        .strip('"')
+    )
+
+setup(
+    name="htmlgenerator",
+    description="Declarative HTML templating system with lazy rendering",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/basxsoftwareassociation/htmlgenerator",
+    author="basx Software Association",
+    author_email="sam@basx.dev",
+    version=version,
+    license="New BSD License",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+)
