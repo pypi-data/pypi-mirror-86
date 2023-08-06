dff89d6ede4bbc026713d0fedda9adecf10559ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:03 +0000

    Add package mattflow/1.3.6/mattflow-1.3.6-py3-none-any.whl

---
+++ packages/mattflow/mattflow-1.3.6-py3-none-any.whl/mattflow/__init__.py
@@ -0,0 +1,22 @@
+# __init__.py is part of MattFlow
+#
+# MattFlow is free software; you may redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version. You should have received a copy of the GNU
+# General Public License along with this program. If not, see
+# <https://www.gnu.org/licenses/>.
+#
+# (C) 2019 Athanasios Mattas
+# ======================================================================
+"""Some special variables lie here"""
+
+
+__name__ = 'mattflow'
+__version__ = '1.3.6'
+__author__ = 'Athanasios Mattas'
+__author_email__ = 'thanasismatt@gmail.com'
+__description__ = 'A CFD python package for the shallow water equations'
+__url__ = 'https://github.com/ThanasisMattas/mattflow.git'
+__license__ = 'GNU General Public License v3'
+__copyright__ = 'Copyright 2019 Athanasios Mattas'
