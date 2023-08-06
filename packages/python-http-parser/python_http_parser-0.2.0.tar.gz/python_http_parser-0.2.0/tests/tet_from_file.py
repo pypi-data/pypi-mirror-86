35cba700f352ea582f9702423c72f340dbecb144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:08 +0000

    Add package python-http-parser/0.2.0/python_http_parser-0.2.0.tar.gz

---
+++ packages/python-http-parser/python_http_parser-0.2.0.tar.gz/python_http_parser-0.2.0/tests/tet_from_file.py
@@ -0,0 +1,14 @@
+"""Tests with content from files."""
+
+from .context import python_http_parser
+
+requests = []
+responses = []
+
+with open() as file:
+  pass
+with open() as file:
+  pass
+
+def test_rand_response():
+  pass
