68ee9ca4f106aef58bc0dabf55136eb418ba7659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:11 +0000

    Add package swachhdata/0.1.7b1/swachhdata-0.1.7b1-py3-none-any.whl

---
+++ packages/swachhdata/swachhdata-0.1.7b1-py3-none-any.whl/swachhdata/text/_base.py
@@ -0,0 +1,28 @@
+class TextFormatter:
+    """
+    Base Data Formatter for all recasts in swachhdata.text module
+
+    Accepted text input format:
+        * string
+        * list of string
+        * pandas.core.series.Series
+    """
+
+    def __init__(self):
+        
+        self._dtype = None
+        self._text = None
+        self._count = None
+
+    
+    def __text_formatter(self):
+        
+        self._count = len(self._text)
+
+        if self._dtype == pandas.core.series.Series:
+            self._text = self._text.tolist()
+            self._dtype = type(self._text)
+
+        if self._dtype == str:
+            self._count = len(self._text.split())
+            self._dtype = type(self._text)
