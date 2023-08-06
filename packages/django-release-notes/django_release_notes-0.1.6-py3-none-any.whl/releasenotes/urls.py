5fa7c51d4a1138692d577b86b6696c52bae5600f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:11 +0000

    Add package django-release-notes/0.1.6/django_release_notes-0.1.6-py3-none-any.whl

---
+++ packages/django-release-notes/django_release_notes-0.1.6-py3-none-any.whl/releasenotes/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from releasenotes import views
+
+app_name = "releasenotes"
+
+urlpatterns = [
+    path("<slug:project_slug>/", views.ReleaseNotesProjectView.as_view(), name="project-details"),
+    path("<slug:project_slug>/<str:release_slug>/", views.ReleaseNotesDetailView.as_view(), name="release-details"),
+]
