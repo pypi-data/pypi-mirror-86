70d79ccc365b6fca11c2d9b1ef6fc8e90f694b1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:31 +0000

    Add package oso/0.8.1/oso-0.8.1-pp37-pypy37_pp73-macosx_10_9_x86_64.whl

---
+++ packages/oso/oso-0.8.1-pp37-pypy37_pp73-macosx_10_9_x86_64.whl/polar/__init__.py
@@ -0,0 +1,9 @@
+import json
+from pathlib import Path
+
+from .polar import Polar, polar_class
+from .query import Query, QueryResult
+from .variable import Variable
+from .predicate import Predicate
+from .partial import Partial
+from .expression import Expression, Pattern
