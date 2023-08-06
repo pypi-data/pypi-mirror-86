9f0efc16be016f2b12212da4275ff2f4ec79b0d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:30 +0000

    Add package cryptoasset-data-downloader/1.0.8/cryptoasset-data-downloader-1.0.8.tar.gz

---
+++ packages/cryptoasset-data-downloader/cryptoasset-data-downloader-1.0.8.tar.gz/cryptoasset-data-downloader-1.0.8/application/classes/coin_cls.py
@@ -0,0 +1,45 @@
+"""Provides a class for creating coin objects.
+
+    list of classes:
+        Coin
+    """
+import arrow
+
+
+class Coin:
+    """Provides a coin obj for exchanges
+    """
+
+    def __init__(self, exc, data):
+        """Constructor of Coin class
+
+        Args:
+            exc (obj) : exchange
+            data (dict): data of target coin
+        """
+        self.name = data['Name']
+        self.quote = data['Quote']
+        self.base = data['Base']
+        self.start_date = arrow.get(
+            f"{data['StartDate']} {data['StartHour']}",
+            'DD-MM-YYYY HH:mm:ss')
+        self.end_date = arrow.get(
+            f"{data['EndDate']} {data['EndHour']}",
+            'DD-MM-YYYY HH:mm:ss')
+        self.frequency = data['Frequency'].replace('\n', '')
+        self.last_update = data['LastUpdate']
+        self.file_name = '{}_{}_{}_{}_{}_{}.csv'.format(
+            self.name,
+            self.quote,
+            self.base,
+            self.frequency,
+            exc.name,
+            self.start_date.format('DD-MM-YYYY'))
+
+    def __str__(self):
+        """Provies readable representation of coin obj.
+
+        Returns:
+            str: readable representation of coin
+        """
+        return f'{self.name} object'
