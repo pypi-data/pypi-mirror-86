c032570f93b97e2cbf7c2beeea84dad471e5f93f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:43 +0000

    Add package pynubank/2.7.2/pynubank-2.7.2.tar.gz

---
+++ packages/pynubank/pynubank-2.7.2.tar.gz/pynubank-2.7.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pynubank',
+    version='2.7.2',
+    url='https://github.com/andreroggeri/pynubank',
+    author='André Roggeri Campos',
+    author_email='a.roggeri.c@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    package_data={'pynubank': ['queries/*.gql', 'utils/mocked_responses/*.json']},
+    install_requires=['requests', 'qrcode', 'pyOpenSSL', 'colorama', 'requests-pkcs12'],
+    entry_points={
+        'console_scripts': [
+            'pynubank = pynubank.cli:main'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ]
+)
