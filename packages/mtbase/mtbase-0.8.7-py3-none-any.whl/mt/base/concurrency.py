14899e0f4158f2b8973976c385847e8a47463115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:12 +0000

    Add package mtbase/0.8.7/mtbase-0.8.7-py3-none-any.whl

---
+++ packages/mtbase/mtbase-0.8.7-py3-none-any.whl/mt/base/concurrency.py
@@ -0,0 +1,68 @@
+'''Concurrency in dask way. Needed for streaming workloads.'''
+
+import multiprocessing as _mp
+from dask.distributed import Client
+from distributed.client import Future
+from . import home_dirpath
+from .path import join, make_dirs
+from .deprecated import deprecated_func
+
+
+__all__ = ['get_dd_client', 'reset_dd_client', 'bg_run', 'is_future', 'max_num_threads', 'Counter']
+
+
+def get_dd_client():
+    '''Gets the dask.distributed client created internally.'''
+    if get_dd_client.client is None:
+        home_dd_dirpath = join(home_dirpath, 'dask-worker-space')
+        make_dirs(home_dd_dirpath)
+        get_dd_client.client = Client(local_dir=home_dd_dirpath)
+    return get_dd_client.client
+get_dd_client.client = None
+
+def reset_dd_client():
+    '''Removes the dask.distributed client explicitly.'''
+    if get_dd_client.client is not None:
+        get_dd_client.client.close()
+        get_dd_client.client = None
+
+def max_num_threads(client=None, use_dask=True):
+    '''Retrieves the maximum number of threads the client can handle concurrently. 
+
+    Parameters
+    ----------
+    client : dask.distributed.Client, optional
+        If 'use_dask' is True, this argument specifies the dask distributed client. It uses the default dask.distributed client if None is given.
+    use_dask : bool
+        whether or not we use dask distributed to count the number of threads. If not, we use :func:`multiprocessing.cpu_count`.
+    '''
+    if not use_dask:
+        return _mp.cpu_count()
+
+    if client is None:
+        client = get_dd_client()
+    return sum(client.nthreads().values())
+
+def bg_run(func, *args, **kwargs):
+    '''Runs a function in dask.distributed client's background and return a future object.'''
+    return get_dd_client().submit(func, *args, **kwargs)
+
+def is_future(obj):
+    '''Checks if an object is a dask Future object.'''
+    return isinstance(obj, Future)
+
+
+class Counter(object):
+
+    '''Counter class without the race-condition bug'''
+
+    def __init__(self):
+        self.val = _mp.Value('i', 0)
+
+    def increment(self, n=1):
+        with self.val.get_lock():
+            self.val.value += n
+
+    @property
+    def value(self):
+        return self.val.value
