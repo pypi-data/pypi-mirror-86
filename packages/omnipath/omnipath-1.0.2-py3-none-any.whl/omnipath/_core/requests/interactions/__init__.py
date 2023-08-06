554ba15c545bc8b367c67989edf27766f5d028a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:23 +0000

    Add package omnipath/1.0.2/omnipath-1.0.2-py3-none-any.whl

---
+++ packages/omnipath/omnipath-1.0.2-py3-none-any.whl/omnipath/_core/requests/interactions/__init__.py
@@ -0,0 +1,14 @@
+from omnipath._core.requests.interactions._utils import import_intercell_network
+from omnipath._core.requests.interactions._interactions import (
+    TFmiRNA,
+    Dorothea,
+    OmniPath,
+    TFtarget,
+    KinaseExtra,
+    LigRecExtra,
+    PathwayExtra,
+    AllInteractions,
+    Transcriptional,
+    miRNA,
+    lncRNAmRNA,
+)
