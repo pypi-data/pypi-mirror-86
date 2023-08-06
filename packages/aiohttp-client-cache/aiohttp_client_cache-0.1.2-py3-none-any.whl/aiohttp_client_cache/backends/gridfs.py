81a33e4256bccfe5fe60cab3e46390770884a9b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:13 +0000

    Add package aiohttp-client-cache/0.1.2/aiohttp_client_cache-0.1.2-py3-none-any.whl

---
+++ packages/aiohttp-client-cache/aiohttp_client_cache-0.1.2-py3-none-any.whl/aiohttp_client_cache/backends/gridfs.py
@@ -0,0 +1,67 @@
+import pickle
+from typing import Iterable, Optional
+
+from gridfs import GridFS
+from pymongo import MongoClient
+
+from aiohttp_client_cache.backends import BaseCache, CacheController, ResponseOrKey
+from aiohttp_client_cache.backends.mongo import MongoDBCache
+
+
+class GridFSController(CacheController):
+    """An async-compatible interface for caching objects in MongoDB GridFS.
+    Use this if you need to support documents greater than 16MB.
+    """
+
+    def __init__(self, cache_name: str, *args, connection: MongoClient = None, **kwargs):
+        super().__init__(cache_name, *args, **kwargs)
+        self.responses = GridFSCache(cache_name, connection)
+        self.keys_map = MongoDBCache(cache_name, 'http_redirects', self.responses.connection)
+
+
+# TODO: Incomplete/untested
+class GridFSCache(BaseCache):
+    """A dictionary-like interface for MongoDB GridFS
+
+    Args:
+        db_name: database name (be careful with production databases)
+        connection: MongoDB connection instance to use instead of creating a new one
+    """
+
+    def __init__(self, db_name, connection: MongoClient = None):
+        self.connection = connection or MongoClient()
+        self.db = self.connection[db_name]
+        self.fs = GridFS(self.db)
+
+    # TODO
+    async def contains(self, key: str) -> bool:
+        raise NotImplementedError
+
+    async def clear(self):
+        self.db['fs.files'].drop()
+        self.db['fs.chunks'].drop()
+
+    async def delete(self, key: str):
+        res = self.fs.find_one({'_id': key})
+        if res is not None:
+            self.fs.delete(res._id)
+
+    async def keys(self) -> Iterable[str]:
+        return [d._id for d in self.fs.find()]
+
+    async def read(self, key: str) -> Optional[ResponseOrKey]:
+        result = self.fs.find_one({'_id': key})
+        if result is None:
+            raise KeyError
+        return pickle.loads(bytes(result.read()))
+
+    async def size(self) -> int:
+        return self.db['fs.files'].count()
+
+    # TODO
+    async def values(self) -> Iterable[ResponseOrKey]:
+        raise NotImplementedError
+
+    async def write(self, key: str, item: ResponseOrKey):
+        await self.delete(key)
+        self.fs.put(pickle.dumps(item, protocol=-1), **{'_id': key})
