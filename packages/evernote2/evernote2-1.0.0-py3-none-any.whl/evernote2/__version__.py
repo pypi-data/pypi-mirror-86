a74247ff4aa79ac419f11af0a9ed8b8c5b75ef41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:36 +0000

    Add package evernote2/1.0.0/evernote2-1.0.0-py3-none-any.whl

---
+++ packages/evernote2/evernote2-1.0.0-py3-none-any.whl/evernote2/__version__.py
@@ -0,0 +1,9 @@
+__title__ = 'evernote2'
+__description__ = 'another Evernote SDK for Python'
+__url__ = 'https://github.com/JackonYang/evernote2'
+__version__ = '1.0.0'
+# __build__ = 0x022500
+__author__ = 'Jackon Yang'
+__author_email__ = 'i@jackon.me'
+__license__ = 'BSD'
+__copyright__ = 'Copyright 2020 Jackon Yang'
