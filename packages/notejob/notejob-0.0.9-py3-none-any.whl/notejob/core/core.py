7e5a453ed6cc9053981ae4a090bbd508390b820a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:02 +0000

    Add package notejob/0.0.9/notejob-0.0.9-py3-none-any.whl

---
+++ packages/notejob/notejob-0.0.9-py3-none-any.whl/notejob/core/core.py
@@ -0,0 +1,42 @@
+
+import argparse
+
+from notebuild.shell import run_shell, run_shell_list
+from notetool.tool.log import log
+
+logger = log("notebuild")
+
+
+class JobScheduler:
+    def __init__(self):
+        pass
+
+    def start(self):
+        print("start")
+
+    def stop(self):
+        print("stop")
+
+    def restart(self):
+        self.stop()
+        self.start()
+
+
+def command_line_parser():
+    parser = argparse.ArgumentParser(description="Test")
+    parser.add_argument('command')
+    args = parser.parse_args()
+    return args
+
+
+def notejob():
+    args = command_line_parser()
+    job = JobScheduler()
+    if args.command == 'run':
+        job.start()
+    elif args.command == 'start':
+        job.start()
+    elif args.command == 'stop':
+        job.stop()
+    elif args.command == 'restart':
+        job.restart()
