a73075ba68e8e72370388759ed1b2b7f108df40f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:07 +0000

    Add package stuart-dataops/0.1.1/stuart_dataops-0.1.1.tar.gz

---
+++ packages/stuart-dataops/stuart_dataops-0.1.1.tar.gz/stuart_dataops-0.1.1/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="stuart_dataops", # Replace with your own username
+    version="0.1.1",
+    author="Lan Row KOK",
+    author_email="r.kok@stuart.com",
+    description="Package used by the French Dataops team of Stuart",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/StuartApp/ops-aws-scripts/tree/master/fr-scripts/stuart_dataops",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2',
+    py_modules=['dataops']
+)
+
+
+# to refresh the wheel package do:
+# python3.7 setup.py sdist bdist_wheel
+
+# to new version to pypi
+# twine upload --repository-url https://upload.pypi.org/legacy/ --skip-existing dist/*
