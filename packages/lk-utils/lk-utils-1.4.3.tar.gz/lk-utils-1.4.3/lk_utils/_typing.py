64cdb72318750280ed9fa112e8ad77662b6edf2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:28 +0000

    Add package lk-utils/1.4.3/lk-utils-1.4.3.tar.gz

---
+++ packages/lk-utils/lk-utils-1.4.3.tar.gz/lk-utils-1.4.3/lk_utils/_typing.py
@@ -0,0 +1,69 @@
+"""
+@Author   : Likianta <likianta@foxmail.com>
+@FileName : _typing.py
+@Version  : 0.1.3
+@Created  : 2020-11-17
+@Updated  : 2020-11-22
+@Desc     : 
+"""
+from typing import *
+
+
+class ExcelWriterHint:
+    from xlsxwriter.format import Format as _Format
+    from xlsxwriter.workbook import Workbook as _Workbook
+    from xlsxwriter.workbook import Worksheet as _Worksheet
+    
+    Rowx = int
+    Colx = int
+    Cell = Tuple[Rowx, Colx]
+    
+    CellFormat = _Format
+    CellValue = Union[None, bool, float, int, str]
+    RowValues = Iterable[CellValue]
+    ColValues = Iterable[CellValue]
+    Header = List[CellValue]
+    
+    RowsValues = List[RowValues]
+    ColsValues = List[ColValues]
+    
+    WorkBook = _Workbook
+    WorkSheet = _Worksheet
+    Sheetx = Union[int, str]
+    SheetName = Union[str, None]
+    SheetManager = Dict[Union[str, int], Union[List[str], dict]]
+    ''' {
+            'sheets': [str sheet_name, ...],
+            0: {'sheet_name': str, 'sheetx': int, 'rowx': int,
+                'header': Header, ...},
+            ...
+        }
+    '''
+    
+
+class FilesniffHint:
+    File = str
+    FileName = str
+    FilePath = str
+    
+    FileNames = List[FileName]
+    FilePaths = List[FilePath]
+    
+    FileDict = Dict[FilePath, FileName]
+    FileZip = Iterable[Tuple[FilePath, FileName]]
+    FileDualList = Tuple[FilePaths, FileNames]
+    
+    FinderReturn = Union[FilePaths, FileNames, FileDict, FileZip, FileDualList]
+    
+    Suffix = Union[str, tuple]
+
+
+class ReadAndWriteHint:
+    PlainFileTypes = ('.txt', '.html', '.md', '.rst', '.htm', '.ini')
+    StructFileTypes = ('.json', '.json5', '.yaml')
+    BinaryFileTypes = ('.xlsx', '.xls', '.pdf')
+    
+    LoadedData = Union[str, list, dict]
+    SerializableData = Union[str, list, tuple, dict, None]
+    SerialikableData = Union[set, Generator]
+    DumpableData = Union[SerializableData, SerialikableData]
---
+++ packages/lk-utils/lk-utils-1.4.3.tar.gz/lk-utils-1.4.3/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['lk_utils', 'lk_utils.depreciated', 'lk_utils.resource.metadata']
+
+package_data = \
+{'': ['*'],
+ 'lk_utils': ['resource/*'],
+ 'lk_utils.resource.metadata': ['more/*']}
+
+install_requires = \
+['bs4>=0.0.1,<0.0.2',
+ 'lk-logger>=3.5,<4.0',
+ 'requests>=2.24,<3.0',
+ 'xlrd>=1.2,<2.0',
+ 'xlsxwriter>=1.3,<2.0']
+
+setup_kwargs = {
+    'name': 'lk-utils',
+    'version': '1.4.3',
+    'description': 'LK Utils is made for data processing.',
+    'long_description': None,
+    'author': 'Likianta',
+    'author_email': 'likianta@foxmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
