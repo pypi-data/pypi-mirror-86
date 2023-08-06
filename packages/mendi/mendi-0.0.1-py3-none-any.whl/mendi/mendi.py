e567590c524423b91d6995e2b15fe14bf59a7f20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:10 +0000

    Add package mendi/0.0.1/mendi-0.0.1-py3-none-any.whl

---
+++ packages/mendi/mendi-0.0.1-py3-none-any.whl/mendi/mendi.py
@@ -0,0 +1,42 @@
+''' mendi '''
+
+import os
+from tabulate import tabulate
+
+
+def clear_screen() -> None:
+    ''' Clears the current screen '''
+    input('ENTER to continue: ')  # wait for user to see current screen
+    if os.name == 'posix':
+        # for Linux and Mac
+        os.system('clear')
+    else:
+        # for Windows
+        os.system('cls')
+
+
+def drive_menu(heading: str, menus: dict) -> None:
+    ''' The driver of menus
+
+    Args:
+        heading (str): A suitable heading of your choice
+        menus (dict): Dictionary of menus, where key is choice no. and value is another dictionary containing `desc` and `func`
+    '''
+    table = [[ch, menu['desc']] for ch, menu in menus.items()]
+    menu_chart = f'''
+        MENU for {heading}
+    \n{tabulate(table,tablefmt='fancy_grid',headers=['Choice','Description'])}
+    Enter your choice or X to quit
+    \n>>> '''
+    choice = ''
+    while choice != 'X':
+        clear_screen()
+        choice = input(menu_chart)
+        if choice in menus.keys():
+            val = menus[choice]['func']()
+            if val:
+                print(val)
+        elif choice == 'X':
+            print('Bye 🤗')
+        else:
+            print('INVALID CHOICE')
