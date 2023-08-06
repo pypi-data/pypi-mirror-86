2d1875134b77b8ea56ea7b568e15df6f6ce30276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:55 +0000

    Add package avwx-engine/1.5.7/avwx_engine-1.5.7-py3-none-any.whl

---
+++ packages/avwx-engine/avwx_engine-1.5.7-py3-none-any.whl/avwx/exceptions.py
@@ -0,0 +1,21 @@
+"""
+Contains avwx custom exceptions
+"""
+
+
+class BadStation(Exception):
+    """
+    Station does not exist
+    """
+
+
+class InvalidRequest(Exception):
+    """
+    Unable to fetch data
+    """
+
+
+class SourceError(Exception):
+    """
+    Source servers returned an error code
+    """
