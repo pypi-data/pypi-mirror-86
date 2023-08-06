1314c6dddf8c0f096de84cf3fedc06d2525c863f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:41 +0000

    Add package Discord-Anti-Spam/0.2.3/Discord%20Anti-Spam-0.2.3.tar.gz

---
+++ packages/Discord-Anti-Spam/Discord%20Anti-Spam-0.2.3.tar.gz/Discord Anti-Spam-0.2.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Discord Anti-Spam",
+    version="0.2.3",
+    author="Skelmis",
+    author_email="ethan@koldfusion.xyz",
+    description="An easy to use package for anti-spam features in discord.py.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Skelmis/DPY-Anti-Spam",
+    packages=setuptools.find_packages(),
+    install_requires=["fuzzywuzzy>=0.18", "discord.py>=1"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+    ],
+    python_requires=">=3.6",
+)
