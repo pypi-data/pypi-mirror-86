058ac037fdcfb60db6f800da0aa3bf967502a731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:46 +0000

    Add package AirDataUAV/0.0.3/AirDataUAV-0.0.3.tar.gz

---
+++ packages/AirDataUAV/AirDataUAV-0.0.3.tar.gz/AirDataUAV-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AirDataUAV",
+    version="0.0.3",
+    author="Jan-Hendrik Ewers",
+    author_email="jh.ewers@gmail.com",
+    description="A nice interface to AirDataUAV CSV files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/iwishiwasaneagle/AirDataUAV",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+)
