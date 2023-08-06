5fc76b9c1d3c9006b660f58554eb5addf7004242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:20 +0000

    Add package benzema/0.1.2/benzema-0.1.2.tar.gz

---
+++ packages/benzema/benzema-0.1.2.tar.gz/benzema-0.1.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['benzema']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'benzema',
+    'version': '0.1.2',
+    'description': 'A straight forward and easy to use bencoding library written in Python 3',
+    'long_description': None,
+    'author': 'fishr',
+    'author_email': 'karim@fishr.xyz',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
