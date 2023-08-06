2287f51b7f05852cfd8c8e39b97a3496a4d6bbd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:14 +0000

    Add package zebrazoom/0.25/zebrazoom-0.25.tar.gz

---
+++ packages/zebrazoom/zebrazoom-0.25.tar.gz/zebrazoom-0.25/zebrazoom/__main__.py
@@ -0,0 +1,28 @@
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(dir_path)
+print("The data produced by ZebraZoom can be found in the folder: " + os.path.join(dir_path,'ZZoutput'))
+
+import sys
+sys.path.insert(1, './')
+sys.path.insert(1, './code/')
+sys.path.insert(1, './code/GUI/')
+sys.path.insert(1, './code/getImage/')
+sys.path.insert(1, './code/trackingFolder/')
+sys.path.insert(1, './code/trackingFolder/headTrackingHeadingCalculationFolder/')
+sys.path.insert(1, './code/trackingFolder/tailTrackingFunctionsFolder/')
+sys.path.insert(1, './code/trackingFolder/tailTrackingFunctionsFolder/tailTrackingExtremityDetectFolder/')
+sys.path.insert(1, './code/trackingFolder/tailTrackingFunctionsFolder/tailTrackingExtremityDetectFolder/findTailExtremeteFolder/')
+sys.path.insert(1, './dataAnalysis/')
+sys.path.insert(1, './dataAnalysis/dataanalysis/')
+sys.path.insert(1, './dataAnalysis/datasetcreation/')
+
+
+from vars import getGlobalVariables
+globalVariables = getGlobalVariables()
+
+from GUIAllPy import SampleApp
+
+if __name__ == "__main__":
+  app = SampleApp()
+  app.mainloop()
