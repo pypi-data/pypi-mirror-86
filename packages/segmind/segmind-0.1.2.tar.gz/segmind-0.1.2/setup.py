a73142c1b094c4ab2c05f43e1454d7f429fc40dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:53 +0000

    Add package segmind/0.1.2/segmind-0.1.2.tar.gz

---
+++ packages/segmind/segmind-0.1.2.tar.gz/segmind-0.1.2/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+import os
+from importlib.machinery import SourceFileLoader
+
+version = SourceFileLoader('segmind.version',
+                           os.path.join('segmind',
+                                        'version.py')).load_module().VERSION
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='segmind',
+    version=version,
+    author='T Pratik',
+    author_email='pratik@segmind.com',
+    description='A tracking tool for deep-learning',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    # url="https://github.com/pk00095/keras_jukebox/archive/0.0.3.tar.gz",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pycocotools', 'click', 'protobuf==3.13.0', 'lxml',
+        'pascal_voc_writer', 'GPUtil', 'PYyaml', 'pandas',
+        'entrypoints', 'psutil'
+    ],
+    entry_points={'console_scripts': ['segmind=segmind.cli:cli']},
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux'
+    ],
+    python_requires='>=3.6')
