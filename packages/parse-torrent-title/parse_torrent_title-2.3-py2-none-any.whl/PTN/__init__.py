168d3df8ebb08294453e779a93cdff4047acec46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:00 +0000

    Add package parse-torrent-title/2.3/parse_torrent_title-2.3-py2-none-any.whl

---
+++ packages/parse-torrent-title/parse_torrent_title-2.3-py2-none-any.whl/PTN/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import pkgutil
+import re
+import sys
+
+# Regex in python 2 is very slow so we check if the faster 'regex' library is available.
+faster_regex = pkgutil.find_loader('regex')
+if faster_regex is not None and sys.version_info[0] < 3:
+    re = faster_regex.load_module('regex')
+
+from .parse import PTN
+
+__author__ = 'Giorgio Momigliano'
+__email__ = 'gmomigliano@protonmail.com'
+__version__ = '2.3'
+__license__ = 'MIT'
+
+ptn = PTN()
+
+
+def parse(name, standardise=True):
+    return ptn.parse(name, standardise)
