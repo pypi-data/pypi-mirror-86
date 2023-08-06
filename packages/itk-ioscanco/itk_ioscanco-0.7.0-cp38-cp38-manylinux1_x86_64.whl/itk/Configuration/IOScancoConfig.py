bfc9e49747453ffaffbbfe0c0689622cd09c2afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:08 +0000

    Add package itk-ioscanco/0.7.0/itk_ioscanco-0.7.0-cp38-cp38-manylinux1_x86_64.whl

---
+++ packages/itk-ioscanco/itk_ioscanco-0.7.0-cp38-cp38-manylinux1_x86_64.whl/itk/Configuration/IOScancoConfig.py
@@ -0,0 +1,6 @@
+depends = ('ITKPyBase', 'ITKIOImageBase', )
+templates = (
+  ('ScancoImageIO', 'itk::ScancoImageIO', 'itkScancoImageIO', True),
+  ('ScancoImageIOFactory', 'itk::ScancoImageIOFactory', 'itkScancoImageIOFactory', True),
+)
+snake_case_functions = ()
