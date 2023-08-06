3ece6425c562be45df49dbda99baa07a7cc284d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:54 +0000

    Add package amocrm-api/2.0.1/amocrm_api-2.0.1.tar.gz

---
+++ packages/amocrm-api/amocrm_api-2.0.1.tar.gz/amocrm_api-2.0.1/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import re
+
+
+def get_version():
+    init_py = open('amocrm/__init__.py').read()
+    metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", init_py))
+    return metadata['version']
+
+version = get_version()
+
+
+setup(
+    name='amocrm_api',
+    version=version,
+    packages=find_packages(),
+    url='https://github.com/Krukov/amocrm_api',
+    download_url='https://github.com/Krukov/amocrm_api/tarball/%s' % version,
+    license='MIT license',
+    author='Dmitry Kryukov',
+    author_email='glebov.ru@gmail.com',
+    description='Python API for Amocrm',
+    long_description=open('README.rst').read(),
+    requires=[
+        'requests',
+        'pyjwt'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
