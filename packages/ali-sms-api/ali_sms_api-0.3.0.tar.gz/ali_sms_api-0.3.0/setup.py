eec8599a3565b40784109e2ce59ada948657160f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:02 +0000

    Add package ali-sms-api/0.3.0/ali_sms_api-0.3.0.tar.gz

---
+++ packages/ali-sms-api/ali_sms_api-0.3.0.tar.gz/ali_sms_api-0.3.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ali_sms_api']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiohttp>=3.7,<4', 'pydantic>=1.6,<2', 'requests>=2.24,<3']
+
+setup_kwargs = {
+    'name': 'ali-sms-api',
+    'version': '0.3.0',
+    'description': '阿里云 短信 SDK',
+    'long_description': '# 简介\n\n阿里云短信 SDK [官方文档](https://help.aliyun.com/document_detail/101339.html)\n\n## 注意:\n\n    这个接口仅仅是为了内部使用，虽然您可以获取它的源代码，但是请不要使用在您的项目中.\n    当前我们并不对兼容性以及可用性做任何保证。(如果需要使用请联系开发者获取支持)\n',
+    'author': 'dev',
+    'author_email': 'dev@qiyutech.tech',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
