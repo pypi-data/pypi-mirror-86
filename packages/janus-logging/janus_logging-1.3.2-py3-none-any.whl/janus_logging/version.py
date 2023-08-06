58e2bb9e511b572becbe4bb8dc5fd1b6dc2f1fcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:55 +0000

    Add package janus-logging/1.3.2/janus_logging-1.3.2-py3-none-any.whl

---
+++ packages/janus-logging/janus_logging-1.3.2-py3-none-any.whl/janus_logging/version.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# janus_logging.version
+'''
+:author:    madkote
+:contact:   madkote(at)bluewin.ch
+:copyright: Copyright 2020, madkote
+
+janus_logging.version
+---------------------
+Package version
+'''
+
+from __future__ import absolute_import
+
+VERSION = (1, 3, 2)
+
+__all__ = []
+__author__ = 'madkote <madkote(at)bluewin.ch>'
+__version__ = '.'.join(str(x) for x in VERSION)
+__copyright__ = 'Copyright 2019, madkote'
