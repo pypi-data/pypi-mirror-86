6cdb1b3cfa540817848d2f07d108adacb52fd1ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:53 +0000

    Add package python-myanmar/1.10.0/python-myanmar-1.10.0.tar.gz

---
+++ packages/python-myanmar/python-myanmar-1.10.0.tar.gz/python-myanmar-1.10.0/README.rst
@@ -0,0 +1,23 @@
+Introduction
+============
+
+
+.. image:: https://travis-ci.org/trhura/python-myanmar.svg
+     :alt: Build Status
+.. image:: https://api.codeclimate.com/v1/badges/51163159d264485328a5/test_coverage
+   :alt: Test Coverage
+.. image:: https://badge.fury.io/py/python-myanmar.svg
+    :alt: Pypi Version
+
+This package contains a variety of python modules for Myanmar text proccesing, such as *syllabification*,
+*romanization*, *encoding conversion*, *nrc validation* etc. Only python3 is currently supported at the moment.
+
+Installation
+============
+
+The package is distributed on PyPI and can be installed with pip::
+
+   pip install python-myanmar
+
+For more information, please read the full documentation
+`here <https://python-myanmar.readthedocs.io/en/latest/>`__.
---
+++ packages/python-myanmar/python-myanmar-1.10.0.tar.gz/python-myanmar-1.10.0/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+requirements = []
+
+setup_requirements = [
+    'pytest-runner',
+]
+
+test_requirements = [
+    'pytest',
+]
+
+setup(
+    author="Thura Hlaing",
+    author_email='trhura@gmail.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    description="Various Python Routines for Myanmar Language",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme,
+    include_package_data=True,
+    keywords='python-myanmar',
+    name='python-myanmar',
+    packages=find_packages(include=['myanmar']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/trhura/python-myanmar',
+    version='1.10.0',
+    zip_safe=False,
+    package_data={
+        'myanmar': ['data/*.json'],
+    },
+)
